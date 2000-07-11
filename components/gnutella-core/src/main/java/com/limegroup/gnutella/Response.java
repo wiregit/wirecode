f322ec970b308fd6a91229419b9e9439aaa03013
crohrs <crohrs>
2000-07-11 22:34:51 +0000

    *** empty log message ***

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/Response.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/Response.java
@@ -1,15 +1,58 @@
 package com.limegroup.gnutella;
 
 /** 
- * A query response record, i.e., a single response from a query search.
- * Create these to respond to a search.   This should have a full suite
- * of observers/mutators, but I'm too lazy. 
+ * A single result from a query reply message.
+ * Create these to respond to a search.   Immutable.
  */
 public class Response {
-    public int index;
-    public int size;
+    /** Both index and size must fit into 4 unsigned bytes; see
+     *  constructor for details. */
+    private long index;
+    private long size;
     /** The name of the file matching the search.  This does NOT
      *  include the double null terminator.
      */
-    public byte[] name;
+    private String name;
+
+    /** Creates a fresh new response. 
+      *
+      * @requires index and size can fit in 4 unsigned bytes, i.e.,
+      *  0 <= index, size < 2^32
+      */
+    public Response(long index, long size, String name) {
+	Assert.that((index & 0xFFFFFFFF00000000l)==0, 
+		    "Response constructor: index too big!");
+	Assert.that((size &  0xFFFFFFFF00000000l)==0, 
+		    "Response constructor: size too big!");
+
+	this.index=index;
+	this.size=size;
+	this.name=name;
+    }
+
+    public long getIndex() {
+	return index;
+    }
+
+    public long getSize() {
+	return size;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public boolean equals(Object o) {
+	if (! (o instanceof Response))
+	    return false;
+	Response r=(Response)o;
+	return r.getIndex()==getIndex() 
+	    && r.getSize()==getSize()
+	    && r.getName().equals(getName());
+    }
+
+    public int hashCode() {
+	//Good enough for the moment
+	return name.hashCode()+(int)size+(int)index;
+    }
 }
