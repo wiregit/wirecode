32145e3d10ba200df7d44315a329a24856bd2e16
crohrs <crohrs>
2000-07-11 01:52:51 +0000

    Representation is now just byte array. Handles signs and large values correctly.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/QueryRequest.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/QueryRequest.java
@@ -2,49 +2,74 @@ package com.limegroup.gnutella;
 
 import java.io.*;
 
-public class QueryRequest extends Message {    
-    private short minSpeed;
-    /** The query string itself, WITHOUT null terminator. */
-    private byte[] query;
+public class QueryRequest extends Message {   
+    /** The minimum speed and query request, including the null terminator.
+     *  We extract the minimum speed and String lazily. */
+    byte[] payload;
 
-    /** Build a new query from scratch */
-    public QueryRequest(byte ttl, short minSpeed, byte[] query) {
-	//2 bytes for min speed plus query string and null terminator
-	super(Message.F_QUERY, ttl, 2+query.length+1);
-	this.minSpeed=minSpeed;
-	int n=query.length+1;
-	this.query=query;
+    /**
+     * Builds a new query from scratch 
+     *
+     * @requires 0&<;=minSpeed&<;2^32 (i.e., can fit in 4 unsigned bytes)
+     */
+    public QueryRequest(byte ttl, long minSpeed, String query) {
+	//Allocate two bytes for min speed plus query string and null terminator
+	super(Message.F_QUERY, ttl, 2+query.length()+1);
+	payload=new byte[2+query.length()+1];
+	//Extract minimum speed.  It's ok if "(int)minSpeed" is negative.
+	ByteOrder.int2leb((int)minSpeed, payload, 0);
+	//Copy bytes from query string to payload
+	byte[] qbytes=query.getBytes();
+	System.arraycopy(qbytes,0,payload,2,qbytes.length);
+	//Null terminate it.
+	payload[payload.length-1]=(byte)0;
     }
 
     /*
-     * @effects Build a new query with given data
+     * Build a new query with data snatched from network
+     *
+     * @requires payload.length>=3
      */
     public QueryRequest(byte[] guid, byte ttl, byte hops,
 			byte[] payload) {
 	super(guid, Message.F_QUERY, ttl, hops, payload.length);
-	this.minSpeed=ByteOrder.leb2short(payload,0);
-	int n=payload.length-3;
-	this.query=new byte[n];
-	for (int i=0; i<n; i++)
-	    this.query[i]=payload[i+2];
+	this.payload=payload;
     }
 
     protected void writePayload(OutputStream out) throws IOException {
-	byte[] buf=new byte[2+query.length+1];
-	ByteOrder.short2leb(minSpeed, buf, 0);
-	for (int i=0; i<query.length; i++)
-	    buf[i+2]=query[i];
-	buf[buf.length-1]=(byte)0; //null terminate
-	for (int i=0; i<buf.length; i++) { //TODO3: buffer and send in batch.
-	    out.write(buf[i]);	
-	}
+	out.write(payload);
     }
     
+    public String getQuery() {
+	return new String(payload,2,payload.length-3);
+    }
+
+    /** 
+      * Note: the minimum speed can be represented as a 4-byte unsigned 
+      * number, but Java ints are signed.  Hence we must use a long.  The
+      * value returned is always smaller than 2^32.
+      */
+    public long getMinSpeed() {
+	int speed=ByteOrder.leb2int(payload,0);
+	long ret=ByteOrder.ubytes2long(speed);
+	Assert.that(ret>=0, "getMinSpeed got negative value");
+	return ret;
+    }
+
     public boolean isRequest() {
 	return true;
     }
-
+    
     public String toString() {
-	return "QueryRequest("+new String(query)+", "+super.toString()+")";
+	return "QueryRequest("+getQuery()+", "+getMinSpeed()
+	    +", "+super.toString()+")";
     }
+
+//      /** Unit test */
+//      public static void main(String args[]) {
+//  	long u4=0x00000000FFFFFFFFl;
+//  	QueryRequest qr=new QueryRequest((byte)3,u4,"query");
+//  	Assert.that(qr.getMinSpeed()==u4);
+//  	Assert.that(qr.getQuery().equals("query"));
+//      }
 }
