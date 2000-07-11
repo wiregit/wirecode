99085d1845bc57f0b5d17410761d9602d9bc5fd0
crohrs <crohrs>
2000-07-11 01:51:53 +0000

    Maintains the payload internally. Handles large values and signs properly.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/PingReply.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/PingReply.java
@@ -3,74 +3,92 @@ package com.limegroup.gnutella;
 import java.io.*;
 
 /**
- * A Gnutella ping reply, aka, "pong".
+ * A ping reply message, aka, "pong".
  */
 
 public class PingReply extends Message {
-    private short port;
-    /** The LITTLE-ENDIAN ip address of the host replying to a ping.
-     *  Invariant: ip.length==4. */
-    private byte[] ip;
-    private int files;
-    private int kbytes;
+    /** All the data.  We extract the port, ip address, number of files,
+     *  and number of kilobytes lazily. */
+    private byte[] payload;
     
-    /** Create a new PingReply from scratch */
+    /** 
+     * Create a new PingReply from scratch 
+     *
+     * @requires ip.length==4 and ip is in <i>big-endian</i> byte order,
+     *  0&<; port &<;2^16 (i.e., can fit in 2 unsigned bytes),
+     *  0&<; files and kbytes &<;2^32 (i.e., can fit in 4 unsigned bytes)
+     */
     public PingReply(byte[] guid, byte ttl, 
-		     short port, byte[] ip, int files, int kbytes) {
+		     int port, byte[] ip, long files, long kbytes) {
 	super(guid, Message.F_PING_REPLY, ttl, (byte)0, 14);
-	this.port=port;
-	this.ip=ip;
-	this.files=files;
-	this.kbytes=kbytes;
-    }
-
-    /** Wrap a PingReply around stuff snatched from the network. */
-    public PingReply(byte[] guid, byte ttl, byte hops, 
-		     byte[] payload) {
-	super(guid, Message.F_PING_REPLY, ttl, hops, 14);
-	//TODO1: Is port, file count, and file size really little-endian?
-	this.port=ByteOrder.leb2short(payload,0);
-	this.ip=new byte[4];
-	this.ip[0]=payload[2];
-	this.ip[1]=payload[3];
-	this.ip[2]=payload[4];
-	this.ip[3]=payload[5];
-	this.files=ByteOrder.leb2int(payload,6);
-	this.kbytes=ByteOrder.leb2int(payload,10);
-    }
-
-    protected void writePayload(OutputStream out) throws IOException {	
-	//TODO1: Is port, file count, and file size really little-endian?
-	byte[] buf=new byte[14];
-	ByteOrder.short2leb(port, buf, 0);
-	buf[2]=ip[0];
-	buf[3]=ip[1];
-	buf[4]=ip[2];
-	buf[5]=ip[3];
-	ByteOrder.int2leb(files, buf, 6);
-	ByteOrder.int2leb(kbytes, buf, 10);
-	for (int i=0; i<14; i++) { //TODO3: buffer and send in batch.
-	    out.write(buf[i]);	
-	}
-    }
-
-    public String toString() {
-	return "PingReply("+super.toString()+")";
+	this.payload=new byte[14];
+	//It's ok if casting port, files, or kbytes turns negative.
+	ByteOrder.short2leb((short)port, payload, 0);
+	//Payload stores IP in LITTLE-ENDIAN
+	payload[2]=ip[3];
+	payload[3]=ip[2];
+	payload[4]=ip[1];
+	payload[5]=ip[0];
+	ByteOrder.int2leb((int)files, payload, 6);
+	ByteOrder.int2leb((int)kbytes, payload, 9);
     }
 
     /** 
-     * Returns the ip field in dotted decimal format, e.g., 
-     * "127.0.0.1".
+     * Wrap a PingReply around stuff snatched from the network. 
+     *
+     * @requires payload.length==14
      */
-    public String getIP() {
-	return ip2string(ip);
+    public PingReply(byte[] guid, byte ttl, byte hops, 
+		     byte[] payload) {
+	super(guid, Message.F_PING_REPLY, ttl, hops, 14);
+	this.payload=payload;
     }
 
-    public int getFiles() {
-	return files;
+    protected void writePayload(OutputStream out) throws IOException {	
+	out.write(payload);
+    }
+
+    public String toString() {
+	return "PingReply("+getIP()+":"+getPort()
+	    +", "+super.toString()+")";
+    }
+
+    public int getPort() {
+	return	ByteOrder.ubytes2int(ByteOrder.leb2short(payload,0));
+    }
+
+    /** 
+     * Returns the ip field in standard dotted decimal format, e.g., 
+     * "127.0.0.1".  The most significant byte is written first.
+     */
+    public String getIP() {
+	byte[] ip=new byte[4];
+	ip[0]=payload[2];
+	ip[1]=payload[3];
+	ip[2]=payload[4];
+	ip[3]=payload[5];
+	return ip2string(ip); //takes care of byte order and signs
+    }
+
+    public long getFiles() {
+	return ByteOrder.ubytes2long(ByteOrder.leb2int(payload,6));
     }
     
-    public int getKbytes() {
-	return kbytes;
+    public long getKbytes() {
+	return ByteOrder.ubytes2long(ByteOrder.leb2int(payload,10));
     }
+
+//      /** Unit test */
+//      public static void main(String args[]) {
+//  	long u4=0x00000000FFFFFFFFl;
+//  	int u2=0x0000FFFF;
+//  	byte[] ip={(byte)0xFF, (byte)0x00, (byte)0x00, (byte)0x01};
+//  	PingReply pr=new PingReply(new byte[16], (byte)0,
+//  				   u2, ip, u4, u4);
+//  	Assert.that(pr.getPort()==u2);
+//  	Assert.that(pr.getFiles()==u4);
+//  	Assert.that(pr.getKbytes()==u4);
+//  	String ip2=pr.getIP();
+//  	Assert.that(ip2.equals("255.0.0.1"), ip2);
+//      }
 }
