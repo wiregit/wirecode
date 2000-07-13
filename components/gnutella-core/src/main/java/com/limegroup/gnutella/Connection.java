06466cd98098ba9b3505d40349c7e3a7e0304959
crohrs <crohrs>
2000-07-13 16:32:58 +0000

    1. Fixed synchronization problems regarding send/receive problems. 2. Changed specification so a connection manager is no longer needed.  This makes    the class more general purpose.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/Connection.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/Connection.java
@@ -12,40 +12,66 @@ import java.io.*;
  * details of routing.  The Message class (and subclasses)
  * actually does the tedious reading/writing of bytes from socket.<p>
  * 
- * Typical use:
+ * The class can be used in a number of ways.  The typical use, to
+ * handle a normal Gnutella connection, involves creating a
+ * ConnectionManager and a thread:<p>
+ *
  * <pre>
  *   Socket s=new Socket(...);
  *   Connection c=new Connection(.., s, false); //does handshake
- *   Thread t=new Thread(c);
- *   t.start(); //services connection 
+ *   Thread t=new Thread(c);       //create new handler thread
+ *   t.start();                    //services connection by calling run()
  * </pre>
+ *
+ * The second use is for "do it yourselfers".  This is useful for
+ * Gnutella spiders.  In this case, you don't pass a ConnectionManager
+ * as a constructor argument and don't call the run() method.
  */
 public class Connection implements Runnable { 
-    private ConnectionManager manager;
-    private RouteTable routeTable;
-    Socket sock;
+    private ConnectionManager manager=null; //may be null
+    private RouteTable routeTable=null;     //may be null
+    /** The underlying socket.  For thread synchronization reasons, it is important
+     *  that this only be modified by the send(m) and receive() methods. */
+    private Socket sock;
     private boolean incoming;
 
     /** The number of packets I sent and received.  This includes bad packets. */
     private int sent;
     private int received;
 
-    /** A stub for testing only! */
-    Connection() { }
 
-    /** @modifies network, manager
-     *  @effects creates an outgoing socket to the following host and port.
+    /** 
+     * Creates an outgoing connection with a fresh socket.
+     * 
+     * @modifies network, manager
+     * @effects creates an outgoing socket to the following host and port and does 
+     *   the Gnutella handshake.   Throws IOException if the connection couldn't be 
+     *   established.  If manager is non-null, also sends an initial PingRequest
+     *   and register this with manager.
+     *
+     * @param manager the manager managing me and my "sibling" connections, or null
+     *   if the run() method will not be called.
+     * @param host the address of the host to contact, e.g., "192.168.0.1" or
+     *   "gnutella.limegroup.com"
+     * @param port the port of the host to contact, e.g, 6346
+     * @exception IOException if the connection or the handshake failed
      */
-    public Connection(ConnectionManager manager, String host, int port) throws IOException {
+    public Connection(ConnectionManager manager, String host, int port) 
+	throws IOException {
 	this(manager, new Socket(host, port), false);
     }
 
     /** 
-     * @modifies network, modifies
-     * @effects Throws IOException if handshake failed.  Otherwise
-     *  adds this manager
+     * Creates an outgoing or incoming connection around an existing socket.
      *
-     * @param manager the manager managing me and my "sibling" connections
+     * @modifies network, manager
+     * @effects wraps a connection around sock and does the Gnutella handshake.
+     *  Throws IOException if the connection couldn't be established.
+     *  If manager is non-null, also registers this with manager and sends
+     *  an initial PingRequest if this is outgoing..
+     *
+     * @param manager the manager managing me and my "sibling" connections, or null
+     *   if the run() method will not be called.
      * @param sock the socket used for communication
      * @param incoming true if this is an incoming connection.
      *    False otherwise.
@@ -55,8 +81,9 @@ public class Connection implements Runnable {
 	throws IOException {
 	this.manager=manager;
 	this.sock=sock;
-	this.incoming=incoming;       
-	this.routeTable=manager.routeTable;
+	this.incoming=incoming;
+	if (manager!=null)
+	    this.routeTable=manager.routeTable;
 
 	//Handshake
 	final String CONNECT="GNUTELLA CONNECT/0.4\n\n";
@@ -64,16 +91,18 @@ public class Connection implements Runnable {
 	if (incoming) {
 	    expectString(CONNECT);
 	    sendString(OK);
-	} else {
+	} else { //outgoing
 	    sendString(CONNECT);
 	    expectString(OK);
-	    Message m=new PingRequest(Const.TTL);
-	    send(m);
-	    //Hack: don't want to (later) forward message starting from me.
-	    routeTable.put(m.getGUID(), this);
+	    if (manager!=null) {
+		Message m=new PingRequest(Const.TTL);
+		send(m);
+		routeTable.put(m.getGUID(), this);
+	    }
 	}
 
-	manager.add(this);
+	if (manager!=null)
+	    manager.add(this);
   	System.out.println("Established "+(incoming?"incoming":"outgoing")
   			   +" connection on "+sock.toString());
     }
@@ -102,38 +131,47 @@ public class Connection implements Runnable {
     //////////////////////////////////////////////////////////////////////
 
     /**
-     * @modifies: the network underlying this
-     * @effects: send m on the network.  Throws IOException if problems
-     *   arise.
+     * @modifies the network underlying this
+     * @effects send m on the network.  Throws IOException if problems
+     *   arise.  This is thread-safe.
      */
-    public synchronized void send(Message m) throws IOException {
-	//System.out.println("Wrote "+m.toString()+"\n   to "+sock.toString());
+    public void send(Message m) throws IOException {
+	//Can't use same lock as receive()!
 	OutputStream out=sock.getOutputStream();
-	m.write(out);
-	out.flush();
-	sent++;
+	synchronized (out) {
+	    m.write(out);
+	    out.flush();
+	    sent++;
+	}
+	System.out.println("Wrote "+m.toString()+"\n   to "+sock.toString());
     }
 
     /** 
      * See specification of Message.read. Note that this is
      *  non-blocking, but there is no hard guarantee on the maximum
-     *  block time.  
+     *  block time.  This is thread-safe.
      */
     public Message receive() throws IOException, BadPacketException {
-	Message m=Message.read(sock.getInputStream());
-	received++;  //keep statistics.
-	//if (m!=null)
-	//  System.out.println("Read "+m.toString()+"\n    from "+sock.toString());
-	return m;
+	InputStream in=sock.getInputStream();
+	//Can't use same lock as send()!
+	synchronized(in) {
+	    Message m=Message.read(in);
+	    received++;  //keep statistics.
+	    if (m!=null)
+		System.out.println("Read "+m.toString()+"\n    from "+sock.toString());
+	    return m;
+	}
     }
 
     /**
-     * @modifies: the network underlying this, manager
-     * @effects: receives request and sends appropriate replies.
+     * @requires the manager to this is non-null
+     * @modifies the network underlying this, manager
+     * @effects receives request and sends appropriate replies.
      *   Returns if either the connection is closed or an error happens.
      *   If this happens, removes itself from the manager's connection list.
      */
     public void run() {
+	Assert.that(manager!=null && routeTable!=null);
 	try {
 	    while (true) {
 		Message m=null;
@@ -209,4 +247,25 @@ public class Connection implements Runnable {
     public boolean isOutgoing() {
 	return !incoming;
     }
+
+    /** Returns the port of the foreign host this is connected to. */
+    public int getPort() {
+	return sock.getPort();
+    }
+
+    /** Returns the port this is connected to locally. */
+    public int getLocalPort() {
+	return sock.getLocalPort();
+    }
+
+    /** Returns the address of the foreign host this is connected to. */
+    public InetAddress getInetAddress() {
+	return sock.getInetAddress();
+    }
+
+    /** Returns the local address of this. */
+    public InetAddress getLocalAddress() {
+	return sock.getLocalAddress();
+    }
+
 }
