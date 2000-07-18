ad218ec0a0667e34fd5fff7587cbd8a12f2eaa0f
gbildson <gbildson>
2000-07-18 16:25:40 +0000

    new

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/ActivityCallback.java
@@ -0,0 +1,39 @@
+package com.limegroup.gnutella;
+
+/**
+ *  Interface for connection events to be fed from the router into the 
+ *  visual world.
+ */
+public interface ActivityCallback
+{	
+	/**  Flag an outgoing connection type */
+	public static final int CONNECTION_OUTGOING = 1;
+	/**  Flag an incoming connection type */
+	public static final int CONNECTION_INCOMING = 2;
+
+	/**  The connection is being setup */
+	public static final int STATUS_CONNECTING = 1;
+	/**  The connection is established */
+	public static final int STATUS_CONNECTED = 2;
+
+    /**
+     *  Handle a new connection.
+     */
+    public void addConnection(String host, int port, int type, int status);
+
+    /**
+     *  Handle a removed connection.
+     */
+    public void removeConnection(String host, int port);
+
+    /**
+     *  Change the status of a connection
+     */
+    public void updateConnection(String host, int port, int status);
+
+    /**
+     *  Add a known host/port
+     */
+    public void knownHost(Endpoint e);
+
+}
