6faaee4172c08bc47e71529f34fd2fa3e20358ae
rsoule <rsoule>
2000-07-19 15:06:06 +0000

    *** empty log message ***

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/FileDesc.java
@@ -0,0 +1,24 @@
+/**
+ * auth: rsoule
+ * file: FileDesc.java
+ * desc: This class is a wrapper for file information.
+ *
+ */
+
+package com.limegroup.gnutella;
+
+public class FileDesc {
+
+    public int _index;
+    public String _path;
+    public String _name;
+    public int _size;
+
+    public FileDesc(int i, String n, String p, int s) {
+	_index = i;
+	_name = n;
+	_path = p;
+	_size = s;
+    }
+
+}
