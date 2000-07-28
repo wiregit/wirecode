73a2f18787b9bba218660e7af80cc707ea300354
crohrs <crohrs>
2000-07-28 19:58:20 +0000

    Has optional file count and size. Useful for sending cached pings in HostCatcher.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/Endpoint.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/Endpoint.java
@@ -5,10 +5,14 @@ import java.util.*;
 import java.io.*;
 
 
-/** Immutable IP/port pair. */
+/** Immutable IP/port pair.  Also contains an optional number and size of files. */
 public class Endpoint implements Cloneable, Serializable{
     String hostname;
     int port;
+    /** Number of files at the host, or -1 if unknown */
+    private long files=-1l;
+    /** Size of all files on the host, or -1 if unknown */
+    private long kbytes=-1l;
 
 /**
 * Needed for Network Discovery. Records information regarding wthether the neighbours
@@ -19,6 +23,16 @@ public boolean processed = false;
 public Endpoint(String hostname, int port) {
 	this.hostname=hostname;
 	this.port=port;
+}
+
+/**
+ * @param files the number of files the host has
+ * @param kbytes the size of all of the files, in kilobytes
+ */
+public Endpoint(String hostname, int port, long files, long kbytes) {
+        this(hostname, port);
+        this.files=files;
+	this.kbytes=kbytes;
 }
 
 public String toString() {
@@ -34,7 +48,22 @@ public int getPort()
 {
 	return port;
 }
-    
+
+/** Returns the number of files the host has, or -1 if I don't know */
+public long getFiles() {
+        return files;
+}
+ 
+/** Returns the size of all files the host has, in kilobytes, 
+ *  or -1 if I don't know */   
+public long getKbytes() {
+        return kbytes;
+}
+
+/** 
+ * Endpoints are equal if their hostnames and ports are.  The number
+ * and size of files does not matter.
+ */
 public boolean equals(Object o) {
 	if (! (o instanceof Endpoint))
 	    	return false;
