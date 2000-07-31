7f6cf81d027b2bdaf78546a7092bddc1e8d55ed9
crohrs <crohrs>
2000-07-31 22:10:41 +0000

    Got rid of "chosen" set.  Instead, uses the isConnected method in ConnectionManager. If performance is a problem, tweak the isConnected method.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/HostCatcher.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/HostCatcher.java
@@ -10,15 +10,17 @@ import java.io.*;
  * servent may then connect to these addresses as necessary to
  * maintain full connectivity.<p>
  *
- * The host catcher maintains two sets of address: the "likelys" and the
- * "maybe" sets.  Likelys hosts are the results of ping replies and hence
- * are known to be good.  Maybe hosts haven't been tried yet, so they may
- * or may not be good.  Hosts we are connected to are NOT stored, but they
- * may be written to disk.
+ * The host catcher maintains two sets of address: the "likelys" and
+ * the "maybe" sets.  Likelys hosts are the results of ping replies
+ * and hence are known to be good.  Maybe hosts are read from the
+ * gnutella.net, so they may or may not be good.  Hosts we are
+ * connected to will not be returned by choose(), but they may be
+ * written to disk.
  */
 public class HostCatcher {
-    /** INVARIANT: "likelys" and "maybes" and "chosen" are disjoint */
-
+    /* INVARIANT: "likelys" and "maybes" and the current list of 
+     * connections are disjoint */
+     
     /** The hosts read from disk, or otherwise unlikely to be
       * unconnected.  This could ultimately be prioritized by extra
       * info (e.g., connection speed, number of files) gathered in
@@ -31,9 +33,6 @@ public class HostCatcher {
 
     /** The hosts discovered by pings, or otherwise likely to be connected. */
     protected Set /* of Endpoint */ likelys=new HashSet();
-    
-    /** The hosts given out by choose().  Ignore their pings. */
-    protected Set /* of Endpoint */ chosen=new HashSet();
 
     private void error(String message) {
 	if (manager!=null) {
@@ -44,8 +43,8 @@ public class HostCatcher {
     }
 
     /** 
-     * Creates an empty host catcher.  manager is used for callbacks,
-     * or null if we don't care.
+     * Creates an empty host catcher.  manager is used for callbacks
+     * and checking my list of current connections, or null if we don't care.
      */
     public HostCatcher(ConnectionManager manager) { this.manager=manager; }
 
@@ -107,10 +106,18 @@ public class HostCatcher {
      */
     public synchronized void write(String filename) throws IOException {
 	FileWriter out=new FileWriter(filename);
-	//Write connections we've given out first...
-	synchronized(chosen) {
-	    writeInternal(out, chosen);
+	//Write connections we're connected to...
+	if (manager!=null) {
+	    Set tmp=new HashSet();
+	    for (Iterator iter=manager.connections(); iter.hasNext(); ) {
+		Connection c=(Connection)iter.next();
+		Endpoint e=new Endpoint(c.getInetAddress().getHostAddress(),
+					c.getPort());
+		tmp.add(e);
+	    }
+	    writeInternal(out, tmp);
 	}
+	
 	//Followed by pings...
 	synchronized(likelys) {
 	    writeInternal(out, likelys);
@@ -135,23 +142,26 @@ public class HostCatcher {
     
     /** 
      * @modifies this
-     * @effects ensures any hosts listed in m are in this' likelys set
+     * @effects may choose to add hosts listed in m to this' likelys set.
+     *  c is the connection this message came in on; if m came directly
+     *  from the foreign host of c, m is ignored.
      */
     public void spy(Message m) {	
-	//ignore m if I'm connected to it, i.e. if hops==0
 	//We could also get ip addresses from query hits and push
 	//requests, but then we have to guess the ports for incoming
 	//connections.  (These only give ports for HTTP.) 
-	int hops=m.getHops();
-	if ((! (m instanceof PingReply)) || hops==0)
+	if (! (m instanceof PingReply))
 	    return;
-	
+	    	
 	PingReply pr=(PingReply)m;
 	Endpoint e=new Endpoint(pr.getIP(), pr.getPort(), 
 				pr.getFiles(), pr.getKbytes());
 
-	//Don't record addresses I've already seen.
-	if (chosen.contains(e))
+	//Ignore m if I'm connected to it.  This implies that hops==0,
+	//but converse does not hold; cached pongs from Gnucache
+	//servers have hops==0.   TODO3: if this is slow, change implementation
+	//of ConnectionManager.isConnected.
+	if (manager!=null && manager.isConnected(pr.getIP(), pr.getPort()))
 	    return;
 
 	//Add it to the likely set, remove it from the maybes set if
@@ -169,8 +179,9 @@ public class HostCatcher {
     /**
      * @modifies this
      * @effects returns a new outgoing, connected connection to some
-     *  host in this and removes it from this (atomically).  Does not
-     *  modify manager.  If no such host can be found, throws
+     *  host in this and removes it from this (atomically).  If manager is non-null, 
+     *  it is (almost) guaranteed that we do not return any hosts we are connected to.  
+     *  Does not modify manager.  If no such host can be found, throws
      *  NoSuchElementException.  This method <i>is</i> thread-safe, but
      *  it is cleverly synchronized so that multiple invocations can run
      *  at the same time.
@@ -181,16 +192,15 @@ public class HostCatcher {
 	    if ( manager != null && !manager.doYouWishToContinue() )
 		return null;
 	    Endpoint e=getAnEndpoint();	
+	    // Skip anything we are connected to.
+	    if (manager != null && manager.isConnected(e.getHostname(),e.getPort()))
+		continue;
 	    Connection ret = null;
 	    try {
 		ret = new Connection(e.hostname,e.port);
 		if (manager!=null)
 		    manager.tryingToConnect(ret, false);
 		ret.connect();
-		//Note that we've given out e for future reference
-		synchronized(chosen) {
-		    chosen.add(e);
-		}
 		return ret;
 	    } catch (IOException exc) {
 		if (manager!=null)
@@ -211,7 +221,7 @@ public class HostCatcher {
 	//Use pings before gnutella.net if possible.
 	if (likelys.size()>0) {
 	    try {
-		return choose(likelys);
+		Endpoint e=choose(likelys);
 	    } catch (NoSuchElementException e) { 
 		//This can happen if another thread removed the last likelys
 		//host before I got to acquire the monitor.  If it happens,
