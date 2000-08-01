a1d74435906d358f7ae8a42f22e21d944f47434c
asingla <asingla>
2000-08-01 20:25:23 +0000

    //removed connectivity from fn toString()

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/Endpoint.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/Endpoint.java
@@ -71,7 +71,7 @@ public Endpoint(String hostname, int port, long files, long kbytes) {
 }
 
 public String toString() {
-	return hostname+":"+port + " connectivity=" + connectivity;
+	return hostname+":"+port;
 }
 
 public String getHostname()
