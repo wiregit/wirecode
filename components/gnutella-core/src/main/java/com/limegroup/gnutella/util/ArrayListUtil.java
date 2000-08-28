d002d71357bed12c89c0dd888562a64b1754e06e
gbildson <gbildson>
2000-08-28 13:31:19 +0000

    New utility for Array manipulation

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/util/ArrayListUtil.java
@@ -0,0 +1,27 @@
+package com.limegroup.gnutella.util;
+
+import java.util.*;
+
+/** 
+ * Coomon Array manipulation routines
+ */
+public class ArrayListUtil
+{
+
+    /** 
+     *  Build an ArrayList of Integer from and array of int.
+     */
+    public static ArrayList buildArray(int [] list) 
+    {
+	ArrayList nlist = new ArrayList();
+	for ( int i = 0; i < list.length; i++ )
+	{
+	    Integer val = new Integer(list[i]);
+	    nlist.add(val);
+	}
+	return(nlist);
+    }
+
+    
+
+}
