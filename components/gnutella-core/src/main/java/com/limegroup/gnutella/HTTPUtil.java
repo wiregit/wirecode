e774dbc4bbd4fe328e14039d3f82e6b7392a7f87
crohrs <crohrs>
2000-08-30 22:22:17 +0000

    Changed specification of splitString to not discard whitespace. Currently just implemented with split().

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/HTTPUtil.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/HTTPUtil.java
@@ -13,41 +13,53 @@ import java.util.*;
 
 public class HTTPUtil {
 
+    /** Returns the tokens of s delimited by the given delimeter,
+     *  without returning the delimeter.  Examples:
+     *  <pre>
+     *    stringSplit("a//b/ c /","/")=={"a","b"," c "}
+     *    stringSplit("a b", "/")=={"a b"}.
+     *    stringSplit("///", "/")=={}.
+     *  </pre>
+     *
+     * <b>Note that whitespace is preserved if it is not part of the delimeter.</b>
+     * An older version of this trim()'ed each token of whitespace.
+     */
     public static String[] stringSplit(String s, char delimeter) {
-           	                         /* given a character delimeter */ 
-	s = s.trim();                    /* stringSplit will return an */
-	                                 /* array of the Strings separated */
-	int n = s.length();              /* by that delimeter */ 
+	return split(s, delimeter+"");
+//             	                         /* given a character delimeter */ 
+//  	s = s.trim();                    /* stringSplit will return an */
+//  	                                 /* array of the Strings separated */
+//  	int n = s.length();              /* by that delimeter */ 
 	
-	if (n==0) return new String[0];
+//  	if (n==0) return new String[0];
 	
-	Vector buf=new Vector();         /* are vectors bad? inneficient? */
-	                                 /* s[i] is the start of the word to */
-	for (int i=0; i<n; ) {           /* add to buf */
-        	                         /* s[j] is just past the end */
-	    if (s.charAt(i)== delimeter) i++;
+//  	Vector buf=new Vector();         /* are vectors bad? inneficient? */
+//  	                                 /* s[i] is the start of the word to */
+//  	for (int i=0; i<n; ) {           /* add to buf */
+//          	                         /* s[j] is just past the end */
+//  	    if (s.charAt(i)== delimeter) i++;
 	
-	    int j=s.indexOf(delimeter,i+1);
+//  	    int j=s.indexOf(delimeter,i+1);
 	    
-	    if (j==-1)
-		j=n;
+//  	    if (j==-1)
+//  		j=n;
 
-	    buf.add(s.substring(i,j));
+//  	    buf.add(s.substring(i,j));
 
-	    for (i=j+1; j<n-1 ; ) {       /* Ignore white space after s[j]*/
-  		if (s.charAt(i)!=' ')
-  		    break;
-  		i++;
-  	    }			
+//  	    for (i=j+1; j<n-1 ; ) {       /* Ignore white space after s[j]*/
+//    		if (s.charAt(i)!=' ')
+//    		    break;
+//    		i++;
+//    	    }			
 
-	}
+//  	}
 
-	String[] ret=new String[buf.size()];
+//  	String[] ret=new String[buf.size()];
 
-	for (int i=0; i<ret.length; i++)
-	    ret[i]=(String)buf.get(i);
+//  	for (int i=0; i<ret.length; i++)
+//  	    ret[i]=(String)buf.get(i);
 
-	return ret;
+//  	return ret;
 
     }
 
@@ -81,8 +93,24 @@ public class HTTPUtil {
 	return ret;
 	
     } 
-  
 
+//      public static void main(String args[]) {
+//  	String in;
+//  	String[] expected;
+//  	String[] result;
+
+//  	in="a//b/ c /"; expected=new String[] {"a","b"," c "};
+//  	result=stringSplit(in, '/');
+//  	Assert.that(Arrays.equals(expected, result));
+	
+//  	in="a b";       expected=new String[] {"a b"};
+//  	result=stringSplit(in, '/');
+//  	Assert.that(Arrays.equals(expected, result));
+
+//  	in="///";       expected=new String[] {};
+//  	result=stringSplit(in, '/');
+//  	Assert.that(Arrays.equals(expected, result));
+//      }
 }
 
 
