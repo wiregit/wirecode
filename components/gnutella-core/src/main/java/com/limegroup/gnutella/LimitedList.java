d2726ca6b9fcdb4e962be0b6d9bd784c90f6712e
afisk <afisk>
2000-10-02 23:18:52 +0000

    changed dir action to allow file scanning on install and changed the tabs in limited list to conform with new guidelines

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/LimitedList.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/LimitedList.java
@@ -15,54 +15,50 @@ public class LimitedList {
     private int _size;
 
     public LimitedList() {
-	_list = new ArrayList();
-	_size = 0;
+		_list = new ArrayList();
+		_size = 0;
     }
-
+	
     public Object[] getAllElements() {
-	Object[] elems = new Object[_size];
-	elems = _list.toArray(elems);
-	return elems;
+		Object[] elems = new Object[_size];
+		elems = _list.toArray(elems);
+		return elems;
     }
-
-    public void print() {
-
-	for (int i=0; i < _size; i++) {
-	    Pair p = (Pair)_list.get(i);
-	    p.print();
-	}
 	
+    public void print() {		
+		for (int i=0; i < _size; i++) {
+			Pair p = (Pair)_list.get(i);
+			p.print();
+		}		
     }
-
+	
     public void add(Object elem, int key) {
-
-	if (key == 0)
-	    return;
-	int index = _size - 1;
-	Pair p;
-	while (index > -1) {
-	    p = (Pair)_list.get(index);
-	    if (key <= p.getKey()) {
-		if (index != MAX-1) { 
-		    _list.add(index+1, elem);
-		    if (_size == MAX)
-			_list.remove(MAX);
-		    else
-			_size++;
-		    return;
+		
+		if (key == 0)
+			return;
+		int index = _size - 1;
+		Pair p;
+		while (index > -1) {
+			p = (Pair)_list.get(index);
+			if (key <= p.getKey()) {
+				if (index != MAX-1) { 
+					_list.add(index+1, elem);
+					if (_size == MAX)
+						_list.remove(MAX);
+					else
+						_size++;
+					return;
+				}
+				else 
+					return;
+			}
+			index--;
 		}
+		
+		_list.add(0, elem);
+		if (_size == MAX)
+			_list.remove(MAX);
 		else 
-		    return;
-	    }
-	    index--;
-	}
-
-	_list.add(0, elem);
-	if (_size == MAX)
-	    _list.remove(MAX);
-	else 
-	    _size++;
-	
+			_size++;	
     }
-
 }
