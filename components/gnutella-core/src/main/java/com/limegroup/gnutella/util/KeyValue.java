5c2875e306fdb4c47bc75431d9febe8177fe8b68
asingla <asingla>
2000-10-23 15:18:46 +0000

    //updated ForgetfulHashMap's put method, so that it returns Map.Entry (key-value) //pair (i.e. the entry deleted) as the returned object

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/util/KeyValue.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/util/KeyValue.java
@@ -1,7 +1,7 @@
 
 package com.limegroup.gnutella.util;
 
-import java.util.*;
+import com.sun.java.util.collections.*;
 
 /**
 * This class stores a pair of a property key, and its corresponding value.
