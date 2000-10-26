63069e73e22588d5bfc70c4c1450282bc661587b
asingla <asingla>
2000-10-26 02:09:59 +0000

    *** empty log message ***

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/util/FixedsizeHashMap.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/util/FixedsizeHashMap.java
@@ -4,82 +4,99 @@ import com.sun.java.util.collections.*;
 import com.limegroup.gnutella.util.*;
 
 /**
-* A mapping that stores only fixed number of mappings in the underlying
-* hashmap data structure. Right now, its not really strictly a fixed size
-* map but eventually will be.
-* The victim entry to be removed, when the hashmap is full,
-* is found using FIFO policy.
-* The functionality is more or less similar to ForgetfulHashMap, with slight
-* variation in certain methods, and is thus encapsulated in a separate class.
-* @see ForgetfulHashMap for more details on implementation.
+ * This class implements fixed size HashMap. If its get full, no new entry
+ * can be inserted into it, except by removing some entry first.
+ * An attempt to add new entry throws an NoMoreStorageException
+ * @see NoMoreStorageException
+ */
+public class FixedSizeHashMap
+{
+
+/**
+* The number of elements in the underlying storage
 */
-public class FixedsizeHashMap extends HashMap {
+private int count = 0;
 
-    
-    private Object[] queue;
-    private int next;
-    private int n;
-    
-    /** 
-     * Create a new RouteTable that holds only the last "size" entries.
-     *
-     * @param size the number of entries to hold
-     * @exception IllegalArgumentException if size is less < 1.
-     */
-    public FixedsizeHashMap(int size) {
-        if (size < 1)
-            throw new IllegalArgumentException();
-        queue=new Object[size];
-        next=0;
-        n=size;
-    }
-
-    /**
-     * @modifies this
-     * @effects Maps the given key to the given value, ensuring that
-     * (key, value) is the newest pair in this.  If adding the key
-     * would make this contain more elements than the size given at
-     * construction, removes the oldest key-value pair and returns it;
-     * otherwise returns null.  
-     */
-    public Object put(Object key, Object value) {
-        Object ret=super.put(key,value);
-        if (ret!=null) {
-            return null;
-        }
-
-        //declare an object to store the returnValuePair
-        //in case we delete something from the map
-        Map.Entry returnValue = null;
-        
-        //Purge oldest entry if we're all full, or if we'll become full
-        //after adding this entry.  It's ok if queue[next] is no longer
-        //in the map.
-        Object oldestKey=queue[next];
-        if (queue[next]!=null) {
-            //get the old value
-            returnValue = new KeyValue(oldestKey, super.get(oldestKey));
-            super.remove(oldestKey);
-        }
-        //And make (key,value) the newest entry.  It is ok
-        //if key is already in queue.
-        queue[next]=key;
-        next++;
-        if (next>=n) {
-            next=0;
-        }
-        return returnValue;
-    }
-
-    public void putAll(Map t) {
-        Iterator iter=t.keySet().iterator();
-        while (iter.hasNext()) {
-            Object key=iter.next();
-            put(key,t.get(key));
-        }
-        
-    }
+/**
+* The max number of elements that can be stored
+*/
+private int size = 0;
 
 
+private HashMap hashMap = null;
+
+/**
+ * Create a new hashMap that stores only the specified number of entries
+ *
+ * @param size the number of entries to hold
+ * @exception IllegalArgumentException if size is less < 1.
+ */
+public FixedSizeHashMap(int size)
+{
+    hashMap = new HashMap(size * 4/3); //it might throw IllegalArgumentException
+                       //in case the size is negative
+    this.size = size;
 }
 
+/**
+ * @modifies this
+ * @effects Maps the given key to the given value, ensuring that
+ * (key, value) is the newest pair in this.  If adding the key
+ * would make this contain more elements than the size given at
+ * construction, removes the oldest key-value pair and returns it;
+ * otherwise returns null.
+ */
+public Object put(Object key, Object value) throws NoMoreStorageException
+{
+    Object retValue = null;
+    
+    //check if the count is less than size, in that case no problem
+    //inserting this new entry
+    if(count < size)
+    {
+        retValue = hashMap.put(key,value);
+        if(retValue != null) //i.e if the mapping already existed
+        {               //in that case no new mapping added
+                        //and so no need to increment counts
+        }
+        else
+        {
+            //increment the count
+            count++;
+        }
+    }
+    else //if the hashmap is full
+    {
+        //if the entry already existed, we can safely add this new pair
+        //without affecting the size
+        retValue = hashMap.get(key);
+        if(retValue != null)
+        {
+            //mapping existed, so update the mapping
+            retValue = hashMap.put(key,value);
+        }
+        else //no space to enter anything more
+        {
+            //throw an exception
+            throw new NoMoreStorageException();
+        }
+    }
+    
+    return retValue;
+}
+
+
+
+
+/**
+ * checks if the hash Map is full or not (ie if for adding new item we need
+ * to remove some item
+ * @return true if the map is full, false otherwise
+ */
+public boolean isFull()
+
+{
+    return count >= size ;
+}
+
+}
