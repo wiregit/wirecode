a1f0fb4aa9c866841ae7d19def7f5a85fd56abeb
crohrs <crohrs>
2000-11-28 19:49:17 +0000

    Replacement for IllegalArgumentException2.

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/BadConnectionSettingException.java
@@ -0,0 +1,41 @@
+package com.limegroup.gnutella;
+
+/** 
+ * The exception thrown when you try to set your incoming/outgoing connections
+ * to a bad value.  Contains suggested new values for the settings and a reason
+ * for the exception.<p>
+ *
+ * Design note: some may argue that this should be multiple classes or an enum.
+ * But there could be multiple reasons for an exception, so we may use masks.
+ */
+public class BadConnectionSettingException extends RuntimeException {
+    public final static int NEGATIVE_VALUE=0x1;
+    public final static int TOO_HIGH_FOR_SPEED=0x2;
+    public final static int OUT_GREATER_THAN_IN=0x4;
+    
+    /** INVARIANT: One of the static fields defined above. */
+    private int reason;
+    /** The suggested KEEP_ALIVE. */
+    private int suggestedOutgoing;
+    /** The suggested MAX_INCOMING_CONNECTIONS */
+    private int suggestedIncoming;
+
+    /**
+     * @param reason why the settings were rejected.  This value
+     *  must be one of NEGATIVE_VALUE, TOO_HIGH_FOR_SPEED, or
+     *  OUT_GREATER_THAN_IN.
+     * @param suggestedOutgoing the suggested KEEP_ALIVE
+     * @param suggestedIncoming the suggested MAX_INCOMING_CONNECTIONS
+     */
+    public BadConnectionSettingException(int reason,
+                                         int suggestedOutgoing,
+                                         int suggestedIncoming) {
+        this.reason=reason;
+        this.suggestedIncoming=suggestedIncoming;
+        this.suggestedOutgoing=suggestedOutgoing;
+    }
+
+    public int getReason() { return reason; }
+    public int getSuggestedOutgoing() { return suggestedOutgoing; }
+    public int getSuggestedIncoming() { return suggestedIncoming; }
+}
