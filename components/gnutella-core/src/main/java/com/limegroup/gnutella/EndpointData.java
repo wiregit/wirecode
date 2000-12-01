6e0492593c0538818f26a1ca1829feb21d833318
asingla <asingla>
2000-12-01 19:42:04 +0000

    storing GUID as byte[] now, instead of as an instance of GUID

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/EndpointData.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/EndpointData.java
@@ -21,7 +21,7 @@ public class EndpointData extends Endpoint implements Cloneable,
 /**
 * Client GUID of the host this endpoint refers to
 */
-private GUID clientGUID = null;
+private byte[] clientGUID = null;
 
 /**
 * Speed in kilobytes of the host this endpoint refers to
@@ -41,11 +41,36 @@ public EndpointData(Endpoint endpoint)
 
 /**
 * Creates a new EndpointData instance
+* @param hostname Hostname of the node this endpoint refers to
+* @param port The port number for the host
+*/
+public EndpointData(String hostname, int port)
+{
+    //initialize the fields in the super class
+    super(hostname,port);
+}
+
+/**
+* Creates a new EndpointData instance
+* @param hostname Hostname of the node this endpoint refers to
+* @param port The port number for the host
+* @param files the number of files the host has
+* @param kbytes the size of all of the files, in kilobytes
+*/
+public EndpointData(String hostname, int port, long files, long kbytes)
+{
+    //initialize the fields in the super class
+    super(hostname, port, files, kbytes);
+}
+
+
+/**
+* Creates a new EndpointData instance
 * @param endpoint from where to copy the fields for the super class
 * @param speed Speed of the host
 * @param clientGUID Unique GUID for the host
 */
-public EndpointData(Endpoint endpoint, long speed, GUID clientGUID)
+public EndpointData(Endpoint endpoint, long speed, byte[] clientGUID)
 {
     //initialize the fields in the super class
     super(endpoint);
@@ -59,7 +84,7 @@ public EndpointData(Endpoint endpoint, long speed, GUID clientGUID)
 * @param speed Speed of the host
 * @param clientGUID Unique GUID for the host
 */
-public void set(long speed, GUID clientGUID)
+public void set(long speed, byte[] clientGUID)
 {
     //set the fields
     this.speed = speed;
