884f552fc26574da7caff941f43629d4d5e94ff7
crohrs <crohrs>
2001-01-19 22:15:53 +0000

    Added top-secret LimeWire GUID marking technique.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/GUID.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/GUID.java
@@ -5,10 +5,26 @@ import com.sun.java.util.collections.*;
 /**
  * A 16-bit globally unique ID.  Immutable.<p>
  *
- * This provides minimal functionality now, but I hope to integrate it
- * with the rest of the system in the future when I refactor code.
+ * Let the bytes of a GUID G be labelled G[0]..G[15].  All bytes are unsigned.
+ * Let a "short" be a 2 byte little-endian** unsigned number.  Let AB be the
+ * short formed by concatenating bytes A and B, with B being the most
+ * significant byte.  Then LimeWire GUID's have the following properties.
+ *
+ * <ol>
+ * <li>G[8]==0xFF.  This serves to identify "new GUIDs", e.g. from BearShare.
+ * <li>G[15]=0x00.  This is reserved for future use.
+ * <li>G[9][10]= 0xFFFF & ((G[4]G[5]+2)*(G[6][7]+3) >> 8).  This is LimeWire's
+ *   proprietary marking.  In other words, the result is obtained by first taking
+ *   the two byte values before the 0xFF and adding "secret" constants.  These
+ *   two byte values are then multiplied together to form a 4 byte product.  The
+ *   middle two bytes of this product are then stored after the 0xFF.  <b>Sign
+ *   IS considered during this process, since Java does that by default.</b>
+ * </ol>
+ *
+ * Note that this still leaves 12 bytes for randomness.  That's plenty of
+ * distinct GUID's.  And there's only a 1 in 65000 chance of mistakenly
+ * identifying a LimeWire. 
  */
-
 public class GUID /* implements Comparable */ {
     /** The size of a GUID. */
     private static final int SZ=16;
@@ -25,18 +41,52 @@ public class GUID /* implements Comparable */ {
 
     /** Returns the bytes for a new GUID. */
     public static byte[] makeGuid() {
+        //Start with random bytes.  You could avoid filling them all in,
+        //but it's not worth it.
         byte[] ret=new byte[16];
         rand.nextBytes(ret);
+
+        //Apply common tags.
         ret[8]=(byte)0xFF;    //Mark as "new" GUID.  (See isNewGUID().)
         ret[15]=(byte)0x00;   //Version number is 0.
+
+        //Apply LimeWire's marking.  You could probably avoid calls to ByteOrder
+        //as an optimization.
+        short a=ByteOrder.leb2short(ret, 4);
+        short b=ByteOrder.leb2short(ret, 6);
+        short tag=tag(a, b);
+        ByteOrder.short2leb(tag, ret, 9);
         return ret;
     }
+
+    /** Returns LimeWire's secret tag described above. */
+    private static short tag(short a, short b) {
+        int product=(a+2)*(b+3);
+        //No need to actually do the AND since the downcast does that.
+        short productMiddle=(short)(product >> 8);
+        return productMiddle;
+    }
+    
+    /** Same as isLimeGUID(this.bytes) */
+    public boolean isLimeGUID() {    
+        return isLimeGUID(this.bytes);
+    }
+
+    /** Returns true if this is a specially marked LimeWire GUID.
+     *  This does NOT mean that it's a new GUID as well; the caller
+     *  will probably want to check that. */
+    public static boolean isLimeGUID(byte[] bytes) {    
+        short a=ByteOrder.leb2short(bytes, 4);
+        short b=ByteOrder.leb2short(bytes, 6);
+        short foundTag=ByteOrder.leb2short(bytes, 9);
+        short expectedTag=tag(a, b); 
+        return foundTag==expectedTag;
+    }    
 
     /** Same as isWindowsGUID(this.bytes). */
     public final boolean isWindowsGUID() {    
         return isWindowsGUID(bytes);
     }
-
 
     /** Returns true if this is a GUID created by the Windows CoCreateGUID()
      *  routine.  Note that the converse does not hold; this method may
@@ -196,11 +246,15 @@ public class GUID /* implements Comparable */ {
         }
 
         //Try new GUID generator.
-        bytes=GUID.makeGuid();     g1=new GUID(bytes);
-        Assert.that(bytes[8]==(byte)0xFF);
-        Assert.that(bytes[15]==(byte)0x00);
-        Assert.that(g1.isNewGUID());
-        Assert.that(! g1.isWindowsGUID());               
+        for (int i=0; i<50; i++) {
+            bytes=GUID.makeGuid();     g1=new GUID(bytes);
+            Assert.that(bytes[8]==(byte)0xFF);
+            Assert.that(bytes[15]==(byte)0x00);
+            Assert.that(g1.isNewGUID());
+            Assert.that(g1.isLimeGUID());
+            Assert.that(! g1.isWindowsGUID());
+            System.out.println(g1);
+        }               
         
         //Try isWindowsGUID (again)
         bytes[8]=(byte)0xc0;   g1=new GUID(bytes);
@@ -213,8 +267,26 @@ public class GUID /* implements Comparable */ {
         Assert.that(! g1.isNewGUID());
         bytes[8]=(byte)0xFF;  bytes[15]=(byte)0x00;   g1=new GUID(bytes);
         Assert.that(g1.isNewGUID());
+
+        //Try isLimeGUID (again)
+        bytes[9]=(byte)0xFF;   g1=new GUID(bytes);
+        Assert.that(! g1.isLimeGUID());
+        bytes[4]=(byte)0x02;
+        bytes[5]=(byte)0x01;
+        bytes[6]=(byte)0x00;
+        bytes[7]=(byte)0x05;
+        //Note everything is LITTLE endian.
+        //(0x0102+2)*(0x0500+3)=0x0104*0x0503=0x5170C ==> 0x0517
+        bytes[9]=(byte)0x17;
+        bytes[10]=(byte)0x05;
+        g1=new GUID(bytes);
+        short s1=ByteOrder.leb2short(bytes, 4);
+        short s2=ByteOrder.leb2short(bytes, 6);
+        short tag=tag(s1, s2);
+        Assert.that(s1==(short)0x0102, Integer.toHexString(s1));
+        Assert.that(s2==(short)0x0500, Integer.toHexString(s2));
+        Assert.that(tag==(short)0x0517, Integer.toHexString(tag));
+        Assert.that(g1.isLimeGUID());
     }
     */
 }
-
-
