ef0e34b695f632aab47c63996b09827152cc962e
crohrs <crohrs>
2001-01-30 19:03:18 +0000

    Merged from chris-after-adams-cleanup on trunk. +Kept all changes to HTTPDownloader on smart-download-branch, with minor  tweak for User-agent. +Took all changes to HTTPUploader from main, discarding changes on  smart-download-branch

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/Message.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/Message.java
@@ -20,12 +20,9 @@ public abstract class Message implements Serializable{
     protected static final byte F_QUERY=(byte)0x80;
     protected static final byte F_QUERY_REPLY=(byte)0x81;
 
-    //Temporary code for managing GUIDs
-    private static Random rand=new Random();
+    /** Same as GUID.makeGUID.  This exists for backwards compatibility. */
     static byte[] makeGuid() {
-        byte[] ret=new byte[16];
-        rand.nextBytes(ret); //TODO1: not guaranteed unique
-        return ret;
+        return GUID.makeGuid();
     }
 
     ////////////////////////// Instance Data //////////////////////
