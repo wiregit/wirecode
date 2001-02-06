a00a253d46276b1d6f6b8f381a38dc92f5c18a90
gbildson <gbildson>
2001-02-06 22:05:05 +0000

    Added for backward update compatibility

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/filters/Main.java
@@ -0,0 +1,15 @@
+package com.limegroup.gnutella.filters;
+
+
+/**
+ *  This class kicks off the construction of the gui and
+ *  backend, catching any exceptions that could occur.
+ *
+ *  @author Adam Fisk
+ */
+public class Main {
+
+    public static void main(String args[]) {
+    	com.limegroup.gnutella.gui.Main.main(args);
+    }
+}
