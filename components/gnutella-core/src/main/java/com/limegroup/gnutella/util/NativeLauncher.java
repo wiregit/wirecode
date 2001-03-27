9757ec3af7225582792d96a4ce2ffa26f2692073
afisk <afisk>
2001-03-27 02:48:28 +0000

    added comments

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/util/NativeLauncher.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/util/NativeLauncher.java
@@ -1,13 +1,28 @@
+/**
+ * auth: afisk
+ * file: NativeLauncher.java
+ * desc: Wrapper for calls to native code that launches
+ *       files in their associated applications.
+ */
+
 package com.limegroup.gnutella.util;
 
 public class NativeLauncher {
 	public NativeLauncher() {
+		// load the native libary
 		System.loadLibrary("NativeLauncher");
 	}
 
-	public void launchWindowsFile(String name) {
-		nativeLaunchWindowsFile(name);
+	/**
+	 * @requires that we are running on Windows and
+	 *  that the String specify a valid file pathname
+	 *  or a valid URL
+	 * @effects launches the file with it's associated
+	 *  application on Windows. */
+	public void launchFileWindows(String name) {
+		nativeLaunchFileWindows(name);
 	}
 
-	private native void nativeLaunchWindowsFile(String name);
+	// native method for launching the specific file 
+	private native void nativeLaunchFileWindows(String name);
 }
