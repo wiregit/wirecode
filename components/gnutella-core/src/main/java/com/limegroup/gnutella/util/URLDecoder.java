8a5914fcb7a617dd1d4d5046bf7e0cd3b935e9d7
afisk <afisk>
2001-04-27 19:10:34 +0000

    added URLDecoded class to decode an encoded url string

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/util/URLDecoder.java
@@ -0,0 +1,51 @@
+/**
+ * auth: afisk
+ * file: URLDecoder.java
+ * desc: Decodes a string from x-www-urlencoded format
+ */
+//2345678|012345678|012345678|012345678|012345678|012345678|012345678|012345678|
+package com.limegroup.gnutella.util;
+
+import java.io.*;
+
+public class URLDecoder {
+
+/**
+ * decodes a strong in x-www-urldecoded format and returns the 
+ * the decoded string.
+ */
+    public static String decode(String s) {
+        StringBuffer sb = new StringBuffer();
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case '+':
+                    sb.append(' ');
+                    break;
+                case '%':
+                    try {
+                        sb.append((char)Integer.parseInt(
+                                        s.substring(i+1,i+3),16));
+                    } catch (NumberFormatException e) {
+                        throw new IllegalArgumentException();
+                    }
+                    i += 2;
+                    break;
+                default:
+                    sb.append(c);
+                    break;
+            }
+        }
+        // Undo conversion to external encoding
+        String result = sb.toString();
+        try {
+            byte[] inputBytes = result.getBytes("8859_1");
+            result = new String(inputBytes);
+        } catch (UnsupportedEncodingException e) {
+            // The system should always have 8859_1
+        }
+        return result;
+    }
+}
+
+
