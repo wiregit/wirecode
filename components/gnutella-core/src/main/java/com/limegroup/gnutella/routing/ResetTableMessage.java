844578e62884c4cb3446551dacc2e6e3cd73ad87
crohrs <crohrs>
2001-05-11 16:01:07 +0000

    Simplified protocol so 'M' is always 4 bytes. Added the tableSize parameter to ResetTableMessage. Finished first implementation of SetDenseTableMessage.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/routing/ResetTableMessage.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/routing/ResetTableMessage.java
@@ -3,53 +3,82 @@ package com.limegroup.gnutella.routing;
 import com.limegroup.gnutella.*;
 import java.io.*;
 
+
+/**
+ * The RESET route table update message.
+ */
 public class ResetTableMessage extends RouteTableMessage {
+    private int tableSize;
+
+    /////////////////////////////// Encoding //////////////////////////////
+
     /**
-     * Creates a new ResetTableMessage from scratch.
+     * Creates a new ResetTableMessage from scratch.  The receiver
+     * should reset its tables for TTL tableTTL and higher.
+     *
+     * @param tableSize the size of the table in <b>bits</b>
+     *  REQUIRES: tableSize>1.
+     * @see RouteTableMessage
      */
-    public ResetTableMessage(byte ttl, byte tableTTL, byte tableAddressSize) {
-        //Paylod length is 3--not 0--because it includes common arguments
-        super(ttl, (byte)3, RouteTableMessage.RESET_VARIANT,
-              tableTTL, tableAddressSize);
+    public ResetTableMessage(byte ttl,
+                             byte tableTTL,
+                             int tableSize) {
+        //Payload length includes common arguments
+        super(ttl, (byte)(2+4), RouteTableMessage.RESET_VARIANT, tableTTL);
+        this.tableSize=tableSize;
     }
+
+    protected void writePayloadData(OutputStream out) throws IOException {
+        byte[] buf=new byte[4];
+        ByteOrder.int2leb(tableSize, buf, 0);
+        out.write(buf);   
+    }
+
+    
+    /////////////////////////////// Decoding ///////////////////////////////
 
     /**
      * Creates a new ResetTableMessage with data read from the network.  The
      * payload argument is the complete payload of the message.  The first byte
-     * is guaranteed to be RESET_VARIANT, and the second and third bytes are
-     * tableTTL and tableAddressSize, respectively.  These values will be passed
-     * to the superclass.
+     * is guaranteed to be RESET_VARIANT, and the second byte is tableTTL.
+     * These values will be passed to the superclass.
      * 
      * @exception BadPacketException the remaining values in payload are not
-     *  well-formed, e.g., because it's the wrong length.
+     *  well-formed, e.g., because it's the wrong length.  
      */
     protected ResetTableMessage(byte[] guid, 
                                 byte ttl, 
                                 byte hops,
-                                byte tableTTL, 
-                                byte tableAddressSize,
+                                byte tableTTL,
                                 byte[] payload) throws BadPacketException {
-        super(guid, ttl, hops, payload.length, RouteTableMessage.RESET_VARIANT,
-              tableTTL, tableAddressSize);
-        //TODO: maybe we shouldn't enforce this.
-        if (payload.length!=3)
+        super(guid, ttl, hops, payload.length, 
+              RouteTableMessage.RESET_VARIANT, tableTTL);
+        //TODO: maybe we shouldn't enforce this
+        if (payload.length!=(2+4))
             throw new BadPacketException("Extra arguments in reset message.");
+        tableSize=ByteOrder.leb2int(payload, 2);
+        if (tableSize<0)
+            throw new BadPacketException("Table too big.");                 
     }
 
-    protected void writePayloadData(OutputStream out) throws IOException {
-        //Does nothing; there's no data in this message.
+
+    /////////////////////////////// Accessors ///////////////////////////////
+
+    /** Returns the new size of the route table. */
+    public int getTableSize() {
+        return tableSize;
     }
+
 
     /** Unit test */
     public static void main(String args[]) {
         //From scratch.  Check encode/decode.
-        ResetTableMessage m=new ResetTableMessage((byte)3, (byte)7,
-                                                  (byte)2);
+        ResetTableMessage m=new ResetTableMessage((byte)3, (byte)7, 1024);
         Assert.that(m.getVariant()==ResetTableMessage.RESET_VARIANT);
         Assert.that(m.getTTL()==(byte)3);
         Assert.that(m.getTableTTL()==(byte)7);
-        Assert.that(m.getTableAddressSize()==(byte)2);
-        Assert.that(m.getLength()==3);
+        Assert.that(m.getTableSize()==1024);
+        Assert.that(m.getLength()==6);
         ByteArrayOutputStream out=new ByteArrayOutputStream();
         try {
             m.write(out);
@@ -61,20 +90,21 @@ public class ResetTableMessage extends RouteTableMessage {
         Assert.that(m.getVariant()==ResetTableMessage.RESET_VARIANT);
         Assert.that(m.getTTL()==(byte)3);
         Assert.that(m.getTableTTL()==(byte)7);
-        Assert.that(m.getTableAddressSize()==(byte)2);
-        Assert.that(m.getLength()==3);        
+        Assert.that(m.getTableSize()==1024);
+        Assert.that(m.getLength()==6);        
 
         //Read from bytes
-        byte[] message=new byte[23+3];
+        byte[] message=new byte[23+6];
         message[16]=Message.F_ROUTE_TABLE_UPDATE;            //function code
-        message[19]=(byte)3;                                 //payload length
+        message[17]=(byte)3;                                 //TTL
+        message[19]=(byte)6;                                 //payload length
         message[23+0]=(byte)RouteTableMessage.RESET_VARIANT; //reset variant
         message[23+1]=(byte)7;                               //table TTL
-        message[23+2]=(byte)2;                               //table address sz
+        message[23+3]=(byte)1;                               //size==256
         m=read(message);
         Assert.that(m.getVariant()==RouteTableMessage.RESET_VARIANT);
         Assert.that(m.getTableTTL()==(byte)7);
-        Assert.that(m.getTableAddressSize()==(byte)2);
+        Assert.that(m.getTableSize()==256);
     }
 
     static ResetTableMessage read(byte[] bytes) {
@@ -82,7 +112,7 @@ public class ResetTableMessage extends RouteTableMessage {
         try {
             return (ResetTableMessage)Message.read(in);
         } catch (Exception e) {
-            Assert.that(false, "Bad message");
+            Assert.that(false, "Bad message: "+e);
             return null;  //never executed
         } 
     }
--- components/gnutella-core/src/main/java/com/limegroup/gnutella/routing/RouteTableMessage.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/routing/RouteTableMessage.java
@@ -5,9 +5,15 @@ import java.io.*;
 
 /**
  * An abstract class representing all variants of the new ROUTE_TABLE_UPDATE
- * message.  Like Message, it has no public constructors.  To decode bytes from 
- * call the static read(..) method.  To create a new message from scratch, call
- * one of the subclass constructors.
+ * message.  Contains the variant name and the TTTL argument shared among all
+ * messages.<p>
+ *
+ * Like Message, this has no public constructors.  To decode bytes from call the
+ * static read(..) method.  To create a new message from scratch, call one of
+ * its subclass' constructors.<p>
+ *
+ * Subclasses of RouteTableMessage can handle tables of up to 2^31 bits.  Note
+ * however, that the protocol allows for tables of up to 2^32 bits. 
  */
 public abstract class RouteTableMessage extends Message {
     public static final byte RESET_VARIANT=(byte)0x0;
@@ -19,10 +25,11 @@ public abstract class RouteTableMessage extends Message {
      * Representation rationale: we never forward one of these packets without
      * decoding.  Hence we store the decoded packet, not the raw bytes.
      */
-
     private byte variant;
     private byte tableTTL;
-    private byte tableAddressSize;
+
+
+    //////////////////////////// Encoding /////////////////////////////////////
 
     /**
      * Creates a new RouteTableMessage from scratch.  The variant, tableTTL, and
@@ -32,41 +39,30 @@ public abstract class RouteTableMessage extends Message {
     protected RouteTableMessage(byte ttl,
                                 byte length, 
                                 byte variant,
-                                byte tableTTL,
-                                byte tableAddressSize) {
+                                byte tableTTL) {
         super(Message.F_ROUTE_TABLE_UPDATE, ttl, length);
         this.variant=variant;
         this.tableTTL=tableTTL;
-        this.tableAddressSize=tableAddressSize;
     }
 
-    /*
-     * Creates a new RouteTableMessage with data read from the network.
-     * 
-     * @param variant the type of update message.  REQUIRES: one of RESET_VARIANT,
-     *  SET_DENSE_BLOCK_VARIANT, ADD_SPARSE_BLOCK_VARIANT, or
-     *  REMOVE_SPARSE_BLOCK_VARIANT.
-     * @param tableTTL routing tables for queries with hops of tableTTL
-     *  and higher will be affected.  REQUIRES: tableTTL>0.
-     * @param tableAddressSize the number of bytes needed to store the size
-     *  of the routing table.  Typically 2 or 3, which allows for 2^(2*8) or
-     *  2^(3*8) bits in the bitmap.  REQUIRES: tableAddressSize>0.
-     *
-     * All other parameters are the same as in Message.
+
+    protected void writePayload(OutputStream out) throws IOException {
+        out.write(variant);
+        out.write(tableTTL);
+        writePayloadData(out);
+    }
+
+    /** 
+     * @modifies out
+     * @effects writes the data following the common variant and table TTL 
+     *  to out.  Does NOT flush out.
      */
-    protected RouteTableMessage(byte[] guid,
-                                byte ttl,
-                                byte hops,
-                                int  length,
-                                byte variant,
-                                byte tableTTL,
-                                byte tableAddressSize) {
-        super(guid, Message.F_ROUTE_TABLE_UPDATE, ttl, hops, length);
-        this.variant=variant;
-        this.tableTTL=tableTTL;
-        this.tableAddressSize=tableAddressSize;
-    }
+    protected abstract void writePayloadData(OutputStream out) 
+                                                      throws IOException;
 
+
+
+    //////////////////////////////// Decoding ////////////////////////////////
 
     /**
      * Creates a new RouteTableMessage from raw bytes read from the network.
@@ -82,51 +78,57 @@ public abstract class RouteTableMessage extends Message {
                                          byte[] payload) 
                                     throws BadPacketException {
         //Parse the common bytes of the payload...
-        if (payload.length<3)
+        if (payload.length<2)
             throw new BadPacketException("Payload too small");
         byte variant=payload[0];
+        //TODO: should this code be moved into the subclasses?
         byte tableTTL=payload[1];
-        byte tableAddressSize=payload[2];
-        if (tableTTL<0 || tableAddressSize<0)
-            throw new BadPacketException("Negative variant argument");
+        if (tableTTL<0)
+            throw new BadPacketException("Negative tableTTL");
         
         //...and pass them to the subclass' constructor, which will in turn
         //call this constructor.
         switch (variant) {
         case RESET_VARIANT:
-            return new ResetTableMessage(guid, ttl, hops,
-                                         tableTTL, tableAddressSize,
-                                         payload);
-//          case ADD_SPARSE_BLOCK_VARIANT:
-//              return new AddSparseTableMessage(guid, ttl, hops,
-//                                               tableTTL, tableAddressSize,
-//                                               payload);            
-//          case REMOVE_SPARSE_BLOCK_VARIANT:
-//              return new RemoveSparseTableMessage(guid, ttl, hops,
-//                                                  tableTTL, tableAddressSize,
-//                                                  payload);
+            return new ResetTableMessage(guid, ttl, hops, tableTTL, payload);
         case SET_DENSE_BLOCK_VARIANT:
-            return new SetDenseTableMessage(guid, ttl, hops,
-                                            tableTTL, tableAddressSize,
-                                            payload);
+            return new SetDenseTableMessage(guid, ttl, hops, tableTTL, payload);
+        case ADD_SPARSE_BLOCK_VARIANT:
+        case REMOVE_SPARSE_BLOCK_VARIANT:
         default:
+            //TODO: this should really return a dummy message that can 
+            //be forwarded without parsing.
             throw new BadPacketException("Unknown table variant");
         }
     }
 
-    protected void writePayload(OutputStream out) throws IOException {
-        out.write(variant);
-        out.write(tableTTL);
-        out.write(tableAddressSize);
-        writePayloadData(out);
+
+    /*
+     * Creates a new RouteTableMessage with data read from the network.  This
+     * is called by subclasses after the payload is fully decoded.
+     * 
+     * @param variant the type of update message.  REQUIRES: one of RESET_VARIANT,
+     *  SET_DENSE_BLOCK_VARIANT, ADD_SPARSE_BLOCK_VARIANT, or
+     *  REMOVE_SPARSE_BLOCK_VARIANT.
+     * @param tableTTL routing tables for queries with hops of tableTTL
+     *  and higher will be affected.  REQUIRES: tableTTL>0.
+     *
+     * @see com.limegroup.gnutella.Message
+     */
+    protected RouteTableMessage(byte[] guid,
+                                byte ttl,
+                                byte hops,
+                                int  length,
+                                byte variant,
+                                byte tableTTL) {
+        super(guid, Message.F_ROUTE_TABLE_UPDATE, ttl, hops, length);
+        this.variant=variant;
+        this.tableTTL=tableTTL;
     }
 
-    /** 
-     * @modifies out
-     * @effects writes the data following the common variant to the header.  
-     *  Does NOT flush out.
-     */
-    protected abstract void writePayloadData(OutputStream out) throws IOException;
+
+
+    ///////////////////////////// Accessors //////////////////////////////
 
     /**
      * Returns the variant of this, i.e. one of RESET_VARIANT,
@@ -144,21 +146,15 @@ public abstract class RouteTableMessage extends Message {
         return tableTTL;
     }
 
-    /** 
-     * Returns the number of bytes needed to represent the size of the route
-     * table.  Typically this value is quite small, e.g. 2 or 3. 
-     */
-    public byte getTableAddressSize() {
-        return tableAddressSize;
-    }
 
-   
-    public static void main(String[] args) {     
+    /** Unit test */
+    public static void main(String[] args) {                  
         //Read bytes with bad variant
-        byte[] message=new byte[23+3];
+        byte[] message=new byte[23+2];
         message[16]=Message.F_ROUTE_TABLE_UPDATE;            //function code
-        message[19]=(byte)3;                                 //payload length
-        message[23+0]=(byte)0xFF;                            //table address sz
+        message[17]=(byte)3;                                 //TTL
+        message[19]=(byte)2;                                 //payload length
+        message[23+0]=(byte)0xFF;                            //bogus variant
         InputStream in=new ByteArrayInputStream(message);
         try {
             Message m=(ResetTableMessage)Message.read(in);
@@ -169,12 +165,12 @@ public abstract class RouteTableMessage extends Message {
         }
 
         //Read bytes with negative values
-        message=new byte[23+3];
+        message=new byte[23+6];
         message[16]=Message.F_ROUTE_TABLE_UPDATE;            //function code
-        message[19]=(byte)3;                                 //payload length
+        message[17]=(byte)3;                                 //TTL
+        message[19]=(byte)6;                                 //payload length
         message[23+0]=(byte)RouteTableMessage.RESET_VARIANT; //reset variant
         message[23+1]=(byte)-2;                              //table TTL
-        message[23+2]=(byte)2;                               //table address sz
         in=new ByteArrayInputStream(message);
         try {
             Message m=(ResetTableMessage)Message.read(in);
@@ -185,3 +181,6 @@ public abstract class RouteTableMessage extends Message {
         }
     }
 }
+
+
+ 
