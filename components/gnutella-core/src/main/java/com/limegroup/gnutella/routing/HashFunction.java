07fe93b204aa0fc8226ef3fc64f06c8a5ca093b2
crohrs <crohrs>
2001-05-23 20:33:17 +0000

    Changed interface to allow tables that aren't a power of 2.  Added new implementation for slow case.  Clarified documentation.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/routing/HashFunction.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/routing/HashFunction.java
@@ -4,36 +4,58 @@ import com.limegroup.gnutella.util.StringUtils;
 import com.limegroup.gnutella.FileManager;
 
 /** 
- * The official platform-independent hashing function for query-routing.
+ * The official platform-independent hashing function for query-routing.  The
+ * key property is that it allows interpolation of hash tables of different
+ * sizes.  More formally k*hash(x,n)<=hash(x, kn)<=k*hash(x,n)+1.<p>
+ *
  * This experimental version does not necessarily work cross-platform,
- * not is it secure in any sense.
+ * however, nor is it secure in any sense.   See Chapter 12.3.2. of 
+ * for details of multiplication-based algorithms.
  */
 public class HashFunction {
     private static final double A=(Math.sqrt(5.0)-1.0)/2.0;
     private static final long TWO_31=0x80000000l;
     private static final int A_INT=(int)(A*TWO_31);
     
-    
-    /**
-     * Returns the n-bit hash of x, where n="bits".  That is, the returned value
-     * value can fit in "bits" unsigned bits.
+    /*
+     * Implementation note: we special case the algorith depending on 
+     * whether the hash table size is a power of two.
      */
-    private static int hash(int x, int bits) {
+    
+//      /**
+//       * Returns the n-<b>bit</b> hash of x, where n="bits".  That is, the
+//       * returned value value can fit in "bits" unsigned bits.
+//       *     @requires n is a power of two.
+//       */
+//      private static int hashFast(int x, int n) {
+//          Assert.that(n%2==0, "Precondition to hashFast violated.");
+//          //Multiplication-based hash function.  See Chapter 12.3.2. of CLR.
+//          int prod=x*A_INT;
+//          int ret=prod>>>(31-log2(n));
+//          return ret;
+//      }
+
+    /**
+     * Returns the hash of x.  The returned value is greater than 0 and
+     * less than n.
+     */
+    private static int hashSlow(int x, int n) {
         //Multiplication-based hash function.  See Chapter 12.3.2. of CLR.
-        int prod=x*A_INT;
-        int ret=prod>>>(31-bits);
-        return ret;
+        double prod=A*x;
+        double scale=prod-Math.floor(prod);
+        return (int)Math.floor(((float)n) * scale);
     }
 
     /**
-     * Returns the n-bit hash of x, where n="bits".  That is, the returned value
-     * value can fit in "bits" unsigned bits.
+     * Returns the hash of x.  The returned value is greater than 0 and
+     * less than n.
+     *     @requires 1<=bits<=32 
      */    
-    public static int hash(String x, int bits) {
+    public static int hash(String x, int n) {
         //TODO: This is just a temporary hack.  The real algorithm obviously shouldn't
         //be tied to the JDK.  Note that we don't just return x.hashCode()%bits;
         //that wouldn't allow resizing of tables.
-        return hash(x.hashCode(), bits);
+        return hashSlow(x.hashCode(), n);
     }       
 
     /** 
@@ -52,11 +74,19 @@ public class HashFunction {
     }
 
     public static void main(String args[]) {
-        for (int i=0; i<100; i++) {
-            System.out.println(hash(i, 7));
-        }
-        System.out.println(hash("Hello Chris", 10));
-        System.out.println(hash("Hallo Chris", 10));
+        //TODO: we should verify the scaling property described in the header
+        //above.
+        System.out.println(hash("Hello", 256));
+        System.out.println(hash("Hallo", 256));
+        System.out.println(hash("Hellu", 256));
+        System.out.println();
+        System.out.println(hash("Hello", 257));
+        System.out.println(hash("Hallo", 257));
+        System.out.println(hash("Hellu", 257));
+        System.out.println();                           
+        System.out.println(hash("Hello", 512));
+        System.out.println(hash("Hallo", 512));
+        System.out.println(hash("Hellu", 512));
     }
 
 
