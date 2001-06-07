427a2d35de9ec89b1b1e856a0dd3928331ab6595
afisk <afisk>
2001-06-07 19:26:32 +0000

    added .img extension for mac users

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/SettingsInterface.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/SettingsInterface.java
@@ -51,7 +51,7 @@ public interface SettingsInterface {
     public static final String  DEFAULT_EXTENSIONS     =
     "html;htm;xml;txt;pdf;ps;rtf;doc;tex;mp3;wav;au;aif;aiff;ra;ram;"+
     "mpg;mpeg;asf;qt;mov;avi;mpe;swf;dcr;gif;jpg;jpeg;jpe;png;tif;tiff;"+
-    "exe;zip;gz;gzip;hqx;tar;tgz;z;rmj;lqt;rar;ace;sit;smi";
+    "exe;zip;gz;gzip;hqx;tar;tgz;z;rmj;lqt;rar;ace;sit;smi;img";
 
 
 	/** the number of uplads allowed per person at a given time */
