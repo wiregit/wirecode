2202aecbb3ed4e99f156dbf32073bfff8c7de451
asingla <asingla>
2001-07-05 18:33:04 +0000

    commented out the code, as it didnt compile

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/tests/UpdateTester.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/tests/UpdateTester.java
@@ -17,70 +17,70 @@ import com.limegroup.gnutella.gui.LimeLookAndFeel;
 
 public class UpdateTester extends JFrame {
 
-	private UpdateManager _manager;
-
-	private JComboBox _versionBox;
-
-	private JComboBox _osBox;
-
-	public UpdateTester() {
-		super("Update Tester");
-		_manager = UpdateManager.instance();
-		Dimension dim = new Dimension(400, 200);
-		setSize(dim);
-		JPanel mainPanel = new JPanel();
-		JPanel boxPanel = new JPanel();
-		JPanel buttonPanel = new JPanel();
-		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-		boxPanel.setLayout(new BoxLayout(boxPanel, BoxLayout.X_AXIS));
-		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
-		
-		String[] versionArray = {"1.1", "1.2","1.2b","1.3","1.4","1.4b"}; 
-		String[] osArray = {"Windows NT", "Windows 95", "Windows 98", "Mac OS",
-							"Mac OS X", "Linux", "Solaris", "Other"};
-
-		_versionBox = new JComboBox(versionArray);
-		_osBox = new JComboBox(osArray);
-
-		JButton startTestButton = new JButton("Start Test");
-		startTestButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				startTest();
-			}
-		});
-
-		buttonPanel.add(startTestButton);
-		boxPanel.add(_versionBox);
-		boxPanel.add(Box.createHorizontalStrut(6));
-		boxPanel.add(_osBox);
-		
-		mainPanel.add(boxPanel);
-		mainPanel.add(Box.createVerticalStrut(6));
-		mainPanel.add(buttonPanel);
-		getContentPane().add(mainPanel);
-	}
-
-	/**
-	 * starts the test.
-	 */
-	private void startTest() {
-		String version = (String)_versionBox.getSelectedItem();
-		String os = (String)_osBox.getSelectedItem();
-		//_manager.startTest(version,os);
-	}
-
-	/**
-	 * main method to kick off the test.
-	 */
-	public static void main(String args[]) {
-		CommonUtils.initialize();
-		SettingsManager.instance();
-        FileManager.instance();
-        try {UIManager.setLookAndFeel(new LimeLookAndFeel());}
-        catch (UnsupportedLookAndFeelException ulafe) {}
-		UpdateTester tester = new UpdateTester();
-		//UpdateManager manager = UpdateManager.instance();
-		//manager.startTest("1.1", "Mac OS");
-		tester.setVisible(true);
-	}
+//	private UpdateManager _manager;
+//
+//	private JComboBox _versionBox;
+//
+//	private JComboBox _osBox;
+//
+//	public UpdateTester() {
+//		super("Update Tester");
+//		_manager = UpdateManager.instance();
+//		Dimension dim = new Dimension(400, 200);
+//		setSize(dim);
+//		JPanel mainPanel = new JPanel();
+//		JPanel boxPanel = new JPanel();
+//		JPanel buttonPanel = new JPanel();
+//		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+//		boxPanel.setLayout(new BoxLayout(boxPanel, BoxLayout.X_AXIS));
+//		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+//		
+//		String[] versionArray = {"1.1", "1.2","1.2b","1.3","1.4","1.4b"}; 
+//		String[] osArray = {"Windows NT", "Windows 95", "Windows 98", "Mac OS",
+//							"Mac OS X", "Linux", "Solaris", "Other"};
+//
+//		_versionBox = new JComboBox(versionArray);
+//		_osBox = new JComboBox(osArray);
+//
+//		JButton startTestButton = new JButton("Start Test");
+//		startTestButton.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//				startTest();
+//			}
+//		});
+//
+//		buttonPanel.add(startTestButton);
+//		boxPanel.add(_versionBox);
+//		boxPanel.add(Box.createHorizontalStrut(6));
+//		boxPanel.add(_osBox);
+//		
+//		mainPanel.add(boxPanel);
+//		mainPanel.add(Box.createVerticalStrut(6));
+//		mainPanel.add(buttonPanel);
+//		getContentPane().add(mainPanel);
+//	}
+//
+//	/**
+//	 * starts the test.
+//	 */
+//	private void startTest() {
+//		String version = (String)_versionBox.getSelectedItem();
+//		String os = (String)_osBox.getSelectedItem();
+//		//_manager.startTest(version,os);
+//	}
+//
+//	/**
+//	 * main method to kick off the test.
+//	 */
+//	public static void main(String args[]) {
+//		CommonUtils.initialize();
+//		SettingsManager.instance();
+//        FileManager.instance();
+//        try {UIManager.setLookAndFeel(new LimeLookAndFeel());}
+//        catch (UnsupportedLookAndFeelException ulafe) {}
+//		UpdateTester tester = new UpdateTester();
+//		//UpdateManager manager = UpdateManager.instance();
+//		//manager.startTest("1.1", "Mac OS");
+//		tester.setVisible(true);
+//	}
 }
