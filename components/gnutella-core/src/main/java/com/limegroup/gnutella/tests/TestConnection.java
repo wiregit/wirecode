e4e893e150c5ba3092d4d7354bdb537ece28d4ef
crohrs <crohrs>
2001-08-13 16:13:38 +0000

    Added scary license, reduced imports.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/tests/TestConnection.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/tests/TestConnection.java
@@ -1,6 +1,35 @@
+/*
+ * Copyright (c) 2001 by Lime Wire LLC.  All Rights Reserved.
+ *
+ * Lime Wire grants you ("Licensee") a non-exclusive, royalty free, license to
+ * use, modify and redistribute this software in source and binary code form,
+ * provided that i) this copyright notice and license appear on all copies of
+ * the software; and ii) Licensee does not utilize the software in a manner
+ * which is disparaging to Lime Wire.
+ *
+ * This software is provided "AS IS," without a warranty of any kind. ALL
+ * EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING ANY
+ * IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR
+ * NON-INFRINGEMENT, ARE HEREBY EXCLUDED. LIME WIRE AND ITS LICENSORS SHALL NOT
+ * BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING,
+ * MODIFYING OR DISTRIBUTING THE SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL
+ * LIME WIRE OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR
+ * FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES,
+ * HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE
+ * USE OF OR INABILITY TO USE SOFTWARE, EVEN IF LIME WIRE HAS BEEN ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGES.
+ *
+ * This software is not designed or intended for use in on-line control of
+ * aircraft, air traffic, aircraft navigation or aircraft communications; or in
+ * the design, construction, operation or maintenance of any nuclear
+ * facility.  Licensee represents and warrants that it will not use or
+ * redistribute the Software for such purposes.  
+ */
 package com.limegroup.gnutella.tests;
 
-import com.limegroup.gnutella.*;
+import com.limegroup.gnutella.Message;
+import com.limegroup.gnutella.PingRequest;
+import com.limegroup.gnutella.BadPacketException;
 import java.io.*;
 import java.net.*;
 
