4638e0be760d6a27226c2a4fcd14268bb27030a5
crohrs <crohrs>
2001-08-14 20:18:19 +0000

    Changed version to 2.0-alpha.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/util/CommonUtils.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/util/CommonUtils.java
@@ -15,7 +15,7 @@ import java.io.*;
 public class CommonUtils {
 
 	// constant for the current version of LimeWire
-	private static final String LIMEWIRE_VERSION = "1.7-alpha";
+	private static final String LIMEWIRE_VERSION = "2.0-alpha";
 	
 	// variable for the system properties
 	private static Properties _props;
