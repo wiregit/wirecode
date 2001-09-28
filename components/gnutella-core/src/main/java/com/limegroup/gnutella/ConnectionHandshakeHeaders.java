95a72078175ea38fe724be4f14ff2e4fe5c92275
anurag <anurag>
2001-09-28 14:25:11 +0000

    defines the string constants for the headers used in connection handshake

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/ConnectionHandshakeHeaders.java
@@ -0,0 +1,18 @@
+/*
+ * ConnectionHandshakeHeaders.java
+ *
+ * Created on September 27, 2001, 3:20 PM
+ */
+
+package com.limegroup.gnutella;
+
+/**
+ * Provides names for the headers used in the gnutella connection handshake
+ * @author Anurag Singla
+ */
+public class ConnectionHandshakeHeaders
+{
+    public static final String MY_ADDRESS       = "My-Address";
+    public static final String SUPERNODE        = "Supernode";
+    public static final String X_TRY            = "X-Try";
+}
