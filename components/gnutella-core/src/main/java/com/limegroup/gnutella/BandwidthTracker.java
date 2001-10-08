fb36507415a44ca3394ca1f9783067048876f07c
afisk <afisk>
2001-10-08 21:01:33 +0000

    new interface defining the skeletal methods for classes that want to track their bandwidth usage

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/BandwidthTracker.java
@@ -0,0 +1,19 @@
+package com.limegroup.gnutella;
+
+/**
+ * This interface outlines the functionality that any class wanting to track
+ * bandwidth must implement.
+ */
+//2345678|012345678|012345678|012345678|012345678|012345678|012345678|012345678|
+public interface BandwidthTracker {
+	
+	/**
+	 * Returns the number of new bytes passed since the last time this
+	 * method was called.
+	 *
+	 * @return the number of bytes passed since the last time this method 
+	 *         was called
+	 */
+	int getNewBytesTransferred();
+}
+
