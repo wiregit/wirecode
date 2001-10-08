e8253550eed331f725bb69f47c9bd8c95b9fcb3c
afisk <afisk>
2001-10-08 21:25:25 +0000

    added implementation of new BandwidthTracker interface for tracking of bandwidth used for downloads

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/downloader/ManagedDownloader.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/downloader/ManagedDownloader.java
@@ -129,6 +129,12 @@ public class ManagedDownloader implements Downloader, Serializable {
     /** The number of tries we've made.  0 means on the first try. */
     private int tries;
 
+	/**
+	 * Stores the number of bytes read the last time there was a reading
+	 * taken.
+	 */
+	private int _oldAmountRead = 0;
+
     private FileManager fileManager;
 
 
@@ -844,7 +850,20 @@ public class ManagedDownloader implements Downloader, Serializable {
         return retArray;
     }
 
-
+	/**
+	 * Implements the <tt>BandwidthTracker</tt> interface.
+	 * Returns the number of bytes read since the last time this method
+	 * was called.
+	 *
+	 * @return the number of new bytes read since the last time this method
+	 *         was called
+	 */
+	public synchronized int getNewBytesTransferred() {
+  		int newAmountRead = getAmountRead();
+  		int newBytesTransferred = newAmountRead - _oldAmountRead;
+  		_oldAmountRead = newAmountRead;
+  		return newBytesTransferred;
+	}
 }
 
 /** A RemoteFileDesc and the number of times we've tries to push it. */
