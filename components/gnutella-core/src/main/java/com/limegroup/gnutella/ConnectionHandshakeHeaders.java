6cfb5b11ce763d76777706143984e17da6665414
anurag <anurag>
2001-10-24 22:26:02 +0000

    updated from supernode-branch

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/ConnectionHandshakeHeaders.java
@@ -0,0 +1,21 @@
+/*
+ * ConnectionHandshakeHeaders.java
+ *
+ * Created on September 27, 2001, 3:20 PM
+ */
+
+package com.limegroup.gnutella;
+
+/**
+ * Provides names for the headers used in the gnutella connection handshake
+ * @author Anurag Singla
+ */
+public class ConnectionHandshakeHeaders
+{
+    public static final String MY_ADDRESS       = "My-Address";
+    public static final String SUPERNODE        = "Supernode";
+    public static final String X_TRY            = "X-Try";
+    public static final String QUERY_ROUTING    = "Query-Routing";
+    public static final String PONG_CACHING     = "Pong-Caching";
+    public static final String SUPERNODE_NEEDED = "Supernode-Needed";
+}
