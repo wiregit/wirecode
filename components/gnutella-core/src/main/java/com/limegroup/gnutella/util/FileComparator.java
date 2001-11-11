2df2d64367476ce07dc6d307b42181e589d58746
crohrs <crohrs>
2001-11-11 18:24:31 +0000

    Made Serializable, for use with downloads.dat.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/util/FileComparator.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/util/FileComparator.java
@@ -1,6 +1,6 @@
 package com.limegroup.gnutella.util;
 
-import java.io.File;
+import java.io.*;
 import com.sun.java.util.collections.Comparator;
 
 /**
@@ -8,7 +8,7 @@ import com.sun.java.util.collections.Comparator;
  * Files in Java 1.2+ sorted collections classes.  This is needed because Files
  * in 1.1.8 do not implement the Comparable interface, unlike Strings in 1.2+.
  */
-public class FileComparator implements Comparator {
+public class FileComparator implements Comparator, Serializable {
     /** Returns (((File)a).getAbsolutePath()).compareTo(
      *              ((File)b).getAbsolutePath()) 
      *  Typically you'll want to make sure a and b are canonical files,
