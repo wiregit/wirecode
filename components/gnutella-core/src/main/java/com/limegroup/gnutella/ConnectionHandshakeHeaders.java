11bef58ef718d830f977989f771b697e659095bf
anurag <anurag>
2001-11-12 20:53:57 +0000

    Added a property to send Supernode Endpoints in headers

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/ConnectionHandshakeHeaders.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/ConnectionHandshakeHeaders.java
@@ -15,6 +15,7 @@ public class ConnectionHandshakeHeaders
     public static final String MY_ADDRESS       = "My-Address";
     public static final String SUPERNODE        = "Supernode";
     public static final String X_TRY            = "X-Try";
+    public static final String X_TRY_SUPERNODES = "X-Try-Supernodes";
     public static final String QUERY_ROUTING    = "Query-Routing";
     public static final String PONG_CACHING     = "Pong-Caching";
     public static final String SUPERNODE_NEEDED = "Supernode-Needed";
