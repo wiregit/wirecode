427269ee97bea6f98a48d94836f191978d53eb6b
crohrs <crohrs>
2001-12-05 20:46:56 +0000

    Replaced the word 'Supernode' with 'Ultrapeer' in GUI and protocol.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/ConnectionHandshakeHeaders.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/ConnectionHandshakeHeaders.java
@@ -13,11 +13,11 @@ package com.limegroup.gnutella;
 public class ConnectionHandshakeHeaders
 {
     public static final String X_MY_ADDRESS         = "X-My-Address";
-    public static final String X_SUPERNODE          = "X-Supernode";
+    public static final String X_SUPERNODE          = "X-Ultrapeer";
     public static final String X_TRY                = "X-Try";
-    public static final String X_TRY_SUPERNODES     = "X-Try-Supernodes";
+    public static final String X_TRY_SUPERNODES     = "X-Try-Ultrapeers";
     public static final String X_QUERY_ROUTING      = "X-Query-Routing";
     public static final String X_PONG_CACHING       = "X-Pong-Caching";
-    public static final String X_SUPERNODE_NEEDED   = "X-Supernode-Needed";
+    public static final String X_SUPERNODE_NEEDED   = "X-Ultrapeer-Needed";
     public static final String USER_AGENT           = "User-Agent";
 }
