a8fb1c76cffb2f214490915cec6b0a1dd9a135c9
crohrs <crohrs>
2001-12-14 22:25:37 +0000

    Returns zero bytes/sec until some progress has been made.  This helps prevent spikes when resuming uploads and downloads.  Unfortunately this doesn't quite solve all problems with downloaders.  Also avoids negative bandwidth measurements and divide by zero.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/BandwidthTrackerImpl.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/BandwidthTrackerImpl.java
@@ -24,9 +24,17 @@ public class BandwidthTrackerImpl implements Serializable {
      */
     public synchronized void measureBandwidth(int amountRead) {
         long currentTime=System.currentTimeMillis();
-        //Remember that bytes/msec=KB/sec
-        measuredBandwidth=(float)(amountRead-lastAmountRead)
-                            / (float)(currentTime-lastTime);
+        //We always discard the first sample, and any others until after
+        //progress is made.  This prevents sudden bandwidth spikes when resuming
+        //uploads and downloads.  Remember that bytes/msec=KB/sec.
+        if (lastAmountRead==0 || currentTime==lastTime) {
+            measuredBandwidth=0.f;
+        } else {            
+            measuredBandwidth=(float)(amountRead-lastAmountRead)
+                                / (float)(currentTime-lastTime);
+            //Ensure positive!
+            measuredBandwidth=Math.max(measuredBandwidth, 0.f);
+        }
         lastTime=currentTime;
         lastAmountRead=amountRead;        
     }
