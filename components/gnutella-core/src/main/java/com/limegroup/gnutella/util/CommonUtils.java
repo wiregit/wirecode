d272306ab1835ff79a416c25e9157baa18c95646
afisk <afisk>
2001-12-17 22:39:25 +0000

    Changed the version to 2.0

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/util/CommonUtils.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/util/CommonUtils.java
@@ -15,7 +15,7 @@ public final class CommonUtils {
 	/** 
 	 * Constant for the current version of LimeWire.
 	 */
-	private static final String LIMEWIRE_VERSION = "1.9c";
+	private static final String LIMEWIRE_VERSION = "2.0";
 	
 	/** 
 	 * Constant for the java system properties.
