63b66b81d2aacdc052fca43cc9d39c2fa7421062
crohrs <crohrs>
2002-01-11 19:57:46 +0000

    Removed 'rich query without schema' assertion, replacing it with an IF statement.  We're not sure why the repeat search feature could pass a null schemaURI, but it's easy to program around it.  Besides, the code that used schemaURI isn't currently used.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/RouterService.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/RouterService.java
@@ -603,26 +603,26 @@ public class RouterService
      */
     public byte[] query(String query, String richQuery, 
                         int minSpeed, MediaType type, String schemaURI) {
-        //System.out.println("Sumeet rich query coming...");
-        Assert.that(schemaURI!=null,"rich query without schema");
         QueryRequest qr=new QueryRequest(SettingsManager.instance().getTTL(),
                                          minSpeed, query, richQuery);
         verifier.record(qr, type);
         router.broadcastQueryRequest(qr);
         //Rich query? Check if there are special servers to send this query to
         //Then spawn the RichConnectionThread and send the rich query out w/ it
-        try{
-            XMLHostCache xhc = new XMLHostCache();
-            String[] ips = xhc.getCachedHostsForURI(schemaURI);
-            if(ips!=null){
-                for(int i=0;i<ips.length;i++){//usually just  1 iteration
-                    Thread rcThread=new 
-                                      RichConnectionThread(ips[i],qr,callback);
-                    rcThread.start();
+        if (schemaURI!=null) {
+            try{
+                XMLHostCache xhc = new XMLHostCache();
+                String[] ips = xhc.getCachedHostsForURI(schemaURI);
+                if(ips!=null){
+                    for(int i=0;i<ips.length;i++){//usually just  1 iteration
+                        Thread rcThread=new 
+                        RichConnectionThread(ips[i],qr,callback);
+                        rcThread.start();
+                    }
                 }
+            }catch(Exception e){
+                //do nothing
             }
-        }catch(Exception e){
-            //do nothing
         }
         return qr.getGUID();
     }
