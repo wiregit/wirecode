0bd6f4c2d357555ac9d3245865da8e0d4bb93086
crohrs <crohrs>
2002-01-11 00:29:05 +0000

    A first pass.  Only checks redirects, not routing.

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/tests/LeafTester.java
@@ -0,0 +1,221 @@
+package com.limegroup.gnutella.tests;
+
+import com.limegroup.gnutella.*;
+import com.limegroup.gnutella.routing.*;
+import java.util.Properties;
+import java.util.StringTokenizer;
+import com.sun.java.util.collections.*;
+import java.io.*;
+import java.net.*;
+
+/**
+ * Out-of-process test to check whether (multi)leaves avoid forwarding messages
+ * to ultrapeers, do redirects properly, etc.  This test is interactive; you
+ */
+public class LeafTester {
+    static final int PORT=6347;
+    static final int TIMEOUT=500;
+    static final byte[] ultrapeerIP=
+        new byte[] {(byte)18, (byte)239, (byte)0, (byte)144};
+    static final byte[] oldIP=
+        new byte[] {(byte)111, (byte)22, (byte)33, (byte)44};
+
+    static Connection ultrapeer1;
+    static Connection ultrapeer2;
+    static Connection old1;
+    static Connection old2;
+
+    public static void main(String args[]) {
+        System.out.println(
+            "Please make sure you have a leaf running with no connections,\n"
+           +"listening on port 6346, without any connection fetchers\n");
+        
+        try {
+            connect();
+            testRedirect();
+            shutdown();
+        } catch (IOException e) { 
+            System.err.println("Mysterious IOException:");
+            e.printStackTrace();
+        }   catch (BadPacketException e) { 
+            System.err.println("Mysterious bad packet:");
+            e.printStackTrace();
+        }
+        
+        System.out.println("Done");
+    }
+
+    ////////////////////////// Initialization ////////////////////////
+
+    private static void connect() throws IOException, BadPacketException {
+        System.out.println("Please establish a connection to localhost:6350\n");
+        ultrapeer1=new Connection(accept(6350), new UltrapeerResponder());
+        ultrapeer1.initialize();
+        replyToPing(ultrapeer1, true);
+
+        System.out.println("Please establish a connection to localhost:6351\n");
+        ultrapeer2=new Connection(accept(6351), new UltrapeerResponder());
+        ultrapeer2.initialize();
+        replyToPing(ultrapeer2, true);
+
+        System.out.println("Please establish a connection to localhost:6352\n");
+        old1=new Connection(accept(6352), new OldResponder());
+        old1.initialize();
+        replyToPing(old1, false);
+
+        System.out.println("Please establish a connection to localhost:6353\n");
+        old2=new Connection(accept(6353), new OldResponder());
+        old2.initialize();
+        replyToPing(old2, false);
+    }
+
+    private static Socket accept(int port) throws IOException { 
+        ServerSocket ss=new ServerSocket(port);
+        Socket s=ss.accept();
+        InputStream in=s.getInputStream();
+        String word=readWord(in);
+        if (! word.equals("GNUTELLA"))
+            throw new IOException("Bad word: "+word);
+        return s;
+    }
+
+    /**
+     * Acceptor.readWord
+     *
+     * @modifies sock
+     * @effects Returns the first word (i.e., no whitespace) of less
+     *  than 8 characters read from sock, or throws IOException if none
+     *  found.
+     */
+    private static String readWord(InputStream sock) throws IOException {
+        final int N=9;  //number of characters to look at
+        char[] buf=new char[N];
+        for (int i=0 ; i<N ; i++) {
+            int got=sock.read();
+            if (got==-1)  //EOF
+                throw new IOException();
+            if ((char)got==' ') { //got word.  Exclude space.
+                return new String(buf,0,i);
+            }
+            buf[i]=(char)got;
+        }
+        throw new IOException();
+    }
+
+    private static void replyToPing(Connection c, boolean ultrapeer) 
+            throws IOException, BadPacketException {
+        Message m=c.receive(500);
+        Assert.that(m instanceof PingRequest);
+        PingRequest pr=(PingRequest)m;
+        byte[] localhost=new byte[] {(byte)127, (byte)0, (byte)0, (byte)1};
+        PingReply reply=new PingReply(pr.getGUID(), (byte)7,
+                                      c.getLocalPort(), 
+                                      ultrapeer ? ultrapeerIP : oldIP,
+                                      0, 0, ultrapeer);
+        reply.hop();
+        c.send(reply);
+        c.flush();
+    }
+
+    ///////////////////////// Actual Tests ////////////////////////////
+
+    private static void testRedirect() {
+        Connection c=new Connection("127.0.0.1", 6346,
+                                    new Properties(),
+                                    new OldResponder(),
+                                    false);
+        try {
+            c.initialize();
+            Assert.that(false, "Handshake succeeded!");
+        } catch (IOException e) {
+            String hosts=c.getProperty(ConnectionHandshakeHeaders.X_TRY);
+            System.out.println("X-Try: "+hosts);
+            Assert.that(hosts!=null);
+            Set s=list2set(hosts);
+            Assert.that(s.size()==2);
+            Assert.that(s.contains(new Endpoint(oldIP, 6352)));
+            Assert.that(s.contains(new Endpoint(oldIP, 6353)));
+
+            hosts=c.getProperty(
+                ConnectionHandshakeHeaders.X_TRY_SUPERNODES);
+            System.out.println("X-Try-Ultrapeers: "+hosts);
+            Assert.that(hosts!=null);
+            s=list2set(hosts);
+            Assert.that(s.size()==4);
+            byte[] localhost=new byte[] {(byte)127, (byte)0, (byte)0, (byte)1};
+            Assert.that(s.contains(new Endpoint(ultrapeerIP, 6350)));
+            Assert.that(s.contains(new Endpoint(ultrapeerIP, 6351)));
+            Assert.that(s.contains(new Endpoint(localhost, 6350))); 
+            Assert.that(s.contains(new Endpoint(localhost, 6351)));
+        }
+    }
+
+
+    /** Converts the given X-Try[-Ultrapeer] header value to
+     *  a Set of Endpoints. */
+    private static Set /* of Endpoint */ list2set(String addresses) {
+        Set ret=new HashSet();
+        StringTokenizer st = new StringTokenizer(addresses,
+            Constants.HTTP_ENTRY_SEPARATOR);
+        while(st.hasMoreTokens()){
+            //get an address
+            String address = ((String)st.nextToken()).trim();
+            Endpoint e;
+            try {
+                e = new Endpoint(address);
+                ret.add(e);
+            } catch(IllegalArgumentException iae) {
+                Assert.that(false, "Bad endpoint");
+            }
+        }
+        return ret;
+    }
+
+    //////////////////////////////////////////////////////////////////
+
+    /** Tries to receive any outstanding messages on c 
+     *  @return true if this got a message */
+    private static boolean drain(Connection c) throws IOException {
+        boolean ret=false;
+        while (true) {
+            try {
+                Message m=c.receive(500);
+                ret=true;
+                //System.out.println("Draining "+m+" from "+c);
+            } catch (InterruptedIOException e) {
+                return ret;
+            } catch (BadPacketException e) {
+            }
+        }
+    }
+
+    private static void shutdown() throws IOException {
+        System.out.println("\nShutting down.");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) { }
+        ultrapeer1.close();
+        ultrapeer2.close();
+        old1.close();
+        old2.close();
+    }
+}
+
+class UltrapeerResponder implements HandshakeResponder {
+    public HandshakeResponse respond(HandshakeResponse response, 
+            boolean outgoing) throws IOException {
+        Properties props=new Properties();
+        props.put(ConnectionHandshakeHeaders.X_QUERY_ROUTING, "0.1");
+        props.put(ConnectionHandshakeHeaders.X_SUPERNODE, "True");
+        return new HandshakeResponse(props);
+    }
+}
+
+
+class OldResponder implements HandshakeResponder {
+    public HandshakeResponse respond(HandshakeResponse response, 
+            boolean outgoing) throws IOException {
+        Properties props=new Properties();
+        return new HandshakeResponse(props);
+    }
+}
