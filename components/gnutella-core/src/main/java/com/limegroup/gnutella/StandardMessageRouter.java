9ea4788d136d4f3e6a69589515c0a1806b63974c
anurag <anurag>
2002-01-24 21:15:28 +0000

    factored out the reusable code from the sendResponses() method in StandardMessageRouter to responsesToQueryReplies() in MessageRouter This const method is now used by other classes also (including BrowseHostUploadState)

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/StandardMessageRouter.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/StandardMessageRouter.java
@@ -228,93 +228,19 @@ public class StandardMessageRouter
         if ( (responses == null) || ((responses.length < 1)) )
             return;
 
-        // get the appropriate queryReply information
-        byte[] guid = queryRequest.getGUID();
-        byte ttl = (byte)(queryRequest.getHops() + 1);
-        int port = acceptor.getPort();
-        byte[] ip = acceptor.getAddress();
-
-        //Return measured speed if possible, or user's speed otherwise.
-        long speed = _uploadManager.measuredUploadSpeed();
-        boolean measuredSpeed=true;
-        if (speed==-1) {
-            speed=SettingsManager.instance().getConnectionSpeed();
-            measuredSpeed=false;
-        }
-
-        int numResponses = responses.length;
-        int index = 0;
-
-        int numHops = queryRequest.getHops();
-
-        QueryReply queryReply;
-
-        // modified by rsoule, 11/16/00
-
-        while (numResponses > 0)
-        {
-            int arraySize;
-            // if there are more than 255 responses,
-            // create an array of 255 to send in the queryReply
-            // otherwise, create an array of whatever size is left.
-            if (numResponses < 255) {
-                // break;
-                arraySize = numResponses;
-            }
-            else
-                arraySize = 255;
-
-            Response[] res;
-            // a special case.  in the common case where there
-            // are less than 256 responses being sent, there
-            // is no need to copy one array into another.
-            if ( (index == 0) && (arraySize < 255) )
-            {
-                res = responses;
-            }
-            else
-            {
-                res = new Response[arraySize];
-                // copy the reponses into bite-size chunks
-                for(int i =0; i < arraySize; i++) {
-                    res[i] = responses[index];
-                    index++;
-                }
-            }
-
-            // decrement the number of responses we have left
-            numResponses-= arraySize;
-
-			// see id there are any open slots
-			boolean busy = _uploadManager.isBusy();
-            boolean uploaded = _uploadManager.hadSuccesfulUpload();
-
-			// see if we have ever accepted an incoming connection
-			boolean incoming = _acceptor.acceptedIncoming();
-
-			boolean chat = SettingsManager.instance().getChatEnabled();
-
-            // create the new queryReply
-            queryReply = new QueryReply(guid, ttl, port, ip,
-                                        speed, res, clientGUID, 
-										!incoming, busy, uploaded, measuredSpeed,
-                                        chat);  //supports chat
-
-            // try to send the new queryReply
-            try {
+        //convert responses to QueryReplies
+        Iterator /*<QueryReply>*/ iterator = responsesToQueryReplies(responses,
+            queryRequest);
+        
+        //send the query replies
+        try {
+            while(iterator.hasNext()) {
+                QueryReply queryReply = (QueryReply)iterator.next();
                 sendQueryReply(queryReply);
-            } catch (IOException e) {
-                // if there is an error, do nothing..
             }
-
-            // we only want to send multiple queryReplies
-            // if the number of hops is small.
-            if (numHops > 2) {
-                break;
-            }
-
-        }//end of while
-
+        } catch (IOException e) {
+            // if there is an error, do nothing..
+        }
     }
 
     /** @see MessageRouter.addQueryRoutingEntries */
