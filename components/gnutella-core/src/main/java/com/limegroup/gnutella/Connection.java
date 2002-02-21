6d98fdfb19b5ec687d6b6c46ed927d03ae262b3a
crohrs <crohrs>
2002-02-21 23:54:49 +0000

    Moved MiniAcceptor from Connection into its own class in tests package. Useful for testing.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/Connection.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/Connection.java
@@ -901,7 +901,8 @@ public class Connection {
 //                                            Properties outProperties1,
 //                                            HandshakeResponder outProperties2) {
 //          ConnectionPair ret=new ConnectionPair();
-//          MiniAcceptor acceptor=new MiniAcceptor(inProperties);
+//          com.limegroup.gnutella.tests.MiniAcceptor acceptor=
+//              new com.limegroup.gnutella.tests.MiniAcceptor(inProperties);
 //          try {
 //              ret.out=new Connection("localhost", 6346,
 //                                     outProperties1, outProperties2,
@@ -920,60 +921,5 @@ public class Connection {
 //              cp.in.close();
 //          if (cp.out!=null)
 //              cp.out.close();
-//      }
-    
-
-//      private static class MiniAcceptor implements Runnable {
-//          Object lock=new Object();
-//          Connection c=null;
-//          boolean done=false;
-
-//          HandshakeResponder properties;
-        
-//          /** Starts the listen socket without blocking. */
-//          public MiniAcceptor(HandshakeResponder properties) {
-//              this.properties=properties;
-//              Thread runner=new Thread(this);
-//              runner.start();
-//              Thread.yield();  //hack to make sure runner creates socket
-//          }
-
-//          /** Blocks until a connection is available, and returns it. 
-//           *  Returns null if something went awry. */
-//          public Connection accept() {
-//              synchronized (lock) {
-//                  while (! done) {
-//                      try {
-//                          lock.wait();
-//                      } catch (InterruptedException e) {
-//                          return null;
-//                      }
-//                  }
-//                  return c;
-//              }
-//          }
-        
-//          /** Don't call.  For internal use only. */
-//          public void run() {
-//              try {
-//                  ServerSocket ss=new ServerSocket(6346);
-//                  Socket s=ss.accept();
-//                  //Technically "GNUTELLA " should be read from s.  Turns out that
-//                  //out implementation doesn't care;
-//                  Connection c=new Connection(s, properties);
-//                  c.initialize();
-//                  ss.close();
-//                  synchronized (lock) {
-//                      this.c=c;
-//                      done=true;
-//                      lock.notify();
-//                  } 
-//              } catch (IOException e) {
-//                  synchronized (lock) {
-//                      done=true;
-//                      lock.notify();
-//                  } 
-//              }
-//          }
-//      }
+//      }    
 }
