c821f38da25aa2e7e5fe0cf5e6de6e613eff7bb3
crohrs <crohrs>
2002-03-06 21:43:44 +0000

    Changing version to 2.2.4.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/util/CommonUtils.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/util/CommonUtils.java
@@ -15,7 +15,7 @@ public final class CommonUtils {
 	/** 
 	 * Constant for the current version of LimeWire.
 	 */
-	private static final String LIMEWIRE_VERSION = "2.2.2";
+	private static final String LIMEWIRE_VERSION = "2.2.4";
 	
 	/** 
 	 * Constant for the java system properties.
