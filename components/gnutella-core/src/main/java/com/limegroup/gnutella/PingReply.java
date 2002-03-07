800d52f6a3abab6244afca9fb6e44ec93a4f5786
crohrs <crohrs>
2002-03-07 18:57:09 +0000

    Optimized isPowerOf2. Submitted by: andy.hedges@aspect-proteus.com Reviewed by: crohrs@limewire.com

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/PingReply.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/PingReply.java
@@ -124,31 +124,8 @@ public class PingReply extends Message implements Serializable{
     private static boolean isPowerOf2(int x) {
         if (x<=0)
             return false;
-        else {
-            //TODO3: faster, more clever algorithms are possible by using 
-            //the bit inversion operation and addition.
-            switch (x) {
-            case (1<<0):    case(1<<1):
-            case (1<<2):    case(1<<3):
-            case (1<<4):    case(1<<5):
-            case (1<<6):    case(1<<7):
-            case (1<<8):    case(1<<9):
-            case (1<<10):    case(1<<11):
-            case (1<<12):    case(1<<13):
-            case (1<<14):    case(1<<15):
-            case (1<<16):    case(1<<17):
-            case (1<<18):    case(1<<19):
-            case (1<<20):    case(1<<21):
-            case (1<<22):    case(1<<23):
-            case (1<<24):    case(1<<25):
-            case (1<<26):    case(1<<27):
-            case (1<<28):    case(1<<29):
-            case (1<<30):    case(1<<31):
-                return true;
-            default:
-                return false; 
-            }               
-        }
+        else
+            return (x&(x - 1)) == 0;
     }
 
     /** Marks the given kbytes field */
