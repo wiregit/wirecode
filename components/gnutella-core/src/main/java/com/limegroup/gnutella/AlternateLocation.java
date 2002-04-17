9f96f186e070a0709a3c1d489140f07e520334eb
afisk <afisk>
2002-04-17 21:58:37 +0000

    new class that abstracts out HUGE's alternate location header, providing parsing and a public utility API

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/AlternateLocation.java
@@ -0,0 +1,241 @@
+package com.limegroup.gnutella;
+
+import java.net.*;
+import java.util.*;
+import java.text.*;
+import java.io.*;
+
+/**
+ * This class encapsulates the data for an alternate resource location, as 
+ * specified in HUGE v0.93.  This also provides utility methods for such 
+ * operations as comparing alternate locations based on the date they were 
+ * stored.<p>
+ *
+ * This class is immutable.
+ */
+//2345678|012345678|012345678|012345678|012345678|012345678|012345678|012345678|
+public final class AlternateLocation {
+
+	/**
+	 * A <tt>URL</tt> instance for the URL specified in the header.
+	 */
+	private final URL URL;
+
+	/**
+	 * <tt>Date</tt> instance for conveniently making time comparisons
+	 * betwween <tt>AlternateLocation</tt> instances.
+	 */
+	private final Date DATE;
+
+	/**
+	 * The <tt>String</tt> instance as it is written in the header.
+	 */
+	private final String OUTPUT_DATE_TIME;
+	
+
+	/**
+	 * Constructs a new <tt>AlternateLocation</tt> instance 
+	 * based on the specified string argument.  
+	 *
+	 * @param LOCATION a string containing a single alternate 
+	 *  location, including a full URL for a file and an
+	 *  optional date
+	 */
+	public AlternateLocation(final String LOCATION) throws IOException {
+		URL = AlternateLocation.extractHttpUrl(LOCATION);
+		OUTPUT_DATE_TIME = AlternateLocation.extractDateTimeString(LOCATION);
+		DATE = AlternateLocation.createDateInstance(OUTPUT_DATE_TIME);
+	}
+
+	/**
+	 * Returns the <tt>URL</tt> instance for this alternate location.
+	 *
+	 * @return the <tt>URL</tt> instance for this alternate location
+	 */
+	public URL getUrl() {
+		// this is fine because the URL class is immutable
+		return URL;
+	}
+
+	/**
+	 * Returns the <tt>Date</tt> instance for this alternate location.
+	 *
+	 * @return the <tt>Date</tt> instance for this alternate location
+	 */
+	public Date getDate() {
+		// this is fine because the Date class is immutable
+		return DATE;
+	}
+
+	/**
+	 * Parses out the time-date string from the alternate location
+	 * header string, throwing an exception if there is any error.
+	 *
+	 * @param LOCATION the full alternate-location HTTP header string,
+	 *  as specified in HUGE v0.93
+	 * @return the date/time string from the the alternate location
+	 *  header
+	 * @throws <tt>IOException</tt> if there's any error extracting the
+	 *  the date/time from the alternate location
+	 */
+	private static String extractDateTimeString(final String LOCATION) 
+		throws IOException {
+		int dateIndex = LOCATION.lastIndexOf(" ");
+		if((dateIndex == -1) ||
+		   ((dateIndex+1) >= LOCATION.length())) {
+			throw new IOException("ERROR EXTRACTING DATE TIME STRING");
+		}
+		return LOCATION.substring(dateIndex+1); 
+	}
+
+	/**
+	 * Creates a new <tt>Date</tt> instance from the date specified in
+	 * the alternate location header.
+	 *
+	 * @param DATE_TIME_STRING the extracted date-time string from the
+	 *  alternate location header
+	 * @return a new <tt>Date</tt> instance for the date specified in
+	 *  the header
+	 * @throws <tt>IOException</tt> if there is any problem creating
+	 *  the date
+	 */
+	private static Date createDateInstance(final String DATE_TIME_STRING) 
+		throws IOException {
+		int dateTimeSepIndex = DATE_TIME_STRING.indexOf("T");
+
+		// if there's no "T", or it f
+		if((dateTimeSepIndex == -1) || 
+		   ((dateTimeSepIndex+1) >= DATE_TIME_STRING.length()) ||
+		   (DATE_TIME_STRING.length() != 20)) {
+			throw new IOException("INVALID DATE/TIME STRING");
+		}
+		String YYYYMMDD = DATE_TIME_STRING.substring(0, dateTimeSepIndex);
+		System.out.println("YYYYMMDD: "+YYYYMMDD); 
+		String hhmmss = 
+		    DATE_TIME_STRING.substring(dateTimeSepIndex+1, 
+									   DATE_TIME_STRING.length()-1);
+		System.out.println("hhmmss: "+hhmmss);
+		StringTokenizer stdate = new StringTokenizer(YYYYMMDD, "-");
+		if(stdate.countTokens() != 3) {
+			throw new IOException("INVALID DATE FORMAT");
+		}
+		String YYYYStr = stdate.nextToken();
+		String MMStr   = stdate.nextToken();
+		String DDStr   = stdate.nextToken();
+		int YYYY = Integer.parseInt(YYYYStr);
+
+		// we subtract 1 because the Calendar class uses 0 for January,
+		// whereas the ISO 8601 subset we're using uses 1 for January
+		int MM   = Integer.parseInt(MMStr)-1; 
+		int DD   = Integer.parseInt(DDStr);		
+ 
+		StringTokenizer sttime = new StringTokenizer(hhmmss, ":");
+		if(sttime.countTokens() != 3) {
+			throw new IOException("INVALID TIME FORMAT");
+		}
+		String hhStr = sttime.nextToken();
+		String mmStr = sttime.nextToken();
+		String ssStr = sttime.nextToken();
+		int hh = Integer.parseInt(hhStr);
+		int mm = Integer.parseInt(mmStr);
+		int ss = Integer.parseInt(ssStr);		
+
+		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+		cal.clear();
+		cal.set(YYYY, MM, DD, hh, mm, ss);
+		return cal.getTime();
+	}
+
+	private static URL extractHttpUrl(final String LOCATION_HEADER) 
+		throws IOException {
+		int urlIndex  = LOCATION_HEADER.indexOf(" ");
+		int dateIndex = LOCATION_HEADER.lastIndexOf(" ");
+		if((urlIndex == -1) ||
+		   (urlIndex == dateIndex) ||
+		   ((dateIndex+1) >= LOCATION_HEADER.length())) {
+			throw new IOException("ERROR EXTRACTING URL STRING");
+		}
+		String urlString = LOCATION_HEADER.substring(urlIndex+1, dateIndex);
+		System.out.println("urlString:"+urlString); 
+		return new URL(urlString);
+	}
+
+	/**
+	 * Returns whether this <tt>AlternateLocation</tt> instance is
+	 * older than the <tt>AlternateLocation</tt> argument.
+	 * 
+	 * @return <tt>true</tt> if the date for this <tt>AlternateLocation</tt>
+	 *  is older than the date for the <tt>AlternateLocation</tt>
+	 *  argument, and otherwise returns <tt>false</tt> 
+	 */
+	public boolean isOlderThan(AlternateLocation loc) {
+		return DATE.before(loc.getDate());
+	}
+
+	/**
+	 * Returns whether this <tt>AlternateLocation</tt> instance is
+	 * newer than the <tt>AlternateLocation</tt> argument.
+	 * 
+	 * @return <tt>true</tt> if the date for this <tt>AlternateLocation</tt>
+	 *  is newer than the date for the <tt>AlternateLocation</tt>
+	 *  argument, and otherwise returns <tt>false</tt> 
+	 */
+	public boolean isNewerThan(AlternateLocation loc) {
+		return DATE.after(loc.getDate());
+	}
+
+	/**
+	 * Overrides the equals method to accurately compare 
+	 * <tt>AlternateLocation</tt> instances.  <tt>AlternateLocation</tt>s 
+	 * are equal if their <tt>URL</tt>s and <tt>Date</tt>s are equal.
+	 *
+	 * @param obj the <tt>Object</tt> instance to compare to
+	 * @return <tt>true</tt> if the <tt>URL</tt> and <tt>Date</tt> of this
+	 *  <tt>AlternateLocation</tt> are equal to the <tt>URL</tt> and 
+	 *  <tt>Date</tt> of the <tt>AlternateLocation</tt> location argument,
+	 *  and otherwise returns <tt>false</tt>
+	 */
+	public boolean equals(Object obj) {
+		if(obj == this) return true;
+		if(!(obj instanceof AlternateLocation)) return false;
+		AlternateLocation al = (AlternateLocation)obj;
+		Date date = al.getDate();
+		URL url = al.getUrl();
+		return (this.DATE.equals(date) ||
+				this.URL.equals(url));
+	}
+
+	/**
+	 * Overrides toString to return a string representation of this 
+	 * <tt>AlternateLocation</tt>, namely the url and the date.
+	 *
+	 * @return the string representation of this alternate location
+	 */
+	public String toString() {
+		return this.URL.toString()+" "+OUTPUT_DATE_TIME;
+	}
+
+	/*
+	public static void main(String[] args) {
+		String alt = "Alt-Location: http://Y.Y.Y.Y:6352/get/2/"+
+		"lime%20capital%20management%2001.mpg "+
+		"2002-04-09T20:32:33Z";
+		try {
+			AlternateLocation al = new AlternateLocation(alt);
+			//System.out.println(al.getDate()); 
+			System.out.println(al); 
+		} catch(IOException e) {
+			e.printStackTrace();
+		}		
+	}
+	*/
+}
+
+
+
+
+
+
+
+
+
