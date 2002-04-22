02d3e63d5700b45db819e7c65f50f5945dd0f80d
afisk <afisk>
2002-04-22 19:24:16 +0000

    changed one constructor to also take a Set of URNs as an argument and changed the equals method to fix other problems and incorporate the URNs

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/RemoteFileDesc.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/RemoteFileDesc.java
@@ -24,6 +24,7 @@ public class RemoteFileDesc implements Serializable {
 	private final boolean _chatEnabled;
     private final int _quality;
     private final LimeXMLDocument[] _xmlDocs;
+	private final Set _urns;
 
 	/** 
      * Constructs a new RemoteFileDesc without metadata.
@@ -43,7 +44,7 @@ public class RemoteFileDesc implements Serializable {
 						  int size, byte[] clientGUID, int speed, 
 						  boolean chat, int quality) {	   
         this(host, port, index, filename, size,
-             clientGUID, speed, chat, quality, null);
+             clientGUID, speed, chat, quality, null, null);
 	}
 
 	/** 
@@ -63,7 +64,8 @@ public class RemoteFileDesc implements Serializable {
 	 */
 	public RemoteFileDesc(String host, int port, long index, String filename,
 						  int size, byte[] clientGUID, int speed, 
-						  boolean chat, int quality, LimeXMLDocument[] xmlDocs) {
+						  boolean chat, int quality, LimeXMLDocument[] xmlDocs,
+						  Set urns) {
 		_speed = speed;
 		_host = host;
 		_port = port;
@@ -74,13 +76,21 @@ public class RemoteFileDesc implements Serializable {
 		_chatEnabled = chat;
         _quality = quality;
 
-		if(xmlDocs != null) {
+		if(xmlDocs == null) {
+			_xmlDocs = null;
+		}
+		else {
 			// make a defensive copy of the xml docs array so no one can 
 			// mutate this class
 			_xmlDocs = new LimeXMLDocument[xmlDocs.length];
 			System.arraycopy(xmlDocs, 0, _xmlDocs, 0, xmlDocs.length);
-		} else {
-			_xmlDocs = null;
+		}
+		if(urns == null) {
+			_urns = null;
+		}
+		else {
+			// make a defensive copy
+			_urns = new HashSet(urns);
 		}
 	}
 
@@ -117,6 +127,7 @@ public class RemoteFileDesc implements Serializable {
 	/** Returns true iff o is a RemoteFileDesc with the same value as this.
      *  Priority and number of attempts is ignored in doing the comparison! */
     public boolean equals(Object o) {
+		if(o == this) return true;
         if (! (o instanceof RemoteFileDesc))
             return false;
         RemoteFileDesc other=(RemoteFileDesc)o;
@@ -129,7 +140,11 @@ public class RemoteFileDesc implements Serializable {
             && _index==other._index 
             && Arrays.equals(_clientGUID, other._clientGUID)
             && _speed==other._speed
-            && _size==other._size;
+            && _size==other._size
+		// this doesn't work since XMLDocuments doesn't override equals, but
+		// that's another issue
+		    && Arrays.equals(_xmlDocs, other._xmlDocs) 
+		    && _urns.equals(other._urns);
     }
 
     public String toString() {
