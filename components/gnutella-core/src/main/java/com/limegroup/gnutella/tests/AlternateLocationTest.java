d65f8ae3868979a57995ca8d4583d6eecc104d60
afisk <afisk>
2002-05-10 18:56:35 +0000

    added test of the equals method

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/tests/AlternateLocationTest.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/tests/AlternateLocationTest.java
@@ -2,10 +2,11 @@ package com.limegroup.gnutella.tests;
 
 import com.sun.java.util.collections.*;
 import com.limegroup.gnutella.*;
+import com.limegroup.gnutella.util.CommonUtils;
 import junit.framework.*;
 import junit.extensions.*;
 import java.io.*;
-import java.util.*;
+import java.util.Date;
 import java.net.*;
 
 /**
@@ -13,24 +14,24 @@ import java.net.*;
  */
 public final class AlternateLocationTest extends TestCase {
 	private static final String[] validTimestampedLocs = {
-		"Alternate-Location: http://Y.Y.Y.Y:6352/get/2/"+
-		"lime%20capital%20management%2001.mpg "+
-		"2002-04-09T20:32:33Z",
-		"Alt-Location: http://Y.Y.Y.Y:6352/get/2/"+
-		"lime%20capital%20management%2001.mpg "+
-		"2002-04-09T20:32:33Z",
-		"Alt-Location: http://Y.Y.Y.Y:6352/get/2/"+
-		"lime%20capital%20management%2001.mpg "+
-		"2002-04-09T20:32:33Z",
-		"X-Gnutella-Alternate-Location: http://Y.Y.Y.Y:6352/get/2/"+
-		"lime%20capital%20management%2001.mpg "+
-		"2002-04-09T20:32:33Z",
 		"http://Y.Y.Y.Y:6352/get/2/"+
-		"lime%20capital%20management%2001.mpg "+
-		"2002-04-09T20:32:33Z",
-		"http: //Y.Y.Y.Y:6352/get/2/"+
-		"lime%20capital%20management%2001.mpg "+
-		"2002-04-09T20:32:33Z"
+		                     "lime%20capital%20management%2001.mpg "+
+		                     "2002-04-09T20:32:33Z",
+		"http://Y.Y.Y.Y:6352/get/2/"+
+		               "lime%20capital%20management%2002.mpg "+
+		               "2002-04-09T20:32:33Z",
+		"http://Y.Z.Y.Y:6352/get/2/"+
+		               "lime%20capital%20management%2001.mpg "+
+		               "2002-04-09T20:32:33Z",
+		"http://Y.W.Y.Y:6352/get/2/"+
+		               "lime%20capital%20management%2001.mpg "+
+		               "2002-04-09T20:32:33Z",
+		"http://Y.T.Y.Y:6352/get/2/"+
+		               "lime%20capital%20management%2001.mpg "+
+		               "2002-04-09T20:32:33Z",
+		"http: //Y.R.Y.Y:6352/get/2/"+
+		               "lime%20capital%20management%2001.mpg "+
+		               "2002-04-09T20:32:33Z",
 	};
 
 	private static final String[] validlocs = {
@@ -110,23 +111,11 @@ public final class AlternateLocationTest extends TestCase {
 				AlternateLocation al4 = 
 				    new AlternateLocation("http://"+validURLS[i] + "/test.htm"+
 										  " "+AlternateLocation.convertDateToString(new Date()));
-				URL urlTest1 = al1.getUrl();
-				URL urlTest2 = al2.getUrl();
-				String dateStr1 = "2002-04-09T20:32:33Z";
-				String dateStr2 = "2002-04-09T20:32:33Z";
-				assertEquals(al1, al3);
-				assertEquals(al2, al4);
-				assertEquals(url1, urlTest1);
-				assertEquals(url2, urlTest2);
-				assertEquals(dateStr1, dateStr2);
 			}
 		} catch(IOException e) {
 			// this also catches MalformedURLException
-
-			assertTrue(false);
-			System.out.println("TEST FAILED WITH EXCEPTION: "); 
-			System.out.println(e); 
-			e.printStackTrace();
+			assertTrue("AlternateLocation constructor should not have thrown an "+
+					   "exception", false);
 		}
 	}
 
@@ -178,5 +167,72 @@ public final class AlternateLocationTest extends TestCase {
 			assertTrue("test failed: valid date not considered valid\r\ndate: "+date+
 					   "\r\ndate string: "+dateStr, false);
 		}	   
+	}
+
+	/**
+	 * Test the equals method.
+	 */
+	public void testAlternateLocationEquals() {
+		TreeMap timeStampedAltLocs0 = new TreeMap();
+		TreeMap testMap = new TreeMap();
+		for(int i=0; i<validTimestampedLocs.length; i++) {
+			try {
+				AlternateLocation al0 = new AlternateLocation(validTimestampedLocs[i]);
+				AlternateLocation al1 = new AlternateLocation(validTimestampedLocs[i]);
+				timeStampedAltLocs0.put(al0, al0);
+				testMap.put(al1, al1);
+				//timeStampedAltLocs0.add(al);
+			} catch(IOException e) {
+				assertTrue("unexpected exception: "+e, false);
+			}
+		}
+
+		assertEquals("maps should be equal", timeStampedAltLocs0, testMap);
+		TreeMap timeStampedAltLocs1 = new TreeMap(timeStampedAltLocs0);
+		Iterator iter0 = timeStampedAltLocs0.values().iterator();
+		Iterator iter1 = timeStampedAltLocs1.values().iterator();
+		while(iter0.hasNext()) {
+			AlternateLocation al0 = (AlternateLocation)iter0.next();
+			AlternateLocation al1 = (AlternateLocation)iter1.next();
+			assertEquals("alternate location values should be equal", al0, al1);
+		}
+
+		Iterator iter20 = timeStampedAltLocs0.keySet().iterator();
+		Iterator iter21 = timeStampedAltLocs1.keySet().iterator();
+		while(iter20.hasNext()) {
+			AlternateLocation al0 = (AlternateLocation)iter20.next();
+			AlternateLocation al1 = (AlternateLocation)iter21.next();
+			assertEquals("alternate location keys should be equal", al0, al1);
+		}
+
+		Iterator iter30 = timeStampedAltLocs0.entrySet().iterator();
+		Iterator iter31 = timeStampedAltLocs1.entrySet().iterator();
+		while(iter20.hasNext()) {
+			Map.Entry al0 = (Map.Entry)iter30.next();
+			Map.Entry al1 = (Map.Entry)iter31.next();
+			assertEquals("alternate location entries should be equal", al0, al1);
+		}
+
+		assertEquals("sizes should be equal", timeStampedAltLocs1.size(), 
+					 timeStampedAltLocs0.size());
+		Iterator i = timeStampedAltLocs0.entrySet().iterator();
+		while (i.hasNext()) {
+			Map.Entry e = (Map.Entry) i.next();
+			Object key   = e.getKey();
+			Object value = e.getValue();
+			if (value == null) {
+				if (!(timeStampedAltLocs1.get(key)==null && 
+					  timeStampedAltLocs1.containsKey(key)))
+					assertTrue("key not found", false);
+			} else {
+				if (!value.equals(timeStampedAltLocs1.get(key)))
+					assertTrue("value not found", false);
+			}
+		}
+
+		// the following does not work for some reason, but I'm not at all sure 
+		// why, especially considering that all of the above code passes the 
+		// assertions??
+		//assertEquals("maps should be equal: "+timeStampedAltLocs0, timeStampedAltLocs1);
 	}
 }
