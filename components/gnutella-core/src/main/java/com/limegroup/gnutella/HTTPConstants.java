9d4e2f997b9fca3edfc2223cdc615ffba6ca3aab
afisk <afisk>
2002-05-14 17:54:02 +0000

    removed constants that are now handled by HTTPHeaderName and HTTPUtils

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/HTTPConstants.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/HTTPConstants.java
@@ -10,44 +10,6 @@ public final class HTTPConstants {
      * instantiated.
      */
     private HTTPConstants() {}
-	
-	/**
-	 * Constant for the character that marks the end of an HTTP 
-	 * header key.
-	 */
-	private static final String HEADER_KEY_END = ":";
-
-	/**
-	 * Constant for the "X-Gnutella-Content-URN" header for specifying
-	 * the URN in a HTTP request or an HTTP response.
-	 */
-	public static final String CONTENT_URN_HEADER = 
-		"X-Gnutella-Content-URN" + HEADER_KEY_END;
-	
-	/**
-	 * Constant for the "X-Gnutella-Alternate-Location" header for 
-	 * specifying alternate file locations in a HTTP request or an 
-	 * HTTP response.
-	 */
-	public static final String ALTERNATE_LOCATION_HEADER = 
-		"X-Gnutella-Alternate-Location" + HEADER_KEY_END;
-
-	/**
-	 * Constant for a US-ASCII carriage return followed by a US-ASCII 
-	 * linefeed -- the standard ending for various HTTP messages and 
-	 * fields.
-	 */
-	public static final String CRLF = "\r\n";
-
-	/**
-	 * Constant for the Server response-header field that identifies
-	 * the server for the request.  Note that this does not specify
-	 * the version for security reasons, as specifying the version 
-	 * would allow any versions with security holes to be more 
-	 * easily identified and potentially compromised.
-	 */
-	public static final String SERVER_FIELD = 
-		"Server: LimeWire" + CRLF;
 
 	/**
 	 * Constant for the beginning "GET" of an HTTP URN get request.
