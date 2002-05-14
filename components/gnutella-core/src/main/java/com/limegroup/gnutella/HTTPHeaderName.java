bc3662a1a2438eea30d6b2de8738e2110a10140a
afisk <afisk>
2002-05-14 17:53:29 +0000

    added a constant for the lower-case representation of the header name as well as a method for checking if a string starts with the string representation of this header name, ignoring case

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/HTTPHeaderName.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/HTTPHeaderName.java
@@ -12,13 +12,21 @@ public class HTTPHeaderName {
 	private final String NAME;
 
 	/**
+	 * Constant for the lower-case representation of the header name.
+	 */
+	private final String LOWER_CASE_NAME;
+
+	/**
 	 * Private constructor for creating the "enum" of header names.
+	 * Making the constructor private also ensures that this class
+	 * cannot be subclassed.
 	 *
 	 * @param name the string header as it is written out to the
 	 *  network
 	 */
 	private HTTPHeaderName(final String name) {
 		NAME = name;
+		LOWER_CASE_NAME = name.toLowerCase();
 	}
 
 	/**
@@ -32,6 +40,19 @@ public class HTTPHeaderName {
 	 */
 	public static final HTTPHeaderName CONTENT_URN =
 		new HTTPHeaderName("X-Gnutella-Content-URN");
+
+	/**
+	 * Returns whether or not the start of the passed in string matches the 
+	 * string representation of this HTTP header, ignoring case.
+	 *
+	 * @param str the string to check for a match
+	 * @return <tt>true</tt> if the passed in string matches the string
+	 *  representation of this HTTP header (ignoring case), otherwise
+	 *  returns <tt>false</tt>
+	 */
+	public boolean matchesStartOfString(String str) {
+		return LOWER_CASE_NAME.startsWith(str.toLowerCase());
+	}
 
 	/**
 	 * Accessor to obtain the string representation of the header
