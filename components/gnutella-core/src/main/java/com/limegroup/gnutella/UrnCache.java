25b4a9eef9e30e379ac38d5b4322c0bd679b1840
afisk <afisk>
2002-05-17 21:03:51 +0000

    added a private key class for the urn cache as opposed to the former laborious string parsing and modified comments

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/UrnCache.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/UrnCache.java
@@ -4,6 +4,9 @@
  *
  * Modified by Gordon Mohr (2002/02/19): Added URN storage, calculation, caching
  * Repackaged by Greg Bildson (2002/02/19): Moved to dedicated class.
+ *
+ * @see FileDesc
+ * @see URN
  */
 
 package com.limegroup.gnutella;
@@ -29,7 +32,7 @@ public final class UrnCache {
     /**
      * UrnCache container
      */
-    private Map /* String -> HashSet */ theUrnCache;
+    private Map /* URNSetKey -> HashSet */ theUrnCache;
 
     /**
 	 * Returns the <tt>UrnCache</tt> instance.
@@ -52,34 +55,29 @@ public final class UrnCache {
     
     /**
      * Find any URNs remembered from a previous session for the specified
-	 * <tt>File</tt> instance.  The returned <tt>HashSet</tt> is
+	 * <tt>File</tt> instance.  The returned <tt>Set</tt> is
 	 * guaranteed to be non-null, but it may be empty.
 	 *
 	 * @param file the <tt>File</tt> instance to look up URNs for
-	 * @return a new <tt>HashSet</tt> containing any cached URNs for the
+	 * @return a new <tt>Set</tt> containing any cached URNs for the
 	 *  speficied <tt>File</tt> instance, guaranteed to be non-null, but
 	 *  possibly empty
      */
-    public Collection getUrns(File file) {
-		long modTime = file.lastModified();
-		String path = file.getAbsolutePath();
-		Collection urns = new HashSet();
-
-        /** one or more "urn:" names for this file */
-
+    public Set getUrns(File file) {
         // don't trust failed mod times
-        if (modTime==0L) 
-		    return urns; 
+        if (file.lastModified() == 0L) {
+			return Collections.EMPTY_SET;
+		} 
+		URNSetKey key = new URNSetKey(file);
+
+        // one or more "urn:" names for this file 
         
-        Collection cachedUrns = (Collection)theUrnCache.get(modTime+" "+path);
-        if(cachedUrns!=null) {
-            Iterator iter = cachedUrns.iterator();
-            while(iter.hasNext()){
-                URN urn = (URN)iter.next();
-                urns.add(urn);
-            }
-        } // else just leave urns empty for now
-		return urns;
+		Set cachedUrns = (Set)theUrnCache.get(key);
+		if(cachedUrns == null) {
+			return Collections.EMPTY_SET;
+		}
+
+		return cachedUrns;
     }
 
 
@@ -89,12 +87,9 @@ public final class UrnCache {
 	 *
 	 * @param fileDesc the <tt>FileDesc</tt> instance containing URNs to store
      */
-    public void persistUrns(FileDesc fileDesc) {
-		File file = fileDesc.getFile();
-		Collection urns = fileDesc.getUrns();
-		long modTime = file.lastModified();
-		String path = file.getAbsolutePath();
-        theUrnCache.put(modTime+" "+path, urns);
+    public void addUrns(File file, Set urns) {
+		URNSetKey key = new URNSetKey(file);
+        theUrnCache.put(key, urns);
     }
     
     //
@@ -120,12 +115,11 @@ public final class UrnCache {
         // discard outdated info
         Iterator iter = theUrnCache.keySet().iterator();
         while (iter.hasNext()) {
-            String key = (String)iter.next();
-            long modTime=Long.parseLong(key.substring(0,key.indexOf(' ')));
-            String path=key.substring(key.indexOf(' ')+1);
+			URNSetKey key = (URNSetKey)iter.next();
+
             // check to see if file still exists unmodified
-            File f = new File(path);
-            if (!f.exists()||f.lastModified()!=modTime) {
+            File f = new File(key.PATH);
+            if (!f.exists() || f.lastModified() != key.MOD_TIME) {
                 iter.remove();
             }
         }
@@ -144,6 +138,82 @@ public final class UrnCache {
             // no great loss
         }
     }
+
+	/**
+	 * Private class for the key for the set of URNs for files.
+	 */
+	private static class URNSetKey {
+		
+		/**
+		 * Constant for the file modification time.
+		 */
+		private final long MOD_TIME;
+
+		/**
+		 * Constant for the file path.
+		 */
+		private final String PATH;
+
+		/**
+		 * Constant cached hash code, since this class is used exclusively
+		 * as a hash key.
+		 */
+		private final int HASH_CODE;
+
+		/**
+		 * Constructs a new <tt>URNSetKey</tt> instance from the specified
+		 * <tt>File</tt> instance.
+		 *
+		 * @param file the <tt>File</tt> instance to use in constructing the
+		 *  key
+		 */
+		private URNSetKey(File file) {
+			MOD_TIME = file.lastModified();
+			PATH = file.getAbsolutePath();
+			HASH_CODE = calculateHashCode();
+		}
+
+		/**
+		 * Helper method to calculate the hash code.
+		 *
+		 * @return the hash code for this instance
+		 */
+		private int calculateHashCode() {
+			int result = 17;
+			result = result*37 + (int)(MOD_TIME ^(MOD_TIME >>> 32));
+			result = result*37 + PATH.hashCode();
+			return result;
+		}
+
+		/**
+		 * Overrides Object.equals so that keys with equal paths and modification
+		 * times will be considered equal.
+		 *
+		 * @param o the <tt>Object</tt> instance to compare for equality
+		 * @return <tt>true</tt> if the specified object is the same instance
+		 *  as this object, or if it has the same modification time and the same
+		 *  path, otherwise returns <tt>false</tt>
+		 */
+		public boolean equals(Object o) {
+			if(this == o) return true;
+			if(!(o instanceof URNSetKey)) return false;
+			URNSetKey key = (URNSetKey)o;
+
+			// note that the path is guaranteed to be non-null
+			return ((MOD_TIME == key.MOD_TIME) &&
+					(PATH.equals(key.PATH)));
+		}
+
+		/**
+		 * Overrides Object.hashCode to meet the specification of Object.equals
+		 * and to make this class function properly as a hash key.
+		 *
+		 * @return the hash code for this instance
+		 */
+		public int hashCode() {
+			return HASH_CODE;
+		}
+	}
 }
 
 
