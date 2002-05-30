2dfe6ca1b7c5e41332d9ed9551f011e96826b07b
afisk <afisk>
2002-05-30 21:46:11 +0000

    new additions from Phillipe to keep this working

--- components/gnutella-core/buildCoreJar.bat
+++ components/gnutella-core/buildCoreJar.bat
@@ -1,8 +1,10 @@
 @ECHO OFF
 cd ..\core
-jar -cvf ..\gui\lib\core.jar com\limegroup\gnutella\connection\*.class
+jar -cvf ..\gui\lib\core.jar com\bitzi\util\*.class
+jar -uvf ..\gui\lib\core.jar com\limegroup\gnutella\connection\*.class
 jar -uvf ..\gui\lib\core.jar com\limegroup\gnutella\chat\*.class
 jar -uvf ..\gui\lib\core.jar com\limegroup\gnutella\mp3\*.class
+jar -uvf ..\gui\lib\core.jar com\limegroup\gnutella\messages\*.class
 jar -uvf ..\gui\lib\core.jar com\limegroup\gnutella\security\*.class
 jar -uvf ..\gui\lib\core.jar com\limegroup\gnutella\routing\*.class
 jar -uvf ..\gui\lib\core.jar com\limegroup\gnutella\uploader\*.class
@@ -13,4 +15,3 @@ jar -uvf ..\gui\lib\core.jar com\limegroup\gnutella\xml\*.class
 jar -uvf ..\gui\lib\core.jar com\limegroup\gnutella\util\*.class
 jar -uvf ..\gui\lib\core.jar com\limegroup\gnutella\*.class
 cd ..\gui
-
