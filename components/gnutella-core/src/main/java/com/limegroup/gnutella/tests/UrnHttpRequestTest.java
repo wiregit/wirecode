ec55faf2ec928a889e59eb7691f43977a6d672d5
afisk <afisk>
2002-06-10 03:27:21 +0000

    added test for URN HTTP requests

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/tests/UrnHttpRequestTest.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/tests/UrnHttpRequestTest.java
@@ -1,6 +1,7 @@
 package com.limegroup.gnutella.tests;
 
 import com.limegroup.gnutella.*;
+import com.limegroup.gnutella.security.*;
 import com.limegroup.gnutella.xml.*;
 import com.limegroup.gnutella.util.*;
 import com.limegroup.gnutella.tests.stubs.*;
@@ -9,6 +10,7 @@ import junit.framework.*;
 import junit.extensions.*;
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 /**
  * This class tests HTTP requests involving URNs, as specified in HUGE v094,
@@ -84,13 +86,53 @@ public final class UrnHttpRequestTest extends TestCase {
 		assertTrue("FileManager should have loaded files", 
 				   4 < _fileManager.getNumFiles());
 
+		RouterService rs = new RouterService(callback, router, _fileManager, 
+											 new ServerAuthenticator());
+
 	}
 
 	/**
 	 * Test requests by URN.
 	 */
 	public void testHttpUrnRequest() {
-		
+		for(int i=0; i<_fileManager.getNumFiles(); i++) {
+			try {
+				FileDesc fd = _fileManager.get(i);
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				baos.write("GET /uri-res/N2R?".getBytes());
+				baos.write(fd.getSHA1Urn().toString().getBytes());
+				baos.write(" HTTP/1.1\r\n".getBytes());
+				Socket sock = new TestSocket(new ByteArrayInputStream(baos.toByteArray()));
+				_uploadManager.acceptUpload(sock);
+				String reply = sock.getOutputStream().toString();
+				StringTokenizer st = new StringTokenizer("\r\n");
+				while(st.hasMoreTokens()) {
+					String curString = st.nextToken();
+					if(HTTPHeaderName.ALT_LOCATION.matchesStartOfString(curString)) {
+						continue;
+					} else if(HTTPHeaderName.CONTENT_URN.matchesStartOfString(curString)) {
+						URN curUrn = null;
+						try {
+							curUrn = URNFactory.createUrnFromContentUrnHttpHeader(curString);
+						} catch(IOException e) {
+							assertTrue("unexpeced exception: "+e, false);
+						}
+						assertEquals(HTTPHeaderName.CONTENT_URN.toString()+"s should be equal",
+									 fd.getSHA1Urn(), curUrn);
+					} else if(HTTPHeaderName.CONTENT_RANGE.matchesStartOfString(curString)) {
+						continue;
+					} else if(HTTPHeaderName.CONTENT_TYPE.matchesStartOfString(curString)) {
+						String value = HTTPUtils.extractHeaderValue(curString);
+						assertEquals("sizes should match", (int)fd.getSize(), 
+									 Integer.parseInt(value));
+					} else if(HTTPHeaderName.SERVER.matchesStartOfString(curString)) {
+						continue;
+					}
+				}
+			} catch (IOException e) {
+				assertTrue("unexpeced exception: "+e, false);
+			}
+		}		
 	}
 
 	/**
@@ -100,13 +142,27 @@ public final class UrnHttpRequestTest extends TestCase {
 	private static class TestSocket extends Socket {
 		
 		InputStream INPUT_STREAM;
+		OutputStream OUTPUT_STREAM;
 
-		public TestSocket() {
-			byte[] bytes = new byte[300];
-			INPUT_STREAM = new ByteArrayInputStream(bytes);
+		private TestSocket(InputStream is) {
+			INPUT_STREAM = is;
+			OUTPUT_STREAM = new ByteArrayOutputStream();
 		}
+
 		public InputStream getInputStream() {
 			return INPUT_STREAM;
+		}
+
+		public OutputStream getOutputStream() {
+			return OUTPUT_STREAM;
+		}
+		
+		public InetAddress getInetAddress() {
+			try {
+				return InetAddress.getLocalHost();
+			} catch(UnknownHostException e) {
+				return null;
+			}
 		}
 	}
 }
