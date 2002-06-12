5ae491303bb1726f028ab6be231e6dfe8f403922
afisk <afisk>
2002-06-12 02:07:20 +0000

    added a test that makes sure that 404 status codes are returned when a file is requested with a URN that does not match

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/tests/UrnHttpRequestTest.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/tests/UrnHttpRequestTest.java
@@ -96,50 +96,127 @@ public final class UrnHttpRequestTest extends TestCase {
 	 */
 	public void testHttpUrnRequest() {
 		for(int i=0; i<_fileManager.getNumFiles(); i++) {
-			try {
-				FileDesc fd = _fileManager.get(i);
-				ByteArrayOutputStream baos = new ByteArrayOutputStream();
-				baos.write("GET /uri-res/N2R?".getBytes());
-				baos.write(fd.getSHA1Urn().toString().getBytes());
-				baos.write(" HTTP/1.1\r\n".getBytes());
-				Socket sock = new TestSocket(new ByteArrayInputStream(baos.toByteArray()));
-				_uploadManager.acceptUpload(sock);
-				String reply = sock.getOutputStream().toString();
-				StringTokenizer st = new StringTokenizer(reply, "\r\n");
-				boolean contentUrnHeaderPresent = false;
-				while(st.hasMoreTokens()) {
-					String curString = st.nextToken();
-					if(HTTPHeaderName.ALT_LOCATION.matchesStartOfString(curString)) {
-						continue;
-					} else if(HTTPHeaderName.CONTENT_URN.matchesStartOfString(curString)) {
-						URN curUrn = null;
-						try {
-							curUrn = URNFactory.createUrnFromContentUrnHttpHeader(curString);
-						} catch(IOException e) {
-							assertTrue("unexpeced exception: "+e, false);
-						}
-						assertEquals(HTTPHeaderName.CONTENT_URN.toString()+"s should be equal",
-									 fd.getSHA1Urn(), curUrn);
-						contentUrnHeaderPresent = true;
-					} else if(HTTPHeaderName.CONTENT_RANGE.matchesStartOfString(curString)) {
-						continue;
-					} else if(HTTPHeaderName.CONTENT_TYPE.matchesStartOfString(curString)) {
-						continue;
-					} else if(HTTPHeaderName.CONTENT_LENGTH.matchesStartOfString(curString)) { 
-						String value = HTTPUtils.extractHeaderValue(curString);
-						assertEquals("sizes should match", (int)fd.getSize(), 
-									 Integer.parseInt(value));						
-					} else if(HTTPHeaderName.SERVER.matchesStartOfString(curString)) {
-						continue;
-					}					
-				}
-				assertTrue("content URN header should always be reported: "+fd,
-						   contentUrnHeaderPresent);
-			} catch (IOException e) {
-				assertTrue("unexpeced exception: "+e, false);
-			}
-		}		
+			FileDesc fd = _fileManager.get(i);
+			String request = "GET /uri-res/N2R?"+fd.getSHA1Urn().httpStringValue()+
+			" HTTP/1.1\r\n\r\n";
+			sendRequestThatShouldSucceed(request, fd);
+		}
 	}
+
+	/**
+	 * Tests requests that follow the traditional "get" syntax to make sure that 
+	 * the X-Gnutella-Content-URN header is always returned.
+	 */
+	public void testTraditionalGetForReturnedUrn() {
+		for(int i=0; i<_fileManager.getNumFiles(); i++) {
+			FileDesc fd = _fileManager.get(i);
+			String request = "GET /get/"+fd.getIndex()+"/"+fd.getName()+" HTTP/1.1\r\n"+
+			HTTPHeaderName.CONTENT_URN.httpStringValue()+": "+fd.getSHA1Urn()+"\r\n\r\n";
+			sendRequestThatShouldSucceed(request, fd);
+		}				
+	}
+
+	/**
+	 * Tests requests that follow the traditional "get" syntax but that also include
+	 * the X-Gnutella-Content-URN header.  In these requests, both the URN and the file
+	 * name and index are correct, so a valid result is expected.
+	 */
+	public void testTraditionalGetWithContentUrn() {
+		for(int i=0; i<_fileManager.getNumFiles(); i++) {
+			FileDesc fd = _fileManager.get(i);
+			sendRequestThatShouldSucceed("GET /get/"+fd.getIndex()+"/"+fd.getName()+ 
+										 " HTTP/1.1\r\n\r\n", fd);
+		}
+	}
+
+	/**
+	 * Tests get requests that follow the traditional Gnutella get format and that
+	 * include an invalid content URN header -- these should fail with error code
+	 * 404.
+	 */
+	public void testTraditionalGetWithInvalidContentUrn() {
+		for(int i=0; i<_fileManager.getNumFiles(); i++) {
+			FileDesc fd = _fileManager.get(i);
+			String request = "GET /get/"+fd.getIndex()+"/"+fd.getName()+" HTTP/1.1\r\n"+
+			HTTPHeaderName.CONTENT_URN.httpStringValue()+": "+
+			"urn:sha1:PLSTHIPQGSSZTS5FJUPAKUZWUGYQYPFB"+"\r\n\r\n";
+			sendRequestThatShouldFail(request, fd);
+		}				
+	}
+
+	/**
+	 * Sends an HTTP request that should succeed and send back all of the
+	 * expected headers.
+	 */
+	private void sendRequestThatShouldSucceed(String request, FileDesc fd) {
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			baos.write(request.getBytes());
+			Socket sock = new TestSocket(new ByteArrayInputStream(baos.toByteArray()));
+			_uploadManager.acceptUpload(sock);
+			String reply = sock.getOutputStream().toString();
+			StringTokenizer st = new StringTokenizer(reply, "\r\n");
+			boolean contentUrnHeaderPresent = false;
+			while(st.hasMoreTokens()) {
+				String curString = st.nextToken();
+				if(HTTPHeaderName.ALT_LOCATION.matchesStartOfString(curString)) {
+					continue;
+				} else if(HTTPHeaderName.CONTENT_URN.matchesStartOfString(curString)) {
+					URN curUrn = null;
+					try {
+						curUrn = URNFactory.createUrnFromContentUrnHttpHeader(curString);
+					} catch(IOException e) {
+						assertTrue("unexpeced exception: "+e, false);
+					}
+					assertEquals(HTTPHeaderName.CONTENT_URN.toString()+"s should be equal",
+								 fd.getSHA1Urn(), curUrn);
+					contentUrnHeaderPresent = true;
+				} else if(HTTPHeaderName.CONTENT_RANGE.matchesStartOfString(curString)) {
+					continue;
+				} else if(HTTPHeaderName.CONTENT_TYPE.matchesStartOfString(curString)) {
+					continue;
+				} else if(HTTPHeaderName.CONTENT_LENGTH.matchesStartOfString(curString)) { 
+					String value = HTTPUtils.extractHeaderValue(curString);
+					assertEquals("sizes should match", (int)fd.getSize(), 
+								 Integer.parseInt(value));						
+				} else if(HTTPHeaderName.SERVER.matchesStartOfString(curString)) {
+				continue;
+				}					
+			}
+			assertTrue("content URN header should always be reported:\r\n"+
+					   fd+"\r\n"+
+					   "reply: "+reply,
+					   contentUrnHeaderPresent);
+		} catch(IOException e) {
+			assertTrue("unexpected exception: "+e, false);
+		}
+	}
+
+	/**
+	 * Sends an HTTP request that should fail if everything is working 
+	 * correctly.
+	 */
+	private void sendRequestThatShouldFail(String request, FileDesc fd) {
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			baos.write(request.getBytes());
+			Socket sock = new TestSocket(new ByteArrayInputStream(baos.toByteArray()));
+			_uploadManager.acceptUpload(sock);
+			String reply = sock.getOutputStream().toString();
+			StringTokenizer st = new StringTokenizer(reply, "\r\n");
+			boolean sent404 = false;
+			while(st.hasMoreTokens()) {
+				String curString = st.nextToken();
+				if(curString.equals("HTTP/1.1 404 Not Found")) {
+					sent404 = true;
+				}
+			}
+			assertTrue("404 status should have been sent", sent404);
+		} catch(IOException e) {
+			assertTrue("unexpected exception: "+e, false);
+		}
+	}
+
 
 	/**
 	 * Helper class that allows us to control the InputStream returned from
