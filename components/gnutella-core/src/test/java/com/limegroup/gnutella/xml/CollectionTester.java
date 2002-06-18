6f2bf84bca54ab95c49c561bc2ca5efcc2a0e13d
sdaswani <sdaswani>
2002-06-18 14:55:05 +0000

    nice unit test to make sure getMatchingReplies() works as specified

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/xml/CollectionTester.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/xml/CollectionTester.java
@@ -2,6 +2,7 @@ package com.limegroup.gnutella.xml;
 
 import java.io.*;
 import com.sun.java.util.collections.*;
+import com.limegroup.gnutella.util.*;
 import junit.framework.*;
 
 /** Unit test for LimeXMLReplyCollection.
@@ -9,31 +10,45 @@ import junit.framework.*;
  */
 public class CollectionTester extends TestCase {
 
+    final Set files = new HashSet();
+    final File mason = new File("nullfile.null");
+    final File vader = new File("vader.mp3");
+    final File swing = new File("swing.mp3");
+    final String schemaURI =  "http://www.limewire.com/schemas/audio.xsd";
+    final MetaFileManager mfm = new MFMStub();
+    final boolean audio = true;
+
+    private final String KEY_PREFIX = "audios" + XMLStringUtils.DELIMITER +
+        "audio" + XMLStringUtils.DELIMITER;
+
+    private final String ARTIST_KEY =   KEY_PREFIX + "artist" + 
+        XMLStringUtils.DELIMITER;
+    private final String ALBUM_KEY =    KEY_PREFIX + "album" + 
+        XMLStringUtils.DELIMITER;
+    private final String TITLE_KEY =    KEY_PREFIX + "title" + 
+        XMLStringUtils.DELIMITER;
+    private final String TRACK_KEY =    KEY_PREFIX + "track" + 
+        XMLStringUtils.DELIMITER;
+        
+
     public CollectionTester(String name) {
         super(name);
     }
-
+    
     protected void setUp() {
-    }
-
-    public void testBasic() {
-        Set files = new HashSet();
-        File mason = new File("nullfile.null");
         if (!mason.exists())
             Assert.assertTrue("Could not find necessary file!", false);
-        File vader = new File("vader.mp3");
         if (!vader.exists())
             Assert.assertTrue("Could not find necessary file!", false);
-        File swing = new File("swing.mp3");
         if (!swing.exists())
             Assert.assertTrue("Could not find necessary file!", false);
+        files.clear();
         files.add(mason);
         files.add(vader);
         files.add(swing);
-        final String schemaURI =  "http://www.limewire.com/schemas/audio.xsd";
-        MetaFileManager mfm = new MFMStub();
-        final boolean audio = true;
-        
+    }
+
+    public void testBasic() {
         // test construction
         LimeXMLReplyCollection collection = 
         new LimeXMLReplyCollection(files, schemaURI, mfm, audio);
@@ -67,7 +82,82 @@ public class CollectionTester extends TestCase {
 
     
     public void testAdvanced() {
-        // current empty.
+        LimeXMLReplyCollection collection = 
+        new LimeXMLReplyCollection(files, schemaURI, mfm, audio);
+
+        // make sure that a simple match works....
+        List nameVals = new ArrayList();
+        nameVals.add(new NameValue(TITLE_KEY, "vade"));
+        LimeXMLDocument searchDoc = new LimeXMLDocument(nameVals,
+                                                        schemaURI);
+        List results = collection.getMatchingReplies(searchDoc);
+        Assert.assertTrue("Not the correct amount of results, # = " +
+                          results.size(), (results.size() == 1));
+
+        // make sure a little more complex match works, no mp3 has album but one
+        // has a title beginning with s
+        nameVals.clear();
+        nameVals.add(new NameValue(ALBUM_KEY, "susheel"));
+        nameVals.add(new NameValue(TITLE_KEY, "s"));
+        searchDoc = new LimeXMLDocument(nameVals, schemaURI);
+        results = collection.getMatchingReplies(searchDoc);
+        Assert.assertTrue("Not the correct amount of results, # = " +
+                          results.size(), (results.size() == 1));        
+
+        // has a title beginning with s
+        nameVals.clear();
+        nameVals.add(new NameValue(ALBUM_KEY, "susheel"));
+        nameVals.add(new NameValue(TITLE_KEY, "s"));
+        searchDoc = new LimeXMLDocument(nameVals, schemaURI);
+        results = collection.getMatchingReplies(searchDoc);
+        Assert.assertTrue("Not the correct amount of results, # = " +
+                          results.size(), (results.size() == 1));        
+
+        // has two matching, one null
+        nameVals.clear();
+        nameVals.add(new NameValue(TITLE_KEY, "vader"));
+        nameVals.add(new NameValue(ALBUM_KEY, "darth"));
+        nameVals.add(new NameValue(ARTIST_KEY, "darth"));
+        searchDoc = new LimeXMLDocument(nameVals, schemaURI);
+        results = collection.getMatchingReplies(searchDoc);
+        Assert.assertTrue("Not the correct amount of results, # = " +
+                          results.size(), (results.size() == 1));        
+
+        // has two matching
+        nameVals.clear();
+        nameVals.add(new NameValue(TITLE_KEY, "swi"));
+        nameVals.add(new NameValue(ARTIST_KEY, "ferr"));
+        searchDoc = new LimeXMLDocument(nameVals, schemaURI);
+        results = collection.getMatchingReplies(searchDoc);
+        Assert.assertTrue("Not the correct amount of results, # = " +
+                          results.size(), (results.size() == 1));        
+
+        // has one off by one....
+        nameVals.clear();
+        nameVals.add(new NameValue(TITLE_KEY, "swi"));
+        nameVals.add(new NameValue(ARTIST_KEY, "ferb"));
+        searchDoc = new LimeXMLDocument(nameVals, schemaURI);
+        results = collection.getMatchingReplies(searchDoc);
+        Assert.assertTrue("Not the correct amount of results, # = " +
+                          results.size(), (results.size() == 0));        
+
+        // has all nulls, this was a terrible bug in previous versions...
+        nameVals.clear();
+        nameVals.add(new NameValue(ALBUM_KEY, "swi"));
+        nameVals.add(new NameValue(TRACK_KEY, "ferb"));
+        searchDoc = new LimeXMLDocument(nameVals, schemaURI);
+        results = collection.getMatchingReplies(searchDoc);
+        Assert.assertTrue("Not the correct amount of results, # = " +
+                          results.size(), (results.size() == 0));        
+
+        // has all nulls, this was a terrible bug in previous versions...
+        nameVals.clear();
+        nameVals.add(new NameValue(TRACK_KEY, "ferb"));
+        searchDoc = new LimeXMLDocument(nameVals, schemaURI);
+        results = collection.getMatchingReplies(searchDoc);
+        Assert.assertTrue("Not the correct amount of results, # = " +
+                          results.size(), (results.size() == 0));        
+
     }
 
 
