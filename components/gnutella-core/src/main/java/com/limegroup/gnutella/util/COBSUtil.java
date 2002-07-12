294d5df5349521be516ef598774b0398d367d5b4
sdaswani <sdaswani>
2002-07-12 19:32:08 +0000

    first pass, need to write unit test

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/util/COBSUtil.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/util/COBSUtil.java
@@ -1,6 +1,65 @@
 package com.limegroup.gnutella.util;
 
-public class COBSUtil {
+import java.io.*;
 
+/** For implementation details, please see:
+ *  http://www.acm.org/sigcomm/sigcomm97/papers/p062.pdf 
+ */
+public class COBSUtil {
+    
+    /** Encode a byte array with COBS.  The non-allowable byte value is 0.
+     *  PRE: src is not null.
+     *  POST: the return array will be a cobs encoded version of src.  namely,
+     *  cobsDecode(cobsEncode(src)) ==  src.
+     */
+    public static byte[] cobsEncode(byte[] src) {
+        final int srcLen = src.length;
+        byte code = (byte) 0x01;
+        int currIndex = 0;
+        ByteArrayOutputStream sink = new ByteArrayOutputStream();
+        
+        while (currIndex < srcLen) {
+            if (src[currIndex] == 0) {
+                sink.write((int)code);
+                code = (byte) 0x01;
+            }
+            else {
+                sink.write((int)src[currIndex]);
+                code++;
+                if (code == 0xFF) {
+                    sink.write((int)code);
+                    code = (byte) 0x01;
+                }
+            }
+            currIndex++;
+        }
+
+        sink.write((int)code);
+        return sink.toByteArray();
+    }
+
+
+
+    /** Decode a COBS-encoded byte array.  The non-allowable byte value is 0.
+     *  PRE: src is not null.
+     *  POST: the return array will be a cobs decoded version of src.  namely,
+     *  cobsDecode(cobsEncode(src)) ==  src.
+     */
+    public static byte[] cobsDecode(byte[] src) {
+        final int srcLen = src.length;
+        int currIndex = 0;
+        byte code = src[currIndex];
+        ByteArrayOutputStream sink = new ByteArrayOutputStream();        
+
+        while (currIndex < srcLen) {            
+            for (int i = 1; i < code; i++) {
+                sink.write((int)src[currIndex]);
+                currIndex++;
+            }
+            if (code < 0xFF) sink.write(0);
+        }
+
+        return sink.toByteArray();
+    }
 
 }
