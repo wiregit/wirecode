ee171f468e426ddfd7b09b7ba1808f5ab29ceb58
sdaswani <sdaswani>
2002-07-12 19:44:56 +0000

    unit test for COBSUtil, currently empth

---
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/util/COBSUtilTest.java
@@ -0,0 +1,32 @@
+package com.limegroup.gnutella.util;
+
+import junit.framework.*;
+
+/**
+ * Tests COBSUtil
+ */
+public class COBSUtilTest extends TestCase {
+    public COBSUtilTest(String name) {
+        super(name);
+    }
+
+    public static Test suite() {
+        return new TestSuite(COBSUtilTest.class);
+    }  
+
+    public void testEncode() {
+    }
+
+    public void testDecode() {
+    }
+
+    public void testSymmetry() {
+    }
+
+
+    public static void main(String argv[]) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+
+}
