02c4cfa3127c8e07cca8f5875db78693679dd828
crohrs <crohrs>
2002-07-29 15:52:09 +0000

    Added set of empty urns to FileDesc constructor to get everything compiling properly.

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/stubs/FileManagerStub.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/stubs/FileManagerStub.java
@@ -2,6 +2,7 @@ package com.limegroup.gnutella.stubs;
 
 import com.limegroup.gnutella.*;
 import java.io.*;
+import com.sun.java.util.collections.*;
 
 /**
  * A simple FileManager that shares one file of (near) infinite length.
@@ -9,6 +10,7 @@ import java.io.*;
 public class FileManagerStub extends FileManager {
     public FileDesc get(int i) {
         return new FileDesc(new File("com.limegroup.gnutella.tests.stubs.FileManagerStub.java"), 
+                            new HashSet(),
 							i);
     }
     public InputStream getInputStream(FileDesc ignored) {
