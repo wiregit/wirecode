83d4a8ad36237e7e1d3bdf88666fde011933874d
afisk <afisk>
2002-08-20 22:03:06 +0000

    small changes

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/update/ServletRemoteUpdateInfoTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/update/ServletRemoteUpdateInfoTest.java
@@ -1,7 +1,10 @@
 package com.limegroup.gnutella.update;
 
+import com.limegroup.gnutella.gui.*;
+import com.limegroup.gnutella.util.Launcher;
 import junit.framework.*;
 import junit.extensions.*;
+import java.io.*;
 
 /**
  * Class for testing updates.  This may change frequently as updates
@@ -50,27 +53,51 @@ public final class ServletRemoteUpdateInfoTest extends TestCase {
 		junit.textui.TestRunner.run(suite());
 	}	
 
+	protected void setUp() {
+		try {
+			System.loadLibrary("LimeWire20");
+		} catch(UnsatisfiedLinkError e) {
+			fail("unexpected exception: "+e);
+		}
+	}
+
 	/**
 	 * Tests update with multiple values for operating system and LimeWire
 	 * version.
 	 */
 	public void testVaryingUpdateValues() {
 		try {
-			for(int i=0; i<OPERATING_SYSTEMS.length; i++) {
+			for(int i=0; i<1; i++) {//OPERATING_SYSTEMS.length; i++) {
 				ServletLocalUpdateInfo localInfo = new ServletLocalUpdateInfo();
 				localInfo.addKeyValuePair(localInfo.OS, OPERATING_SYSTEMS[i]);
 				for(int j=0; j<VERSIONS.length; j++) {
 					localInfo.addKeyValuePair(localInfo.LIMEWIRE_VERSION, VERSIONS[j]);
 					ServletRemoteUpdateInfo srui = 
 						new ServletRemoteUpdateInfo(localInfo);
+
 					String urlString = srui.getURLEncodedString();
+					InputStream is = new ByteArrayInputStream(urlString.getBytes());
+					InputStreamReader isr = new InputStreamReader(is);
 					
+					// create the buffered reader to read from the stream
+					BufferedReader in = new BufferedReader(isr);           
+				
+					StringBuffer lineBuffer = new StringBuffer();
+					String line;
+					while((line = in.readLine()) != null) {
+						lineBuffer.append(line + "\r\n");
+					}
+
 					ClientRemoteUpdateInfo crui = new ClientRemoteUpdateInfo();
-					crui.addRemoteInfo(urlString);
+					crui.addRemoteInfo(lineBuffer.toString());
+
 					Updator updator = crui.getUpdator();
 					if(OPERATING_SYSTEMS[i].startsWith("Win")) {
 						assertTrue("should be a web page updator: "+updator, 
 								   (updator instanceof WebPageUpdator));
+						//String updatorStr = updator.toString();
+						//System.out.println(updatorStr); 
+						//updator.doUpdate();
 					} else if(OPERATING_SYSTEMS[i].startsWith("Mac")) {
 						assertTrue("should be a web page updator: "+updator, 
 								   (updator instanceof WebPageUpdator));
