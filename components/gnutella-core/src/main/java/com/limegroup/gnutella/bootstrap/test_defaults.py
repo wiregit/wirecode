4829002af02b557fd13768101b1b180243fe42f0
crohrs <crohrs>
2002-09-04 16:49:25 +0000

    Removed default bootstrap servers that were unreachable or sent old-style data.  Added Python script to automate validation.

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/bootstrap/test_defaults.py
@@ -0,0 +1,43 @@
+#Input:  DefaultBootstrapServers.java
+#Output: which of the defaults in DefaultBootstrapServers.java are reachable
+
+from string import *
+from urllib import *
+
+def valid_line(line):
+    return count(line, ".")==3 and count(line, ":")==1
+assert valid_line("18.239.0.144:6346")
+assert not valid_line("18.239.0:6346")
+assert not valid_line("18.239.0.144")
+
+def valid_file(f):
+    line1=f.readline()
+    line2=f.readline()
+    return valid_line(line1) and valid_line(line2)
+
+file=open("DefaultBootstrapServers.java")
+while 1:
+    line=file.readline()
+    if line=="":
+        break
+
+    #Remove whitespace
+    line=strip(line)
+    #Ignore comments
+    if (line[:2]=="//"):
+        line=line[2:]
+    #Remove leading quote and trailing quote plus comma.
+    #TODO: there's no comma on last line
+    line=line[1:][:-2]
+    
+    if line[:4]=="http":
+        try:
+            f=urlopen(line+"?hostfile=1")
+            if not valid_file(f):
+                raise "Invalid file"
+            f.close()
+            #print "%s success" % line
+        except:
+            print "%s FAILURE" % line
+
+
