04c1c931ef4178e5c430cd35f508b5318adfed8b
afisk <afisk>
2002-09-05 14:00:39 +0000

    new test for testing IOUtils class

---
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/util/IOUtilsTest.java
@@ -0,0 +1,44 @@
+package com.limegroup.gnutella.util;
+
+import junit.framework.*;
+import junit.extensions.*;
+import java.io.*;
+
+public final class IOUtilsTest extends TestCase {
+
+	/**
+	 * Constructs a new <tt>IOUtilsTest</tt> with the specified name.
+	 */
+	public IOUtilsTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Runs this test individually.
+	 */
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
+
+	/**
+	 * Runs this suite of tests.
+	 */
+	public static Test suite() {
+		return new TestSuite(IOUtilsTest.class);
+	}
+
+	/**
+	 * Tests the readWord method.
+	 */
+	public void testIOUtilsReadWord() {
+		String firstWord = "GET";
+		String test0 = firstWord+" /get/0/file.txt";
+		InputStream stream = new ByteArrayInputStream(test0.getBytes());
+		try {
+			String result = IOUtils.readWord(stream, 4);
+			assertEquals("result should equal first word", result, firstWord);
+		} catch(IOException e) {
+			fail("unexpected exception: "+e);
+		}
+	}
+}
