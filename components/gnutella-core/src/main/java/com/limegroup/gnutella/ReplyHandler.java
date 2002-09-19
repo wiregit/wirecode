affe11c993a52ac739d525be26433b27f481ff9c
afisk <afisk>
2002-09-19 17:28:00 +0000

    added method for checking whether or not it's a leaf connection

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/ReplyHandler.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/ReplyHandler.java
@@ -9,8 +9,8 @@ import com.sun.java.util.collections.*;
  * This interface is implemented by ManagedConnection and by
  * MessageRouter.ForMeReplyHandler.
  */
-interface ReplyHandler
-{
+interface ReplyHandler {
+
     /**
      * Handle the PingReply, failing silently
      */
@@ -37,6 +37,18 @@ interface ReplyHandler
 	boolean isOutgoing();
 
 	boolean isKillable();
+
+	/**
+	 * Returns whether or not this <tt>ReplyHandler</tt> sends replies
+	 * from an Ultrapeer to a leaf.  This returns <tt>true</tt> only
+	 * if this node is an Ultrapeer, and the node receiving these 
+	 * replies is a leaf of that Ultrapeer.
+	 *
+	 * @return <tt>true</tt> if this node is an Ultrapeer, and the node
+	 *  it is sending replies to is a leaf, otherwise returns 
+	 *  <tt>false</tt>
+	 */
+	boolean isSupernodeClientConnection();
 
     /**
      * Returns true if the reply handler is still able to handle
