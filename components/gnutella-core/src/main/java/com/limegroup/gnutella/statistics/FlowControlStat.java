9da594223dc5403a75059b81b3612c5d36d947b7
afisk <afisk>
2002-12-03 23:55:00 +0000

    changed to record stats more precisely -- this will change more soon

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/statistics/FlowControlStat.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/statistics/FlowControlStat.java
@@ -13,8 +13,25 @@ public class FlowControlStat extends AdvancedStatistic {
 	private FlowControlStat() {}
 
 	/**
+	 * Private class for keeping track of all dropped sent messages.
+	 */
+  	private static class GeneralSentMessagesDropped extends FlowControlStat {
+		public void incrementStat() {
+			super.incrementStat();
+			ALL_SENT_MESSAGES_DROPPED.incrementStat();
+		}
+	}
+
+	/**
 	 * <tt>Statistic</tt> for the number of Gnutella messages dropped.
 	 */
-    public static final Statistic SENT_MESSAGES_DROPPED =
+    public static final Statistic ALL_SENT_MESSAGES_DROPPED =
         new FlowControlStat();
+
+
+	/**
+	 * <tt>Statistic</tt> for the number of query hits dropped.
+	 */
+    public static final Statistic SENT_QUERY_REPLIES_DROPPED =
+        new GeneralSentMessagesDropped();
 }
