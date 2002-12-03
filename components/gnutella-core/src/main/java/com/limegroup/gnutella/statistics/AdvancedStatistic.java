62f2cd865ffe6e7cc445f8cad1150a2d02682e81
afisk <afisk>
2002-12-03 00:28:03 +0000

    now overrides incrementStat and addData methods so that they're only invoked if advanced stats are being recorded

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/statistics/AdvancedStatistic.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/statistics/AdvancedStatistic.java
@@ -11,4 +11,17 @@ abstract class AdvancedStatistic extends AbstractStatistic {
 	protected AdvancedStatistic() {
 		StatisticsManager.instance().addAdvancedStatistic(this);
 	}
+
+	public void incrementStat() {
+		// if we're not recording advanced stats, ignore the call
+		if(!STATS_MANAGER.getRecordAdvancedStats()) return;
+		super.incrementStat();
+	}
+
+	// override to only record
+	public void addData(int data) {
+		// if we're not recording advanced stats, ignore the call
+		if(!STATS_MANAGER.getRecordAdvancedStats()) return;
+		super.addData(data);
+	}
 }
