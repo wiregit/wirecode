9f0135e1040069a142f098b4b4395f9c3bd83ab4
sdaswani <sdaswani>
2002-12-06 21:35:46 +0000

    working nicely.  now i need to make it do a GUESS test when it gets a guess pong.

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/GUESSMonitor.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/GUESSMonitor.java
@@ -1,6 +1,6 @@
 package com.limegroup.gnutella;
 
-import com.limegroup.gnutella.util.*;
+import com.limegroup.gnutella.stubs.*;
 import com.limegroup.gnutella.xml.*;
 import java.io.*;
 
@@ -9,35 +9,18 @@ import java.io.*;
  *  it a query and take statistics on how many acks it recieves.
  *  It seems like you need to start this guy from a directory which has a com
  *  underneath it.
- *  This class will stop when you enter anything and press RETURN.
+ *  If you run main, this will stop when you enter anything and press RETURN.
  */
 public class GUESSMonitor {
 
     private Backend _backend;
 
     public GUESSMonitor() {
-        _backend = Backend.createLongLivedBackend();
-        try {
-            // get the router....
-            RouterService router = (RouterService) 
-            PrivilegedAccessor.getValue(_backend, "ROUTER_SERVICE");
-            // get the ActivityCallback (could do this easier, but whatever)
-            ActivityCallback callback = (ActivityCallback)
-            PrivilegedAccessor.getValue(router, "callback");
-            // get the FileManager            
-            FileManager fm = (FileManager)
-            PrivilegedAccessor.getValue(router, "fileManager");
-            // make my own MessageRouter....            
-            MyMessageRouter messageRouter = new MyMessageRouter(callback, fm);
-            messageRouter.initialize();
-            // set my MessageRouter as THE MessageRouter
-            PrivilegedAccessor.setValue(router, "router", messageRouter);
-        }
-        catch (Exception e) {
-            System.out.println("Could not set up Backend appropriately!!");
-            e.printStackTrace();
-            _backend.shutdown("GUESSMonitor exiting!");
-        }
+        // make my own MessageRouter....            
+        ActivityCallback stub = new ActivityCallbackStub();
+        FileManager staticFM = RouterService.getFileManager();
+        MyMessageRouter messageRouter = new MyMessageRouter(stub, staticFM);
+        _backend = Backend.createLongLivedBackend(stub, messageRouter);
     }
 
     public void shutdown() {
@@ -45,7 +28,7 @@ public class GUESSMonitor {
     }
 
 
-    public static void main(String argv[]) {
+    public static void main(String argv[]) throws Exception {
         System.out.println("Type Anything to Exit....");
         GUESSMonitor guessMon = new GUESSMonitor();
         //  open up standard input
