9b70b40b37d493dd1769733ad29850bd0947d886
sdaswani <sdaswani>
2002-12-06 22:25:07 +0000

    now just need to add a call to GUESS stuff and statistics, etc....

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/GUESSMonitor.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/GUESSMonitor.java
@@ -3,6 +3,7 @@ package com.limegroup.gnutella;
 import com.limegroup.gnutella.stubs.*;
 import com.limegroup.gnutella.xml.*;
 import java.io.*;
+import java.util.*;
 
 /** Starts a BackEnd (which should have a .props file configured to be an
  *  Ultrapeer) and any time it gets a pong from a GUESS Ultrapeer, it sends
@@ -14,16 +15,19 @@ import java.io.*;
 public class GUESSMonitor {
 
     private Backend _backend;
+    private MyMessageRouter _messageRouter;
 
     public GUESSMonitor() {
         // make my own MessageRouter....            
         ActivityCallback stub = new ActivityCallbackStub();
         FileManager staticFM = RouterService.getFileManager();
-        MyMessageRouter messageRouter = new MyMessageRouter(stub, staticFM);
-        _backend = Backend.createLongLivedBackend(stub, messageRouter);
+        _messageRouter = new MyMessageRouter(stub, staticFM);
+        _backend = Backend.createLongLivedBackend(stub, _messageRouter);
     }
 
     public void shutdown() {
+        _messageRouter.shutdown();
+        _messageRouter.join();
         _backend.shutdown("GUESSMonitor exiting!");
     }
 
@@ -44,15 +48,65 @@ public class GUESSMonitor {
 
     
     private class MyMessageRouter extends MetaEnabledMessageRouter {
+
+        private List _guessPongs = new Vector();
+
+        private boolean _shouldRun = true;
+        public void shutdown() {
+            _shouldRun = false;
+            _pongLoop.interrupt();
+        }
+
+        private Thread _pongLoop = null;
+        public void join() {
+            try {
+                _pongLoop.join();
+            }
+            catch (Exception ignored) {}
+        }
+
         public MyMessageRouter(ActivityCallback ac, FileManager fm) {
             super(ac, fm);
+            _pongLoop = new Thread() {
+                    public void run() {
+                        guessPongLoop();
+                    }
+                };
+            _pongLoop.start();
         }
         
         protected void handlePingReply(PingReply reply,
                                        ReplyHandler handler) {
             super.handlePingReply(reply, handler);
-            System.out.println("GUESSMonitor.MyMessageRouter.handlePingReply()"
-                               + ": got a pong!!");
+            try {
+                if (reply.supportsUnicast()) {
+                    synchronized (_guessPongs) {
+                        _guessPongs.add(reply);
+                        _guessPongs.notify();
+                    }
+                }
+            }
+            catch (BadPacketException ignored) {}
+        }
+                
+        private void guessPongLoop() {
+            System.out.println("guessPongLoop(): starting.");
+            while (_shouldRun) {
+                synchronized (_guessPongs) {
+                    while (_shouldRun && _guessPongs.size() == 0) {
+                        try {
+                            _guessPongs.wait();
+                        }
+                        catch (InterruptedException ignored) {}
+                    }
+                }
+                if (_shouldRun && (_guessPongs.size() > 0)) {
+                    PingReply currPong = (PingReply) _guessPongs.remove(0);
+                    System.out.println("guessPongLoop(): consuming Pong = " + 
+                                       currPong);
+                }
+            }
+            System.out.println("guessPongLoop(): returning.");
         }
 
     }
