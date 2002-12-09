0c9372b7a0f3d9c70b2dcff2d171d95b51192b99
sdaswani <sdaswani>
2002-12-09 03:07:25 +0000

    added some help info. only tries to connect to non-private, notMe addresses.

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/GUESSMonitor.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/GUESSMonitor.java
@@ -5,6 +5,7 @@ import com.limegroup.gnutella.xml.*;
 import com.limegroup.gnutella.guess.*;
 import java.io.*;
 import java.util.*;
+import java.net.*;
 
 /** Starts a BackEnd (which should have a .props file configured to be an
  *  Ultrapeer) and any time it gets a pong from a GUESS Ultrapeer, it sends
@@ -16,7 +17,8 @@ import java.util.*;
 public class GUESSMonitor {
 
     public final static String INSTRUCTIONS = 
-        "? - Help; verbose - switch verbose on/off";
+        "? - Help; verbose - switch verbose on/off; connect - start the " +
+        "backend; disconnect - stop the backend";
 
     private Backend _backend;
     private MyMessageRouter _messageRouter;
@@ -103,7 +105,10 @@ public class GUESSMonitor {
                                        ReplyHandler handler) {
             super.handlePingReply(reply, handler);
             try {
-                if (reply.supportsUnicast()) {
+                if (!Endpoint.isPrivateAddress(reply.getIPBytes()) &&
+                    notMe(InetAddress.getByName(reply.getIP()), 
+                          reply.getPort()) &&
+                    reply.supportsUnicast()) {
                     synchronized (_guessPongs) {
                         _guessPongs.add(reply);
                         _guessPongs.notify();
@@ -111,6 +116,7 @@ public class GUESSMonitor {
                 }
             }
             catch (BadPacketException ignored) {}
+            catch (UnknownHostException ignored) {}
         }
                 
         private void guessPongLoop() {
@@ -127,14 +133,42 @@ public class GUESSMonitor {
                 if (_shouldRun && (_guessPongs.size() > 0)) {
                     PingReply currPong = (PingReply) _guessPongs.remove(0);
                     debug("guessPongLoop(): consuming Pong = " + currPong);
-                    GUESSStatistics.getAckStatisticsAndPrint(currPong.getIP(),
-                                                             currPong.getPort());
+                    Object[] retObjs = 
+                        GUESSStatistics.getAckStatistics(currPong.getIP(),
+                                                         currPong.getPort());
+                    float numSent = ((Float)retObjs[1]).floatValue();
+                    float numGot = ((Float)retObjs[0]).floatValue();
+                    float averageTime = ((Float)retObjs[2]).floatValue();
+
+                    debug("Sent Queries to " + currPong.getIP() + ":" +
+                          currPong.getPort() + " . " + "Success Rate = " +
+                          ((numGot/numSent)*100) + " at an average of " +
+                          averageTime + " ms per Query.");
+                    
                 }
             }
             debug("guessPongLoop(): returning.");
         }
 
     }
+
+
+    /** 
+     * Returns whether or not the Endpoint refers to me!  True if it doesn't,
+     * false if it does (NOT not me == me).
+     */
+    private boolean notMe(InetAddress address, int port) {
+        boolean retVal = true;
+
+        if ((port == RouterService.getPort()) &&
+				 Arrays.equals(address.getAddress(), 
+							   RouterService.getAddress())) {			
+			retVal = false;
+		}
+
+        return retVal;
+    }
+
 
     private boolean debugOn = false;
     private void debug(String out) {
