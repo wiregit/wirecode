73f39e3d5697e278677f33f2cfd966faef458c65
afisk <afisk>
2002-12-13 16:59:41 +0000

    added a file name parameter for specifying the file name to write to

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/statistics/AbstractMessageStatHandler.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/statistics/AbstractMessageStatHandler.java
@@ -10,17 +10,23 @@ abstract class AbstractMessageStatHandler extends AbstractStatHandler {
 	/**
 	 * Constant for the class that records TTL and hops data.
 	 */
-	public final TTLHopsRecorder TTL_HOPS = new TTLHopsRecorder();
+	public final TTLHopsRecorder TTL_HOPS;
 
 	protected AbstractMessageStatHandler(Statistic numberStat, 
 										 Statistic byteStat,
 										 Statistic limeNumberStat,
 										 Statistic limeByteStat,
-										 Statistic bandwidthStat) {
+										 Statistic bandwidthStat,
+										 String fileName) {
 		super(numberStat, byteStat, limeNumberStat, limeByteStat, bandwidthStat);
-		
+		TTL_HOPS = new TTLHopsRecorder(fileName);
 	}
 
+	/**
+	 * Overridden to also add data to the TTL/hops recorder.
+	 *
+	 * @param msg the <tt>Message</tt> to record
+	 */
 	public void addMessage(Message msg) {
 		super.addMessage(msg);
 		TTL_HOPS.addMessage(msg);
