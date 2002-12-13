4fd41e5edd40a3d9125b0ababcf68b1fff76cc2e
afisk <afisk>
2002-12-13 17:05:15 +0000

    changed the constructor to delegate to the superclass with the name of the stats file to write to

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/statistics/GeneralStatHandler.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/statistics/GeneralStatHandler.java
@@ -1,9 +1,13 @@
 package com.limegroup.gnutella.statistics;
 
+/**
+ * This class acts as a generalized stat handler that records Gnutella message 
+ * data by the raw number of messages and by the number of bytes passed.  It 
+ * also keeps this data for LimeWire vs. non-LimeWire messages.
+ */
 final class GeneralStatHandler extends AbstractStatHandler {
 
-	GeneralStatHandler() {
-		super(new AdvancedStatistic(), new AdvancedKilobytesStatistic(),
-			  new AdvancedStatistic(), new AdvancedKilobytesStatistic());
+	GeneralStatHandler(String fileName) {
+		super(fileName);
 	}
 }
