417304c265f8ab347744c5cb0a0e32bbdad2bd62
afisk <afisk>
2002-12-27 01:21:55 +0000

    added ping testing

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/UDPServiceTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/UDPServiceTest.java
@@ -14,14 +14,17 @@ public class UDPServiceTest extends TestCase {
 	//private static final Backend BACKEND_0 = Backend.instance();
 
 	private final ActivityCallback CALLBACK = new ActivityCallbackTest();
-	private final Backend BACKEND = 
-		Backend.createBackend(CALLBACK, 40*1000);
+	private final MessageRouter ROUTER = new MessageRouterTestStub();
 
-	private final RouterService ROUTER_SERVICE = BACKEND.getRouterService();
+	private Backend BACKEND;
+
+	private RouterService ROUTER_SERVICE;
 	
 	private final int BUFFER_SIZE = 1024;
 	
 	private QueryReply _reply = null;
+
+	private PingReply _pingReply = null;
 
 	/**
 	 * Constructs a new <tt>UDPServiceTest</tt> instance.
@@ -44,15 +47,17 @@ public class UDPServiceTest extends TestCase {
 		junit.textui.TestRunner.run(suite());
 	}
 
-	/**
-	 * Tests sending query requests to the UDP port.
-	 */
-	public void testQueryRequests() {
-		FileManager fm = ROUTER_SERVICE.getFileManager();
-		File[] sharedDirs = SettingsManager.instance().getDirectories();
-		FileDesc[] fds = fm.getSharedFileDescriptors(sharedDirs[0]);
+	public void setUp() {
+		BACKEND = Backend.createLongLivedBackend(CALLBACK, ROUTER);
+		ROUTER_SERVICE = BACKEND.getRouterService();
+	}
+
+	public void tearDown() {
+		BACKEND.shutdown("GUESS CLIENT");
+	}
 
 
+	private void establishConnection() {
 		byte[] ipBytes = ROUTER_SERVICE.getAddress();
 		String address = Message.ip2string(ipBytes);
 		try {
@@ -65,7 +70,48 @@ public class UDPServiceTest extends TestCase {
 		}
 		
 		RouterService.disconnect();
+	}
 
+	/**
+	 * Test that sending pings to GUESS server returns the appropriate pongs.
+	 */
+	public void testPings() {
+		establishConnection();
+		PingRequest ping = new PingRequest((byte)1);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try {
+			ping.write(baos);
+			byte[] data = baos.toByteArray();
+			InetAddress ip = InetAddress.getLocalHost();
+			
+			System.out.println("localhost: "+ip); 
+			UDPService service = ROUTER_SERVICE.getUdpService();			
+			service.send(ping, ip, 6346);
+			System.out.println("send ping: "+ping); 
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {}
+
+			if(_pingReply == null) {
+				fail("PingReply unexpectedly null");
+			}
+			assertEquals("GUID of pong should equal GUID of ping", ping.getGUID(), 
+						 _pingReply.getGUID());
+			
+		} catch(Throwable t) {
+			t.printStackTrace();
+			fail("unexpected throwable: "+t);
+		}		
+	}
+
+	/**
+	 * Tests sending query requests to the UDP port.
+	 */
+	public void testQueryRequests() {
+		establishConnection();
+		FileManager fm = ROUTER_SERVICE.getFileManager();
+		File[] sharedDirs = SettingsManager.instance().getDirectories();
+		FileDesc[] fds = fm.getSharedFileDescriptors(sharedDirs[0]);		
 		for(int i=0; i<fds.length; i++) {
 			String curName = fds[i].getName(); 
 			QueryRequest qr = new QueryRequest((byte)1, (byte)0, 
@@ -113,8 +159,6 @@ public class UDPServiceTest extends TestCase {
 				fail("unexpected exception: "+e);
 			}		
 		}
-
-		BACKEND.shutdown("GUESS CLIENT");
 	}
 
 
@@ -124,6 +168,17 @@ public class UDPServiceTest extends TestCase {
 	private class ActivityCallbackTest extends ActivityCallbackStub {
 		public void handleQueryReply(QueryReply reply) {
 			_reply = reply;
+		}
+	}
+
+	/**
+	 * Helper class to intercept any desired messages coming back from the server.
+	 */
+	private class MessageRouterTestStub extends MessageRouterStub {
+		public void handleUDPPingReply(PingReply reply, ReplyHandler handler,
+									   InetAddress address, int port) {
+			super.handleUDPPingReply(reply, handler, address, port);
+			_pingReply = reply;
 		}
 	}
 }
