a86b6143c01f9e319bc10e27fecae2f3ca915f28
afisk <afisk>
2002-12-30 03:29:49 +0000

    added more convenience members for alt locs

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/HugeTestUtils.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/HugeTestUtils.java
@@ -10,23 +10,14 @@ import java.net.*;
  * This class provides convenient data and utility functions to
  * huge test classes.
  */
-final class HugeTestUtils {
+public final class HugeTestUtils {
 
-	static final URL[] BAD_PORT_URLS = new URL[2];
-
-	static {
-		try {
-			BAD_PORT_URLS[0] = new URL("http", "www.limewire.org", -1, "test");
-			BAD_PORT_URLS[1] = new URL("http", "www.limewire.org", 66000, "test");				
-		} catch(MalformedURLException e) {
-			e.printStackTrace();
-		}
-	}
+	public static final URL[] BAD_PORT_URLS = new URL[2];
 
 	/**
 	 * Strings representing invalid URNs.
 	 */
-	static final String[] INVALID_URN_STRINGS = {
+	public static final String[] INVALID_URN_STRINGS = {
 		"urn:sha1:PLSTHIPQGSSZTS5FJUPAKUZWUGYQYPFBC",
 		"urn:sh1:PLSTHIPQGSSZTS5FJUPAKUZWUGYQYPFB",
 		"ur:sha1:PLSTHIPQGSSZTS5FJUPAKUZWUGYQYPFB",
@@ -49,7 +40,7 @@ final class HugeTestUtils {
 	/**
 	 * String representing valid URNs.
 	 */
-	static final String[] VALID_URN_STRINGS = {
+	public static final String[] VALID_URN_STRINGS = {
 		"urn:sha1:PLSTHIPQGSSZTS5FJUPAKUZWUGYQYPFB",
 		"urn:sha1:ZLSTHIPQGSSZTS5FJUPAKUZWUGZQYPFB",
 		"Urn:sha1:ALSTHIPQGSSZTS5FJUPAKUZWUGZQYPFB",
@@ -59,13 +50,20 @@ final class HugeTestUtils {
 		"UrN:sha1:RLSTHIPQGSSZTS5FJUPAKUZWUGYQYPFB",
 		"urn:sHa1:ILSTIIPQGSSZTS5FJUPAKUZWUGYQYPFB",
 		"urn:sha1:FLSTXIPQGSSZTS5FJUPAKUZWUGYQYPFB",
-		"urn:sha1:ULSTTIPQGSSZTS5FJUPAKUZWUGYQYPFB"
+		"urn:sha1:ULSTTIPQGSSZTS5FJUPAKUZWUGYQYPFB",
+		"urn:sha1:ULSTTIPQGSSZTS5FJUPAKUZWUGYQYPZB",
+		"urn:sha1:ULSTTIPQGSSZTS5FJUPAKUZWUGYQYPZC",
+		"urn:sha1:ULSTTIPQGSSZTS5FJUPAKUZWUGYQYPZD",
+		"urn:sha1:ULSTTIPQGSSZTS5FJUPAKUZWUGYQYPZE",
+		"urn:sha1:ULSTTIPQGSSZTS5FJUPAKUZWUGYQYPRC",
+		"urn:sha1:ULSTTIPQGSSZTS5FJUPAKUZWUGYQYPSD",
+		"urn:sha1:ULSTTIPQGSSZTS5FJUPAKUZWUGYQYPTE",
 	};
 
 	/**
 	 * Array of query strings.
 	 */
-	static final String[] QUERY_STRINGS = {
+	public static final String[] QUERY_STRINGS = {
 		"urn:Sha1:PLSTHIPQGSSZTS5FJUPAKUZWUGYQYPFB",
 		"file",
 		"different file",
@@ -90,7 +88,7 @@ final class HugeTestUtils {
 	/**
 	 * URLs for general use.
 	 */
-	static final String [] URL_STRINGS = {
+	public static final String [] HOST_STRINGS = {
 		"www.limewire.com",
 		"www.cnn.com",
 		"www.test.org",
@@ -106,13 +104,14 @@ final class HugeTestUtils {
 		"www.apple.com",
 		"www.gnutella.com",
 		"www.gnutellanews.com",
-		"java.sun.com"
+		"java.sun.com",
+		"www.jakarta.org",
 	};
 
 	/**
 	 * Alternate locations with timestamps.
 	 */
-	static final String[] VALID_TIMESTAMPED_LOCS = {
+	public static final String[] VALID_TIMESTAMPED_LOCS = {
 		"http://201.34.78.2:6352/get/2/"+
 		                     "lime%20capital%20management%2001.mpg "+
 		                     "2002-04-09T20:32:33Z",
@@ -148,7 +147,7 @@ final class HugeTestUtils {
 	/**
 	 * Alternate locations without timestamps.
 	 */
-	static final String[] VALID_NONTIMESTAMPED_LOCS = {
+	public static final String[] VALID_NONTIMESTAMPED_LOCS = {
 		"http://23.40.39.40:6352/get/2/"+
 		    "lime%20capital%20management%2001.mpg",
 		"http://221.20.12.36:6352/get/2/"+
@@ -168,7 +167,7 @@ final class HugeTestUtils {
 	/**
 	 * Alternate locations without timestamps that are not firewalled.
 	 */
-	static final String[] NON_FIREWALLED_LOCS = {
+	public static final String[] NON_FIREWALLED_LOCS = {
 		"http://50.40.39.40:6352/get/2/"+
 		    "lime%20capital%20management%2001.mpg",
 		"http://51.20.12.36:6352/get/2/"+
@@ -183,7 +182,7 @@ final class HugeTestUtils {
 		    "lime%20capital%20management%2001.mpg"
 	};
 	
-    static final String[] FIREWALLED_LOCS = {
+    public static final String[] FIREWALLED_LOCS = {
 		"http://192.168.39.40:6352/get/2/"+
 		    "lime%20capital%20management%2001.mpg",
 		"http://127.20.12.36:6352/get/2/"+
@@ -203,7 +202,7 @@ final class HugeTestUtils {
 	/**
 	 * Invalid alternate locations.
 	 */
-	static final String[] INVALID_LOCS = {
+	public static final String[] INVALID_LOCS = {
 		"",
 		null,
 		"http",
@@ -223,7 +222,7 @@ final class HugeTestUtils {
 	/**
 	 * Array of GUIDs for use by tests.
 	 */
-	static final GUID[] GUIDS = {
+	public static final GUID[] GUIDS = {
 		new GUID(GUID.makeGuid()),
 		new GUID(GUID.makeGuid()),
 		new GUID(GUID.makeGuid()),
@@ -235,24 +234,126 @@ final class HugeTestUtils {
 		new GUID(GUID.makeGuid())
 	};
 
-
 	/**
 	 * Array of URNs for use by tests.
 	 */
-	static final URN[] URNS = new URN[VALID_URN_STRINGS.length];
+	public static final URN[] URNS = new URN[VALID_URN_STRINGS.length];
 
 	/**
 	 * Array of URNType instances for use by tests.
 	 */
-	static final UrnType[] URN_TYPES = new UrnType[VALID_URN_STRINGS.length];
-	private static final ArrayList URN_SETS = new ArrayList();
+	public static final UrnType[] URN_TYPES = new UrnType[VALID_URN_STRINGS.length];
+
+	public static final URL[] UNEQUAL_URLS = new URL[HOST_STRINGS.length];
+	public static final URL[] EQUAL_URLS = new URL[HOST_STRINGS.length];
+
+	/**
+	 * Array of unequal alternate locations for testing convenience.
+	 */
+	public static final AlternateLocation[] UNEQUAL_SHA1_LOCATIONS = 
+		new AlternateLocation[UNEQUAL_URLS.length];
+
+	/**
+	 * Array of alternate locations with equal hashes but unequal host names 
+	 * for testing convenience.
+	 */
+	public static final AlternateLocation[] EQUAL_SHA1_LOCATIONS = 
+		new AlternateLocation[EQUAL_URLS.length];
+
+	public static final Set[] URN_SETS = new Set[VALID_URN_STRINGS.length];
+
+	/**
+	 * A "unique" SHA1 for convenience.
+	 */
+	public static URN UNIQUE_SHA1;
+
 	private static final HugeTestUtils INSTANCE = new HugeTestUtils();
+
+	static {
+		
+		try {
+			UNIQUE_SHA1 = 
+				URN.createSHA1Urn("urn:sha1:PLSTHIFQGSJZT45FJUPAKUZWUGYQYPFB");
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			BAD_PORT_URLS[0] = new URL("http", "www.limewire.org", -1, "test");
+			BAD_PORT_URLS[1] = new URL("http", "www.limewire.org", 66000, "test");				
+		} catch(MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+		for(int i=0; i<VALID_URN_STRINGS.length; i++) {
+			try {
+				URN urn = URN.createSHA1Urn(VALID_URN_STRINGS[i]);
+				URNS[i] = urn;
+				URN_TYPES[i] = urn.getUrnType();
+				Set urnSet = new HashSet();
+				urnSet.add(urn);
+				URN_SETS[i] = urnSet;
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		for(int i=0; i<HOST_STRINGS.length; i++) {
+			try {
+				UNEQUAL_URLS[i] = 
+					new URL("http", HOST_STRINGS[i], 6346, 
+							"/uri-res/N2R?"+URNS[i].httpStringValue());
+			} catch(MalformedURLException e) {
+				// this should not happen
+				e.printStackTrace();
+			}
+		}
+
+		for(int i=0; i<HOST_STRINGS.length; i++) {
+			try {
+				EQUAL_URLS[i] = 
+					new URL("http", HOST_STRINGS[i], 6346, 
+							"/uri-res/N2R?"+URNS[0].httpStringValue());
+			} catch(MalformedURLException e) {
+				// this should not happen
+				e.printStackTrace();
+			}
+		}
+
+		for(int i=0; i<UNEQUAL_SHA1_LOCATIONS.length; i++) {
+			try {
+				UNEQUAL_SHA1_LOCATIONS[i] = 
+					AlternateLocation.createAlternateLocation(UNEQUAL_URLS[i]);
+			} catch(MalformedURLException e) {
+				// this should not happen
+				e.printStackTrace();
+			}
+		}
+
+		for(int i=0; i<EQUAL_SHA1_LOCATIONS.length; i++) {
+			try {
+				EQUAL_SHA1_LOCATIONS[i] = 
+					AlternateLocation.createAlternateLocation(EQUAL_URLS[0]);
+			} catch(MalformedURLException e) {
+				// this should not happen
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	/**
+	 * Array of URNType instances for use by tests.
+	 */
+	//static final UrnType[] URN_TYPES = new UrnType[VALID_URN_STRINGS.length];
+	//private static final ArrayList URN_SETS = new ArrayList();
+	//private static final HugeTestUtils INSTANCE = new HugeTestUtils();
 
 	static HugeTestUtils instance() {
 		return INSTANCE;
 	}
 
 	private HugeTestUtils() {
+		/*
 		for(int i=0; i<VALID_URN_STRINGS.length; i++) {
 			try {
 				URN urn = URN.createSHA1Urn(VALID_URN_STRINGS[i]);
@@ -265,6 +366,7 @@ final class HugeTestUtils {
 				e.printStackTrace();
 			}
 		}
+		*/
 	}
 	
 }
