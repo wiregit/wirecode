ec05992e80d539921e75dc9160bb6fc3f10a46e6
afisk <afisk>
2002-12-30 03:31:13 +0000

    conforms with newer MessageRouter

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/stubs/MessageRouterStub.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/stubs/MessageRouterStub.java
@@ -2,55 +2,26 @@ package com.limegroup.gnutella.stubs;
 
 import com.limegroup.gnutella.*;
 import com.limegroup.gnutella.routing.*;
+import java.net.DatagramPacket;
 
 /** A stub for MessageRouter that does nothing. */
 public class MessageRouterStub extends MessageRouter {
 
-    protected  void respondToPingRequest(PingRequest pingRequest,
-                                                 Acceptor acceptor) { 
-    }
-
-
     protected  void respondToQueryRequest(QueryRequest queryRequest,
-                                                  Acceptor acceptor,
-                                                  byte[] clientGUID) {
+										  byte[] clientGUID) {
     }
 
-    protected  void respondToQueryRequest(QueryRequest queryRequest,
-                                                  byte[] clientGUID) {
-    }
-
-    protected  void handlePingReplyForMe(
-        PingReply pingReply,
-        ManagedConnection receivingConnection) {
-    }
-
-    protected  void handleQueryReplyForMe(
-        QueryReply queryReply,
-        ManagedConnection receivingConnection) {
-    }
-
-
-    protected  void handlePushRequestForMe(
-        PushRequest pushRequest,
-        ManagedConnection receivingConnection) {
-    }
     
-    protected void addQueryRoutingEntries(QueryRouteTable qrt)
-    {
+    protected void addQueryRoutingEntries(QueryRouteTable qrt) {
     }    
-
-    public GroupPingRequest createGroupPingRequest(String group) {
-        return new GroupPingRequest((byte)5, 6346, new byte[0], 0l, 0l, "");
-    }
-
-    protected void respondToUDPPingRequest(PingRequest request,
-													Acceptor acceptor) {
-	}
 
     protected void respondToPingRequest(PingRequest request) {
 	}
 
-    protected void respondToUDPPingRequest(PingRequest request) {
+    protected void respondToUDPPingRequest(PingRequest request, 
+		DatagramPacket datagram) {}
+
+	public GroupPingRequest createGroupPingRequest(String group) {
+		return new GroupPingRequest((byte)5, 6346, new byte[0], 0l, 0l, "");
 	}
 }
