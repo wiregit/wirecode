43faa5ea2b78e255d3c481413c7e9789493c60c0
afisk <afisk>
2003-01-13 04:49:55 +0000

    new settings class for Ultrapeer settings

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/settings/UltrapeerSettings.java
@@ -0,0 +1,41 @@
+package com.limegroup.gnutella.settings;
+
+/**
+ * Settings for Ultrapeers.
+ */
+public final class UltrapeerSettings extends AbstractSettings {
+
+	/**
+	 * Setting for whether or not we've ever been Ultrapeer capable.
+	 */
+	public static final BooleanSetting EVER_ULTRAPEER_CAPABLE =
+		FACTORY.createBooleanSetting("EVER_ULTRAPEER_CAPABLE", false);
+
+
+	/**
+	 * Setting for whether or not to force Ultrapeer mode.
+	 */
+	public static final BooleanSetting FORCE_ULTRAPEER_MODE =
+		FACTORY.createBooleanSetting("FORCE_ULTRAPEER_MODE", false);
+
+	/**
+	 * Setting for whether or not to disable Ultrapeer mode.
+	 */
+	public static final BooleanSetting DISABLE_ULTRAPEER_MODE =
+		FACTORY.createBooleanSetting("DISABLE_SUPERNODE_MODE", false);
+
+	
+	/**
+	 * Setting for the maximum leaf connections.
+	 */
+	public static final IntSetting MAX_LEAVES =
+		FACTORY.createIntSetting("MAX_LEAVES", 75);
+
+	/**
+	 * Setting for the minimum leaf connections.
+	 */
+	public static final IntSetting MIN_LEAVES =
+		FACTORY.createIntSetting("MIN_LEAVES", 4);
+
+}
+
