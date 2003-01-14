7765388312b2d01d42f5739c65bcabcc6293ddf0
sdaswani <sdaswani>
2003-01-14 20:26:51 +0000

    added new jar for crypto stuff.

--- components/gnutella-core/run.bat
+++ components/gnutella-core/run.bat
@@ -1,4 +1,4 @@
 
-set CLASSPATH=.;lib/collections.jar;lib/xerces.jar;lib/MRJToolkitStubs.jar
+set CLASSPATH=.;lib/collections.jar;lib/xerces.jar;lib/MRJToolkitStubs.jar;lib/logicrypto.jar
 
 java com.limegroup.gnutella.Main
