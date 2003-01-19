d8daa34dcc6f2bbaf4bd44792a52acd28e33a11c
afisk <afisk>
2003-01-19 20:48:57 +0000

    added the header for reporting support of query routing at one hop between ultrapeers

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/handshaking/SupernodeProperties.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/handshaking/SupernodeProperties.java
@@ -13,6 +13,10 @@ public class SupernodeProperties extends LazyProperties
         super(remoteIP);
         //set supernode property
         put(ConnectionHandshakeHeaders.X_SUPERNODE, "True");
+        put(ConnectionHandshakeHeaders.X_DEGREE, 
+			Integer.toString(ConnectionManager.MIN_CONNECTIONS_FOR_SUPERNODE));
+		put(ConnectionHandshakeHeaders.X_ULTRAPEER_QUERY_ROUTING,
+			"true");
         addCommonProperties(this);
     }
     
