3cdff9149e203309fb2871f908e58957fd9ea52f
afisk <afisk>
2003-01-31 19:53:36 +0000

    slight changes

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/LocalBackend.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/LocalBackend.java
@@ -1,4 +1,5 @@
 package com.limegroup.gnutella;
+import com.limegroup.gnutella.settings.*;
 
 /**
  * Specialized class that creates a <tt>Backend</tt> that only accepts local
@@ -8,12 +9,13 @@ package com.limegroup.gnutella;
 public class LocalBackend extends Backend {
 
 	LocalBackend(ActivityCallback callback, MessageRouter router,
-				 int timeout) {
-		super(callback, router, timeout);
+				 int timeout, int port) {
+		super(callback, router, timeout, port);
+        ConnectionSettings.LOCAL_IS_PRIVATE.setValue(false);
 		SettingsManager settings = SettingsManager.instance();
         settings.setBannedIps(new String[] {"*.*.*.*"});
         settings.setAllowedIps(new String[] {
-			"127.*.*.*", "18.239.0.*", "10.254.0.*",
+			"127.*.*.*", "10.254.0.*",
 		});		
 	}
 }
