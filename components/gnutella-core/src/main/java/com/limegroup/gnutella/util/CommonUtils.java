406d0c1da41e53cc479ea1d2632c7bec922d5a8f
afisk <afisk>
2003-02-05 17:28:10 +0000

    added port checking method

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/util/CommonUtils.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/util/CommonUtils.java
@@ -915,7 +915,16 @@ public final class CommonUtils {
 		} 
 	}
         
-
+	/**
+	 * Returns whether or not the specified port is within the valid range of
+	 * ports.
+	 *
+	 * @param port the port number to check
+	 */
+	public static boolean isValidPort(int port) {
+		if((port & 0xFFFF0000) != 0) return false;
+		return true;
+	}
 
     /*
     public static void main(String args[]) {
