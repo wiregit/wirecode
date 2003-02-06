31a8c7f7eca4850bcde4829e5bdb0e7976a1f410
sthadani <sthadani>
2003-02-06 16:24:42 +0000

    ProManager: Added code to connect to a server, and authenticate it using the the certificate we will distribute with LimeWire.
    limecert.cert : The certificate to use added to the TrustedSources of the clients. So that the limewire certificates are treates as if they were signed by an authority.
    
    Added jsse.jar, jnet.jar and jcert.jar

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/updates/ProManager.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/updates/ProManager.java
@@ -1,6 +1,17 @@
 package com.limegroup.gnutella.updates;
 
 import com.limegroup.gnutella.util.*;
+import com.limegroup.gnutella.*;
+import com.sun.net.ssl.SSLContext;
+import com.sun.net.ssl.TrustManagerFactory;
+import com.sun.net.ssl.TrustManager;
+import javax.net.ssl.*;
+import javax.net.*;
+import java.net.*;
+import java.io.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.*;
 
 /**
  * Manages, buying of PRO from within LimeWire. Also, tells the rest of the code
@@ -22,9 +33,49 @@ public class ProManager {
         return(macAddress!=null && CommonUtils.isJava13OrLater());
     }
     
-    public boolean buyPro(String creditCardNumber) {
+    public boolean buyPro(String ccNumber, String expDate)  {
+        try {
+        Assert.that(canBuyPro());
+		Security.addProvider(
+			new com.sun.net.ssl.internal.ssl.Provider());
+        CertificateFactory certFact = CertificateFactory.getInstance("X.509");
+        FileInputStream fis = new FileInputStream("core/lib/limecert.cert");
+        Certificate cert = certFact.generateCertificate(fis);
+        KeyStore store = KeyStore.getInstance("JKS");
+        store.load(null,null);
+        store.setCertificateEntry("lime",cert);        
+        TrustManagerFactory tf = TrustManagerFactory.getInstance("SunX509","SunJSSE");
+        tf.init(store);
+        SSLContext context = SSLContext.getInstance("SSLv3");
+        com.sun.net.ssl.TrustManager[] mans = 
+                          (com.sun.net.ssl.TrustManager[])tf.getTrustManagers();
+        context.init(null,mans,null);
+        SSLSocketFactory factory = context.getSocketFactory();
+        SSLSocket socket = (SSLSocket)factory.createSocket(
+                             InetAddress.getByName("127.0.0.1"),7000);
+        //Force the socket to do the SSL handshake now.
+        socket.startHandshake();
+        OutputStream os = socket.getOutputStream();
+        while(true) {
+            String a = "Sumeet\r\n";
+            System.out.println("\t\t\t"+a);
+            os.write(a.getBytes());
+            os.flush();
+            Thread.sleep(10000);
+        }
+        //Limewires will ship with the certificate that we use at the client
+        //end to authenticate the server. This is however not fool proof - what
+        //happens if someone manages to replace that certificate?
+        //We will use the other key pair that ships with limewire to have
+        //the server sign something unique from the client. 
+        //Lets send out client GUID, and have the server sign it.
+        
+        
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         //1. open a secure socket to LimeWire server.
-        //2. Send the Credit card info as well as the credit card number
+        //2. Send the Credit card info as well as macAddress.
         //3. Server will send back a signed file, save it.
         //4. close the socket.
         return true;
@@ -43,5 +94,15 @@ public class ProManager {
         //6. If verified  and  currTimeMillis() < expiry time return true
         //7. Else return false
         return false;
+    }
+
+    public static void main(String[] args) {
+        try {
+            ProManager man = new ProManager();
+            man.buyPro("Sumeet", "Ashish");
+        } catch(Exception e) {
+            e.printStackTrace();
+            System.out.println("Sumeet: test failed");
+        }
     }
 }
