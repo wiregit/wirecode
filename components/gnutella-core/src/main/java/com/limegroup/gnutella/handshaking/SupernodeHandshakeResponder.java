009c5098a47a342c44a017655e65a911ef7ba53a
afisk <afisk>
2003-02-14 22:25:34 +0000

    significantly simplified handshake logic

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/handshaking/SupernodeHandshakeResponder.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/handshaking/SupernodeHandshakeResponder.java
@@ -30,75 +30,80 @@ public class SupernodeHandshakeResponder
     
     //inherit doc comment
     protected HandshakeResponse respondUnauthenticated(
-        HandshakeResponse response, boolean outgoing) throws IOException
-    {
+        HandshakeResponse response, boolean outgoing) throws IOException {
         
-        if(!outgoing)
-        {
-            //Incoming connection....
-            Properties ret=new SupernodeProperties(getRemoteIP());
-            
-            //guide the incoming connection to be a supernode/clientnode
-            ret.put(ConnectionHandshakeHeaders.X_SUPERNODE_NEEDED,
-            (new Boolean(_manager.supernodeNeeded())).toString());
-            
-            //give own IP address
-            ret.put(ConnectionHandshakeHeaders.LISTEN_IP,
-					Message.ip2string(RouterService.getAddress())+":"
-					+ RouterService.getPort());
-            
-            //also add some host addresses in the response
-            addHostAddresses(ret, _manager);
+		if(outgoing) return respondToOutgoing(response);
+		return respondToIncoming(response);
+		
+		
+	}
 
-            //Decide whether to allow or reject.  Somewhat complicated because
-            //of ultrapeer guidance.
-            if (reject(outgoing, response.getHeaders())) {
-                return new HandshakeResponse(
-                    HandshakeResponse.SLOTS_FULL,
-                    HandshakeResponse.SLOTS_FULL_MESSAGE,
-                    ret);
-            } else {
-                return new HandshakeResponse(ret);
-            }
-        } else
-        {
-            //Outgoing connection.  If the other guy is ultrapeer unaware and I
-            //already have enough old-fashioned connections, reject it.  We've
-            //already given ultrapeer guidance at this point, so there's no
-            //"second chance" like in the reject(..) method.
-            Properties ret=new Properties();
+	/**
+	 * Respond to an outgoing connection request.
+	 *
+	 * @param response the headers read from the connection
+	 */
+	private HandshakeResponse respondToOutgoing(HandshakeResponse response) {
+		//Outgoing connection.  If the other guy is ultrapeer unaware and I
+		//already have enough old-fashioned connections, reject it.  We've
+		//already given ultrapeer guidance at this point, so there's no
+		//"second chance" like in the reject(..) method.
 
-			Properties headers = response.getHeaders();			
-			String ultrapeerHeader =
-				headers.getProperty(ConnectionHandshakeHeaders.X_SUPERNODE);
-
-			boolean isUltrapeer=ConnectionHandshakeHeaders.isTrue(ultrapeerHeader);
-			int connections = getNumIntraUltrapeerConnections(headers, isUltrapeer);
-            if (!_manager.allowConnection(outgoing,
-										  ultrapeerHeader,
-                                          headers.getProperty(
-                                              ConnectionHandshakeHeaders.USER_AGENT),
-										  connections)) {
-                return new HandshakeResponse(
-                    HandshakeResponse.SLOTS_FULL,
-                    HandshakeResponse.SLOTS_FULL_MESSAGE,
-                    ret);
-            }
-            //Did the server request we become a leaf?
-            String neededS=response.getHeaders().
+		if(!_manager.allowConnection(response)) {			
+			return new HandshakeResponse(HandshakeResponse.SLOTS_FULL,
+										 HandshakeResponse.SLOTS_FULL_MESSAGE,
+										 new Properties());
+		}
+		//Did the server request we become a leaf?
+		String neededS=response.getHeaders().
             getProperty(ConnectionHandshakeHeaders.X_SUPERNODE_NEEDED);
-            if (neededS!=null && 
-				!Boolean.valueOf(neededS).booleanValue() && 
-				isNotBearshare(headers) &&
-				_manager.allowLeafDemotion())
-            {
-                //Fine, we'll become a leaf.
-                ret.put(ConnectionHandshakeHeaders.X_SUPERNODE,
-                "False");
-            }
-            return new HandshakeResponse(ret);
-        }
-    }
+
+		Properties ret = new Properties();
+		if (neededS!=null && 
+			!Boolean.valueOf(neededS).booleanValue() && 
+			isNotBearshare(response) &&
+			_manager.allowLeafDemotion()) {
+			//Fine, we'll become a leaf.
+			ret.put(ConnectionHandshakeHeaders.X_SUPERNODE,
+					"False");
+		}
+		return new HandshakeResponse(ret);
+	}
+
+	/**
+	 * Respond to an incoming connection request.
+	 *
+	 * @param response the headers read from the connection
+	 */
+	private HandshakeResponse respondToIncoming(HandshakeResponse response) {
+		//Incoming connection....
+		Properties ret=new SupernodeProperties(getRemoteIP());
+		
+		//guide the incoming connection to be a supernode/clientnode
+		ret.put(ConnectionHandshakeHeaders.X_SUPERNODE_NEEDED,
+				(new Boolean(_manager.supernodeNeeded())).toString());
+		
+		//give own IP address
+		ret.put(ConnectionHandshakeHeaders.LISTEN_IP,
+				Message.ip2string(RouterService.getAddress())+":"
+				+ RouterService.getPort());
+		
+		//also add some host addresses in the response
+		addHostAddresses(ret, _manager);
+		
+		//Decide whether to allow or reject.  Somewhat complicated because
+		//of ultrapeer guidance.
+
+		// TODO::add special cases for the type of client we're connecting to
+		if (reject(response)) {
+			return new HandshakeResponse(HandshakeResponse.SLOTS_FULL,
+										 HandshakeResponse.SLOTS_FULL_MESSAGE,
+										 ret);
+		}
+		
+		return new HandshakeResponse(ret);
+	}
+
 
 	/**
 	 * Returns whether or not the set of connection headers denotes that the
@@ -108,9 +113,8 @@ public class SupernodeHandshakeResponder
 	 *
 	 * @param headers the connection headers
 	 */
-	private boolean isNotBearshare(Properties headers) {
-        String userAgent =
-			headers.getProperty(ConnectionHandshakeHeaders.USER_AGENT);		
+	private boolean isNotBearshare(HandshakeResponse headers) {
+        String userAgent = headers.getUserAgent();
 		if(userAgent == null) return false;
 		return !userAgent.startsWith("BearShare");
 	}
@@ -118,7 +122,7 @@ public class SupernodeHandshakeResponder
     /** 
      * Returns true if this incoming connections should be rejected with a 503. 
      */
-    private boolean reject(boolean outgoing, Properties headers) { 
+    private boolean reject(HandshakeResponse response) { 
         //Under some circumstances, we can decide to reject a connection during
         //handshaking because no slots are available.  You might think you could
         //reject the connection if !_manager.allowConnection(A, L), where A
@@ -128,46 +132,14 @@ public class SupernodeHandshakeResponder
         //guidance; we don't know whether they'll become a leaf node or not.  So
         //we use the following conservative test, and depend on the
         //old-fashioned reject connection mechanism in ConnectionManager for the
-        //other cases.
-        
-        String useragentHeader =
-			headers.getProperty(ConnectionHandshakeHeaders.USER_AGENT);
-        String ultrapeerHeader =
-			headers.getProperty(ConnectionHandshakeHeaders.X_SUPERNODE);
-        boolean isUltrapeer=ConnectionHandshakeHeaders.isTrue(ultrapeerHeader);
+        //other cases.       
 
-		int degree = getNumIntraUltrapeerConnections(headers, isUltrapeer);
+        boolean allowedNow = _manager.allowConnection(response);
 
-        boolean allowedNow=_manager.allowConnection(
-            outgoing, ultrapeerHeader, useragentHeader, degree);
-        boolean allowedAsLeaf=_manager.allowConnection(
-            outgoing, ConnectionHandshakeHeaders.FALSE, useragentHeader, degree);
+		boolean allowedAsLeaf = _manager.allowConnectionAsLeaf(response);
 
         //Reject if not allowed now and guidance not possible.
-        return ! (allowedNow || (isUltrapeer && allowedAsLeaf));
-    }    
-
-
-	/**
-	 * Accessor for the reported number of intra-Ultrapeer connections
-	 * this connection attempts to maintain.  If the node is not an
-	 * Ultrapeer, this returns 0.  If it is an Ultrapeer but does not
-	 * support this header, we assume that it tries to maintain 6 intra-
-	 * Ultrapeer connections.
-	 *
-	 * @return the number of intra-Ultrapeer connections the connected node
-	 *  attempts to maintain, as reported in the X-Degree handshake header
-	 *  or guessed at otherwise
-	 */
-	private int getNumIntraUltrapeerConnections(Properties headers, 
-												boolean isUltrapeer) {
-		String connections = 
-			headers.getProperty(ConnectionHandshakeHeaders.X_DEGREE);
-		if(connections == null) {
-			if(isUltrapeer) return 6;
-			return 0;
-		}
-		return Integer.valueOf(connections).intValue();
-	}
+        return ! (allowedNow || (response.isSupernodeConnection() && allowedAsLeaf));
+    }
 }
 
