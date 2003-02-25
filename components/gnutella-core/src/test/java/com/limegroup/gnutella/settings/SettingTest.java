323691eff93f6d2a52e1595a1eceb26004c6730e
afisk <afisk>
2003-02-25 16:47:39 +0000

    changes by ken and myself to get file setting tests to work on Windows as well

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/settings/SettingTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/settings/SettingTest.java
@@ -65,6 +65,21 @@ public class SettingTest extends com.limegroup.gnutella.util.BaseTestCase {
 
     public void testSetting() throws Exception {
         
+        String testFilePath;
+        if(CommonUtils.isUnix() || CommonUtils.isMacOSX()) {
+            testFilePath = "/mickey-mouse.com";
+        } else {
+            // assume we're on windows
+            testFilePath = "c:/mickey-mouse.com";
+        }
+
+        String testFilePath2;
+        if(CommonUtils.isUnix() || CommonUtils.isMacOSX()) {
+            testFilePath2 = "/temp/turkey.txt";
+        } else {
+            // assume we're on windows
+            testFilePath2 = "c:/temp/turkey.txt";
+        }
     
         /* Delete any existing file and create a new set of settings */
         TestSettings settings = new TestSettings(settingsFile);
@@ -90,7 +105,7 @@ public class SettingTest extends com.limegroup.gnutella.util.BaseTestCase {
         settings.BOOL_SETTING.setValue(false);
         settings.BYTE_SETTING.setValue((byte)6);
         settings.COLOR_SETTING.setValue(new Color(66, 44, 67));
-        settings.FILE_SETTING.setValue(new File("/mickey-mouse.com"));
+        settings.FILE_SETTING.setValue(new File(testFilePath));
         settings.INT_SETTING.setValue(234);
         settings.LONG_SETTING.setValue(555555);
         settings.STRING_SETTING.setValue("OK so far");
@@ -98,7 +113,8 @@ public class SettingTest extends com.limegroup.gnutella.util.BaseTestCase {
         assertEquals("Bool set", false, settings.BOOL_SETTING.getValue());
         assertEquals("Byte set", (byte)6, settings.BYTE_SETTING.getValue());
         assertEquals("Color set", new Color(66,44,67), settings.COLOR_SETTING.getValue());
-        //assertEquals("File set", "/mickey-mouse.com", settings.FILE_SETTING.getValue().toString());
+        assertEquals("File set", new File(testFilePath), 
+                                 settings.FILE_SETTING.getValue());
         assertEquals("Int set", 234, settings.INT_SETTING.getValue());
         assertEquals("Long set", 555555, settings.LONG_SETTING.getValue());
         assertEquals("String set", "OK so far", settings.STRING_SETTING.getValue());
@@ -110,7 +126,8 @@ public class SettingTest extends com.limegroup.gnutella.util.BaseTestCase {
         assertEquals("Bool set", false, settings.BOOL_SETTING.getValue());
         assertEquals("Byte set", (byte)6, settings.BYTE_SETTING.getValue());
         assertEquals("Color set", new Color(66,44,67), settings.COLOR_SETTING.getValue());
-        //assertEquals("File set", "/mickey-mouse.com", settings.FILE_SETTING.getValue().toString());
+        assertEquals("File set", new File(testFilePath), 
+                                 settings.FILE_SETTING.getValue());
         assertEquals("Int set", 234, settings.INT_SETTING.getValue());
         assertEquals("Long set", 555555, settings.LONG_SETTING.getValue());
         assertEquals("String set", "OK so far", settings.STRING_SETTING.getValue());
@@ -119,7 +136,8 @@ public class SettingTest extends com.limegroup.gnutella.util.BaseTestCase {
         assertEquals("Bool default", true, settings2.BOOL_SETTING.getValue());
         assertEquals("Byte default", (byte)23, settings2.BYTE_SETTING.getValue());
         assertEquals("Color default", new Color(255,127,63), settings2.COLOR_SETTING.getValue());
-        //assertEquals("File default", "/temp/turkey.txt", settings2.FILE_SETTING.getValue().toString());
+        assertEquals("File default", new File(testFilePath2), 
+                                     settings2.FILE_SETTING.getValue());
         assertEquals("Int default", 143, settings2.INT_SETTING.getValue());
         assertEquals("Long default", 666666, settings2.LONG_SETTING.getValue());
         assertEquals("String default", "terrific", settings2.STRING_SETTING.getValue());
@@ -129,7 +147,8 @@ public class SettingTest extends com.limegroup.gnutella.util.BaseTestCase {
         assertEquals("Bool set", false, settings2.BOOL_SETTING.getValue());
         assertEquals("Byte set", (byte)6, settings2.BYTE_SETTING.getValue());
         assertEquals("Color set", new Color(66,44,67), settings2.COLOR_SETTING.getValue());
-        //assertEquals("File set", "/mickey-mouse.com", settings2.FILE_SETTING.getValue().toString());
+        assertEquals("File set", new File(testFilePath), 
+                                 settings2.FILE_SETTING.getValue());
         assertEquals("Int set", 234, settings2.INT_SETTING.getValue());
         assertEquals("Long set", 555555, settings2.LONG_SETTING.getValue());
         assertEquals("String set", "OK so far", settings2.STRING_SETTING.getValue());
