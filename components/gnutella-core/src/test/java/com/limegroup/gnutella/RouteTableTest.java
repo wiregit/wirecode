fd7164f7e0eda142f0b989cf2999b5021f4d5991
sdaswani <sdaswani>
2003-03-03 16:48:49 +0000

    moved new tests to a new method, also increased sleep time because the legacy test was failing sometimes due to race conditions....

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/RouteTableTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/RouteTableTest.java
@@ -34,40 +34,17 @@ public final class RouteTableTest extends BaseTestCase {
         ReplyHandler c4=new StubReplyHandler();
 
         //1. Test time replacement policy (glass box).
-        int MSECS=1000;
+        int MSECS=1250;
         rt=new RouteTable(MSECS/1000, Integer.MAX_VALUE);
         rt.routeReply(g1, c1);
         rt.routeReply(g2, c2);                    //old, new:
         rt.routeReply(g3, c3);                    //{}, {g1, g2, g3}
-        rt.setTTL(g3, (byte)1);
-        rt.setTTL(g2, (byte)2);
-        rt.setTTL(g1, (byte)3);
-        try {
-            rt.getAndSetTTL(g1, (byte)0, (byte)2);
-            assertTrue(false);
-        }
-        catch (IllegalArgumentException expected) {}
-        try {
-            rt.getAndSetTTL(g1, (byte)2, (byte)2);
-            assertTrue(false);
-        }
-        catch (IllegalArgumentException expected) {}
-        try {
-            rt.getAndSetTTL(g1, (byte)3, (byte)4);
-        }
-        catch (IllegalArgumentException expected) {
-            assertTrue(false);
-        }
         assertSame(c1, rt.getReplyHandler(g1));
         assertSame(c2, rt.getReplyHandler(g2));
         assertSame(c3, rt.getReplyHandler(g3, 0, (short) 0).getReplyHandler());
         assertNull(rt.getReplyHandler(g4, 0, (short) 0));
         try { Thread.sleep(MSECS); } catch (InterruptedException e) { }
         assertNotNull(rt.tryToRouteReply(g4, c4));  //{g1, g2, g3}, {g4}
-        assertTrue(rt.getTTL(g3) == (byte) 1);
-        assertTrue(rt.getTTL(g2) == (byte) 2);
-        assertTrue(rt.getTTL(g1) == (byte) 4);
-        assertTrue(rt.getTTL(g4) == (byte) 0);
         assertSame(c1, rt.getReplyHandler(g1));
         rt.routeReply(g1, c1);                    //{g2, g3}, {g1, g4}
         assertSame(c1, rt.getReplyHandler(g1));
@@ -76,10 +53,6 @@ public final class RouteTableTest extends BaseTestCase {
         assertSame(c4, rt.getReplyHandler(g4));
         try { Thread.sleep(MSECS); } catch (InterruptedException e) { }
         rt.routeReply(g2, c3);                     //{g1, g4}, {g2}
-        assertTrue(rt.getTTL(g1) == (byte) 4);
-        assertTrue(rt.getTTL(g4) == (byte) 0);
-        assertTrue(rt.getTTL(g2) == (byte) 0);
-        rt.setTTL(g2, (byte)4);
         debug(rt.toString());
         assertSame(c1, rt.getReplyHandler(g1));
         assertSame(c3, rt.getReplyHandler(g2));
@@ -87,7 +60,6 @@ public final class RouteTableTest extends BaseTestCase {
         assertSame(c4, rt.getReplyHandler(g4));
         try { Thread.sleep(MSECS); } catch (InterruptedException e) { }
         assertNull(rt.tryToRouteReply(g2, c2));  //{g2}, {}}
-        assertTrue(rt.getTTL(g2) == (byte) 4);
         assertNull(rt.getReplyHandler(g1));
         assertSame(c3, rt.getReplyHandler(g2));
         assertNull(rt.getReplyHandler(g3));
@@ -190,6 +162,68 @@ public final class RouteTableTest extends BaseTestCase {
         assertNull(rt.getReplyHandler(g1,0, (short) 0));                    
     }
 
+
+    public void testTTLAdditions() {
+        RouteTable rt=null;
+        int MSECS=1000;
+        rt=new RouteTable(MSECS/1000, Integer.MAX_VALUE);
+        byte[] g1=new byte[16]; g1[0]=(byte)1;
+        byte[] g2=new byte[16]; g2[0]=(byte)2;
+        byte[] g3=new byte[16]; g3[0]=(byte)3;
+        byte[] g4=new byte[16]; g4[0]=(byte)4;
+        ReplyHandler c1=new StubReplyHandler();
+        ReplyHandler c2=new StubReplyHandler();
+        ReplyHandler c3=new StubReplyHandler();
+        ReplyHandler c4=new StubReplyHandler();
+
+        // test setTTL and getAndSetTTL
+        rt.setTTL(rt.tryToRouteReply(g2, c2), (byte)2);
+        rt.setTTL(rt.tryToRouteReply(g1, c1), (byte)3);
+        try {
+            rt.setTTL(null, (byte) 1);
+            assertTrue(false);
+        }
+        catch (IllegalArgumentException expected) {}
+        try {
+            rt.setTTL(rt.tryToRouteReply(g3, c3), (byte) 0);
+            assertTrue(false);
+        }
+        catch (IllegalArgumentException expected) {}
+        try {
+            rt.setTTL(new ResultCounterImpl(), (byte) 1);
+            assertTrue(false);
+        }
+        catch (IllegalArgumentException expected) {}
+        try {
+            rt.getAndSetTTL(g3, (byte)0, (byte)2);
+            assertTrue(false);
+        }
+        catch (IllegalArgumentException expected) {}
+        try {
+            rt.getAndSetTTL(g2, (byte)2, (byte)2);
+            assertTrue(false);
+        }
+        catch (IllegalArgumentException expected) {}
+        try {
+            assertTrue(rt.getAndSetTTL(g1, (byte)3, (byte)4));
+        }
+        catch (IllegalArgumentException expected) {
+            assertTrue(false);
+        }
+        try {
+            assertTrue(rt.getAndSetTTL(g1, (byte)4, (byte)5));
+        }
+        catch (IllegalArgumentException expected) {
+            assertTrue(false);
+        }
+        try {
+            assertTrue(!rt.getAndSetTTL(g2, (byte)3, (byte)5));
+        }
+        catch (IllegalArgumentException expected) {
+            assertTrue(false);
+        }
+    }
+
     
     private Map getMap(RouteTable rt, String mapName) 
         throws IllegalAccessException, NoSuchFieldException {
@@ -206,6 +240,14 @@ public final class RouteTableTest extends BaseTestCase {
             System.out.println(out);
     }
 
+
+    private static class ResultCounterImpl 
+        implements com.limegroup.gnutella.search.ResultCounter {
+
+        public int getNumResults() {
+            return 0;
+        }
+    }
 
     private static class StubReplyHandler implements ReplyHandler {
         public boolean isOpen() {
