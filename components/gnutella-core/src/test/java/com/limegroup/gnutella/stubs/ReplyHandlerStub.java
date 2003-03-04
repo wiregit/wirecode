b0dcf81c65299fc06efd481233f1376e5bb7b3b1
afisk <afisk>
2003-03-04 01:30:23 +0000

    new stub for reply handlers

---
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/stubs/ReplyHandlerStub.java
@@ -0,0 +1,58 @@
+package com.limegroup.gnutella.stubs;
+
+import com.limegroup.gnutella.*;
+import com.limegroup.gnutella.messages.*;
+import com.sun.java.util.collections.*;
+
+/**
+ * Stub for the <tt>ReplyHandler</tt> interface.
+ */
+public class ReplyHandlerStub implements ReplyHandler {
+    
+    public boolean isOpen() {
+        return true;
+    }
+    public void handlePingReply(PingReply pingReply, 
+                                ReplyHandler receivingConnection) {
+    }
+    public void handlePushRequest(PushRequest pushRequest, 
+                                  ReplyHandler receivingConnection) {
+    }
+    public void handleQueryReply(QueryReply queryReply, 
+                                 ReplyHandler receivingConnection) {
+    }
+    public int getNumMessagesReceived() {
+        return 0;
+    }
+    public void countDroppedMessage() {
+    }
+    public Set getDomains() {
+        return null;
+    }
+    public boolean isPersonalSpam(Message m) {
+        return false;
+    }
+    public boolean isOutgoing() {
+        return false;
+    }
+    public boolean isKillable() {
+        return false;
+    }
+    public boolean isSupernodeClientConnection() {
+        return false;
+    }
+    public boolean isLeafConnection() {
+        return false;
+    }
+    public boolean isHighDegreeConnection() {
+        return false;
+    }
+    
+    public boolean isUltrapeerQueryRoutingConnection() {
+        return false;
+    }
+    
+    public boolean isGoodConnection() {
+        return false;
+    }
+}
