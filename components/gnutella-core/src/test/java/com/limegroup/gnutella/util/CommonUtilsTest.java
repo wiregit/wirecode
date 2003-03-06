6664fee5becdcba12a078bcfb291366e88922add
afisk <afisk>
2003-03-06 03:09:55 +0000

    moved network-related tests to NetworkUtilsTest

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/util/CommonUtilsTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/util/CommonUtilsTest.java
@@ -20,85 +20,6 @@ public class CommonUtilsTest extends com.limegroup.gnutella.util.BaseTestCase {
         return buildTestSuite(CommonUtilsTest.class);
     }  
 
-    public void testIsPrivateAddress() throws Exception {
-        ConnectionSettings.LOCAL_IS_PRIVATE.setValue(true);
-        byte[] address = new byte[4];
-        
-        address = InetAddress.getByName("2.32.0.1").getAddress();
-        assertTrue("should not be a private address"+address,
-                   !CommonUtils.isPrivateAddress(address));
-
-        address = InetAddress.getByName("1.32.0.1").getAddress();
-        assertTrue("should not be a private address"+address,
-                   !CommonUtils.isPrivateAddress(address));
-
-        address = InetAddress.getByName("127.0.0.1").getAddress();
-        assertTrue("should be a private address"+address,
-                   CommonUtils.isPrivateAddress(address));
-
-        address = InetAddress.getByName("127.2.0.1").getAddress();
-        assertTrue("should be a private address"+address,
-                   CommonUtils.isPrivateAddress(address));
-
-        address = InetAddress.getByName("192.168.0.1").getAddress();
-        assertTrue("should be a private address"+address,
-                   CommonUtils.isPrivateAddress(address));
-
-        address = InetAddress.getByName("172.16.0.1").getAddress();
-        assertTrue("should be a private address"+address,
-                   CommonUtils.isPrivateAddress(address));
-
-        address = InetAddress.getByName("172.31.0.1").getAddress();
-        assertTrue("should be a private address"+address,
-                   CommonUtils.isPrivateAddress(address));
-
-        address = InetAddress.getByName("169.254.0.1").getAddress();
-        assertTrue("should be a private address"+address,
-                   CommonUtils.isPrivateAddress(address));
-
-        address = InetAddress.getByName("0.254.0.1").getAddress();
-        assertTrue("should be a private address"+address,
-                   CommonUtils.isPrivateAddress(address));
-
-        //address = InetAddress.getByName("240.254.0.1").getAddress();
-        //assertTrue("should be a private address"+address,
-        //         CommonUtils.isPrivateAddress(address));
-
-        //address = InetAddress.getByName("255.254.0.1").getAddress();
-        //assertTrue("should be a private address"+address,
-        //         CommonUtils.isPrivateAddress(address));
-
-
-        // try boundary cases to make sure they pass
-        address = InetAddress.getByName("172.15.0.1").getAddress();
-        assertTrue("should not be a private address"+address,
-                   !CommonUtils.isPrivateAddress(address));
-
-        address = InetAddress.getByName("172.32.0.1").getAddress();
-        assertTrue("should not be a private address"+address,
-                   !CommonUtils.isPrivateAddress(address));
-
-        address = InetAddress.getByName("192.167.0.1").getAddress();
-        assertTrue("should not be a private address"+address,
-                   !CommonUtils.isPrivateAddress(address));
-
-        address = InetAddress.getByName("192.169.0.1").getAddress();
-        assertTrue("should not be a private address"+address,
-                   !CommonUtils.isPrivateAddress(address));
-
-        address = InetAddress.getByName("239.32.0.1").getAddress();
-        assertTrue("should not be a private address"+address,
-                   !CommonUtils.isPrivateAddress(address));
-
-        address = InetAddress.getByName("1.32.0.1").getAddress();
-        assertTrue("should not be a private address"+address,
-                   !CommonUtils.isPrivateAddress(address));
-
-        address = InetAddress.getByName("180.32.0.1").getAddress();
-        assertTrue("should not be a private address"+address,
-                   !CommonUtils.isPrivateAddress(address));
-    }
-
 
     public void testMajorRevisionMethod() {
         int majorVersion = CommonUtils.getMajorVersionNumber();
@@ -134,14 +55,6 @@ public class CommonUtilsTest extends com.limegroup.gnutella.util.BaseTestCase {
     }
 
 
-	public void testCommonUtilsPortCheck() {
-		int port = -1;
-		assertTrue("port should not be valid", !CommonUtils.isValidPort(port));
-		port = 99999999;
-		assertTrue("port should not be valid", !CommonUtils.isValidPort(port));
-		port = 20;
-		assertTrue("port should be valid", CommonUtils.isValidPort(port));
-	}
     
 
 	/**
