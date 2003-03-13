874f88f74ac2981b8c5b81fe717e4ab33907e4c2
sberlin <sberlin>
2003-03-13 22:53:29 +0000

    removed ability to ignore/default a delete file message.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/settings/QuestionsHandler.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/settings/QuestionsHandler.java
@@ -27,12 +27,6 @@ public class QuestionsHandler extends AbstractSettings {
     */ 
     public static final BooleanSetting MONITOR_VIEW = 
         FACTORY.createBooleanSetting("MONITOR_VIEW", false);
-
-    /**
-     * Setting for whether or not to ask if you want to delete files.
-     */
-    public static final IntSetting SHOULD_DELETE_FILE =
-        FACTORY.createIntSetting("SHOULD_DELETE_FILE", 0);
         
     /**
      * Setting for whether or not to ask about discarding corrupt downloads
