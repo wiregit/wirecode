dc5aac1ed6635610498f3474cb7d9dc715f559d7
afisk <afisk>
2003-04-01 16:59:43 +0000

    removed the use of collections classes, as the import of collections was causing a problem with the installer

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/util/CommonUtils.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/util/CommonUtils.java
@@ -4,7 +4,6 @@ import java.util.Properties;
 import java.io.*;
 import java.net.*;
 import java.util.Locale;
-import com.sun.java.util.collections.*;
 
 /**
  * This class handles common utility functions that many classes
@@ -17,14 +16,6 @@ public final class CommonUtils {
 	 * Constant for the current version of LimeWire.
 	 */
 	private static final String LIMEWIRE_VERSION = "@version@";
-
-	/**
-	 * Constant for an empty, unmodifiable <tt>Set</tt>.  This is necessary
-	 * because Collections.EMPTY_SET is not serializable in the collections 
-	 * 1.1 implementation.
-	 */
-	public static final Set EMPTY_SET = 
-		Collections.unmodifiableSet(new HashSet());
 
     /**
      * The cached value of the major revision number.
