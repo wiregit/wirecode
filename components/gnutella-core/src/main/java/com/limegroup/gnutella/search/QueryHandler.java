59eda8d2b192107b97072d0c5fe0f037610206e7
afisk <afisk>
2003-04-09 01:02:49 +0000

    moved probe queries into their own class, which this now delegates to

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/search/QueryHandler.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/search/QueryHandler.java
@@ -113,14 +113,7 @@ public final class QueryHandler {
 	/**
 	 * Constant for the <tt>QueryRequest</tt> used to build new queries.
 	 */
-	private final QueryRequest QUERY;
-
-    
-    /**
-     * Boolean for whether or not the probe query has been sent to the
-     * desired number of hosts.
-     */
-    private boolean _probeCompleted = false;
+	final QueryRequest QUERY;
 
     /**
      * Boolean for whether or not the query has been forwarded to leaves of 
@@ -131,31 +124,27 @@ public final class QueryHandler {
     /**
      * Constant TTL 1 query for this dynamic query;
      */
-    private final QueryRequest TTL_1_QUERY;
+    final QueryRequest TTL_1_QUERY;
 
     /**
      * Constant TTL 2 query for this dynamic query;
      */
-    private final QueryRequest TTL_2_QUERY;
+    final QueryRequest TTL_2_QUERY;
 
     /**
      * Constant TTL 3 query for this dynamic query;
      */
-    private final QueryRequest TTL_3_QUERY;
+    final QueryRequest TTL_3_QUERY;
 
     /**
      * Constant TTL 4 query for this dynamic query;
      */
-    private final QueryRequest TTL_4_QUERY;
+    final QueryRequest TTL_4_QUERY;
 
     /**
-     * Array of connections to query for the "probe" query.  The probe
-     * determines more accurately how widely distributed the desired
-     * content is.
+     * Constant <tt>ProbeQuery</tt> instance for managing the probe.
      */
-    //private final LinkedList[] PROBE_LISTS;
-
-    private ProbeQuery _probeQuery;
+    private final ProbeQuery PROBE_QUERY;
 
 	/**
 	 * Private constructor to ensure that only this class creates new
@@ -186,12 +175,9 @@ public final class QueryHandler {
         TTL_2_QUERY = createQuery(QUERY, (byte)2);
         TTL_3_QUERY = createQuery(QUERY, (byte)3);
         TTL_4_QUERY = createQuery(QUERY, (byte)4);
-        _probeQuery = 
+        PROBE_QUERY = 
             new ProbeQuery(_connectionManager.getInitializedConnections2(),
-                           TTL_1_QUERY);
-        //PROBE_LISTS = 
-        //  createProbeLists(_connectionManager.getInitializedConnections2(),
-        //                   TTL_1_QUERY);
+                           this);
 	}
 
 
@@ -304,32 +290,13 @@ public final class QueryHandler {
             return;
         }
 
-        if(!_probeQuery.finishedProbe()) {
-            _theoreticalHostsQueried += _probeQuery.sendProbe();
+        if(!PROBE_QUERY.finishedProbe()) {
+            _theoreticalHostsQueried += PROBE_QUERY.sendProbe();
         } else {
             // otherwise, just send a normal query
             _theoreticalHostsQueried += 
                 sendQuery(this, _connectionManager.getInitializedConnections2());             
         }
-        /*
-        if(!_probeCompleted) {
-            _theoreticalHostsQueried += 
-                sendProbeQuery(this, 
-                               _connectionManager.getInitializedConnections2()); 
-            if(_probeCompleted) {
-                _nextQueryTime = System.currentTimeMillis() + 7000;
-            } else {
-                // allow time for connections to become established
-                _nextQueryTime = System.currentTimeMillis() + 2000;
-            }
-            return;
-		}
-
-        
-        _theoreticalHostsQueried += 
-            sendQuery(this, _connectionManager.getInitializedConnections2()); 
-        _nextQueryTime = System.currentTimeMillis() + 2000;
-        */
     }
 
     /**
@@ -345,6 +312,7 @@ public final class QueryHandler {
      *  query iteration
      */
     private static int sendQuery(QueryHandler handler, List list) {
+        System.out.println("QueryHandler::sendQuery"); 
 		int length = list.size();
         int newHosts = 0;
         for(int i=0; i<length; i++) {
@@ -374,7 +342,7 @@ public final class QueryHandler {
 			
 			int resultsNeeded = handler.RESULTS - results;
 			
-			int hostsToQuery = 40000;
+			int hostsToQuery = 20000;
 			if(resultsPerHost != 0) {
 				hostsToQuery = (int)((double)resultsNeeded/resultsPerHost);
 			}
@@ -388,7 +356,7 @@ public final class QueryHandler {
                                 mc.getNumIntraUltrapeerConnections(),
                                 mc.headers().getMaxTTL());
 
-			QueryRequest query = null;//createQuery(handler.QUERY, ttl);
+			QueryRequest query = null;
             if(ttl == 1) query = handler.TTL_1_QUERY;
             if(ttl == 2) query = handler.TTL_2_QUERY;
             if(ttl == 3) query = handler.TTL_3_QUERY;
@@ -405,306 +373,6 @@ public final class QueryHandler {
 	}
     
 
-    private final class ProbeQuery {
-       
-        /**
-         * Constant list of hosts to probe query at ttl=1.
-         */
-        private final LinkedList TTL_1_PROBES;
-
-        /**
-         * Constant list of hosts to probe query at ttl=2.
-         */
-        private final LinkedList TTL_2_PROBES;
-
-        ProbeQuery(List connections, QueryRequest query) {
-            LinkedList[] lists = createProbeLists(connections, query);
-            TTL_1_PROBES = lists[0];
-            TTL_2_PROBES = lists[1];
-            System.out.println("ProbeQuery::ProbeQuery::"+
-                               " ttl1: "+TTL_1_PROBES.size()+
-                               " ttl2: "+TTL_2_PROBES.size()); 
-        }
-
-        /**
-         * Determines whether or not the probe is finished.
-         *
-         * @return <tt>true</tt> if the probe is finished, otherwise
-         *  <tt>false</tt>
-         */
-        boolean finishedProbe() {
-            return (TTL_1_PROBES.isEmpty() &&  TTL_2_PROBES.isEmpty());
-        }
-
-        /**
-         * Sends the next probe query out on the network if there 
-         * are more to send.
-         *
-         * @return the number of hosts theoretically hit by this
-         *  new probe
-         */
-        int sendProbe() {
-            Iterator iter = TTL_1_PROBES.iterator();
-            while(iter.hasNext()) {
-                ManagedConnection mc = (ManagedConnection)iter.next();
-                return sendQueryToHost(TTL_1_QUERY, mc, QueryHandler.this);
-            }
-            
-            iter = TTL_2_PROBES.iterator();
-            while(iter.hasNext()) {
-                ManagedConnection mc = (ManagedConnection)iter.next();
-                return sendQueryToHost(TTL_2_QUERY, mc, QueryHandler.this);
-            }
-
-            // this should never happen, as this method should not
-            // be called when both lists are empty
-            return 0;
-        }
-    }
-
-    /**
-     * Helper method that creates the list of nodes to query for the probe.
-     * This list will vary in size depending on how popular the content appears
-     * to be.
-     */
-    private static LinkedList[] createProbeLists(List connections, QueryRequest query) {
-        Iterator iter = connections.iterator();
-        
-        LinkedList missConnections = new LinkedList();
-        LinkedList oldConnections  = new LinkedList();
-        LinkedList hitConnections  = new LinkedList();
-        while(iter.hasNext()) {
-            ManagedConnection mc = (ManagedConnection)iter.next();
-            
-            if(mc.isGoodUltrapeer()) {
-                //ManagedConnectionQueryInfo qi = mc.getQueryRouteState();
-                
-                //QueryRouteTable qrt = mc.getQueryKey
-                //if(qi.lastReceived == null) continue;
-                if(mc.hitsQueryRouteTable(query)) { 
-                    hitConnections.add(mc);
-                } else {
-                    missConnections.add(mc);
-                }
-            } else {
-                oldConnections.add(mc);
-            }
-        }
-
-        System.out.println(query.getQuery()+
-                           " hitConnections:  "+hitConnections.size()+
-                           " missConnections: "+missConnections.size()+
-                           " oldConnections:  "+oldConnections.size()); 
-        // final list of connections to query
-        LinkedList[] returnLists = new LinkedList[2];
-        LinkedList ttl1List = new LinkedList();
-        LinkedList ttl2List = new LinkedList();
-        returnLists[0] = ttl1List;
-        returnLists[1] = ttl2List;        
-
-        // do we have adequate data to determine some measure of the file's popularity?
-        boolean adequateData = 
-            (missConnections.size()+hitConnections.size()) > 8;
-
-        // if we don't have enough data from QRP tables, just send out a traditional probe
-        // also, if we don't have an adequate number of QRP tables to access the 
-        // popularity of the file, just send out an old-style probe at TTL=2
-        if(hitConnections.size() == 0 || !adequateData) {
-            return createAggressiveProbe(oldConnections, missConnections, 
-                                         hitConnections, returnLists);
-        } 
-
-        int numHitConnections = hitConnections.size();
-        double popularity = 
-            (double)((double)numHitConnections/
-                     ((double)missConnections.size()+numHitConnections));
-
-        // if there were no matches, then it's almost definitely a fairly
-        // rare file, so send out a more aggressive probe
-        if(popularity == 0.0) {
-            return createAggressiveProbe(oldConnections, missConnections, 
-                                         hitConnections, returnLists);
-            
-        }
-        
-        // if the file appears to be very popular, send it to only one host
-        if(popularity == 1.0) {
-            ttl1List.add(hitConnections.getFirst());
-            return returnLists;
-        }
-
-        // mitigate the extremes of the popularity measurement a bit
-        popularity = popularity * 0.75;
-        
-        // the number of TTL=1 nodes we would hit if we had that many
-        // connections with hits
-        int idealTTL1ConnectionsToHit =
-            Math.max(2, (int)(30.0 * (double)(1.0-popularity)));
-
-        int realTTL1ConnectionsToHit =
-            Math.min(numHitConnections, idealTTL1ConnectionsToHit);
-
-        ttl1List.addAll(hitConnections.subList(0, realTTL1ConnectionsToHit));        
-
-        // the "left over" number of nodes we need to hit after all
-        // of our hit connections are used up.
-        int extraNodesNeeded =
-            idealTTL1ConnectionsToHit - realTTL1ConnectionsToHit;
-        
-        // add more TTL=2 nodes to the probe if we need them
-        if(extraNodesNeeded > 0) {
-            if(extraNodesNeeded < 10) {
-                addToList(ttl2List, oldConnections, missConnections, 1);
-            } else {
-                addToList(ttl2List, oldConnections, missConnections, 2);
-            }
-        }
-
-        //System.out.println("popularity: "+popularity); 
-        //System.out.println("idealTTL1ConnectionsToHit: "+idealTTL1ConnectionsToHit); 
-        //System.out.println("extra nodes needed: "+extraNodesNeeded); 
-        //System.out.println("ttl1ConnectionsToUse: "+ttl1ConnectionsToUse); 
-        
-        return returnLists;        
-    }
-
-
-    /**
-     * Helper method that adds as many elements as possible up to the
-     * desired number from two lists into a third list.  This method
-     * takes as many elements as possible from <tt>list1</tt>, only
-     * using elements from <tt>list2</tt> if the desired number of
-     * elements to add cannot be fulfilled from <tt>list1</tt> alone.
-     *
-     * @param listToAddTo the list that elements should be added to
-     * @param list1 the first list to add elements from, with priority 
-     *  given to this list
-     * @param list2 the second list to add elements from -- only used
-     *  in the case where <tt>list1</tt> is smaller than <tt>numElements</tt>
-     * @param numElements the desired number of elements to add to 
-     *  <tt>listToAddTo</tt> -- note that this number will not be reached
-     *  if the list1.size()+list2.size() < numElements
-     */
-    private static void addToList(List listToAddTo, List list1, List list2, 
-                                  int numElements) {
-        if(list1.size() >= numElements) {
-            listToAddTo.addAll(list1.subList(0, numElements));
-            return;
-        } else {
-            listToAddTo.addAll(list1);
-        }
-
-        numElements = numElements - list1.size();
-
-        if(list2.size() >= numElements) {
-            listToAddTo.addAll(list2.subList(0, numElements));
-        } else {
-            listToAddTo.addAll(list2);
-        }
-    }
-       
-
-    /**
-     * Helper method that creates lists of TTL=1 and TTL=2 connections to query
-     * for an aggressive probe.  This is desired, for example, when the desired
-     * file appears to be rare or when there is not enough data to determine
-     * the file's popularity.
-     *
-     * @param oldConnections the <tt>List</tt> of old-style connections
-     * @param missConnections the <tt>List</tt> of new connections that did
-     *  not have a hit for this query
-     * @param hitConnections the <tt>List</tt> of connections with hits
-     * @param returnLists the array of TTL=1 and TTL=2 connections to query
-     */
-    private static LinkedList[] 
-        createAggressiveProbe(List oldConnections, List missConnections,
-                              List hitConnections, LinkedList[] returnLists) {
-        
-        // add as many connections as possible from first the old connections
-        // list, then the connections that did not have hits
-        addToList(returnLists[1], oldConnections, missConnections, 3);
-
-        // add any hits there are to the TTL=1 list
-        int maxIndex = Math.min(4, hitConnections.size());
-        returnLists[0].addAll(hitConnections.subList(0, maxIndex));
-
-        return returnLists;        
-    }
-
-	/**
-	 * Send the initial "probe" query to get an idea of how widely distributed
-     * the content is.
-     *
-     * @param queriedHosts the set of hosts that have already been queried
-     * @return the next time to send out the query -- another query should
-     *  not be sent before this
-	 */
-    /*
-	private static int sendProbeQuery(QueryHandler handler, List list) {
-        // send a TTL=1 query to hosts that have matches in their
-        // QRP tables, running one at a time with pauses.
-        
-        QueryRequest query = createQuery(handler.QUERY, (byte)1);
-        //RouterService.getMessageRouter().forwardQueryRequestToLeaves(query, handler.REPLY_HANDLER);
-
-
-        //List[] probeLists = createProbeLists(list, query);
-        //List probeList = probeLists[0];
-        Iterator iter = list.iterator();
-        int connectionsUsed = 0;
-        
-        // Holder for the number of Ultrapeer connections we have
-        // that use query routing.  This is used to decide how
-        // good our sample "probe" has been
-        int qrConnections = 0;
-        int qrConnectionsUsed = 0;
-        while(iter.hasNext()) {
-            ManagedConnection mc = (ManagedConnection)iter.next();
-
-            // this could be more fine-grained, since the connection
-            // only really needs to support intra-Ultrapeer QRP and
-            // to forward TTL=1 probe queries, but this is fine
-            if(!mc.isGoodUltrapeer()) continue;
-
-            ManagedConnectionQueryInfo qi = mc.getQueryRouteState();
-            if (qi.lastReceived == null) continue;
-
-            // don't consider this as a host used in our sample
-            // until we know it has a QRP table
-            qrConnections++;
-            if(qi.lastReceived.contains(query)) {
-                sendQueryToHost(query, mc, handler);
-                qrConnectionsUsed++;
-            }
-        }
-		
-        query = createQuery(handler.QUERY, PROBE_TTL);
-        int newHosts = 0;
-
-        int hostsQueried = 0;
-        int i = 0;
-        while(hostsQueried<3 && i<list.size()) {
-			ManagedConnection mc = (ManagedConnection)list.get(i);
-
-            // if we've already queried this host, go to the next one,
-            // or if it's not yet stable
-            if(!mc.isStable() || handler.QUERIED_HANDLERS.contains(mc)) {
-                // count the index
-                i++;
-                continue;
-            }
-
-            newHosts += sendQueryToHost(query, mc, handler);
-            hostsQueried++;
-            i++;
-		}
-        if(hostsQueried >= 3) {
-            handler._probeCompleted = true;
-        }
-        return newHosts;
-	}
-    */
-
     /**
      * Sends a query to the specified host.
      *
@@ -713,11 +381,14 @@ public final class QueryHandler {
      * @param handler the <tt>QueryHandler</tt> 
      * @return the number of new hosts theoretically hit by this query
      */
-    private static int sendQueryToHost(QueryRequest query, 
-                                       ManagedConnection mc, 
-                                       QueryHandler handler) {
-        RouterService.getMessageRouter().sendQueryRequest(query, mc, 
-                                                          handler.REPLY_HANDLER);
+    static int sendQueryToHost(QueryRequest query, 
+                               ManagedConnection mc, 
+                               QueryHandler handler) {
+        System.out.println("QueryHandler::sendQueryToHost:: "+
+                           query.getQuery()+" TTL="+query.getTTL()); 
+        
+        // send the query directly along the connection
+        mc.originateQuery(query);
         
         byte ttl = query.getTTL();
 
@@ -804,8 +475,8 @@ public final class QueryHandler {
         // it enough results
         // precisely what this number should be is somewhat hard to determine
         // because, while connection have a specfic degree, the degree of 
-        // the connections they have cannot be determined
-		if(_theoreticalHostsQueried > 100000) return true;
+        // the connections on subsequent hops cannot be determined
+		if(_theoreticalHostsQueried > 80000) return true;
 
 		// return true if we've been querying for longer than the specified 
 		// maximum
