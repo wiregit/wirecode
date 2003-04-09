f48492b822333fcbf3588a477ebdd0d1b5ac0f86
afisk <afisk>
2003-04-09 21:42:59 +0000

    changed to cache the common NoGnutellaOKExceptions, as they are generated quite frequently

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/handshaking/NoGnutellaOkException.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/handshaking/NoGnutellaOkException.java
@@ -6,10 +6,94 @@ import java.io.IOException;
  * Exception thrown when someone understands responds with a handshaking
  * code other than 200 or 401.
  */
-public class NoGnutellaOkException extends IOException
-{
-    private boolean wasMe;
-    private int code;
+public class NoGnutellaOkException extends IOException {
+
+    /**
+     * Constant for whether or not the <tt>NoGnutellaOkException</tt>
+     * came from us.
+     */
+    private final boolean wasMe;
+
+    /**
+     * Constant for the status code of the handshake header that 
+     * caused the exception.
+     */
+    private final int code;
+
+    /**
+     * Constant for the default message for exceptions due to fatal
+     * server responses.
+     */
+    private static final String FATAL_SERVER_MSG =
+        "Server sent fatal response: ";        
+
+    /**
+     * Constant for the default message for exceptions due to fatal
+     * responses from us when we reject connections.
+     */
+    private static final String FATAL_CLIENT_MSG =
+        "We sent fatal response: ";        
+
+    /**
+     * Cached <tt>NoGnutellaOkException</tt> for the case where
+     * the server rejected the connection with a 503.
+     */
+    public static final NoGnutellaOkException SERVER_REJECT =
+        new NoGnutellaOkException(false, HandshakeResponse.SLOTS_FULL, 
+                                  FATAL_SERVER_MSG+
+                                  HandshakeResponse.SLOTS_FULL);
+
+    /**
+     * Cached <tt>NoGnutellaOkException</tt> for the case where
+     * we as the client are rejecting the connection with a 503.
+     */
+    public static final NoGnutellaOkException CLIENT_REJECT =
+        new NoGnutellaOkException(false, HandshakeResponse.SLOTS_FULL, 
+                                  FATAL_CLIENT_MSG+
+                                  HandshakeResponse.SLOTS_FULL);
+
+    /**
+     * Cached <tt>NoGnutellaOkException</tt> for the case where
+     * the handshake never resolved successfully on the cleint
+     * side.
+     */
+    public static final NoGnutellaOkException UNRESOLVED_CLIENT =
+        new NoGnutellaOkException(true,
+                                  HandshakeResponse.UNAUTHORIZED_CODE,
+                                  "Too much handshaking, no conclusion");
+
+    /**
+     * Cached <tt>NoGnutellaOkException</tt> for the case where
+     * the handshake never resolved successfully on the server
+     * side.
+     */
+    public static final NoGnutellaOkException UNRESOLVED_SERVER =
+        new NoGnutellaOkException(false,
+                                  HandshakeResponse.UNAUTHORIZED_CODE,
+                                  "Too much handshaking, no conclusion");
+
+
+    /**
+     * Creates a new <tt>NoGnutellaOkException</tt> from an unknown
+     * client response.
+     *
+     * @param code the response code from the server
+     */
+    public static NoGnutellaOkException createClientUnknown(int code) {
+        return new NoGnutellaOkException(true, code,
+                                         FATAL_SERVER_MSG+code);
+    }
+
+    /**
+     * Creates a new <tt>NoGnutellaOkException</tt> from an unknown
+     * server response.
+     *
+     * @param code the response code from the server
+     */
+    public static NoGnutellaOkException createServerUnknown(int code) {
+        return new NoGnutellaOkException(false, code,
+                                         FATAL_SERVER_MSG+code);
+    }
 
     /**
      * @param wasMe true if I returned the non-standard code.
@@ -18,10 +102,9 @@ public class NoGnutellaOkException extends IOException
      * @param message a human-readable message for debugging purposes
      *  NOT necessarily the message given during the interaction.
      */
-    public NoGnutellaOkException(boolean wasMe, 
-                                 int code,
-                                 String message)
-    {
+    private NoGnutellaOkException(boolean wasMe, 
+                                  int code,
+                                  String message) {
         super(message);
         this.wasMe=wasMe;
         this.code=code;
