2854f30383594bb1f350a04a3d5250885ea573bb
ynaito <ynaito>
2003-04-17 20:11:05 +0000

    options for autocomplete

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/settings/UISettings.java
@@ -0,0 +1,16 @@
+package com.limegroup.gnutella.settings;
+
+/**
+ * Settings to deal with UI.
+ */ 
+public final class UISettings extends LimeProps {
+
+    private UISettings() {}
+
+    /**
+     * Setting for autocompletion
+     */
+    public static final BooleanSetting AUTOCOMPLETE_ENABLED =
+		FACTORY.createBooleanSetting("AUTOCOMPLETE_ENABLED", false);
+
+}
