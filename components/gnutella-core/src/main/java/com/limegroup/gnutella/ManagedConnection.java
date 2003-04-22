39dfe91dfb3cac716ea64875e1c0050a43a98a5f
afisk <afisk>
2003-04-22 22:31:27 +0000

    removed print lines

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/ManagedConnection.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/ManagedConnection.java
@@ -673,11 +673,6 @@ public class ManagedConnection extends Connection
                     Message m=null;
                     synchronized (_outputQueueLock) {
                         m=(Message)queue.removeNext();
-                        if(m != null &&
-                           i == PRIORITY_OUR_QUERY && 
-                           m.getHops() == 0) {
-                            System.out.println("ManagedConnection::sendQueued: "+m); 
-                        }
                         int dropped=queue.resetDropped();
                         addDropped(dropped);
                         _queued-=(m==null?0:1)+dropped;  //maintain invariant
