7581402615639029f8bb8050966cae88a1bcb182
afisk <afisk>
2003-04-25 22:01:08 +0000

    new class that periodically pings the network for new pong data for ultrapeers

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/Pinger.java
@@ -0,0 +1,66 @@
+package com.limegroup.gnutella;
+
+import com.limegroup.gnutella.messages.*;
+
+/**
+ * This class continually sends broadcast pings on behalf of an Ultrapeer
+ * to update the host caches of both itself and its leaves.  This class 
+ * reduces overall ping and pong traffic because it allows us not to forward
+ * pings received from other hosts.  Instead, we use pong caching to respond
+ * to those pings with cached pongs, and send pings periodically in this 
+ * class to obtain fresh host data.
+ */
+public final class Pinger implements Runnable {
+
+    /**
+     * Single <tt>Pinger</tt> instance, following the singleton pattern.
+     */
+    private static final Pinger INSTANCE = new Pinger();
+
+    /**
+     * Constant ping instance sent to all connections.
+     */
+    private static final PingRequest PING =
+        new PingRequest((byte)5);
+
+    /**
+     * Returns the single <tt>Pinger</tt> instance.
+     */
+    public static Pinger instance() {
+        return INSTANCE;
+    }
+
+    /**
+     * Private constructor to avoid this class being constructed multiple
+     * times, following the singleton pattern.
+     */
+    private Pinger() {}
+
+    /**
+     * Starts the thread that continually sends broadcast pings on behalf of
+     * this node if it's an Ultrapeer.
+     */
+    public void start() {
+        Thread pingThread = new Thread(this, "pinger thread");
+        pingThread.setDaemon(true);
+        pingThread.start();
+    }
+
+
+    /**
+     * Implements the <tt>Runnable</tt> interface.  Periodically sends broadcast
+     * pings along all connections.
+     */
+    public void run() {
+        while(true) {
+            if(RouterService.isSupernode()) {
+                RouterService.getMessageRouter().broadcastPingRequest(PING);
+            }
+            try {
+                Thread.sleep(3000);
+            } catch(InterruptedException e) {
+                // this should not happen
+            }
+        }
+    }
+}
