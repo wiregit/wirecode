cdedc704494eebe271336c52c309b9738a5187d2
afisk <afisk>
2003-05-01 21:53:07 +0000

    more tests

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/PongCacherTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/PongCacherTest.java
@@ -13,6 +13,8 @@ import com.sun.java.util.collections.*;
  */
 public final class PongCacherTest extends BaseTestCase {
 
+    private static final PongCacher PC = PongCacher.instance();
+
     public PongCacherTest(String name) {
         super(name);        
     }
@@ -25,6 +27,10 @@ public final class PongCacherTest extends BaseTestCase {
         junit.textui.TestRunner.run(suite());
     }
 
+    public static void globalSetUp() throws Exception {
+        PC.start();
+    }
+
     /**
      * Tests the method for adding a pong to the cacher.
      */
@@ -32,26 +38,26 @@ public final class PongCacherTest extends BaseTestCase {
         ConnectionManager cm = 
             new UltrapeerConnectionManager(new ServerAuthenticator());
         PrivilegedAccessor.setValue(RouterService.class, "manager", cm);    
-        PongCacher pc = PongCacher.instance();
-        
-        PingReply pong = PingReply.create(new GUID().bytes(), (byte)5);
-        pc.addPong(pong);
 
-        BucketQueue bq = (BucketQueue)PrivilegedAccessor.getValue(PongCacher.class, "_pongs");
+        PingReply pong = PingReply.create(new GUID().bytes(), (byte)5);
+        PC.addPong(pong);
+
+        BucketQueue bq = 
+            (BucketQueue)PrivilegedAccessor.getValue(PongCacher.class, 
+                                                     "_pongs");
         assertEquals("unexpected bucket queue size", 1, bq.size());
 
         pong = PingReply.create(new GUID().bytes(), (byte)5);
-        pc.addPong(pong);
+        PC.addPong(pong);
         assertEquals("unexpected bucket queue size", 2, bq.size());
         assertEquals("unexpected bucket queue size", 2, bq.size(0));
 
-        int pongsPerHop = 
-            ((Integer)PrivilegedAccessor.getValue(PongCacher.class, "PONGS_PER_HOP")).intValue();
-        for(int i=bq.size(0); i<pongsPerHop+2; i++) {
-            pc.addPong(pong);
+        for(int i=bq.size(0); i<PongCacher.PONGS_PER_HOP+2; i++) {
+            PC.addPong(pong);
         }
         
-        assertEquals("unexpected bucket queue size", pongsPerHop, bq.size(0));
+        assertEquals("unexpected bucket queue size", 
+                     PongCacher.PONGS_PER_HOP, bq.size(0));
     }
 
     /**
@@ -61,24 +67,83 @@ public final class PongCacherTest extends BaseTestCase {
         ConnectionManager cm = 
             new UltrapeerConnectionManager(new ServerAuthenticator());
         PrivilegedAccessor.setValue(RouterService.class, "manager", cm);    
-        PongCacher pc = PongCacher.instance();        
-        pc.start();
+        Thread.sleep(2000);
         
-        Set pongs = pc.getBestPongs();
+        Set pongs = PC.getBestPongs();
         int startSize = pongs.size();
 
         PingReply pong = PingReply.create(new GUID().bytes(), (byte)5);
-        pc.addPong(pong);        
+        PC.addPong(pong);        
 
         Thread.sleep(2000);
-        pongs = pc.getBestPongs();
-        assertEquals("unexpected number of cached pongs", startSize+1, pongs.size());        
+        pongs = PC.getBestPongs();
+        assertEquals("unexpected number of cached pongs", 
+                     startSize+1, pongs.size());        
 
         pong = PingReply.create(new GUID().bytes(), (byte)5);
-        pc.addPong(pong);        
+        PC.addPong(pong);        
 
         Thread.sleep(2000);
-        pongs = pc.getBestPongs();
-        assertEquals("unexpected number of cached pongs", startSize+2, pongs.size());  
+        pongs = PC.getBestPongs();
+        assertEquals("unexpected number of cached pongs", 
+                     startSize+2, pongs.size());  
+
+        // fill up the pongs at the default hop
+        for(int i=0; i<30; i++) {
+            PingReply curPong = 
+                PingReply.create(new GUID().bytes(), (byte)5);
+            PC.addPong(curPong);
+        }
+
+        Thread.sleep(2000);
+        pongs = PC.getBestPongs();
+
+        assertEquals("unexpected number of cached pongs", 
+                     PongCacher.NUM_CACHED_PONGS, pongs.size());
+
+        PingReply highHopPong = 
+            PingReply.create(new GUID().bytes(), (byte)5);
+        
+        highHopPong.hop();
+        highHopPong.hop();
+        PC.addPong(highHopPong);
+
+        Thread.sleep(2000);
+        pongs = PC.getBestPongs();
+        assertEquals("unexpected number of cached pongs", 
+                     PongCacher.NUM_CACHED_PONGS, pongs.size());
+
+        BucketQueue queue = 
+            (BucketQueue)PrivilegedAccessor.getValue(PC, "_pongs");
+
+        assertEquals("first pong should be high hops", highHopPong, 
+                     queue.extractMax());        
+
+        Iterator iter = pongs.iterator();
+        boolean equal = false;
+        PingReply pr = null;
+        int z = 0;
+        while(iter.hasNext()) {
+            pr = (PingReply)iter.next();
+            if(pr.equals(highHopPong)) {
+                System.out.println("high hops found at index: "+z); 
+                equal = true;
+            }
+            z++;
+        }
+        //assertTrue("should have had high hops pong",equal);
+        //assertEquals("first pong should be high hops", highHopPong, 
+        //           pongs.extractMax());
+        //assertEquals("first pong should be high hops");
     }
 }
+
+
+
+
+
+
+
+
+
+
