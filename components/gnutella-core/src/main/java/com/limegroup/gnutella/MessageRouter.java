71b4a12389f27d27041bb85556c7775ec22fcfff
afisk <afisk>
2003-05-07 16:21:33 +0000

    added call to update the next time a ping will be accepted on a connection

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/MessageRouter.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/MessageRouter.java
@@ -567,6 +567,7 @@ public abstract class MessageRouter {
         // allow new pings on this connection, allow it.
         if((ping.getHops() == 1) || handler.allowNewPings()) {
             respondToPingRequest(ping);
+            handler.updatePingTime();
         } 
     }
 
