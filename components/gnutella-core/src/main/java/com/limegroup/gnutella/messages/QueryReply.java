6eac97412d8265e6dde530773714b3fcdb43a2e3
sdaswani <sdaswani>
2003-05-08 22:44:18 +0000

    fixed some error revealed during testing.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/messages/QueryReply.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/messages/QueryReply.java
@@ -174,13 +174,50 @@ public class QueryReply extends Message implements Serializable{
             boolean isMulticastReply) 
         throws IllegalArgumentException {
         this(guid, ttl, port, ip, speed, responses, clientGUID, 
+             xmlBytes, needsPush, isBusy,  finishedUpload, measuredSpeed, 
+             supportsChat, isMulticastReply, new PushProxyInterface[0]);
+    }
+
+    /** 
+     * Creates a new QueryReply with a BearShare 2.2.0-style QHD.  The QHD with
+     * the LIME vendor code and the given busy and push flags.  Note that this
+     * constructor has no support for undefined push or busy bits.
+     * The Browse Host GGEP extension is ON by default.  
+     *
+     * @param needsPush true iff this is firewalled and the downloader should
+     *  attempt a push without trying a normal download.
+     * @param isBusy true iff this server is busy, i.e., has no more upload slots.  
+     * @param finishedUpload true iff this server has successfully finished an 
+     *  upload
+     * @param measuredSpeed true iff speed is measured, not as reported by the
+     *  user
+     * @param xmlBytes The (non-null) byte[] containing aggregated
+     * and indexed information regarding file metadata.  In terms of byte-size, 
+     * this should not be bigger than 65535 bytes.  Anything larger will result
+     * in an Exception being throw.  This String is assumed to consist of
+     * compressed data.
+     * @param supportsChat true iff the host currently allows chatting.
+     * @param proxies an array of PushProxy interfaces.  will be included in 
+     * the replies GGEP extension.
+     * @exception IllegalArgumentException Thrown if 
+     * xmlBytes.length > XML_MAX_SIZE
+     */
+    public QueryReply(byte[] guid, byte ttl, 
+            int port, byte[] ip, long speed, Response[] responses,
+            byte[] clientGUID, byte[] xmlBytes,
+            boolean needsPush, boolean isBusy,
+            boolean finishedUpload, boolean measuredSpeed,boolean supportsChat,
+            boolean isMulticastReply, PushProxyInterface[] proxies) 
+        throws IllegalArgumentException {
+        this(guid, ttl, port, ip, speed, responses, clientGUID, 
              xmlBytes, true, needsPush, isBusy, 
              finishedUpload, measuredSpeed,supportsChat, true, isMulticastReply,
-             new PushProxyInterface[0]);
+             proxies);
         if (xmlBytes.length > XML_MAX_SIZE)
             throw new IllegalArgumentException();
         _xmlBytes = xmlBytes;        
     }
+
 
     /** Creates a new query reply with data read from the network. */
     public QueryReply(byte[] guid, byte ttl, byte hops,byte[] payload) 
@@ -1165,7 +1202,7 @@ public class QueryReply extends Message implements Serializable{
                 }
 
             }
-            if (supportsBH && isMulticastResponse)
+            else if (supportsBH && isMulticastResponse)
                 retGGEPBlock = _comboGGEP;
             else if (supportsBH)
                 retGGEPBlock = _bhGGEP;
@@ -1253,7 +1290,7 @@ public class QueryReply extends Message implements Serializable{
 
     /** A simple utility class for doling out PushProxy information.
      */
-    private static class PushProxyContainer implements PushProxyInterface {
+    static class PushProxyContainer implements PushProxyInterface {
         IPPortCombo _combo;
 
         public PushProxyContainer(String hostAddress, int port) 
@@ -1272,6 +1309,15 @@ public class QueryReply extends Message implements Serializable{
         public InetAddress getPushProxyAddress() {
             return _combo.getAddress();
         }
+
+        public boolean equals(Object other) {
+            if (other instanceof PushProxyContainer) {
+                PushProxyContainer iface = (PushProxyContainer) other;
+                return _combo.equals(iface._combo);
+            }
+            return false;
+        }
+        
     }
 
     /** Another utility class the encapsulates some complexity.
@@ -1294,7 +1340,7 @@ public class QueryReply extends Message implements Serializable{
                 throw new IllegalArgumentException("Weird Input");
             
             String host = NetworkUtils.ip2string(fromNetwork, 0);
-            int port = (int) ByteOrder.leb2short(fromNetwork, 5);
+            int port = (int) ByteOrder.leb2short(fromNetwork, 4);
 
             try {
                 return new IPPortCombo(host, port);
@@ -1326,14 +1372,22 @@ public class QueryReply extends Message implements Serializable{
          *  //TODO if IPv6 kicks in, this may fail, don't worry so much now.
          */
         public byte[] toBytes() {
-            byte[] retVal = new byte[4];
+            byte[] retVal = new byte[6];
             
             for (int i=0; i < 4; i++)
                 retVal[i] = _addr.getAddress()[i];
 
-            ByteOrder.short2leb((short)_port, retVal, 5);
+            ByteOrder.short2leb((short)_port, retVal, 4);
 
             return retVal;
+        }
+
+        public boolean equals(Object other) {
+            if (other instanceof IPPortCombo) {
+                IPPortCombo combo = (IPPortCombo) other;
+                return _addr.equals(combo._addr) && (_port == combo._port);
+            }
+            return false;
         }
 
     }
