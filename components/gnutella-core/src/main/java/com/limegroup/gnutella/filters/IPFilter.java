93e80e527cea74685b768cffec7f2d79f5858d82
sberlin <sberlin>
2003-05-08 15:53:40 +0000

    Fixed problem of not being able to connect to DNS names.  Now IPFilter.allow will perform a DNS lookup if it is unable to construct an IP out of the given host.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/filters/IPFilter.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/filters/IPFilter.java
@@ -5,6 +5,9 @@ import com.limegroup.gnutella.*;
 import com.limegroup.gnutella.messages.*;
 import com.limegroup.gnutella.util.NetworkUtils;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * Blocks messages and hosts based on IP address.  Formerly know as
  * BlackListFilter.  Immutable.  
@@ -44,23 +47,36 @@ public final class IPFilter extends SpamFilter {
 
     /** 
      * Checks if a given host is banned.  This method will be
-     * called when accepting an incomming or outgoing connection. 
-     * @return false iff host is a banned address, true otherwise.
+     * called when accepting an incomming or outgoing connection.
+     * @param host preferably an IP in the form of A.B.C.D, but if
+     *  it is a DNS name then a lookup will be performed.
+     * @return true if this host is allowed, false if it is banned
+     *  or we are unable to create correct IP addr out of it.
      */
     public boolean allow(String host) {
         IP ip;
         try {
             ip = new IP(host);
-        } catch (IllegalArgumentException e) {
-            return false;
+        } catch (IllegalArgumentException badHost) {
+            try {
+                InetAddress lookUp = InetAddress.getByName(host);
+                host = lookUp.getHostAddress();
+                ip = new IP(host);
+            } catch(UnknownHostException unknownHost) {
+                // could not look up this host.
+                return false;
+            } catch(IllegalArgumentException stillBadHost) {
+                // couldn't construct IP still.
+                return false;
+            }
         }        
         return goodHosts.contains(ip) || !badHosts.contains(ip);
     }
 
     /** 
-     * Returns false if m is a ping or query reply message with a banned
-     * address.
-     * @return false iff host is a banned address, true otherwise.     
+     * Checks if a given Message's host is banned.
+     * @return true if this Message's host is allowed, false if it is banned
+     *  or we are unable to create correct IP addr out of it.
      */
     public boolean allow(Message m){
         String ip;
