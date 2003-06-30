b35c2c3a2cf8d4afc7b21f773a9a1083d27b7de8
afisk <afisk>
2003-06-30 14:50:24 +0000

    new class for writing messages to the network using nio

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/connection/MessageWriter.java
@@ -0,0 +1,74 @@
+package com.limegroup.gnutella.connection;
+
+import com.limegroup.gnutella.*;
+import com.limegroup.gnutella.messages.*;
+import com.limegroup.gnutella.statistics.CompressionStat;
+import com.limegroup.gnutella.util.CommonUtils;
+
+import java.nio.*;
+import java.nio.channels.*;
+import java.io.*;
+
+/**
+ * Writes Gnutella messages to SocketChannel.  Used by Connnection to send
+ * messages in a non-blocking manner (which requires state).  Queues at most
+ * one message at a time.  Thread-safe; in a true single-thread core, this
+ * would not be necessary.
+ */
+public final class MessageWriter {
+
+	private final SocketChannel CHANNEL;
+
+    /**
+	 * Variable for the current message being written.
+	 */
+	private ByteBuffer _message;
+	
+	/**
+	 * Ensure that this class cannot be constructed.
+	 */
+	public MessageWriter(SocketChannel channel) {
+		CHANNEL = channel;
+	}
+	
+	/**
+	 * Returns true if this has any queued data, i.e., if we're in the process
+	 * of sending a message.  
+	 */
+	public synchronized boolean hasQueued() {
+		return _message != null;
+	}
+	
+    public synchronized boolean write(Message msg) throws IOException {
+        //System.out.println("MessageWriter::write");
+        //Copy m to a ByteBuffer.  TODO: avoid allocating ByteBuffer each time.
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            msg.write(baos);
+        } catch (IOException e) {
+            Assert.that(false, "IO problem from ByteArrayOutputStream");
+            return false;
+        }
+		int n = msg.getTotalLength();
+        _message = ByteBuffer.allocate(n);
+        _message.put(baos.toByteArray());
+        _message.flip();        //prepare for writing        
+        
+        return write();
+    }
+    
+    
+    public synchronized boolean write() throws IOException {
+    	if(!hasQueued()) {
+    		return false;
+    	}
+    	CHANNEL.write(_message);
+    	if(!_message.hasRemaining()) {
+    		_message = null;
+    		return false;
+    	} else {
+    		return true;
+    	}
+    }
+    
+}
