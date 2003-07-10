fcdfed3195e7fd8ad4464e455a904b841eb99a55
afisk <afisk>
2003-07-10 17:48:27 +0000

    changed constructors so that subclasses can require that queries have matches in QRP tables and changed send method to verify tables if it should

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/util/TestConnection.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/util/TestConnection.java
@@ -3,7 +3,6 @@ package com.limegroup.gnutella.util;
 import com.limegroup.gnutella.messages.*;
 import com.limegroup.gnutella.routing.*;
 import com.limegroup.gnutella.*;
-import java.io.*;
 
 /**
  * Helper class that overrides getNumIntraUltrapeerConnections for
@@ -12,6 +11,8 @@ import java.io.*;
  */
 public abstract class TestConnection extends ManagedConnection {
     
+    private boolean _queriesMustBeInRoutingTables;
+
     private final int CONNECTIONS;
     
     private int _numQueries = 0;
@@ -20,16 +21,27 @@ public abstract class TestConnection extends ManagedConnection {
     
     private boolean _receivedQuery;
     
-    /**
-     * Constant for the query route table for this connection.
+     /**
+     * Constant for the <tt>QueryRouteTable</tt> for this
+     * connection -- can be used by subclasses.
      */
-    private final QueryRouteTable QRT = new QueryRouteTable();
+    protected QueryRouteTable QRT;
     
     TestConnection(int connections) {
         super("60.76.5.3", 4444);
         CONNECTIONS = connections;
     }
     
+    /**
+     * @param connections2
+     * @param b
+     */
+    public TestConnection(int connections, boolean b) {
+        this(connections);
+        _queriesMustBeInRoutingTables = b;
+        // TODO Auto-generated constructor stub
+    }
+
     public int getNumIntraUltrapeerConnections() {
         return CONNECTIONS;
     }
@@ -59,7 +71,7 @@ public abstract class TestConnection extends ManagedConnection {
     /**
      * Overridden to keep track of messages sent.
      */
-    public void send(Message msg) {            
+    public void send(Message msg) {         
         if(msg instanceof ResetTableMessage) {
             QRT.reset((ResetTableMessage)msg);
         } else if(msg instanceof PatchTableMessage) {
@@ -75,6 +87,9 @@ public abstract class TestConnection extends ManagedConnection {
         _receivedQuery = true;
         _numQueries++;
         QueryRequest qr = (QueryRequest)msg;
+        if(_queriesMustBeInRoutingTables && !hitsQueryRouteTable(qr))  {
+            throw new IllegalArgumentException("received query that's not in table1: "+qr+" "+this);
+        }
         int ttl = qr.getTTL();
         _totalTTL += ttl;
     }
