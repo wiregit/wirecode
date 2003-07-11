80e4dd4c1445518a80e0ef805a227546d7d8f062
sberlin <sberlin>
2003-07-11 22:41:46 +0000

    fixed signature and use SHA1 isntead of mini xml hash.

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/xml/MFMStub.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/xml/MFMStub.java
@@ -1,15 +1,18 @@
 package com.limegroup.gnutella.xml;
 
+import com.limegroup.gnutella.URN;
+import java.io.File;
+
 /**
  * A simple MetaFileManager.
  * Currently, overrides no methods.
  */
 public class MFMStub extends MetaFileManager {
 
-    public String readFromMap(Object file) {
-        String hash = null;
+    public URN readFromMap(Object file) {
+        URN hash = null;
         try {
-            hash = new String(LimeXMLUtils.hashFile((java.io.File)file));
+            hash = URN.createSHA1Urn((File)file);
         }
         catch (Exception ignored) {}
         return hash;
