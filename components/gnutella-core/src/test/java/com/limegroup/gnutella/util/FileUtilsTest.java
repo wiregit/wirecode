843f37f01de27938d75992ec3f31bf28e92dfd07
afisk <afisk>
2003-07-17 14:57:24 +0000

    new class for testing the FileUtils class -- for now only tests the getFileExtension method that looked sketchy and was sketchy.

---
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/util/FileUtilsTest.java
@@ -0,0 +1,63 @@
+package com.limegroup.gnutella.util;
+
+import java.io.File;
+
+import junit.framework.Test;
+
+/**
+ * Class for testing the FileUtils utility methods.
+ */
+public final class FileUtilsTest extends BaseTestCase {
+
+    /**
+     * @param name
+     */
+    public FileUtilsTest(String name) {
+        super(name);
+    }
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+    
+    public static Test suite() {
+        return buildTestSuite(FileUtilsTest.class);
+    }
+
+    /**
+     * Tests the method for extracting the file extension from a file.
+     * 
+     * @throws Exception if any error occurs.
+     */
+    public void testGetFileExtension() throws Exception  {
+        final String EXT = "ext";
+        File[] testFiles = new File[]  {
+          new File("standard."+EXT),  
+          new File("a.little.weird."+EXT), 
+          new File(".ext."+EXT), 
+        };
+        
+       
+        
+        for(int i=0; i<testFiles.length; i++)  {
+            System.out.println("TESTING EXTS");
+            System.out.println("testing: "+testFiles[i]+" extension: "+FileUtils.getFileExtension(testFiles[i]));
+            assertEquals("unexpected extension extracted", 
+                EXT, FileUtils.getFileExtension(testFiles[i]));
+        }
+        
+        // test files that should return null
+        File[] nullFiles = new File[]  {
+          new File("standard."),  
+          new File("."+EXT), 
+          new File("noextension"), 
+        };
+        
+        for(int i=0; i<nullFiles.length; i++)  {
+            System.out.println("TESTING NULLS");
+            System.out.println("testing: "+nullFiles[i]+" extension: "+FileUtils.getFileExtension(nullFiles[i]));
+            assertNull("extension should be null", 
+                FileUtils.getFileExtension(nullFiles[i]));
+        };
+    }
+}
