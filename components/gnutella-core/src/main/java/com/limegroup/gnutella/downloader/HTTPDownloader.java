a5fb994446325b78ca3e79f3de89c516617dde3e
sberlin <sberlin>
2003-07-21 20:22:46 +0000

    hello port 1.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/downloader/HTTPDownloader.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/downloader/HTTPDownloader.java
@@ -838,7 +838,7 @@ public class HTTPDownloader implements BandwidthTracker {
 		_byteReader = new ByteReader(stream);
 		_alternateLocationsReceived = null;
 		_alternateLocationsToSend = null;
-		_rfd =  new RemoteFileDesc("127.0.0.1", 0,
+		_rfd =  new RemoteFileDesc("127.0.0.1", 1,
                                   0, "a", 0, new byte[16],
                                   0, false, 0, false, null, null,
                                   false, false, "", 0, null);
