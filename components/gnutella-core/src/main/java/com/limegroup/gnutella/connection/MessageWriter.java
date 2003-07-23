e77013068e0e405ff5798f9cf22f4037e7b0e92d
afisk <afisk>
2003-07-23 20:26:20 +0000

    added many comments as well as new exceptions that are throws if incorrect methods are ever called, particularly calls to write new messages when there's already a message that has not been completely written.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/connection/MessageWriter.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/connection/MessageWriter.java
@@ -16,6 +16,9 @@ import com.limegroup.gnutella.messages.Message;
  */
 public final class MessageWriter {
 
+    /**
+     * Constant for the <tt>SocketChannel</tt> that this writer writes to.
+     */
 	private final SocketChannel CHANNEL;
 
     /**
@@ -23,10 +26,22 @@ public final class MessageWriter {
 	 */
 	private ByteBuffer _message;
 	
+    
+    /**
+     * Factory method for creating <tt>MessageWriter</tt> instances for 
+     * associated channels.
+     * 
+     * @param channel the <tt>SocketChannel</tt> for this writer
+     * @return a new <tt>MessageWriter</tt> instance
+     */
+    public static MessageWriter createWriter(SocketChannel channel) {
+        return new MessageWriter(channel);
+    }
+    
 	/**
-	 * Ensure that this class cannot be constructed.
+	 * Creates a new <tt>MessageWriter</tt> instance for the specified channel.
 	 */
-	public MessageWriter(SocketChannel channel) {
+	private MessageWriter(SocketChannel channel) {
 		CHANNEL = channel;
 	}
 	
@@ -34,11 +49,29 @@ public final class MessageWriter {
 	 * Returns true if this has any queued data, i.e., if we're in the process
 	 * of sending a message.  
 	 */
-	public synchronized boolean hasQueued() {
+	public synchronized boolean hasPendingMessage() {
 		return _message != null;
 	}
 	
+    /**
+     * Writes the specified <tt>Message</tt> to this channel.
+     * 
+     * @param msg the <tt>Message</tt> to write
+     * @return <tt>true</tt> if the entire message was successfully written,
+     *  otherwise <tt>false</tt>.  A message may not be completely written in 
+     *  the case where upstream TCP buffers are full
+     * @throws IOException if there is an IO error writing to the channel
+     * @throws IllegalStateException if there is a previous message that was 
+     *  not fully sent
+     */
     public synchronized boolean write(Message msg) throws IOException {
+        
+        // this method should not be called if there's a previouos message
+        // that has not been fully sent -- throw IllegalStateExcepion in this
+        // case
+        if(hasPendingMessage()) {
+            throw new IllegalStateException("previous message pending");
+        }
         //System.out.println("MessageWriter::write");
         //Copy m to a ByteBuffer.  TODO: avoid allocating ByteBuffer each time.
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -48,25 +81,35 @@ public final class MessageWriter {
             Assert.that(false, "IO problem from ByteArrayOutputStream");
             return false;
         }
-		int n = msg.getTotalLength();
-        _message = ByteBuffer.allocate(n);
+        _message = ByteBuffer.allocate(msg.getTotalLength());
         _message.put(baos.toByteArray());
         _message.flip();        //prepare for writing        
         
         return write();
     }
     
-    
+    /**
+     * Writes any messages that have not been fully written to our TCP buffers.
+     * This must be called in the case where only part of a message has been
+     * sent.  If it is called in this case, <tt>IllegalStateException</tt> will
+     * be thrown.
+     * 
+     * @return <tt>true</tt> if all of the message was successfully written to
+     *  our TCP buffers, otherwise <tt>false</tt>
+     * @throws IOException if any IO error occurs during the write
+     * @throws IllegalStateException if there are no pending messages to be 
+     *  sent
+     */
     public synchronized boolean write() throws IOException {
-    	if(!hasQueued()) {
-    		return false;
+    	if(!hasPendingMessage()) {
+    		throw new IllegalStateException("no pending message");
     	}
     	CHANNEL.write(_message);
     	if(!_message.hasRemaining()) {
     		_message = null;
-    		return false;
-    	} else {
     		return true;
+    	} else {
+    		return false;
     	}
     }
     
