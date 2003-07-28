a2bced4578957c73077d99fe374179b88350b59e
afisk <afisk>
2003-07-28 04:13:53 +0000

    new class that acts as a proxy for the underlying MessageWriter delegate.

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/connection/MessageWriterProxy.java
@@ -0,0 +1,66 @@
+package com.limegroup.gnutella.connection;
+
+import java.io.IOException;
+
+import com.limegroup.gnutella.ManagedConnection;
+import com.limegroup.gnutella.messages.Message;
+import com.limegroup.gnutella.settings.ConnectionSettings;
+import com.limegroup.gnutella.util.CommonUtils;
+
+/**
+ * Proxy class the delegates to the approppriate <tt>MessageWriter</tt>
+ * implementation.
+ */
+public final class MessageWriterProxy implements MessageWriter {
+
+    /**
+     * Constant for the delegate class that implements all writing methods.
+     */
+    private MessageWriter DELEGATE;
+    
+    /**
+     * Creates a new <tt>MessageWriterProxy</tt> instance that delegates to
+     * the appropriate <tt>MessageWriter</tt> implementation depending on
+     * whether or not we're running on Java 1.4 and whether NIO is active.
+     * 
+     * @param mc the <tt>ManagedConnection</tt> that ultimately handles some
+     *  of the writing
+     */
+    public MessageWriterProxy(ManagedConnection mc) {
+        if(CommonUtils.isJava14OrLater() && 
+           ConnectionSettings.USE_NIO.getValue()) {
+            DELEGATE = NIOMessageWriter.createWriter(mc);       
+        } else {
+            DELEGATE = BIOMessageWriter.createWriter(mc);
+        }    
+    }
+    
+    /* (non-Javadoc)
+     * @see com.limegroup.gnutella.connection.MessageWriter#write()
+     */
+    public boolean write() throws IOException {
+        return DELEGATE.write();
+    }
+
+    /* (non-Javadoc)
+     * @see com.limegroup.gnutella.connection.MessageWriter#write(com.limegroup.gnutella.messages.Message)
+     */
+    public boolean write(Message msg) throws IOException {
+        return DELEGATE.write(msg);
+    }
+
+    /* (non-Javadoc)
+     * @see com.limegroup.gnutella.connection.MessageWriter#hasPendingMessage()
+     */
+    public boolean hasPendingMessage() {
+        return DELEGATE.hasPendingMessage();
+    }
+
+    /* (non-Javadoc)
+     * @see com.limegroup.gnutella.connection.MessageWriter#close()
+     */
+    public void close() {
+        DELEGATE.close();
+    }
+
+}
