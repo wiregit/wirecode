76987a96b1b4c6c81471ffb82fe609be1e3da85b
sberlin <sberlin>
2003-08-01 19:36:58 +0000

    new bug settings class.

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/settings/BugSettings.java
@@ -0,0 +1,22 @@
+package com.limegroup.gnutella.settings;
+
+/**
+ * Settings to deal with bugs
+ */ 
+public final class BugSettings extends LimeProps {
+
+    private BugSettings() {}
+
+    /**
+     * Setting for whether or not to automatically report bugs
+     * to the bug servlet.
+     */
+    public static final BooleanSetting USE_BUG_SERVLET =
+		FACTORY.createBooleanSetting("USE_BUG_SERVLET", true);
+		
+    /**
+     * Setting for whether or not to completely ignore all bugs.
+     */
+    public static final BooleanSetting IGNORE_ALL_BUGS =
+        FACTORY.createBooleanSetting("IGNORE_ALL_BUGS", false);
+}
