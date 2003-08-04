dbca886029d7777606415c67253a1a75192bbdcc
afisk <afisk>
2003-08-04 00:19:04 +0000

    Added call to buildAndStartQueues when connecting LimeWires to avoid NullPointerExceptions that would otherwise happen while writing.  Also added other little fixes.

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/ServerSideDynamicQueryTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/ServerSideDynamicQueryTest.java
@@ -169,12 +169,16 @@ public final class ServerSideDynamicQueryTest extends BaseTestCase {
 		buildConnections();
         //1. first Ultrapeer connection 
         ULTRAPEER_2.initialize();
+        ULTRAPEER_2.buildAndStartQueues();
 
         //2. second Ultrapeer connection
         ULTRAPEER_1.initialize();
+        ULTRAPEER_1.buildAndStartQueues();
         
         //3. routed leaf, with route table for "test"
         LEAF.initialize();
+        LEAF.buildAndStartQueues();
+        
         QueryRouteTable qrt = new QueryRouteTable();
         qrt.add("berkeley");
         qrt.add("susheel");
