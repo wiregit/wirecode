6a22c31df4f6971a5785abdc5eef416bd1ab24ad
afisk <afisk>
2003-08-11 18:39:09 +0000

    Changes to get everything up to speed with this class.  It looks like this class and extra thread will not, in fact, prove necessary, but these changes are required if we do ever end up using it.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/connection/NIOMessageWriteManager.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/connection/NIOMessageWriteManager.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import com.limegroup.gnutella.Assert;
 import com.limegroup.gnutella.Connection;
 import com.limegroup.gnutella.ErrorService;
+import com.limegroup.gnutella.RouterService;
 import com.sun.java.util.collections.Collections;
 import com.sun.java.util.collections.Iterator;
 import com.sun.java.util.collections.LinkedList;
@@ -16,7 +17,10 @@ import com.sun.java.util.collections.List;
  * messages out to their respective channels.
  */
 public final class NIOMessageWriteManager implements Runnable {
-
+    
+    /**
+     * Instance of <tt>NIOMessageWriteManager</tt>, following singleton.
+     */
     private static final NIOMessageWriteManager INSTANCE =
         new NIOMessageWriteManager();
 
@@ -24,80 +28,99 @@ public final class NIOMessageWriteManager implements Runnable {
      * A lock for pending messages status.
      */
     private final Object MESSAGE_LOCK = new Object();        
-        
+    
+    /**
+     * <tt>List</tt> of <tt>MessageWriter</tt>s that have buffered messages to
+     * write to the network.
+     */    
     private final List PENDING_WRITERS = 
         Collections.synchronizedList(new LinkedList());
     
+    /**
+     * Instance accessor for the single <tt>NIOMessageWriteManager</tt> 
+     * instance.
+     * 
+     * @return the <tt>MessageWriteManager</tt> instance
+     */
     public static NIOMessageWriteManager instance() {
         return INSTANCE;
     }
     
+    /**
+     * Creates an <tt>NIOMessageWriteManager</tt>, starting the writing thread.
+     */
     private NIOMessageWriteManager() {
         Thread managerThread = new Thread(this, "NIO manager thread");
         managerThread.setDaemon(true);
         managerThread.start();
     }
     
-    public void addConnection(Connection conn) {
-        PENDING_WRITERS.add(conn);
-    }
-    
-    public void removeConnection(Connection conn) {
-        PENDING_WRITERS.remove(conn);
-    }
-    
-    /* (non-Javadoc)
-     * @see com.limegroup.gnutella.connection.MessageWriter#write(com.limegroup.gnutella.messages.Message)
+    /**
+     * Adds a <tt>MessageWriter</tt> to the collection of message writers 
+     * needing write notifications.
+     * 
+     * @param writer the <tt>MessageWriter</tt> with buffered messages to add
      */
-    public void messageAdded(Connection conn) {
+    public void addWriter(Connection writer) {
+        System.out.println("NIOMessageWriteManager::addWriter*********");
+        PENDING_WRITERS.add(writer);
         synchronized (MESSAGE_LOCK) {
             MESSAGE_LOCK.notify();
         }
     }
     
+    /**
+     * Removes the specified <tt>MessageWriter</tt> from the collection of 
+     * writers needing write notifications.
+     * 
+     * @param writer the <tt>MessageWriter</tt> to remove
+     */
+    public void removeWriter(Connection writer) {
+        PENDING_WRITERS.remove(writer);
+    }
+    
+    /**
+     * Waits for connections needing write notifications and writes 
+     * notifications when there are writers needing servicing.
+     */
     public void run() {
-        //Exceptions are only caught to set the _runnerDied variable
-        //to make testing easier.  For non-IOExceptions, Throwable
-        //is caught to notify ErrorService.
         try {
             while (true) {
                 waitForMessages();
                 writeMessages();
             }                
-        } catch (IOException e) {
         } catch(Throwable t) {    
             ErrorService.error(t);
         }
     }
     
     /** 
-     * Wait until the queue is (probably) non-empty or closed. 
-     * @throws IOException this was closed while waiting
+     * Wait until there are message writers needing notification to write.
      */
-    private void waitForMessages() throws IOException {
-        //The synchronized statement is outside the while loop to
-        //protect _queued.
+    private void waitForMessages() {
         synchronized (MESSAGE_LOCK) {
             while (PENDING_WRITERS.isEmpty()) {        
                 try {
                     MESSAGE_LOCK.wait();
                 } catch (InterruptedException e) {
-                    Assert.that(false, "NIOMessageWriteManager nterrupted");
+                    Assert.that(false, "NIOMessageWriteManager interrupted");
                 }
             }
         }
     }
     
+    /**
+     * Loops through the pending message writers, notifying each one to write.
+     */
     private void writeMessages() {
         synchronized(PENDING_WRITERS) {
             Iterator iter = PENDING_WRITERS.iterator();
             while(iter.hasNext()) {
                 Connection conn = (Connection)iter.next();
-                MessageWriter writer = conn.writer();
                 try {
-                    writer.write();
+                    conn.writer().write();
                 } catch (IOException e) {
-                    // TODO:: remove connection here??
+                    RouterService.removeConnection(conn);
                     
                 }
             }
