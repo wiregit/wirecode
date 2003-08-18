07bd5115b17ad87479d6ed4003608de4e6b4da6a
afisk <afisk>
2003-08-18 20:44:48 +0000

    Moved message creation factory code for blocking messages to BIOMessageReader.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/messages/Message.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/messages/Message.java
@@ -4,10 +4,7 @@ import com.sun.java.util.collections.*;
 import java.io.*;
 import com.limegroup.gnutella.*;
 import com.limegroup.gnutella.settings.*;
-import com.limegroup.gnutella.statistics.ReceivedErrorStat;
-import com.limegroup.gnutella.routing.RouteTableMessage;
 import com.limegroup.gnutella.util.CommonUtils;
-import com.limegroup.gnutella.messages.vendor.*;
 
 /**
  * A Gnutella message (packet).  This class is abstract; subclasses
@@ -20,21 +17,47 @@ import com.limegroup.gnutella.messages.vendor.*;
  */
 public abstract class Message 
     implements Serializable, com.sun.java.util.collections.Comparable {
-    //Functional IDs defined by Gnutella protocol.
-    public static final byte F_PING=(byte)0x0;
-    public static final byte F_PING_REPLY=(byte)0x1;
-    public static final byte F_PUSH=(byte)0x40;
-    public static final byte F_QUERY=(byte)0x80;
-    public static final byte F_QUERY_REPLY=(byte)0x81;
-    public static final byte F_ROUTE_TABLE_UPDATE=(byte)0x30;
-    public static final byte F_VENDOR_MESSAGE=(byte)0x31;
-    public static final byte F_VENDOR_MESSAGE_STABLE=(byte)0x32;
-    
-    public static final int N_UNKNOWN = -1;
-    public static final int N_TCP = 1;
-    public static final int N_UDP = 2;
-    public static final int N_MULTICAST = 3;
 
+    /**
+     * Operation code for a ping.
+     */
+    public static final byte F_PING = (byte)0x0;
+    
+    /**
+     * Operation code for a pong.
+     */
+    public static final byte F_PING_REPLY = (byte)0x1;
+    
+    /**
+     * Operation code for a push.
+     */
+    public static final byte F_PUSH = (byte)0x40;
+    
+    /**
+     * Operation code for a query.
+     */
+    public static final byte F_QUERY = (byte)0x80;
+    
+    /**
+     * Operation code for a query hit.
+     */
+    public static final byte F_QUERY_REPLY = (byte)0x81;
+    
+    /**
+     * Operation code for a route table update.
+     */
+    public static final byte F_ROUTE_TABLE_UPDATE = (byte)0x30;
+    
+    /**
+     * Operation code for a vendor message.
+     */
+    public static final byte F_VENDOR_MESSAGE = (byte)0x31;
+    
+    /**
+     * Operation code for a vendor message.
+     */
+    public static final byte F_VENDOR_MESSAGE_STABLE = (byte)0x32;
+        
     /**
      * Cached soft max ttl -- if the TTL+hops is greater than SOFT_MAX,
      * the TTL is set to SOFT_MAX-hops.
@@ -74,22 +97,12 @@ public abstract class Message
 	 * Constant for whether or not to record stats.
 	 */
 	protected static final boolean RECORD_STATS = !CommonUtils.isJava118();
+    
+    public static final int N_UNKNOWN = -1;
+    public static final int N_TCP = 1;
+    public static final int N_UDP = 2;
+    public static final int N_MULTICAST = 3;
    
-    /** Rep. invariant */
-    protected void repOk() {
-        Assert.that(guid.length==16);
-        Assert.that(func==F_PING || func==F_PING_REPLY
-                    || func==F_PUSH
-                    || func==F_QUERY || func==F_QUERY_REPLY
-                    || func==F_VENDOR_MESSAGE 
-                    || func == F_VENDOR_MESSAGE_STABLE,
-                    "Bad function code");
-
-        if (func==F_PUSH) Assert.that(length==26, "Bad push length: "+length);
-        Assert.that(ttl>=0, "Negative TTL: "+ttl);
-        Assert.that(hops>=0, "Negative hops: "+hops);
-        Assert.that(length>=0, "Negative length: "+length);
-    }
 
     ////////////////////// Constructors and Producers /////////////////
 
@@ -119,224 +132,12 @@ public abstract class Message
     protected Message(byte[] guid, byte func, byte ttl,
               byte hops, int length, int network) {
 		if(guid.length != 16) {
-			throw new IllegalArgumentException("invalid guid length: "+guid.length);
+			throw new IllegalArgumentException("invalid guid length: "+
+                guid.length);
 		} 		
         this.guid=guid; this.func=func; this.ttl=ttl;
         this.hops=hops; this.length=length; this.network = network;
         //repOk();
-    }
-	
-    /**
-     * Reads a Gnutella message from the specified input stream.  The returned
-     * message can be any one of the recognized Gnutella message, such as
-     * queries, query hits, pings, pongs, etc.
-     *
-     * @param in the <tt>InputStream</tt> instance containing message data
-     * @return a new Gnutella message instance
-     * @throws <tt>BadPacketException</tt> if the message is not considered
-     *  valid for any reason
-     * @throws <tt>IOException</tt> if there is any IO problem reading the
-     *  message
-     */
-    public static Message read(InputStream in)
-		throws BadPacketException, IOException {
-        return Message.read(in, new byte[23], N_UNKNOWN, SOFT_MAX);
-    }
-
-    /**
-     * @modifies in
-     * @effects reads a packet from the network and returns it as an
-     *  instance of a subclass of Message, unless one of the following happens:
-     *    <ul>
-     *    <li>No data is available: returns null
-     *    <li>A bad packet is read: BadPacketException.  The client should be
-     *      able to recover from this.
-     *    <li>A major problem occurs: IOException.  This includes reading packets
-     *      that are ridiculously long and half-completed messages. The client
-     *      is not expected to recover from this.
-     *    </ul>
-     */
-    public static Message read(InputStream in, byte softMax)
-		throws BadPacketException, IOException {
-        return Message.read(in, new byte[23], N_UNKNOWN, softMax);
-    }
-    
-    /**
-     * @modifies in
-     * @effects reads a packet from the network and returns it as an
-     *  instance of a subclass of Message, unless one of the following happens:
-     *    <ul>
-     *    <li>No data is available: returns null
-     *    <li>A bad packet is read: BadPacketException.  The client should be
-     *      able to recover from this.
-     *    <li>A major problem occurs: IOException.  This includes reading packets
-     *      that are ridiculously long and half-completed messages. The client
-     *      is not expected to recover from this.
-     *    </ul>
-     */
-    public static Message read(InputStream in, int network)
-		throws BadPacketException, IOException {
-        return Message.read(in, new byte[23], network, SOFT_MAX);
-    }    
-    
-    /**
-     * @requires buf.length==23
-     * @effects exactly like Message.read(in), but buf is used as scratch for
-     *  reading the header.  This is an optimization that lets you avoid
-     *  repeatedly allocating 23-byte arrays.  buf may be used when this returns,
-     *  but the contents are not guaranteed to contain any useful data.  
-     */
-    public static Message read(InputStream in, byte[] buf, byte softMax)
-		throws BadPacketException, IOException {
-        return Message.read(in, buf, N_UNKNOWN, softMax);
-    }
-
-
-    /**
-     * @param network the network this was received from.
-     * @requires buf.length==23
-     * @effects exactly like Message.read(in), but buf is used as scratch for
-     *  reading the header.  This is an optimization that lets you avoid
-     *  repeatedly allocating 23-byte arrays.  buf may be used when this returns,
-     *  but the contents are not guaranteed to contain any useful data.  
-     */
-    public static Message read(InputStream in, byte[] buf, int network, byte softMax)
-		throws BadPacketException, IOException {
-
-        //1. Read header bytes from network.  If we timeout before any
-        //   data has been read, return null instead of throwing an
-        //   exception.
-        for (int i=0; i<23; ) {
-            int got;
-            try {
-                got=in.read(buf, i, 23-i);
-            } catch (InterruptedIOException e) {
-                //have we read any of the message yet?
-                if (i==0) return null;
-                else throw e;
-            }
-            if (got==-1) {
-                if( RECORD_STATS )
-                ReceivedErrorStat.CONNECTION_CLOSED.incrementStat();
-                throw new IOException("Connection closed.");
-            }
-            i+=got;
-        }
-
-        //2. Unpack.
-        byte func=buf[16];
-        byte ttl=buf[17];
-        byte hops=buf[18];
-        int length=ByteOrder.leb2int(buf,19);
-        //2.5 If the length is hopelessly off (this includes lengths >
-        //    than 2^31 bytes, throw an irrecoverable exception to
-        //    cause this connection to be closed.
-        if (length<0 || length > MessageSettings.MAX_LENGTH.getValue()) {
-            if( RECORD_STATS )
-                ReceivedErrorStat.INVALID_LENGTH.incrementStat();
-            throw new IOException("Unreasonable message length: "+length);
-        }
-
-        //3. Read rest of payload.  This must be done even for bad
-        //   packets, so we can resume reading packets.
-        byte[] payload=null;
-        if (length!=0) {
-            payload=new byte[length];
-            for (int i=0; i<length; ) {
-                int got=in.read(payload, i, length-i);
-                if (got==-1) {
-                    if( RECORD_STATS )
-                        ReceivedErrorStat.CONNECTION_CLOSED.incrementStat();
-                    throw new IOException("Connection closed.");
-                }
-                i+=got;
-            }
-        }
-        else
-            payload = new byte[0];
-
-        //4. Check values.   These are based on the recommendations from the
-        //   GnutellaDev page.  This also catches those TTLs and hops whose
-        //   high bit is set to 0.
-        byte hardMax = (byte)14;
-        if (hops<0) {
-            if( RECORD_STATS )
-                ReceivedErrorStat.INVALID_HOPS.incrementStat();
-            throw new BadPacketException("Negative (or very large) hops");
-        } else if (ttl<0) {
-            if( RECORD_STATS )
-                ReceivedErrorStat.INVALID_TTL.incrementStat();
-            throw new BadPacketException("Negative (or very large) TTL");
-        } else if ((hops >= softMax) && 
-                 (func != F_QUERY_REPLY) &&
-                 (func != F_PING_REPLY)) {
-            if( RECORD_STATS )
-                ReceivedErrorStat.HOPS_EXCEED_SOFT_MAX.incrementStat();
-            throw BadPacketException.HOPS_EXCEED_SOFT_MAX;
-        }
-        else if (ttl+hops > hardMax) {
-            if( RECORD_STATS )
-                ReceivedErrorStat.HOPS_AND_TTL_OVER_HARD_MAX.incrementStat();
-            throw new BadPacketException("TTL+hops exceeds hard max; probably spam");
-        } else if ((ttl+hops > softMax) && 
-                 (func != F_QUERY_REPLY) &&
-                 (func != F_PING_REPLY)) {
-            ttl=(byte)(softMax - hops);  //overzealous client;
-                                         //readjust accordingly
-            Assert.that(ttl>=0);     //should hold since hops<=softMax ==>
-                                     //new ttl>=0
-        }
-
-		// Delayed GUID allocation
-        byte[] guid=new byte[16];
-        for (int i=0; i<16; i++) //TODO3: can optimize
-            guid[i]=buf[i];
-
-        //Dispatch based on opcode.
-        switch (func) {
-            //TODO: all the length checks should be encapsulated in the various
-            //constructors; Message shouldn't know anything about the various
-            //messages except for their function codes.  I've started this
-            //refactoring with PushRequest and PingReply.
-            case F_PING:
-				if (length>0) //Big ping
-                    return new PingRequest(guid,ttl,hops,payload);
-                return new PingRequest(guid,ttl,hops);
-
-            case F_PING_REPLY:
-                return PingReply.createFromNetwork(guid, ttl, hops, payload);
-            case F_QUERY:
-                if (length<3) break;
-				return QueryRequest.createNetworkQuery(
-				    guid, ttl, hops, payload, network);
-            case F_QUERY_REPLY:
-                if (length<26) break;
-                return new QueryReply(guid,ttl,hops,payload);
-            case F_PUSH:
-                return new PushRequest(guid,ttl,hops,payload, network);
-            case F_ROUTE_TABLE_UPDATE:
-                //The exact subclass of RouteTableMessage returned depends on
-                //the variant stored within the payload.  So leave it to the
-                //static read(..) method of RouteTableMessage to actually call
-                //the right constructor.
-                return RouteTableMessage.read(guid, ttl, hops, payload);
-            case F_VENDOR_MESSAGE:
-                if ((ttl != 1) || (hops != 0))
-                    throw new BadPacketException("VM with bad ttl/hops: " +
-                                                 ttl + "/" + hops);
-                return VendorMessage.deriveVendorMessage(guid, ttl, hops, 
-                                                         payload);
-            case F_VENDOR_MESSAGE_STABLE:
-                if ((ttl != 1) || (hops != 0))
-                    throw new BadPacketException("VM with bad ttl/hops: " +
-                                                 ttl + "/" + hops);
-                return VendorMessage.deriveVendorMessage(guid, ttl, hops, 
-                                                         payload);
-        }
-        
-        if( RECORD_STATS )
-                ReceivedErrorStat.INVALID_CODE.incrementStat();
-        throw new BadPacketException("Unrecognized function code: "+func);
     }
 
     /**
