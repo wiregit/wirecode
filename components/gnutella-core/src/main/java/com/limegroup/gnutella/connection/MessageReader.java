bd50b2d7c43e041227d7e99854244de00fc3e817
afisk <afisk>
2003-08-18 13:42:32 +0000

    Added startReading and two read methods to interface.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/connection/MessageReader.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/connection/MessageReader.java
@@ -2,11 +2,11 @@ package com.limegroup.gnutella.connection;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InterruptedIOException;
 import java.nio.channels.SelectionKey;
 
 import com.limegroup.gnutella.messages.BadPacketException;
 import com.limegroup.gnutella.messages.Message;
-
 /**
  * Interface for Gnutella message reading.
  */
@@ -17,4 +17,26 @@ public interface MessageReader {
     
     Message createMessageFromTCP(SelectionKey key)
         throws BadPacketException, IOException;
+        
+    /**
+     * Notifies the <tt>MessageReader</tt> that it should start reading
+     * messages.
+     * 
+     * @throws IOException if there is an IO error reading messages
+     */    
+    void startReading() throws IOException;
+
+    /**
+     * 
+     * @return
+     */
+    Message read() throws IOException, BadPacketException;
+
+    /**
+     * @param i
+     * @return
+     */
+    Message read(int i) throws IOException, BadPacketException, 
+        InterruptedIOException;
+    
 }
