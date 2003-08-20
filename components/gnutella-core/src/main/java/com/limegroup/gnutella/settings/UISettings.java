9cb0410952c8bb10a32e1908efc8ef34c5c11bf6
sberlin <sberlin>
2003-08-20 17:15:20 +0000

    No more global row stripe option.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/settings/UISettings.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/settings/UISettings.java
@@ -12,11 +12,4 @@ public final class UISettings extends LimeProps {
      */
     public static final BooleanSetting AUTOCOMPLETE_ENABLED =
 		FACTORY.createBooleanSetting("AUTOCOMPLETE_ENABLED", true);
-		
-    /**
-     * Setting for whether or not row striping is enabled.
-     */
-    public static final BooleanSetting ROW_STRIPE_ENABLED =
-        FACTORY.createBooleanSetting("ROW_STRIPE_ENABLED", true);
-
 }
