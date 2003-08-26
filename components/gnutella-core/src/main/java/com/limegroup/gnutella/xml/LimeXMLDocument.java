7288dc152057bb8f31ea59d843345e1d4bbe9a1c
sberlin <sberlin>
2003-08-26 18:13:57 +0000

    more validation on LimeXMLDocuments.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/xml/LimeXMLDocument.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/xml/LimeXMLDocument.java
@@ -73,6 +73,8 @@ public class LimeXMLDocument implements Serializable {
         InputSource doc = new InputSource(new StringReader(XMLStr));
         initialize(doc);
         this.xmlString = ripIdentifier(XMLStr);
+        if(xmlString.equals(""))
+            throw new SAXException("empty after identifier ripped");
     }
     
     
@@ -81,6 +83,8 @@ public class LimeXMLDocument implements Serializable {
         grabDocInfo(rootElement,true);
         grabDocInfo(node,false);
         createMap(node,rootElement.getNodeName());
+        if(fieldToValue.isEmpty())
+            throw new IOException("no elements.");
     }
 
     /**
@@ -95,6 +99,8 @@ public class LimeXMLDocument implements Serializable {
        
         //set the schema URI
         this.schemaUri = schemaURI;
+        if(nameValueList.isEmpty())
+            throw new IllegalArgumentException("empty list");
                 
         //iterate over the passed list of fieldnames & values
         for(Iterator i = nameValueList.iterator(); i.hasNext(); ) {
@@ -115,6 +121,16 @@ public class LimeXMLDocument implements Serializable {
         }
     }
     
+    /**
+     * Determines whether or not this LimeXMLDocument is valid.
+     */
+    boolean isValid() {
+        try {
+            return !getXMLString().equals("") && !fieldToValue.isEmpty();
+        } catch(SchemaNotFoundException snfe) {
+            return false;
+        }
+    }
 
     private void readObject(java.io.ObjectInputStream in)
         throws IOException, ClassNotFoundException {
