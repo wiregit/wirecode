3635877b7a7a6d34b3e7171af67332f56b90a594
gbildson <gbildson>
2003-08-26 18:55:36 +0000

    reverted changes

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/browser/HTTPAcceptor.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/browser/HTTPAcceptor.java
@@ -14,8 +14,7 @@ import com.limegroup.gnutella.ErrorService;
 import com.limegroup.gnutella.util.CommonUtils;
 import com.limegroup.gnutella.util.IOUtils;
 import com.limegroup.gnutella.util.URLDecoder;
-import java.util.StringTokenizer;
-import java.net.URLEncoder;
+
 
 /**
  * Listens on an HTTP port, accepts incoming connections, and dispatches 
@@ -32,10 +31,6 @@ public class HTTPAcceptor implements Runnable {
     private static final String MAGNET         = "magnet:?";
 	/** HTTP no content return */
 	private static final String NOCONTENT      = "HTTP/1.1 204 No Content\r\n";
-	/** Magnet detail command */
-    private static final String MAGNETDETAIL   = "magcmd/detail?";
-    /** Magnet email command */
-    private static final String MAGNETEMAIL    = "magcmd/email?";
 
     /**
      * The socket that listens for incoming connections. Can be changed to
@@ -122,13 +117,6 @@ public class HTTPAcceptor implements Runnable {
             return;
         }
     }
-
-    /**
-     *  Return the listening port.
-     */
-    public int getPort() {
-		return _port;
-	}
 
     /** @modifies this, network
      *  @effects accepts http/magnet requests
@@ -305,26 +293,6 @@ public class HTTPAcceptor implements Runnable {
 			    // upload browsed files
 		        HTTPHandler.create(socket, str);
 			}
-        } else if (str.indexOf(MAGNETDETAIL) >= 0) {
-            int loc = 0;
-            if ((loc = str.indexOf(MAGNETDETAIL)) < 0)
-                return;
-            int loc2 = str.lastIndexOf(" HTTP");
-            String command = 
-                  str.substring(loc+MAGNETDETAIL.length(), loc2);
-            String page=buildMagnetDetailPage(command);
-            
-            HTTPHandler.createPage(socket, page);
-		} else if (str.indexOf(MAGNETEMAIL) >= 0) {
-			int loc = 0;
-			if ((loc = str.indexOf(MAGNETEMAIL)) < 0)
-				return;
-			int loc2 = str.lastIndexOf(" HTTP");
-			String command = 
-				  str.substring(loc+MAGNETEMAIL.length(), loc2);
-			String page=buildMagnetEmailPage(command);
-			
-		    HTTPHandler.createPage(socket, page);
 		} else if (str.indexOf(MAGNET) >= 0) {
 			// trigger an operation
 			int loc  = str.indexOf(MAGNET);
@@ -354,240 +322,18 @@ public class HTTPAcceptor implements Runnable {
 		returnNoContent(socket);
 	}
 
-    private String buildMagnetDetailPage(String cmd) {
-        StringTokenizer st = new StringTokenizer(cmd, "&");
-        String keystr;
-        String valstr;
-        int    start;
-        String address = "";
-        String fname   = "";
-        String sha1    = "";
-        String ret= magnetDetailPageHeader();
-        
-        // Process each key=value pair
-        while (st.hasMoreTokens()) {
-            keystr = st.nextToken();
-            keystr = keystr.trim();
-            start  = keystr.indexOf("=")+1;
-            valstr = keystr.substring(start);
-            keystr = keystr.substring(0,start-1);
-            valstr=URLDecoder.decode(valstr);   
-            if ( keystr.equals("addr") ) {
-                address = valstr;
-            } else if ( keystr.startsWith("n") ) {
-                fname = valstr;
-            } else if ( keystr.startsWith("u") ) {
-                sha1 = valstr;
-                ret += magnetDetail(address, fname, sha1);
-            }
-        }
-        ret += 
-          "</table>"+
-          "</body></html>";
-        return ret;
-    }
-
-	private String buildMagnetEmailPage(String cmd) {
-		StringTokenizer st = new StringTokenizer(cmd, "&");
-		String keystr;
-		String valstr;
-		int    start;
-		String address = "";
-		String fname   = "";
-		String sha1    = "";
-        String ret     = "";
-        int    count   = 0;
-        
-		// Process each key=value pair
-     	while (st.hasMoreTokens()) {
-		    keystr = st.nextToken();
-			keystr = keystr.trim();
-			start  = keystr.indexOf("=")+1;
-		    valstr = keystr.substring(start);
-			keystr = keystr.substring(0,start-1);
-			valstr=URLDecoder.decode(valstr);	
-			if ( keystr.equals("addr") ) {
-				address = valstr;
-                ret= magnetEmailHeader(address);
-			} else if ( keystr.startsWith("n") ) {
-				fname = valstr;
-			} else if ( keystr.startsWith("u") ) {
-				sha1 = valstr;
-				ret += magnetEmailDetail(address, fname, sha1, count);
-                count++;
-			}
+	private void returnNoContent(Socket socket) {
+		try {
+			
+			BufferedOutputStream out =
+			  new BufferedOutputStream(socket.getOutputStream());
+			String s = NOCONTENT;
+			byte[] bytes=s.getBytes();
+			out.write(bytes);
+			out.flush();
+			
+			socket.close();
+		} catch (IOException e) {
 		}
-        ret += magnetEmailTrailer(count);
-		return ret;
 	}
-
-    private String magnetDetail(String address, String fname, String sha1) {
-        String ret =
-         "  <tr> "+
-         "    <td bgcolor=\"#CCCCCC\" class=\"text\"><b>Name</b></td>"+
-         "    <td bgcolor=\"#FFFFFF\" class=\"name\">"+fname+"</td>"+
-         "  </tr>"+
-         "  <tr> "+
-         "    <td bgcolor=\"#CCCCCC\" class=\"text\"><b>SHA1</b></td>"+
-         "    <td bgcolor=\"#ffffff\" class=\"text\">"+sha1+"</td>"+
-         "  </tr>"+
-         "  <tr> "+
-         "    <td bgcolor=\"#CCCCCC\" class=\"text\"><b>Link</b></td>"+
-         "    <td bgcolor=\"#ffffff\" class=\"text\"><a href=\"magnet:?xt=urn:sha1:"+sha1+"&dn="+fname+"&xs=http://"+address+"/uri-res/N2R?urn:sha1:"+sha1+"\">"+
-         fname+"</a></td>"+
-         "  </tr>"+
-         "  <tr> "+
-         "    <td bgcolor=\"#CCCCCC\" class=\"text\"><b>Magnet</b></td>"+
-         "    <td bgcolor=\"#ffffff\"><textarea name=\"textarea\" cols=\"80\" rows=\"4\" wrap=\"VIRTUAL\" class=\"area\">magnet:?xt=urn:sha1:"+sha1+"&dn="+fname+"&xs=http://"+address+"/uri-res/N2R?urn:sha1:"+sha1+"</textarea></td>"+
-         "  </tr>"+
-         "  <tr> "+
-         "    <td bgcolor=\"#CCCCCC\" class=\"text\"><b>Html link</b></td>"+
-         "    <td bgcolor=\"#ffffff\"><textarea name=\"textarea\" cols=\"80\" rows=\"5\" wrap=\"VIRTUAL\" class=\"area\"><a href=\"magnet:?xt=urn:sha1:"+sha1+"&dn="+fname+"&xs=http://"+address+"/uri-res/N2R?urn:sha1:"+sha1+"\">"+fname+"</a></textarea></td>"+
-         "  </tr>"+
-         "  <tr bgcolor=\"#333333\"> "+
-         "    <td colspan=\"2\" class=\"text\" height=\"5\"></td></tr>";
-
-        return ret;
-    }
-
-
-    private static String magnetDetailPageHeader() {
-       String ret= 
-         "<html>"+
-         "<head>"+
-         "<title>LimeWire Magnet Descriptions</title>"+
-         "<style type=\"text/css\">"+
-         "<!--"+
-         ".text {"+
-         "    font-family: Verdana, Arial, Helvetica, sans-serif;"+
-         "    font-size: 11px;"+
-         "    color: #333333;"+
-         "}"+
-         ".header {"+
-         "    font-family: Arial, Helvetica, sans-serif;"+
-         "    font-size: 14pt;"+
-         "    color: #ffffff;"+
-         "}"+
-         ".name {"+
-         "    font-family: Verdana, Arial, Helvetica, sans-serif;"+
-         "    font-size: 11px;"+
-         "    font-weight: bold;"+
-         "    color: #000000;"+
-         "}"+
-         ".area  { "+
-         "border: 1px solid;"+
-          "margin: 0;"+
-          "padding: 4px;"+
-          "background: #FFFEF4;"+
-          "color: #333333;"+
-          "font: 11px Verdana, Arial;"+
-          "text-align: left;"+
-         "}"+
-         "-->"+
-         "</style>"+
-         "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\">"+
-         "</head>"+
-         "<body bgcolor=\"#666666\">"+
-         "<span class=\"header\"><center>"+
-         "  LimeWire Magnet Details "+
-         "</center></span><br>"+
-         "<table border=\"0\" cellpadding=\"5\" cellspacing=\"1\" bgcolor=\"#999999\" align=\"center\">";
-
-        return ret;
-    }
-
-
-    private String magnetEmailHeader(String address){
-        return
-          "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">\n"+
-          "<html>\n"+
-          "<head>\n"+
-          "<title>LimeWire Send Magnet Form</title>\n"+
-          "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\">\n"+
-          "<script LANGUAGE=\"JavaScript1.2\" SRC=\"/magnet10/scripts.js\" TYPE=\"text/javascript\"></script>\n"+
-          "<link href=\"/magnet10/style.css\" rel=\"stylesheet\" type=\"text/css\">\n"+
-          "</head>\n"+
-          "<body>\n"+
-          "<form name=\"form1\" method=\"post\" action=\""+
-          "http://email.limewire.com/cgi-bin/send_email.cgi"+
-          "\" onsubmit=\"return Validator(this)\">\n"+
-          "    <input name=\"address\" type=\"hidden\" value=\"\n"+address+"\">\n"+
-          "    <table width=\"500\" border=\"0\" align=\"center\" cellpadding=\"5\" cellspacing=\"1\" bgcolor=\"#FFFFFF\" align=\"center\">" +
-          "    <tr> \n"+
-          "      <td colspan=\"2\">To EMAIL direct links to the selected files in your Shared Library, fill out and submit the form below.</td>\n"+
-          "    </tr>\n"+
-          "    <tr class=\"yellow\"> \n"+
-          "      <td width=\"42\" align=\"right\"><b>To:</b></td>\n"+
-          "      <td width=\"415\"> \n"+
-          "        <input name=\"to_email\" type=\"text\" maxlength=\"100\">\n"+
-          "         (required)</td>\n"+
-          "    </tr>\n"+
-          "    <tr class=\"yellow\"> \n"+
-          "      <td align=\"right\"><b>CC:</b></td>\n"+
-          "      <td> \n"+
-          "        <input name=\"cc_email\" type=\"text\" maxlength=\"100\"></td>\n"+
-          "    </tr>\n"+
-          "    <tr class=\"yellow\"> \n"+
-          "      <td align=\"right\"><b>From: </b></td>\n"+
-          "      <td> \n"+
-          "        <input name=\"from_email\" type=\"text\" maxlength=\"100\">\n"+
-          "        (required)</td>\n"+
-          "    </tr>\n"+
-          "    <tr class=\"yellow\"> \n"+
-          "      <td align=\"right\"><b>Subject: </b></td>\n"+
-          "      <td> \n"+
-          "        <input name=\"subject\" type=\"text\" maxlength=\"100\">\n"+
-          "      </td>\n"+
-          "    </tr>\n"+
-          "    <tr class=\"yellow\"> \n"+
-          "      <td>&nbsp;</td>\n"+
-          "      <td>Message to send with links: <br> <textarea name=\"comment\" cols=\"40\" rows=\"5\" wrap=\"VIRTUAL\"></textarea> </td>\n"+
-          "    </tr>\n"+
-          "    <tr> \n"+
-          "      <td colspan=\"2\" class=\"yellow2\"> LINKS TO BE MAILED:<br>\n"+
-          "        <ol>";
-    }
-
-
-    private String magnetEmailDetail(String address, String fname, String sha1,
-         int count) {
-        return
-          "          <li><span class=\"links\">"+
-          "<a href=\"magnet:?xt=urn:sha1:"+sha1+"&dn="+fname+
-          "&xs=http://"+address+"/uri-res/N2R?urn:sha1:"+sha1+"\">"+
-          fname+"</a>"+
-          "</span><br>\n"+
-          "          <input name=\"f"+count+"\" type=\"hidden\" value=\""+fname+"\">\n"+
-          "          <input name=\"s"+count+"\" type=\"hidden\" value=\""+sha1+"\">\n"+
-          "            <br>\n"+
-          "            Link Description:<br>\n"+
-          "            <textarea name=\"desc"+count+"\" cols=\"40\" rows=\"2\" wrap=\"VIRTUAL\"></textarea>\n"+
-          "            <hr size=\"1\">\n"+
-          "          </li>\n";
-    }
-
-
-    private String magnetEmailTrailer(int count) {
-        return
-          "        </ol>\n"+
-          "        <input name=\"count\" type=\"hidden\" value=\""+count+"\">"+
-          "        <center><input name=\"Submit\" type=\"submit\" value=\"Submit and Send\"></center></td>\n"+
-          "    </tr>";
-    }
-
-    private void returnNoContent(Socket socket) {
-        try {
-            
-            BufferedOutputStream out =
-              new BufferedOutputStream(socket.getOutputStream());
-            String s = NOCONTENT;
-            byte[] bytes=s.getBytes();
-            out.write(bytes);
-            out.flush();
-            
-            socket.close();
-        } catch (IOException e) {
-        }
-    }
 }
-
