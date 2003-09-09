877df3f54f31695221f6c925d1f8d2210fdb3ea7
sdaswani <sdaswani>
2003-09-09 15:27:35 +0000

    partial check in, things not working, getting rid of MP3Info, fisk is a friskface

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/mp3/MP3Test.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/mp3/MP3Test.java
@@ -2,7 +2,7 @@ package com.limegroup.gnutella.mp3;
 
 
 import junit.framework.*;
-import com.limegroup.gnutella.mp3.*;
+import de.ueberdosis.mp3info.*;
 import com.limegroup.gnutella.util.CommonUtils;
 import com.sun.java.util.collections.*;
 /**
@@ -62,8 +62,10 @@ public class MP3Test extends com.limegroup.gnutella.util.BaseTestCase {
         
     }
     
-    private MP3Info newMP3Info(String loc) throws Exception {
-        return new MP3Info(CommonUtils.getResourceFile(loc).getPath());
+    private ExtendedID3Tag newMP3Info(String loc) throws Exception {
+        de.ueberdosis.mp3info.ID3Reader reader = 
+            new de.ueberdosis.mp3info.ID3Reader(CommonUtils.getResourceFile(loc).getPath());
+        return reader.getExtendedID3Tag();
     }
 
     
@@ -79,232 +81,174 @@ public class MP3Test extends com.limegroup.gnutella.util.BaseTestCase {
     public void testMP3()
         throws Exception {
         
-        MP3Info info = null;
+        de.ueberdosis.util.OutputCtr.setLevel(0);
+        ExtendedID3Tag info = null;
         
         /*
           mpg1layI_0h_448k_frame14_48000hz_dual_fl5Orig.mp3
         */
         info  = newMP3Info(file[1]);
-        assertEquals(info.getFileSize(), 21952L);
-        assertEquals(info.getBitRate() , 448);		
-        assertTrue(!info.isCoprighted());		
-        assertTrue(info.isOriginal());	
-        assertTrue(!info.isPadded()); 	
-        assertTrue(!info.isPrivate());	
-        assertTrue(info.isProtected());	
-        assertTrue(!info.isRiffWav());		
+        assertEquals(info.getSize(), 21952L);
+        assertEquals(info.getBitrate() , 448);		
+        assertTrue(!info.getCopyright());		
+        assertTrue(info.getOriginal());	
+        assertTrue(!info.getPadding()); 	
         assertEquals(info.getFrequency() , 48000);	
-        assertEquals(info.getHeaderBitRate() , 448);
-        assertEquals(info.getLayer_Numeric() , 1); 		
-        assertEquals(info.getLengthInSeconds() , 0); 
-        assertEquals(info.getMode() , "Dual Channel");	
-        assertEquals(info.getVersion_Numeric() , 1.0d, 0D);
-        assertEquals(info.getNumberOfFrames() , 1); //14-encspot 16-winamp//////////////////////////////////////
-        assertEquals(info.getEmphasis() , "none");
-        assertTrue(!info.hasVariableBitRate());
+        assertEquals(info.getLayer() , 1); 		
+        assertEquals(info.getRuntime() , 0); 
+        assertEquals(info.getChannelModeS() , "Dual Channel");	
+        assertEquals(info.getEmphasisS() , "none");
+        //        assertTrue(!info.hasVariableBitRate());
         
         /*
           mpg1layI_32k_f256_441hz_dual_5015emph_CRCcopyOrig_sectest.mp3
         */
         info  = newMP3Info(file[2]);
-        assertEquals(info.getFileSize(), 26645L);
-        assertEquals(info.getBitRate() , 32);		
-        assertTrue(info.isCoprighted());		
-        assertTrue(info.isOriginal());	
-        assertTrue(!info.isPadded()); 	
-        assertTrue(info.isPrivate());	
-        assertTrue(info.isProtected());	
-        assertTrue(!info.isRiffWav());		
+        assertEquals(info.getSize(), 26645L);
+        assertEquals(info.getBitrate() , 32);		
+        assertTrue(info.getCopyright());		
+        assertTrue(info.getOriginal());	
+        assertTrue(!info.getPadding()); 	
         assertEquals(info.getFrequency() , 44100);	
-        assertEquals(info.getHeaderBitRate() , 32);
-        assertEquals(info.getLayer_Numeric() , 1); 		
-        assertEquals(info.getLengthInSeconds() , 6); 
-        assertEquals(info.getMode() , "Dual Channel");	
-        assertEquals(info.getVersion_Numeric() , 1.0d, 0D);
-        assertEquals(info.getNumberOfFrames() , 2); //256-winamp no encspot/////////////////////////////////////
-        assertEquals(info.getEmphasis() , "50/15 ms");
-        assertTrue(!info.hasVariableBitRate());
+        assertEquals(info.getLayer() , 1); 		
+        assertEquals(info.getRuntime() , 6); 
+        assertEquals(info.getChannelModeS() , "Dual Channel");	
+        assertEquals(info.getEmphasisS() , "50/15 ms");
+        //        assertTrue(!info.hasVariableBitRate());
         
         /*
           mpg1layII_384k_f24_48hz_stereo_CRCOrig_Gogo1sectest.mp3
         */
         info  = newMP3Info(file[3]);
-        assertEquals(info.getFileSize(), 31104L);
-        assertEquals(info.getBitRate() , 384);		
-        assertTrue(!info.isCoprighted());		
-        assertTrue(info.isOriginal());	
-        assertTrue(!info.isPadded()); 	
-        assertTrue(!info.isPrivate());	
-        assertTrue(info.isProtected());	
-        assertTrue(!info.isRiffWav());		
+        assertEquals(info.getSize(), 31104L);
+        assertEquals(info.getBitrate() , 384);		
+        assertTrue(!info.getCopyright());		
+        assertTrue(info.getOriginal());	
+        assertTrue(!info.getPadding()); 	
         assertEquals(info.getFrequency() , 48000);	
-        assertEquals(info.getHeaderBitRate() , 384);
-        assertEquals(info.getLayer_Numeric() , 2); 		
-        assertEquals(info.getLengthInSeconds() , 0); 
-        assertEquals(info.getMode() , "Stereo");	
-        assertEquals(info.getVersion_Numeric() , 1.0d, 0D);
-        assertEquals(info.getNumberOfFrames() , 27); //24-encspot 27-winamp/////////////////////////////////////
-        assertEquals(info.getEmphasis() , "none");
-        assertTrue(!info.hasVariableBitRate());
+        assertEquals(info.getLayer() , 2); 		
+        assertEquals(info.getRuntime() , 0); 
+        assertEquals(info.getChannelModeS() , "Stereo");	
+        assertEquals(info.getEmphasisS() , "none");
+        //        assertTrue(!info.hasVariableBitRate());
 
         /*
           mpg1layIII_58k-VBRq30_f1211_441hz_joint_XingTAG_sample.mp3
         */
         info  = newMP3Info(file[5]);
-        assertEquals(info.getFileSize(), 232295L); 
-        assertEquals(info.getBitRate() , 58); //59-winamp(wrong) 58-encspot	
-        assertTrue(info.isCoprighted());		
-        assertTrue(info.isOriginal());	
-        assertTrue(!info.isPadded()); 	
-        assertTrue(!info.isPrivate());	
-        assertTrue(!info.isProtected());	
-        assertTrue(!info.isRiffWav());		
+        assertEquals(info.getSize(), 232295L); 
+        assertEquals(info.getBitrate() , 58); //59-winamp(wrong) 58-encspot	
+        assertTrue(info.getCopyright());		
+        assertTrue(info.getOriginal());	
+        assertTrue(!info.getPadding()); 	
         assertEquals(info.getFrequency() , 44100);	
-        assertEquals(info.getHeaderBitRate() , 48);
-        assertEquals(info.getLayer_Numeric() , 3); 		
-        assertEquals(info.getLengthInSeconds() , 31); 
-        assertEquals(info.getMode() , "Joint Stereo");	
-        assertEquals(info.getVersion_Numeric() , 1.0d, 0D);
-        assertEquals(info.getNumberOfFrames() , 1212);
-        assertEquals(info.getEmphasis() , "none");
-        assertTrue(info.hasVariableBitRate());
-		assertEquals(info.getVBRHeader().getScale() , 30);
-		assertNotNull(info.getVBRHeader().getTableOfContents());
+        assertEquals(info.getLayer() , 3); 		
+        assertEquals(info.getRuntime() , 31); 
+        assertEquals(info.getChannelModeS() , "Joint Stereo");	
+        assertEquals(info.getEmphasisS() , "none");
+        //        assertTrue(info.hasVariableBitRate());
+        //		assertEquals(info.getVBRHeader().getScale() , 30);
+        //		assertNotNull(info.getVBRHeader().getTableOfContents());
 			
         /*
           mpg1layII_0h_192k_frame44_44100hz_joint_CRCfl11.mp2
         */	
         info  = newMP3Info(file[8]);
-        assertEquals(info.getFileSize(), 30720L);
-        assertEquals(info.getBitRate() , 192);		
-        assertTrue(!info.isCoprighted());	
-        assertTrue(!info.isOriginal());	
-        assertTrue(info.isPadded()); 	
-        assertTrue(!info.isPrivate());	
-        assertTrue(info.isProtected());	
-        assertTrue(!info.isRiffWav());		
+        assertEquals(info.getSize(), 30720L);
+        assertEquals(info.getBitrate() , 192);		
+        assertTrue(!info.getCopyright());	
+        assertTrue(!info.getOriginal());	
+        assertTrue(info.getPadding()); 	
         assertEquals(info.getFrequency() , 44100);	
-        assertEquals(info.getHeaderBitRate() , 192);
-        assertEquals(info.getLayer_Numeric() , 2);		
-        assertEquals(info.getLengthInSeconds() , 1);	
-        assertEquals(info.getMode() , "Stereo");	
-        assertEquals(info.getVersion_Numeric() , 1.0d, 0D);
-        assertEquals(info.getNumberOfFrames() , 48); //44-encspot 49-winamp/////////////////////////////////////
-        assertEquals(info.getEmphasis() , "none");
-        assertTrue(!info.hasVariableBitRate());
+        assertEquals(info.getLayer() , 2);		
+        assertEquals(info.getRuntime() , 1);	
+        assertEquals(info.getChannelModeS() , "Stereo");	
+        assertEquals(info.getEmphasisS() , "none");
+        //        assertTrue(!info.hasVariableBitRate());
 	
         /*
           mpg2.5layIII_8k-VBRq95_f149_8khz_mono_ID3v2Orig_XingTAGLame.mp3
           !!Can't find Xing tag!! which is why stuff is messed up
         */	
         info  = newMP3Info(file[9]);
-        assertEquals(info.getFileSize(), 14336L);
-        assertEquals(info.getBitRate() , 32); //18-winamp 8-encspot/////////////////////////////////////////////
-        assertTrue(!info.isCoprighted());	
-        assertTrue(info.isOriginal());	
-        assertTrue(!info.isPadded()); 	
-        assertTrue(!info.isPrivate());	
-        assertTrue(!info.isProtected());	
-        assertTrue(!info.isRiffWav());		
+        assertEquals(info.getSize(), 14336L);
+        assertEquals(info.getBitrate() , 32); //18-winamp 8-encspot/////////////////////////////////////////////
+        assertTrue(!info.getCopyright());	
+        assertTrue(info.getOriginal());	
+        assertTrue(!info.getPadding()); 	
         assertEquals(info.getFrequency() , 8000);	
-        assertEquals(info.getHeaderBitRate() , 32);	//18-winamp 8-encspot//////////////////////////////////////
-        assertEquals(info.getLayer_Numeric() , 3);		
-        assertEquals(info.getLengthInSeconds() , 3); //5-winamp 10-encspot/////////////////////////////////////
-        assertEquals(info.getMode() , "Single Channel");	
-        assertEquals(info.getVersion_Numeric() , 2.5d, 0D);
-        assertEquals(info.getNumberOfFrames() , 24); //149-winamp&encspot//////////////////////////////////////
-        assertEquals(info.getEmphasis() , "none");
-        assertTrue(!info.hasVariableBitRate());
+        assertEquals(info.getLayer() , 3);		
+        assertEquals(info.getRuntime() , 3); //5-winamp 10-encspot/////////////////////////////////////
+        assertEquals(info.getChannelModeS() , "Single Channel");	
+        assertEquals(info.getEmphasisS() , "none");
+        //        assertTrue(!info.hasVariableBitRate());
         //	assertEquals(info.getVBRHeader().getScale() , 95);
         //	assertNotNull(info.getVBRHeader().getTableOfContents());
         /*
           mpg2layI_128k_f54_22050hz_joint_CRCOrig_test33.mp3
         */	
         info  = newMP3Info(file[10]);
-        assertEquals(info.getFileSize(), 22572L);
-        assertEquals(info.getBitRate() , 128);		
-        assertTrue(!info.isCoprighted());	
-        assertTrue(info.isOriginal());	
-        assertTrue(info.isPadded()); 	
-        assertTrue(!info.isPrivate());	
-        assertTrue(info.isProtected());	
-        assertTrue(!info.isRiffWav());		
+        assertEquals(info.getSize(), 22572L);
+        assertEquals(info.getBitrate() , 128);		
+        assertTrue(!info.getCopyright());	
+        assertTrue(info.getOriginal());	
+        assertTrue(info.getPadding()); 	
         assertEquals(info.getFrequency() , 22050);	
-        assertEquals(info.getHeaderBitRate() , 128);
-        assertEquals(info.getLayer_Numeric() , 1);		
-        assertEquals(info.getLengthInSeconds() , 1);	
-        assertEquals(info.getMode() , "Joint Stereo");	
-        assertEquals(info.getVersion_Numeric() , 2.0d, 0D);
-        assertEquals(info.getNumberOfFrames() , 1); //54-winamp no encspot//////////////////////////////////////
-        assertEquals(info.getEmphasis() , "none");
-        assertTrue(!info.hasVariableBitRate());
+        assertEquals(info.getLayer() , 1);		
+        assertEquals(info.getRuntime() , 1);	
+        assertEquals(info.getChannelModeS() , "Joint Stereo");	
+        assertEquals(info.getEmphasisS() , "none");
+        //        assertTrue(!info.hasVariableBitRate());
 	
         /*
           mpg2layII_16k_f56_24000hz_joint_CRCOrigID3v1&2_test27.mp3
         */	
         info  = newMP3Info(file[11]);
-        assertEquals(info.getFileSize(), 4224L);
-        assertEquals(info.getBitRate() , 16);		
-        assertTrue(!info.isCoprighted());	
-        assertTrue(info.isOriginal());	
-        assertTrue(!info.isPadded()); 	
-        assertTrue(!info.isPrivate());	
-        assertTrue(info.isProtected());	
-        assertTrue(!info.isRiffWav());		
+        assertEquals(info.getSize(), 4224L);
+        assertEquals(info.getBitrate() , 16);		
+        assertTrue(!info.getCopyright());	
+        assertTrue(info.getOriginal());	
+        assertTrue(!info.getPadding()); 	
         assertEquals(info.getFrequency() , 24000);	
-        assertEquals(info.getHeaderBitRate() , 16);
-        assertEquals(info.getLayer_Numeric() , 2);		
-        assertEquals(info.getLengthInSeconds() , 2); //1-winamp 43-musicmatch(correct)//////////////////////////
-        assertEquals(info.getMode() , "Joint Stereo");	
-        assertEquals(info.getVersion_Numeric() , 2.0d, 0D);
-        assertEquals(info.getNumberOfFrames() , 44); // 56-winamp  no encspot//////////////////////////////////
-        assertEquals(info.getEmphasis() , "none");
-        assertTrue(!info.hasVariableBitRate());
+        assertEquals(info.getLayer() , 2);		
+        assertEquals(info.getRuntime() , 2); //1-winamp 43-musicmatch(correct)//////////////////////////
+        assertEquals(info.getChannelModeS() , "Joint Stereo");	
+        assertEquals(info.getEmphasisS() , "none");
+        //        assertTrue(!info.hasVariableBitRate());
 	
 	
         /*
           mpgPlus_210k-VBR_f1149_441hz_joint_protectedUnt_NOTm1lay-448k-f540-5015.mpc
         */	
         info  = newMP3Info(file[14]);
-        assertEquals(info.getFileSize(), 789752L);
-        assertEquals(info.getBitRate() , 448); //448-winamp	210-encspot
-        assertTrue(info.isCoprighted());	
-        assertTrue(!info.isOriginal());	
-        assertTrue(info.isPadded()); 	
-        assertTrue(!info.isPrivate());	
-        assertTrue(!info.isProtected());	
-        assertTrue(!info.isRiffWav());		
+        assertEquals(info.getSize(), 789752L);
+        assertEquals(info.getBitrate() , 448); //448-winamp	210-encspot
+        assertTrue(info.getCopyright());	
+        assertTrue(!info.getOriginal());	
+        assertTrue(info.getPadding()); 	
         assertEquals(info.getFrequency() , 44100);	
-        assertEquals(info.getHeaderBitRate() , 448); //448-winamp 210-encspot
-        assertEquals(info.getLayer_Numeric() , 1);		
-        assertEquals(info.getLengthInSeconds() , 14); //30-encspot
-        assertEquals(info.getMode() , "Joint Stereo");	
-        assertEquals(info.getVersion_Numeric() , 1.0d, 0D);
-        assertEquals(info.getNumberOfFrames() , 65); // 540-winamp 1149-encspot
-        assertEquals(info.getEmphasis() , "50/15 ms");
-        assertTrue(!info.hasVariableBitRate()); //encspot true
+        assertEquals(info.getLayer() , 1);		
+        assertEquals(info.getRuntime() , 14); //30-encspot
+        assertEquals(info.getChannelModeS() , "Joint Stereo");	
+        assertEquals(info.getEmphasisS() , "50/15 ms");
+        //        assertTrue(!info.hasVariableBitRate()); //encspot true
 	
         /*
           mpg4_golem160x90first120.avi
           MPEG 4 files contain a 'possible' MP3 header
 
           info  = newMP3Info(file[18] );
-          assertEquals(info.getFileSize(), 743933L);
-          assertEquals(info.getBitRate() , 40);		
-          assertTrue(!info.isCoprighted());	
-          assertTrue(!info.isOriginal());	
-          assertTrue(!info.isPadded()); 	
-          assertTrue(info.isPrivate());	
-          assertTrue(!info.isProtected());	
-          assertTrue(!info.isRiffWav());		
+          assertEquals(info.getSize(), 743933L);
+          assertEquals(info.getBitrate() , 40);		
+          assertTrue(!info.getCopyright());	
+          assertTrue(!info.getOriginal());	
+          assertTrue(!info.getPadding()); 	
           assertEquals(info.getFrequency() , 8000);	
-          assertEquals(info.getHeaderBitRate() , 40);
-          assertEquals(info.getLayer_Numeric() , 2);		
-          assertEquals(info.getLengthInSeconds() , 555);	
-          assertEquals(info.getMode() , "Joint Stereo");	
-          assertEquals(info.getVersion_Numeric() , 2.5d, 0D);
+          assertEquals(info.getLayer() , 2);		
+          assertEquals(info.getRuntime() , 555);	
+          assertEquals(info.getChannelModeS() , "Joint Stereo");	
           assertEquals(info.getNumberOfFrames() , 3857); //39626us - 39624 winamp
-          assertEquals(info.getEmphasis() , "none");
+          assertEquals(info.getEmphasisS() , "none");
           assertTrue(!info.hasVariableBitRate());
         */	
         /*
