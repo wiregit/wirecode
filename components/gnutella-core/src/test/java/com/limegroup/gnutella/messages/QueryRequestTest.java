b3e1d405218ca4fd67e9fc3de255b6e9028c06a4
sdaswani <sdaswani>
2003-09-16 21:52:19 +0000

    make sure that Queries accept URNs from the network.

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/messages/QueryRequestTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/messages/QueryRequestTest.java
@@ -280,7 +280,7 @@ public final class QueryRequestTest extends BaseTestCase {
     /**
      * Tests to make sure that network queries with URNS are not accepted.
      */
-	public void testThatNetworkQueriesWithURNsAreNotAccepted() throws Exception {
+	public void testThatNetworkQueriesWithURNsAreAccepted() throws Exception {
 		ByteArrayOutputStream[] URN_BYTES = 
 		    new ByteArrayOutputStream[HugeTestUtils.URNS.length];
 		for(int i=0; i<HugeTestUtils.URNS.length; i++) {
@@ -312,9 +312,8 @@ public final class QueryRequestTest extends BaseTestCase {
                     QueryRequest.createNetworkQuery(GUID.makeGuid(), ttl, hops, 
                                                     URN_BYTES[i].toByteArray(), 
                                                     Message.N_UNKNOWN);
-                fail("should not have accepted query: "+qr);
             } catch(BadPacketException e) {
-                // should be thrown for queries with URNS
+                fail("should have accepted query!!");
             }
         }
     }
