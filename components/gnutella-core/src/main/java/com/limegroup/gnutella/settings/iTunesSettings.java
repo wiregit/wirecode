85273c82534318d5b937c35d9a86892ca38be6de
sdaswani <sdaswani>
2003-09-29 20:04:43 +0000

    integration of Roger's iTunes improvements - hopefully this fixes the launching issues on OS X.1.x.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/settings/iTunesSettings.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/settings/iTunesSettings.java
@@ -13,4 +13,13 @@ public class iTunesSettings extends LimeProps {
      */
     public static BooleanSetting ITUNES_SUPPORT_ENABLED =
         FACTORY.createBooleanSetting("ITUNES_SUPPORT_ENABLED", true);
+
+      
+    /**
+     * Supported file types
+     */
+    public static StringArraySetting ITUNES_SUPPORTED_FILE_TYPES = 
+        FACTORY.createStringArraySetting("ITUNES_SUPPORTED_FILE_TYPES", 
+            new String[]{".mp3", ".aif", ".aiff", ".wav", ".mp2", ".mp4", 
+                        ".aac", ".mid", ".m4a", ".m4p", ".ogg"});
 }
