fee16516077beabef89c59890a8a26a8a6efe1ec
afisk <afisk>
2003-10-03 22:07:10 +0000

    slightly changed interface to return success or failure of writes and added a new method

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/connection/HeaderWriter.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/connection/HeaderWriter.java
@@ -2,13 +2,23 @@ package com.limegroup.gnutella.connection;
 
 import java.io.*;
 
+/**
+ * Interface for writing Gnutella connection headers.  Implementations may 
+ * write headers using any mode they like, including blocking and non-blocking
+ * network IO.
+ */
 public interface HeaderWriter {
 
     boolean write() throws IOException;
 
-    void writeHeader(String header) throws IOException;
+    boolean writeHeader(String header) throws IOException;
 
-    void closeHeaderWriting() throws IOException;
+    boolean closeHeaderWriting() throws IOException;
 
     void setWriteRegistered(boolean registered);
+
+    /**
+     * @return
+     */
+    boolean hasBufferedData();
 }
