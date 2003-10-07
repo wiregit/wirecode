9277ddf4c4c7d01208320f48426f4a938c6ce91f
afisk <afisk>
2003-10-07 18:16:42 +0000

    adding IDX format as per Jens-Uwe's suggestion.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/settings/SharingSettings.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/settings/SharingSettings.java
@@ -155,7 +155,7 @@ public class SharingSettings extends LimeProps {
 		"ra;ram;wma;wm;wmv;mp2v;mlv;mpa;mpv2;mid;midi;rmi;aifc;snd;"+
 		"mpg;mpeg;asf;qt;mov;avi;mpe;swf;dcr;gif;jpg;jpeg;jpe;png;tif;tiff;"+
 		"exe;zip;gz;gzip;hqx;tar;tgz;z;rmj;lqt;rar;ace;sit;smi;img;ogg;rm;"+
-		"bin;dmg;jve;nsv;med;mod;7z;iso;lwtp;pmf;m4a";
+		"bin;dmg;jve;nsv;med;mod;7z;iso;lwtp;pmf;m4a;idx";
     
     /**
 	 * The shared directories (an array of Files!).
