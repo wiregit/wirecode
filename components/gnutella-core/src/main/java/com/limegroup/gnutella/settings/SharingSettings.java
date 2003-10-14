184c931b81c5b898de30619e66546a68fbb61223
sberlin <sberlin>
2003-10-14 20:41:28 +0000

    added semicolon.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/settings/SharingSettings.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/settings/SharingSettings.java
@@ -156,7 +156,7 @@ public class SharingSettings extends LimeProps {
 		"mpg;mpeg;asf;qt;mov;avi;mpe;swf;dcr;gif;jpg;jpeg;jpe;png;tif;tiff;"+
 		"exe;zip;gz;gzip;hqx;tar;tgz;z;rmj;lqt;rar;ace;sit;smi;img;ogg;rm;"+
 		"bin;dmg;jve;nsv;med;mod;7z;iso;lwtp;pmf;m4a;idx;bz2;sea;pf;arc;arj;"+
-		"bz;tbz;mime;taz;ua;toast;lit"
+		"bz;tbz;mime;taz;ua;toast;lit";
     
     /**
 	 * The shared directories (an array of Files!).
