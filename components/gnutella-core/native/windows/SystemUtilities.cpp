67e02797c1a3a2c46899511c9d75d674e1d9f147
sberlin <sberlin>
2003-10-16 19:31:35 +0000

    windows side of retrieving idle time.

---
+++ components/gnutella-core/native/windows/SystemUtilities.cpp
@@ -0,0 +1,17 @@
+/**
+ * Handles various system utility functions that require native code.
+ */
+
+#include "com_limegroup_gnutella_util_SystemUtils.h"
+#include "windows.h"
+
+extern "C"
+JNIEXPORT jlong JNICALL Java_com_limegroup_gnutella_util_SystemUtils_idleTime
+  (JNIEnv *env, jclass clazz) {
+    
+    LASTINPUTINFO lpi;
+    lpi.cbSize = sizeof(lpi);
+    GetLastInputInfo(&lpi);
+    DWORD dwStart = GetTickCount();
+    return (jlong)(dwStart - lpi.dwTime);
+}
---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/util/SystemUtils.java
@@ -0,0 +1,29 @@
+package com.limegroup.gnutella.util;
+
+/**
+ * A collection of core-related systems utilities,
+ * most of which will require native code to do correctly.
+ */
+public class SystemUtils {
+    
+    static {
+        System.loadLibrary("SystemUtilities");
+    }
+    
+    private SystemUtils() {}
+    
+    
+    /**
+     * Retrieves the amount of time the system has been idle, where
+     * idle means the user has not pressed a key, mouse button, or moved
+     * the mouse.  The time returned is in milliseconds.
+     */
+    public static long getIdleTime() {
+        if(CommonUtils.isWindows2000orXP())
+            return idleTime();
+        else
+            return 0;
+    }
+    
+    private static final native long idleTime();
+}
