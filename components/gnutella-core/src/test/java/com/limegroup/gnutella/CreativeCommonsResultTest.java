8c835ca235a82b19a23f1edd9935e15f8c6c2fc6
sdaswani <sdaswani>
2003-10-24 15:38:41 +0000

    no real tests yet, just checking in framework.

---
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/CreativeCommonsResultTest.java
@@ -0,0 +1,233 @@
+package com.limegroup.gnutella;
+
+import com.limegroup.gnutella.messages.*;
+import com.limegroup.gnutella.messages.vendor.*;
+import com.limegroup.gnutella.settings.*;
+import com.limegroup.gnutella.*;
+import com.limegroup.gnutella.search.*;
+import com.limegroup.gnutella.handshaking.*;
+import com.limegroup.gnutella.routing.*;
+import com.limegroup.gnutella.security.*;
+import com.limegroup.gnutella.stubs.*;
+import com.limegroup.gnutella.util.*;
+import com.bitzi.util.*;
+
+import junit.framework.*;
+import java.util.Properties;
+import java.util.StringTokenizer;
+import com.sun.java.util.collections.*;
+import java.io.*;
+import java.net.*;
+
+/**
+ * Checks whether (multi)leaves avoid forwarding messages to ultrapeers, do
+ * redirects properly, etc.  The test includes a leaf attached to 3 
+ * Ultrapeers.
+ */
+public class CreativeCommonsResultTest 
+    extends com.limegroup.gnutella.util.BaseTestCase {
+    private static final int PORT=6669;
+    private static final int TIMEOUT=3000;
+    private static final byte[] ultrapeerIP=
+        new byte[] {(byte)18, (byte)239, (byte)0, (byte)144};
+    private static final byte[] oldIP=
+        new byte[] {(byte)111, (byte)22, (byte)33, (byte)44};
+
+    private static Connection[] testUPs = new Connection[4];
+    private static RouterService rs;
+
+    private static MyActivityCallback callback;
+
+    /**
+     * Ultrapeer 1 UDP connection.
+     */
+    private static DatagramSocket UDP_ACCESS;
+
+    public CreativeCommonsResultTest(String name) {
+        super(name);
+    }
+    
+    public static Test suite() {
+        return buildTestSuite(CreativeCommonsResultTest.class);
+    }    
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+    
+    private static void doSettings() {
+        ConnectionSettings.PORT.setValue(PORT);
+		ConnectionSettings.CONNECT_ON_STARTUP.setValue(false);
+		UltrapeerSettings.EVER_ULTRAPEER_CAPABLE.setValue(false);
+		UltrapeerSettings.DISABLE_ULTRAPEER_MODE.setValue(true);
+		UltrapeerSettings.FORCE_ULTRAPEER_MODE.setValue(false);
+		ConnectionSettings.NUM_CONNECTIONS.setValue(0);
+		ConnectionSettings.LOCAL_IS_PRIVATE.setValue(false);
+		SharingSettings.EXTENSIONS_TO_SHARE.setValue("txt;");
+        // get the resource file for com/limegroup/gnutella
+        File cc1 = 
+            CommonUtils.getResourceFile("com/limegroup/gnutella/mp3/ccverifytest0.mp3");
+        File cc2 = 
+            CommonUtils.getResourceFile("com/limegroup/gnutella/mp3/ccverifytest1.mp3");
+        assertTrue(cc1.exists());
+        assertTrue(cc2.exists());
+        // now move them to the share dir
+        CommonUtils.copy(cc1, new File(_sharedDir, "ccverifytest0.mp3"));
+        CommonUtils.copy(cc2, new File(_sharedDir, "ccverifytest1.mp3"));
+        // make sure results get through
+        SearchSettings.MINIMUM_SEARCH_QUALITY.setValue(-2);
+    }        
+    
+    public static void globalSetUp() throws Exception {
+        doSettings();
+
+        callback=new MyActivityCallback();
+        rs=new RouterService(callback);
+        assertEquals("unexpected port",
+            PORT, ConnectionSettings.PORT.getValue());
+        rs.start();
+        rs.clearHostCatcher();
+        rs.connect();
+        Thread.sleep(1000);
+        assertEquals("unexpected port",
+            PORT, ConnectionSettings.PORT.getValue());
+    }        
+    
+    public void setUp() throws Exception  {
+        doSettings();
+    }
+     
+     private static Connection connect(RouterService rs, int port, 
+                                       boolean ultrapeer) 
+         throws Exception {
+         ServerSocket ss=new ServerSocket(port);
+         rs.connectToHostAsynchronously("127.0.0.1", port);
+         Socket socket = ss.accept();
+         ss.close();
+         
+         socket.setSoTimeout(3000);
+         InputStream in=socket.getInputStream();
+         String word=readWord(in);
+         if (! word.equals("GNUTELLA"))
+             throw new IOException("Bad word: "+word);
+         
+         HandshakeResponder responder;
+         if (ultrapeer) {
+             responder = new UltrapeerResponder();
+         } else {
+             responder = new OldResponder();
+         }
+         Connection con = new Connection(socket, responder);
+         con.initialize();
+         replyToPing(con, ultrapeer);
+         return con;
+     }
+     
+     /**
+      * Acceptor.readWord
+      *
+      * @modifies sock
+      * @effects Returns the first word (i.e., no whitespace) of less
+      *  than 8 characters read from sock, or throws IOException if none
+      *  found.
+      */
+     private static String readWord(InputStream sock) throws IOException {
+         final int N=9;  //number of characters to look at
+         char[] buf=new char[N];
+         for (int i=0 ; i<N ; i++) {
+             int got=sock.read();
+             if (got==-1)  //EOF
+                 throw new IOException();
+             if ((char)got==' ') { //got word.  Exclude space.
+                 return new String(buf,0,i);
+             }
+             buf[i]=(char)got;
+         }
+         throw new IOException();
+     }
+
+     private static void replyToPing(Connection c, boolean ultrapeer) 
+             throws Exception {
+        // respond to a ping iff one is given.
+        Message m = null;
+        byte[] guid;
+        try {
+            while (!(m instanceof PingRequest)) {
+                m = c.receive(500);
+            }
+            guid = ((PingRequest)m).getGUID();            
+        } catch(InterruptedIOException iioe) {
+            //nothing's coming, send a fake pong anyway.
+            guid = new GUID().bytes();
+        }
+        
+        Socket socket = (Socket)PrivilegedAccessor.getValue(c, "_socket");
+        PingReply reply = 
+        PingReply.createExternal(guid, (byte)7,
+                                 socket.getLocalPort(), 
+                                 ultrapeer ? ultrapeerIP : oldIP,
+                                 ultrapeer);
+        reply.hop();
+        c.send(reply);
+        c.flush();
+     }
+
+    ///////////////////////// Actual Tests ////////////////////////////
+
+    public void testCCResultsNormalSearch() {
+        
+    }
+
+
+    
+    //////////////////////////////////////////////////////////////////
+
+    private void drainAll() throws Exception {
+        drainAll(testUPs);
+    }
+    
+    private static byte[] myIP() {
+        return new byte[] { (byte)127, (byte)0, 0, 1 };
+    }
+
+    private static final boolean DEBUG = false;
+    
+    static void debug(String message) {
+        if(DEBUG) 
+            System.out.println(message);
+    }
+
+    private static class UltrapeerResponder implements HandshakeResponder {
+        public HandshakeResponse respond(HandshakeResponse response, 
+                boolean outgoing) throws IOException {
+            Properties props = new UltrapeerHeaders("127.0.0.1"); 
+            props.put(HeaderNames.X_DEGREE, "42");           
+            return HandshakeResponse.createResponse(props);
+        }
+    }
+
+
+    private static class OldResponder implements HandshakeResponder {
+        public HandshakeResponse respond(HandshakeResponse response, 
+                boolean outgoing) throws IOException {
+            Properties props=new Properties();
+            return HandshakeResponse.createResponse(props);
+        }
+    }
+
+    public static class MyActivityCallback extends ActivityCallbackStub {
+        private RemoteFileDesc rfd = null;
+        public RemoteFileDesc getRFD() {
+            return rfd;
+        }
+
+        public void handleQueryResult(RemoteFileDesc rfd,
+                                      HostData data,
+                                      Set locs) {
+            this.rfd = rfd;
+        }
+    }
+
+
+}
+
