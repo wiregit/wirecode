180344fb6d402ac366c82ab433a0cea7a35e354f
afisk <afisk>
2003-11-06 22:52:17 +0000

    Lots of changes and refactorings to implement reading residual data left over from handshaking.  The message creation code is broken up more to allow as much code as possible to be shared between the implementation for creating messages from ByteBuffers (the new implementation) and the implementation for creating messages from channels.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/connection/NIOMessageReader.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/connection/NIOMessageReader.java
@@ -7,7 +7,7 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
 
-import com.limegroup.gnutella.*;
+import com.limegroup.gnutella.Assert;
 import com.limegroup.gnutella.messages.BadPacketException;
 import com.limegroup.gnutella.messages.Message;
 import com.limegroup.gnutella.settings.MessageSettings;
@@ -25,8 +25,8 @@ import com.limegroup.gnutella.statistics.*;
  */
 public class NIOMessageReader extends AbstractMessageReader {
 	 
-	
-	/**
+
+    /**
 	 * Constant for the <tt>ByteBuffer</tt> for reading headers.  Note that 
      * this is fixed size because all Gnutella headers have the same size.
 	 */
@@ -44,22 +44,77 @@ public class NIOMessageReader extends AbstractMessageReader {
      * instances for each connection.
      * 
      * @param conn the <tt>Connection</tt> this reader reads for
+     * @param handshaker the <tt>Handshaker</tt> instance that can contain 
+     *  extra, unprocessed data read in after all headers were read -- message
+     *  data that should be processed right away
      * @return a new <tt>MessageReader</tt> instance
      */
-    public static NIOMessageReader createReader(Connection conn) {
-        return new NIOMessageReader(conn);
+    public static NIOMessageReader createReader(Connection conn, 
+        Handshaker handshaker) throws IOException {
+        return new NIOMessageReader(conn, handshaker);
     }
     
 	/**
 	 * Ensure that this class cannot be constructed by another class.
      *
      * @param conn the <tt>Connection</tt> this reader reads for
+     * @param handshaker the <tt>Handshaker</tt> instance that can contain 
+     *  extra, unprocessed data read in after all headers were read -- message
+     *  data that should be processed right away
 	 */
-	protected NIOMessageReader(Connection conn) {
+	protected NIOMessageReader(Connection conn, Handshaker handshaker) 
+        throws IOException {
         super(conn);
         HEADER.order(java.nio.ByteOrder.LITTLE_ENDIAN);
+        ByteBuffer buffer = handshaker.getRemainingData();
+
+        /*
+        // read all data in the buffer
+        while(buffer.hasRemaining()) {
+            Message msg = createMessage(HEADER, PAYLOAD, conn, buffer);
+            if(msg != null) { 
+                System.out.println("NIOMessageReader::routing left over message!!");
+                System.out.println(msg);
+                routeMessage(msg);
+            } else {
+                Assert.that(!buffer.hasRemaining(), 
+                    "should not be any data in the buffer");
+            }
+        }
+        */
     }
-		
+	
+    /**
+     * Specialized method for creating a message from a pre-read 
+     * <tt>ByteBuffer</tt>.  This is particularly used for handling message 
+     * data that was read after a handshake but that was not processed.  This
+     * often happens because we just read chunks of data in from the channel
+     * during handshaking, and so can read well beyond the end of the 
+     * handshaking headers and into the message data.
+     * 
+     * @param header the <tt>ByteBuffer</tt> that should be filled with message
+     *  header data
+     * @param payload the <tt>ByteBuffer</tt> that should be filled with 
+     *  message payload data
+     * @param conn the <tt>Connection</tt> that read this message
+     * @param buffer the <tt>ByteBuffer</tt> containing the unprocessed data
+     * @return a new <tt>Message</tt> instance from the specified payload data,
+     *  or <tt>null</tt> if only an incomplete message was read
+     * @throws IOException if there was an IO error reading the data
+     * @throws BadPacketException if the data did not match the format of any
+     *  known messages
+     */
+    private static Message createMessage(ByteBuffer header, ByteBuffer payload,
+        Connection conn, ByteBuffer buffer) throws IOException {
+
+        if(!readHeader(header, buffer)) {
+            return null;
+        }
+        
+        handlePayloadLimit(header, payload);
+
+        return readPayload(header, payload, conn, buffer);
+    }	
 	
 	/**
 	 * Creates a new <tt>Message</tt> from incoming data over TCP.
@@ -108,6 +163,44 @@ public class NIOMessageReader extends AbstractMessageReader {
         return readPayload(key, network);
     }
 
+    
+    private static boolean readHeader(ByteBuffer header, ByteBuffer buffer) {
+        if(buffer.remaining() < HEADER_SIZE) {
+            header.put(buffer);
+            return false;           
+        }
+        
+        while(header.hasRemaining()) {
+            header.put(buffer.get());
+        }
+        return true;
+    }
+
+    private static Message readPayload(ByteBuffer header, ByteBuffer payload, 
+        Connection conn, ByteBuffer buffer) {
+        // If the buffer does not contain the full payload, just copy
+        // what it has and wait to read more data from the channel.
+        if(buffer.remaining() < payload.limit()) {
+            payload.put(buffer);
+            return null;
+        }
+        
+        // Otherwise, there's a complete message in the buffer, so
+        // create it.
+        while(payload.hasRemaining()) {
+            payload.put(buffer.get());
+        } 
+           
+        try {
+            return createMessage(header, payload, conn, Message.N_TCP);
+        } catch (BadPacketException e) {
+            // Ignore this packet and hope to read valid packets on subsequent
+            // reads.
+            MessageReadErrorStat.BAD_PACKET_EXCEPTIONS.incrementStat();
+            return null;
+        } 
+    }
+
     /**
      * Reads the message header into the HEADER ByteBuffer.
      * 
@@ -122,10 +215,8 @@ public class NIOMessageReader extends AbstractMessageReader {
         
         if(channel.read(HEADER) < 0) {
             HEADER.clear();
-            //PAYLOAD.clear();
             MessageReadErrorStat.CONNECTION_CLOSED_READING_HEADER.
                 incrementStat();
-            //Thread.dumpStack();
             throw new IOException("end of stream reading header");
         } else if(HEADER.hasRemaining()) {
             // if we still haven't read all of the header, return
@@ -151,28 +242,12 @@ public class NIOMessageReader extends AbstractMessageReader {
      */   
     private Message readPayload(SelectionKey key, int network) 
         throws IOException, BadPacketException {
-        SocketChannel channel = (SocketChannel)key.channel();
-        // if we've made it this far, the entire header has been
+        
+        // If we've made it this far, the entire header has been
         // successfully read, so read the payload
+        handlePayloadLimit(HEADER, PAYLOAD);
 		
-		int length = HEADER.getInt(LENGTH_OFFSET);  //little endian
-        
-       
-		// If the length is hopelessly off (this includes lengths > than 2^31
-		// bytes, throw an irrecoverable exception to cause this connection
-		// to be closed.  Note we don't bother checking other fields here.
-		if (length<0 || 
-			length>MessageSettings.MAX_LENGTH.getValue())  {
-            HEADER.clear();
-
-            MessageReadErrorStat.BAD_MESSAGE_LENGTH.incrementStat();
-			throw new IOException("unreasonable message length");
-		}
-        
-        // now set the read limit on the payload buffer so we'll only
-        // read to the end of the message
-        PAYLOAD.limit(length); 
-		
+        SocketChannel channel = (SocketChannel)key.channel();
         // read in the payload
 		if (channel.read(PAYLOAD) < 0) {
             // free memory as soon as possible
@@ -183,24 +258,51 @@ public class NIOMessageReader extends AbstractMessageReader {
 			throw new IOException("connection closed reading payload");
 		}
 
+        return handleReadPayload(HEADER, PAYLOAD, (Connection)key.attachment(), 
+            network); 
+    }
+    
+    private static void handlePayloadLimit(ByteBuffer header, 
+        ByteBuffer payload) throws IOException {
+        // If we've made it this far, the entire header has been
+        // successfully read, so read the payload
+    
+        int length = header.getInt(LENGTH_OFFSET);  //little endian
+    
+   
+        // If the length is hopelessly off (this includes lengths > than 2^31
+        // bytes, throw an irrecoverable exception to cause this connection
+        // to be closed.  Note we don't bother checking other fields here.
+        if (length<0 || 
+            length>MessageSettings.MAX_LENGTH.getValue())  {
+            header.clear();
+
+            MessageReadErrorStat.BAD_MESSAGE_LENGTH.incrementStat();
+            throw new IOException("unreasonable message length");
+        }
+    
+        // now set the read limit on the payload buffer so we'll only
+        // read to the end of the message
+        payload.limit(length);             
+    }
+    
+    private static Message handleReadPayload(ByteBuffer header, 
+        ByteBuffer payload, Connection conn, int network) throws IOException, 
+        BadPacketException {
         // if there's still more to read, return to read it on the next pass
-		if(PAYLOAD.hasRemaining()) {
-			return null;
-		}
-			
-		try {
-            return createMessage(HEADER, PAYLOAD, 
-                (Connection)key.attachment(), network);
+        if(payload.hasRemaining()) {
+            return null;
+        }
+            
+        try {
+            return createMessage(header, payload, conn, network);
         } finally {
             // either the message was returned, or it was corrupt in some way,
             // so clear our buffers for new messages
-            HEADER.clear();
-            PAYLOAD.clear();
-        }
+            header.clear();
+            payload.clear();
+        }        
     }
-    
-
-    
     /**
      * Creates a new <tt>Message</tt> instance from the specified header,
      * payload, connection, and network.
@@ -216,8 +318,8 @@ public class NIOMessageReader extends AbstractMessageReader {
      * @throws BadPacketException if the data does not match the expected 
      *  message format
      */  
-    private Message createMessage(ByteBuffer header, ByteBuffer payloadBuffer, 
-        Connection conn, int network) 
+    private static Message createMessage(ByteBuffer header, 
+        ByteBuffer payloadBuffer, Connection conn, int network) 
         throws BadPacketException {
         
         // make sure the limit on the payload buffer is set correctly    
@@ -283,31 +385,6 @@ public class NIOMessageReader extends AbstractMessageReader {
             throw new IllegalStateException("this method should not be called");
         }    
         return null;
-    }
-
-    /* (non-Javadoc)
-     * @see com.limegroup.gnutella.connection.MessageReader#handleMessage(java.nio.channels.SelectionKey)
-     */
-    public void handleMessage(SelectionKey key) {
-        try {
-            Message msg = createMessageFromTCP(key);
-                    
-            if(msg == null) {
-                // the message was not read completely -- we'll get
-                // another read event on the channel and keep reading
-                return;
-            }
-    
-            routeMessage(msg);
-        } catch (BadPacketException e) {
-            MessageReadErrorStat.BAD_PACKET_EXCEPTIONS.incrementStat();
-        } catch (IOException e) {
-            key.cancel();
-            // remove the connection if we got an IOException
-            RouterService.removeConnection((Connection)key.attachment());
-            MessageReadErrorStat.IO_EXCEPTIONS.incrementStat();
-        }
-        
     }
 }
 
