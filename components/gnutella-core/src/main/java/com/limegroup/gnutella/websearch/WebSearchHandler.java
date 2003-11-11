da6b5cc1fac06ba26d5e2fe42c33e3a2db7cd0f3
sthadani <sthadani>
2003-11-11 18:55:33 +0000

    1. Added code to make a Http get request to the server with the query with the correct format. 2. changed a parameter to tell the server not to send us the response without encoding specification.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/websearch/WebSearchHandler.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/websearch/WebSearchHandler.java
@@ -2,6 +2,10 @@ package com.limegroup.gnutella.websearch;
 
 import com.limegroup.gnutella.util.*;
 import com.limegroup.gnutella.*;
+import com.limegroup.gnutella.http.*;
+import org.apache.commons.httpclient.*;
+import org.apache.commons.httpclient.methods.*;
+import java.io.*;
 
 
 public class WebSearchHandler {
@@ -17,7 +21,7 @@ public class WebSearchHandler {
 
     private static final String FIL = "&fl=0";//no filtering
     
-    private static final String ENC = "&df=1";
+    private static final String ENC = "&df=0";
 
 
     private static final String IP = "&ip_addr=";
@@ -26,11 +30,25 @@ public class WebSearchHandler {
     public void search(String searchString, int searchIndex) {
         //TODO1: Find a way to get the base parameter from the GUI or store it
         //somewhere else
+        //1. Create the search url
         String ip = NetworkUtils.ip2string(RouterService.getAddress());
         String urlStr = HOST+BASE+0+SRCH+searchString+AFF_ID+FIL+ENC+IP+ip;
-        //TODO1: make a http client connection with this url and parse the
-        //results. I will need to do this after I recreate this branch
-        System.out.println(urlStr);
+        //2. Make a HttpClient and send the GET Request
+        GetMethod getMethod = new GetMethod(urlStr);
+        getMethod.addRequestHeader("Cache-Control", "no-cache");
+        getMethod.addRequestHeader("User-Agent", CommonUtils.getHttpServer());
+        HttpClient client = HttpClientManager.getNewClient();
+        byte[] response = null;
+        //TODO1: remove these asserts, they are really for debugging for now. 
+        try {
+            client.executeMethod(getMethod);
+            response = getMethod.getResponseBody();
+            if(response == null)
+                Assert.that(false, "server is not responding correctly");
+        } catch (IOException iox) {
+            Assert.that(false, "server is not responding correctly");
+        }
+        System.out.println("Sumeet: got xml response:\n"+new String(response));
     }
 
 }
