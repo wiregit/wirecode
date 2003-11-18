9c82715f34be4780c122e23125ebfbf68ddb9196
ynaito <ynaito>
2003-11-18 23:13:07 +0000

    added test for the QRP table that is now kept by FileManager

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/FileManagerTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/FileManagerTest.java
@@ -12,6 +12,7 @@ import com.limegroup.gnutella.messages.QueryRequest;
 import com.limegroup.gnutella.util.CommonUtils;
 import com.limegroup.gnutella.downloader.VerifyingFile;
 import com.limegroup.gnutella.stubs.SimpleFileManager;
+import com.limegroup.gnutella.routing.QueryRouteTable;
 import com.sun.java.util.collections.*;
 import java.io.*;
 import java.net.*;
@@ -526,7 +527,95 @@ public class FileManagerTest extends com.limegroup.gnutella.util.BaseTestCase {
 		}
 		assertTrue("wasn't able to find any unique classes to check against.", checked);
     }	
-	
+
+    
+    /**
+     * tests for the QRP thats kept by the FileManager 
+     * tests that the function getQRP of FileManager returns
+     * the correct table after addition, removal, and renaming
+     * of shared files.
+     */
+    public void testFileManagerQRP() throws Exception {
+        f1 = createNewNamedTestFile(10, "hello");
+        f2 = createNewNamedTestFile(10, "\u5bae\u672c\u6b66\u8535\u69d8");
+        f3 = createNewNamedTestFile(10, "\u00e2cc\u00e8nts");
+        waitForLoad();
+
+        //get the QRT from the filemanager
+        QueryRouteTable qrt = fman.getQRT();
+
+        //test that QRT doesn't contain random keyword
+        QueryRequest qr = QueryRequest.createQuery("asdfasdf");
+        assertFalse("query should not be in qrt",
+                   qrt.contains(qr));
+
+        //test that the qrt contains the three files 
+        qr = get_qr(f1);
+        assertTrue("query not in QRT", qrt.contains(qr));
+        
+        qr = get_qr(f2);
+        assertTrue("query not in QRT", qrt.contains(qr));
+
+        qr = get_qr(f3);
+        assertTrue("query not in QRT", qrt.contains(qr));
+
+
+        //now remove one of the files
+        fman.removeFileIfShared(f3);
+        
+        qrt = fman.getQRT();
+        
+        //make sure the removed file is no longer in qrt
+        assertFalse("query should not be in qrt",
+                   qrt.contains(qr));
+        
+        //just check that the one of the other files is still 
+        //in the qrt
+        qr = get_qr(f2);
+        assertTrue("query not in QRT", qrt.contains(qr));
+
+        
+
+        //test rename
+        f4 = createNewNamedTestFile(10, "miyamoto_musashi_desu");
+        
+        //check that this file doesn't hit
+        qr = get_qr(f4);
+        assertFalse("query should not be in qrt", qrt.contains(qr));
+
+        //now rename one of the files
+        fman.renameFileIfShared(f2, f4);
+        qrt = fman.getQRT();
+        
+        //check hit with new name
+        qr = get_qr(f4);
+        assertTrue("query not in qrt", qrt.contains(qr));
+        
+        //check old name
+        qr = get_qr(f2);
+        assertFalse("query should not be in qrt", qrt.contains(qr));
+    }
+
+    //helper method to create queryrequest with I18N
+    private QueryRequest get_qr(File f) {
+        return 
+            QueryRequest.createQuery
+            (I18NConvert.instance().getNorm(f.getName()));
+    }
+                                      
+
+    File createNewNamedTestFile(int size, String name) throws Exception {
+		File file = File.createTempFile(name, 
+                                        "." + EXTENSION , _sharedDir);
+		file.deleteOnExit();
+        OutputStream out=new FileOutputStream(file);
+        out.write(new byte[size]);
+        out.flush();
+        out.close();
+        //Needed for comparisons between "C:\Progra~1" and "C:\Program Files".			
+        return FileUtils.getCanonicalFile(file);
+    }
+
 	
 	private void addFilesToLibrary() throws Exception {
 		String dirString = "com/limegroup/gnutella";
@@ -631,3 +720,4 @@ public class FileManagerTest extends com.limegroup.gnutella.util.BaseTestCase {
         }
     }    
 }
+
