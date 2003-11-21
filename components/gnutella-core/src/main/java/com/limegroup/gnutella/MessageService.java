2dc8faa1595e30b6c47f9bd5cd6cdec0d4f0b95d
sberlin <sberlin>
2003-11-21 22:43:45 +0000

    updated to allow optional BooleanSetting parameters to possibly include a 'do not show this again' checkbox.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/MessageService.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/MessageService.java
@@ -1,5 +1,7 @@
 package com.limegroup.gnutella;
 
+import com.limegroup.gnutella.settings.BooleanSetting;
+
 /**
  * Implementation of the <tt>MessageService</tt> interface for displaying 
  * messages to the user.
@@ -39,6 +41,18 @@ public class MessageService {
 
     /**
      * Shows a locale-specific message to the user using the specified key to
+     * look up the message in the resource bundles if the BooleanSetting
+     * indicates to do so.
+     * 
+     * @param messageKey the key for looking up the message to display in the
+     *  resource bundles
+     */
+    public static void showError(String messageKey, BooleanSetting ignore) {
+        _callback.showError(messageKey, ignore);
+    }
+
+    /**
+     * Shows a locale-specific message to the user using the specified key to
      * look up the message in the resource bundles.  Also appends a second,
      * non-locale-specific string to this message, such as a file name.
      * 
@@ -53,6 +67,23 @@ public class MessageService {
 
     /**
      * Shows a locale-specific message to the user using the specified key to
+     * look up the message in the resource bundles if the BooleanSetting
+     * indicates to do so.  Also appends a second,
+     * non-locale-specific string to this message, such as a file name.
+     * 
+     * @param messageKey the key for looking up the message to display in the
+     *  resource bundles
+     * @param message a non-locale-specific message that will be appended as-is
+     *  to the message displayed to the user, such as a file name
+     */
+    public static void showError(String messageKey,
+                                 String message,
+                                 BooleanSetting ignore) {
+        _callback.showError(messageKey, message, ignore);
+    }
+
+    /**
+     * Shows a locale-specific message to the user using the specified key to
      * look up the message in the resource bundles.
      * 
      * @param messageKey the key for looking up the message to display in the
@@ -60,6 +91,18 @@ public class MessageService {
      */
     public static void showMessage(String messageKey) {
         _callback.showMessage(messageKey);
+    }
+
+    /**
+     * Shows a locale-specific message to the user using the specified key to
+     * look up the message in the resource bundles if the BooleanSetting
+     * indicates to do so.
+     * 
+     * @param messageKey the key for looking up the message to display in the
+     *  resource bundles
+     */
+    public static void showMessage(String messageKey, BooleanSetting ignore) {
+        _callback.showMessage(messageKey, ignore);
     }
     
     /**
@@ -72,6 +115,11 @@ public class MessageService {
             System.out.println("error key: "+messageKey);
         }
 
+        // Inherit doc domment.        
+        public void showError(String messageKey, BooleanSetting ignore) {
+            System.out.println("error key: "+messageKey);
+        }
+
         // Inherit doc comment.
         public void showError(String messageKey, String message) {
             System.out.println("error key: "+messageKey+" extra message: "+
@@ -79,7 +127,20 @@ public class MessageService {
         }
 
         // Inherit doc comment.
+        public void showError(String messageKey,
+                              String message,
+                              BooleanSetting ignore) {
+            System.out.println("error key: "+messageKey+" extra message: "+
+                message);
+        }
+
+        // Inherit doc comment.
         public void showMessage(String messageKey) {
+            System.out.println("message key: "+messageKey); 
+        }
+
+        // Inherit doc comment.
+        public void showMessage(String messageKey, BooleanSetting ignore) {
             System.out.println("message key: "+messageKey); 
         }
         
