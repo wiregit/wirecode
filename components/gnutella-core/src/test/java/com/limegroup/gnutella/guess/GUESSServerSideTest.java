ac7933096db1c00d742f03d64e8a22fff36ab6c4
afisk <afisk>
2003-12-11 21:30:46 +0000

    Removed unused imports, unused methods, and unused local variables.

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/guess/GUESSServerSideTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/guess/GUESSServerSideTest.java
@@ -5,12 +5,12 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 import com.limegroup.gnutella.messages.*;
+import com.limegroup.gnutella.util.BaseTestCase;
 import com.limegroup.gnutella.*;
-import com.limegroup.gnutella.stubs.*;
 
 /** Tests the Server Side of GUESS....
  */ 
-public class GUESSServerSideTest extends com.limegroup.gnutella.util.BaseTestCase {
+public class GUESSServerSideTest extends BaseTestCase {
 
 	/**
 	 * Constant for the size of UDP messages to accept -- dependent upon
@@ -20,16 +20,8 @@ public class GUESSServerSideTest extends com.limegroup.gnutella.util.BaseTestCas
 
 	private final int SOCKET_TIMEOUT = 5*1000; // 5 second wait for a message
 
-    private boolean _shouldRun = true;
-    private boolean shouldRun() {
-        return _shouldRun;
-    }
-
-    private Backend _backend;
     private DatagramSocket _socket = null;
 
-    // produces should add(), consumers should firstElement()
-    private Vector _messages = new Vector();
     
     public GUESSServerSideTest(String name) {
         super(name);
@@ -82,7 +74,7 @@ public class GUESSServerSideTest extends com.limegroup.gnutella.util.BaseTestCas
         // first try to get a QueryKey....
         PingRequest pr = PingRequest.createQueryKeyRequest();
         QueryKey qkToUse = null;
-        send(pr, address, Backend.PORT);
+        send(pr, address, Backend.BACKEND_PORT);
         //try {
             PingReply pRep = (PingReply) receive();
             assertNotNull("query key should not be null", pRep.getQueryKey());
@@ -96,7 +88,7 @@ public class GUESSServerSideTest extends com.limegroup.gnutella.util.BaseTestCas
 
         // send a normal ping, should get a pong....
         pr = new PingRequest((byte)1);
-        send(pr, address, Backend.PORT);
+        send(pr, address, Backend.BACKEND_PORT);
         //try {
             pRep = (PingReply) receive();
             assertNull("query key should be null", pRep.getQueryKey());
@@ -118,7 +110,7 @@ public class GUESSServerSideTest extends com.limegroup.gnutella.util.BaseTestCas
 		//  new QueryRequest(GUID.makeGuid(), (byte) 1, 0, "susheel", null, 
 		//                   false, null, null, 
 		//                   QueryKey.getQueryKey(fakeQueryKey, true), false);
-        send(crapQuery, address, Backend.PORT);
+        send(crapQuery, address, Backend.BACKEND_PORT);
         try {
             receive();
             //cleanUp();
@@ -138,7 +130,7 @@ public class GUESSServerSideTest extends com.limegroup.gnutella.util.BaseTestCas
 			QueryRequest.createQueryKeyQuery("susheel", qkToUse);
 
 		byte[] guid = goodQuery.getGUID();
-        send(goodQuery, address, Backend.PORT);
+        send(goodQuery, address, Backend.BACKEND_PORT);
         //try {
             pRep = (PingReply) receive();
             assertTrue("guids should be equal",
@@ -174,7 +166,6 @@ public class GUESSServerSideTest extends com.limegroup.gnutella.util.BaseTestCas
                                                      BUFFER_SIZE);
         _socket.receive(datagram);
         byte[] data = datagram.getData();
-        int length = datagram.getLength();
         // construct a message out of it...
         InputStream in = new ByteArrayInputStream(data);
         Message message = Message.read(in);		
