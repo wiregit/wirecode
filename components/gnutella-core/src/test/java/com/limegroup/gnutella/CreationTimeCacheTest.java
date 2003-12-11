478a2b95a430d10ddbc68cd3edc3614b2187413b
sdaswani <sdaswani>
2003-12-11 17:13:58 +0000

    necessary change after last commit

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/CreationTimeCacheTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/CreationTimeCacheTest.java
@@ -496,7 +496,7 @@ public class CreationTimeCacheTest
             toExclude = null;
         }
 
-        public FileDesc getFileDescForUrn(URN urn){
+        public FileDesc getAnyFileDescForUrn(URN urn){
             if (fd == null) {
                 File cacheFile = new File(_settingsDir, CREATION_CACHE_FILE);
                 Set urnSet = new HashSet();
