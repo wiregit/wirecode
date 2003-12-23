753a77ef6c87c6970360c777d7210728da52be96
sberlin <sberlin>
2003-12-23 23:44:16 +0000

    removed invalid test.

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/downloader/MagnetDownloaderTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/downloader/MagnetDownloaderTest.java
@@ -23,36 +23,6 @@ public class MagnetDownloaderTest extends BaseTestCase {
         super(name);
     }
     
-    /**
-     * Test to make sure that URL encoding is working properly.
-     */ 
-    public void testEncoding() throws Exception {
-        
-        // URL based on a real error reported in the field.  The space causes
-        // the encoding problem, so this could just as easily be:
-        // "http://somehost.com/somefile p", although the example we use throws
-        // in more tricky characters.
-        String testUrl = "http://ftp.crihan.fr/mirrors/ftp.redhat.com/" +
-            "redhat/linux/9/en/iso/i386/shrike-i386-disc1.iso6-d p?";
-    
-        Method createRFD = 
-            PrivilegedAccessor.getMethod(
-                MagnetDownloader.class, "createRemoteFileDesc", 
-                    new Class[] {String.class, String.class, URN.class});
-        
-        String fileName = "file.txt";
-        URN urn = HugeTestUtils.SHA1;
-        
-        // Now, the following RFD creation would fail in the HTTP client code
-        // if we were not properly encoding the URL.
-        createRFD.invoke(MagnetDownloader.class, 
-            new Object[]{testUrl, fileName, urn});
-        
-        // Try one without the space just as a sanity check.
-        testUrl = "http://ftp.crihan.fr/mirrors/ftp.redhat.com/" +
-            "redhat/linux/9/en/iso/i386/shrike-i386-disc1.iso6-d";
-        createRFD.invoke(MagnetDownloader.class, 
-            new Object[]{testUrl, fileName, urn});
-    }
-
+    // TODO add some tests.
+    public void testNothing() {}
 }
