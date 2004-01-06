2904968f14a276710f12b285b270b8a7f7e5285e
sdaswani <sdaswani>
2004-01-06 18:42:18 +0000

    uses new ClientSideTestCase class.

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/ClientSideLeafGuidanceTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/ClientSideLeafGuidanceTest.java
@@ -18,16 +18,7 @@ import java.net.*;
  * redirects properly, etc.  The test includes a leaf attached to 3 
  * Ultrapeers.
  */
-public class ClientSideLeafGuidanceTest extends BaseTestCase {
-    private static final int TEST_PORT = 6669;
-    private static final int TIMEOUT=2000;
-    private static final byte[] ultrapeerIP=
-        new byte[] {(byte)18, (byte)239, (byte)0, (byte)144};
-    private static final byte[] oldIP=
-        new byte[] {(byte)111, (byte)22, (byte)33, (byte)44};
-
-    private static Connection[] testUPs = new Connection[4];
-    private static RouterService rs;
+public class ClientSideLeafGuidanceTest extends ClientSideTestCase {
 
     private static MyActivityCallback callback;
 
@@ -46,148 +37,12 @@ public class ClientSideLeafGuidanceTest extends BaseTestCase {
         junit.textui.TestRunner.run(suite());
     }
     
-    private static void doSettings() {
-        //Setup LimeWire backend.  For testing other vendors, you can skip all
-        //this and manually configure a client in leaf mode to listen on port
-        //6669, with no slots and no connections.  But you need to re-enable
-        //the interactive prompts below.
-        ConnectionSettings.PORT.setValue(TEST_PORT);
-		ConnectionSettings.CONNECT_ON_STARTUP.setValue(false);
-		UltrapeerSettings.EVER_ULTRAPEER_CAPABLE.setValue(false);
-		UltrapeerSettings.DISABLE_ULTRAPEER_MODE.setValue(true);
-		UltrapeerSettings.FORCE_ULTRAPEER_MODE.setValue(false);
-		ConnectionSettings.NUM_CONNECTIONS.setValue(0);
-		ConnectionSettings.LOCAL_IS_PRIVATE.setValue(false);
-		SharingSettings.EXTENSIONS_TO_SHARE.setValue("txt;");
-        // get the resource file for com/limegroup/gnutella
-        File berkeley = 
-            CommonUtils.getResourceFile("com/limegroup/gnutella/berkeley.txt");
-        File susheel = 
-            CommonUtils.getResourceFile("com/limegroup/gnutella/susheel.txt");
-        // now move them to the share dir
-        CommonUtils.copy(berkeley, new File(_sharedDir, "berkeley.txt"));
-        CommonUtils.copy(susheel, new File(_sharedDir, "susheel.txt"));
-        // make sure results get through
-        SearchSettings.MINIMUM_SEARCH_QUALITY.setValue(-2);
-    }        
-    
-    public static void globalSetUp() throws Exception {
-        doSettings();
-
-        callback=new MyActivityCallback();
-        rs=new RouterService(callback);
-        assertEquals("unexpected port",
-            TEST_PORT, ConnectionSettings.PORT.getValue());
-        rs.start();
-        RouterService.clearHostCatcher();
-        RouterService.connect();
-        Thread.sleep(1000);
-        assertEquals("unexpected port",
-            TEST_PORT, ConnectionSettings.PORT.getValue());
-        connect();
-    }        
-    
-    public void setUp() throws Exception  {
-        doSettings();
-    }
-    
-    public static void globalTearDown() throws Exception {
-        shutdown();
-    }
-
-     ////////////////////////// Initialization ////////////////////////
-
-     private static void connect() 
-     throws IOException, BadPacketException {
-         debug("-Establish connections");
-         //Ugh, there is a race condition here from the old days when this was
-         //an interactive test.  If rs connects before the listening socket is
-         //created, the test will fail.
-
-         //System.out.println("Please establish a connection to localhost:6350\n");
-     }
-     
-     private static Connection connect(int port, 
-                                       boolean ultrapeer) 
-         throws Exception {
-         ServerSocket ss=new ServerSocket(port);
-         RouterService.connectToHostAsynchronously("127.0.0.1", port);
-         Socket socket = ss.accept();
-         ss.close();
-         
-         socket.setSoTimeout(3000);
-         InputStream in=socket.getInputStream();
-         String word=readWord(in);
-         if (! word.equals("GNUTELLA"))
-             throw new IOException("Bad word: "+word);
-         
-         HandshakeResponder responder;
-         if (ultrapeer) {
-             responder = new UltrapeerResponder();
-         } else {
-             responder = new OldResponder();
-         }
-         Connection con = new Connection(socket, responder);
-         con.initialize();
-         replyToPing(con, ultrapeer);
-         return con;
-     }
-     
-     /**
-      * Acceptor.readWord
-      *
-      * @modifies sock
-      * @effects Returns the first word (i.e., no whitespace) of less
-      *  than 8 characters read from sock, or throws IOException if none
-      *  found.
-      */
-     private static String readWord(InputStream sock) throws IOException {
-         final int N=9;  //number of characters to look at
-         char[] buf=new char[N];
-         for (int i=0 ; i<N ; i++) {
-             int got=sock.read();
-             if (got==-1)  //EOF
-                 throw new IOException();
-             if ((char)got==' ') { //got word.  Exclude space.
-                 return new String(buf,0,i);
-             }
-             buf[i]=(char)got;
-         }
-         throw new IOException();
-     }
-
-     private static void replyToPing(Connection c, boolean ultrapeer) 
-             throws Exception {
-        // respond to a ping iff one is given.
-        Message m = null;
-        byte[] guid;
-        try {
-            while (!(m instanceof PingRequest)) {
-                m = c.receive(500);
-            }
-            guid = ((PingRequest)m).getGUID();            
-        } catch(InterruptedIOException iioe) {
-            //nothing's coming, send a fake pong anyway.
-            guid = new GUID().bytes();
-        }
-        
-        Socket socket = (Socket)PrivilegedAccessor.getValue(c, "_socket");
-        PingReply reply = 
-        PingReply.createExternal(guid, (byte)7,
-                                 socket.getLocalPort(), 
-                                 ultrapeer ? ultrapeerIP : oldIP,
-                                 ultrapeer);
-        reply.hop();
-        c.send(reply);
-        c.flush();
-     }
-
     /** @return The first QueyrRequest received from this connection.  If null
      *  is returned then it was never recieved (in a timely fashion).
      */
     private static QueryStatusResponse getFirstQueryStatus(Connection c) {
         return (QueryStatusResponse)
-            getFirstMessageOfType(c, QueryStatusResponse.class, TIMEOUT);
+            getFirstInstanceOfMessageType(c, QueryStatusResponse.class, TIMEOUT);
     }
 
     ///////////////////////// Actual Tests ////////////////////////////
@@ -195,22 +50,17 @@ public class ClientSideLeafGuidanceTest extends BaseTestCase {
     // THIS TEST SHOULD BE RUN FIRST!!
     public void testBasicGuidance() throws Exception {
         
-        for (int i = 0; i < testUPs.length; i++) {
-            testUPs[i] = connect(6355+i, true);
-
+        for (int i = 0; i < testUP.length; i++)
             // send a MessagesSupportedMessage
-            testUPs[i].send(MessagesSupportedVendorMessage.instance());
-            testUPs[i].flush();
-            drain(testUPs[i]);
-        }
+            testUP[i].send(MessagesSupportedVendorMessage.instance());
 
         // spawn a query and make sure all UPs get it
         GUID queryGuid = new GUID(RouterService.newQueryGUID());
         RouterService.query(queryGuid.bytes(), "susheel");
         Thread.sleep(250);
 
-        for (int i = 0; i < testUPs.length; i++) {
-            QueryRequest qr = getFirstQueryRequest(testUPs[i]);
+        for (int i = 0; i < testUP.length; i++) {
+            QueryRequest qr = getFirstQueryRequest(testUP[i]);
             assertNotNull(qr);
             assertEquals(new GUID(qr.getGUID()), queryGuid);
         }
@@ -218,8 +68,8 @@ public class ClientSideLeafGuidanceTest extends BaseTestCase {
         // now send back results and make sure that we get a QueryStatus
         // from the leaf
         Message m = null;
-        assertGreaterThan(REPORT_INTERVAL, 4*testUPs.length);
-        for (int i = 0; i < testUPs.length; i++) {
+        assertGreaterThan(REPORT_INTERVAL, 4*testUP.length);
+        for (int i = 0; i < testUP.length; i++) {
             Response[] res = new Response[4];
             res[0] = new Response(10, 10, "susheel"+i);
             res[1] = new Response(10, 10, "susheel smells good"+i);
@@ -229,13 +79,13 @@ public class ClientSideLeafGuidanceTest extends BaseTestCase {
                                GUID.makeGuid(), new byte[0], false, false, true,
                                true, false, false, null);
 
-            testUPs[i].send(m);
-            testUPs[i].flush();
+            testUP[i].send(m);
+            testUP[i].flush();
         }
         
         // all UPs should get a QueryStatusResponse
-        for (int i = 0; i < testUPs.length; i++) {
-            QueryStatusResponse stat = getFirstQueryStatus(testUPs[i]);
+        for (int i = 0; i < testUP.length; i++) {
+            QueryStatusResponse stat = getFirstQueryStatus(testUP[i]);
             assertNotNull(stat);
             assertEquals(new GUID(stat.getGUID()), queryGuid);
             assertEquals(4, stat.getNumResults());
@@ -245,8 +95,8 @@ public class ClientSideLeafGuidanceTest extends BaseTestCase {
         RouterService.stopQuery(queryGuid);
 
         // all UPs should get a QueryStatusResponse with 65535
-        for (int i = 0; i < testUPs.length; i++) {
-            QueryStatusResponse stat = getFirstQueryStatus(testUPs[i]);
+        for (int i = 0; i < testUP.length; i++) {
+            QueryStatusResponse stat = getFirstQueryStatus(testUP[i]);
             assertNotNull(stat);
             assertEquals(new GUID(stat.getGUID()), queryGuid);
             assertEquals(65535, stat.getNumResults());
@@ -256,15 +106,15 @@ public class ClientSideLeafGuidanceTest extends BaseTestCase {
 
     public void testAdvancedGuidance1() throws Exception {
 
-        for (int i = 0; i < testUPs.length; i++)
-            drain(testUPs[i]);
+        for (int i = 0; i < testUP.length; i++)
+            drain(testUP[i]);
         
         // spawn a query and make sure all UPs get it
         GUID queryGuid = new GUID(RouterService.newQueryGUID());
         RouterService.query(queryGuid.bytes(), "susheel daswanu");
 
-        for (int i = 0; i < testUPs.length; i++) {
-            QueryRequest qr = getFirstQueryRequest(testUPs[i]);
+        for (int i = 0; i < testUP.length; i++) {
+            QueryRequest qr = getFirstQueryRequest(testUP[i]);
             assertNotNull(qr);
             assertEquals(new GUID(qr.getGUID()), queryGuid);
         }
@@ -272,7 +122,7 @@ public class ClientSideLeafGuidanceTest extends BaseTestCase {
         // now send back results and make sure that we get a QueryStatus
         // from the leaf
         Message m = null;
-        for (int i = 0; i < testUPs.length; i++) {
+        for (int i = 0; i < testUP.length; i++) {
             // since i know REPORT_INTERVAL is 15, i'm sending 45, then 90, then
             // 135, then 180 - after 180 leaf guidance should shut off....
             Response[] res = new Response[REPORT_INTERVAL*3];
@@ -283,15 +133,15 @@ public class ClientSideLeafGuidanceTest extends BaseTestCase {
                                GUID.makeGuid(), new byte[0], false, false, true,
                                true, false, false, null);
 
-            testUPs[i].send(m);
-            testUPs[i].flush();
+            testUP[i].send(m);
+            testUP[i].flush();
         }
         
         // all UPs should get a QueryStatusResponse
         boolean maxResultsEncountered = false;
-        for (int i = 0; i < testUPs.length; i++) {
-            for (int j = 0; j < testUPs.length; j++) {
-                QueryStatusResponse stat = getFirstQueryStatus(testUPs[j]);
+        for (int i = 0; i < testUP.length; i++) {
+            for (int j = 0; j < testUP.length; j++) {
+                QueryStatusResponse stat = getFirstQueryStatus(testUP[j]);
                 assertNotNull(stat);
                 assertEquals(new GUID(stat.getGUID()), queryGuid);
                 // depending on how far along the query is we could have a
@@ -300,7 +150,7 @@ public class ClientSideLeafGuidanceTest extends BaseTestCase {
                 assertTrue((stat.getNumResults() == 11*(i+1)) ||
                            (stat.getNumResults() == MAX_RESULTS));
                 if (stat.getNumResults() == MAX_RESULTS) {
-                    assertTrue(i == testUPs.length-1);
+                    assertTrue(i == testUP.length-1);
                     maxResultsEncountered = true;
                 }
             }
@@ -316,17 +166,17 @@ public class ClientSideLeafGuidanceTest extends BaseTestCase {
                            GUID.makeGuid(), new byte[0], false, false, true,
                            true, false, false, null);
         
-        testUPs[0].send(m);
-        testUPs[0].flush();
+        testUP[0].send(m);
+        testUP[0].flush();
 
         // no UPs should get a QueryStatusResponse
-        for (int i = 0; i < testUPs.length; i++) {
+        for (int i = 0; i < testUP.length; i++) {
             final int index = i;
             Thread newThread = new Thread() {
                     public void run() {
                         try {
                             QueryStatusResponse stat = 
-                                getFirstQueryStatus(testUPs[index]);
+                                getFirstQueryStatus(testUP[index]);
                             assertNull(stat);
                         }
                         catch (Exception e) {
@@ -343,15 +193,15 @@ public class ClientSideLeafGuidanceTest extends BaseTestCase {
 
         Message m = null;
 
-        for (int i = 0; i < testUPs.length; i++)
-            drain(testUPs[i]);
+        for (int i = 0; i < testUP.length; i++)
+            drain(testUP[i]);
         
         // spawn a query and make sure all UPs get it
         GUID queryGuid = new GUID(RouterService.newQueryGUID());
         RouterService.query(queryGuid.bytes(), "anita kesavan");
 
-        for (int i = 0; i < testUPs.length; i++) {
-            QueryRequest qr = getFirstQueryRequest(testUPs[i]);
+        for (int i = 0; i < testUP.length; i++) {
+            QueryRequest qr = getFirstQueryRequest(testUP[i]);
             assertNotNull(qr);
             assertEquals(new GUID(qr.getGUID()), queryGuid);
         }
@@ -366,12 +216,12 @@ public class ClientSideLeafGuidanceTest extends BaseTestCase {
                            GUID.makeGuid(), new byte[0], false, false, true,
                            true, false, false, null);
         
-        testUPs[0].send(m);
-        testUPs[0].flush();
+        testUP[0].send(m);
+        testUP[0].flush();
 
         // all UPs should get a QueryStatusResponse
-        for (int i = 0; i < testUPs.length; i++) {
-            QueryStatusResponse stat = getFirstQueryStatus(testUPs[i]);
+        for (int i = 0; i < testUP.length; i++) {
+            QueryStatusResponse stat = getFirstQueryStatus(testUP[i]);
             assertNotNull(stat);
             assertEquals(new GUID(stat.getGUID()), queryGuid);
             assertEquals(REPORT_INTERVAL, stat.getNumResults());
@@ -388,12 +238,12 @@ public class ClientSideLeafGuidanceTest extends BaseTestCase {
                            GUID.makeGuid(), new byte[0], false, false, true,
                            true, false, false, null);
         
-        testUPs[2].send(m);
-        testUPs[2].flush();
+        testUP[2].send(m);
+        testUP[2].flush();
 
         // no UPs should get a QueryStatusResponse
-        for (int i = 0; i < testUPs.length; i++) {
-            QueryStatusResponse stat = getFirstQueryStatus(testUPs[i]);
+        for (int i = 0; i < testUP.length; i++) {
+            QueryStatusResponse stat = getFirstQueryStatus(testUP[i]);
             assertNull(stat);
         }
 
@@ -406,12 +256,12 @@ public class ClientSideLeafGuidanceTest extends BaseTestCase {
                            GUID.makeGuid(), new byte[0], false, false, true,
                            true, false, false, null);
         
-        testUPs[1].send(m);
-        testUPs[1].flush();
+        testUP[1].send(m);
+        testUP[1].flush();
 
         // and all UPs should get a QueryStatusResponse
-        for (int i = 0; i < testUPs.length; i++) {
-            QueryStatusResponse stat = getFirstQueryStatus(testUPs[i]);
+        for (int i = 0; i < testUP.length; i++) {
+            QueryStatusResponse stat = getFirstQueryStatus(testUP[i]);
             assertNotNull(stat);
             assertEquals(new GUID(stat.getGUID()), queryGuid);
             assertEquals(REPORT_INTERVAL+((REPORT_INTERVAL+1)/4), 
@@ -422,8 +272,8 @@ public class ClientSideLeafGuidanceTest extends BaseTestCase {
         RouterService.stopQuery(queryGuid);
 
         // all UPs should get a QueryStatusResponse with 65535
-        for (int i = 0; i < testUPs.length; i++) {
-            QueryStatusResponse stat = getFirstQueryStatus(testUPs[i]);
+        for (int i = 0; i < testUP.length; i++) {
+            QueryStatusResponse stat = getFirstQueryStatus(testUP[i]);
             assertNotNull(stat);
             assertEquals(new GUID(stat.getGUID()), queryGuid);
             assertEquals(65535, stat.getNumResults());
@@ -438,17 +288,17 @@ public class ClientSideLeafGuidanceTest extends BaseTestCase {
                            GUID.makeGuid(), new byte[0], false, false, true,
                            true, false, false, null);
         
-        testUPs[0].send(m);
-        testUPs[0].flush();
+        testUP[0].send(m);
+        testUP[0].flush();
 
         // no UPs should get a QueryStatusResponse
-        for (int i = 0; i < testUPs.length; i++) {
+        for (int i = 0; i < testUP.length; i++) {
             final int index = i;
             Thread newThread = new Thread() {
                     public void run() {
                         try {
                             QueryStatusResponse stat = 
-                                getFirstQueryStatus(testUPs[index]);
+                                getFirstQueryStatus(testUP[index]);
                             assertNull(stat);
                         }
                         catch (Exception e) {
@@ -460,41 +310,16 @@ public class ClientSideLeafGuidanceTest extends BaseTestCase {
         }
     }
 
-    private static void shutdown() throws IOException {
-        //System.out.println("\nShutting down.");
-        debug("-Shutting down");
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) { }
-    }
-    
     private static byte[] myIP() {
         return new byte[] { (byte)127, (byte)0, 0, 1 };
     }
 
-    private static final boolean DEBUG = false;
-    
-    static void debug(String message) {
-        if(DEBUG) 
-            System.out.println(message);
+    public static Integer numUPs() {
+        return new Integer(4);
     }
 
-    private static class UltrapeerResponder implements HandshakeResponder {
-        public HandshakeResponse respond(HandshakeResponse response, 
-                boolean outgoing) throws IOException {
-            Properties props = new UltrapeerHeaders("127.0.0.1"); 
-            props.put(HeaderNames.X_DEGREE, "42");           
-            return HandshakeResponse.createResponse(props);
-        }
-    }
-
-
-    private static class OldResponder implements HandshakeResponder {
-        public HandshakeResponse respond(HandshakeResponse response, 
-                boolean outgoing) throws IOException {
-            Properties props=new Properties();
-            return HandshakeResponse.createResponse(props);
-        }
+    public static ActivityCallback getActivityCallback() {
+        return new MyActivityCallback();
     }
 
     public static class MyActivityCallback extends ActivityCallbackStub {
