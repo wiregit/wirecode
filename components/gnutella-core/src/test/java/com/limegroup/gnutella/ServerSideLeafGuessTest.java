f4cb8a2b300167d8d3c21c96dd8c5c858cbdd169
sdaswani <sdaswani>
2004-01-07 18:17:00 +0000

    uses new ClientSideTestCase class.

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/ServerSideLeafGuessTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/ServerSideLeafGuessTest.java
@@ -42,24 +42,10 @@ import com.sun.java.util.collections.Iterator;
 import com.sun.java.util.collections.Set;
 
 /**
- * Checks whether (multi)leaves avoid forwarding messages to ultrapeers, do
- * redirects properly, etc.  The test includes a leaf attached to 3 
- * Ultrapeers.
+ * Checks whether a leaf node handles GUESS queries correctly.  Tests the
+ * Server side of GUESS.
  */
-public class ServerSideLeafGuessTest 
-    extends com.limegroup.gnutella.util.BaseTestCase {
-    private static final int PORT=6669;
-    private static final int TIMEOUT=3000;
-    private static final byte[] ultrapeerIP=
-        new byte[] {(byte)18, (byte)239, (byte)0, (byte)144};
-    private static final byte[] oldIP=
-        new byte[] {(byte)111, (byte)22, (byte)33, (byte)44};
-
-    private static Connection[] testUPs = new Connection[4];
-    private static RouterService rs;
-
-    private static MyActivityCallback callback;
-
+public class ServerSideLeafGuessTest extends ClientSideTestCase {
 	/**
 	 * Constant for the size of UDP messages to accept -- dependent upon
 	 * IP-layer fragmentation.
@@ -83,142 +69,6 @@ public class ServerSideLeafGuessTest
         junit.textui.TestRunner.run(suite());
     }
     
-    private static void doSettings() {
-        //Setup LimeWire backend.  For testing other vendors, you can skip all
-        //this and manually configure a client in leaf mode to listen on port
-        //6669, with no slots and no connections.  But you need to re-enable
-        //the interactive prompts below.
-        ConnectionSettings.PORT.setValue(PORT);
-		ConnectionSettings.CONNECT_ON_STARTUP.setValue(false);
-		UltrapeerSettings.EVER_ULTRAPEER_CAPABLE.setValue(false);
-		UltrapeerSettings.DISABLE_ULTRAPEER_MODE.setValue(true);
-		UltrapeerSettings.FORCE_ULTRAPEER_MODE.setValue(false);
-		ConnectionSettings.NUM_CONNECTIONS.setValue(0);
-		ConnectionSettings.LOCAL_IS_PRIVATE.setValue(false);
-		SharingSettings.EXTENSIONS_TO_SHARE.setValue("txt;");
-        // get the resource file for com/limegroup/gnutella
-        File berkeley = 
-            CommonUtils.getResourceFile("com/limegroup/gnutella/berkeley.txt");
-        File susheel = 
-            CommonUtils.getResourceFile("com/limegroup/gnutella/susheel.txt");
-        // now move them to the share dir
-        CommonUtils.copy(berkeley, new File(_sharedDir, "berkeley.txt"));
-        CommonUtils.copy(susheel, new File(_sharedDir, "susheel.txt"));
-        // make sure results get through
-        SearchSettings.MINIMUM_SEARCH_QUALITY.setValue(-2);
-    }        
-    
-    public static void globalSetUp() throws Exception {
-        doSettings();
-
-        callback=new MyActivityCallback();
-        rs=new RouterService(callback);
-        assertEquals("unexpected port",
-            PORT, ConnectionSettings.PORT.getValue());
-        rs.start();
-        rs.clearHostCatcher();
-        rs.connect();
-        Thread.sleep(1000);
-        assertEquals("unexpected port",
-            PORT, ConnectionSettings.PORT.getValue());
-        connect(rs);
-    }        
-    
-    public void setUp() throws Exception  {
-        doSettings();
-    }
-    
-    public static void globalTearDown() throws Exception {
-        shutdown();
-    }
-
-     ////////////////////////// Initialization ////////////////////////
-
-     private static void connect(RouterService rs) 
-     throws IOException, BadPacketException {
-         debug("-Establish connections");
-         //Ugh, there is a race condition here from the old days when this was
-         //an interactive test.  If rs connects before the listening socket is
-         //created, the test will fail.
-
-         //System.out.println("Please establish a connection to localhost:6350\n");
-     }
-     
-     private static Connection connect(RouterService rs, int port, 
-                                       boolean ultrapeer) 
-         throws Exception {
-         ServerSocket ss=new ServerSocket(port);
-         rs.connectToHostAsynchronously("127.0.0.1", port);
-         Socket socket = ss.accept();
-         ss.close();
-         
-         socket.setSoTimeout(3000);
-         InputStream in=socket.getInputStream();
-         String word=readWord(in);
-         if (! word.equals("GNUTELLA"))
-             throw new IOException("Bad word: "+word);
-         
-         HandshakeResponder responder;
-         if (ultrapeer) {
-             responder = new UltrapeerResponder();
-         } else {
-             responder = new OldResponder();
-         }
-         Connection con = new Connection(socket, responder);
-         con.initialize();
-         replyToPing(con, ultrapeer);
-         return con;
-     }
-     
-     /**
-      * Acceptor.readWord
-      *
-      * @modifies sock
-      * @effects Returns the first word (i.e., no whitespace) of less
-      *  than 8 characters read from sock, or throws IOException if none
-      *  found.
-      */
-     private static String readWord(InputStream sock) throws IOException {
-         final int N=9;  //number of characters to look at
-         char[] buf=new char[N];
-         for (int i=0 ; i<N ; i++) {
-             int got=sock.read();
-             if (got==-1)  //EOF
-                 throw new IOException();
-             if ((char)got==' ') { //got word.  Exclude space.
-                 return new String(buf,0,i);
-             }
-             buf[i]=(char)got;
-         }
-         throw new IOException();
-     }
-
-     private static void replyToPing(Connection c, boolean ultrapeer) 
-             throws Exception {
-        // respond to a ping iff one is given.
-        Message m = null;
-        byte[] guid;
-        try {
-            while (!(m instanceof PingRequest)) {
-                m = c.receive(500);
-            }
-            guid = ((PingRequest)m).getGUID();            
-        } catch(InterruptedIOException iioe) {
-            //nothing's coming, send a fake pong anyway.
-            guid = new GUID().bytes();
-        }
-        
-        Socket socket = (Socket)PrivilegedAccessor.getValue(c, "_socket");
-        PingReply reply = 
-        PingReply.createExternal(guid, (byte)7,
-                                 socket.getLocalPort(), 
-                                 ultrapeer ? ultrapeerIP : oldIP,
-                                 ultrapeer);
-        reply.hop();
-        c.send(reply);
-        c.flush();
-     }
-
     ///////////////////////// Actual Tests ////////////////////////////
 
     // MUST RUN THIS TEST FIRST
@@ -226,12 +76,11 @@ public class ServerSideLeafGuessTest
         DatagramPacket pack = null;
         UDP_ACCESS = new DatagramSocket();
 
-        for (int i = 0; i < testUPs.length; i++) {
-            testUPs[i] = connect(rs, 6355+i, true);
-            drain(testUPs[i]);
+        for (int i = 0; i < testUP.length; i++) {
+            drain(testUP[i]);
             // OOB client side needs server side leaf guidance
-            testUPs[i].send(MessagesSupportedVendorMessage.instance());
-            testUPs[i].flush();
+            testUP[i].send(MessagesSupportedVendorMessage.instance());
+            testUP[i].flush();
         }
 
         // first we need to set up GUESS capability
@@ -244,8 +93,8 @@ public class ServerSideLeafGuessTest
                                  UDP_ACCESS.getLocalPort(), 
                                  InetAddress.getLocalHost().getAddress(), 
                                  10, 10, true, 900, true);
-            testUPs[0].send(pong);
-            testUPs[0].flush();
+            testUP[0].send(pong);
+            testUP[0].flush();
 
             // wait for the ping request from the test UP
             UDP_ACCESS.setSoTimeout(2000);
@@ -276,14 +125,14 @@ public class ServerSideLeafGuessTest
         // set up unsolicited UDP support
         {
             // resend this to start exchange
-            testUPs[0].send(MessagesSupportedVendorMessage.instance());
-            testUPs[0].flush();
+            testUP[0].send(MessagesSupportedVendorMessage.instance());
+            testUP[0].flush();
 
             byte[] cbGuid = null;
             int cbPort = -1;
             while (cbGuid == null) {
                 try {
-                    Message m = testUPs[0].receive(TIMEOUT);
+                    Message m = testUP[0].receive(TIMEOUT);
                     if (m instanceof UDPConnectBackVendorMessage) {
                         UDPConnectBackVendorMessage udp = 
                             (UDPConnectBackVendorMessage) m;
@@ -303,7 +152,7 @@ public class ServerSideLeafGuessTest
             pr.write(baos);
             pack = new DatagramPacket(baos.toByteArray(), 
                                       baos.toByteArray().length,
-                                      testUPs[0].getInetAddress(), cbPort);
+                                      testUP[0].getInetAddress(), cbPort);
             UDP_ACCESS.send(pack);
         }
 
@@ -312,8 +161,8 @@ public class ServerSideLeafGuessTest
             Socket sock = null;
             OutputStream os = null;
             try {
-                sock = Sockets.connect(InetAddress.getLocalHost().getHostAddress(), 
-                                       PORT, 12);
+                sock = Sockets.connect(InetAddress.getLocalHost().getHostAddress(),
+                                       SERVER_PORT, 12);
                 os = sock.getOutputStream();
                 os.write("\n\n".getBytes());
             } catch (IOException ignored) {
@@ -330,7 +179,7 @@ public class ServerSideLeafGuessTest
 
         InetAddress localHost = InetAddress.getLocalHost();
         // first send a QueryKey request....
-        send(PingRequest.createQueryKeyRequest(), localHost, PORT);
+        send(PingRequest.createQueryKeyRequest(), localHost, SERVER_PORT);
 
         // we should get a QueryKey....
         Message m = null;
@@ -350,7 +199,7 @@ public class ServerSideLeafGuessTest
         QueryRequest goodQuery = QueryRequest.createQueryKeyQuery("susheel", 
                                                                   qkToUse);
         byte[] guid = goodQuery.getGUID();
-        send(goodQuery, localHost, PORT);
+        send(goodQuery, localHost, SERVER_PORT);
         
         // now we should get an ack
         m = receive();
@@ -369,7 +218,7 @@ public class ServerSideLeafGuessTest
     public void testGoodURNQuery() throws Exception {
         InetAddress localHost = InetAddress.getLocalHost();
         // first send a QueryKey request....
-        send(PingRequest.createQueryKeyRequest(), localHost, PORT);
+        send(PingRequest.createQueryKeyRequest(), localHost, SERVER_PORT);
 
         // we should get a QueryKey....
         Message m = null;
@@ -395,7 +244,7 @@ public class ServerSideLeafGuessTest
         QueryRequest goodQuery = QueryRequest.createQueryKeyQuery(berkeleyURN, 
                                                                   qkToUse);
         byte[] guid = goodQuery.getGUID();
-        send(goodQuery, localHost, PORT);
+        send(goodQuery, localHost, SERVER_PORT);
         
         // now we should get an ack
         m = receive();
@@ -418,7 +267,7 @@ public class ServerSideLeafGuessTest
     public void testQueryWithNoHit() throws Exception {
         InetAddress localHost = InetAddress.getLocalHost();
         // first send a QueryKey request....
-        send(PingRequest.createQueryKeyRequest(), localHost, PORT);
+        send(PingRequest.createQueryKeyRequest(), localHost, SERVER_PORT);
 
         // we should get a QueryKey....
         Message m = null;
@@ -438,7 +287,7 @@ public class ServerSideLeafGuessTest
         QueryRequest goodQuery = QueryRequest.createQueryKeyQuery("anita", 
                                                                   qkToUse);
         byte[] guid = goodQuery.getGUID();
-        send(goodQuery, localHost, PORT);
+        send(goodQuery, localHost, SERVER_PORT);
         
         // now we should get an ack
         m = receive();
@@ -467,7 +316,7 @@ public class ServerSideLeafGuessTest
             QueryRequest goodQuery = QueryRequest.createQueryKeyQuery("susheel", 
                                                                       qkToUse);
             byte[] guid = goodQuery.getGUID();
-            send(goodQuery, localHost, PORT);
+            send(goodQuery, localHost, SERVER_PORT);
             
             try {
                 // now we should NOT get an ack            
@@ -504,60 +353,17 @@ public class ServerSideLeafGuessTest
         UDP_ACCESS.send(dg);
 	}
 
-    private void drainAll() throws Exception {
-        drainAll(testUPs);
-    }
-
-    private static void shutdown() throws IOException {
-        //System.out.println("\nShutting down.");
-        debug("-Shutting down");
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) { }
-    }
-    
     private static byte[] myIP() {
         return new byte[] { (byte)127, (byte)0, 0, 1 };
     }
 
-    private static final boolean DEBUG = false;
-    
-    static void debug(String message) {
-        if(DEBUG) 
-            System.out.println(message);
+    public static Integer numUPs() {
+        return new Integer(4);
     }
 
-    private static class UltrapeerResponder implements HandshakeResponder {
-        public HandshakeResponse respond(HandshakeResponse response, 
-                boolean outgoing) throws IOException {
-            Properties props = new UltrapeerHeaders("127.0.0.1"); 
-            props.put(HeaderNames.X_DEGREE, "42");           
-            return HandshakeResponse.createResponse(props);
-        }
+    public static ActivityCallback getActivityCallback() {
+        return new ActivityCallbackStub();
     }
-
-
-    private static class OldResponder implements HandshakeResponder {
-        public HandshakeResponse respond(HandshakeResponse response, 
-                boolean outgoing) throws IOException {
-            Properties props=new Properties();
-            return HandshakeResponse.createResponse(props);
-        }
-    }
-
-    public static class MyActivityCallback extends ActivityCallbackStub {
-        private RemoteFileDesc rfd = null;
-        public RemoteFileDesc getRFD() {
-            return rfd;
-        }
-
-        public void handleQueryResult(RemoteFileDesc rfd,
-                                      HostData data,
-                                      Set locs) {
-            this.rfd = rfd;
-        }
-    }
-
 
 }
 
