f20b361f2b4e50bc71720f43a1a04f3f1543da1b
sberlin <sberlin>
2004-01-15 00:19:43 +0000

    added new setOpenFileLimit0 call, so we don't need a prelude script.

--- components/gnutella-core/native/osx/SystemUtilities.m
+++ components/gnutella-core/native/osx/SystemUtilities.m
@@ -3,7 +3,9 @@
  * require native code.
  */
 
-#include <jni.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <JavaVM/jni.h>
 #include <ApplicationServices/ApplicationServices.h>
 
 extern double CGSSecondsSinceLastInputEvent(unsigned long envType);
@@ -17,4 +19,27 @@ JNIEXPORT jlong JNICALL Java_com_limegroup_gnutella_util_SystemUtils_idleTime
     if(idleTime >= 18446744000.0) idleTime = 0.0; //18446744073.0
 
     return (jlong)idleTime*1000;
+}
+
+JNIEXPORT jint JNICALL Java_com_limegroup_gnutella_util_SystemUtils_setOpenFileLimit0
+  (JNIEnv *env, jclass clazz, jint limit) {
+    int retval = 0;
+	int maxFiles = (int)limit;
+    struct rlimit rl;
+    
+    // retrieve current values.
+    if (getrlimit(RLIMIT_NOFILE, &rl) == -1)
+        rl.rlim_cur = rl.rlim_max = 0;
+
+    // if either are below, raise them as necessary.
+	if (rl.rlim_cur < maxFiles || rl.rlim_max < maxFiles) {
+        // raise only what we need to.
+        if(rl.rlim_cur < maxFiles)
+            rl.rlim_cur = maxFiles;
+        if( rl.rlim_max < maxFiles )
+            rl.rlim_max = maxFiles;
+        // set with new values.
+        retval = setrlimit(RLIMIT_NOFILE, &rl);
+    }
+    return (jint)retval;
 }
