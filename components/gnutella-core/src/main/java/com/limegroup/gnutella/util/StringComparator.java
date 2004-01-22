1de603eb54830f4508f0b787efa169fbeaf22639
afisk <afisk>
2004-01-22 00:35:13 +0000

    Made this class package-access to prevent classes from other packages instantiating this class -- they should use the Comparators class instead to reduce object creation.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/util/StringComparator.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/util/StringComparator.java
@@ -4,12 +4,12 @@ import com.sun.java.util.collections.Comparator;
 import java.io.Serializable;
 
 /**
- * Compares to Strings via String.compareTo(String).  Useful for storing Java
+ * Compares two Strings via String.compareTo(String).  Useful for storing Java
  * 1.1.8 Strings in Java 1.2+ sorted collections classes.  This is needed
  * because Strings in 1.1.8 do not implement the Comparable interface, unlike
  * Strings in 1.2+. 
  */
-public class StringComparator implements Comparator, Serializable {
+final class StringComparator implements Comparator, Serializable {
     static final long serialVersionUID = -624599003446177506L;
 
     /** Returns ((String)a).compareTo((String)b). */
