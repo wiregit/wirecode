37ec58bd9c905d9ef46da92d8427643f9fa5c909
sdaswani <sdaswani>
2004-01-28 23:10:54 +0000

    This test makes sure that the UP only proxies under the correct conditions.

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/ServerSideOOBProxyTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/ServerSideOOBProxyTest.java
@@ -23,8 +23,10 @@ import com.limegroup.gnutella.messages.vendor.LimeACKVendorMessage;
 import com.limegroup.gnutella.messages.vendor.MessagesSupportedVendorMessage;
 import com.limegroup.gnutella.messages.vendor.ReplyNumberVendorMessage;
 import com.limegroup.gnutella.messages.vendor.UDPConnectBackVendorMessage;
+import com.limegroup.gnutella.messages.vendor.QueryStatusResponse;
 import com.limegroup.gnutella.routing.QueryRouteTable;
 import com.limegroup.gnutella.routing.RouteTableMessage;
+import com.limegroup.gnutella.search.SearchResultHandler;
 import com.limegroup.gnutella.settings.ConnectionSettings;
 import com.limegroup.gnutella.settings.FilterSettings;
 import com.limegroup.gnutella.settings.SharingSettings;
@@ -49,6 +51,7 @@ import com.sun.java.util.collections.Random;
  *
  */
 public final class ServerSideOOBProxyTest extends ServerSideTestCase {
+    private final int MAX_RESULTS = SearchResultHandler.MAX_RESULTS;
 
     protected static int TIMEOUT = 2000;
 
@@ -74,7 +77,7 @@ public final class ServerSideOOBProxyTest extends ServerSideTestCase {
     }
 
     public static Integer numLeaves() {
-        return new Integer(1);
+        return new Integer(2);
     }
 	
     public static ActivityCallback getActivityCallback() {
@@ -89,6 +92,14 @@ public final class ServerSideOOBProxyTest extends ServerSideTestCase {
         for (Iterator iter=qrt.encode(null).iterator(); iter.hasNext(); ) {
             LEAF[0].send((RouteTableMessage)iter.next());
 			LEAF[0].flush();
+        }
+
+        qrt = new QueryRouteTable();
+        qrt.add("berkeley");
+        qrt.addIndivisible(HugeTestUtils.UNIQUE_SHA1.toString());
+        for (Iterator iter=qrt.encode(null).iterator(); iter.hasNext(); ) {
+            LEAF[1].send((RouteTableMessage)iter.next());
+			LEAF[1].flush();
         }
 
         qrt = new QueryRouteTable();
@@ -110,8 +121,180 @@ public final class ServerSideOOBProxyTest extends ServerSideTestCase {
     // BEGIN TESTS
     // ------------------------------------------------------
 
+    // PLEASE RUN THIS TEST FIRST!!!
     public void testDoesNotProxy() throws Exception {
 
+        // need to set up GUESS
+        DatagramPacket pack = null;
+        UDP_ACCESS = new DatagramSocket();
+        // set up solicited UDP support
+        {
+            drainAll();
+            PingReply pong = 
+                PingReply.create(GUID.makeGuid(), (byte) 4,
+                                 UDP_ACCESS.getLocalPort(), 
+                                 InetAddress.getLocalHost().getAddress(), 
+                                 10, 10, true, 900, true);
+            ULTRAPEER[0].send(pong);
+            ULTRAPEER[0].flush();
+
+            // wait for the ping request from the test UP
+            UDP_ACCESS.setSoTimeout(500);
+            pack = new DatagramPacket(new byte[1000], 1000);
+            try {
+                UDP_ACCESS.receive(pack);
+            }
+            catch (IOException bad) {
+               fail("Did not get ping", bad);
+            }
+            InputStream in = new ByteArrayInputStream(pack.getData());
+            // as long as we don't get a ClassCastException we are good to go
+            PingRequest ping = (PingRequest) Message.read(in);
+            
+            // send the pong in response to the ping
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            pong = PingReply.create(ping.getGUID(), (byte) 4,
+                                    UDP_ACCESS.getLocalPort(), 
+                                    InetAddress.getLocalHost().getAddress(), 
+                                    10, 10, true, 900, true);
+            pong.write(baos);
+            pack = new DatagramPacket(baos.toByteArray(), 
+                                      baos.toByteArray().length,
+                                      pack.getAddress(), pack.getPort());
+            UDP_ACCESS.send(pack);
+        }
+
+        // set up unsolicited UDP support
+        {
+            // tell the UP i can support UDP connect back
+            MessagesSupportedVendorMessage support = 
+                MessagesSupportedVendorMessage.instance();
+            ULTRAPEER[0].send(support);
+            ULTRAPEER[0].flush();
+
+            byte[] cbGuid = null;
+            int cbPort = -1;
+            while (cbGuid == null) {
+                try {
+                    Message m = ULTRAPEER[0].receive(TIMEOUT);
+                    if (m instanceof UDPConnectBackVendorMessage) {
+                        UDPConnectBackVendorMessage udp = 
+                            (UDPConnectBackVendorMessage) m;
+                        cbGuid = udp.getConnectBackGUID().bytes();
+                        cbPort = udp.getConnectBackPort();
+                    }
+                }
+                catch (Exception ie) {
+                    fail("did not get the UDP CB message!", ie);
+                }
+            }
+
+            // ok, now just do a connect back to the up so unsolicited support
+            // is all set up
+            PingRequest pr = new PingRequest(cbGuid, (byte) 1, (byte) 0);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            pr.write(baos);
+            pack = new DatagramPacket(baos.toByteArray(), 
+                                      baos.toByteArray().length,
+                                      ULTRAPEER[0].getInetAddress(), cbPort);
+            UDP_ACCESS.send(pack);
+        }
+        
+        Thread.sleep(500);
+        assertTrue(ROUTER_SERVICE.isGUESSCapable());
+        assertTrue(ROUTER_SERVICE.isOOBCapable());
+        
+        //no one has sent a MessagesSupportedVM yet so no queries should be
+        //proxied
+        //------------------------------
+        {
+        drainAll();    
+        QueryRequest query = QueryRequest.createQuery("berkeley");
+        sendF(LEAF[0], query);
+        
+        Thread.sleep(1000);
+
+        // the Ultrapeer should get it.
+        QueryRequest queryRec = 
+            (QueryRequest) getFirstInstanceOfMessageType(ULTRAPEER[0],
+                                                         QueryRequest.class);
+        assertNotNull(queryRec);
+        assertEquals(new GUID(query.getGUID()), new GUID(queryRec.getGUID()));
+
+        // shut off query
+        QueryStatusResponse resp = 
+            new QueryStatusResponse(new GUID(queryRec.getGUID()), MAX_RESULTS);
+        sendF(LEAF[0], resp);
+        }
+        //------------------------------
+
+        //now send a MSM and make sure that the query is proxied
+        //------------------------------
+        {
+        drainAll();    
+        sendF(LEAF[0], MessagesSupportedVendorMessage.instance());
+        Thread.sleep(100); // wait for processing of msvm
+
+        QueryRequest query = QueryRequest.createQuery("berkeley");
+        sendF(LEAF[0], query);
+        
+        Thread.sleep(1000);
+
+        // the Ultrapeer should get it and proxy it
+        QueryRequest queryRec = 
+            (QueryRequest) getFirstInstanceOfMessageType(ULTRAPEER[0],
+                                                         QueryRequest.class);
+        assertNotNull(queryRec);
+        assertTrue(queryRec.desiresOutOfBandReplies());
+        byte[] proxiedGuid = new byte[queryRec.getGUID().length];
+        System.arraycopy(queryRec.getGUID(), 0, proxiedGuid, 0, 
+                         proxiedGuid.length);
+        GUID.addressEncodeGuid(proxiedGuid, ROUTER_SERVICE.getAddress(),
+                               ROUTER_SERVICE.getPort());
+        assertEquals(new GUID(proxiedGuid), new GUID(queryRec.getGUID()));
+
+        // shut off query
+        QueryStatusResponse resp = 
+            new QueryStatusResponse(new GUID(queryRec.getGUID()), MAX_RESULTS);
+        sendF(LEAF[0], resp);
+        }
+        //------------------------------
+
+        //now send a OOB query and make sure it isn't proxied
+        //------------------------------
+        {
+        drainAll();    
+        QueryRequest query = 
+        QueryRequest.createOutOfBandQuery("leehsus",
+                                          LEAF[0].getInetAddress().getAddress(),
+                                          LEAF[0].getPort());
+        sendF(LEAF[0], query);
+        
+        Thread.sleep(1000);
+
+        // the Ultrapeer should get it.
+        QueryRequest queryRec = 
+            (QueryRequest) getFirstInstanceOfMessageType(ULTRAPEER[1],
+                                                         QueryRequest.class);
+        assertNotNull(queryRec);
+        assertTrue(queryRec.desiresOutOfBandReplies());
+        assertEquals(new GUID(query.getGUID()), new GUID(queryRec.getGUID()));
+        assertEquals(LEAF[0].getPort(), queryRec.getReplyPort());
+
+        // shut off query
+        QueryStatusResponse resp = 
+            new QueryStatusResponse(new GUID(queryRec.getGUID()), MAX_RESULTS);
+        sendF(LEAF[0], resp);
+        }
+        //------------------------------
+
+
+    }
+
+    
+    private final void sendF(Connection c, Message m) throws Exception {
+        c.send(m);
+        c.flush();
     }
 
 }
