8f9616d530d0e887dca43208cfc00afe8bbf0e27
sdaswani <sdaswani>
2004-02-10 23:21:59 +0000

    tests for MediaType.Aggregator

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/ServerSideMetaQueryTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/ServerSideMetaQueryTest.java
@@ -11,6 +11,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
+import com.sun.java.util.collections.List;
 
 import junit.framework.Test;
 
@@ -96,5 +97,77 @@ public class ServerSideMetaQueryTest extends ClientSideTestCase {
     }
 
     ///////////////////////// Actual Tests ////////////////////////////
+
+    public void testMediaTypeAggregator() throws Exception {
+        {
+        QueryRequest query = 
+            new QueryRequest(GUID.makeGuid(), (byte)3, "whatever", "", null,
+                             null, null, true, Message.N_TCP, false, 0, 0);
+       
+        MediaType.Aggregator filter = MediaType.getAggregator(query);
+        assertNull(filter);
+        }
+
+        int[] flags = new int[6];
+        flags[0] = QueryRequest.AUDIO_MASK;
+        flags[1] = QueryRequest.VIDEO_MASK;
+        flags[2] = QueryRequest.DOC_MASK;
+        flags[3] = QueryRequest.IMAGE_MASK;
+        flags[4] = QueryRequest.WIN_PROG_MASK;
+        flags[5] = QueryRequest.LIN_PROG_MASK;
+        for (int i = 0; i < flags.length; i++) {
+            testAggregator(0 | flags[i]);
+            for (int j = 0; j < flags.length; j++) {
+                if (j == i) continue;
+                testAggregator(0 | flags[i] | flags[j]);
+                for (int k = 0; k < flags.length; k++) {
+                    if (k == j) continue;
+                    testAggregator(0 | flags[i] | flags[j] | flags[k]);
+                    for (int l = 0; l < flags.length; l++) {
+                        if (l == k) continue;
+                        testAggregator(0 | flags[i] | flags[j] | flags[k] |
+                                       flags[l]);
+                        for (int m = 0; m < flags.length; m++) {
+                            if (m == l) continue;
+                            testAggregator(0 | flags[i] | flags[j] | flags[k] |
+                                           flags[l] | flags[m]);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    private void testAggregator(int flag) throws Exception {
+        QueryRequest query = 
+            new QueryRequest(GUID.makeGuid(), (byte)3, "whatever", "", null,
+                             null, null, true, Message.N_TCP, false, 0, flag);
+       
+        MediaType.Aggregator filter = MediaType.getAggregator(query);
+        assertNotNull(filter);
+        List filterList = (List) PrivilegedAccessor.getValue(filter, 
+                                                             "_filters");
+        int numFilters = 0;
+        if ((flag & QueryRequest.AUDIO_MASK) > 0) numFilters++;
+        if ((flag & QueryRequest.VIDEO_MASK) > 0) numFilters++;
+        if ((flag & QueryRequest.DOC_MASK) > 0) numFilters++;
+        if ((flag & QueryRequest.IMAGE_MASK) > 0) numFilters++;
+        if (((flag & QueryRequest.WIN_PROG_MASK) > 0) ||
+            ((flag & QueryRequest.LIN_PROG_MASK) > 0)) numFilters++;
+        assertEquals(numFilters, filterList.size());
+        assertEquals(((flag & QueryRequest.AUDIO_MASK) > 0),
+                     filter.allow("susheel.mp3"));
+        assertEquals(((flag & QueryRequest.VIDEO_MASK) > 0),
+                     filter.allow("susheel.wmv"));
+        assertEquals(((flag & QueryRequest.DOC_MASK) > 0),
+                     filter.allow("susheel.txt"));
+        assertEquals(((flag & QueryRequest.IMAGE_MASK) > 0),
+                     filter.allow("susheel.png"));
+        assertEquals((((flag & QueryRequest.WIN_PROG_MASK) > 0) ||
+                      ((flag & QueryRequest.LIN_PROG_MASK) > 0)),
+                     (filter.allow("susheel.exe") || 
+                      filter.allow("susheel.bin")));
+    }
 
 }
