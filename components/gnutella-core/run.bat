6a09c491b722e774dce24a216e407714b977b599
cvs2svn <admin@example.com>
2004-02-26 19:17:56 +0000

    This commit was manufactured by cvs2svn to create branch 'test-fixing'.

--- components/gnutella-core/run.bat
+++
@@ -1,5 +0,0 @@
-
-set CLASSPATH=.;../lib/jars/collections.jar;../lib/jars/xerces.jar;../lib/jars/MRJToolkitStubs.jar;../lib/jars/logicrypto.jar
-
-@set PATH=%PATH%;../core/bin
-java com.limegroup.gnutella.Main
