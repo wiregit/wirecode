c202f5984767055e9a6f8705b7ecee64338c9f7c
zlatinb <zlatinb>
2004-03-03 16:30:57 +0000

    verifier testing

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/updates/UpdateMessageVerifierTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/updates/UpdateMessageVerifierTest.java
@@ -13,6 +13,11 @@ import com.limegroup.gnutella.util.Expand;
  * Unit tests for UpdateMessageVerifier
  */
 public class UpdateMessageVerifierTest extends BaseTestCase {
+
+	private static final String testXMLPath = "com"+File.separator+
+												"limegroup"+File.separator+
+												"gnutella"+File.separator+
+												"updates"+File.separator;
         
 	public UpdateMessageVerifierTest(String name) {
 		super(name);
@@ -26,21 +31,70 @@ public class UpdateMessageVerifierTest extends BaseTestCase {
 		junit.textui.TestRunner.run(suite());
 	}
 	
+	public void testBadXml() throws Exception {
+		test("bad_xmlFile.xml",false);
+	}
+	
+	public void testRandomBytes() throws Exception {
+		test("random_bytesFile.xml",false);
+	}
+	
+		public void testMiddleVerFile() throws Exception {
+		test("middle_verFile.xml",true);
+	}
+	
+	public void testDefMessageFile() throws Exception {
+		test("def_messageFile.xml",true);
+	}
+	
+	public void testDefVerFile() throws Exception {
+		test("def_verFile.xml",true);
+	}
+	
+
+	public void testOldVerFile() throws Exception {
+		test("old_verFile.xml",true);
+	}
+	
+	public void testNewVerFile() throws Exception {
+		test("new_verFile.xml",true);
+	}
+		
 	public void testLegacy() throws Exception {
 	    Expand.expandFile( getUpdateVer(),
 	                       CommonUtils.getUserSettingsDir() );
-        RandomAccessFile f=new RandomAccessFile(
-            new File(CommonUtils.getUserSettingsDir(), "update.xml"), "r");
-        byte[] content = new byte[(int)f.length()];
-        f.readFully(content);
-        f.close();
-        UpdateMessageVerifier tester = new UpdateMessageVerifier(content,true);
-        assertTrue(tester.verifySource());
+	                       
+	    File _f = new File(CommonUtils.getUserSettingsDir(), "update.xml");
+	   
+	     
+	    test(_f.getName(),true);
+        
+        
     }
     
     private static File getUpdateVer() throws Exception {
                   // tests/TestData    /tests          / ..
         File f = getTestDirectory().getParentFile().getParentFile();
         return new File(f, "gui/update.ver");
+    }
+    
+    private void test(String filename, boolean good) throws Exception{
+    	File _f = new File(testXMLPath+filename);
+    	
+    	assertTrue(_f.exists());
+    	assertGreaterThan(0,_f.length());
+    	
+    	RandomAccessFile f=new RandomAccessFile(_f, "r");
+        
+        byte[] content = new byte[(int)f.length()];
+        f.readFully(content);
+        f.close();
+    	
+    	//System.out.println(new String(content));
+    	UpdateMessageVerifier tester = new UpdateMessageVerifier(content,true);
+    	if (good) 
+         assertTrue(tester.verifySource());
+        else
+         assertFalse(tester.verifySource());
     }
 }
