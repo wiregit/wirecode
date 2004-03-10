8191c86fde36b6858398cd978158f48bf4eb126e
sthadani <sthadani>
2004-03-10 20:18:48 +0000

    Fixing mistakes from merging from bugfix branch 1. WinTab should not have been removed 2. Conflict in ConnectionChecker not resolved correctly -- fixed now.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/connection/ConnectionChecker.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/connection/ConnectionChecker.java
@@ -128,7 +128,6 @@ public final class ConnectionChecker implements Runnable {
         ConnectionChecker checker = new ConnectionChecker();
         Thread connectionThread = 
         new ManagedThread(checker, "check for live connection");
-        connectionThread.setDaemon(false);
         connectionThread.setDaemon(true);
         connectionThread.start();
         return checker;
