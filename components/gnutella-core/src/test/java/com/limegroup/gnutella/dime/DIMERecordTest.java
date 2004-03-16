3a9bf18e95410706422de9296a3f7977bcbab929
sberlin <sberlin>
2004-03-16 01:23:33 +0000

    layout for a dime test.

---
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/dime/DIMERecordTest.java
@@ -0,0 +1,33 @@
+package com.limegroup.gnutella.dime;
+
+import java.io.*;
+
+import junit.framework.Test;
+
+/**
+ * Tests for DIMERecord.
+ */
+public final class DIMERecordTest extends com.limegroup.gnutella.util.BaseTestCase {
+
+	/**
+	 * Constructs a new test instance for responses.
+	 */
+	public DIMERecordTest(String name) {
+		super(name);
+	}
+
+	public static Test suite() {
+		return buildTestSuite(DIMERecordTest.class);
+	}
+
+	/**
+	 * Runs this test individually.
+	 */
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
+	
+	public void testStuff() {
+	    // do tests.
+	}
+}
