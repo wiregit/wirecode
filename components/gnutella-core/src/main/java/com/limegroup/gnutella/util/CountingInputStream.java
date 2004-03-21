a72eb360ea7b59557233e75745623bfe40758ebb
sberlin <sberlin>
2004-03-21 22:50:43 +0000

    forgot to add CountingInputStream  -- new InputStream subclass that just keeps track of how many bytes have been read.

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/util/CountingInputStream.java
@@ -0,0 +1,34 @@
+package com.limegroup.gnutella.util;
+
+import java.io.FilterInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+
+/**
+ * InputStream that counts the number of bytes succesfully read.
+ */
+public final class CountingInputStream extends FilterInputStream {
+    
+    private int _count = 0;
+    
+    public CountingInputStream (final InputStream in) {
+        super(in);
+    }
+    
+    public int read() throws IOException {
+        int read = super.read();
+        _count++;
+        return read;
+    }
+    
+    public int read(byte[] b, int off, int len) throws IOException {
+        int read = super.read(b, off, len);
+        _count += read;
+        return read;
+    }
+    
+    public int getAmountRead() {
+        return _count;
+    }
+    
+} // class
