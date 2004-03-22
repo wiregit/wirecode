781c3a22dcd5c46ce4c080351a3cf7e71ca06dd2
zlatinb <zlatinb>
2004-03-22 18:11:53 +0000

    tests for the best candidate table

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/upelection/BestCandidatesTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/upelection/BestCandidatesTest.java
@@ -45,8 +45,15 @@ public class BestCandidatesTest extends BaseTestCase {
 	
 	protected void setUp() {
 		
+		try {
+			BestCandidates instance = (BestCandidates) PrivilegedAccessor.getValue(BestCandidates.class,"instance");
+			PrivilegedAccessor.setValue(instance,"_best", new Candidate[3]);
+		}catch(Exception what) {
+			fail("could not (re)set the candidates table",what);
+		}
+		
 		//create the set of test candidates
-		_candidates = new TreeSet(ExtendedEndpoint.priorityComparator());
+		_candidates = new TreeSet(Candidate.priorityComparator());
 		try {
 			for (int i = 0;i<_candidateStats.length;i++)
 				_candidates.add(new Candidate(_candidateStats[i]));
@@ -83,5 +90,128 @@ public class BestCandidatesTest extends BaseTestCase {
 		
 		//associate more as needed
 		
+		//test if we have only one candidate
+		assertNotNull(BestCandidates.getCandidates()[0]);
+		assertNull(BestCandidates.getCandidates()[1]);
+		assertNull(BestCandidates.getCandidates()[2]);
 	}
+	
+	/**
+	 * an ultrapeer advertises a single candidate which is worse than our current best.
+	 * Since we have no best candidate at ttl 1 we should still enter it in the table.
+	 */
+	public void testUpdateSingleCandidate() throws Exception {
+		//reset the table.
+		setUp();
+		
+	
+		assertEquals(mediocreCandidate.getInetAddress(), BestCandidates.getBest().getInetAddress());
+		
+		//update with the worse candidate at ttl 1.  It should enter the table but
+		//the best overall candidate should still be our previous candidate.
+		Candidate []update = new Candidate[2];
+		update[0] =badCandidate;
+		update[1] =null;
+		
+		BestCandidates.update(update);
+		
+		assertNotNull(BestCandidates.getCandidates()[0]);
+		assertNotNull(BestCandidates.getCandidates()[1]);
+		assertNull(BestCandidates.getCandidates()[2]);
+		
+		assertEquals(mediocreCandidate.getInetAddress(), BestCandidates.getBest().getInetAddress());
+		
+		//now update with the best candidate at ttl 1.  Our mediocre candidate should 
+		//still be in the table, but the bad candidate on ttl 1 should be removed.
+		
+		update[0]=goodCandidate;
+		
+		BestCandidates.update(update);
+		
+		assertNotNull(BestCandidates.getCandidates()[0]);
+		assertNotNull(BestCandidates.getCandidates()[1]);
+		assertNull(BestCandidates.getCandidates()[2]);
+		
+		assertEquals(goodCandidate.getInetAddress(),BestCandidates.getCandidates()[1].getInetAddress());
+		assertEquals(goodCandidate.getInetAddress(), BestCandidates.getBest().getInetAddress());
+		
+		//now try to update again with a not-so-good candidate.  the table should not be changed.
+		
+		update[0]=mediocreCandidate;
+		
+		BestCandidates.update(update);
+		
+		assertNotNull(BestCandidates.getCandidates()[0]);
+		assertNotNull(BestCandidates.getCandidates()[1]);
+		assertNull(BestCandidates.getCandidates()[2]);
+		
+		assertEquals(goodCandidate.getInetAddress(),BestCandidates.getCandidates()[1].getInetAddress());
+		assertEquals(goodCandidate.getInetAddress(), BestCandidates.getBest().getInetAddress());
+		
+	}
+	
+	/**
+	 * tests the simple getBest scenario
+	 */
+	public void testGetBest() throws Exception {
+		setUp();
+		
+		//put the worst guy at ttl 1 and the best guy at ttl 2
+		Candidate [] update = new Candidate [2];
+		update[0]=badCandidate;
+		update[1]=goodCandidate;
+		
+		BestCandidates.update(update);
+		
+		//all ranges should be full
+		assertNotNull(BestCandidates.getCandidates()[0]);
+		assertNotNull(BestCandidates.getCandidates()[1]);
+		assertNotNull(BestCandidates.getCandidates()[2]);
+		
+		//and the best one should be at ttl 2
+		
+		assertEquals(BestCandidates.getBest().getInetAddress(),
+				BestCandidates.getCandidates()[2].getInetAddress());
+	}
+	
+	/**
+	 * updates the candidates table with the same candidate at ttl 1 2 and 3.
+	 * then it repeats the process with a better candidate.
+	 */
+	public void testUpdateSameCandidate() throws Exception{
+		setUp();
+		Candidate []update = new Candidate[2];
+		update[0]= mediocreCandidate;
+		update[1]=update[0];
+		
+		BestCandidates.update(update);
+		
+		//all ranges should be full
+		assertNotNull(BestCandidates.getCandidates()[0]);
+		assertNotNull(BestCandidates.getCandidates()[1]);
+		assertNotNull(BestCandidates.getCandidates()[2]);
+		
+		//and the best candidate should be the same as all others
+		Candidate best = BestCandidates.getBest();
+		assertEquals(best.getInetAddress(),
+				BestCandidates.getCandidates()[0].getInetAddress());
+		assertEquals(best.getInetAddress(),
+				BestCandidates.getCandidates()[1].getInetAddress());
+		assertEquals(best.getInetAddress(),
+				BestCandidates.getCandidates()[2].getInetAddress());
+		
+		//now update the ttl 1 and 2 candidates to a better guy
+		update[0]=goodCandidate;
+		update[1]=update[0];
+		
+		//and the best candidate should be identical for ttl 1 and 2
+		best = BestCandidates.getBest();
+		assertEquals(best.getInetAddress(),
+				BestCandidates.getCandidates()[1].getInetAddress());
+		assertEquals(best.getInetAddress(),
+				BestCandidates.getCandidates()[2].getInetAddress());
+	}
+	
+	
+	
 }
