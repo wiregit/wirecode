c60db9f573bb05f2d185172f8df0cdd0284d04a4
zlatinb <zlatinb>
2004-03-23 00:15:40 +0000

    one more test

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/ServerSideBestCandidatesTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/ServerSideBestCandidatesTest.java
@@ -1,6 +1,9 @@
 /*
  * This class tests the serverside functionality of the Best Candidates propagation.
  * The actual table management is tested in a unit test, so this a black box test
+ * 
+ * Since there are hardcoded timeouts in several places which influence the decision whether
+ * a given leaf is a candidate or not, please do not change the order of the tests.  
  */
 package com.limegroup.gnutella;
 
@@ -10,6 +13,7 @@ import com.limegroup.gnutella.util.*;
 import com.limegroup.gnutella.*;
 import com.limegroup.gnutella.messages.*;
 import com.limegroup.gnutella.messages.vendor.*;
+import com.limegroup.gnutella.upelection.*;
 
 import junit.framework.Test;
 
@@ -51,7 +55,7 @@ public class ServerSideBestCandidatesTest extends ServerSideTestCase {
 		Class advertiser = PrivilegedAccessor.getClass(MessageRouter.class,"CandidateAdvertiser");
 		Runnable r = (Runnable)PrivilegedAccessor.invokeConstructor(advertiser,new Object[0],null);
 		RouterService.schedule (r, 4000, 300);
-		
+
 	}
 	
 	public static void setSettings() throws Exception {
@@ -77,18 +81,82 @@ public class ServerSideBestCandidatesTest extends ServerSideTestCase {
 	 */
 	public void testPropagation() throws Exception {
 		
-		Message m;
+
 		
+		drainInitialVMs();
+		
+		//sleep some time, let the leaf connection age a little
+		try {Thread.sleep(7100);}catch(InterruptedException iex){}
+		
+		BestCandidatesVendorMessage bcvm = readMessage();
+		
+		//compare the received message with a Candidate derived from the originally connected leaf
+		Candidate expectedCandidate = new Candidate(LEAF[0]);
+		
+		Candidate []receivedUpdate = bcvm.getBestCandidates();
+		
+		assertEquals(expectedCandidate.getInetAddress(),receivedUpdate[0].getInetAddress());
+		
+		//the best candidate at ttl 2 should be null
+		assertNull(receivedUpdate[1]);
+		
+		
+	}
+	
+	
+	/**
+	 * the current best candidate goes offline, so the next update should contain
+	 * the second best leaf.
+	 */
+	public void testChangingMind() throws Exception {
+		
+		//connect the second leaf now.
+		_secondLeaf.initialize();
+		
+		//wait some time
+		try {Thread.sleep(2100);}catch(InterruptedException iex){}
+		
+		//close the leaf
+		LEAF[0].close();
+		
+		//make sure the second leaf is open and initialized
+		assertTrue(_secondLeaf.isOpen());
+		assertTrue(_secondLeaf.isInitialized());
+		
+		//wait some more time
+		try {Thread.sleep(3000);}catch(InterruptedException iex){}
+		
+		BestCandidatesVendorMessage bcvm = readMessage();
+		Candidate []update = bcvm.getBestCandidates();
+		
+		//verify that there is still only one update
+		assertNull(update[1]);
+		
+		//and that it is equivalent to the new leaf
+		Candidate expectedCandidate = new Candidate(_secondLeaf);
+		
+		assertEquals(expectedCandidate.getInetAddress(),update[0].getInetAddress());
+		
+		//close the second leaf, we don't need it anymore
+		_secondLeaf.close();
+		//TODO: don't forget to re-initialize the first leaf before the next test, otherwise setUp fails.
+	}
+	
+	
+	private void drainInitialVMs() throws Exception {
+		Message m;
 		//drain the capabilities and support vms on all UPs
 		for (int i = 0;i<ULTRAPEER.length;i++) {
 			m = ULTRAPEER[i].receive(120);
 			m = ULTRAPEER[i].receive(120);
 		}
-		
-		//sleep some time, let the leaf connection age a little
-		try {Thread.sleep(7100);}catch(InterruptedException iex){}
-		
-		
+	}
+	
+	/**
+	 * reads a VM on each ultrapeer and checks if its the right kind
+	 */
+	private BestCandidatesVendorMessage readMessage() throws Exception {
+		Message m=null;
 		//make sure each UP gets at least one message
 		for (int i=0;i<ULTRAPEER.length;i++) {
 			try {
@@ -103,6 +171,7 @@ public class ServerSideBestCandidatesTest extends ServerSideTestCase {
 				fail("ultrapeer "+i+" out of  "+ULTRAPEER.length+" should have received at least one message");
 			}
 		}
+		return (BestCandidatesVendorMessage)m;
 		
 	}
 }
