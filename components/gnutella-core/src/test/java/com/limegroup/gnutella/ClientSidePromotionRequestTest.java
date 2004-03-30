ba0a54e375cb07c8331fccf9702355f44171a28b
zlatinb <zlatinb>
2004-03-30 17:10:25 +0000

    test as you go :)

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/ClientSidePromotionRequestTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/ClientSidePromotionRequestTest.java
@@ -10,7 +10,12 @@ import com.limegroup.gnutella.messages.*;
 import com.limegroup.gnutella.stubs.*;
 import com.limegroup.gnutella.messages.vendor.*;
 
+import java.net.*;
+import java.io.*;
+
 public class ClientSidePromotionRequestTest extends ClientSideTestCase {
+	
+	static DatagramSocket _socket;
 	
 	static PromotionRequestVendorMessage wrongLeaf, promotingLeaf;
 	
@@ -39,7 +44,7 @@ public class ClientSidePromotionRequestTest extends ClientSideTestCase {
 				1);
 		promotingLeaf = new PromotionRequestVendorMessage(
 				new QueryReply.IPPortCombo("127.0.0.1",SERVER_PORT),
-				new QueryReply.IPPortCombo("1.2.3.4",15),
+				new QueryReply.IPPortCombo("127.0.0.1",8000),
 				2);
 	}
 	
@@ -55,9 +60,40 @@ public class ClientSidePromotionRequestTest extends ClientSideTestCase {
 	}
 	
 	public void testPromotingLeaf() throws Exception {
+		
+		_socket = new DatagramSocket(8000);
+		_socket.setSoTimeout(500);
+		
+		//send the promotion message
+		RouterService.getMessageRouter().handlePromotionRequestVM(promotingLeaf,UltrapeerStub);
+			
+		//listen for the ping
+		DatagramPacket ping = new DatagramPacket(new byte[1000],1000);
+		
+		_socket.receive(ping);
+		
+		ByteArrayInputStream bais = new ByteArrayInputStream(ping.getData());
+		LimeACKVendorMessage challenge=null;
 		try {
-			RouterService.getMessageRouter().handlePromotionRequestVM(promotingLeaf,UltrapeerStub);
-			fail("leaf should have started promotion process");
-		} catch (RuntimeException expected) {}
+			challenge = (LimeACKVendorMessage)Message.read(bais);
+		} catch (BadPacketException bpe) {
+			fail(" could not parse the ping", bpe);
+		}
+		
+		//verify the ack request
+		assertEquals(new String(promotingLeaf.getGUID()), new String(challenge.getGUID()));
+		assertEquals(0, challenge.getNumResults());
+		
+		//create our response
+		LimeACKVendorMessage response = new LimeACKVendorMessage(new GUID(challenge.getGUID()),0);
+		
+		//call the handle method with the wrong datagram - shouldn't matter.
+		//note we call the main handle method, otherwise the listener won't get notified.
+		try {
+			RouterService.getMessageRouter().handleUDPMessage(response,ping);
+			fail("should have thrown.");
+		}catch (RuntimeException expected) {
+			
+		}
 	}
 }
