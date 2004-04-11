1ec93d39e3494bbc11ad780dd370b3fbf3871e88
gbildson <gbildson>
2004-04-11 21:57:21 +0000

    debugging

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/udpconnect/UDPConnectionProcessor.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/udpconnect/UDPConnectionProcessor.java
@@ -567,14 +567,17 @@ log("send :"+msg+" ip:"+_ip+" p:"+_port+" t:"+_lastReceivedTime);
      *  If no acks are pending, then do nothing.
      */
     private synchronized void scheduleAckIfNeeded() {
+System.out.println("------scheduleAckIfNeeded");
         DataRecord drec = _sendWindow.getOldestUnackedBlock();
         if ( drec != null ) {
             int rto         = _sendWindow.getRTO();
 			if (rto == 0) 
 				rto = (int) DEFAULT_RTO_WAIT_TIME;
             long waitTime    = drec.sentTime + ((long)rto);
+System.out.println("------scheduled");
             scheduleAckTimeoutEvent(waitTime);
         } else {
+System.out.println("------unscheduled");
             unscheduleAckTimeoutEvent();
         }
     }
