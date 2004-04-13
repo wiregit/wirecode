08bc4b756ef247f5111739aa708df397dfb15fa0
zlatinb <zlatinb>
2004-04-13 16:57:40 +0000

    now you're on the right branch

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/upelection/PromotionManager.java
@@ -0,0 +1,234 @@
+/*
+ * This class maintains state for the election process.
+ * 
+ * TODO: Decide what to do when a promotion request arrives while we are currently promoting
+ * ourselves.  Keep in mind the best leaf is consistent within ttl 2, which is 30x30 = 900 Ultrapeers!
+ * This means the best candidate may end up receiving requests quite often.
+ * 
+ * So either A) add the promoter to the list because we know for sure that
+ * that UP needs is running out of slots, so we will connect to it or B) ignore it. 
+ * 
+ */
+package com.limegroup.gnutella.upelection;
+
+import com.limegroup.gnutella.util.*;
+import com.limegroup.gnutella.*;
+import com.limegroup.gnutella.messages.vendor.*;
+import com.limegroup.gnutella.settings.*;
+
+import com.sun.java.util.collections.*;
+
+public class PromotionManager {
+	
+	/**
+	 * a list of <tt>Endpoint</tt>'s of people requesting promotion.  
+	 */
+	private List _requestors = Collections.synchronizedList(new ArrayList());
+	
+	/**
+	 * if we don't get a pong from the request[or|ee] within 30 secs, discard.
+	 */
+	private static final long REQUEST_TIMEOUT = 30*1000;
+	
+	/**
+	 * a ref to the thread which resets the state
+	 */
+	private Thread _expirer;
+
+	/**
+	 * keeps a list of the people who advertised their best candidate.  
+	 * These should be only neighboring ultrapeers.  
+	 * Furthermore they shouldn't do it too often. 
+	 * TODO: export the values to the proper constants. 
+	 * Note: the also is not final so that the tests won't take forever.
+	 */
+	private FixedSizeExpiringSet _candidateAdvertisers = 
+		new FixedSizeExpiringSet(ConnectionManager.ULTRAPEER_CONNECTIONS, 14 * 60 * 1000);
+	
+	/**
+	 * object to lock on when manipulating this's data structures.
+	 * its easier this way rather than exporting the sync blocks in their own methods,
+	 * and since these methods will be called from core threads they cannot be 
+	 * completely synchronized.
+	 */
+	private Object _promotionLock = new Object();
+	/**
+	 * the endpoint of either the requestor or the candidate for promotion LOCKING: obtain _promotionLock
+	 */
+	private Endpoint _promotionPartner;
+	/**
+	 * keeps a list of the people who have requested our connection lists. used to make sure we don't get ping-flooded. not final so that tests won't take forever.
+	 */
+	private FixedSizeExpiringSet _UDPListRequestors = new FixedSizeExpiringSet(200, 5 * 1000);
+	/**
+	 * some constants related to the propagation of the best candidates.   should eventually be determined by experiments.
+	 */
+	public final static int CANDIDATE_PROPAGATION_DELAY = 2 * 60 * 60 * 1000;
+	public final static int CANDIDATE_PROPAGATION_INTERVAL = 15 * 60 * 1000;
+	
+	/**
+	 * initiates the promotion process.  It sends out an udp ping to the
+     * original requestor and when the ack comes back it the listener will 
+     * start the crawl in a separate thread.
+     * 
+     * TODO: decide what to do if a new request arrives while we're already promoting.
+     */
+    public void initiatePromotion(PromotionRequestVendorMessage msg) {
+    	
+    	//set the promotion partner
+    	Endpoint requestor = new Endpoint (
+				msg.getRequestor().getAddress(),
+				msg.getRequestor().getPort());
+    	
+    	synchronized(_promotionLock) {
+    		if (_promotionPartner == null)
+    			_promotionPartner = requestor; 
+    		else
+    			_requestors.add(requestor);
+    	}
+    	
+    	
+    	//schedule the expiration of the process
+    	_expirer = new ManagedThread(new Expirer());
+    	_expirer.setDaemon(true);
+    	_expirer.start();
+    	
+    	//ping the original requestor
+    	
+    	PromotionACKVendorMessage ping = new PromotionACKVendorMessage();
+    	UDPService.instance().send( ping, _promotionPartner);
+    }
+	/**
+	 * @return Returns the _candidateAdvertisers.
+	 */
+	public FixedSizeExpiringSet getCandidateAdvertisers() {
+		return _candidateAdvertisers;
+	}
+	/**
+	 * @return Returns the _UDPListRequestors.
+	 */
+	public FixedSizeExpiringSet getUDPListRequestors() {
+		return _UDPListRequestors;
+	}
+	
+	/**
+	 * @return whether this node is currently in process of promoting itself.
+	 */
+	public boolean isPromoting() {
+		synchronized(_promotionLock) {
+			return _promotionPartner!=null;
+		}
+	}
+	
+	public boolean isPromoting(String host, int port) {
+		synchronized(_promotionLock) {
+			if (_promotionPartner == null)
+				return false;
+			return (_promotionPartner.getAddress().equals(host) &&
+				_promotionPartner.getPort() == port ) ||
+				_promotionPartner.getInetAddress().isLoopbackAddress(); //for testing
+		}
+	}
+	
+	public void handleACK(PromotionACKVendorMessage message, Endpoint sender) {
+		//cache the current status
+		boolean isSupernode = RouterService.isSupernode();
+		
+		//first see if anyone is indeed a promotion partner
+    	
+    	
+    	synchronized(_promotionLock) {
+    		if (_promotionPartner == null)  
+    			return;
+    	
+    		//check if we received the ACK from the right person
+    		//also allow for loopback addresses for testing 
+    		if (!sender.equals(_promotionPartner) && 
+    				!sender.getInetAddress().isLoopbackAddress())
+    			return;
+    			
+    		//set the promotion partner to null
+    		//have an extra check here to avoid re-acquiring the lock
+			if (!isSupernode)
+				_promotionPartner=null;
+    	}
+    	
+    	//*************************
+    	//we know we have received a proper ACK.
+    	//stop the expiration thread
+    	_expirer.interrupt();
+    	_expirer = null;    	
+    	
+    	
+    	//then, proceed as appropriate:
+    	
+    	//if we are a leaf, start the promotion process
+    	if (!isSupernode) {
+    		Thread promoter = new ManagedThread(
+    				new Promoter(sender));
+    		promoter.setDaemon(true);
+    		promoter.start();
+    	} 
+    	else {
+    		//we are the originally requesting UP, ACK back.
+    		PromotionACKVendorMessage pong = new PromotionACKVendorMessage();
+    		UDPService.instance().send(pong, sender);
+			
+			//postpone the timeout of the promotion for a while
+			_expirer = new ManagedThread(new Expirer());
+			_expirer.setDaemon(true);
+			_expirer.start();
+    	}
+	}
+	
+	/**
+	 * sends a promotion request to the best candidate.
+	 */
+	public void requestPromotion() {
+		
+		Endpoint candidate = new Endpoint (BestCandidates.getBest().getInetAddress().getAddress(),
+							BestCandidates.getBest().getPort());
+		
+		synchronized(_promotionLock) {
+			//check if we are already requesting promotion and if so,
+			if (_promotionPartner!=null)
+				return;
+			
+			//set the promotion partner 
+			_promotionPartner = candidate;
+			
+			//start a resetting thread
+			_expirer = new ManagedThread(new Expirer());
+			_expirer.setDaemon(true);
+			_expirer.start();
+		}
+		
+		//*******
+		//send a PromotionRequestVM to the appropriate route
+		PromotionRequestVendorMessage msg = new PromotionRequestVendorMessage(BestCandidates.getBest());
+		
+		RouterService.getMessageRouter().forwardPromotionRequest(msg);	
+		
+	}
+	
+	/**
+	 * expires the partner if we don't get an ACK.
+	 * interrupt to cancel.
+	 * 
+	 */
+	private class Expirer implements Runnable {
+		public void run() {
+			try {
+				//sleep some time
+				Thread.sleep(REQUEST_TIMEOUT);
+			
+				//clear the state
+				synchronized(_promotionLock) {
+					_promotionPartner=null;
+				}
+			}catch(InterruptedException iex) {
+				//end the thread.
+			}
+		}
+	}
+}
---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/upelection/RadialCrawler.java
@@ -0,0 +1,349 @@
+/*
+ * A crawler which crawls out in a radial fashion - it attempts to find a set of peers which
+ * are at a specific distance from the crawler and are separated from each other like this:
+ *
+ *X-hit, 0-skip, S-start
+ * 
+ *             X
+ *            /|\
+ *             0
+ *             |
+ * X<----0<----S---->0----->X
+ *             |
+ *             0
+ *            \|/
+ *             X
+ * 
+ * If the user has provided an initial BlackList, the crawl starts from those nodes.  
+ * Otherwise the crawl starts from the directly connected ultrapeers.
+ */
+ 
+package com.limegroup.gnutella.upelection;
+
+import com.limegroup.gnutella.*;
+import com.limegroup.gnutella.messages.*;
+import com.limegroup.gnutella.messages.vendor.*;
+
+import com.sun.java.util.collections.*;
+
+
+public class RadialCrawler implements AsyncCrawler, BlacklistCrawler {
+
+	/**
+	 * flag whether we are currently crawling or not.
+	 */
+	private volatile boolean _crawling = false;
+	
+	/**
+	 * a dummy callback in case the crawler will be used in blocking mode
+	 */
+	private CrawlerClient _client;
+	/**
+	 * the breadth degree of the crawl.  -1 means all (32 in our case)
+	 */
+	private volatile int _degree;
+	
+	private static final int MAX_DEGREE=-1;
+	
+	private int _desiredTTL = Crawler.TTL;
+	
+	/**
+	 * the desired number of results.  To be safe we try to fetch 1.5 times as many.
+	 */
+	private int _desiredResults = (int)(Crawler.DEFAULT_RESULTS * 1.5);
+	
+	/**
+	 * the blacklisted nodes.  Synced because the user may add more nodes on-the-fly
+	 */
+	private Set _blackList = Collections.synchronizedSet(new HashSet());
+	
+	/**
+	 * the results of the crawl.
+	 */
+	private Set _results = new HashSet();
+	
+	/**
+	 * a second list of currently active guids.  Necessary for the stopCrawl().
+	 */
+	private Collection _registeredGuids = new LinkedList();
+	
+	/**
+	 * this worker class listens for the udp pong containing the results
+	 * of a specific UDP ping.  It updates the _blacklist set with the nodes which
+	 * have already been crawled, and then it schedules more pings
+	 * and registers other instances of itself to handle them.
+	 * 
+	 */
+	private class CrawlerWorker implements MessageListener {
+		
+		/**
+		 * the udp pong containing the list of ultrapeers
+		 */
+		private UPListVendorMessage _uplvm;
+		
+		/**
+		 * the ttl of this specific worker.  When it reaches 0, the results will
+		 * be added to the final set of results.
+		 */
+		private final int _ttl;
+		
+		
+		/**
+		 * creates a new instance with the specified ttl.
+		 * @param ttl the depth this crawler should go on to.
+		 */
+		public CrawlerWorker(int ttl) {
+			if (ttl < 0)
+				throw new IllegalArgumentException("starting a worker with negative ttl == bad idea!");
+			_ttl = ttl;
+		}
+		
+		/**
+		 * this method is called from the udp receiver thread - so it will be quick.
+		 */
+		public void processMessage(Message m) {
+			
+			//remove myself from both lists of listeners
+			RouterService.getMessageRouter().unregisterMessageListener(new GUID(m.getGUID()));
+			
+			//check if the crawl has ended in the meantime
+			if (!_crawling)
+				return;
+			
+			//check if the crawler has enough results, if yes, stop.
+			if (_results.size() >= _desiredResults)
+				stopCrawl();
+			
+			//check if we got called with the proper type of message
+			if (! (m instanceof UPListVendorMessage)) 
+				throw new Error("peer responded with the wrong kind of message"); //TODO: decide what to do in this case
+			
+			_uplvm = (UPListVendorMessage)m;
+		
+			//first thing to do is remove ourselves from our own list of GUIDs
+			_registeredGuids.remove(new GUID(_uplvm.getGUID()));
+			
+			//extract the results the remote host returned.
+			List results = _uplvm.getUltrapeers();
+			
+			//1. Remove anybody already on the blacklist from the results.
+			results.removeAll(_blackList);
+			
+			//2. Add our results to the blacklist so that other instances won't
+			//crawl them.
+			_blackList.addAll(results);
+			
+			//3. if this is our last ttl, add our results to the final results and return.
+			if (_ttl <= 0) {
+				_results.addAll(results);
+				return;
+			}
+			
+			//4. otherwise, schedule a ping to the people in our list
+			//and register a listener for the corresponding pong.
+			for (Iterator iter = results.iterator();iter.hasNext();) {
+				
+				//the target to crawl
+				Endpoint target = (Endpoint)iter.next();
+				
+				//the guid to listen the pong for
+				GUID key = new GUID(GUID.makeGuid());
+				
+				//the message to send (we do not request any leafs)
+				GiveUPVendorMessage gupvm = new GiveUPVendorMessage(key,_degree,0,GiveUPVendorMessage.PLAIN);
+				
+				//register a new listener with ttl one less than ours
+				RouterService.getMessageRouter().registerMessageListener(key,
+						new CrawlerWorker(_ttl-1));
+				
+				//also add the guid to our own set of registered GUIDs
+				_registeredGuids.add(key);
+				
+				//schedule the message to be sent.
+				UDPService.instance().send(gupvm,target.getInetAddress(),target.getPort());
+			}
+			
+		}
+	}
+	
+	/**
+	 * all crawls are async; this class simply emulates the sync crawls.
+	 */
+	private class DummyClient implements CrawlerClient {
+		
+		/* (non-Javadoc)
+		 * @see com.limegroup.gnutella.upelection.CrawlerClient#crawlFinished(com.sun.java.util.collections.Collection)
+		 */
+		public synchronized void crawlFinished(Collection results) {
+			// at this point the results will already be assigned
+			// just in case compare them
+			if (results.equals(_results));
+			notifyAll();
+		}
+		
+		/* (non-Javadoc)
+		 * @see com.limegroup.gnutella.upelection.CrawlerClient#crawlStarted()
+		 */
+		public void crawlStarted() {
+			// nothing
+		}
+}
+	/**
+	 * gets the degree of the crawler.  Shouldn't really be used other than for
+	 * testing or for writing advanced crawlers
+	 * @return the degree
+	 */
+	public int getDegree() {
+		return _degree;
+	}
+	/**
+	 * sets the degree of the crawler.  Shouldn't really be used other than for
+	 * testing or for writing advanced crawlers
+	 * @param newDegree the new degree.
+	 */
+	public void setDegree(int newDegree) {
+		_degree = newDegree;
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.limegroup.gnutella.upelection.AsyncCrawler#crawlAsync(com.limegroup.gnutella.upelection.CrawlerClient, int)
+	 */
+	public void crawlAsync(CrawlerClient callback, int depth) {
+		_desiredTTL=depth;
+		crawlAsync(callback);
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.limegroup.gnutella.upelection.AsyncCrawler#crawlAsync(com.limegroup.gnutella.upelection.CrawlerClient)
+	 */
+	public void crawlAsync(CrawlerClient callback) {
+		
+		_client = callback;
+		//Unless the user has specified a degree of the radial crawl, we need to 
+		//calculate it based on the number of desired results and the ttl.
+		//degree = desired results ^ 1/ttl.  The default values produce a degree of 3:
+		//48 results ^ 1/4 ttl = 2.65 => 3 degrees.
+		if (_degree == 0) 
+			_degree = (int)Math.round(Math.pow(_desiredResults,1/_desiredTTL));
+		
+		//then check if the user has provided entry points.  If not, use our own ultrapeers
+		if (_blackList.size()==0)
+			for (Iterator iter = RouterService.getConnectionManager().getConnectedGUESSUltrapeers().iterator();
+				iter.hasNext();) {
+				Connection c = (Connection)iter.next();
+				_blackList.add(new Endpoint(c.getAddress(),c.getPort()));
+			}
+			
+		//send the initial pings to these entry points on-thread
+		//and schedule workers for their results
+		for (Iterator iter = _blackList.iterator();iter.hasNext();) {
+			
+			Endpoint target = (Endpoint)iter.next();
+			
+			//create a new vendor message
+			GUID key = new GUID(GUID.makeGuid());
+			GiveUPVendorMessage gupvm = new GiveUPVendorMessage(key,_degree,0,GiveUPVendorMessage.PLAIN);
+			
+			//register on both lists
+			_registeredGuids.add(key);
+			RouterService.getMessageRouter().registerMessageListener(key,
+					new CrawlerWorker(_desiredTTL));
+			
+			UDPService.instance().send(gupvm,target.getInetAddress(),target.getPort());
+		}
+		
+		//notify the callback we have started the crawl
+		_client.crawlStarted();
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.limegroup.gnutella.upelection.AsyncCrawler#stopCrawl()
+	 */
+	public void stopCrawl() {
+		
+		// the simplest way to stop a crawl is to unregister all registered GUIDS.
+		for (Iterator iter = _registeredGuids.iterator();iter.hasNext();) {
+			GUID current = (GUID)iter.next();
+			RouterService.getMessageRouter().unregisterMessageListener(current);
+		}
+		
+		//reset the crawling flag
+		_crawling= false;
+		
+		//notify any threads waiting in blocking crawl mode
+		if (_client!=null) {
+			synchronized(_client) {
+				_client.notifyAll();
+			}
+			_client.crawlFinished(_results);
+		}
+		
+		_client=null;
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.limegroup.gnutella.upelection.BlacklistCrawler#resetBlacklist()
+	 */
+	public void resetBlacklist() {
+		_blackList.clear();
+	}
+	/* (non-Javadoc)
+	 * @see com.limegroup.gnutella.upelection.BlacklistCrawler#updateBlacklist(com.limegroup.gnutella.Endpoint)
+	 */
+	public void updateBlacklist(Endpoint node) {
+		_blackList.add(node);
+	}
+	/* (non-Javadoc)
+	 * @see com.limegroup.gnutella.upelection.BlacklistCrawler#updateBlacklist(com.sun.java.util.collections.Set)
+	 */
+	public void updateBlacklist(Set blacklist) {
+		_blackList.addAll(blacklist);
+	}
+	/* (non-Javadoc)
+	 * @see com.limegroup.gnutella.upelection.Crawler#crawl()
+	 */
+	public Collection crawl() throws Exception {
+		return doBlockingCrawl();
+	}
+	/* (non-Javadoc)
+	 * @see com.limegroup.gnutella.upelection.Crawler#crawl(int)
+	 */
+	public Collection crawl(int ttl) throws Exception {
+		_desiredTTL=ttl;
+		return crawl();
+	}
+	/* (non-Javadoc)
+	 * @see com.limegroup.gnutella.upelection.Crawler#setDesiredResults(int)
+	 */
+	public void setDesiredResults(int number) {
+		_desiredResults = (int)(1.5*number);
+	}
+	
+	/**
+	 * emulates a blocking crawl by creating a dummy client and waiting
+	 * until it finishes
+	 * @return Collection full of Endpoints if the crawl is successful
+	 * @throws Exception if some exception happened on the crawling thread.
+	 */
+	private Collection doBlockingCrawl() throws Exception {
+		
+		if (_client!=null)
+			throw new IllegalStateException("crawler already running.");
+		
+		_client = new DummyClient();
+		crawlAsync(_client);
+		synchronized(_client) {
+			try {
+				_client.wait();
+			}catch (InterruptedException ignored) {  
+				//ignore, just return the results.
+			}
+		}
+		//at this point the crawl is finished or got interrupted
+		
+		//if we got here the crawl has been successful
+		return _results;
+	}
+	
+	
+}
