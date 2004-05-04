8da0aab09e2725f84eb3e0db4de48cad87bb17c2
zlatinb <zlatinb>
2004-05-04 21:46:07 +0000

    BCVM handling test update

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/upelection/BCVMHandlingTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/upelection/BCVMHandlingTest.java
@@ -16,24 +16,16 @@ import junit.framework.Test;
  * 
  * 1.  We have never sent a message on this connection before
  *     and are sending a new one.
- * 2.  We have never sent a message on this connection before
- *     and are calling it for maintenance.
- * 3.  We have sent a message, the minimum interval has passed
+ * 2.  We have sent a message, the minimum interval has passed
  *     and now we send another message.
- * 4.  We have sent a message, the minimum interval has passed
+ * 3.  We have sent a message, the minimum interval has passed
  *     and now we send the same message.
- * 5.  We have sent a message, the minimun interval has passed
- *     and now we are calling it for maintenance, no update needed.
- * 6.  We have sent a message, the minimun interval has passed
- *     and now we are calling it for maintenance, update is needed.
- * 7.  The minimum interval has not passed and we are calling to send
+ * 4.  The minimum interval has not passed and we are calling to send
  *     a new message. (twice)
- * 8.  The minimum interval has not passed and we are calling 
+ * 5.  The minimum interval has not passed and we are calling 
  *     to send the same message.
- * 9.  The minimum interval has not passed, no update needed and we are calling
- * 	   for maintenance.
- * 10.  The minimum interval has not passed, update is needed and we are calling
- * 	   for maintenance.
+ * 6.  The minimum interval has passed, update is needed and we are
+ *     calling to send the same message
  * 
  * And a few more when receiving them:
  * 
@@ -77,7 +69,7 @@ public class BCVMHandlingTest extends BaseTestCase {
 			_bcvm2 = new BestCandidatesVendorMessage(update);
 			
 			
-			PrivilegedAccessor.setValue(Connection.class,"ADVERTISEMENT_INTERVAL",
+			PrivilegedAccessor.setValue(CandidateHandler.class,"ADVERTISEMENT_INTERVAL",
 					new Long(NEW_INTERVAL));
 			
 		}catch(Exception e) {
@@ -109,24 +101,6 @@ public class BCVMHandlingTest extends BaseTestCase {
 		assertGreaterThan(0,_connection.getLastAdvertisementTime());
 	}
 	
-	/**
-	 *  2.  We have never sent a message on this connection before
-	 *      and are calling it for maintenance. 
-	 */
-	public void testNewAdvertisementNull() throws Exception {
-		assertNull(_connection.getLastSent());
-		assertFalse(_connection.needsAdvertisement());
-		assertNull(_connection.getCandidatesSent());
-		assertEquals(0,_connection.getLastAdvertisementTime());
-		
-		//if this connection gets called for maintenance, nothing should have changed.
-		_connection.handleBestCandidatesMessage(null);
-		
-		assertNull(_connection.getLastSent());
-		assertFalse(_connection.needsAdvertisement());
-		assertNull(_connection.getCandidatesSent());
-		assertEquals(0,_connection.getLastAdvertisementTime());
-	}
 	
 	/**
 	 * 3.  We have sent a message, the minimum interval has passed
@@ -185,101 +159,7 @@ public class BCVMHandlingTest extends BaseTestCase {
 		assertFalse(_connection.needsAdvertisement());
 		assertEquals(firstSent,_connection.getLastAdvertisementTime());
 	}
-	
-	/**
-	 * 5.  We have sent a message, the minimum interval has passed
-	 *     and now we are calling it for maintenance, no update needed.
-	 */
-	public void testSecondAdvertisementNull() throws Exception {
-		//send the first message
-		_connection.handleBestCandidatesMessage(_bcvm);
 		
-		long firstSent = _connection.getLastAdvertisementTime();
-		
-		//wait the timeout
-		Thread.sleep(NEW_INTERVAL+20);
-		
-		//call for maintenance
-		_connection.handleBestCandidatesMessage(null);
-		
-		//see if the fields were updated
-		//the message will stay the same
-		//there should be no need to re-advertise
-		//and the last time we sent a message should not have been
-		//updated.
-		assertTrue(_bcvm.isSame(_connection.getCandidatesSent()));
-		assertFalse(_connection.needsAdvertisement());
-		assertEquals(firstSent,_connection.getLastAdvertisementTime());
-	}
-	
-	/**
-	 * 6.  We have sent a message, the minimum interval has passed
-	 *     and now we are calling it for maintenance, update is needed.
-	 */
-	public void testUpdateNeededCallNull() throws Exception {
-		
-		//send the first message
-		_connection.handleBestCandidatesMessage(_bcvm);
-		
-		long firstSent = _connection.getLastAdvertisementTime();
-		
-		//make sure it gets sent
-		assertTrue(
-				_bcvm.isSame((BestCandidatesVendorMessage) _connection.getLastSent()));
-		assertTrue(_bcvm.isSame(_connection.getCandidatesSent()));
-		assertFalse(_connection.needsAdvertisement());
-		
-		//wait half of the timeout
-		Thread.sleep(NEW_INTERVAL/2);
-		
-		//now try to send another update
-		_connection.handleBestCandidatesMessage(_bcvm2);
-		
-		//make sure it does not get sent out.
-		assertFalse(
-				_bcvm2.isSame((BestCandidatesVendorMessage) _connection.getLastSent()));
-		assertTrue(
-				_bcvm.isSame((BestCandidatesVendorMessage) _connection.getLastSent()));
-		assertEquals(firstSent,_connection.getLastAdvertisementTime());
-		
-		//however it should have been put on the send queue.
-		
-		assertTrue(_bcvm2.isSame(_connection.getCandidatesSent()));
-		
-		//and the update flag should be set.
-		assertTrue(_connection.needsAdvertisement());
-	
-		//*****************************************
-		//sleep some time, but not going beyond the interval
-		Thread.sleep(NEW_INTERVAL/4);
-		//call for maintenance
-		_connection.handleBestCandidatesMessage(null);
-		
-		//nothing should happen.
-		assertTrue(_connection.needsAdvertisement()); //should still need update
-		assertFalse( //should not have sent the new message
-				_bcvm2.isSame((BestCandidatesVendorMessage) _connection.getLastSent()));
-		assertTrue(
-				_bcvm.isSame((BestCandidatesVendorMessage) _connection.getLastSent()));
-		assertEquals(firstSent,_connection.getLastAdvertisementTime());
-		assertTrue(_bcvm2.isSame(_connection.getCandidatesSent())); //the new message should be on queue
-		
-		//************************************
-		//sleep some more time, this time going outside of the interval
-		Thread.sleep(NEW_INTERVAL/2);
-		
-		//call for maintenance
-		_connection.handleBestCandidatesMessage(null);
-		
-		//this maintenance call should send the new message that was on the queue
-		//and should clear the update needed flag
-		assertFalse(_connection.needsAdvertisement());
-		assertTrue(_bcvm2.isSame(_connection.getCandidatesSent()));
-		assertTrue( 
-				_bcvm2.isSame((BestCandidatesVendorMessage) _connection.getLastSent()));
-		assertGreaterThan(firstSent,_connection.getLastAdvertisementTime());
-		
-	}
 	
 	/**
 	 * 
@@ -366,68 +246,32 @@ public class BCVMHandlingTest extends BaseTestCase {
 		assertFalse(_connection.needsAdvertisement());
 		assertEquals(firstSent,_connection.getLastAdvertisementTime());
 	}
+	
 	/**
-	 * 9.  The minimum interval has not passed, no update needed and we are calling
-	 * 			for maintenance.
-	 */	   
-	public void testInIntervalNullNoUpdate() throws Exception {
-		//send the first message
-		_connection.handleBestCandidatesMessage(_bcvm);
-		assertFalse(_connection.needsAdvertisement());
-		
-		long firstSent = _connection.getLastAdvertisementTime();
-		
-		//wait half the timeout
-		Thread.sleep(NEW_INTERVAL/2);
-		
-		//perform maintenance
-		_connection.handleBestCandidatesMessage(null);
-		
-		//see if the fields were updated
-		//the message will stay the same
-		//there should be no need to re-advertise
-		//and the last time we sent a message should not have been
-		//updated.
-		assertTrue(_bcvm.isSame(_connection.getCandidatesSent()));
-		assertFalse(_connection.needsAdvertisement());
-		assertEquals(firstSent,_connection.getLastAdvertisementTime());
-	}
-	/**
-	 * 10.  The minimum interval has not passed, update is needed and we are calling
-	 * 	   for maintenance.
+	 * after the timeout has expired, we call the method with the same
+	 * message; however since update is needed the message gets sent out.
 	 */
-	public void testInIntervalNullUpdate() throws Exception {
-		//send the first message
+	public void testSameMessageUpdateNeeded() throws Exception {
+		testCallTwiceInInterval();
+		assertTrue(_connection.needsAdvertisement());
+		assertTrue(_bcvm.isSame(_connection.getCandidatesSent()));
+		
+		
+		
+		Thread.sleep(NEW_INTERVAL);
+		_connection.setLastSent(null);
+		assertNull(_connection.getLastSent());
 		_connection.handleBestCandidatesMessage(_bcvm);
-		assertFalse(_connection.needsAdvertisement());
 		
-		long firstSent = _connection.getLastAdvertisementTime();
+		//the message should be sent now
+		assertTrue(_bcvm.isSame(_connection.getCandidatesSent()));
+		assertNotNull(_connection.getLastSent());
+		BestCandidatesVendorMessage bcvm3 = (BestCandidatesVendorMessage)
+			_connection.getLastSent();
+		assertTrue(_bcvm.isSame(bcvm3));
 		
-		//wait half the timeout
-		Thread.sleep(NEW_INTERVAL/2);
-		
-		//send another message
-		_connection.handleBestCandidatesMessage(_bcvm2);
-		
-		//we should need maintenance now.
-		assertTrue(_connection.needsAdvertisement());
-		assertTrue(_bcvm2.isSame(_connection.getCandidatesSent()));
-		assertEquals(firstSent,_connection.getLastAdvertisementTime());
-		assertTrue(
-				_bcvm.isSame((BestCandidatesVendorMessage) _connection.getLastSent()));
-		
-		//sleep some more, but not passing the interval
-		Thread.sleep(NEW_INTERVAL/4);
-		//perform maintenance
-		_connection.handleBestCandidatesMessage(null);
-		
-		//everything should stay the same
-		assertTrue(_connection.needsAdvertisement());
-		assertTrue(_bcvm2.isSame(_connection.getCandidatesSent()));
-		assertEquals(firstSent,_connection.getLastAdvertisementTime());
-		assertTrue(
-				_bcvm.isSame((BestCandidatesVendorMessage) _connection.getLastSent()));
 	}
+	
 	
 	//********************************
 	//PART II - RECEIVING UPDATES
@@ -511,19 +355,40 @@ public class BCVMHandlingTest extends BaseTestCase {
 	 */
 	static class TestConnection extends ManagedConnectionStub {
 		
+		
+		public TestConnection() {
+			super();
+			_candidateHandler=new CandidateHandler(this);
+		}
 		public BestCandidatesVendorMessage getCandidatesSent() {
-			return _candidatesSent;
+			try {
+				BestCandidatesVendorMessage bcvm =
+					(BestCandidatesVendorMessage) 
+					PrivilegedAccessor.getValue(getCandidateHandler(),"_candidatesSent");
+				return bcvm;
+			}catch(Exception e) {
+				e.printStackTrace();
+				return null;
+			}
 		}
 		
 		public BestCandidatesVendorMessage getCandidatesReceived() {
-			return _candidatesReceived;
+			try {
+				BestCandidatesVendorMessage bcvm =
+					(BestCandidatesVendorMessage) 
+					PrivilegedAccessor.getValue(getCandidateHandler(),"_candidatesReceived");
+				return bcvm;
+			}catch(Exception e) {
+				e.printStackTrace();
+				return null;
+			}
 		}
 		
 		public long getLastAdvertisementTime() {
 			Long last = null;
 			try{
 				last= (Long)
-					PrivilegedAccessor.getValue(this,"_lastSentAdvertisementTime");
+					PrivilegedAccessor.getValue(getCandidateHandler(),"_lastSentAdvertisementTime");
 			}catch(Exception e) {
 				last = new Long(-1);
 			}
@@ -534,7 +399,7 @@ public class BCVMHandlingTest extends BaseTestCase {
 			Long last = null;
 			try{
 				last= (Long)
-					PrivilegedAccessor.getValue(this,"_lastReceivedAdvertisementTime");
+					PrivilegedAccessor.getValue(getCandidateHandler(),"_lastReceivedAdvertisementTime");
 			}catch(Exception e) {
 				last = new Long(-1);
 			}
@@ -545,7 +410,7 @@ public class BCVMHandlingTest extends BaseTestCase {
 			Boolean yes = null;
 			try{
 				yes= (Boolean)
-					PrivilegedAccessor.getValue(this,"_needsAdvertisement");
+					PrivilegedAccessor.getValue(getCandidateHandler(),"_needsAdvertisement");
 			}catch(Exception e) {
 				yes = new Boolean(false);
 			}
@@ -557,6 +422,8 @@ public class BCVMHandlingTest extends BaseTestCase {
 		}
 		
 		public boolean isGoodUltrapeer() {return true;}
+		
+		public short getUptime() {return 20;}
 	}
 	
 	/**
