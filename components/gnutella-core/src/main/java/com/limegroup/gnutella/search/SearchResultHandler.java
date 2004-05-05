feb0f939d4edaf35cbc4b636970166f7c76a4492
sberlin <sberlin>
2004-05-05 18:47:05 +0000

    removed unnecessary thread and code from SearchResultHandler.  the gui search results can now handle everything thrown at it.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/search/SearchResultHandler.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/search/SearchResultHandler.java
@@ -30,49 +30,16 @@ public final class SearchResultHandler {
      */
     private static final int QUERY_EXPIRE_TIME = 30 * 1000; // 30 seconds.
 
-    /** 
-	 * The maximum number of queries to buffer at any time. 
-	 */
-    private static final int BUFFER_SIZE = 2000;
-
-    /** 
-     * The maximum number of results to send in a QueryStatusResponse -
-     * basically sent to say 'shut off query'.
-	 */
-    public static final int MAX_RESULTS = 65535;
-
-    /** 
-	 * The maximum number of replies to display per SECOND.  Must be greater
-     * than 0. Note that one query reply may have many (up to 255!) results. 
-	 */
-    private static final int MAX_RATE = 200;
-
-    /** 
-	 * The (amortized) min time to wait between any two results, in MSECS.
-     * DELAY_TIME==1/(MAX_RATE [replies/sec] * .001 [sec/msec]) 
-	 */
-    private static final int DELAY_TIME = 1000/MAX_RATE;
-
     /**
      * The "delay" between responses to wait to send a QueryStatusResponse.
      */
     public static final int REPORT_INTERVAL = 15;
 
     /** 
-	 * The queue of buffered query replies.  Used to decouple backend,
-     * grouping, and Swing threads.  Also needed for rate limiting.  Follows
-     * stack policy, with replies added to and removed from tail.  This means
-     * that a slow older search won't prevent newer results from coming in. We
-     * use a linked list instead of standard buffer because it shrinks in size
-     * as needed.  We limit the size in handleQueryReply. 
+     * The maximum number of results to send in a QueryStatusResponse -
+     * basically sent to say 'shut off query'.
 	 */
-    private final LinkedList /* of QueryReply */ REPLIES = new LinkedList();
-
-    /** 
-	 * The time of the last result passed to the GUI, as returned by
-     * System.currentTimeMillis().  Used for rate limiting. 
-	 */
-    private long lastTime;
+    public static final int MAX_RESULTS = 65535;
 
 
     /** Used to keep track of the number of non-filtered responses per GUID.
@@ -84,31 +51,14 @@ public final class SearchResultHandler {
       PUBLIC INTERFACE METHODS
      ----------------------------------------------------*/
 
-	/**
-	 * Starts the thread that processes search results.
-	 */
-	public void start() {
-        //Start REPLIES consumer thread.
-		Runnable resultRunner = new ReplyProcessor();
-		Thread resultThread = new ManagedThread(resultRunner, "Search Result Thread");
-        resultThread.setDaemon(true);
-        resultThread.start();
-	}
-
     /**
-     * Adds the query reply.  It may take some time to actually process
-     * the result.
+     * Adds the query reply, immediately processing it and passing
+     * it off to the GUI.
 	 *
 	 * @param qr the <tt>QueryReply</tt> to add
      */
     public void handleQueryReply(QueryReply qr) {
-        synchronized (REPLIES) {
-            REPLIES.addLast(qr);
-            //Ensure bounds on size of REPLIES.
-            if (REPLIES.size() > BUFFER_SIZE)
-                REPLIES.removeFirst();
-            REPLIES.notify();
-        }
+        handleReply(qr);
     }
 
 
@@ -201,48 +151,6 @@ public final class SearchResultHandler {
     /*---------------------------------------------------    
       PRIVATE INTERFACE METHODS
      ----------------------------------------------------*/
-
-	/**
-	 * Private class for processing replies as they come in -- does some
-	 * buffering to avoid brining the ui thread to a crawl.
-	 */
-    private class ReplyProcessor implements Runnable {
-        public void run() {
-			try {
-				while (true) {
-					//1. Wait for result.
-					QueryReply qr = null;
-					synchronized (REPLIES) {
-						while (REPLIES.isEmpty()) {
-							try {
-								REPLIES.wait();
-							} catch (InterruptedException e) { }
-						}
-						qr = (QueryReply)REPLIES.removeLast();
-					}
-					
-					//2. Look at time. If not enough time has elapsed, sleep
-					//long enough so that
-					//       (elapsed+sleepTime)/qr.getResultCount()==DELAY_TIME
-					long now = System.currentTimeMillis();
-					long elapsed = now-lastTime;
-					long sleepTime = DELAY_TIME*qr.getResultCount()-elapsed;
-					if (sleepTime>0) {
-						try {
-							Thread.sleep(sleepTime);
-						} catch (InterruptedException e) { }
-					}
-					
-					//3. Actually handle this.
-					boolean displayed = handleReply(qr);
-					if (displayed) 
-						lastTime=System.currentTimeMillis();
-				}
-			} catch(Throwable t) {
-				ErrorService.error(t);
-			}
-		}
-	}
 
 
     /** 
