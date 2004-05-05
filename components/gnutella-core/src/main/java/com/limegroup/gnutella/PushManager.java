f4d235bc30ae76417773232cdc7827df17e3daaf
sdaswani <sdaswani>
2004-05-05 14:32:56 +0000

    excise unnecessary feature that is invalidated by newer features (hammering limit and connectback).

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/PushManager.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/PushManager.java
@@ -24,14 +24,6 @@ public final class PushManager {
     private static final Log LOG =
       LogFactory.getLog(PushManager.class);
 
-	/**
-	 * The maximum time in SECONDS after an unsuccessful push until we will
-     *  try the push again.  This should be larger than the 15+4*30=135 sec
-     *  window in which Gnutella resends pushes by default
-     */
-    private static final int PUSH_INVALIDATE_TIME=60*5;  //5 minutes
-
-    
     /**
      * The timeout for the connect time while establishing the socket. Set to
      * the same value as NORMAL_CONNECT_TIME is ManagedDownloader.
@@ -39,22 +31,6 @@ public final class PushManager {
     private static final int CONNECT_TIMEOUT = 10000;//10 secs
 
 
-	/**
-     * The list of all files that we've tried unsuccessfully to upload
-     * via pushes.  (Here successful means we were able to connect.
-     * It does not mean the file was actually transferred.) If we get
-     * another push request from one of these hosts (e.g., because the
-     * host is firewalled and sends multiple push packets) we will not
-     * try again.
-     *
-     * INVARIANT: for all i>j, ! failedPushes[i].before(failedPushes[j])
-     * LOCKING: Obtain this' monitor
-	 */
-	private List /* of PushRequestedFile */ _failedPushes=
-        new LinkedList();
-    private List /* of PushRequestedFile */ _attemptingPushes=
-        new LinkedList();
-    
 	/**
 	 * Accepts a new push upload.
      * NON-BLOCKING: creates a new thread to transfer the file.
@@ -97,16 +73,9 @@ public final class PushManager {
         FileManager fm = RouterService.getFileManager();
         final int index = 0;
 
-        // Test if we are either currently attempting a push, or we have
-        // unsuccessfully attempted a push with this host in the past.
-        synchronized(this) {
-    		clearFailedPushes();
-            if ( !forceAllow && (
-                 (! testAttemptingPush(host, index) && !isFWTransfer )  ||
-                 (! testFailedPush(host, index) ) ) )
-                return;
-            insertAttemptingPush(host, index);
-        }    
+        // We used to have code here that tested if the guy we are pushing to is
+        // 1) hammering us, or 2) is actually firewalled.  1) is done above us
+        // now, and 2) isn't as much an issue with the advent of connectback
 
         Thread runner=new ManagedThread("PushUploadThread") {
             public void managedRun() {
@@ -140,9 +109,6 @@ public final class PushManager {
                     }
                 } catch(IOException ioe){//connection failed? do book-keeping
                     UploadStat.PUSH_FAILED.incrementStat();
-                    synchronized(this) { 
-                        insertFailedPush(host, index);  
-                    }
                 } catch(Throwable e) {
 					ErrorService.error(e);
 				}
@@ -158,112 +124,9 @@ public final class PushManager {
                             s.close();
                         } catch(IOException ioe) {}
                     }
-                    // do this here so if the index changes, there is no
-                    // confusion
-                    synchronized(this) {
-                        removeAttemptingPush(host, index);
-                    }                    
                 }
             }
         };
         runner.start();
-	}
-	
-
-
-    /** @requires caller has this's monitor */
-	private synchronized void insertFailedPush(String host, int index) {
-		_failedPushes.add(new PushedFile(host, index));
-	}
-	
-    /** @requires caller has this's monitor */
-	private synchronized boolean testFailedPush(String host, int index) {
-		PushedFile pf = new PushedFile(host, index);
-		PushedFile pfile;
-		Iterator iter = _failedPushes.iterator();
-		while ( iter.hasNext() ) {
-			pfile = (PushedFile)iter.next();
-			if ( pf.equals(pfile) ) 
-				return false;
-		}
-		return true;
-
-	}
-
-    /** @requires caller has this's monitor */
-	private synchronized void insertAttemptingPush(String host, int index) {
-		_attemptingPushes.add(new PushedFile(host, index));
-	}
-
-    /** @requires caller has this's monitor */
-	private synchronized boolean testAttemptingPush(String host, int index) {
-		PushedFile pf = new PushedFile(host, index);
-		PushedFile pfile;
-		Iterator iter = _attemptingPushes.iterator();
-		while ( iter.hasNext() ) {
-			pfile = (PushedFile)iter.next();
-			if ( pf.equals(pfile) )
-				return false;
-		}
-		return true;
-	}
-
-    /** @requires caller has this's monitor */	
-	private synchronized void removeAttemptingPush(String host, int index) {
-		PushedFile pf = new PushedFile(host, index);
-		PushedFile pfile;
-		Iterator iter = _attemptingPushes.iterator();
-		while ( iter.hasNext() ) {
-			pfile = (PushedFile)iter.next();
-			if ( pf.equals(pfile) )
-				// calling iter.remove() rather than
-				// remove on the list, since this will be
-				// safer while iterating through the list.
-				iter.remove();
-		}
-	}
-
-    /** @requires caller has this's monitor */
-	private synchronized void clearFailedPushes() {
-		// First remove all files that were pushed more than a few minutes ago
-		Date time = new Date();
-		time.setTime(time.getTime()-(PUSH_INVALIDATE_TIME*1000));
-		Iterator iter = _failedPushes.iterator();
-		while (iter.hasNext()) {
-			PushedFile pf=(PushedFile)iter.next();
-			if (pf.before(time))
-				iter.remove();
-		}
-		
-	}
-
-	/**
-	 * Keeps track of a push requested file and the host that requested it.
-	 */
-	private static class PushedFile {
-		private final String _host;
-        private final int _index;
-		private final Date _time;        
-
-		public PushedFile(String host, int index) {
-			_host = host;
-            _index = index;
-			_time = new Date();
-		}
-		
-        /** Returns true iff o is a PushedFile with same _host and _index.
-         *  Time doesn't matter. */
-		public boolean equals(Object o) {
-			if(o == this) return true;
-            if (! (o instanceof PushedFile))
-                return false;
-            PushedFile pf=(PushedFile)o;
-			return _index==pf._index && _host.equals(pf._host);
-		}
-		
-		public boolean before(Date time) {
-			return _time.before(time);
-		}
-		
 	}
 }
