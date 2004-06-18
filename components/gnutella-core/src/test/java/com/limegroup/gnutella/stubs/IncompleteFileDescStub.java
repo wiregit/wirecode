16ee1219d9a9b829aa3e7cfbdb8187b93f80d04a
zlatinb <zlatinb>
2004-06-18 21:11:13 +0000

    stub for IncompleteFileDesc

---
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/stubs/IncompleteFileDescStub.java
@@ -0,0 +1,101 @@
+
+package com.limegroup.gnutella.stubs;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.limegroup.gnutella.ErrorService;
+import com.limegroup.gnutella.IncompleteFileDesc;
+import com.limegroup.gnutella.URN;
+import com.sun.java.util.collections.HashSet;
+import com.sun.java.util.collections.Set;
+
+/**
+ * A stub that is identical to FileDescStub.  The code uses instanceof,
+ * so we need this stub for Partial files.
+ * 
+ * It also stubs out some methods.  Feel free to stub out more methods as
+ * need arises.
+ */
+public class IncompleteFileDescStub extends IncompleteFileDesc {
+	public static final String urn = "urn:sha1:PLSTHIPQGSSZTS5FJUPAKUZWUGYQYPFB";
+    public static final Set set;
+    private static Set localSet2,globalSet;
+    public Set localSet;
+    
+    private byte [] _ranges;
+    
+    public static final int size = 1126400;
+    static {
+        set = new HashSet();
+        globalSet=new HashSet();
+        try {
+            set.add(URN.createSHA1Urn(urn));
+            globalSet.addAll(set);
+        } catch(IOException ioe) {
+            ErrorService.error(ioe);
+        }
+    }
+    
+    public IncompleteFileDescStub() {
+        this("abc.txt");
+    }
+    
+    public IncompleteFileDescStub(String name) {
+        super(new File(name), set, 0,name,size,null);
+        localSet=new HashSet();
+    }
+    
+    public IncompleteFileDescStub(String name, URN urn, int index) {
+    	super(new File(name), createUrnSet(urn),index,name,size,null);
+    	localSet=localSet2;
+    }
+    
+    private static Set createUrnSet(URN urn) {
+    	localSet2 = new HashSet();
+    	localSet2.add(urn);
+    	globalSet.add(urn);
+    	return localSet2;
+    }
+    
+    public boolean containsUrn(URN urn) {
+    	if (globalSet.contains(urn))
+    		return true;
+    	else return super.containsUrn(urn);
+    }
+
+    public InputStream createInputStream() {
+        return new InputStream() {
+            public int read() {
+                return 'a';
+            }
+            public int read(byte[] b) {
+                for(int i=0; i < b.length; i++)
+                    b[i] = (byte)'a';
+                return b.length;
+            }
+        };
+    }
+    
+    public long getSize() {
+        return size;
+    }
+    
+    public URN getSHA1Urn() {
+    	if (localSet.isEmpty())
+    		return super.getSHA1Urn();
+    	else return
+			(URN)localSet.toArray()[0];
+    }
+	/* (non-Javadoc)
+	 * @see com.limegroup.gnutella.IncompleteFileDesc#getRangesAsByte()
+	 */
+	public byte[] getRangesAsByte() {
+		return _ranges;
+	}
+	
+	public void setRangesByte(byte [] what) {
+		_ranges=what;
+	}
+}
