fb782428e01833e7cf41276adde60840110577f6
zlatinb <zlatinb>
2004-06-24 23:28:03 +0000

    apply gregorio's patch for magnets

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/browser/MagnetOptions.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/browser/MagnetOptions.java
@@ -1,31 +1,81 @@
 package com.limegroup.gnutella.browser;
 
+import com.sun.java.util.collections.ArrayList;
+import com.sun.java.util.collections.List;
+
 /**
  * Contains information fields extracted from a magnet link.
  */
 public class MagnetOptions {
 	public static final String MAGNET    = "magnet:?";
-	public String xt;
-	public String dn; 
-	public String kt; 
-	public String xs;
-	public String as;  // This is technically suppose to handle multiple
-
+	private String _dn; 
+	private String _kt;
+    private List _xs;
+	private List _xt;
+    private List _as;
+    
+    public MagnetOptions() {
+        _dn = null;
+        _kt = null;
+        _xs = new ArrayList();
+        _xt = new ArrayList();
+        _as = new ArrayList();
+    }
+    
 	public String toString() {
 		String ret = MAGNET;
 		
-		if ( xt != null ) 
-			ret += "&xt="+xt+"";
-		if ( dn != null ) 
-			ret += "&dn="+dn+"";
-		if ( kt != null ) 
-			ret += "&kt="+kt+"";
-		if ( xs != null ) 
-			ret += "&xs="+xs+"";
-		if ( as != null ) 
-			ret += "&as="+as+"";
+        for (int i = 0; i < _xt.size(); i++) 
+            ret += "&xt="+_xt.get(i)+"";
+		if ( _dn != null ) 
+			ret += "&dn="+_dn+"";
+		if ( _kt != null ) 
+			ret += "&kt="+_kt+"";
+        for (int i = 0; i < _xs.size(); i++) 
+            ret += "&xs="+_xs.get(i)+"";
+        for (int i = 0; i < _as.size(); i++) 
+            ret += "&as="+_as.get(i)+"";
 		return ret;
 	}
-}
+    
+    public String getDN() {
+        return _dn;
+    }
+    
+    public void setDN(String str) {
+        _dn = str;
+    }
 
+    public String getKT() {
+        return _kt;
+    }
+    
+    public void setKT(String str) {
+        _kt = str;
+    }
+
+    public List getXT() {
+        return _xt;
+    }
+    
+    public void addXT(String str) {
+        _xt.add(str);
+    }
+
+    public List getXS() {
+        return _xs;
+    }
+    
+    public void addXS(String str) {
+        _xs.add(str);
+    }
+
+    public List getAS() {
+        return _as;
+    }
+    
+    public void addAS(String str) {
+        _as.add(str);
+    }
+}
 
