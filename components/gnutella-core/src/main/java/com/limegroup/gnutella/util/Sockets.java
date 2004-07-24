167723d09944c9b70b44b7321c48b97fdab372b9
sberlin <sberlin>
2004-07-24 19:54:09 +0000

    no need to construct via newInstance either.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/util/Sockets.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/util/Sockets.java
@@ -142,7 +142,7 @@ public class Sockets {
             //   is not done lazily.  (See chapter 12.3.4 of the Java Language
             //   Specification.)  So we use reflection.
             try {
-                Socket ret = (Socket)Socket.class.newInstance();
+                Socket ret = new Socket();
                 Object addr = _inetAddressConstructor.newInstance(
                     new Object[] { host, new Integer(port) });
 
