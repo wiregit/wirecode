556a6bbf9d0490912167a3da5f0ea9a4d1a0c1e0
zlatinb <zlatinb>
2004-07-31 18:35:28 +0000

    more efficient probing.  Thanks to lostlogic for reminding that global objects in c stay alive between jni invokations.

--- components/gnutella-core/native/linux/com_limegroup_gnutella_util_SystemUtils.cpp
+++ components/gnutella-core/native/linux/com_limegroup_gnutella_util_SystemUtils.cpp
@@ -9,8 +9,12 @@ This library gets idle calls from systems that have X and screensavers installed
 #  include <X11/Xutil.h>
 # include <X11/X.h>
 #  include <X11/extensions/scrnsaver.h>
-//#include <iostream.h>
+//#include <iostream>
 
+//global to cache across jni invokations
+Display *display = NULL;
+Window window =0;
+XScreenSaverInfo *mit_info = NULL;
 
 //will do these later
 JNIEXPORT jint JNICALL Java_com_limegroup_gnutella_util_SystemUtils_setOpenFileLimit0
@@ -25,39 +29,44 @@ JNIEXPORT jlong JNICALL Java_com_limegroup_gnutella_util_SystemUtils_idleTime
   (JNIEnv *, jclass){
   //cout << "entered method\n";
   
-  	Display *display = XOpenDisplay(NULL);
+  	if(display == NULL) 
+  		display = XOpenDisplay(NULL);
+
 	
-	//cout << "got display\n";
+	//if we are still null, fail gracefully
 	if (display == NULL)
-		return -1;
+		return 0;
 		
 	//cout << "display not null\n";
 	
-	Window window = DefaultRootWindow(display);
+	if (window==0)
+		window = DefaultRootWindow(display);
+		
+	//if still 0, fail gracefully
+	if (window==0)
+		return 0;
 	
-	//cout << "\ngot window\n";
+	//cout << "got window\n";
 	
-  	XScreenSaverInfo *mit_info = NULL;
   	int event_base, error_base;
-	long idle_time;
+
 		if (XScreenSaverQueryExtension(display, &event_base, &error_base)) {
 	//		cout << "system supports screensavers\n";
 			
 			if (mit_info == NULL) {
 				mit_info = XScreenSaverAllocInfo();
 			}
+			
+			//if still null, fail gracefully.
+			if (mit_info ==NULL) 
+				return 0;
+			
 			XScreenSaverQueryInfo(display, window, mit_info);
+//			cout <<"queried successfully " << mit_info->idle <<"\n";			
+			return mit_info->idle;
 			
-			idle_time = (mit_info->idle);
-			
-	//		cout <<"queried successfully " << idle_time <<"\n";
+
 		} else
-			idle_time = 0;
+			return 0;
 			
-	XFree(mit_info);
-	XCloseDisplay(display);
-	return idle_time;
-  
   }
-  
-  int main() {return 0;}
