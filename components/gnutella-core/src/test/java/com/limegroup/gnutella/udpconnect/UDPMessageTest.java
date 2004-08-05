247edd8022f4c4775d9d400b7e277289df90c9da
gbildson <gbildson>
2004-08-05 23:23:56 +0000

    add some new data message tests

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/udpconnect/UDPMessageTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/udpconnect/UDPMessageTest.java
@@ -29,14 +29,13 @@ public class UDPMessageTest extends BaseTestCase {
 		junit.textui.TestRunner.run(suite());
 	}
 	
-	public void testDataMessage() throws Exception {
+    public void testDataMessage() throws Exception {
 
         // Construct a data message
         byte        connectionID   = 1;
         long        sequenceNumber = 0;
         byte[]      data           = new byte[512];
         int         datalength     = data.length;
-        DataMessage dmWrite;
 
         SequenceNumberExtender extender = new SequenceNumberExtender(); 
 
@@ -45,15 +44,95 @@ public class UDPMessageTest extends BaseTestCase {
             writeAndReadADataMessage(connectionID, i, data, datalength, 
               extender);
         }
+    }
+
+    public void testBoundaryConditions() throws Exception {
+
+        // Construct a data message
+        byte        connectionID   = 1;
+        long        sequenceNumber = 0;
+        byte[]      data           = new byte[512];
+        int         datalength     = data.length;
+
+        SequenceNumberExtender extender;
 
         // Test boundary conditions
-        extender = new SequenceNumberExtender(999999l); 
-        writeAndReadADataMessage((byte)0xff, 999999l, data, datalength, 
+        extender = new SequenceNumberExtender(999999999l); 
+        writeAndReadADataMessage((byte)0xff, 999999999l, data, datalength, 
           extender);
+
+        // Test 1 byte
+        data = new byte[1];
+        datalength = 1;
+        extender = new SequenceNumberExtender(999999999l); 
+        writeAndReadADataMessage((byte)0xff, 999999999l, data, datalength, 
+          extender);
+
+    }
+
+	public void testDataSizes() throws Exception {
+
+        // Construct a data message
+        byte        connectionID   = 1;
+        long        sequenceNumber = 0;
+        byte[]      data;
+        int         datalength;
+
+        SequenceNumberExtender extender = new SequenceNumberExtender(); 
+
+        // Test data sizes and content
+        DataMessage dmRead;
+        data = new byte[0x1000];
+        datalength = data.length;
+        for (int i = 0; i < datalength; i++) {
+            data[i] = (byte)(i % 255);
+        }
+        dmRead = writeAndReadADataMessage((byte)1, 1, data, datalength, 
+          extender);
+        for (int i = 0; i < datalength; i++) {
+            assertEquals(dmRead.getDataAt(i), (byte)(i % 255));
+        }
+    
+        // Test 2K
+        data = new byte[0x2000];
+        datalength = data.length;
+        for (int i = 0; i < datalength; i++) {
+            data[i] = (byte)(i % 255);
+        }
+        dmRead = writeAndReadADataMessage((byte)1, 1, data, datalength, 
+          extender);
+        for (int i = 0; i < datalength; i++) {
+            assertEquals(dmRead.getDataAt(i), (byte)(i % 255));
+        }
+
+        // Test 4K
+        data = new byte[0x4000];
+        datalength = data.length;
+        for (int i = 0; i < datalength; i++) {
+            data[i] = (byte)(i % 255);
+        }
+        dmRead = writeAndReadADataMessage((byte)1, 1, data, datalength, 
+          extender);
+        for (int i = 0; i < datalength; i++) {
+            assertEquals(dmRead.getDataAt(i), (byte)(i % 255));
+        }
+
+        // Test 8K
+        data = new byte[0x8000];
+        datalength = data.length;
+        for (int i = 0; i < datalength; i++) {
+            data[i] = (byte)(i % 255);
+        }
+        dmRead = writeAndReadADataMessage((byte)1, 1, data, datalength, 
+          extender);
+        for (int i = 0; i < datalength; i++) {
+            assertEquals(dmRead.getDataAt(i), (byte)(i % 255));
+        }
+    
     }
 
 
-    private void writeAndReadADataMessage(
+    private DataMessage writeAndReadADataMessage(
       byte                   connectionID,
       long                   sequenceNumber,
       byte[]                 data,
@@ -80,6 +159,8 @@ public class UDPMessageTest extends BaseTestCase {
         assertEquals(connectionID, dmRead.getConnectionID());
         assertEquals(sequenceNumber, dmRead.getSequenceNumber());
         assertEquals(datalength, dmRead.getDataLength());
+
+        return dmRead;
     }
 	
 }        
