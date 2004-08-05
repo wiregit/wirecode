81192ecf215b7de9acd010a661f966156c8ee372
gbildson <gbildson>
2004-08-05 23:02:03 +0000

    getting UDP Message test ready for some action

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/udpconnect/UDPMessageTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/udpconnect/UDPMessageTest.java
@@ -37,30 +37,49 @@ public class UDPMessageTest extends BaseTestCase {
         byte[]      data           = new byte[512];
         int         datalength     = data.length;
         DataMessage dmWrite;
-        DataMessage dmRead;
 
         SequenceNumberExtender extender = new SequenceNumberExtender(); 
 
         // Try a range of sequence numbers
-        for( long i = 0; i <= 0xffff; i++) {
-            dmWrite = new DataMessage(connectionID, i, data, datalength);
-
-            // Write the message out
-            sout = new ByteArrayOutputStream();
-            dmWrite.write(sout);
-
-            // Read the message in
-            sin = new ByteArrayInputStream(sout.toByteArray());
-            dmRead = (DataMessage) Message.read(sin);
-
-            // Extend the msgs sequenceNumber to 8 bytes based on past state
-            dmRead.extendSequenceNumber(
-              extender.extendSequenceNumber( dmRead.getSequenceNumber()) ); 
-
-            assertEquals(connectionID, dmRead.getConnectionID());
-            assertEquals(i, dmRead.getSequenceNumber());
-            assertEquals(datalength, dmRead.getDataLength());
+        for( long i = 0; i <= 0x1ffff; i++) {
+            writeAndReadADataMessage(connectionID, i, data, datalength, 
+              extender);
         }
+
+        // Test boundary conditions
+        extender = new SequenceNumberExtender(999999l); 
+        writeAndReadADataMessage((byte)0xff, 999999l, data, datalength, 
+          extender);
+    }
+
+
+    private void writeAndReadADataMessage(
+      byte                   connectionID,
+      long                   sequenceNumber,
+      byte[]                 data,
+      int                    datalength,
+      SequenceNumberExtender extender) throws Exception {
+
+        DataMessage dmWrite;
+        DataMessage dmRead;
+        dmWrite = 
+          new DataMessage(connectionID, sequenceNumber, data, datalength);
+
+        // Write the message out
+        sout = new ByteArrayOutputStream();
+        dmWrite.write(sout);
+
+        // Read the message in
+        sin = new ByteArrayInputStream(sout.toByteArray());
+        dmRead = (DataMessage) Message.read(sin);
+
+        // Extend the msgs sequenceNumber to 8 bytes based on past state
+        dmRead.extendSequenceNumber(
+          extender.extendSequenceNumber( dmRead.getSequenceNumber()) ); 
+
+        assertEquals(connectionID, dmRead.getConnectionID());
+        assertEquals(sequenceNumber, dmRead.getSequenceNumber());
+        assertEquals(datalength, dmRead.getDataLength());
     }
 	
 }        
