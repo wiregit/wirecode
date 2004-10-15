a8be11f40424bc7d376b7d12621e3d5d945fb749
zlatinb <zlatinb>
2004-10-15 21:58:17 +0000

    filter queries containing any urns

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/filters/HashFilter.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/filters/HashFilter.java
@@ -1,10 +1,8 @@
 
 package com.limegroup.gnutella.filters;
 
-import com.limegroup.gnutella.UrnType;
 import com.limegroup.gnutella.messages.Message;
 import com.limegroup.gnutella.messages.QueryRequest;
-
 /**
  * filters out queries containing hash urns. 
  */
@@ -16,7 +14,7 @@ public class HashFilter extends SpamFilter {
 
 		QueryRequest qr = (QueryRequest)m;
 		
-        return !qr.getRequestedUrnTypes().contains(UrnType.SHA1);
+        return !qr.hasQueryUrns();
     }
 
 }
