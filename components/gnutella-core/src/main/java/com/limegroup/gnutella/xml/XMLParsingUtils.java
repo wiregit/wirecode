7961b0d6ef87583513d0c24b6fb180fef9b7ee55
tjones <tjones>
2004-10-27 22:08:20 +0000

    added old style unit test, passes

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/xml/XMLParsingUtils.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/xml/XMLParsingUtils.java
@@ -1,11 +1,13 @@
 package com.limegroup.gnutella.xml;
 
 import com.limegroup.gnutella.util.Comparators;
+import com.limegroup.gnutella.Assert;
 
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -72,7 +74,7 @@ public class XMLParsingUtils {
         return result;
     }
     /**
-     * Splits aggregated XML string into individual XML strings
+     * Splits an aggregated XML string into individual XML strings
      * @param aggregatedXmlDocuments
      * @return List of Strings
      */
@@ -97,18 +99,45 @@ public class XMLParsingUtils {
         return type+"s";
     }
 //     public static void main(String[] args) throws Exception {
+//         //test parse
 //         String xml = "<?xml version=\"1.0\"?>"+
-//             "<audios xsi:noNamespaceSchemaLocation=\"http://www.limewire.com/schemas/audios.xsd\">"+
+//             "<audios xsi:noNamespaceSchemaLocation=\"http://www.limewire.com/schemas/audio.xsd\">"+
 //             "<audio genre=\"Rock\" identifier=\"def1.txt\" bitrate=\"190\"/>"+
 //             "<audio genre=\"Classical\" identifier=\"def2.txt\" bitrate=\"2192\"/>"+
 //             "<audio genre=\"Blues\" identifier=\"def.txt\" bitrate=\"192\"/></audios>";
 //         Result r = parse(xml);
-//         System.out.println("Schema URI: " + r.schemaURI);
-//         System.out.println("Type: " + r.type);
-//         System.out.println(r.canonicalAttributeMaps);
-//         System.out.println(plural("property"));
-//         System.out.println(plural("book"));
-//         List split = split(xml+xml+xml);
-//         System.out.println(split);
+//         Assert.that(r.schemaURI.equals("http://www.limewire.com/schemas/audio.xsd"));
+//         Assert.that(r.type.equals("audio"));
+//         List list = new ArrayList();
+//         Map map = new TreeMap(Comparators.stringComparator());
+//         map.put("audios__audio__genre__","Rock");
+//         map.put("audios__audio__identifier__","def1.txt");
+//         map.put("audios__audio__bitrate__","190");
+//         list.add(map);
+//         map = new TreeMap(Comparators.stringComparator());
+//         map.put("audios__audio__genre__","Classical");
+//         map.put("audios__audio__identifier__","def2.txt");
+//         map.put("audios__audio__bitrate__","2192");
+//         list.add(map);
+//         map = new TreeMap(Comparators.stringComparator());
+//         map.put("audios__audio__genre__","Blues");
+//         map.put("audios__audio__identifier__","def.txt");
+//         map.put("audios__audio__bitrate__","192");
+//         list.add(map);
+//         Assert.that(r.canonicalAttributeMaps.equals(list));
+//         //test plural
+//         Assert.that(plural("book").equals("books"));
+//         Assert.that(plural("property").equals("properties"));
+//         //test split
+//         String xml1 = "<?xml version='1.0'><text>one</text>";
+//         String xml2 = "<?xml version='1.0'><text>two</text>";
+//         String xml3 = "<?xml version='1.0'><text>three</text>";
+//         List split = split(xml1+xml2+xml3);
+//         Iterator i = split.iterator();
+//         Assert.that(i.next().equals(xml1));
+//         Assert.that(i.next().equals(xml2));
+//         Assert.that(i.next().equals(xml3));
+//         Assert.that(!i.hasNext());
+//         System.out.println("XMLParsingUtils passed unit test");
 //     }
 }
