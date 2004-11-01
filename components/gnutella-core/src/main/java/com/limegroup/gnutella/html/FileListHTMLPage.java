464cb52e3e3a783a2412e72c022820ed1669977f
sberlin <sberlin>
2004-11-01 15:52:05 +0000

    protect against NPE.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/html/FileListHTMLPage.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/html/FileListHTMLPage.java
@@ -69,17 +69,19 @@ public class FileListHTMLPage {
             // get all the Shared files from the FM
             final String beginURL = "\r\n<a href=\"/get/";
             for (int i = 0; i < sharedFiles.length; i++) {
-                if (!(sharedFiles[i] instanceof IncompleteFileDesc)) {
-                    File currFile = sharedFiles[i].getFile();
-                    sb.append(beginURL + sharedFiles[i].getIndex() + "/" + 
-                              UploadManager.FV_PASS + "/" +
-                              StringUtils.replace(URLEncoder.encode(currFile.getName()),
-                                                  "+", "%20") + "\">" + 
-                              currFile.getName() + "</a><br>");
-                    
-                    if (!shouldShowMagnets && hasEnoughAltLocs(sharedFiles[i]))
-                        shouldShowMagnets = true;
-                }
+                if(sharedFiles[i] == null ||
+                   sharedFiles[i] instanceof IncompleteFileDesc)
+                    continue;
+
+                File currFile = sharedFiles[i].getFile();
+                sb.append(beginURL + sharedFiles[i].getIndex() + "/" + 
+                          UploadManager.FV_PASS + "/" +
+                          StringUtils.replace(URLEncoder.encode(currFile.getName()),
+                                              "+", "%20") + "\">" + 
+                          currFile.getName() + "</a><br>");
+                
+                if (!shouldShowMagnets && hasEnoughAltLocs(sharedFiles[i]))
+                    shouldShowMagnets = true;
             }
         }
         
