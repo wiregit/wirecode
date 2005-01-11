ce448c1b5d24c463e7730220e32edfab5667dad1
tjones <tjones>
2005-01-11 15:42:06 +0000

    test updated for new URNResponseFilter interface

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/filters/ResponseFilterTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/filters/ResponseFilterTest.java
@@ -3,7 +3,9 @@ package com.limegroup.gnutella.filters;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import com.limegroup.gnutella.URN;
 import com.limegroup.gnutella.settings.FilterSettings;
+
 
 /**
  * Unit tests for ResponseFilter, URNReponseFilter
@@ -29,8 +31,18 @@ public class ResponseFilterTest extends com.limegroup.gnutella.util.BaseTestCase
 
     public void testURNResponseFilter() {
         URNResponseFilter f = new URNResponseFilter();
-        String urn1 = "urn:sha1:4UWZJVBV3CXEGLA736FK47KAQJQSMZ5J";
-        String urn2 = "urn:sha1:52MWSJ275Y7UUXPHP4JBEKDVUFKW7JQY";
+        String urn1String = "urn:sha1:4UWZJVBV3CXEGLA736FK47KAQJQSMZ5J";
+        String urn2String = "urn:sha1:52MWSJ275Y7UUXPHP4JBEKDVUFKW7JQY";
+        URN urn1;
+        URN urn2;
+        try {
+            urn1 = URN.createSHA1Urn(urn1String);
+            urn2 = URN.createSHA1Urn(urn2String);
+        } catch (java.io.IOException x) {
+            fail(x);
+            return;
+        }
+
         String[] blockedUrns;
 
         blockedUrns = new String[0];
@@ -40,15 +52,15 @@ public class ResponseFilterTest extends com.limegroup.gnutella.util.BaseTestCase
         assertTrue(f.allow(urn2));
 
         blockedUrns = new String[1];
-        blockedUrns[0] = urn1;
+        blockedUrns[0] = urn1String;
         FilterSettings.BLOCKED_URNS.setValue(blockedUrns);
         f.refresh();
         assertFalse(f.allow(urn1));
         assertTrue(f.allow(urn2));
 
         blockedUrns = new String[2];
-        blockedUrns[0] = urn1;
-        blockedUrns[1] = urn2;
+        blockedUrns[0] = urn1String;
+        blockedUrns[1] = urn2String;
         FilterSettings.BLOCKED_URNS.setValue(blockedUrns);
         f.refresh();
         assertFalse(f.allow(urn1));
