a18c0371bcee4c7c078daa520d42b9f88086cc1d
zlatinb <zlatinb>
2005-03-24 22:18:19 +0000

    and all that you touch...

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/downloader/VerifyingFile.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/downloader/VerifyingFile.java
@@ -11,8 +11,6 @@ import java.util.NoSuchElementException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import sun.rmi.runtime.GetThreadPoolAction;
-
 import com.limegroup.gnutella.Assert;
 import com.limegroup.gnutella.tigertree.HashTree;
 import com.limegroup.gnutella.util.FileUtils;
