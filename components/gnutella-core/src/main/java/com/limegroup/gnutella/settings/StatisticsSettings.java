448d35796917eb779f77770d57edfa2bba87ccbd
jschmidt <jschmidt>
2005-04-06 18:52:58 +0000

    Revert of previous commit of Advanced Stats preference persistence.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/settings/StatisticsSettings.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/settings/StatisticsSettings.java
@@ -8,10 +8,4 @@ public class StatisticsSettings extends LimeProps {
     
     public static BooleanSetting RECORD_VM_STATS = 
         FACTORY.createBooleanSetting("RECORD_VM_STATS", false);
-
-    /**
-     * Whether or not advanced stats should be displayed by default.
-     */
-    public static final BooleanSetting DISPLAY_ADVANCED_STATS =
-        FACTORY.createBooleanSetting("DISPLAY_ADVANCED_STATS", false);
 }
