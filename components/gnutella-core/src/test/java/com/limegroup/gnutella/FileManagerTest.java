6239210c1b42902bb89ff0b16c8a72b95ff24336
fberger <fberger>
2005-06-03 14:49:09 +0000

    added a little space

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/FileManagerTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/FileManagerTest.java
@@ -8,7 +8,6 @@ import java.lang.reflect.Method;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
