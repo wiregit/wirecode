e234c887e854067f4dad608e36969987a4ff58db
jschmidt <jschmidt>
2005-06-15 14:58:11 +0000

    Commit to status-bar2-branch

---
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/settings/StatusBarSettings.java
@@ -0,0 +1,27 @@
+package com.limegroup.gnutella.settings;
+
+/**
+ * Settings for Music Player
+ */
+public class StatusBarSettings extends LimeProps {
+    
+    private StatusBarSettings() {}
+    
+    /**
+     * Whether or not connection quality status should be displayed.
+     */
+    public static BooleanSetting CONNECTION_QUALITY_DISPLAY_ENABLED =
+        FACTORY.createBooleanSetting("CONNECTION_QUALITY_DISPLAY_ENABLED", true);
+
+    /**
+     * Whether or not number of shared files should be displayed.
+     */
+    public static BooleanSetting SHARED_FILES_DISPLAY_ENABLED =
+        FACTORY.createBooleanSetting("SHARED_FILES_DISPLAY_ENABLED", true);
+
+    /**
+     * Whether or not firewall status should be displayed.
+     */
+    public static BooleanSetting FIREWALL_DISPLAY_ENABLED =
+        FACTORY.createBooleanSetting("FIREWALL_DISPLAY_ENABLED", true);
+}
