8cb8d35fbee743a303daf328f5a0848258da6608
kmagdsick <kmagdsick>
2005-06-23 21:25:26 +0000

    Added tests for new methods.  Reduced dependencies on test order.

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/util/IntervalSetTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/util/IntervalSetTest.java
@@ -31,6 +31,30 @@ public class IntervalSetTest extends BaseTestCase {
 		junit.textui.TestRunner.run(suite());
 	}
 	
+    // getNumberOfIntervals is used in pretty much every
+    // test, so it should be tested first
+    public void testGetNumberOfIntervals() {
+        iSet = IntervalSet.createSingletonSet(0,4);
+        iSet.add(new Interval(17));
+        iSet.add(new Interval(20));
+        iSet.add(new Interval(28));
+        iSet.add(new Interval(31,32));
+        // [0-4], [17-17], [20-20], [28-28], [31-32]
+        assertEquals(5, iSet.getNumberOfIntervals());
+        iSet = new IntervalSet();
+        assertEquals(0, iSet.getNumberOfIntervals());
+        iSet.add(new Interval(17,192000));
+        assertEquals(1, iSet.getNumberOfIntervals());
+        iSet.delete(new Interval(129));
+        assertEquals(2, iSet.getNumberOfIntervals());
+    }
+    
+    public void testCreateSingletonSet() {
+        iSet = IntervalSet.createSingletonSet(17,28417);
+        assertEquals(1, iSet.getNumberOfIntervals());
+        assertEquals(new Interval(17, 28417), iSet.getFirst());
+    }
+    
 	// these tests run in order and set up the data
 	// as they go on.
 	// all these tests must be run from the start IN ORDER
@@ -40,7 +64,7 @@ public class IntervalSetTest extends BaseTestCase {
         iSet = new IntervalSet();
         interval = new Interval(40,45);
         iSet.add(interval);
-        assertEquals("add method broken", 1, numIntervals());
+        assertEquals("add method broken", 1, iSet.getNumberOfIntervals());
         assertEquals("getSize() broken", 6, iSet.getSize());
         iter = iSet.getNeededIntervals(100);
         interval = (Interval)iter.next();
@@ -55,7 +79,7 @@ public class IntervalSetTest extends BaseTestCase {
         ///testing case 1 (intervals is now {[40-45]}
         interval = new Interval(35,39);
         iSet.add(interval);
-        assertEquals("lower boundry colating failed", 1, numIntervals());
+        assertEquals("lower boundry colating failed", 1, iSet.getNumberOfIntervals());
         interval = getIntervalAt(0);
         assertEquals("lower boundry colating failed", 45, interval.high);
         assertEquals("lower boundry colating failed", 35, interval.low);
@@ -66,7 +90,7 @@ public class IntervalSetTest extends BaseTestCase {
         //testing case 2 intervals is now {[35-45]}
         interval = new Interval(30,37);
         iSet.add(interval);
-        assertEquals("lower overlap colating failed", 1, numIntervals());
+        assertEquals("lower overlap colating failed", 1, iSet.getNumberOfIntervals());
         interval = getIntervalAt(0);
         assertEquals("lower boundry colating failed", 45, interval.high);
         assertEquals("lower boundry colating failed", 30, interval.low);
@@ -76,7 +100,7 @@ public class IntervalSetTest extends BaseTestCase {
         //testing case 3 intervals is now {[30-45]}
         interval = new Interval(20,25);
         iSet.add(interval);
-        assertEquals("lower non-overlap add failed", 2, numIntervals());
+        assertEquals("lower non-overlap add failed", 2, iSet.getNumberOfIntervals());
         assertEquals("getSize() broken", 22, iSet.getSize());
         iter = iSet.getNeededIntervals(100);
         interval = (Interval)iter.next();
@@ -94,7 +118,7 @@ public class IntervalSetTest extends BaseTestCase {
         //testing case 4 intervals is now {[20-25],[30-45]}
         interval = new Interval(50,60);
         iSet.add(interval);
-        assertEquals("upper non-overlap add failed", 3, numIntervals());
+        assertEquals("upper non-overlap add failed", 3, iSet.getNumberOfIntervals());
         assertEquals("getSize() broken", 33, iSet.getSize());
         iter = iSet.getNeededIntervals(100);
         interval = (Interval)iter.next();
@@ -225,10 +249,13 @@ public class IntervalSetTest extends BaseTestCase {
     
     public void testAddHighColation() throws Exception {
         ///OK, back to testing add.
+        iSet = IntervalSet.createSingletonSet(20,25);
+        iSet.add(new Interval(30,45));
+        iSet.add(new Interval(50,60));
         //testing case 5 intervals is [20-25],[30-45],[50-60]
         interval = new Interval(54,70);
         iSet.add(interval);
-        assertEquals(3, numIntervals());
+        assertEquals(3, iSet.getNumberOfIntervals());
         interval = getIntervalAt(2);
         assertEquals(50, interval.low);
         assertEquals(70, interval.high);
@@ -236,10 +263,13 @@ public class IntervalSetTest extends BaseTestCase {
     }
     
     public void testAddHighBoundaryColation() throws Exception {
+        iSet = IntervalSet.createSingletonSet(20,25);
+        iSet.add(new Interval(30,45));
+        iSet.add(new Interval(50,70));
         //testing case 6 intervals is [20-25],[30-45],[50-70]
         interval = new Interval(71,75);
         iSet.add(interval);
-        assertEquals(3, numIntervals());
+        assertEquals(3, iSet.getNumberOfIntervals());
         interval = getIntervalAt(2);
         assertEquals(50, interval.low);
         assertEquals(75, interval.high);
@@ -247,7 +277,7 @@ public class IntervalSetTest extends BaseTestCase {
         //test some boundry conditions [20-25],[30-45],[50-75]
         interval = new Interval(75,80);
         iSet.add(interval);
-        assertEquals(3, numIntervals());
+        assertEquals(3, iSet.getNumberOfIntervals());
         interval = getIntervalAt(2);
         assertEquals(50, interval.low);
         assertEquals(80, interval.high);
@@ -256,7 +286,7 @@ public class IntervalSetTest extends BaseTestCase {
     public void testAddLowBoundaryColation() {
         interval = new Interval(15,20);
         iSet.add(interval);
-        assertEquals(3, numIntervals());
+        assertEquals(3, iSet.getNumberOfIntervals());
         interval = getIntervalAt(0);
         assertEquals(15, interval.low);
         assertEquals(25, interval.high);
@@ -283,7 +313,7 @@ public class IntervalSetTest extends BaseTestCase {
         //[15-25],[30-45],[50-80]
         interval = new Interval(49,81);
         iSet.add(interval);
-        assertEquals(3, numIntervals());
+        assertEquals(3, iSet.getNumberOfIntervals());
         interval = getIntervalAt(2);
         assertEquals(49, interval.low);
         assertEquals(81, interval.high);
@@ -294,7 +324,7 @@ public class IntervalSetTest extends BaseTestCase {
         // {[15-25],[30-45],[49-81]}
         interval = new Interval(55,60);
         iSet.add(interval);
-        assertEquals(3, numIntervals());
+        assertEquals(3, iSet.getNumberOfIntervals());
         interval = getIntervalAt(2);
         assertEquals(49, interval.low);
         assertEquals(81, interval.high);
@@ -304,7 +334,7 @@ public class IntervalSetTest extends BaseTestCase {
         // {[15-25],[30-45],[49-81]}
         interval = new Interval(26,29);
         iSet.add(interval);
-        assertEquals(2, numIntervals());
+        assertEquals(2, iSet.getNumberOfIntervals());
         interval = getIntervalAt(0);
         assertEquals(15, interval.low);
         assertEquals(45, interval.high);
@@ -346,10 +376,10 @@ public class IntervalSetTest extends BaseTestCase {
     
     public void testAddFullLowCoveringInterval() {
         // {[3-5],[7-9],[15-45],[49-81]}
-        assertEquals(4, numIntervals());
+        assertEquals(4, iSet.getNumberOfIntervals());
         interval = new Interval(2,17);
         iSet.add(interval);
-        assertEquals(2, numIntervals());
+        assertEquals(2, iSet.getNumberOfIntervals());
         interval = getIntervalAt(0);
         assertEquals(2, interval.low);
         assertEquals(45, interval.high);
@@ -360,7 +390,7 @@ public class IntervalSetTest extends BaseTestCase {
         //{[2-45],[49-81]}
         interval = new Interval(40,50);
         iSet.add(interval);
-        assertEquals(1, numIntervals());
+        assertEquals(1, iSet.getNumberOfIntervals());
         //{[2-81]}
         interval = getIntervalAt(0);
         assertEquals(2, interval.low);
@@ -398,7 +428,7 @@ public class IntervalSetTest extends BaseTestCase {
         iSet.add(new Interval(28,32));
         iSet.add(new Interval(36,40));
         // [0-4], [8-12], [16-20], [24-24], [28-32], [36-40]
-        assertEquals(6, numIntervals());
+        assertEquals(6, iSet.getNumberOfIntervals());
         iter = iSet.getAllIntervals();
         interval = (Interval)iter.next();
         assertEquals(0, interval.low);
@@ -423,7 +453,7 @@ public class IntervalSetTest extends BaseTestCase {
     public void testDeleteNothing() {
         // [0-4], [8-12], [16-20], [24-24], [28-32], [36-40]
         iSet.delete(new Interval(5,7));
-        assertEquals(6, numIntervals());
+        assertEquals(6, iSet.getNumberOfIntervals());
         iter = iSet.getAllIntervals();
         interval = (Interval)iter.next();
         assertEquals(0, interval.low);
@@ -448,7 +478,7 @@ public class IntervalSetTest extends BaseTestCase {
     public void testDeleteBoundaries() {
         // [0-4], [8-12], [16-20], [24-24], [28-32], [36-40]
         iSet.delete(new Interval(12,16));
-        assertEquals(6, numIntervals());
+        assertEquals(6, iSet.getNumberOfIntervals());
         iter = iSet.getAllIntervals();
         interval = (Interval)iter.next();
         assertEquals(0, interval.low);
@@ -473,7 +503,7 @@ public class IntervalSetTest extends BaseTestCase {
     public void testDeleteFullInterval() {
         // [0-4], [8-11], [17-20], [24-24], [28-32], [36-40]
         iSet.delete(new Interval(24,25));
-        assertEquals(5, numIntervals());
+        assertEquals(5, iSet.getNumberOfIntervals());
         iter = iSet.getAllIntervals();
         interval = (Interval)iter.next();
         assertEquals(0, interval.low);
@@ -495,7 +525,7 @@ public class IntervalSetTest extends BaseTestCase {
     public void testDeleteSplitsIntoTwo() {
         // [0-4], [8-11], [17-20], [28-32], [36-40]
         iSet.delete(new Interval(29,30));
-        assertEquals(6, numIntervals());
+        assertEquals(6, iSet.getNumberOfIntervals());
         iter = iSet.getAllIntervals();
         interval = (Interval)iter.next();
         assertEquals(0, interval.low);
@@ -520,7 +550,7 @@ public class IntervalSetTest extends BaseTestCase {
     public void testDeleteFullUpper() {
         // [0-4], [8-11], [17-20], [28-28], [31-32], [36-40]
         iSet.delete(new Interval(35,41));
-        assertEquals(5, numIntervals());
+        assertEquals(5, iSet.getNumberOfIntervals());
         iter = iSet.getAllIntervals();
         interval = (Interval)iter.next();
         assertEquals(0, interval.low);
@@ -545,7 +575,7 @@ public class IntervalSetTest extends BaseTestCase {
         toDelete.add(new Interval(8, 15));
         toDelete.add(new Interval(18, 19));
         iSet.delete(toDelete);
-        assertEquals(5, numIntervals());
+        assertEquals(5, iSet.getNumberOfIntervals());
         iter = iSet.getAllIntervals();
         interval = (Interval)iter.next();
         assertEquals(0, interval.low);
@@ -565,8 +595,12 @@ public class IntervalSetTest extends BaseTestCase {
     }
     
     public void testGetFirstAndIsEmpty() {
+        iSet = IntervalSet.createSingletonSet(0,4);
+        iSet.add(new Interval(17));
+        iSet.add(new Interval(20));
+        iSet.add(new Interval(28));
+        iSet.add(new Interval(31,32));
         // [0-4], [17-17], [20-20], [28-28], [31-32]
-        assertEquals(5, numIntervals());
         assertEquals(new Interval(0, 4), iSet.getFirst());
         iSet.delete(iSet.getFirst());
         assertFalse(iSet.isEmpty());
@@ -588,9 +622,34 @@ public class IntervalSetTest extends BaseTestCase {
         } catch(NoSuchElementException nsee) {}
     }
     
+    public void testGetLast() {
+        iSet = IntervalSet.createSingletonSet(0,4);
+        iSet.add(new Interval(17));
+        iSet.add(new Interval(20));
+        iSet.add(new Interval(28));
+        iSet.add(new Interval(31,32));
+        // [0-4], [17-17], [20-20], [28-28], [31-32]
+        assertEquals(5, iSet.getNumberOfIntervals());
+        assertEquals(new Interval(31, 32), iSet.getLast());
+        iSet.delete(iSet.getLast());
+        assertEquals(new Interval(28, 28), iSet.getLast());
+        iSet.delete(iSet.getLast());
+        assertEquals(new Interval(20, 20), iSet.getLast());
+        iSet.delete(iSet.getLast());
+        assertEquals(new Interval(17, 17), iSet.getLast());
+        iSet.delete(iSet.getLast());
+        assertEquals(new Interval(0, 4), iSet.getLast());
+        iSet.delete(iSet.getLast());
+        try {
+            iSet.getLast();
+            fail("expected exception");
+        } catch(NoSuchElementException nsee) {}
+    }
+    
     public void testInvert() {
+        iSet = new IntervalSet();
         // no intervals
-        assertEquals(0, numIntervals());
+        assertEquals(0, iSet.getNumberOfIntervals());
         IntervalSet inverted = iSet.invert(100);
         assertEquals(1, inverted.getAllIntervalsAsList().size());
         assertEquals(new Interval(0, 99), inverted.getAllIntervals().next());
@@ -600,14 +659,15 @@ public class IntervalSetTest extends BaseTestCase {
     }
     
     public void testClone() {
+        iSet = IntervalSet.createSingletonSet(0,100);
         // [0-100]
-        assertEquals(1, numIntervals());
+        assertEquals(1, iSet.getNumberOfIntervals());
         IntervalSet clone = (IntervalSet)iSet.clone();
         assertEquals(1, clone.getAllIntervalsAsList().size());
         assertEquals(new Interval(0, 100), clone.getAllIntervals().next());
         clone.delete(clone.getFirst());
         assertEquals(0, clone.getAllIntervalsAsList().size());
-        assertEquals(1, numIntervals());
+        assertEquals(1, iSet.getNumberOfIntervals());
         assertEquals(new Interval(0, 100), getIntervalAt(0));
     }
         
@@ -628,9 +688,8 @@ public class IntervalSetTest extends BaseTestCase {
     	
     	assertEquals(set.getAllIntervalsAsList(),set2.getAllIntervalsAsList());
     }
-    private int numIntervals() {
-        return iSet.getAllIntervalsAsList().size();
-    }
+    
+    ///// Private helper methods ///// 
 
     private Interval getIntervalAt(int i) {
         return (Interval)iSet.getAllIntervalsAsList().get(i);
