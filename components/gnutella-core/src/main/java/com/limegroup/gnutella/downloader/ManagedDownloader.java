251b39a3d453a43d5d5f44173ea1604551b19dea
zlatinb <zlatinb>
2005-06-24 21:51:25 +0000

    check for null value for file size in the properties map

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/downloader/ManagedDownloader.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/downloader/ManagedDownloader.java
@@ -557,7 +557,7 @@ public class ManagedDownloader implements Downloader, MeshHandler, AltLocListene
     protected synchronized void initPropertiesMap(RemoteFileDesc rfd) {
 		if (propertiesMap.get(DEFAULT_FILENAME) == null)
 			propertiesMap.put(DEFAULT_FILENAME,rfd.getFileName());
-		if (!propertiesMap.containsKey(FILE_SIZE))
+		if (propertiesMap.get(FILE_SIZE) == null)
 			propertiesMap.put(FILE_SIZE,new Integer(rfd.getSize()));
     }
     
