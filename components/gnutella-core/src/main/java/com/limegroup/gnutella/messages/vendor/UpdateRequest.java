a597d010b526becab5a2117d65fd6c4ec6fcecde
zlatinb <zlatinb>
2005-07-08 21:32:33 +0000

    receive UpdateRequests with higher version

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/messages/vendor/UpdateRequest.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/messages/vendor/UpdateRequest.java
@@ -21,11 +21,6 @@ public final class UpdateRequest extends VendorMessage {
     UpdateRequest(byte[] guid, byte ttl, byte hops, int version, 
                                   byte[] payload) throws BadPacketException {
         super(guid, ttl, hops, F_LIME_VENDOR_ID, F_UPDATE_REQ, version, payload);
-        
-        if(getVersion() > VERSION) //we don't support it
-            throw new BadPacketException("UNSUPPORTED VERSION");
-
-        //there is no payload 
     }
     
     public UpdateRequest() {
