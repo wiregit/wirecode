f142c586478f0c8aa5e952446800b24446fc0d17
zlatinb <zlatinb>
2005-07-25 17:07:17 +0000

    be less aggressive when opening new sockets on SP2 systems

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/util/Sockets.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/util/Sockets.java
@@ -25,7 +25,7 @@ public class Sockets {
     /**
      * The maximum number of concurrent connection attempts.
      */
-    private static final int MAX_CONNECTING_SOCKETS = 8;
+    private static final int MAX_CONNECTING_SOCKETS = 6;
     
     /**
      * The current number of waiting socket attempts.
