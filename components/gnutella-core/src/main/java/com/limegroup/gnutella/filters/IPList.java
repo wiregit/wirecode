b57d9f9b2a7a235249616f3dfadbd8b4b641a337
kmagdsick <kmagdsick>
2005-08-24 20:13:31 +0000

    Added new methods for determining if 2 IP addresses are in the same /26,/24,/16, etc. subnet

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/filters/IPList.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/filters/IPList.java
@@ -46,4 +46,54 @@ public class IPList {
         }
         return false;
     }
+    
+    /**
+     * Calculates the first set bit in the distance between an IPv4 address and
+     * the ranges represented by this list.
+     * 
+     * This is equivalent to floor(log2(distance)) - 1.
+     *  
+     * @param ip an IPv4 address, represented as an IP object with a /32 netmask.
+     * @return an int on the interval [0,31].
+     */
+    public int logMinDistanceTo(IP ip) {
+        int distance = minDistanceTo(ip);
+        int logDistance = 0;
+        int testMask = -1; // All bits set
+        // Guaranteed to terminate since testMask will reach zero
+        while ((distance | testMask) != 0) {
+            testMask <<= 1;
+            ++logDistance;
+        }
+        return logDistance;
+    }
+    
+    
+    /**
+     * Calculates the minimum distance between an IPv4 address this list of IPv4 address ranges.
+     * 
+     * @param ip an IPv4 address, represented as an IP object with a /32 netmask.
+     * @return 32-bit unsigned distance (using xor metric), represented as Java int
+     */
+    public int minDistanceTo(IP ip) {
+        if (ip.getMask() != -1) {
+            throw new IllegalArgumentException("Expected single IP, not an IP range.");
+        }
+        // Note that this function uses xor with Integer.MIN_VALUE
+        // to reverse the sense of the most significant bit.  This
+        // causes the "<" and ">" operators to work properly even
+        // though we're representing 32-bit unsinged values as
+        // Java ints.
+       int min_distance = Integer.MAX_VALUE;
+       for (Iterator iter=ips.iterator(); iter.hasNext();) {
+           IP ipRange = (IP) iter.next();
+           int distance = Integer.MIN_VALUE ^ ipRange.getDistanceTo(ip);
+           if (min_distance > distance) {
+               min_distance = distance;
+           }
+       }
+        
+       // Change the most significant bit back to its normal sense.
+       return Integer.MIN_VALUE ^ min_distance;
+    }
 }
