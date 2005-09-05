17de7f1a25ec0016e0afc3a7a53adf896f034243
zlatinb <zlatinb>
2005-09-05 04:38:52 +0000

    * Account for the case where ticks from the selector arrive at lower frequency than the tick times * Avoid callling handleWrite twice on WriteObservers.  If we ever have the same channel controlled under more than one throttle, this change will need to be reversed.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/io/NBThrottle.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/io/NBThrottle.java
@@ -201,6 +201,7 @@ public class NBThrottle implements Throttle {
                     i.remove();
                 } else if(key != null) {
                     NIODispatcher.instance().process(key, key.attachment(), _processOp);
+		    keys.remove(key);
                     i.remove();
                     if(_available < MINIMUM_TO_GIVE)
                         break;
@@ -248,7 +249,9 @@ public class NBThrottle implements Throttle {
      */
     void tick(long currentTime) {
         if(currentTime >= _nextTickTime) {
-            _available = _bytesPerTick;
+	    float elapsedTicks = 1 + ((float)(currentTime - _nextTickTime)) / MILLIS_PER_TICK;
+	    elapsedTicks = Math.min(elapsedTicks, 2);
+            _available = (int)(_bytesPerTick * elapsedTicks);
             _nextTickTime = currentTime + MILLIS_PER_TICK;
             spreadBandwidth();
         } else if(_available > MINIMUM_TO_GIVE) {
