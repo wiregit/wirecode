d6dca62bddc12115405dd6128acfa034bdb6f2aa
mkornfilt <mkornfilt>
2005-09-28 22:43:17 +0000

    CCPublisher tab now functional. License MetaData loading only for license type and URL. Removed 'format' field from PublishedCCLicense. Added License type to getRDFRepresentation(). Added input check to MetaEditor and MetaEditorTabbedPane.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/licenses/PublishedCCLicense.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/licenses/PublishedCCLicense.java
@@ -4,11 +4,10 @@ import com.limegroup.gnutella.Assert;
 
 public class PublishedCCLicense implements RDFLicense, EmbeddableLicense {
 
-    private final String holder, year, title, url, description, uri, format;
+    private final String holder, year, title, url, description, uri;
     private final int type;
     
     private static final String DATE_TAG = "dc:date";
-    private static final String FORMAT_TAG = "dc:format";
     private static final String IDENTIFIER_TAG = "dc:identifier"; //magnet link
     private static final String RIGHTS_TAG = "dc:rights";
     private static final String TITLE_TAG = "dc:title";
@@ -19,20 +18,23 @@ public class PublishedCCLicense implements RDFLicense, EmbeddableLicense {
     private static final String LICENSE_TAG = "license rdf:resource=\"";
     
     public PublishedCCLicense(String holder, String title, 
-            String year, String url, String description, String uri, String format, int type) {
+            String year, String url, String description, String uri, int type) {
         this.holder = holder;
         this.year = year;
         this.title = title;
         this.url = url;
         this.description = description;
         this.type = type;
-        this.format = format;
         this.uri = uri;
     }
     
     public String getRDFRepresentation() {
-        StringBuffer ret = new StringBuffer();
-        ret.append(CCConstants.CC_RDF_HEADER);
+    	
+        Assert.that(holder!=null && year!=null && title!=null && url!=null 
+        		&& uri!=null);
+    	
+    	StringBuffer ret = new StringBuffer();
+        ret.append(CCConstants.CC_RDF_HEADER).append("\n");
         
         // append the work opening line
         ret.append("<Work rdf:about=\"");
@@ -43,38 +45,27 @@ public class PublishedCCLicense implements RDFLicense, EmbeddableLicense {
         // year
         if (year != null)
             ret.append("<"+DATE_TAG+">"+year+"</"+DATE_TAG+">");
-        
         // title
         if (title != null)
             ret.append("<"+TITLE_TAG+">"+title+"</"+TITLE_TAG+">");
-        
         // description
         if (description != null)
             ret.append("<"+DESCRIPTION_TAG+">"+description+"</"+DESCRIPTION_TAG+">");
-        
         // identifier (url)
         if (url != null)
             ret.append("<"+IDENTIFIER_TAG+">"+url+"</"+IDENTIFIER_TAG+">");
-        
         // copyright holder is an Agent
         if (holder != null)
             ret.append("<"+RIGHTS_TAG+"><"+AGENT+"><"+TITLE_TAG+">"+
                     holder+"</"+TITLE_TAG+"></"+AGENT+"></"+RIGHTS_TAG+">");
-
-        // identifier (url)
-        if (format != null)
-            ret.append("<"+FORMAT_TAG+">"+format+"</"+FORMAT_TAG+">");
-        
         // hardcode sound
         ret.append("<dc:type rdf:resource=\"http://purl.org/dc/dcmitype/Sound\" />");
-        
         // the license
         ret.append("<"+LICENSE_TAG+CCConstants.getLicenseURI(type)+"\" />");
-        ret.append("</Work");
+        ret.append("</Work>").append("\n");
         
         // the license element
-        ret.append(CCConstants.getLicenseElement(type));
-        
+        ret.append(CCConstants.getLicenseElement(type)).append("\n");
         ret.append(CCConstants.CC_RDF_FOOTER);
         return ret.toString();
     }
@@ -90,8 +81,8 @@ public class PublishedCCLicense implements RDFLicense, EmbeddableLicense {
             ret.append("Description: "+description+"\n");
         if (holder != null)
             ret.append("Holder: "+holder+"\n");
-        
-        ret.append("verify at: "+url);
+        	ret.append("Licensed under: "+ CCConstants.getLicenseURI(type)+" ");
+        ret.append("verify at "+url);
         return ret.toString();
     }
 }
--- components/gnutella-core/src/main/java/com/limegroup/gnutella/settings/CCPublisherSettings.java
+++
@@ -1,9 +0,0 @@
-package com.limegroup.gnutella.settings;
-
-public class CCPublisherSettings extends LimeProps {  
-
-	private CCPublisherSettings(){}
-	
-	public static final BooleanSetting CCPUBLISHER_WARN= 
-		FACTORY.createBooleanSetting("CCPUBLISHER_WARN", false);
-}
