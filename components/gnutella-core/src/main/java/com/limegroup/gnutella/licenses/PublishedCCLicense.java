62977bef06aca7ae1d1c341a59343e3691f612d6
mkornfilt <mkornfilt>
2005-09-29 20:34:48 +0000

    Changed embedded license string to follow creative common's pseudo-standard. Modified LibraryTableMediator to make publish button follow the annotate button's behavior.CCPublisher tab now only publishes cc licenses. Fixed meta refresh bug by hacking MetaEditor's 'ok' thread.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/licenses/PublishedCCLicense.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/licenses/PublishedCCLicense.java
@@ -38,26 +38,21 @@ public class PublishedCCLicense implements RDFLicense, EmbeddableLicense {
         
         // append the work opening line
         ret.append("<Work rdf:about=\"");
-        if (uri != null)
-            ret.append(uri);
+        ret.append(uri);
         ret.append("\">");
         
         // year
-        if (year != null)
-            ret.append("<"+DATE_TAG+">"+year+"</"+DATE_TAG+">");
+        ret.append("<"+DATE_TAG+">"+year+"</"+DATE_TAG+">");
         // title
-        if (title != null)
-            ret.append("<"+TITLE_TAG+">"+title+"</"+TITLE_TAG+">");
+        ret.append("<"+TITLE_TAG+">"+title+"</"+TITLE_TAG+">");
         // description
         if (description != null)
-            ret.append("<"+DESCRIPTION_TAG+">"+description+"</"+DESCRIPTION_TAG+">");
+        ret.append("<"+DESCRIPTION_TAG+">"+description+"</"+DESCRIPTION_TAG+">");
         // identifier (url)
-        if (url != null)
-            ret.append("<"+IDENTIFIER_TAG+">"+url+"</"+IDENTIFIER_TAG+">");
+        ret.append("<"+IDENTIFIER_TAG+">"+url+"</"+IDENTIFIER_TAG+">");
         // copyright holder is an Agent
-        if (holder != null)
-            ret.append("<"+RIGHTS_TAG+"><"+AGENT+"><"+TITLE_TAG+">"+
-                    holder+"</"+TITLE_TAG+"></"+AGENT+"></"+RIGHTS_TAG+">");
+        ret.append("<"+RIGHTS_TAG+"><"+AGENT+"><"+TITLE_TAG+">"+
+                holder+"</"+TITLE_TAG+"></"+AGENT+"></"+RIGHTS_TAG+">");
         // hardcode sound
         ret.append("<dc:type rdf:resource=\"http://purl.org/dc/dcmitype/Sound\" />");
         // the license
@@ -71,17 +66,13 @@ public class PublishedCCLicense implements RDFLicense, EmbeddableLicense {
     }
     
     public String getEmbeddableString() {
-        Assert.that(url != null);
+    	 Assert.that(holder!=null && year!=null && title!=null && url!=null 
+         		&& uri!=null);
         StringBuffer ret = new StringBuffer();
-        if (holder != null)
-            ret.append("Holder: "+holder+"\n");
-        if (title != null)
-            ret.append("Title: "+title+"\n");
-        if (description != null)
-            ret.append("Description: "+description+"\n");
-        if (holder != null)
-            ret.append("Holder: "+holder+"\n");
-        	ret.append("Licensed under: "+ CCConstants.getLicenseURI(type)+" ");
+        ret.append(year+" ");
+        ret.append(holder+". ");
+        ret.append("Licensed to the public under ");
+    	ret.append(CCConstants.getLicenseURI(type)+" ");
         ret.append("verify at "+url);
         return ret.toString();
     }
