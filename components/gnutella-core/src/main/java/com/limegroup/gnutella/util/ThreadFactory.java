4ba17219b0d44e3967bc35b6cfa59b7badafcf87
sberlin <sberlin>
2006-02-02 23:28:48 +0000

    preserve the thread's name for error reports.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/util/ThreadFactory.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/util/ThreadFactory.java
@@ -1,11 +1,14 @@
 package com.limegroup.gnutella.util;
 
+import com.limegroup.gnutella.ErrorService;
+
 /** Contains a global ThreadPool & methods for adding named threads to it. */
 public class ThreadFactory {
 
     private static final ThreadFactory INSTANCE = new ThreadFactory();
     
-    private final ThreadPool POOL = new DefaultThreadPool("IdleThread");
+    // create an unmanaged ThreadPool -- we'll manage it ourselves.
+    private final ThreadPool POOL = new DefaultThreadPool("IdleThread", false);
 
     public static void startThread(final Runnable runner, final String name) {
         INSTANCE.POOL.invokeLater(new Runnable() {
@@ -13,6 +16,10 @@ public class ThreadFactory {
                 try {
                     Thread.currentThread().setName(name);
                     runner.run();
+                } catch(Throwable t) {
+                    // must handle ErrorService internally, 
+                    // otherwise the thread name will be lost.
+                    ErrorService.error(t);
                 } finally {
                     Thread.currentThread().setName("IdleThread");
                 }
