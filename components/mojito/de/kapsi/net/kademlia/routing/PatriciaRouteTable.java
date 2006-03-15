85f2126b2652a7905a54768bbd7bd5f8cccd0b4f
mkornfilt <mkornfilt>
2006-03-15 22:00:50 +0000

    commiting bucket split count bug

--- components/mojito/de/kapsi/net/kademlia/routing/PatriciaRouteTable.java
+++ components/mojito/de/kapsi/net/kademlia/routing/PatriciaRouteTable.java
@@ -118,10 +118,16 @@ public class PatriciaRouteTable implements RoutingTable{
                 //attempt the put the new contact again with the split buckets
                 BucketNode newBucket = (BucketNode)bucketsTrie.select(nodeId);
                 //this should never happen
-                if(countLeft+countRight != K) {
+                if(countLeft+countRight != bucket.getNodeCount()) {
                     if (LOG.isErrorEnabled()) {
                         LOG.error("Bucket did not split correctly!");
                     }
+                    System.out.println(bucket);
+                    System.out.println(leftSplitBucket);
+                    System.out.println(rightSplitBucket);
+//                    System.out.println(nodesTrie);
+                    System.out.println(nodesTrie.range(leftSplitBucket.getNodeID(),leftSplitBucket.getDepth()-1));
+                    System.out.println(nodesTrie.range(rightSplitBucket.getNodeID(),rightSplitBucket.getDepth()-1));
                     return;
                 }
                 if(newBucket.equals(bucket)) {
