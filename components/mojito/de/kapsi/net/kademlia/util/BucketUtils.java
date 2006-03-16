0218fa684f5c1fc83712dc7af9d5f71c73bab4ec
rkapsi <rkapsi>
2006-03-16 20:13:14 +0000

    Comments and renaming getLeastSeen to getMostRecentlySeen

--- components/mojito/de/kapsi/net/kademlia/util/BucketUtils.java
+++ components/mojito/de/kapsi/net/kademlia/util/BucketUtils.java
@@ -15,14 +15,26 @@ public final class BucketUtils {
     
     private BucketUtils() {}
     
-    public static ContactNode getLastSeen(List bucketList) {
+    /**
+     * Returns the most recently seen contact from the list.
+     * Use BucketUtils.sort() prior to calling this Method!
+     */
+    public static ContactNode getMostRecentlySeen(List bucketList) {
         return (ContactNode)bucketList.get(0);
     }
     
+    /**
+     * Returns the least recently seen contact from the list.
+     * Use BucketUtils.sort() prior to calling this Method!
+     */
     public static ContactNode getLeastRecentlySeen(List bucketList) {
         return (ContactNode)bucketList.get(bucketList.size()-1);
     }
     
+    /**
+     * Sorts the contacts from most recently seen to
+     * least recently seen
+     */
     public static List sort(List bucketList) {
         Collections.sort(bucketList, new Comparator() {
             public int compare(Object a, Object b) {
