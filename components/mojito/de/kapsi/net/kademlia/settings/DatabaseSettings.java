621962b8ac0e0e08b3b8d366296a2fe14c03dcb6
rkapsi <rkapsi>
2006-04-12 16:00:33 +0000

    Comments

--- components/mojito/de/kapsi/net/kademlia/settings/DatabaseSettings.java
+++ components/mojito/de/kapsi/net/kademlia/settings/DatabaseSettings.java
@@ -19,7 +19,9 @@
  
 package de.kapsi.net.kademlia.settings;
 
-
+/**
+ * Settings for Database, KeyValues and for the republisher 
+ */
 public final class DatabaseSettings extends LimeDHTProps {
     
     //public static final long MILLIS_PER_DAY = 24L * 60L * 60L * 1000L; // 24 horus
@@ -31,23 +33,47 @@ public final class DatabaseSettings extends LimeDHTProps {
     
     private DatabaseSettings() {}
     
-    // TODO reasonable min and max values
+    /**
+     * The maximum number of Keys a single Node can store
+     * 
+     * TODO reasonable min and max values
+     */
     public static final IntSetting MAX_DATABASE_SIZE
         = FACTORY.createSettableIntSetting("MAX_DATABASE_SIZE", 16384, "max_database_size", 16384, 65536);
-        
-    // TODO reasonable min and max values
+    
+    /**
+     * The maximum number of Values per Key a single Node can store
+     * 
+     * TODO reasonable min and max values
+     */
     public static final IntSetting MAX_KEY_VALUES
         = FACTORY.createSettableIntSetting("MAX_KEY_VALUES", 5, "max_key_values", 1, 10);
     
-    // TODO reasonable min and max values
+    /**
+     * The maximum number of KeyValues that can be store-forwarded.
+     * 
+     * TODO reasonable min and max values
+     */
     public static final IntSetting MAX_STORE_FORWARD
         = FACTORY.createSettableIntSetting("MAX_STORE_FORWARD", 16384, "max_store_forward", 16384, 65536);
     
-    // TODO reasonable min and max values
+    /**
+     * The maximum number of KeyValues a Node can request or send
+     * at once.
+     * 
+     * TODO reasonable min and max values
+     */
     public static final IntSetting MAX_STORE_FORWARD_ONCE
         = FACTORY.createSettableIntSetting("MAX_STORE_FORWARD_ONCE", 5, "max_store_forward_once", 1, 10);
     
-    // TODO reasonable min and max values
+    /**
+     * The republishing interval in milliseconds.
+     * 
+     * TODO it is currently set to 3 Minutes for TESTING purposes!
+     *    CHANGE IT OR IT WILL SCREW US!
+     *    
+     * TODO reasonable min and max values
+     */
     public static final LongSetting REPUBLISH_INTERVAL
         = FACTORY.createSettableLongSetting("REPUBLISH_INTERVAL", 3L*60L*1000L, "republish_interval", 3L*60L*1000L, 3L*60L*1000L);
 }
