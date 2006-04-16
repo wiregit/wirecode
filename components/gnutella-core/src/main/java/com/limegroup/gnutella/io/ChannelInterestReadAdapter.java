2cc79f953be02a1452a51ed143efcd302f5a6cbb
zlatinb <zlatinb>
2006-04-16 19:43:20 +0000

    ChannelInterestReadAdapter needs to support scattering reads for incoming bt connections

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/io/ChannelInterestReadAdapter.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/io/ChannelInterestReadAdapter.java
@@ -3,7 +3,8 @@ package com.limegroup.gnutella.io;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
-public abstract class ChannelInterestReadAdapter implements ChannelReadObserver, InterestReadChannel {
+public abstract class ChannelInterestReadAdapter implements ChannelReadObserver, 
+InterestScatteringByteChannel {
     protected ByteBuffer buffer;
     protected InterestReadChannel source;
     protected boolean shutdown;
@@ -16,6 +17,14 @@ public abstract class ChannelInterestReadAdapter implements ChannelReadObserver,
 
     public int read(ByteBuffer dst) {
         return BufferUtils.transfer(buffer, dst);
+    }
+    
+    public long read(ByteBuffer []dest, int offset, int length) {
+    	return BufferUtils.transfer(buffer, dest, offset, length);
+    }
+    
+    public long read(ByteBuffer []dest) {
+    	return read(dest,0,dest.length);
     }
 
     public void shutdown() {
