f35104c28e3a7f2ce4322b4fe34a7ad594abf9d4
mkornfilt <mkornfilt>
2006-05-10 19:37:09 +0000

    record global FIND_VALUE stats on branch

--- components/mojito/com/limegroup/gnutella/dht/statistics/FindValueLookupStatisticContainer.java
+++ components/mojito/com/limegroup/gnutella/dht/statistics/FindValueLookupStatisticContainer.java
@@ -47,35 +47,46 @@ public class FindValueLookupStatisticContainer extends SingleLookupStatisticCont
     public Statistic FIND_VALUE_FAILURE =
         new SimpleStatistic();
     
+    private final GlobalLookupStatisticContainer globalLookupStats;
     
     
     public FindValueLookupStatisticContainer(Context context, KUID lookupKey) {
         super(context, lookupKey);
+        this.globalLookupStats = context.getGlobalLookupStats();
     }
 
 
     public void setHops(int hops) {
+        super.setHops(hops);
+        globalLookupStats.GLOBAL_FIND_VALUE_LOOKUP_HOPS.incrementStat(); 
+        globalLookupStats.GLOBAL_FIND_VALUE_LOOKUP_HOPS.storeCurrentStat();
         FIND_VALUE_LOOKUP_HOPS.addData(hops);
         FIND_VALUE_LOOKUP_HOPS.storeCurrentStat();
     }
     
     public void setTime(int time) {
+        super.setTime(time);
+        globalLookupStats.GLOBAL_FIND_VALUE_LOOKUP_TIME.addData(time); 
+        globalLookupStats.GLOBAL_FIND_VALUE_LOOKUP_TIME.storeCurrentStat();
         FIND_VALUE_LOOKUP_TIME.addData(time);
         FIND_VALUE_LOOKUP_TIME.storeCurrentStat();
     }
 
 
     public void addReply() {
+        super.addReply();
         FIND_VALUE_LOOKUP_REPLIES.incrementStat();
     }
 
 
     public void addRequest() {
+        super.addRequest();
         FIND_VALUE_LOOKUP_REQUESTS.incrementStat();
     }
 
 
     public void addTimeout() {
+        super.addTimeout();
         FIND_VALUE_LOOKUP_TIMEOUTS.incrementStat();
     }
 }
