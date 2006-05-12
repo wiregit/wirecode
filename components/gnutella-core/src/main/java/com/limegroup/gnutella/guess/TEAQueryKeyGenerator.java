d8a9f519d1b06cb9680db3c96ea9aa69c658dbab
rkapsi <rkapsi>
2006-05-12 20:18:50 +0000

    Removing an unused byte Array

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/guess/TEAQueryKeyGenerator.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/guess/TEAQueryKeyGenerator.java
@@ -76,7 +76,6 @@ import com.limegroup.gnutella.security.SecurityUtils;
      *  0x1C and 0x00, to accomidate clients that poorly parse GGEP.
      */
     public byte[] getKeyBytes(InetAddress ip, int port) {
-        byte[] toEncrypt = new byte[8];
         // get all the input bytes....
         byte[] ipBytes = ip.getAddress();
         int ipInt = 0;
