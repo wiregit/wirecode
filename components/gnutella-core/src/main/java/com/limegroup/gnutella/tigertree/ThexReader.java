106261b4a09a0fb917b954902b606ddc69e81043
cvs2svn <admin@example.com>
2006-05-22 22:03:33 +0000

    This commit was manufactured by cvs2svn to create tag 'signed-stats-branch- point'.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/tigertree/ThexReader.java
+++
@@ -1,9 +0,0 @@
-package com.limegroup.gnutella.tigertree;
-
-import java.io.IOException;
-
-import com.limegroup.gnutella.io.IOState;
-
-public interface ThexReader extends IOState {    
-    public HashTree getHashTree() throws IOException;
-}
