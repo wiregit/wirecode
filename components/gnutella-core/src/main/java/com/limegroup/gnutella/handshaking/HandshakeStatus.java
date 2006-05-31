a0f0ce0a745addbd9190bdff440efa9aae17e6e7
sberlin <sberlin>
2006-05-31 22:37:13 +0000

    shorter & sweeter msgs.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/handshaking/HandshakeStatus.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/handshaking/HandshakeStatus.java
@@ -19,20 +19,20 @@ public class HandshakeStatus {
     
     /* All bad statuses. */
     public static final HandshakeStatus NO_X_ULTRAPEER = new HandshakeStatus("No X-Ultrapeer.");
-    public static final HandshakeStatus DISCONNECTED = new HandshakeStatus("I am disconnected");
-    public static final HandshakeStatus WE_ARE_LEAVES = new HandshakeStatus("You and I are leaves");
-    public static final HandshakeStatus NOT_GOOD_UP = new HandshakeStatus("You are not a 'good' Ultrapeer.");
-    public static final HandshakeStatus IDLE_LIMEWIRE = new HandshakeStatus("I am idle and want a LimeWire");
-    public static final HandshakeStatus STARTING_LIMEWIRE = new HandshakeStatus("I am just starting and want a LimeWire");
-    public static final HandshakeStatus TOO_MANY_UPS = new HandshakeStatus("I have too many ultrapeer connections");
-    public static final HandshakeStatus NOT_ALLOWED_LEAF = new HandshakeStatus("This leaf connection not allowed");
-    public static final HandshakeStatus NOT_GOOD_LEAF = new HandshakeStatus("You are not a 'good' leaf.");
-    public static final HandshakeStatus TOO_MANY_LEAF = new HandshakeStatus("I have too many leaf connections");
-    public static final HandshakeStatus NOT_ALLOWED_UP = new HandshakeStatus("This ultrapeer connection not allowed");
-    public static final HandshakeStatus NON_LIME_RATIO = new HandshakeStatus("Non-LimeWire peer ratio exceeded.");
-    public static final HandshakeStatus NO_LIME_SLOTS = new HandshakeStatus("No LimeWire slots available");
-    public static final HandshakeStatus NO_HEADERS = new HandshakeStatus("No headers received.");
-    public static final HandshakeStatus UNKNOWN = new HandshakeStatus("Unknown connection failure");
+    public static final HandshakeStatus DISCONNECTED = new HandshakeStatus("I'm Disconnected");
+    public static final HandshakeStatus WE_ARE_LEAVES = new HandshakeStatus("We're Leaves");
+    public static final HandshakeStatus NOT_GOOD_UP = new HandshakeStatus("Not Good Ultrapeer.");
+    public static final HandshakeStatus IDLE_LIMEWIRE = new HandshakeStatus("Idle, Need LimeWire");
+    public static final HandshakeStatus STARTING_LIMEWIRE = new HandshakeStatus("Starting, Need LimeWire");
+    public static final HandshakeStatus TOO_MANY_UPS = new HandshakeStatus("No Ultrapeer Slots");
+    public static final HandshakeStatus NOT_ALLOWED_LEAF = new HandshakeStatus("Leaf Connection Failed");
+    public static final HandshakeStatus NOT_GOOD_LEAF = new HandshakeStatus("Not Good Leaf");
+    public static final HandshakeStatus TOO_MANY_LEAF = new HandshakeStatus("No Leaf Slots");
+    public static final HandshakeStatus NOT_ALLOWED_UP = new HandshakeStatus("Ultrapeer Connection Failed");
+    public static final HandshakeStatus NON_LIME_RATIO = new HandshakeStatus("Non-LimeWire Slots Full");
+    public static final HandshakeStatus NO_LIME_SLOTS = new HandshakeStatus("No LimeWire Slots");
+    public static final HandshakeStatus NO_HEADERS = new HandshakeStatus("No Headers Received.");
+    public static final HandshakeStatus UNKNOWN = new HandshakeStatus("Unknown Handshake Failure");
     
     public String getMessage() {
         return msg;
