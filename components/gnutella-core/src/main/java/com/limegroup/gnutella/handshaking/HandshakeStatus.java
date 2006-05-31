00c2ca75c02954e067fa03e373d2904c2c459272
sberlin <sberlin>
2006-05-31 22:39:24 +0000

    removed periods.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/handshaking/HandshakeStatus.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/handshaking/HandshakeStatus.java
@@ -21,7 +21,7 @@ public class HandshakeStatus {
     public static final HandshakeStatus NO_X_ULTRAPEER = new HandshakeStatus("No X-Ultrapeer.");
     public static final HandshakeStatus DISCONNECTED = new HandshakeStatus("I'm Disconnected");
     public static final HandshakeStatus WE_ARE_LEAVES = new HandshakeStatus("We're Leaves");
-    public static final HandshakeStatus NOT_GOOD_UP = new HandshakeStatus("Not Good Ultrapeer.");
+    public static final HandshakeStatus NOT_GOOD_UP = new HandshakeStatus("Not Good Ultrapeer");
     public static final HandshakeStatus IDLE_LIMEWIRE = new HandshakeStatus("Idle, Need LimeWire");
     public static final HandshakeStatus STARTING_LIMEWIRE = new HandshakeStatus("Starting, Need LimeWire");
     public static final HandshakeStatus TOO_MANY_UPS = new HandshakeStatus("No Ultrapeer Slots");
@@ -31,7 +31,7 @@ public class HandshakeStatus {
     public static final HandshakeStatus NOT_ALLOWED_UP = new HandshakeStatus("Ultrapeer Connection Failed");
     public static final HandshakeStatus NON_LIME_RATIO = new HandshakeStatus("Non-LimeWire Slots Full");
     public static final HandshakeStatus NO_LIME_SLOTS = new HandshakeStatus("No LimeWire Slots");
-    public static final HandshakeStatus NO_HEADERS = new HandshakeStatus("No Headers Received.");
+    public static final HandshakeStatus NO_HEADERS = new HandshakeStatus("No Headers Received");
     public static final HandshakeStatus UNKNOWN = new HandshakeStatus("Unknown Handshake Failure");
     
     public String getMessage() {
