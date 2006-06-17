fc5e53aef4e707e60a6fbe388d7fb35c79208317
rkapsi <rkapsi>
2006-06-17 17:33:27 +0000

    ByteBufferInputStream can read from an array of ByteBuffers now

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/util/ByteBufferInputStream.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/util/ByteBufferInputStream.java
@@ -19,7 +19,6 @@
 
 package com.limegroup.gnutella.util;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
@@ -30,26 +29,48 @@ import java.nio.ByteBuffer;
  */
 public class ByteBufferInputStream extends InputStream {
 
-    protected final ByteBuffer buffer;
-
+    private int index = 0;
+    private int mark = -1;
+    
+    private ByteBuffer[] buffers;
+    
     public ByteBufferInputStream(ByteBuffer buffer) {
-        this.buffer = buffer;
+        this(new ByteBuffer[]{buffer});
+    }
+    
+    public ByteBufferInputStream(ByteBuffer[] buffers) {
+        this.buffers = buffers;
     }
     
     public int available() throws IOException {
-        return buffer.remaining();
+        int available = 0;
+        for(int i = buffers.length-1; i >= index; --i) {
+            available += buffers[i].remaining();
+        }
+        return available;
     }
-
+    
     /** Does nothing */
     public void close() throws IOException {
     }
 
     public void mark(int readlimit) {
-        buffer.mark();
+        if (index < buffers.length) {
+            mark = index;
+            for(int i = buffers.length-1; i >= mark; --i) {
+                buffers[i].mark();
+            }
+        }
     }
 
     public void reset() throws IOException {
-        buffer.reset();
+        if (mark != -1) {
+            index = mark;
+            for(int i = buffers.length-1; i >= index ; --i) {
+                buffers[i].reset();
+            }
+            mark = -1;
+        }
     }
 
     public boolean markSupported() {
@@ -57,11 +78,17 @@ public class ByteBufferInputStream extends InputStream {
     }
 
     public int read() throws IOException {
-        if (available() == 0) {
-            return -1;
+        while(index < buffers.length) {
+            ByteBuffer b = buffers[index];
+            if (b.hasRemaining()) {
+                return b.get() & 0xFF;
+            }
+            
+            // Try next ByteBuffer
+            ++index;
         }
-
-        return buffer.get() & 0xFF;
+        
+        return -1;
     }
 
     public int read(byte[] b, int off, int len) throws IOException {
@@ -71,34 +98,63 @@ public class ByteBufferInputStream extends InputStream {
                 || ((off + len) > b.length) || ((off + len) < 0)) {
             throw new IndexOutOfBoundsException();
         }
-
-        if (len == 0) {
-            return 0;
-        }
-
-        int available = available();
-        if (available == 0) {
+        
+        if (index >= buffers.length) {
             return -1;
-        } else if (len > available) {
-            len = available;
-        }
-
-        buffer.get(b, off, len);
-        return len;
-    }
-
-    public long skip(long n) throws IOException {
-        long newPos = ((long) buffer.position() & 0xFFFFFFFFL) + n;
-        long limit = (long) buffer.limit() & 0xFFFFFFFFL;
-        if (newPos > limit) {
-            newPos = limit;
-        }
-
-        if (n < 0L || newPos < 0L) {
-            return 0L;
+        } else if (index == buffers.length-1 
+                && !buffers[index].hasRemaining()) {
+            return -1;
         }
         
-        buffer.position((int) newPos);
-        return n;
+        int r = 0;
+        boolean zero = true;
+        while(index < buffers.length) {
+            ByteBuffer buf = buffers[index];
+            
+            // The number of bytes we can read
+            int l = Math.min(buf.remaining(), len-r);
+            
+            // Remember if any bytes were available
+            if (zero && buf.hasRemaining()) {
+                zero = false;
+            }
+            
+            // Do read
+            buf.get(b, off+r, l);
+            r += l;
+            
+            // Exit if done
+            if (r == len) {
+                break;
+            }
+            
+            // Try next ByteBuffer
+            ++index;
+        }
+        
+        // Return EOF if no bytes were available
+        return (zero ? -1 : r);
+    }
+    
+    public long skip(long n) throws IOException {
+        long s = 0L;
+        while(index < buffers.length) {
+            ByteBuffer b = buffers[index];
+            
+            // The number of bytes we can skip in this ByteBuffer
+            long l = Math.min((long)b.remaining(), n-s);
+            b.position(b.position() + (int)l);
+            s += l;
+            
+            // Exit if done
+            if (s == n) {
+                break;
+            }
+            
+            // Try next ByteBuffer
+            ++index;
+        }
+        
+        return s;
     }
 }
