095f11d23cb82b393fdbe407ec026529f5063778
zlatinb <zlatinb>
2006-09-26 17:39:04 +0000

    remove files added to the tag 2

--- components/gnutella-core/src/main/java/com/limegroup/bittorrent/TorrentEventListener.java
+++
@@ -1,7 +0,0 @@
-package com.limegroup.bittorrent;
-
-import java.util.EventListener;
-
-public interface TorrentEventListener extends EventListener{
-	public void handleTorrentEvent(TorrentEvent evt);
-}
--- components/gnutella-core/src/main/java/com/limegroup/gnutella/util/SchedulingThreadPool.java
+++
@@ -1,7 +0,0 @@
-package com.limegroup.gnutella.util;
-
-import java.util.concurrent.Future;
-
-public interface SchedulingThreadPool extends ThreadPool {
-	public Future invokeLater(Runnable r, long delay);
-}
