60b001984a9273f9a1cc3339e86cf91053b61eec
rkapsi <rkapsi>
2006-09-29 15:59:19 +0000

    And yet another println()

--- components/mojito/src/test/com/limegroup/mojito/manager/BootstrapManagerTest.java
+++ components/mojito/src/test/com/limegroup/mojito/manager/BootstrapManagerTest.java
@@ -50,8 +50,6 @@ public class BootstrapManagerTest extends BaseTestCase {
     protected void setUp() throws Exception {
         setSettings();
         
-        System.out.println("HERE!!!");
-        
         //setup bootstrap node
         BOOTSTRAP_DHT = MojitoFactory.createDHT("bootstrapNode");
         BOOTSTRAP_DHT.bind(BOOTSTRAP_DHT_PORT);
