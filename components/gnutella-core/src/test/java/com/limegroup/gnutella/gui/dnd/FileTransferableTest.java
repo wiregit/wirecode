202f622a2ecfcea762c6728c9afdcd987f9f5170
fberger <fberger>
2006-10-10 17:28:35 +0000

    Moved older dnd files from gui.util to gui.dnd. Made FileTransferable support the linux uri flavor. Added rudimentary test case.

---
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/gui/dnd/FileTransferableTest.java
@@ -0,0 +1,27 @@
+package com.limegroup.gnutella.gui.dnd;
+
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import com.limegroup.gnutella.util.BaseTestCase;
+
+public class FileTransferableTest extends BaseTestCase {
+
+	public FileTransferableTest(String name) {
+		super(name);
+	}
+	
+	public void testGetTransferData() throws UnsupportedFlavorException, IOException {
+		List<File> files = Arrays.asList(new File("/test/file1"), new File("/test/file2"));
+		Transferable transferable = new FileTransferable(files);
+		assertEquals(files, transferable.getTransferData(DataFlavor.javaFileListFlavor));
+		assertEquals("file:/test/file1\nfile:/test/file2",
+				transferable.getTransferData(FileTransferable.linuxURIFlavor));
+	}
+
+}
