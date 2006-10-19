a1fff821625c55db64dbcfed530a2e57368e5e95
fberger <fberger>
2006-10-19 15:56:44 +0000

    Removed imports. Disallow internal file drops.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/downloader/ManagedDownloader.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/downloader/ManagedDownloader.java
@@ -19,7 +19,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
