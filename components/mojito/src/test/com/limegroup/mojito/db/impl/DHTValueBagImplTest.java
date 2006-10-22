496ced0ca26d3966cd7bce83559b45d2b0169723
mkornfilt <mkornfilt>
2006-10-22 22:40:49 +0000

    Added a test

--- components/mojito/src/test/com/limegroup/mojito/db/impl/DHTValueBagImplTest.java
+++ components/mojito/src/test/com/limegroup/mojito/db/impl/DHTValueBagImplTest.java
@@ -1,10 +1,17 @@
 package com.limegroup.mojito.db.impl;
 
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+
 import junit.framework.Test;
 
 import com.limegroup.gnutella.util.BaseTestCase;
 import com.limegroup.gnutella.util.PrivilegedAccessor;
+import com.limegroup.mojito.Contact;
 import com.limegroup.mojito.KUID;
+import com.limegroup.mojito.db.DHTValueFactory;
+import com.limegroup.mojito.db.DHTValue.ValueType;
+import com.limegroup.mojito.routing.ContactFactory;
 import com.limegroup.mojito.settings.DatabaseSettings;
 
 public class DHTValueBagImplTest extends BaseTestCase {
@@ -51,6 +58,28 @@ public class DHTValueBagImplTest extends BaseTestCase {
         load = bag.incrementRequestLoad();
         //should now have increased (a lot!)
         assertGreaterThan(1f, load);
+    }
+    
+    public void testAddInvalid() throws Exception {
+    	KUID valueId = KUID.createRandomID();
+    	DHTValueBagImpl bag = new DHTValueBagImpl(valueId);
+    	
+    	SocketAddress addr = new InetSocketAddress(6666);
+        Contact orig = ContactFactory.createLiveContact(addr, 0, 0, 
+        		KUID.createRandomID(), addr, 0, false);
+        Contact sender = ContactFactory.createLiveContact(addr, 0, 0, 
+        		KUID.createRandomID(), addr, 0, false);   
+        
+        try {
+        	bag.add(DHTValueFactory.createRemoteValue(orig, sender, 
+        			KUID.createRandomID(), ValueType.BINARY, "test".getBytes()));
+        	assertTrue("Should have thrown an exception: wrong KUID", false);
+        } catch(IllegalArgumentException ex) {}
+        
+        bag.add(DHTValueFactory.createRemoteValue(orig, sender, 
+    			valueId, ValueType.BINARY, "test".getBytes()));
+        assertEquals(1, bag.size());
+        assertFalse(bag.isEmpty());
     }
 
 }
