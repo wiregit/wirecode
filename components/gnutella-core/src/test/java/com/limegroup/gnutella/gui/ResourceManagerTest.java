b8d568ab77b200c4ab0e4750729a6157f4010faa
fberger <fberger>
2006-12-20 18:34:44 +0000

    Added test.

---
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/gui/ResourceManagerTest.java
@@ -0,0 +1,30 @@
+package com.limegroup.gnutella.gui;
+
+import java.util.Locale;
+
+import com.limegroup.gnutella.settings.ApplicationSettings;
+import com.limegroup.gnutella.util.BaseTestCase;
+
+public class ResourceManagerTest extends BaseTestCase {
+
+	public ResourceManagerTest(String name) {
+		super(name);
+	}
+	
+	static void setLocaleSettings(Locale locale) {
+		ApplicationSettings.LANGUAGE.setValue(locale.getLanguage());
+        ApplicationSettings.COUNTRY.setValue(locale.getCountry());
+        ApplicationSettings.LOCALE_VARIANT.setValue(locale.getVariant());
+        ResourceManager.resetLocaleOptions();
+	}
+	
+	public void testResetLocaleOptions() {
+		setLocaleSettings(Locale.US);
+		assertEquals(Locale.US, ResourceManager.getLocale());
+		setLocaleSettings(Locale.GERMAN);
+		assertEquals(Locale.GERMAN, ResourceManager.getLocale());
+		setLocaleSettings(Locale.GERMANY);
+		assertEquals(Locale.GERMANY, ResourceManager.getLocale());
+	}
+
+}
