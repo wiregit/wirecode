76226001e21d5781bb9487c40d50a6556f46f396
sberlin <sberlin>
2007-01-04 18:17:22 +0000

    require when setting up the test that BaseTestCase exposes all the methods available in ErrorCallback, instead of failing later when the method is needed.

--- components/common/src/test/java/org/limewire/util/DuckType.java
+++ components/common/src/test/java/org/limewire/util/DuckType.java
@@ -30,11 +30,36 @@ public class DuckType implements InvocationHandler {
      * 
      * @param interfaceToImplement The interface that must be implemented
      * @param object The object calls will delegate to
-     * @return
+     * @return A new object that implements the interface.
      */
     public static Object implement(Class interfaceToImplement, Object object) {
+        if(!instanceOf(interfaceToImplement, object))
+            throw new IllegalArgumentException("object: " + object + " does not have all the methods of: " + interfaceToImplement);
         return Proxy.newProxyInstance(interfaceToImplement.getClassLoader(), 
             new Class[] {interfaceToImplement}, new DuckType(object));
     }
+    
+    /**
+     * Indicates if object is a (DuckType) instance of intrface.  That is,
+     * is every method in intrface present on object.
+     * 
+     * @param intrface The interface to implement
+     * @param object The object to test
+     * @return true if every method in intrface is present on object.  false otherwise
+     */
+    public static boolean instanceOf(Class intrface, Object object) {
+        final Method[] methods = intrface.getMethods();
+        Class candclass=object.getClass();
+        for (int methodidx = 0; methodidx < methods.length; methodidx++) {
+            Method method=methods[methodidx];
+            try {
+                candclass.getMethod(method.getName(), method.getParameterTypes());
+            } catch (NoSuchMethodException e) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 }
