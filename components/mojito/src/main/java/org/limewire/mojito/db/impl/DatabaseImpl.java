990aa4f110b106710d95378b5d2a199c8ee4f3ad
rkapsi <rkapsi>
2007-02-13 18:57:11 +0000

    Don't ban the localhost

--- components/mojito/src/main/java/org/limewire/mojito/db/impl/DatabaseImpl.java
+++ components/mojito/src/main/java/org/limewire/mojito/db/impl/DatabaseImpl.java
@@ -275,7 +275,7 @@ public class DatabaseImpl implements Database {
         DHTValueBag bag = database.get(entity.getKey());
         
         // TODO: exclude signed value also
-        if (bag == null) {
+        if (bag == null && !entity.isLocalValue()) {
             // First check if the node is flooding us with keys:
             // We can only check for flooding by the value creator, not by the sender, 
             // because that could be misused to prevent us from storing real values
