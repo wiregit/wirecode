dbed7fe0e4194b1034222dd36a6cdba4b19753e8
rkapsi <rkapsi>
2007-03-16 15:52:43 +0000

    Two more tests

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/dht/PassiveLeafForwardContactsTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/dht/PassiveLeafForwardContactsTest.java
@@ -77,20 +77,11 @@ public class PassiveLeafForwardContactsTest extends LimeTestCase {
         UltrapeerSettings.NEED_MIN_CONNECT_TIME.setValue(false);
         
         ConnectionSettings.CONNECT_ON_STARTUP.setValue(false);
-        ConnectionSettings.LOCAL_IS_PRIVATE.setValue(false);    
+        ConnectionSettings.LOCAL_IS_PRIVATE.setValue(false);
         ConnectionSettings.USE_GWEBCACHE.setValue(false);
         ConnectionSettings.WATCHDOG_ACTIVE.setValue(false);
         PingPongSettings.PINGS_ACTIVE.setValue(false);
         ConnectionSettings.EVER_ACCEPTED_INCOMING.setValue(false);
-        
-        DHTSettings.DISABLE_DHT_USER.setValue(false);
-        DHTSettings.DISABLE_DHT_NETWORK.setValue(false);
-        DHTSettings.ENABLE_PASSIVE_LEAF_MODE.setValue(true);
-        DHTSettings.PERSIST_DHT.setValue(false);
-        KademliaSettings.SHUTDOWN_MULTIPLIER.setValue(0);
-        NetworkSettings.TIMEOUT.setValue(500);
-        NetworkSettings.BOOTSTRAP_TIMEOUT.setValue(500);
-        NetworkSettings.STORE_TIMEOUT.setValue(500);
         
         FilterSettings.BLACK_LISTED_IP_ADDRESSES.setValue(
                 new String[] {"*.*.*.*"});
@@ -103,13 +94,30 @@ public class PassiveLeafForwardContactsTest extends LimeTestCase {
         assertEquals("unexpected port", PORT, 
                  ConnectionSettings.PORT.getValue());
         
-        // Start an instance of LimeWire in Ultrapeer mode
-        RouterService routerService 
-            = new RouterService(new ActivityCallbackStub());
-        routerService.start();
+        DHTSettings.DISABLE_DHT_USER.setValue(false);
+        DHTSettings.DISABLE_DHT_NETWORK.setValue(false);
+        DHTSettings.ENABLE_PASSIVE_LEAF_MODE.setValue(true);
+        DHTSettings.PERSIST_DHT.setValue(false);
+        KademliaSettings.SHUTDOWN_MULTIPLIER.setValue(0);
+        NetworkSettings.TIMEOUT.setValue(500);
+        NetworkSettings.BOOTSTRAP_TIMEOUT.setValue(500);
+        NetworkSettings.STORE_TIMEOUT.setValue(500);
         
-        RouterService.clearHostCatcher();
-        RouterService.connect();
+        if (!RouterService.isLoaded()) {
+            // Start an instance of LimeWire in Ultrapeer mode
+            RouterService routerService 
+                = new RouterService(new ActivityCallbackStub());
+            routerService.start();
+            
+            RouterService.clearHostCatcher();
+            RouterService.connect();
+            
+            // Make sure LimeWire is running as an Ultrapeer
+            assertTrue(RouterService.isStarted());
+            assertTrue(RouterService.isSupernode());
+            assertFalse(RouterService.isActiveSuperNode());
+            assertFalse(RouterService.isShieldedLeaf());
+        }
         
         // Start and bootstrap a bunch of DHT Nodes
         k = KademliaSettings.REPLICATION_PARAMETER.getValue();
@@ -134,174 +142,249 @@ public class PassiveLeafForwardContactsTest extends LimeTestCase {
             dht.close();
         }
         dhts = null;
-        
-        RouterService.disconnect();
+    }
+    
+    public static void globalTearDown() throws Exception {
         RouterService.shutdown();
     }
     
     public void testForwardContacts() throws Exception {
-        // Make sure LimeWire is running as an Ultrapeer
-        assertTrue(RouterService.isStarted());
-        assertTrue(RouterService.isSupernode());
-        assertFalse(RouterService.isActiveSuperNode());
-        assertFalse(RouterService.isShieldedLeaf());
-        
         // There should be no connections
         ConnectionManager cm = RouterService.getConnectionManager();
         assertEquals(0, cm.getNumConnections());
 
         // Connect a leaf Node to the Ultrapeer
         Connection out = createLeafConnection();
-        assertTrue(out.isOpen());
-        assertTrue(out.isOutgoing());
-        drain(out);
         
-        // There should be one connection now
-        assertEquals(1, cm.getNumConnections());
-        assertTrue(RouterService.isActiveSuperNode());
-        
-        // Check a few more things
-        ManagedConnection in = (ManagedConnection)cm.getConnections().get(0);
-        assertFalse(in.isOutgoing());
-        assertTrue(in.isSupernodeClientConnection());
-        assertEquals(-1, in.remoteHostIsPassiveLeafNode());
-        assertFalse(in.isPushProxyFor());
-        
-        // Pretend the leaf is running in PASSIVE_LEAF mode.
-        // Thanks to Singletons trivial to do... ;)
-        Method m = CapabilitiesVM.class.getDeclaredMethod("getSupportedMessages", new Class[]{});
-        m.setAccessible(true);
-        Set capabilites = (Set)m.invoke(null, new Object[0]);
-        
-        Class clazz = Class.forName("com.limegroup.gnutella.messages.vendor.CapabilitiesVM$SupportedMessageBlock");
-        Object smb = PrivilegedAccessor.invokeConstructor(clazz, 
-                new Object[]{ DHTMode.PASSIVE_LEAF.getCapabilityName(), Integer.valueOf(0) }, 
-                new Class[] { byte[].class, int.class });
-        capabilites.add(smb);
-        
-        CapabilitiesVM vm = (CapabilitiesVM)PrivilegedAccessor.invokeConstructor(
-                CapabilitiesVM.class, new Object[] { capabilites }, new Class[] { Set.class });
-        assertEquals(0, vm.isPassiveLeafNode());
-        
-        // Tell our Ultrapeer that we've PASSIVE_LEAF mode enabled
-        out.send(vm);
-        out.flush();
-        
-        // A second requirement is that the given Ultrapeer is our
-        // Push Proxy
-        out.send(new PushProxyRequest(new GUID(GUID.makeGuid())));
-        out.flush();
-        
-        Thread.sleep(250);
-        
-        // And did it work?
-        assertEquals(0, in.remoteHostIsPassiveLeafNode());
-        assertTrue(in.isPushProxyFor());
-        
-        RouterService.getDHTManager().start(DHTMode.PASSIVE);
-        Thread.sleep(250);
-        assertEquals(DHTMode.PASSIVE, RouterService.getDHTMode());
-        
-        // -------------------------------------
-        
-        // Bootstrap the Ultrapeer
-        RouterService.getDHTManager().getMojitoDHT()
-            .bootstrap(dhts.get(0).getContactAddress()).get();
-        
-        // And check what the leaf is receiving...
-        int count = 0;
-        Set<Contact> nodes = new LinkedHashSet<Contact>();
         try {
-            while(true) {
-                Message msg = out.receive(2000);
-                if (msg instanceof DHTContactsMessage) {
-                    DHTContactsMessage message = (DHTContactsMessage)msg;
-                    nodes.addAll(message.getContacts());
-                    count += message.getContacts().size();
+            assertTrue(out.isOpen());
+            assertTrue(out.isOutgoing());
+            drain(out);
+            
+            // There should be one connection now
+            assertEquals(1, cm.getNumConnections());
+            assertTrue(RouterService.isActiveSuperNode());
+            
+            // Check a few more things
+            ManagedConnection in = (ManagedConnection)cm.getConnections().get(0);
+            assertFalse(in.isOutgoing());
+            assertTrue(in.isSupernodeClientConnection());
+            assertEquals(-1, in.remoteHostIsPassiveLeafNode());
+            assertFalse(in.isPushProxyFor());
+            
+            // Pretend the leaf is running in PASSIVE_LEAF mode.
+            // Thanks to Singletons trivial to do... ;)
+            Method m = CapabilitiesVM.class.getDeclaredMethod("getSupportedMessages", new Class[]{});
+            m.setAccessible(true);
+            Set capabilites = (Set)m.invoke(null, new Object[0]);
+            
+            Class clazz = Class.forName("com.limegroup.gnutella.messages.vendor.CapabilitiesVM$SupportedMessageBlock");
+            Object smb = PrivilegedAccessor.invokeConstructor(clazz, 
+                    new Object[]{ DHTMode.PASSIVE_LEAF.getCapabilityName(), Integer.valueOf(0) }, 
+                    new Class[] { byte[].class, int.class });
+            capabilites.add(smb);
+            
+            CapabilitiesVM vm = (CapabilitiesVM)PrivilegedAccessor.invokeConstructor(
+                    CapabilitiesVM.class, new Object[] { capabilites }, new Class[] { Set.class });
+            assertEquals(0, vm.isPassiveLeafNode());
+            
+            // Tell our Ultrapeer that we've PASSIVE_LEAF mode enabled
+            out.send(vm);
+            out.flush();
+            
+            // A second requirement is that the given Ultrapeer is our
+            // Push Proxy
+            out.send(new PushProxyRequest(new GUID(GUID.makeGuid())));
+            out.flush();
+            
+            Thread.sleep(250);
+            
+            // And did it work?
+            assertNotEquals(-1, in.remoteHostIsPassiveLeafNode());
+            assertTrue(in.isPushProxyFor());
+            
+            RouterService.getDHTManager().start(DHTMode.PASSIVE);
+            Thread.sleep(250);
+            assertEquals(DHTMode.PASSIVE, RouterService.getDHTMode());
+            
+            
+            // Bootstrap the Ultrapeer
+            RouterService.getDHTManager().getMojitoDHT()
+                .bootstrap(dhts.get(0).getContactAddress()).get();
+            
+            // -------------------------------------
+            
+            // And check what the leaf is receiving...
+            int count = 0;
+            Set<Contact> nodes = new LinkedHashSet<Contact>();
+            try {
+                while(true) {
+                    Message msg = out.receive(2000);
+                    if (msg instanceof DHTContactsMessage) {
+                        DHTContactsMessage message = (DHTContactsMessage)msg;
+                        nodes.addAll(message.getContacts());
+                        count += message.getContacts().size();
+                    }
+                }
+            } catch (InterruptedIOException err) {
+                if (nodes.isEmpty()) {
+                    fail(err);
                 }
             }
-        } catch (InterruptedIOException err) {
-            if (nodes.isEmpty()) {
-                fail(err);
-            }
+            
+            assertGreaterThanOrEquals(nodes.size(), count);
+            
+            // Note: This assert can sometimes fail! It's not a bug!
+            // The reason for this lies in the lookup algorithm which
+            // terminates as soon as it can't find any closer Nodes.
+            // That means our Ultrapeer may not hit all 2*k DHT Nodes
+            // and will therefore send us less. 
+            //assertEquals(2*k, nodes.size());
+            
+            // See above
+            assertTrue("k=" + k + ", size=" + nodes.size(), 
+                    nodes.size() >= k && nodes.size() <= 2*k);
+        } finally {
+            out.close();
         }
-        
-        assertGreaterThanOrEquals(nodes.size(), count);
-        
-        // Note: This assert can sometimes fail! It's not a bug!
-        // The reason for this lies in the lookup algorithm which
-        // terminates as soon as it can't find any closer Nodes.
-        // That means our Ultrapeer may not hit all 2*k DHT Nodes
-        // and will therefore send us less. 
-        //assertEquals(2*k, nodes.size());
-        
-        // See above
-        assertTrue("k=" + k + ", size=" + nodes.size(), 
-                nodes.size() >= k && nodes.size() <= 2*k);
     }
     
-    /*public void testDoNotSendCapabilitiesVM() throws Exception {
-        // Make sure LimeWire is running as an Ultrapeer
-        assertTrue(RouterService.isStarted());
-        assertTrue(RouterService.isSupernode());
-        assertFalse(RouterService.isActiveSuperNode());
-        assertFalse(RouterService.isShieldedLeaf());
-        
+    public void testDoNotSendCapabilitiesVM() throws Exception {
         // There should be no connections
         ConnectionManager cm = RouterService.getConnectionManager();
         assertEquals(0, cm.getNumConnections());
 
         // Connect a leaf Node to the Ultrapeer
         Connection out = createLeafConnection();
-        assertTrue(out.isOpen());
-        assertTrue(out.isOutgoing());
-        drain(out);
-        
-        // There should be one connection now
-        assertEquals(1, cm.getNumConnections());
-        assertTrue(RouterService.isActiveSuperNode());
-        
-        // Check a few more things
-        ManagedConnection in = (ManagedConnection)cm.getConnections().get(0);
-        assertFalse(in.isOutgoing());
-        assertTrue(in.isSupernodeClientConnection());
-        assertEquals(-1, in.remoteHostIsPassiveLeafNode());
-        assertFalse(in.isPushProxyFor());
-        
-        // --- WE DO NOT PRETEND TO BE IN PASSIVE_LEAF MODE !!! ---
-        
-        // A second requirement is that the given Ultrapeer is our
-        // Push Proxy
-        out.send(new PushProxyRequest(new GUID(GUID.makeGuid())));
-        out.flush();
-        
-        Thread.sleep(2000);
-        
-        // And did it work?
-        assertEquals(-1, in.remoteHostIsPassiveLeafNode());
-        assertTrue(in.isPushProxyFor());
-        
-        RouterService.getDHTManager().start(DHTMode.PASSIVE);
-        Thread.sleep(250);
-        assertEquals(DHTMode.PASSIVE, RouterService.getDHTMode());
-        
-        // -------------------------------------
-        
-        // Bootstrap the Ultrapeer
-        RouterService.getDHTManager().getMojitoDHT()
-            .bootstrap(dhts.get(0).getContactAddress()).get();
-        
-        // And check what the leaf is receiving
         try {
-            while(true) {
-                Message msg = out.receive(2000);
-                if (msg instanceof DHTContactsMessage) {
-                    fail("Should not have received a DHTContactsMessage: " + msg);
+            assertTrue(out.isOpen());
+            assertTrue(out.isOutgoing());
+            drain(out);
+            
+            // There should be one connection now
+            assertEquals(1, cm.getNumConnections());
+            assertTrue(RouterService.isActiveSuperNode());
+            
+            // Check a few more things
+            ManagedConnection in = (ManagedConnection)cm.getConnections().get(0);
+            assertFalse(in.isOutgoing());
+            assertTrue(in.isSupernodeClientConnection());
+            assertEquals(-1, in.remoteHostIsPassiveLeafNode());
+            assertFalse(in.isPushProxyFor());
+            
+            // --- WE DO NOT PRETEND TO BE IN PASSIVE_LEAF MODE !!! ---
+            
+            // A second requirement is that the given Ultrapeer is our
+            // Push Proxy
+            out.send(new PushProxyRequest(new GUID(GUID.makeGuid())));
+            out.flush();
+            
+            Thread.sleep(2000);
+            
+            // And did it work?
+            assertEquals(-1, in.remoteHostIsPassiveLeafNode());
+            assertTrue(in.isPushProxyFor());
+            
+            RouterService.getDHTManager().start(DHTMode.PASSIVE);
+            Thread.sleep(250);
+            assertEquals(DHTMode.PASSIVE, RouterService.getDHTMode());
+            
+            // Bootstrap the Ultrapeer
+            RouterService.getDHTManager().getMojitoDHT()
+                .bootstrap(dhts.get(0).getContactAddress()).get();
+            
+            // -------------------------------------
+            
+            // And check what the leaf is receiving
+            try {
+                while(true) {
+                    Message msg = out.receive(2000);
+                    if (msg instanceof DHTContactsMessage) {
+                        fail("Should not have received a DHTContactsMessage: " + msg);
+                    }
                 }
+            } catch (InterruptedIOException expected) {
             }
-        } catch (InterruptedIOException expected) {
+        } finally {
+            out.close();
         }
-    }*/
+    }
+    
+    public void testDoNotSendPushProxyRequest() throws Exception {
+        // There should be no connections
+        ConnectionManager cm = RouterService.getConnectionManager();
+        assertEquals(0, cm.getNumConnections());
+
+        // Connect a leaf Node to the Ultrapeer
+        Connection out = createLeafConnection();
+        try {
+            assertTrue(out.isOpen());
+            assertTrue(out.isOutgoing());
+            drain(out);
+            
+            // There should be one connection now
+            assertEquals(1, cm.getNumConnections());
+            assertTrue(RouterService.isActiveSuperNode());
+            
+            // Check a few more things
+            ManagedConnection in = (ManagedConnection)cm.getConnections().get(0);
+            assertFalse(in.isOutgoing());
+            assertTrue(in.isSupernodeClientConnection());
+            assertEquals(-1, in.remoteHostIsPassiveLeafNode());
+            assertFalse(in.isPushProxyFor());
+            
+            // Pretend the leaf is running in PASSIVE_LEAF mode.
+            // Thanks to Singletons trivial to do... ;)
+            Method m = CapabilitiesVM.class.getDeclaredMethod("getSupportedMessages", new Class[]{});
+            m.setAccessible(true);
+            Set capabilites = (Set)m.invoke(null, new Object[0]);
+            
+            Class clazz = Class.forName("com.limegroup.gnutella.messages.vendor.CapabilitiesVM$SupportedMessageBlock");
+            Object smb = PrivilegedAccessor.invokeConstructor(clazz, 
+                    new Object[]{ DHTMode.PASSIVE_LEAF.getCapabilityName(), Integer.valueOf(0) }, 
+                    new Class[] { byte[].class, int.class });
+            capabilites.add(smb);
+            
+            CapabilitiesVM vm = (CapabilitiesVM)PrivilegedAccessor.invokeConstructor(
+                    CapabilitiesVM.class, new Object[] { capabilites }, new Class[] { Set.class });
+            assertEquals(0, vm.isPassiveLeafNode());
+            
+            // Tell our Ultrapeer that we've PASSIVE_LEAF mode enabled
+            out.send(vm);
+            out.flush();
+            
+            // --- But don't tell the Ultrapeer its our Push Proxy! ---
+            
+            Thread.sleep(2000);
+            
+            // And did it work?
+            assertNotEquals(-1, in.remoteHostIsPassiveLeafNode());
+            assertFalse(in.isPushProxyFor());
+            
+            RouterService.getDHTManager().start(DHTMode.PASSIVE);
+            Thread.sleep(250);
+            assertEquals(DHTMode.PASSIVE, RouterService.getDHTMode());
+            
+            // Bootstrap the Ultrapeer
+            RouterService.getDHTManager().getMojitoDHT()
+                .bootstrap(dhts.get(0).getContactAddress()).get();
+            
+            // -------------------------------------
+            
+            // And check what the leaf is receiving
+            try {
+                while(true) {
+                    Message msg = out.receive(2000);
+                    if (msg instanceof DHTContactsMessage) {
+                        fail("Should not have received a DHTContactsMessage: " + msg);
+                    }
+                }
+            } catch (InterruptedIOException expected) {
+            }
+        } finally {
+            out.close();
+        }
+    }
     
     protected Connection createLeafConnection() throws Exception {
         return createConnection(new LeafHeaders("localhost"));
