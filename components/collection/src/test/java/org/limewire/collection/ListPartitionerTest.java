34272610ae24c4c4ed4d8edc147103082aa7e5d1
zlatinb <zlatinb>
2007-04-12 19:24:49 +0000

    more tests for partition growing and for first/lastPartition

--- components/collection/src/test/java/org/limewire/collection/ListPartitionerTest.java
+++ components/collection/src/test/java/org/limewire/collection/ListPartitionerTest.java
@@ -33,12 +33,47 @@ public class ListPartitionerTest extends BaseTestCase {
         List list = new ArrayList();
         ListPartitioner partitioner = new ListPartitioner(list, 5);
         
-        // add one element - the first partition should have 1 element, 
+        // add one element - the first and last partition should
+        // be the same and have 1 element
+        // (the objects themselves are not the same since a new sublist is created every time)
+        Object o = new Object();
+        list.add(o);
+        List zero = partitioner.getPartition(0);
+        List first = partitioner.getFirstPartition();
+        List last = partitioner.getLastPartition();
+        assertEquals(1, zero.size());
+        assertEquals(1, first.size());
+        assertEquals(1, last.size());
+        assertSame(o, zero.get(0));
+        assertSame(o, first.get(0));
+        assertSame(o, last.get(0));
+        
         // the rest empty
-        list.add(new Object());
-        assertEquals(1, partitioner.getPartition(0).size());
         for (int i = 1; i < 5; i++)
             assertTrue(partitioner.getPartition(i).isEmpty());
+        
+        // add a second object, an new partition should appear
+        Object o2 = new Object();
+        list.add(o2);
+        zero = partitioner.getPartition(0);
+        first = partitioner.getFirstPartition();
+        last = partitioner.getLastPartition();
+        List one = partitioner.getPartition(1);
+        assertEquals(1, zero.size());
+        assertEquals(1, one.size());
+        assertEquals(1, first.size());
+        assertEquals(1, last.size());
+        
+        assertSame(o, zero.get(0));
+        assertSame(o, first.get(0));
+        
+        assertSame(o2, one.get(0));
+        assertSame(o2, last.get(0));
+        
+        // the rest empty
+        for (int i = 2; i < 5; i++)
+            assertTrue(partitioner.getPartition(i).isEmpty());
+        
     }
     
     public void testPartitionsResize() throws Exception {
