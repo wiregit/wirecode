2d8fc0be2108159ecb73a866236834d3346a75c9
rkapsi <rkapsi>
2007-04-25 17:59:10 +0000

    PassiveLeafForwardContactsTest was failing for the same reason (Class C filtering was on).

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/dht/PassiveLeafForwardContactsTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/dht/PassiveLeafForwardContactsTest.java
@@ -13,6 +13,7 @@ import junit.framework.Test;
 import org.limewire.mojito.MojitoDHT;
 import org.limewire.mojito.MojitoFactory;
 import org.limewire.mojito.routing.Contact;
+import org.limewire.mojito.settings.ContextSettings;
 import org.limewire.mojito.settings.KademliaSettings;
 import org.limewire.mojito.settings.NetworkSettings;
 import org.limewire.util.PrivilegedAccessor;
@@ -102,9 +103,19 @@ public class PassiveLeafForwardContactsTest extends LimeTestCase {
         DHTSettings.PERSIST_DHT_DATABASE.setValue(false);
         
         KademliaSettings.SHUTDOWN_MESSAGES_MULTIPLIER.setValue(0);
+        
+        NetworkSettings.FILTER_CLASS_C.setValue(false);
+        NetworkSettings.LOCAL_IS_PRIVATE.setValue(false);
+        
+        // We're working on the loopback. Everything should be done
+        // in less than 500ms
         NetworkSettings.DEFAULT_TIMEOUT.setValue(500);
-        NetworkSettings.BOOTSTRAP_TIMEOUT.setValue(500);
-        NetworkSettings.STORE_TIMEOUT.setValue(500);
+        NetworkSettings.BOOTSTRAP_TIMEOUT.setValue(1000);
+        NetworkSettings.STORE_TIMEOUT.setValue(1000);
+        
+        // Nothing should take longer than 1.5 seconds. If we start seeing
+        // LockTimeoutExceptions on the loopback then check this Setting!
+        ContextSettings.WAIT_ON_LOCK.setValue(1500);
         
         if (!RouterService.isLoaded()) {
             // Start an instance of LimeWire in Ultrapeer mode
