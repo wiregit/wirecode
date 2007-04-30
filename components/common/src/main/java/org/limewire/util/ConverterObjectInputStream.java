129a617ba8531ff94ecfc1688fb0528f4336d6f0
fberger <fberger>
2007-04-30 21:30:31 +0000

    Fixed LCC-1. Added test case for default package mapping.

--- components/common/src/main/java/org/limewire/util/ConverterObjectInputStream.java
+++ components/common/src/main/java/org/limewire/util/ConverterObjectInputStream.java
@@ -28,7 +28,7 @@ import java.util.Map;
 // TODO: Add support for adding arbitrary migrations.
 public class ConverterObjectInputStream extends ObjectInputStream { 
     
-    private Map<String, String> lookups = new HashMap<String, String>(3);
+    private Map<String, String> lookups = new HashMap<String, String>();
 
     /**
      * Constructs a new ConverterObjectInputStream wrapping the specified
@@ -45,29 +45,54 @@ public class ConverterObjectInputStream extends ObjectInputStream {
         lookups.put("com.limegroup.gnutella.util.IntervalSet", "org.limewire.collection.IntervalSet");
         lookups.put("com.limegroup.gnutella.util.Comparators$CaseInsensitiveStringComparator", "org.limewire.collection.Comparators$CaseInsensitiveStringComparator");
         lookups.put("com.limegroup.gnutella.util.StringComparator", "org.limewire.collection.StringComparator");
-                
+        lookups.put("com.sun.java.util.collections", "java.util");
+    }
+
+    /**
+     * Adds a mapping between an old package or class name to a new name.
+     * @param oldName the name of the old package or class
+     * @param newName the name of the new package or class
+     */
+    public void addLookup(String oldName, String newName) {
+        lookups.put(oldName, newName);
     }
      
     /** 
      * Overriden to manually alter the class descriptors. 
+     * Note that this does NOT require the original class to be loadable.
      * 
-     * Note that this does NOT require the original class to be loadable. 
+     * Lookup works as follows:
+     * <ul>
+     * <li>The serialized (old) class name is looked up, if a corresponding new
+     * class name exists the ObjectStreamClass object for it is returned.</li>
+     * <li>The package name of the serialized class name is extracted and
+     * looked up if a new package name exists, it is prepended to the name of
+     * the class the corresponding class is loaded.</li>
+     * <li>Otherwise the original ObjectStreamClass is returned.</li> 
+     * <ul>
      */ 
     protected ObjectStreamClass readClassDescriptor() throws 
       IOException, ClassNotFoundException { 
         ObjectStreamClass read = super.readClassDescriptor(); 
         String className = read.getName(); 
         
-        if(className.startsWith("com.sun.java.util.collections")) {
-            // 29 == length of com.sun.java.util.collections 
-            className = "java.util" + className.substring(29); 
-            return ObjectStreamClass.lookup(Class.forName(className));
-        } else {
-            String alter = lookups.get(className);
-            if(alter != null)
-                return ObjectStreamClass.lookup(Class.forName(alter));
-            else
-                return read;
+        String newName = lookups.get(className);
+        if (newName != null) {
+            return ObjectStreamClass.lookup(Class.forName(newName));
         }
+        else {
+            int index = className.lastIndexOf('.');
+            // use "" as lookup key for default package
+            String oldPackage = index != -1 ? className.substring(0, index) : "";
+            String newPackage = lookups.get(oldPackage);
+            if (newPackage != null) {
+                if (newPackage.length() == 0) {
+                    // mapped to default package
+                    ObjectStreamClass.lookup(Class.forName(className.substring(index + 1)));
+                }
+                return ObjectStreamClass.lookup(Class.forName(newPackage + '.' + className.substring(index + 1)));
+            }
+        }
+        return read;
     } 
 }
--- components/common/src/test/java/org/limewire/util/ConverterObjectInputStreamTest.java
+++ components/common/src/test/java/org/limewire/util/ConverterObjectInputStreamTest.java
@@ -58,6 +58,9 @@ public class ConverterObjectInputStreamTest extends BaseTestCase {
     }
     
     public void testOldCollectionClasses() throws Exception {
+        // import com.sun.java.util.collections.*;
+        // ArrayList list = new ArrayList();
+        // list.add(new HashMap());
         byte[] hashMapInsideOfArrayList = new byte[] { -84, -19, 0, 5, 115, 114, 0, 39, 99, 111, 109, 46, 115, 117, 110, 46, 106, 97, 118, 97, 46, 117, 116, 105, 108, 46, 99, 111, 108, 108, 101, 99, 116, 105, 111, 110, 115, 46, 65, 114, 114, 97, 121, 76, 105, 115, 116, 77, -94, -80, -33, 64, 10, 78, -108, 3, 0, 1, 73, 0, 4, 115, 105, 122, 101, 120, 112, 0, 0, 0, 1, 119, 4, 0, 0, 0, 10, 115, 114, 0, 37, 99, 111, 109, 46, 115, 117, 110, 46, 106, 97, 118, 97, 46, 117, 116, 105, 108, 46, 99, 111, 108, 108, 101, 99, 116, 105, 111, 110, 115, 46, 72, 97, 115, 104, 77, 97, 112, 80, 0, 33, 58, -95, -15, 79, 40, 3, 0, 2, 70, 0, 10, 108, 111, 97, 100, 70, 97, 99, 116, 111, 114, 73, 0, 9, 116, 104, 114, 101, 115, 104, 111, 108, 100, 120, 112, 63, 64, 0, 0, 0, 0, 0, 75, 119, 8, 0, 0, 0, 101, 0, 0, 0, 0, 120, 120 };
         
         ConverterObjectInputStream in = getSerializedInputStream(hashMapInsideOfArrayList);
@@ -70,11 +73,23 @@ public class ConverterObjectInputStreamTest extends BaseTestCase {
         assertTrue(instance instanceof HashMap);
     }
     
-    private static class SerializableClass implements Serializable {
+    public void testDefaultPackageClassMappedToThisPackage() throws Exception {
+        // serializable static inner class
+        // new ConverterObjectInputStreamTest.SerializableClass();
+        byte[] serializbleClassInDefaultPackage = new byte[] { -84, -19, 0, 5, 115, 114, 0, 48, 67, 111, 110, 118, 101, 114, 116, 101, 114, 79, 98, 106, 101, 99, 116, 73, 110, 112, 117, 116, 83, 116, 114, 101, 97, 109, 84, 101, 115, 116, 36, 83, 101, 114, 105, 97, 108, 105, 122, 97, 98, 108, 101, 67, 108, 97, 115, 115, -6, 49, 117, -36, -108, -33, -69, 70, 2, 0, 0, 120, 112 };
+        
+        ConverterObjectInputStream in = getSerializedInputStream(serializbleClassInDefaultPackage);
+        in.addLookup("", this.getClass().getName().substring(0, this.getClass().getName().lastIndexOf('.')));
+        
+        Object instance = in.readObject();
+        assertTrue(instance instanceof SerializableClass);
+    }
+    
+    static class SerializableClass implements Serializable {
         
     }
     
-    private static class NewSerializableClass implements Serializable {
+    static class NewSerializableClass implements Serializable {
         
     }
     
