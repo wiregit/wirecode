9db39eb03fd2ff79bfaa587a8a3e2634925184a0
spingel <spingel>
2007-04-30 22:59:04 +0000

    CORE-153: Use NIO for browse: javadoc

--- components/http/src/main/java/org/limewire/http/AbstractHttpNIOEntity.java
+++ components/http/src/main/java/org/limewire/http/AbstractHttpNIOEntity.java
@@ -12,28 +12,59 @@ import org.apache.http.nio.IOControl;
 import org.limewire.nio.channel.InterestWritableByteChannel;
 import org.limewire.nio.observer.WriteObserver;
 
-public abstract class AbstractHttpNIOEntity extends AbstractHttpEntity implements HttpNIOEntity, InterestWritableByteChannel {
+/**
+ * Provides a default implementation of an event based HTTP entity that
+ * implements {@link InterestWritableByteChannel}. Inherited methods from
+ * {@HttpEntity} for streaming throw {@link UnsupportedOperationException}.
+ * <p>
+ * NOTE: This class only supports writing.
+ */
+public abstract class AbstractHttpNIOEntity extends AbstractHttpEntity
+        implements HttpNIOEntity, InterestWritableByteChannel {
 
     private ContentEncoder encoder;
+
     private IOControl ioctrl;
 
+    /**
+     * Throws <code>UnsupportedOperationException</code>.
+     */
     public InputStream getContent() throws IOException, IllegalStateException {
         throw new UnsupportedOperationException();
     }
 
-    public long getContentLength() {
-        return -1;
-    }
-
+    /**
+     * Throws <code>UnsupportedOperationException</code>.
+     */
     public boolean isRepeatable() {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Throws <code>UnsupportedOperationException</code>.
+     */
     public boolean isStreaming() {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Throws <code>UnsupportedOperationException</code>.
+     */
     public void writeTo(OutputStream outstream) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Throws <code>UnsupportedOperationException</code>.
+     */
+    public void close() throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Throws <code>UnsupportedOperationException</code>.
+     */
+    public boolean isOpen() {
         throw new UnsupportedOperationException();
     }
 
@@ -48,11 +79,13 @@ public abstract class AbstractHttpNIOEntity extends AbstractHttpEntity implement
         }
     }
 
-    public int consumeContent(ContentDecoder decoder, IOControl ioctrl) throws IOException {
+    public int consumeContent(ContentDecoder decoder, IOControl ioctrl)
+            throws IOException {
         throw new RuntimeException("Not supported");
     }
-    
-    public void produceContent(ContentEncoder encoder, IOControl ioctrl) throws IOException {
+
+    public void produceContent(ContentEncoder encoder, IOControl ioctrl)
+            throws IOException {
         if (this.encoder == null) {
             this.encoder = encoder;
             this.ioctrl = ioctrl;
@@ -62,31 +95,53 @@ public abstract class AbstractHttpNIOEntity extends AbstractHttpEntity implement
             encoder.complete();
         }
     }
-    
+
+    /**
+     * Writes data from <code>src</code> to encoder.
+     * 
+     * @return number of bytes written
+     */
     public int write(ByteBuffer src) throws IOException {
         return encoder.write(src);
     }
 
-    public void close() throws IOException {
-        throw new UnsupportedOperationException();
-    }
-
-    public boolean isOpen() {
-        throw new UnsupportedOperationException();
-    }
-
+    /**
+     * Invoked before the first call to {@link #handleWrite()}.
+     * 
+     * @throws IOException indicates an I/O error which will abort the
+     *         connection
+     */
     public abstract void initialize() throws IOException;
-    
+
+    /**
+     * Sub-classes need to implement this and invoke {@link #write(ByteBuffer)}
+     * to transmit data.
+     * 
+     * @throws IOException indicates an I/O error which will abort the
+     *         connection
+     * @return true, if more data is expected; false, if the transfer is complete  
+     */
     public abstract boolean handleWrite() throws IOException;
 
-    //public abstract boolean handleRead() throws IOException;
+    // public abstract boolean handleRead() throws IOException;
 
+    /**
+     * Invoked after transfer has completed. This is true if either
+     * {@link #handleWrite()} returns false or {@link #handleWrite()} or
+     * {@link #initialize()} thrown an exception.
+     */
     public abstract void finished();
 
+    /**
+     * Never invoked, throws <code>UnsupportedOperationException</code>.
+     */
     public void handleIOException(IOException iox) {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Never invoked, throws <code>UnsupportedOperationException</code>.
+     */
     public void shutdown() {
         throw new UnsupportedOperationException();
     }
