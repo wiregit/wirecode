48a5adbfdec8f29b29fa539468ccf49046eeeca7
spingel <spingel>
2007-05-11 13:51:35 +0000

    CORE-153: Use NIO for browse: replaces Thread.sleep() by wait statements

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/AllUploadTests.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/AllUploadTests.java
@@ -3,13 +3,19 @@ package com.limegroup.gnutella;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import com.limegroup.gnutella.uploader.FilePieceReaderTest;
+import com.limegroup.gnutella.uploader.UploadSlotManagerTest;
+
 public class AllUploadTests {
 
     public static Test suite() {
-        TestSuite suite = new TestSuite("Test for com.limegroup.gnutella");
+        TestSuite suite = new TestSuite();
         //$JUnit-BEGIN$
         suite.addTest(HTTPUploadManagerTest.suite());
+        suite.addTest(UploadSlotManagerTest.suite());
         suite.addTest(UrnHttpRequestTest.suite());
+        suite.addTest(HTTPAcceptorTest.suite());
+        suite.addTest(FilePieceReaderTest.suite());
         //suite.addTest(UploadTest.suite());
         suite.addTest(UploaderTest.suite());
         //$JUnit-END$
--- components/gnutella-core/src/test/java/com/limegroup/gnutella/gui/chat/ChatUIManagerTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/gui/chat/ChatUIManagerTest.java
@@ -5,6 +5,7 @@ import java.awt.event.WindowEvent;
 import junit.framework.Test;
 
 import com.limegroup.gnutella.Acceptor;
+import com.limegroup.gnutella.LimeTestUtils;
 import com.limegroup.gnutella.RouterService;
 import com.limegroup.gnutella.chat.ChatManager;
 import com.limegroup.gnutella.chat.InstantMessenger;
@@ -16,9 +17,13 @@ import com.limegroup.gnutella.settings.ConnectionSettings;
 public class ChatUIManagerTest extends GUIBaseTestCase {
 
     private static final int CHAT_PORT = 9999;
+
     private static Acceptor acceptThread;
+
     private ChatUIManager chatManager;
+
     private ChatFrame outgoing;
+
     private ChatFrame incoming;
 
     public ChatUIManagerTest(String name) {
@@ -33,51 +38,49 @@ public class ChatUIManagerTest extends GUIBaseTestCase {
         junit.textui.TestRunner.run(suite());
     }
 
-
     public static void globalSetUp() throws Exception {
         doSettings();
-        
+
         GUITestUtils.initializeUI();
         ChatManager.instance().initialize();
-        
+
         // start it up!
         acceptThread = new Acceptor();
         acceptThread.start();
         acceptThread.setListeningPort(CHAT_PORT);
-        
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException ex) {}
     }
 
     public static void globealTearDown() throws Exception {
         acceptThread.setListeningPort(0);
     }
-    
+
     private static void doSettings() {
         ConnectionSettings.LOCAL_IS_PRIVATE.setValue(false);
     }
-    
+
     @Override
     protected void setUp() throws Exception {
         super.setUp();
 
         doSettings();
-        
+
         chatManager = ChatUIManager.instance();
     }
 
-    private void setupChat() throws InterruptedException {
+    private void setupChat() throws Exception {
         // the outgoing chat frame can not be registered with ChatUIManager
         // otherwise the connection will be closed since there can only be
         // one chat frame per host
-        InstantMessenger chat = new InstantMessenger("localhost", CHAT_PORT, ChatManager.instance(), RouterService.getCallback());
+        InstantMessenger chat = new InstantMessenger("localhost", CHAT_PORT,
+                ChatManager.instance(), RouterService.getCallback());
         outgoing = new ChatFrame(chat);
         chat.start();
-        Thread.sleep(4000);
+        chat.waitForConnect(4000);
+        LimeTestUtils.waitForNIO();
+        GUITestUtils.waitForSwing();
         incoming = getIncomingChat();
     }
-    
+
     @Override
     protected void tearDown() throws Exception {
         super.tearDown();
@@ -87,17 +90,19 @@ public class ChatUIManagerTest extends GUIBaseTestCase {
             outgoing.dispose();
             outgoing = null;
         }
-        
+
         chatManager.clear();
     }
 
     // tests that chats to localhost are closed right away
     public void testChatLocalhost() throws Exception {
-        //String localAddress = NetworkUtils.ip2string(acceptThread.getAddress(false));
         outgoing = GUIMediator.createChat("127.0.0.1", CHAT_PORT);
-        ((InstantMessenger)outgoing.getChat()).waitForConnect(1000);
+        ((InstantMessenger) outgoing.getChat()).waitForConnect(4000);
+        LimeTestUtils.waitForNIO();
         // wait for SwingUtilities.invokeLater() to create chat frame
         GUITestUtils.waitForSwing();
+        // wait for socket disconnect
+        LimeTestUtils.waitForNIO();
         assertTrue(!outgoing.getChat().isConnected());
     }
 
@@ -113,15 +118,18 @@ public class ChatUIManagerTest extends GUIBaseTestCase {
         assertEquals("", incoming.getMessage());
         outgoing.setMessage("bar");
         outgoing.send();
-        Thread.sleep(1000);
+        LimeTestUtils.waitForNIO();
+        GUITestUtils.waitForSwing();
         assertTrue(incoming.getText().indexOf("bar") > 0);
         assertTrue(outgoing.getText().indexOf("bar") > 0);
     }
 
     public void testCloseIncoming() throws Exception {
         setupChat();
-        incoming.dispatchEvent(new WindowEvent(incoming, WindowEvent.WINDOW_CLOSING));
-        Thread.sleep(1000);
+        incoming.dispatchEvent(new WindowEvent(incoming,
+                WindowEvent.WINDOW_CLOSING));
+        GUITestUtils.waitForSwing();
+        LimeTestUtils.waitForNIO();
         assertTrue(!outgoing.getChat().isConnected());
         assertTrue(!incoming.getChat().isConnected());
     }
@@ -129,10 +137,12 @@ public class ChatUIManagerTest extends GUIBaseTestCase {
     public void testCloseOutgoing() throws Exception {
         setupChat();
         outgoing.getChat().stop();
-        Thread.sleep(1000);
+        LimeTestUtils.waitForNIO();
         assertTrue(!outgoing.getChat().isConnected());
-        assertTrue(!incoming.getChat().isConnected());       
-        assertTrue(incoming.getText().indexOf(GUIMediator.getStringResource("CHAT_HOST_UNAVAILABLE")) != -1);
+        assertTrue(!incoming.getChat().isConnected());
+        GUITestUtils.waitForSwing();
+        assertTrue(incoming.getText().indexOf(
+                GUIMediator.getStringResource("CHAT_HOST_UNAVAILABLE")) != -1);
     }
 
     private ChatFrame getIncomingChat() {
@@ -144,5 +154,5 @@ public class ChatUIManagerTest extends GUIBaseTestCase {
         fail("Incoming chat not found");
         return null;
     }
-    
+
 }
