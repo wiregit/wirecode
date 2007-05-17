4f97e11a921c629489f3fb524a019eda12b57ba2
rkapsi <rkapsi>
2007-05-17 18:31:06 +0000

    Comments

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/settings/PluginSettings.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/settings/PluginSettings.java
@@ -1,24 +1,20 @@
 package com.limegroup.gnutella.settings;
 
-import org.limewire.setting.BooleanSetting;
 import org.limewire.setting.StringSetting;
 
 /**
- * 
+ * Misc Settings for the PluginManager
  */
 public class PluginSettings extends LimeProps {
     
     private PluginSettings() {}
     
     /**
-     * 
+     * A custom path which either points to a custom plugins directory
+     * or a specific plugin. The latter can be a JAR or an unpacket
+     * plugin (i.e. a directory)
      */
-    public static final BooleanSetting CACHE_PROFILE_DIR
-        = FACTORY.createBooleanSetting("CACHE_PROFILE_DIR", true);
-    
-    /**
-     * 
-     */
-    public static final StringSetting CUSTOM_PLUGINS_DIR
-        = FACTORY.createStringSetting("CUSTOM_PLUGINS_DIR", "DEFAULT_CUSTOM_PLUGINS_DIR_VALUE");
+    public static final StringSetting CUSTOM_PLUGINS_PATH
+        = FACTORY.createStringSetting("CUSTOM_PLUGINS_PATH", 
+                "DEFAULT_CUSTOM_PLUGINS_PATH_VALUE");
 }
