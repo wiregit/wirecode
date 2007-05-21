12c25dc748447d6c916e7a9929c0811f579aaafe
rkapsi <rkapsi>
2007-05-21 18:22:37 +0000

    Renaming LimePluginManager to PluginLoader and adding comments

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/plugin/PluginLoader.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/plugin/PluginLoader.java
@@ -13,7 +13,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.java.plugin.JpfException;
 import org.java.plugin.ObjectFactory;
-import org.java.plugin.PluginLifecycleException;
 import org.java.plugin.PluginManager;
 import org.java.plugin.PluginManager.PluginLocation;
 import org.java.plugin.registry.PluginDescriptor;
@@ -24,66 +23,99 @@ import org.limewire.service.ErrorService;
 import org.limewire.util.CommonUtils;
 
 import com.limegroup.gnutella.settings.PluginSettings;
+import com.limegroup.gnutella.util.LimeWireUtils;
 
-public class LimePluginManager {
+/**
+ * Provides a simple interface to load LimeWire Plugins. For advanced
+ * Plugin operations you may use the actual PluginManager.
+ * 
+ * The lookup order of the Plugins is as follows:
+ * 
+ * ../lib/plugins
+ * ./plugins
+ * ~/plugins
+ * [path to settings]/plugins
+ * [custom path]
+ * 
+ * Plugins that have the same ID are loaded in first come, first 
+ * served basis.
+ */
+public class PluginLoader {
     
-    private static final Log LOG = LogFactory.getLog(LimePluginManager.class);
+    private static final Log LOG = LogFactory.getLog(PluginLoader.class);
     
-    private static final String PLUGINS = "plugins";
+    /**
+     * Name of the Plugins directory
+     */
+    private static final String PLUGINS_DIR = "plugins";
     
-    private static final AtomicLazyReference<LimePluginManager> PLUGIN_MANAGER_REFERENCE
-        = new AtomicLazyReference<LimePluginManager>() {
+    /**
+     * An AtomicLazyReference of the PluginLoader
+     */
+    private static final AtomicLazyReference<PluginLoader> PLUGIN_LOADER_REFERENCE
+        = new AtomicLazyReference<PluginLoader>() {
             @Override
-            protected LimePluginManager createObject() {
-                return new LimePluginManager();
+            protected PluginLoader createObject() {
+                return new PluginLoader();
             }
         };
     
-    public static LimePluginManager instance() {
-        return PLUGIN_MANAGER_REFERENCE.get();
+    /**
+     * Returns the singleton instance of the PluginLoader
+     */
+    public static PluginLoader instance() {
+        return PLUGIN_LOADER_REFERENCE.get();
     }
     
+    /**
+     * A handle of the PluginManager
+     */
     private final PluginManager manager;
     
+    /**
+     * Flag for whether or not the PluginLoader is running
+     */
     private boolean running = false;
     
-    private LimePluginManager() {
-        //File shadow = new File("shadow");
-        //System.setProperty("org.java.plugin.standard.ShadingPathResolver.shadowFolder", shadow.getAbsolutePath());
-        //System.setProperty("org.java.plugin.standard.ShadingPathResolver.unpackMode", "always");
-        
+    private PluginLoader() {
         manager = ObjectFactory.newInstance().createManager();
     }
     
+    /**
+     * Returns the PluginManager
+     */
     public PluginManager getPluginManager() {
         return manager;
     }
     
+    /**
+     * Returns true if the PluginLoader is running
+     */
     public synchronized boolean isRunning() {
         return running;
     }
     
     /**
-     * Starts the Plugin Manager
+     * Starts the PluginLoader and publishes all Plugins
+     * but doesn't start them.
      */
-    public synchronized void start() {
+    public synchronized void launch() {
         if (!isRunning()) {
             try {
                 PluginLocation[] locations = getPluginLocations();
                 manager.publishPlugins(locations);
-                startAll();
                 running = true;
             } catch (JpfException err) {
                 LOG.error("JpfException", err);
-                err.printStackTrace();
+                ErrorService.error(err);
             }
         }
     }
     
     /**
-     * Stops the Plugin Manager
+     * Stops the PluginLoader and all Plugins
      */
-    public synchronized void stop() {
+    public synchronized void shutdown() {
         if (isRunning()) {
             try {
                 stopAll();
@@ -94,6 +126,9 @@ public class LimePluginManager {
         }
     }
     
+    /**
+     * Starts all Plugins
+     */
     public synchronized void startAll() {
         PluginRegistry registry = manager.getRegistry();
         Collection<PluginDescriptor> descriptors = registry.getPluginDescriptors();
@@ -102,12 +137,16 @@ public class LimePluginManager {
                 try {
                     manager.activatePlugin(descriptor.getId());
                 } catch (Throwable err) {
+                    LOG.error("Throwable", err);
                     ErrorService.error(err);
                 }
             }
         }
     }
 
+    /**
+     * Stops all Plugins
+     */
     public synchronized void stopAll() {
         PluginRegistry registry = manager.getRegistry();
         Collection<PluginDescriptor> descriptors = registry.getPluginDescriptors();
@@ -118,24 +157,29 @@ public class LimePluginManager {
         }
     }
     
+    /**
+     * Returns all available Plugins that are found in the various paths
+     */
     private static PluginLocation[] getPluginLocations() {
         List<PluginLocation> locations = new ArrayList<PluginLocation>();
         
+        // "../lib/plugins" -- DEVELOPMENT ONLY
+        if (LimeWireUtils.isCVS()) {
+            File libPluginsDir = new File("../lib/", PLUGINS_DIR);
+            locations.addAll(Arrays.asList(getPluginLocations(libPluginsDir)));
+        }
+        
         // "./plugins"
-        File mainPluginsDir = new File(PLUGINS);
+        File mainPluginsDir = new File(PLUGINS_DIR);
         locations.addAll(Arrays.asList(getPluginLocations(mainPluginsDir)));
         
         // "~/plugins"
-        File userPluginsDir = new File(CommonUtils.getUserHomeDir(), PLUGINS);
+        File userPluginsDir = new File(CommonUtils.getUserHomeDir(), PLUGINS_DIR);
         locations.addAll(Arrays.asList(getPluginLocations(userPluginsDir)));
         
         // "<settings>/plugins"
-        File settingsPluginsDir = new File(CommonUtils.getUserSettingsDir(), PLUGINS);
+        File settingsPluginsDir = new File(CommonUtils.getUserSettingsDir(), PLUGINS_DIR);
         locations.addAll(Arrays.asList(getPluginLocations(settingsPluginsDir)));
-        
-        // "../lib/plugins"
-        File libPluginsDir = new File("../lib/", PLUGINS);
-        locations.addAll(Arrays.asList(getPluginLocations(libPluginsDir)));
         
         // "<custom path>"
         if (!PluginSettings.CUSTOM_PLUGINS_PATH.isDefault()) {
@@ -146,13 +190,16 @@ public class LimePluginManager {
         return (PluginLocation[])locations.toArray(new PluginLocation[0]);
     }
     
+    /**
+     * Returns all plugins that are found in the given Path
+     */
     private static PluginLocation[] getPluginLocations(File path) {
         if (path == null || !path.exists()) {
             return new PluginLocation[0];
         }
         
         File[] files = null;
-        if (path.isDirectory() && path.getName().equals(PLUGINS)) {
+        if (path.isDirectory() && path.getName().equals(PLUGINS_DIR)) {
             final String[] extensions = PluginSettings.PLUGIN_EXTENSIONS.getValue();
             
             files = path.listFiles(new FileFilter() {
@@ -188,10 +235,8 @@ public class LimePluginManager {
                     LOG.info("Plugin: " + file + " -> " + loc);
                 }
                 
-                // System.out.println(file + " -> " + loc);
             } catch (MalformedURLException err) {
                 LOG.error("MalformedURLException", err);
-                err.printStackTrace();
             }
         }
         
