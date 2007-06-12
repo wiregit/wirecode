2235b96f36caf2247b28af31141642d73fd7432c
rkapsi <rkapsi>
2007-06-12 14:09:24 +0000

    Typo

--- components/setting/src/main/java/org/limewire/setting/evt/SettingEvent.java
+++ components/setting/src/main/java/org/limewire/setting/evt/SettingEvent.java
@@ -29,7 +29,7 @@ public class SettingEvent {
         /**
          * The privacy flag was changed
          */
-        PRIVACY_CANGED,
+        PRIVACY_CHANGED,
         
         /**
          * The value changed
