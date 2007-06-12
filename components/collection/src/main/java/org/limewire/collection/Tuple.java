0da61855fd6d58c7de4b843dbc5d5317fc1d8e7a
fberger <fberger>
2007-06-12 16:29:41 +0000

    CORE-148: Option to not use recycle bin, renames.

--- components/collection/src/main/java/org/limewire/collection/Tuple.java
+++ components/collection/src/main/java/org/limewire/collection/Tuple.java
@@ -3,19 +3,19 @@ package org.limewire.collection;
 /**
  * Typed tuple that holds two objects of possibly different types.
 
- * @param <O1> the type of the first object
- * @param <O2> the type of the second object
+ * @param <Type1> the type of the first object
+ * @param <Type2> the type of the second object
  */
-public class Tuple<O1, O2> {
+public class Tuple<Type1, Type2> {
 
-    private final O1 obj1;
+    private final Type1 obj1;
     
-    private final O2 obj2;
+    private final Type2 obj2;
 
     /**
      * Constructs a tuple for two objects.
      */
-    public Tuple(O1 obj1, O2 obj2) {
+    public Tuple(Type1 obj1, Type2 obj2) {
         this.obj1 = obj1;
         this.obj2 = obj2;
     }
@@ -23,14 +23,14 @@ public class Tuple<O1, O2> {
     /**
      * Returns the first object.
      */
-    public O1 getFirst() {
+    public Type1 getFirst() {
         return obj1;
     }
     
     /**
      * Returns the second object. 
      */
-    public O2 getSecond() {
+    public Type2 getSecond() {
         return obj2;
     }
     
