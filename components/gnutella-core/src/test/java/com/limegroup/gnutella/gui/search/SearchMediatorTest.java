9eaaa70a8c34654d54edbbbdb70c7ea0534bf7b9
fberger <fberger>
2007-06-25 22:52:00 +0000

    Test stubs to implement.

---
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/gui/search/SearchMediatorTest.java
@@ -0,0 +1,19 @@
+package com.limegroup.gnutella.gui.search;
+
+import com.limegroup.gnutella.gui.GUIBaseTestCase;
+
+public class SearchMediatorTest extends GUIBaseTestCase {
+
+    public SearchMediatorTest(String name) {
+        super(name);
+    }
+    
+    public void testListeners() {
+        // TODO fberger
+    }
+    
+    public void testListenersByGUID() {
+        // TODO fberger
+    }
+
+}
