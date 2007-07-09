47d84fe28d2a873dff96a8ba4ddfa1c18fa2ff25
fberger <fberger>
2007-07-09 21:42:02 +0000

    GUI-222:
    * Advanced and basic sharing panel. Move bittorrent sharing options to advanced panel.
    * Refactored RecursiveSharingDialog into a RecursiveSharingPanel.

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/gui/library/RecursiveSharingDialogTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/gui/library/RecursiveSharingDialogTest.java
@@ -19,42 +19,6 @@ public class RecursiveSharingDialogTest extends GUIBaseTestCase {
         return buildTestSuite(RecursiveSharingDialogTest.class);
     }
 	
-	public void testRetainAncestorsEmptyInput() {
-		assertEquals(new File[0], RecursiveSharingDialog.retainAncestors(new File[0]));
-	}
-	
-	public void testRetainAncestorsSingleFile() {
-		File file = new File("/test/file");
-		assertEquals(new File[] { file }, RecursiveSharingDialog.retainAncestors(file));
-	}
-	
-	public void testRetainAncestorsNoAncestors() {
-		File[] files = new File[] {
-				new File("/test/test/test"),
-				new File("/test/test/hello"),
-				new File("/test/dir"),
-				new File("/test/tmp/dir"),
-				new File("/blah")
-		};
-		assertEquals(files, RecursiveSharingDialog.retainAncestors(files));
-	}
-	
-	public void testRetainAncestorsWithSubfoldersToRemove() {
-		File[] files = new File[] {
-				new File("/test/test/"),
-				new File("/test/test/hello"),
-				new File("/test/dir"),
-				new File("/test/dir"),
-				new File("/blah")
-		};
-		File[] resulting = new File[] {
-				new File("/test/test/"),
-				new File("/test/dir"),
-				new File("/blah")
-		};
-		assertEquals(resulting, RecursiveSharingDialog.retainAncestors(files));
-	}
-
 	public void testShowRecursiveDialogWithEmptyDirs() {
 		File[] dirs = LimeTestUtils.createTmpDirs("emptydir");
 		RecursiveSharingDialog dialog = new RecursiveSharingDialog(null, dirs);
