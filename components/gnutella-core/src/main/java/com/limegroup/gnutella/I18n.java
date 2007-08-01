4ea7bce1d32827828501e5f8e9ab32abbc096395
fberger <fberger>
2007-08-01 23:06:20 +0000

    GUI-271: Refactor code to make gettext conversion easier: Redid GUIMediator.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/I18n.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/I18n.java
@@ -25,25 +25,27 @@ public class I18n {
         return i18n.trn(singularText, pluralText, number, args);
     }
 
-    public ResourceBundle getResources() {
+    public static ResourceBundle getResources() {
         return i18n.getResources();
     }
 
-    public boolean setLocale(Locale arg0) {
+    public static boolean setLocale(Locale arg0) {
         return i18n.setLocale(arg0);
     }
 
-    public void setResources(ResourceBundle arg0) {
+    public static void setResources(ResourceBundle arg0) {
         i18n.setResources(arg0);
     }
 
-    public void setResources(String arg0, Locale arg1, ClassLoader arg2) {
+    public static void setResources(String arg0, Locale arg1, ClassLoader arg2) {
         i18n.setResources(arg0, arg1, arg2);
     }
 
-    public void setSourceCodeLocale(Locale arg0) {
+    public static void setSourceCodeLocale(Locale arg0) {
         i18n.setSourceCodeLocale(arg0);
     }
-
     
+    public static String marktr(String text) {
+        return text;
+    }
 }
