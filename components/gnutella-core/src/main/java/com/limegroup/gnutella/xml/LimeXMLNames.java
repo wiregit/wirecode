8f9b2900ec5657ab448c7e0af2b96ca08ca5684d
spingel <spingel>
2007-08-03 19:16:15 +0000

    GUI-271: Refactor code to make gettext conversion easier: reverted accidental change of XML name values

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/xml/LimeXMLNames.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/xml/LimeXMLNames.java
@@ -9,73 +9,73 @@ public class LimeXMLNames {
 
     public static final String AUDIO_SCHEMA = "http://www.limewire.com/schemas/audio.xsd";
     
-    public static final String AUDIO = "audios_audio";
-    public static final String AUDIO_TITLE = "audios_audio_title_";
-    public static final String AUDIO_TRACK = "audios_audio_track_";
-    public static final String AUDIO_ARTIST = "audios_audio_artist_";
-    public static final String AUDIO_ALBUM = "audios_audio_album_";
-    public static final String AUDIO_GENRE = "audios_audio_genre_";
-    public static final String AUDIO_COMMENTS = "audios_audio_comments_";
-    public static final String AUDIO_YEAR = "audios_audio_year_";
-    public static final String AUDIO_TYPE = "audios_audio_type_";
-    public static final String AUDIO_LANGUAGE = "audios_audio_language_";
-    public static final String AUDIO_SECONDS = "audios_audio_seconds_";
-    public static final String AUDIO_SHA1 = "audios_audio_SHA1_";
-    public static final String AUDIO_BITRATE = "audios_audio_bitrate_";
-    public static final String AUDIO_PRICE = "audios_audio_price_";
-    public static final String AUDIO_LINK = "audios_audio_link_";
-    public static final String AUDIO_ACTION = "audios_audio_action_";
-    public static final String AUDIO_LICENSE = "audios_audio_license_";
-    public static final String AUDIO_LICENSETYPE = "audios_audio_licensetype_";
+    public static final String AUDIO = "audios__audio";
+    public static final String AUDIO_TITLE = "audios__audio__title__";
+    public static final String AUDIO_TRACK = "audios__audio__track__";
+    public static final String AUDIO_ARTIST = "audios__audio__artist__";
+    public static final String AUDIO_ALBUM = "audios__audio__album__";
+    public static final String AUDIO_GENRE = "audios__audio__genre__";
+    public static final String AUDIO_COMMENTS = "audios__audio__comments__";
+    public static final String AUDIO_YEAR = "audios__audio__year__";
+    public static final String AUDIO_TYPE = "audios__audio__type__";
+    public static final String AUDIO_LANGUAGE = "audios__audio__language__";
+    public static final String AUDIO_SECONDS = "audios__audio__seconds__";
+    public static final String AUDIO_SHA1 = "audios__audio__SHA1__";
+    public static final String AUDIO_BITRATE = "audios__audio__bitrate__";
+    public static final String AUDIO_PRICE = "audios__audio__price__";
+    public static final String AUDIO_LINK = "audios__audio__link__";
+    public static final String AUDIO_ACTION = "audios__audio__action__";
+    public static final String AUDIO_LICENSE = "audios__audio__license__";
+    public static final String AUDIO_LICENSETYPE = "audios__audio__licensetype__";
     
     public static final String APPLICATION_SCHEMA = "http://www.limewire.com/schemas/application.xsd";
     
-    public static final String APPLICATION = "applications_application";
-    public static final String APPLICATION_NAME = "applications_application_name_";
-    public static final String APPLICATION_PUBLISHER = "applications_application_publisher_";
-    public static final String APPLICATION_PLATFORM = "applications_application_platform_";
-    public static final String APPLICATION_LICENSETYPE = "applications_application_licensetype_";
-    public static final String APPLICATION_LICENSE = "applications_application_license";
+    public static final String APPLICATION = "applications__application";
+    public static final String APPLICATION_NAME = "applications__application__name__";
+    public static final String APPLICATION_PUBLISHER = "applications__application__publisher__";
+    public static final String APPLICATION_PLATFORM = "applications__application__platform__";
+    public static final String APPLICATION_LICENSETYPE = "applications__application__licensetype__";
+    public static final String APPLICATION_LICENSE = "applications__application__license";
     
     public static final String DOCUMENT_SCHEMA = "http://www.limewire.com/schemas/document.xsd";
     
-    public static final String DOCUMENT = "documents_document";
-    public static final String DOCUMENT_TITLE = "documents_document_title_";
-    public static final String DOCUMENT_TOPIC = "documents_document_topic_";
-    public static final String DOCUMENT_AUTHOR = "documents_document_author_";
-    public static final String DOCUMENT_LICENSE = "documents_document_license_";
-    public static final String DOCUMENT_LICENSETYPE = "documents_document_licensetype_";
+    public static final String DOCUMENT = "documents__document";
+    public static final String DOCUMENT_TITLE = "documents__document__title__";
+    public static final String DOCUMENT_TOPIC = "documents__document__topic__";
+    public static final String DOCUMENT_AUTHOR = "documents__document__author__";
+    public static final String DOCUMENT_LICENSE = "documents__document__license__";
+    public static final String DOCUMENT_LICENSETYPE = "documents__document__licensetype__";
     
     public static final String IMAGE_SCHEMA = "http://www.limewire.com/schemas/image.xsd";
     
-    public static final String IMAGE = "images_image";
-    public static final String IMAGE_TITLE = "images_image_title_";
-    public static final String IMAGE_DESCRIPTION = "images_image_description_";
-    public static final String IMAGE_ARTIST = "images_image_artist_";
-    public static final String IMAGE_LICENSE = "images_image_license_";
-    public static final String IMAGE_LICENSETYPE = "images_image_licensetype_";
+    public static final String IMAGE = "images__image";
+    public static final String IMAGE_TITLE = "images__image__title__";
+    public static final String IMAGE_DESCRIPTION = "images__image__description__";
+    public static final String IMAGE_ARTIST = "images__image__artist__";
+    public static final String IMAGE_LICENSE = "images__image__license__";
+    public static final String IMAGE_LICENSETYPE = "images__image__licensetype__";
     
     public static final String VIDEO_SCHEMA = "http://www.limewire.com/schemas/video.xsd";
     
-    public static final String VIDEO = "videos_video";
-    public static final String VIDEO_TITLE = "videos_video_title_";
-    public static final String VIDEO_TYPE = "videos_video_type_";
-    public static final String VIDEO_YEAR = "videos_video_year_";
-    public static final String VIDEO_RATING = "videos_video_rating_";
-    public static final String VIDEO_LENGTH = "videos_video_length_";
-    public static final String VIDEO_COMMENTS = "videos_video_comments_";
-    public static final String VIDEO_LICENSE = "videos_video_license_";
-    public static final String VIDEO_LICENSETYPE = "videos_video_licensetype_";
-    public static final String VIDEO_HEIGHT = "videos_video_height_";
-    public static final String VIDEO_WIDTH = "videos_video_width_";
-    public static final String VIDEO_BITRATE = "videos_video_bitrate_";
-    public static final String VIDEO_ACTION = "videos_video_action_";
-    public static final String VIDEO_DIRECTOR = "videos_video_director_";
-    public static final String VIDEO_STUDIO = "videos_video_studio_";
-    public static final String VIDEO_LANGUAGE = "videos_video_language_";
-    public static final String VIDEO_STARS = "videos_video_stars_";
-    public static final String VIDEO_PRODUCER = "videos_video_producer_";
-    public static final String VIDEO_SUBTITLES = "videos_video_subtitles_";
+    public static final String VIDEO = "videos__video";
+    public static final String VIDEO_TITLE = "videos__video__title__";
+    public static final String VIDEO_TYPE = "videos__video__type__";
+    public static final String VIDEO_YEAR = "videos__video__year__";
+    public static final String VIDEO_RATING = "videos__video__rating__";
+    public static final String VIDEO_LENGTH = "videos__video__length__";
+    public static final String VIDEO_COMMENTS = "videos__video__comments__";
+    public static final String VIDEO_LICENSE = "videos__video__license__";
+    public static final String VIDEO_LICENSETYPE = "videos__video__licensetype__";
+    public static final String VIDEO_HEIGHT = "videos__video__height__";
+    public static final String VIDEO_WIDTH = "videos__video__width__";
+    public static final String VIDEO_BITRATE = "videos__video__bitrate__";
+    public static final String VIDEO_ACTION = "videos__video__action__";
+    public static final String VIDEO_DIRECTOR = "videos__video__director__";
+    public static final String VIDEO_STUDIO = "videos__video__studio__";
+    public static final String VIDEO_LANGUAGE = "videos__video__language__";
+    public static final String VIDEO_STARS = "videos__video__stars__";
+    public static final String VIDEO_PRODUCER = "videos__video__producer__";
+    public static final String VIDEO_SUBTITLES = "videos__video__subtitles__";
 
     private static Map<String, String> displayNameByKey;
     
