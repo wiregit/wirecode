2b3c41b1001a561a28e70d2087d38a10eac8673e
spingel <spingel>
2007-09-07 20:32:44 +0000

    CORE-320: fixed compile error

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/stubs/ScheduledExecutorServiceStub.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/stubs/ScheduledExecutorServiceStub.java
@@ -1,16 +1,44 @@
 package com.limegroup.gnutella.stubs;
 
-import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.AbstractExecutorService;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
-public class ScheduledExecutorServiceStub implements ScheduledExecutorService {
+public class ScheduledExecutorServiceStub extends AbstractExecutorService implements ScheduledExecutorService {
+
+    public boolean awaitTermination(long timeout, TimeUnit unit)
+            throws InterruptedException {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean isShutdown() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean isTerminated() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public void shutdown() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public List<Runnable> shutdownNow() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void execute(Runnable command) {
+        // TODO Auto-generated method stub
+        
+    }
 
     public ScheduledFuture<?> schedule(Runnable command, long delay,
             TimeUnit unit) {
@@ -35,77 +63,5 @@ public class ScheduledExecutorServiceStub implements ScheduledExecutorService {
         // TODO Auto-generated method stub
         return null;
     }
-
-    public boolean awaitTermination(long timeout, TimeUnit unit)
-            throws InterruptedException {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    public <T> List<Future<T>> invokeAll(Collection tasks)
-            throws InterruptedException {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public <T> List<Future<T>> invokeAll(
-            Collection tasks, long timeout, TimeUnit unit)
-            throws InterruptedException {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public <T> T invokeAny(Collection tasks)
-            throws InterruptedException, ExecutionException {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public <T> T invokeAny(Collection tasks,
-            long timeout, TimeUnit unit) throws InterruptedException,
-            ExecutionException, TimeoutException {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public boolean isShutdown() {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    public boolean isTerminated() {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    public void shutdown() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    public List<Runnable> shutdownNow() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public <T> Future<T> submit(Callable<T> task) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public Future<?> submit(Runnable task) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public <T> Future<T> submit(Runnable task, T result) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public void execute(Runnable command) {
-        // TODO Auto-generated method stub
-        
-    }
-
+    
 }
