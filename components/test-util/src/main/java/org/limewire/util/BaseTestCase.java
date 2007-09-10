8ad0b58836fef09bf31c69f44d93d007a28924db
spingel <spingel>
2007-09-10 20:13:36 +0000

    CORE-320: fixed compile error

--- components/test-util/src/main/java/org/limewire/util/BaseTestCase.java
+++ components/test-util/src/main/java/org/limewire/util/BaseTestCase.java
@@ -9,8 +9,6 @@ import java.util.StringTokenizer;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import org.apache.commons.logging.LogFactory;
-
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestResult;
@@ -168,7 +166,7 @@ public abstract class BaseTestCase extends AssertComparisons {
         _currentTestName = getName();
         if(System.getProperty("junit.test.hidetestname", "${hidetestname}").equals("${hidetestname}")) {
             System.out.println("Running test: " + _currentTestName);
-            LogFactory.getLog(this.getClass()).info("Running test: " + _currentTestName);
+            //LogFactory.getLog(this.getClass()).info("Running test: " + _currentTestName);
         }
         assertNotNull(_currentTestName);
         try {
