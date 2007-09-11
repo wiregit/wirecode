4214b0d8f1e4eb22cc9bc57f0ede415acb40c601
abow <abow>
2007-09-11 19:25:57 +0000

    OPEN - issue CORE-320: Fix Tests https://www.limewire.org/jira/browse/CORE-320
    --> made changes according to suggestions
    --> removed dependencies

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/filters/SpamReplyFilterTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/filters/SpamReplyFilterTest.java
@@ -1,36 +1,27 @@
 package com.limegroup.gnutella.filters;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-
 import junit.framework.Test;
 
-import com.limegroup.gnutella.GUID;
-import com.limegroup.gnutella.ProviderHacks;
-import com.limegroup.gnutella.Response;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+
+import org.limewire.util.BaseTestCase;
+
 import com.limegroup.gnutella.messages.QueryReply;
-import com.limegroup.gnutella.util.LimeTestCase;
+import com.limegroup.gnutella.messages.QueryRequest;
+
 
 /**
  * Unit tests for SpamReplyFilter
  */
 @SuppressWarnings( { "unchecked", "cast" } )
-public class SpamReplyFilterTest extends LimeTestCase {
-        
-    /**
-     * A non blank IP
-     */
-    private static final byte[] IP = new byte[] {1, 1, 1, 1};
-
-    private final QueryReply _reply = 
-         ProviderHacks.getQueryReplyFactory().createQueryReply(GUID.makeGuid(), (byte) 1, 6346,
-            IP, (long) 3, new Response[] { ProviderHacks.getResponseFactory().createResponse((long) 2,
-                    (long) 2, "Susheel") }, GUID.makeGuid(), true, true, true,
-            true, true, false);
-
-    private byte[] _replyBytes = null;
-    private int _indexOfVendor = -1;
+public class SpamReplyFilterTest extends BaseTestCase {
+          
     private SpamReplyFilter _filter = new SpamReplyFilter();
+    private Mockery context = new Mockery();
+    private QueryReply queryReplyMock = context.mock(QueryReply.class);
+    private QueryRequest queryRequestMock = context.mock(QueryRequest.class);
+   
 
 	public SpamReplyFilterTest(String name) {
 		super(name);
@@ -44,26 +35,6 @@ public class SpamReplyFilterTest extends LimeTestCase {
 		junit.textui.TestRunner.run(suite());
 	}
 	
-    public void setUp() throws Exception {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        _reply.write(baos);
-        _replyBytes = baos.toByteArray();
-        boolean notFound = true;
-        int index = 0;
-        while (notFound && ((index+3) < _replyBytes.length)) {
-            if ((_replyBytes[index+0] == (byte) 76) &&
-                (_replyBytes[index+1] == (byte) 73) &&
-                (_replyBytes[index+2] == (byte) 77) &&
-                (_replyBytes[index+3] == (byte) 69)) {
-                notFound = false;
-                _indexOfVendor = index;
-            }
-            index++;
-        }
-        // this should NEVER happen
-        assertTrue(!notFound);
-    }
-
     public void testReplies() throws Exception{
         assertTrue( allow("LIME"));
         assertTrue( allow("BEAR"));
@@ -71,18 +42,27 @@ public class SpamReplyFilterTest extends LimeTestCase {
         assertTrue(!allow("MUTE"));
         assertTrue( allow("GTKG"));
         assertTrue( allow("GNUC"));
+        
+        context.assertIsSatisfied();
     }
     
-
-    private boolean allow(String vendorCode) throws Exception {
-        byte[] vendorBytes = vendorCode.getBytes();
-        assertEquals(4, vendorBytes.length);
-        for (int i = 0; i < vendorBytes.length; i++)
-            _replyBytes[_indexOfVendor+i] = vendorBytes[i];
+    public void testOtherMessagesAreIgnored() throws Exception{
+        context.checking(new Expectations()
+        {{ never(queryRequestMock);
+        }});
+       
+        assertTrue( _filter.allow(queryRequestMock));
         
-        QueryReply qr = 
-            (QueryReply) ProviderHacks.getMessageFactory().read(new ByteArrayInputStream(_replyBytes));
-        return _filter.allow(qr);
+        context.assertIsSatisfied();
+    }
+    
+   private boolean allow(final String vendorCode) throws Exception {
+        context.checking(new Expectations() {{
+            exactly(1).of(queryReplyMock).getVendor();
+            will(returnValue(vendorCode));
+        }});
+        
+        return _filter.allow(queryReplyMock);
     }
 
 }
