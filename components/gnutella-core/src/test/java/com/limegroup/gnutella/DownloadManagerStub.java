28bb32aa6a5dde33e4d20ed046765d453d481215
spingel <spingel>
2007-09-18 19:26:04 +0000

    CORE-320: fixed ResumeDownloaderTest

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/DownloadManagerStub.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/DownloadManagerStub.java
@@ -1,9 +1,17 @@
 package com.limegroup.gnutella;
 
 import java.io.File;
+import java.util.concurrent.ScheduledExecutorService;
 
 import org.limewire.inject.Providers;
 
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.name.Named;
+import com.limegroup.bittorrent.BTDownloaderFactory;
+import com.limegroup.bittorrent.TorrentManager;
+import com.limegroup.gnutella.downloader.DownloadReferencesFactory;
+import com.limegroup.gnutella.downloader.GnutellaDownloaderFactory;
 import com.limegroup.gnutella.downloader.IncompleteFileManager;
 import com.limegroup.gnutella.downloader.ManagedDownloader;
 import com.limegroup.gnutella.downloader.PushDownloadManager;
@@ -12,6 +20,8 @@ import com.limegroup.gnutella.messages.QueryRequest;
 
 
 public class DownloadManagerStub extends DownloadManager {
+    
+    // DPINJ: remove me
     public DownloadManagerStub() {
         super(ProviderHacks.getNetworkManager(), ProviderHacks
                 .getDownloadReferencesFactory(), ProviderHacks
@@ -25,7 +35,21 @@ public class DownloadManagerStub extends DownloadManager {
                         ProviderHacks.getGnutellaDownloaderFactory());
     }
 	
-	
+    @Inject
+    public DownloadManagerStub(NetworkManager networkManager,
+            DownloadReferencesFactory downloadReferencesFactory,
+            @Named("inNetwork") DownloadCallback innetworkCallback,
+            BTDownloaderFactory btDownloaderFactory,
+            Provider<DownloadCallback> downloadCallback,
+            Provider<MessageRouter> messageRouter,
+            @Named("backgroundExecutor") ScheduledExecutorService backgroundExecutor,
+            Provider<TorrentManager> torrentManager,
+            Provider<PushDownloadManager> pushDownloadManager,
+            BrowseHostHandlerManager browseHostHandlerManager,
+            GnutellaDownloaderFactory gnutellaDownloaderFactory) {
+        super(networkManager, downloadReferencesFactory, innetworkCallback, btDownloaderFactory, downloadCallback, messageRouter, backgroundExecutor, torrentManager, pushDownloadManager, browseHostHandlerManager, gnutellaDownloaderFactory);
+    }
+
     @Override
     public void initialize() {
         postGuiInit();
@@ -64,10 +88,7 @@ public class DownloadManagerStub extends DownloadManager {
 	public synchronized float getMeasuredBandwidth() { return 0.f; }
     @Override
     public IncompleteFileManager getIncompleteFileManager() { return null; }
-    
-    public PushDownloadManager getPushManager() {return ProviderHacks.getPushDownloadManager();}
-    
-    
+        
     public final Object pump = new Object();
     
     @Override
