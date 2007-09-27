089007ed9ca2f99e1b94fb715aab6cf92a8a6342
zlatinb <zlatinb>
2007-09-27 20:37:00 +0000

    parse the properties manually because of white spaces

--- components/common/src/main/java/org/limewire/inspection/InspectorImpl.java
+++ components/common/src/main/java/org/limewire/inspection/InspectorImpl.java
@@ -1,10 +1,10 @@
 package org.limewire.inspection;
 
-import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Properties;
 
 import com.google.inject.Inject;
@@ -32,18 +32,24 @@ public class InspectorImpl implements Inspector {
         /*
          * Ignore any and all errors as inspection is not critical functionality
          */
-        InputStream fis = null;
+        BufferedReader in = null;
         Properties p = new Properties();
         try {
-            fis = new BufferedInputStream(new FileInputStream(props));
-            p.load(fis);
+            in = new BufferedReader(new InputStreamReader(new FileInputStream(props)));
+            String current = null;
+            while((current = in.readLine()) != null) {
+                String [] k = current.split("=");
+                if (k.length != 2)
+                    continue;
+                p.setProperty(k[0], k[1]);
+            }
             this.props = p;
         } catch(IllegalArgumentException ignored) {
         } catch(StringIndexOutOfBoundsException sioobe) {
         } catch(IOException iox) {}
         finally {
-            if (fis != null)
-                try {fis.close();} catch (IOException ignore){}
+            if (in != null)
+                try {in.close();} catch (IOException ignore){}
         }
     }
 
