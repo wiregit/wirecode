a052aec0577f4f6e00e8dc988b78d2ada0182e17
zlatinb <zlatinb>
2007-09-28 15:42:06 +0000

    re-add annotations check and inspection tool jar

---
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/util/AnnotationsCheck.java
@@ -0,0 +1,37 @@
+package com.limegroup.gnutella.util;
+
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import junit.framework.Test;
+
+import org.limewire.inspection.InspectionTool;
+import org.limewire.util.BaseTestCase;
+
+public class AnnotationsCheck extends BaseTestCase {
+
+    public AnnotationsCheck(String name) {
+        super(name);
+    }
+    
+    public static Test suite() {
+        return buildTestSuite(AnnotationsCheck.class);
+    }
+    
+    public void testAnnotations() throws Exception {
+        String classPath = System.getProperty("java.class.path",".");
+
+        StringTokenizer st = new StringTokenizer(classPath, 
+          System.getProperty("path.separator"));
+        
+        
+        Map<String, String> results = new HashMap<String, String>();
+        while (st.hasMoreTokens()) 
+            results.putAll(InspectionTool.generateMappings(new File(st.nextToken())));
+        
+        assertFalse(results.isEmpty());
+    }
+}
