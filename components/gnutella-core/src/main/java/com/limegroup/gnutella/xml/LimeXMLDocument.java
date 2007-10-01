bebd20389e0ae73233a4f06747db4aef50a08f9f
zlatinb <zlatinb>
2007-10-01 19:40:16 +0000

    frontport volatile license type field

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/xml/LimeXMLDocument.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/xml/LimeXMLDocument.java
@@ -102,7 +102,7 @@ public class LimeXMLDocument implements Serializable, StringLookup {
     private transient List<String> CACHED_KEYWORDS = null;
     
     /** The kind of license this has. */
-    private transient LicenseType licenseType = LicenseType.NO_LICENSE;
+    private transient volatile LicenseType licenseType = LicenseType.NO_LICENSE;
 
     private transient volatile LicenseFactory licenseFactory;
     
