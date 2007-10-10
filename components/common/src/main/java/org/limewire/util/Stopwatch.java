b3c4556806907d308d12620445bb34e842a97f87
zlatinb <zlatinb>
2007-10-10 16:39:41 +0000

    make the stop watch use nanos and apply the windows interrupt workaround earlier

--- components/common/src/main/java/org/limewire/util/Stopwatch.java
+++ components/common/src/main/java/org/limewire/util/Stopwatch.java
@@ -1,25 +1,48 @@
 package org.limewire.util;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.commons.logging.Log;
 
 public class Stopwatch {
     
     private final Log log;
-    private long start = System.currentTimeMillis();
+    private long start = System.nanoTime();
     
-    public Stopwatch(Log log) {
+    public Stopwatch(final Log log) {
         this.log = log;
+        final CountDownLatch threadStarted = new CountDownLatch(1);
+        
+        // start the timer interrupt hack earlier.  Make sure this is removed
+        // once the startup times are improved.
+        Thread timeHack = new Thread() {
+            public void run() {
+                resetAndLog("enabling timehack");
+                threadStarted.countDown();
+                try {
+                    Thread.sleep(Integer.MAX_VALUE);
+                } catch (InterruptedException bleh){}
+            }
+        };
+        timeHack.start();
+        try {
+            threadStarted.await();
+            // sleep some extra to make sure
+            // the time hack is engaged.
+            Thread.sleep(100); 
+        } catch (InterruptedException ignore){}
     }
 
     /**
      * Resets and returns elapsed time in milliseconds.
      */
-    public long reset() {
+    public synchronized long reset() {
         if(log.isTraceEnabled()) {
-            long now = System.currentTimeMillis();
+            long now = System.nanoTime();
             long elapsed = now - start;
             start = now;
-            return elapsed;
+            return TimeUnit.NANOSECONDS.toMillis(elapsed);
         } else {
             return -1;
         }
