7fa06d7460137b7ec5d98a060fadb9ef34b8d637
fberger <fberger>
2007-10-23 18:04:53 +0000

    GUI-303: File Open dialog for magnet and torrent: Fixed by Kevin.

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/browser/MagnetOptionsTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/browser/MagnetOptionsTest.java
@@ -1,10 +1,17 @@
 package com.limegroup.gnutella.browser;
 
+import org.limewire.util.BaseTestCase;
+
 import com.limegroup.gnutella.URN;
-import com.limegroup.gnutella.util.LimeTestCase;
 import com.limegroup.gnutella.util.EncodingUtils;
 
-public class MagnetOptionsTest extends LimeTestCase {
+public class MagnetOptionsTest extends BaseTestCase {
+
+    MagnetOptions[] validMagnets;
+    
+    public void setUp() {
+        validMagnets = MagnetOptions.parseMagnet("magnet:?xt.1=urn:sha1:YNCKHTQCWBTRNJIV4WNAE52SJUQCZO5C&xt.2=urn:sha1:TXGCZQTH26NL6OUQAJJPFALHG2LTGBC7");
+    }
 
 	public MagnetOptionsTest(String name) {
 		super(name);
@@ -144,4 +151,32 @@ public class MagnetOptionsTest extends LimeTestCase {
 		magnet = MagnetOptions.createMagnet(null, null, null, null);
 		assertNotNull(magnet.getFileNameForSaving());
 	}
+    
+    public void testParseValidMagnets() {
+        String magnets = createMultiLineMagnetLinks(validMagnets);
+        MagnetOptions[] opts = MagnetOptions.parseMagnets(magnets);
+        assertEquals("Should have parsed " + validMagnets.length + " magnets", validMagnets.length, opts.length);
+        // and parse again
+        assertEquals("Should have parsed " + opts.length + " magnets", opts.length, 
+                MagnetOptions.parseMagnets(createMultiLineMagnetLinks(opts)).length);
+        
+        // compound magnets with multiple lines
+        opts = MagnetOptions.parseMagnets
+            ("magnet:?xt.1=urn:sha1:YNCKHTQCWBTRNJIV4WNAE52SJUQCZO5C&xt.2=urn:sha1:TXGCZQTH26NL6OUQAJJPFALHG2LTGBC7"
+                    + System.getProperty("line.separator")
+                    + "magnet:?xt.1=urn:sha1:YNCKHTQCWBTRNJIV4WNAE52SJUQCZO5C&xt.2=urn:sha1:TXGCZQTH26NL6OUQAJJPFALHG2LTGBC7");
+        assertEquals("Should have parsed 2 magnets", 4, opts.length);
+    }
+    
+    private String createMultiLineMagnetLinks(MagnetOptions[] opts) {
+        StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < opts.length; i++) {
+            if (i > 0) {
+                buffer.append(System.getProperty("line.separator"));
+            }
+            buffer.append(opts[i].toString());
+        }
+        return buffer.toString();
+    }
+    
 }
