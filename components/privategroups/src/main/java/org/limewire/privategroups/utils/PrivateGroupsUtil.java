aa657dedbae13e1bf17e8b22233738a24687b775
abow <abow>
2007-12-13 21:54:15 +0000

    Private Groups Utility Class

--- components/privategroups/src/main/java/org/limewire/privategroups/utils/PrivateGroupsUtil.java
+++ components/privategroups/src/main/java/org/limewire/privategroups/utils/PrivateGroupsUtil.java
@@ -3,6 +3,9 @@ package org.limewire.privategroups.utils;
 import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Message.Type;
 
+/**
+ * A utility class for buddy lists/private groups
+ */
 public class PrivateGroupsUtil {
     // a method to automatically create a message object given a username, and the message body
     public static Message createMessage(String localUsername, String remoteuser, String body){
