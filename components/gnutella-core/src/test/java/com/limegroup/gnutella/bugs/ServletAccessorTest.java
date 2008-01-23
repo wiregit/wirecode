58d54deefb9cf79732076b051c974df2b18d574a
jjj <jjj>
2008-01-23 20:14:59 +0000

    Fixed comments

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/bugs/ServletAccessorTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/bugs/ServletAccessorTest.java
@@ -35,7 +35,7 @@ public class ServletAccessorTest extends BaseTestCase {
         
     private final String COMMENT_USER_ENTERED = "Testing User Comments";
     
-    String userComments = "";
+    private String userComments = "";
         
     /**
      * The amount of time to wait should the server be down (got it from RemoteClientInfo) 
@@ -49,16 +49,12 @@ public class ServletAccessorTest extends BaseTestCase {
     }
 
     public void testGetRemoteBugInfo() {                                     
-            /**
-             * creates and starts a new thread for our server to run on
-             * for the client to contact
-             */
+            //creates and starts a new thread for our server to run on
+            // for the client to contact
             Thread serverSocket = FACTORY.newThread(new TestSocket());
             serverSocket.start();            
             
-            /**
-             * creates a new error which will be logged and send to a server
-             */
+            //creates a new error which will be logged and send to a server
             PROBLEM = new Throwable("Testing Get Remote Bug Info");        
             CURRENT_THREAD_NAME = Thread.currentThread().getName();        
             LocalClientInfo info = new LocalClientInfo(PROBLEM, CURRENT_THREAD_NAME, null, false, null);
@@ -66,24 +62,19 @@ public class ServletAccessorTest extends BaseTestCase {
             ServletAccessor SA = new ServletAccessor(true, "http://localhost:"+PORT);            
             RemoteClientInfo RCI = SA.getRemoteBugInfo(info);
 
-            /**
-             * Waits until the server gets the data and sends a response back
-             */
+            //Waits until the server gets the data and sends a response back
             try {
                 serverSocket.join();                
             } catch (InterruptedException ex) { }
             
-            /**
-             * makes sure the comment user entered is received by the server
-             */
+            //makes sure the comment user entered is received by the server
             try {
                 assertEquals(COMMENT_USER_ENTERED, URLDecoder.decode(userComments));
             } catch (IOException ex){ }
             
-            /**
-             * makes sure the server is not down and no any error occurred during the process of
-             * sending data to the server and getting back a response
-             */
+            //makes sure the server is not down and no any error occurred during the process of
+            //sending data to the server and getting back a response
+
             assertNotEquals(FAILURE_TIME, RCI.getNextAnyBugTime());
     }
     
@@ -95,47 +86,33 @@ public class ServletAccessorTest extends BaseTestCase {
      
             try {                
                 //ServerSocket ss = new ServerSocket(port, 100, InetAddress.getByName("10.254.0.246")); //if not using localhost
-                /**
-                 * sets up the server and socket
-                 */
+                //sets up the server and socket
                 ServerSocket ss = new ServerSocket(PORT);
                 Socket s = ss.accept();
                 
-                /**
-                 * prepares and receives data from the client
-                 */
+                //prepares and receives data from the client
                 InputStream in = s.getInputStream();
                 BufferedReader BR = new BufferedReader(new InputStreamReader(in));
                 String input = null;
-                /**
-                 * length of the content received from the client
-                 */
+                //length of the content received from the client
                 int length = 0;
                 
-                /**
-                 * processes data
-                 */
+                //processes data
                 while((input=BR.readLine()).length()!=0) {
                     if(input.toLowerCase().startsWith("content-length")) {
                         length = Integer.parseInt(input.substring(input.indexOf(": ")+2));
                     }
                 }
                 
-                /**
-                 * collects content
-                 */
+                //collects content
                 char [] content = new char[length];
                 BR.read(content, 0, length);
                 String contentAsString = new String(content);                
                 
-                /**
-                 * extracts the comments entered by the user
-                 */
+                //extracts the comments entered by the user
                 userComments = contentAsString.substring(contentAsString.indexOf("&63=")+4);                
                 
-                /**
-                 * prepares and sends the response to the client
-                 */
+                //prepares and sends the response to the client
                 OutputStream out = s.getOutputStream();
                 String response;
                 response = "HTTP/1.1 200 OK\r\n";                    
