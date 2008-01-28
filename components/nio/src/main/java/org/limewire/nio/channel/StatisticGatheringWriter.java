b69931a49708a6972d0303ececbc92b042fa0dbc
zlatinb <zlatinb>
2008-01-28 23:19:02 +0000

    LWC-1135 the nano offset doesn't make sense if not global

--- components/nio/src/main/java/org/limewire/nio/channel/StatisticGatheringWriter.java
+++ components/nio/src/main/java/org/limewire/nio/channel/StatisticGatheringWriter.java
@@ -12,6 +12,7 @@ import org.limewire.util.ByteOrder;
 /** A simple writer that maintains statistics about how much was written. */
 public class StatisticGatheringWriter extends AbstractChannelInterestWriter {
 
+    private final static long NANO_START = System.nanoTime();
     private static final int HISTORY = 50;
 
     private final Buffer<Long> handleWrites = new Buffer<Long>(HISTORY);
@@ -19,14 +20,13 @@ public class StatisticGatheringWriter extends AbstractChannelInterestWriter {
     private final Buffer<Boolean> interestWritesStatus = new Buffer<Boolean>(HISTORY);
     private final Buffer<Long> writeTimes = new Buffer<Long>(HISTORY);
     private final Buffer<Long> writeAmounts = new Buffer<Long>(HISTORY);
-    private final long nanoStart = System.nanoTime();
     private final long msStart = System.currentTimeMillis();
     
     private long amountWrote, totalHandleWrite, totalInterestWrite, positiveInterestWrite;
     
     @Override
     public int write(ByteBuffer src) throws IOException {
-        writeTimes.add(System.nanoTime() - nanoStart);
+        writeTimes.add(System.nanoTime() - NANO_START);
         int wrote = super.write(src);
         amountWrote += wrote;
         writeAmounts.add((long)wrote);
@@ -35,14 +35,14 @@ public class StatisticGatheringWriter extends AbstractChannelInterestWriter {
     
     @Override
     public boolean handleWrite() throws IOException {
-        handleWrites.add(System.nanoTime() - nanoStart);
+        handleWrites.add(System.nanoTime() - NANO_START);
         totalHandleWrite++;
         return super.handleWrite();
     }
 
     @Override
     public void interestWrite(WriteObserver observer, boolean status) {
-        interestWrites.add(System.nanoTime() - nanoStart);
+        interestWrites.add(System.nanoTime() - NANO_START);
         interestWritesStatus.add(status);
         totalInterestWrite++;
         if (status)
