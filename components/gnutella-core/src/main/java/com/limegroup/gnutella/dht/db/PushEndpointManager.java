bf80bd3901a022d1df642d9be1f90ee984746814
fberger <fberger>
2008-02-20 20:48:50 +0000

    LWC-1280: Implement requerying for guid urns and use RequeryManager for urn lookups: searches with time restraints.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/dht/db/PushEndpointManager.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/dht/db/PushEndpointManager.java
@@ -1,9 +1,8 @@
 package com.limegroup.gnutella.dht.db;
 
-import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicLong;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -24,7 +23,11 @@ public class PushEndpointManager implements PushEndpointService {
 
     private final PushEndpointCache pushEndpointCache;
     private final DHTPushEndpointFinder pushEndpointFinder;
-    private ConcurrentMap<GUID, List<SearchListener<PushEndpoint>>> listenersByGuid = new ConcurrentHashMap<GUID, List<SearchListener<PushEndpoint>>>();
+    private ConcurrentMap<GUID, AtomicLong> lastSearchTimeByGUID = new ConcurrentHashMap<GUID, AtomicLong>(); 
+    
+    public static final long TIME_BETWEEN_SEARCHES = 5L * 60L * 1000L;
+    
+    private long timeBetweenSearches = TIME_BETWEEN_SEARCHES;
     
     @Inject
     public PushEndpointManager(PushEndpointCache pushEndpointCache, DHTPushEndpointFinder pushEndpointFinder) {
@@ -32,41 +35,52 @@ public class PushEndpointManager implements PushEndpointService {
         this.pushEndpointFinder = pushEndpointFinder;
     }
     
+    /**
+     * Mainly for testing but could be exposed if necessary. 
+     */
+    void setTimeBetweenSearches(long timeBetweenSearches) {
+        this.timeBetweenSearches = timeBetweenSearches;
+    }
+    
+    long getTimeBetweenSearches() {
+        return timeBetweenSearches;
+    }
+    
     public void findPushEndpoint(GUID guid, SearchListener<PushEndpoint> listener) {
         listener = SearchListenerAdapter.nonNullListener(listener);
-        PushEndpoint cachedPushEndpoint = pushEndpointCache.getCached(guid);
+        PushEndpoint cachedPushEndpoint = pushEndpointCache.getPushEndpoint(guid);
         if (cachedPushEndpoint != null) {
-            // TODO create a PushEndpoint here
             listener.handleResult(cachedPushEndpoint);
             listener.handleSearchDone(true);
         } else {
-            // TODO check for timing requirements,
-            final List<SearchListener<PushEndpoint>> newList =
-                new CopyOnWriteArrayList<SearchListener<PushEndpoint>>();
-            List<SearchListener<PushEndpoint>> oldList = listenersByGuid.putIfAbsent(guid, newList);
-            if (oldList != null) {
-                oldList.add(listener);
+            long currentTime = System.currentTimeMillis();
+            AtomicLong lastSearchTime = lastSearchTimeByGUID.putIfAbsent(guid, new AtomicLong(currentTime));
+            // no old value, just start a search, knowing we are set as last search time
+            if (lastSearchTime == null) {
+                startSearch(guid, listener);
             } else {
-                newList.add(listener);
+                long lastSearch = lastSearchTime.longValue();
+                // check if we can start a new search
+                if (currentTime - lastSearch > timeBetweenSearches) {
+                    // only start a search if we set the current time
+                    if (lastSearchTime.compareAndSet(lastSearch, currentTime)) {
+                        startSearch(guid, listener);
+                    }
+                }
             }
-            pushEndpointFinder.findPushEndpoint(guid, new SearchListener<PushEndpoint>() {
-
-                public void handleResult(PushEndpoint result) {
-                    // TODO notify cache
-                    
-                    for (SearchListener<PushEndpoint> listener : newList) {
-                        listener.handleResult(result);
-                    }
-                }
-
-                public void handleSearchDone(boolean success) {
-                    for (SearchListener<PushEndpoint> listener : newList) {
-                        listener.handleSearchDone(success);
-                    }
-                }
-                
-            });
         }
+    }
+    
+    private void startSearch(GUID guid, final SearchListener<PushEndpoint> listener) {
+        pushEndpointFinder.findPushEndpoint(guid, new SearchListener<PushEndpoint>() {
+                public void handleResult(PushEndpoint result) {
+                    result.updateProxies(true);
+                    listener.handleResult(result);
+                }
+                public void handleSearchDone(boolean success) {
+                    listener.handleSearchDone(success);
+                }
+        });
     }
 
     public PushEndpoint getPushEndpoint(GUID guid) {
