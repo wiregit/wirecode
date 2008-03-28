85e4fa30a6336393965fa75185da361895d73bd6
fberger <fberger>
2008-03-28 18:36:03 +0000

    Added test to right branch.

---
+++ components/common/src/test/java/org/limewire/listener/WeakEventListenerListTest.java
@@ -0,0 +1,39 @@
+package org.limewire.listener;
+
+import junit.framework.Test;
+
+import org.limewire.util.BaseTestCase;
+
+/**
+ * A class to test the methods of WeakEventListenerList: adding, removing.
+ * Need to add tests for multiple adds, adds to existing lists, adding different 
+ * types of listeners, as well as broadcasting.
+ */
+public class WeakEventListenerListTest extends BaseTestCase {
+    
+    private WeakEventListenerList<Object> weakList;
+
+    public WeakEventListenerListTest(String name) {
+        super(name);
+    }
+    
+    public static Test suite() {
+        return buildTestSuite(WeakEventListenerListTest.class);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        weakList = new WeakEventListenerList<Object>();
+    }
+
+    /**
+     * A simple test to add a listener (for which the key did not exist before),
+     * and to remove it after
+     */
+    public void testAddRemoveListener(){
+        String ref = "testref";
+        weakList.addListener(ref, null);
+        assertTrue(weakList.removeListener(ref, null));
+    }
+
+}
