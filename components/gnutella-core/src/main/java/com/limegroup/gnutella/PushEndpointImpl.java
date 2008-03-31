0f328d48420f57f802a0939e8b6f2db9f4a801a7
fberger <fberger>
2008-03-31 22:14:43 +0000

    LWC-1345: Fix test cases in branch that broke: clea up.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/PushEndpointImpl.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/PushEndpointImpl.java
@@ -9,61 +9,6 @@ import org.limewire.io.IpPort;
 import org.limewire.io.IpPortSet;
 import org.limewire.io.NetworkInstanceUtils;
 
-
-/**
- * a class that represents an endpoint behind one or more PushProxies.
- * almost everything is immutable including the contents of the set.
- * 
- * the network format this is serialized to is:
- * byte 0 (from right-to-left): 
- *    - bits 0-2 how many push proxies we have (so max is 7)
- *    - bits 3-4 the version of the f2f transfer protocol this altloc supports
- *    - bits 5-6 other possible features.
- *    - bit  7   set if the TLS-capable push proxy indexes byte is included
- * bytes 1-16 : the guid
- * bytes 17-22: ip:port of the address (if FWT version > 0)
- * followed by a byte of TLS-capable PushProxy indexes (if bit 7 of features is set)
- * followed by 6 bytes per PushProxy 
- * 
- * the http format this is serialized to is an ascii string consisting of
- * ';'-delimited tokens.  The first token is the client GUID represented in hex
- * and is the only required token.  The other tokens can be addresses of push proxies
- * or various feature headers.  At most one of the tokens should be the external ip and port 
- * of the firewalled node in a port:ip format. Currently the only feature header we 
- * parse is the fwawt header that contains the version number of the firewall to 
- * firewall transfer protocol supported by the altloc.  In addition, the 'pptls=' field
- * can indicate which, if any, push proxies support TLS.  If the field is present, it 
- * must be immediately before the listing of the push proxies.  The hexadecimal string
- * after the '=' is a bit-representation of which push proxies are valid for TLS.
- * 
- * A PE does not need to know the actual external address of the firewalled host,
- * however without that knowledge we cannot do firewall-to-firewall transfer with 
- * the given host.  Also, the RemoteFileDesc objects requires a valid IP for construction,
- * so in the case we do not know the external address we return a BOGUS_IP.
- * 
- * Examples:
- * 
- *  //altloc with 2 proxies and supports firewall transfer 1 :
- * 
- * <ThisIsTheGUIDASDF>;fwt/1.0;20.30.40.50:60;1.2.3.4:5567
- * 
- *   //altloc with 1 proxy and doesn't support firewall transfer, with external address:
- * 
- * <ThisIsTHeGUIDasfdaa527>;1.2.3.4:5564;6346:2.3.4.5
- * 
- * //altloc with 1 proxy and supports two features we don't know/care about :
- * 
- * <ThisIsTHeGUIDasfdaa527>;someFeature/3.2;10.20.30.40:5564;otherFeature/0.4
- * 
- * //altloc with 1 proxy (the first) that's TLS capable, 1 that isn't:
- * 
- * <ThisIsTheGUIDASDF>;fwt/1.0;pptls=8;20.30.40.50:60;1.2.3.4:5567
- * 
- *  //altloc without any proxies and doesn't support any features
- *  // not very useful, but still valid  
- * 
- * <ThisIsTheGUIDasdf23457>
- */
 public class PushEndpointImpl extends AbstractPushEndpoint {
     /**
 	 * the client guid of the endpoint
