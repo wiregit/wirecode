4a730bea56539c54494f0a8af6e63ea914e4bb8a
meverett <meverett>
2008-05-02 20:48:28 +0000

    build fix

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/stubs/HTTPConnectObserverStub.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/stubs/HTTPConnectObserverStub.java
@@ -6,7 +6,6 @@ import com.limegroup.gnutella.downloader.HTTPConnectObserver;
 
 public class HTTPConnectObserverStub extends HTTPConnectObserver {
 
-    @Override
     public void handleConnect(Socket socket) {
     }
 
