2ff3f79ff25ef57e2de2fabb3774cbd386c58e96
sberlin <sberlin>
2008-07-08 14:46:21 +0000

    cleanup AppFrame.properties

--- private-components/swingui/src/main/java/org/limewire/ui/swing/mainframe/AppFrame.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/mainframe/AppFrame.java
@@ -8,9 +8,7 @@ public class AppFrame extends SingleFrameApplication {
     
     @Override
     protected void startup() {
-        getMainFrame().setTitle("LimeWire");
-        getMainFrame().setJMenuBar(new LimeMenuBar());
-        
+        getMainFrame().setJMenuBar(new LimeMenuBar());        
         LimeWireSwingUI ui = new LimeWireSwingUI();
         show(ui);
         ui.goHome();
