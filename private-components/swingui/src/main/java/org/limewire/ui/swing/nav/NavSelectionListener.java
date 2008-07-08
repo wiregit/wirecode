da0424e10be28a79a9397fc738d5fd008f4efb21
cvs2svn <admin@example.com>
2008-07-08 18:07:29 +0000

    This commit was manufactured by cvs2svn to create tag 'fileManager- listeners-before'.

--- private-components/swingui/src/main/java/org/limewire/ui/swing/nav/NavSelectionListener.java
+++
@@ -1,8 +0,0 @@
-package org.limewire.ui.swing.nav;
-
-
-public interface NavSelectionListener {
-    
-    public void navItemSelected(Navigator.NavItem target, String name);
-
-}
--- private-components/swingui/src/main/java/org/limewire/ui/swing/search/SearchHandler.java
+++
@@ -1,5 +0,0 @@
-package org.limewire.ui.swing.search;
-
-public interface SearchHandler {
-
-}
