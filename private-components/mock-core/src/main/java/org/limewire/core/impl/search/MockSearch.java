462ff6e2df775b5d5b1be4615f9c5a9d72bcded1
sberlin <sberlin>
2008-07-14 04:43:22 +0000

    get mock search compiling again.

--- private-components/mock-core/src/main/java/org/limewire/core/impl/search/MockSearch.java
+++ private-components/mock-core/src/main/java/org/limewire/core/impl/search/MockSearch.java
@@ -1,8 +1,13 @@
 package org.limewire.core.impl.search;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
+import org.limewire.core.api.endpoint.RemoteHost;
+import org.limewire.core.api.endpoint.RemoteHostAction;
 import org.limewire.core.api.search.ResultType;
 import org.limewire.core.api.search.Search;
 import org.limewire.core.api.search.SearchDetails;
@@ -14,11 +19,11 @@ public class MockSearch implements Search {
     public MockSearch(SearchDetails searchDetails) {
         // TODO Auto-generated constructor stub
     }
-    
+
     @Override
     public void stop() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
@@ -26,129 +31,90 @@ public class MockSearch implements Search {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                searchListener.handleSearchResult(new SearchResult() {
-                    @Override
-                    public Map<Object, Object> getProperties() {
-                        return Collections.emptyMap();
-                    }
-                    @Override
-                    public String getUrn() {
-                        return "test";
-                    }
-                    
-                    @Override
-                    public ResultType getResultType() {
-                        return ResultType.AUDIO;
-                    }
-                    
-                    @Override
-                    public String getDescription() {
-                        return "5.0 Blues";
-                    }
-                    
-                    @Override
-                    public String getFileExtension() {
-                        return "ogg";
-                    }
-                    
-                    @Override
-                    public long getSize() {
-                        return 123456789L;
-                    }
-                });
-                searchListener.handleSearchResult(new SearchResult() {
-                    @Override
-                    public Map<Object, Object> getProperties() {
-                        return Collections.emptyMap();
-                    }
-                    @Override
-                    public String getUrn() {
-                        return "test";
-                    }
-                    
-                    @Override
-                    public String getDescription() {
-                        return "5.0 Blues Source 2";
-                    }
-                    
-                    @Override
-                    public ResultType getResultType() {
-                        return ResultType.AUDIO;
-                    }
-
-                    @Override
-                    public String getFileExtension() {
-                        return "ogg";
-                    }
-                    
-                    @Override
-                    public long getSize() {
-                        return 123456789L;
-                    }
-                });
-                searchListener.handleSearchResult(new SearchResult() {
-                    @Override
-                    public Map<Object, Object> getProperties() {
-                        return Collections.emptyMap();
-                    }
-                    @Override
-                    public String getUrn() {
-                        return "test1";
-                    }
-                    
-                    @Override
-                    public String getDescription() {
-                        return "Standdown Situps";
-                    }
-                    
-                    @Override
-                    public ResultType getResultType() {
-                        return ResultType.VIDEO;
-                    }
-                    
-                    @Override
-                    public String getFileExtension() {
-                        return "ogv";
-                    }
-                    
-                    @Override
-                    public long getSize() {
-                        return 123456L;
-                    }
-                });
-                searchListener.handleSearchResult(new SearchResult() {
-                    @Override
-                    public Map<Object, Object> getProperties() {
-                        return Collections.emptyMap();
-                    }
-                    @Override
-                    public String getUrn() {
-                        return "test2";
-                    }
-                    
-                    @Override
-                    public String getDescription() {
-                        return "Craziness";
-                    }
-                    
-                    @Override
-                    public ResultType getResultType() {
-                        return ResultType.UNKNOWN;
-                    }
-                    
-                    @Override
-                    public String getFileExtension() {
-                        return "tmp";
-                    }
-                    
-                    @Override
-                    public long getSize() {
-                        return 1L;
-                    }
-                });
+                searchListener.handleSearchResult(
+                        new MockSearchResult("5.0. Blues", "ogg", ResultType.AUDIO, 123456789L, Arrays.asList(new MockRemoteHost("bob")), "test"));
+                searchListener.handleSearchResult(
+                        new MockSearchResult("5.0 Blues Source 2", "ogg", ResultType.AUDIO, 1234789L, Arrays.asList(new MockRemoteHost("tom"), new MockRemoteHost("bob")), "test"));
+                searchListener.handleSearchResult(
+                        new MockSearchResult("Standdown Situps", "ogv", ResultType.VIDEO, 123456L, Arrays.asList(new MockRemoteHost("dick")), "test1"));
+                searchListener.handleSearchResult(
+                        new MockSearchResult("Craziness", "tmp", ResultType.UNKNOWN, 1L, Arrays.asList(new MockRemoteHost("harry"), new MockRemoteHost("larry")), "test2"));
             }
         }).start();
 
+    }
+
+    private static class MockSearchResult implements SearchResult {
+        private final String description;
+        private final String extension;
+        private final ResultType resultType;
+        private final long size;
+        private final List<RemoteHost> sources;
+        private final String urn;
+
+        public MockSearchResult(String description, String extension,
+                ResultType resultType, long size, List<? extends RemoteHost> sources,
+                String urn) {
+            this.description = description;
+            this.extension = extension;
+            this.resultType = resultType;
+            this.size = size;
+            this.sources = new ArrayList<RemoteHost>(sources);
+            this.urn = urn;
+        }
+
+        @Override
+        public String getDescription() {
+            return description;
+        }
+
+        @Override
+        public String getFileExtension() {
+            return extension;
+        }
+
+        @Override
+        public Map<Object, Object> getProperties() {
+            return Collections.emptyMap();
+        }
+
+        @Override
+        public ResultType getResultType() {
+            return resultType;
+        }
+
+        @Override
+        public long getSize() {
+            return size;
+        }
+
+        @Override
+        public List<RemoteHost> getSources() {
+            return sources;
+        }
+
+        @Override
+        public String getUrn() {
+            return urn;
+        }
+    }
+
+    private static class MockRemoteHost implements RemoteHost {
+        private final String description;
+
+        public MockRemoteHost(String description) {
+            this.description = description;
+        }
+
+        @Override
+        public List<RemoteHostAction> getHostActions() {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public String getHostDescription() {
+            return description;
+        }
     }
 
 }
