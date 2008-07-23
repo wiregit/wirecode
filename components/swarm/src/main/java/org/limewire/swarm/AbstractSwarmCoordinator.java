310ba2ff5ca62289e4c40348bdcf4c86d50dc1bc
pvertenten <pvertenten>
2008-07-23 15:28:37 +0000

    Added comments for the AbstractSwarmCoordinator.

--- components/swarm/src/main/java/org/limewire/swarm/AbstractSwarmCoordinator.java
+++ components/swarm/src/main/java/org/limewire/swarm/AbstractSwarmCoordinator.java
@@ -1,14 +1,27 @@
 package org.limewire.swarm;
 
+/**
+ * Abstract Swarm Coordinator that will hold some common functionality accross Coordinators.
+ * 
+ * @author pvertenten
+ */
 public abstract class AbstractSwarmCoordinator implements SwarmCoordinator {
     
     /** List of listeners. */
     private final SwarmCoordinatorListenerList listeners = new SwarmCoordinatorListenerList(this);
 
+    
+    /**
+     * Adds a Listener to this Coordinator.
+     */
     public void addListener(SwarmCoordinatorListener swarmListener) {
         listeners.add(swarmListener);
     }
 
+    /**
+     * Returns the listeners for this coordinator.
+     * @return
+     */
     protected SwarmCoordinatorListenerList listeners() {
         return listeners;
     }
