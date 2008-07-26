7537f41e71763912c1bc237d25dc99eb66a79ace
tjulien <tjulien>
2008-07-26 05:58:34 +0000

    xmpp gui placeholders

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/net/address/gnutella/PushProxyAddress.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/net/address/gnutella/PushProxyAddress.java
@@ -3,7 +3,7 @@ package com.limegroup.gnutella.net.address.gnutella;
 import org.limewire.net.address.DirectConnectionAddress;
 
 /**
- * Represent the connection information for a pusy proxy
+ * Represent the connection information for a push proxy
  */
 public interface PushProxyAddress extends DirectConnectionAddress {
 }
