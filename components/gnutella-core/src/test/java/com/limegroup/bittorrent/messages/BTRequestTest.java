165eef5a27feceab21285c4cf45f6461e724fa44
pvertenten <pvertenten>
2008-08-01 20:00:27 +0000

    Added unit test for BTRequest object.

---
+++ components/gnutella-core/src/test/java/com/limegroup/bittorrent/messages/BTRequestTest.java
@@ -0,0 +1,38 @@
+package com.limegroup.bittorrent.messages;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import junit.framework.Assert;
+
+import org.limewire.util.BaseTestCase;
+
+import com.limegroup.bittorrent.BTInterval;
+
+public class BTRequestTest extends BaseTestCase {
+
+    public BTRequestTest(String name) {
+        super(name);
+    }
+
+    public void testBTCancel() {
+        int startIndex = 0;
+        int endIndex = 16;
+        int pieceIndex = 0;
+        BTInterval btInterval = new BTInterval(startIndex, endIndex, pieceIndex);
+        BTRequest btRequest = new BTRequest(btInterval);
+        Assert.assertEquals(BTMessage.REQUEST, btRequest.getType());
+        Assert.assertEquals(btInterval, btRequest.getInterval());
+        Assert.assertTrue(btRequest.isUrgent());
+        Assert.assertEquals(12, btRequest.getPayload().limit());
+        ByteBuffer testBuffer = ByteBuffer.allocate(12);
+        testBuffer.order(ByteOrder.BIG_ENDIAN);
+        testBuffer.putInt(pieceIndex);
+        testBuffer.putInt(startIndex);
+        int length = endIndex - startIndex + 1;
+        testBuffer.putInt(length);
+        testBuffer.clear();
+        Assert.assertEquals(testBuffer, btRequest.getPayload());
+
+    }
+}
