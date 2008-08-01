b608436e6037eb22f1725b68ede9323fc8c04084
pvertenten <pvertenten>
2008-08-01 20:20:34 +0000

    Added BTHave and BTPieceMessage tests.

---
+++ components/gnutella-core/src/test/java/com/limegroup/bittorrent/messages/BTHaveTest.java
@@ -0,0 +1,28 @@
+package com.limegroup.bittorrent.messages;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import junit.framework.Assert;
+
+import org.limewire.util.BaseTestCase;
+
+public class BTHaveTest extends BaseTestCase {
+
+    public BTHaveTest(String name) {
+        super(name);
+    }
+
+    public void testBTHave() {
+        int pieceNum = 1;
+        BTHave btHave = new BTHave(pieceNum);
+        Assert.assertEquals(BTMessage.HAVE, btHave.getType());
+        Assert.assertEquals(pieceNum, btHave.getPieceNum());
+        ByteBuffer testPayload = ByteBuffer.allocate(4);
+        testPayload.order(ByteOrder.BIG_ENDIAN);
+        testPayload.putInt(pieceNum);
+        testPayload.clear();
+        Assert.assertEquals(testPayload, btHave.getPayload());
+    }
+
+}
