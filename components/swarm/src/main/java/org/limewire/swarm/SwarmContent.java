429859e358c0dc4f05c078e24c7f47287087e8c0
cvs2svn <admin@example.com>
2008-08-01 19:57:37 +0000

    This commit was manufactured by cvs2svn to create tag 'bittorrent-webseed- branch-pre-refactoring'.

--- components/swarm/src/main/java/org/limewire/swarm/SwarmContent.java
+++
@@ -1,8 +0,0 @@
-package org.limewire.swarm;
-
-import java.io.IOException;
-import java.nio.ByteBuffer;
-
-public interface SwarmContent {
-    public int read(ByteBuffer byteBuffer) throws IOException;
-}
