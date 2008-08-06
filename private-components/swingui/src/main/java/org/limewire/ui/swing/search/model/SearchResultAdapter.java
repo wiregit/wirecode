998180fd83cc248ca3a5da8eeaf6db73e7addddc
mvolkmann <mvolkmann>
2008-08-06 14:19:25 +0000

    added equals and hashCode methods, generated by Eclipse

--- private-components/swingui/src/main/java/org/limewire/ui/swing/search/model/SearchResultAdapter.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/model/SearchResultAdapter.java
@@ -20,24 +20,37 @@ class SearchResultAdapter implements VisualSearchResult {
 
     public SearchResultAdapter(List<SearchResult> sourceValue) {
         this.coreResults = sourceValue;
-        this.remoteHosts = new TreeSet<RemoteHost>(new Comparator<RemoteHost>() {
+        
+        this.remoteHosts =
+            new TreeSet<RemoteHost>(new Comparator<RemoteHost>() {
             @Override
             public int compare(RemoteHost o1, RemoteHost o2) {
-                return o1.getHostDescription().compareToIgnoreCase(o2.getHostDescription());
+                return o1.getHostDescription().compareToIgnoreCase(
+                    o2.getHostDescription());
             }
         });
+        
         update();
     }
 
-    void update() {
-        for(SearchResult result : coreResults) {
-            remoteHosts.addAll(result.getSources());
-        }
+    @Override
+    public ResultType getCategory() {
+        return coreResults.get(0).getResultType();
     }
     
     @Override
     public List<SearchResult> getCoreSearchResults() {
         return coreResults;
+    }
+    
+    @Override
+    public String getDescription() {
+        return coreResults.get(0).getDescription();
+    }
+    
+    @Override
+    public String getFileExtension() {
+        return coreResults.get(0).getFileExtension();
     }
     
     @Override
@@ -55,26 +68,6 @@ class SearchResultAdapter implements VisualSearchResult {
     }
     
     @Override
-    public String toString() {
-        return getDescription() + " with " + getSources().size() + " sources, in category: " + getCategory() + ", with size: " + getSize() + ", and extension: " + getFileExtension();
-    }
-    
-    @Override
-    public ResultType getCategory() {
-        return coreResults.get(0).getResultType();
-    }
-    
-    @Override
-    public String getDescription() {
-        return coreResults.get(0).getDescription();
-    }
-    
-    @Override
-    public String getFileExtension() {
-        return coreResults.get(0).getFileExtension();
-    }
-    
-    @Override
     public long getSize() {
         return coreResults.get(0).getSize();
     }
@@ -83,5 +76,48 @@ class SearchResultAdapter implements VisualSearchResult {
     public Collection<RemoteHost> getSources() {
         return remoteHosts;
     }
+    
+    @Override
+    public String toString() {
+        return getDescription() +
+            " with " + getSources().size() + " sources, " +
+            "in category: " + getCategory() +
+            ", with size: " + getSize() +
+            ", and extension: " + getFileExtension();
+    }
 
+    void update() {
+        for (SearchResult result : coreResults) {
+            remoteHosts.addAll(result.getSources());
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result +
+            ((coreResults == null) ? 0 : coreResults.hashCode());
+        result = prime * result +
+            ((remoteHosts == null) ? 0 : remoteHosts.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (!(obj instanceof SearchResultAdapter)) return false;
+        
+        final SearchResultAdapter other = (SearchResultAdapter) obj;
+        if (coreResults == null) {
+            if (other.coreResults != null) return false;
+        } else if (!coreResults.equals(other.coreResults)) return false;
+        
+        if (remoteHosts == null) {
+            if (other.remoteHosts != null) return false;
+        } else if (!remoteHosts.equals(other.remoteHosts)) return false;
+        
+        return true;
+    }
 }
