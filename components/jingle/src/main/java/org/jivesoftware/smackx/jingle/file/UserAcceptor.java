0da52a099ee465641bbf8fdd3a75510ff6172805
sberlin <sberlin>
2008-08-08 20:03:26 +0000

    remove jingle code & references.

--- components/jingle/src/main/java/org/jivesoftware/smackx/jingle/file/UserAcceptor.java
+++
@@ -1,7 +0,0 @@
-package org.jivesoftware.smackx.jingle.file;
-
-import org.jivesoftware.smackx.packet.file.FileDescription;
-
-public interface UserAcceptor {
-    public boolean userAccepts(FileDescription.FileContainer file);
-}
--- components/jingle/src/main/java/org/jivesoftware/smackx/jingle/states/State.java
+++
@@ -1,8 +0,0 @@
-package org.jivesoftware.smackx.jingle.states;
-
-import org.jivesoftware.smackx.packet.Jingle;
-import org.jivesoftware.smack.XMPPException;
-
-public interface State {
-    State dispatch(Jingle jin) throws XMPPException;
-}
