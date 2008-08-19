b4d84d6d7bab9c6e6441bf6cf5a4c1bba6b3e397
tjulien <tjulien>
2008-08-19 18:16:41 +0000

    fixed xmpp mock module

--- private-components/mock-core/src/main/java/org/limewire/core/impl/xmpp/MockXmppModule.java
+++ private-components/mock-core/src/main/java/org/limewire/core/impl/xmpp/MockXmppModule.java
@@ -1,14 +1,23 @@
 package org.limewire.core.impl.xmpp;
 
 import org.limewire.xmpp.api.client.XMPPService;
+import org.limewire.xmpp.api.client.RosterEvent;
+import org.limewire.listener.EventMulticaster;
+import org.limewire.listener.EventMulticasterImpl;
+import org.limewire.listener.EventListener;
+import org.limewire.listener.ListenerSupport;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.TypeLiteral;
 
 public class MockXmppModule extends AbstractModule {
     
     @Override
     protected void configure() {
         bind(XMPPService.class).to(MockXmppService.class);
+        EventMulticaster<RosterEvent> rosterMulticaster = new EventMulticasterImpl<RosterEvent>(); 
+        bind(new TypeLiteral<EventListener<RosterEvent>>(){}).toInstance(rosterMulticaster);
+        bind(new TypeLiteral<ListenerSupport<RosterEvent>>(){}).toInstance(rosterMulticaster);
     }
 
 }
