17ef19e5ce86ad393bf1ec23243364f9911827cb
pvertenten <pvertenten>
2008-08-19 14:38:12 +0000

    Cleaned up comments.

--- components/swarm/src/main/java/org/limewire/swarm/SwarmFileSystem.java
+++ components/swarm/src/main/java/org/limewire/swarm/SwarmFileSystem.java
@@ -15,11 +15,6 @@ public interface SwarmFileSystem {
     /**
      * Writes data from the byteBuffer to the fileSystem, starting at 'start'.
      * Writes as much as the has available, and returns the amount written.
-     * 
-     * @param byteBuffer
-     * @param start
-     * @return
-     * @throws IOException
      */
     long write(ByteBuffer byteBuffer, long start) throws IOException;
 
@@ -27,55 +22,36 @@ public interface SwarmFileSystem {
      * Reads data from the fileSystem starting at 'start' into the given buffer.
      * Reads as much as is available or will fit into the buffer, and returns
      * the amount read.
-     * 
-     * @param byteBuffer
-     * @param start
-     * @return
-     * @throws IOException
      */
     long read(ByteBuffer byteBuffer, long start) throws IOException;
 
     /**
      * Closes this filesystem, cleaning up resources used for reading/writing.
-     * 
-     * @throws IOException
      */
     void close() throws IOException;
 
     /**
      * Closes the given swarmFile for writing/reading.
-     * 
-     * @param swarmFile
-     * @throws IOException
      */
     void closeSwarmFile(SwarmFile swarmFile) throws IOException;
 
     /**
      * Initializes this filesystem for use.
-     * 
-     * @throws IOException
      */
     void initialize() throws IOException;
 
     /**
      * Returns the complete size of this filesystem. All files contained within.
-     * 
-     * @return
      */
     long getCompleteSize();
 
     /**
-     * Retrieves the swarm file located at the given position.
-     * 
-     * @param position
-     * @return
+     * Retrieves the swarm file located at the given position. null if no such file exists.
      */
     SwarmFile getSwarmFile(long position);
 
     /**
      * Returns the SwarmFiles represented by this file system.
-     * 
-     * @return
      */
     List<SwarmFile> getSwarmFiles();
 
@@ -83,7 +59,6 @@ public interface SwarmFileSystem {
      * Returns the SwarmFiles which are a part of the given range.
      * 
      * @param range range to get the files from.
-     * @return
      */
     List<SwarmFile> getSwarmFiles(Range range);
 
