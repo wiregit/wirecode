aba1bbf441675da380450e9b24f4697493fd5040
maquino <maquino>
2008-08-22 14:34:16 +0000

    Updated conversation display to better match the spec.  Got rid of chat bubbles.

--- private-components/swingui/src/main/java/org/limewire/ui/swing/friends/ChatDocumentBuilder.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/friends/ChatDocumentBuilder.java
@@ -1,6 +1,5 @@
 package org.limewire.ui.swing.friends;
 
-import java.net.URL;
 import java.util.ArrayList;
 
 import org.limewire.ui.swing.friends.Message.Type;
@@ -14,54 +13,24 @@ class ChatDocumentBuilder {
         "<html>" +
             "<head>" +
                 "<style>" +
-                    "body{background-color: #E8E8E8}" +
+                    "body { " +
+                        "background-color: #ffffff;" +
+                        "margin: 0 4px;}" +
                     ".me { " +
                         "color: #0D3065;" +
-                        "margin: 0 8px;" +
+                        "font-size: 105%;" +
                         "font-weight: bold;}" +
                     ".them { " +
                         "color: #771324;" + 
-                        "margin: 0 8px;" +
+                        "font-size: 105%;" +
                         "font-weight: bold;}" +
-                    ".roundcont {" +
-                        "width: 98%;" +
-                        "background-color: #ffffff;" +
-                        "color: #000;}" +
-                    ".roundcont p {" +
-                        "margin: 0 8px;}" +
-                    ".roundtop {" + 
-                        "background: url(" + getURL("/org/limewire/ui/swing/mainframe/resources/icons/friends/top-right.png") +") no-repeat top right;}" +
-                    ".roundbottom {" +
-                        "background: url(" + getURL("/org/limewire/ui/swing/mainframe/resources/icons/friends/bottom-right.png") + ") no-repeat top right;}" +
-                    "img.corner {" +
-                       "border: none;" +
-                       "display: block;" +
-                    "}" +
                 "</style>" +
             "</head>" +
             "<body>";
     
-    private static String ROUND_CONTAINER_TOP = 
-        "<div class=\"roundcont\">" +
-            "<div class=\"roundtop\">" +
-              "<img src=\"" + getURL("/org/limewire/ui/swing/mainframe/resources/icons/friends/top-left.png") + "\" class=\"corner\" style=\"display: none\" />" +
-            "</div>";
-    
-    private static String ROUND_CONTAINER_BOTTOM =
-            "<div class=\"roundbottom\">" +
-              "<img src=\"" + getURL("/org/limewire/ui/swing/mainframe/resources/icons/friends/bottom-left.png") + "\" class=\"corner\" style=\"display: none\" />" +
-            "</div>" +
-        "</div>" +
-        "<br/>";
-
     private static String BOTTOM = 
         "</body>" +
         "</html>";
-    
-    private static String getURL(String path) {
-        URL resource = ChatDocumentBuilder.class.getResource(path);
-        return resource != null ? resource.toExternalForm() : "";
-    }
     
     public static String buildChatText(ArrayList<Message> messages) {
         StringBuilder builder = new StringBuilder();
@@ -70,18 +39,15 @@ class ChatDocumentBuilder {
         for(int i = 0; i < messages.size(); i++) {
             Message message = messages.get(i);
             if(!isPreviousFromSameSender(message, messages, i)) {
-                builder.append(ROUND_CONTAINER_TOP);
                 builder.append("<div class=\"")
                 .append(message.getType() == Type.Sent ? "me" : "them")
                 .append("\">")
                 .append(message.getSenderName())
                 .append(":</div>");
             }
-            builder.append("<p>")
-            .append(message.getMessageText())
-            .append("</p>");
+            builder.append(message.getMessageText());
             
-            builder.append(isNextFromSameSender(message, messages, i) ? "" : ROUND_CONTAINER_BOTTOM);
+            builder.append("<br/>").append(isNextFromSameSender(message, messages, i) ? "" : "<br/>");
         }
         
         builder.append(BOTTOM);
