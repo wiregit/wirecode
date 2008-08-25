731e0b47f89497a89982af36824d0763a9ee1808
pvertenten <pvertenten>
2008-08-25 19:47:35 +0000

    Fixed NorViewTest to test the NorView.

--- components/collection/src/test/java/org/limewire/collection/NorViewTest.java
+++ components/collection/src/test/java/org/limewire/collection/NorViewTest.java
@@ -10,40 +10,46 @@ public class NorViewTest extends BaseTestCase {
     public NorViewTest(String name) {
         super(name);
     }
-    
-    public static Test suite() { 
-        return buildTestSuite(NorViewTest.class); 
-    } 
+
+    public static Test suite() {
+        return buildTestSuite(NorViewTest.class);
+    }
 
     /**
      * 
      * <pre>
      * Output: bf1: 1100 
-     *         av:  0011
+     *         bf2: 1010 
+     *         av:  0001
      * </pre>
      */
     public void testBasic() {
         BitSet bitSet1 = new BitSet(4);
         bitSet1.flip(0);
         bitSet1.flip(1);
-  
+        
+        BitSet bitSet2 = new BitSet(4);
+        bitSet2.flip(0);
+        bitSet2.flip(2);
+
         BitField bitField1 = new BitFieldSet(bitSet1, 4);
- 
-        NotView notView = new NotView(bitField1);
-        Assert.assertEquals(false, notView.get(0));
-        Assert.assertEquals(false, notView.get(1));
-        Assert.assertEquals(true, notView.get(2));
-        Assert.assertEquals(true, notView.get(3));
-        
-        Assert.assertEquals(2, notView.nextSetBit(0));
-        Assert.assertEquals(2, notView.nextSetBit(1));
-        Assert.assertEquals(2, notView.nextSetBit(2));
-        Assert.assertEquals(3, notView.nextSetBit(3));
-        
-        Assert.assertEquals(0, notView.nextClearBit(0));
-        Assert.assertEquals(1, notView.nextClearBit(1));
-        Assert.assertEquals(-1, notView.nextClearBit(2));
-        Assert.assertEquals(-1, notView.nextClearBit(3));
+        BitField bitField2 = new BitFieldSet(bitSet2, 4);
+
+        NorView norView = new NorView(bitField1, bitField2);
+        Assert.assertEquals(false, norView.get(0));
+        Assert.assertEquals(false, norView.get(1));
+        Assert.assertEquals(false, norView.get(2));
+        Assert.assertEquals(true, norView.get(3));
+
+        Assert.assertEquals(3, norView.nextSetBit(0));
+        Assert.assertEquals(3, norView.nextSetBit(1));
+        Assert.assertEquals(3, norView.nextSetBit(2));
+        Assert.assertEquals(3, norView.nextSetBit(3));
+
+        Assert.assertEquals(0, norView.nextClearBit(0));
+        Assert.assertEquals(1, norView.nextClearBit(1));
+        Assert.assertEquals(2, norView.nextClearBit(2));
+        Assert.assertEquals(-1, norView.nextClearBit(3));
     }
-  
+
 }
