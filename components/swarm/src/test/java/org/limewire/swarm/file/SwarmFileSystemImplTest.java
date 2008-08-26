1c8fbad6703abd52fb264a4d4be3e4f77ad5f790
pvertenten <pvertenten>
2008-08-26 17:24:24 +0000

    Cleaned up test cases to not have to call delete explicity. broke dependency on testRead1 from testWrite1.

--- components/swarm/src/test/java/org/limewire/swarm/file/SwarmFileSystemImplTest.java
+++ components/swarm/src/test/java/org/limewire/swarm/file/SwarmFileSystemImplTest.java
@@ -5,27 +5,39 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 
 import junit.framework.Assert;
-import junit.framework.TestCase;
 
+import org.limewire.util.BaseTestCase;
 import org.limewire.util.FileUtils;
 
-public class SwarmFileSystemImplTest extends TestCase {
+public class SwarmFileSystemImplTest extends BaseTestCase {
 
-    public void testGetCompleteSize() {
+    private final File TEST_DIR;
+
+    public SwarmFileSystemImplTest(String name) {
+        super(name);
+        this.TEST_DIR = new File(System.getProperty("java.io.tmpdir") + "/limetests/");
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        FileUtils.deleteRecursive(TEST_DIR);
+    }
+
+    public void testGetCompleteSize() throws Exception {
         SwarmFileSystemImpl swarmFileSystem = new SwarmFileSystemImpl();
         File file1 = createFile("testGetCompleteSize.1.txt");
-        file1.delete();
         swarmFileSystem.addSwarmFile(new SwarmFileImpl(file1, 1024));
         Assert.assertEquals(1024, swarmFileSystem.getCompleteSize());
         File file2 = createFile("testGetCompleteSize.2.txt");
-        file2.delete();
         swarmFileSystem.addSwarmFile(new SwarmFileImpl(file2, 1023));
+        swarmFileSystem.close();
         Assert.assertEquals(2047, swarmFileSystem.getCompleteSize());
     }
 
     private File createFile(String fileName) {
-        File file = new File(System.getProperty("java.io.tmpdir") + "/limetests/" + fileName);
-        file.mkdirs();
+        TEST_DIR.mkdirs();
+        File file = new File(TEST_DIR.getAbsolutePath() + "/" + fileName);
+        file.delete();
         file.deleteOnExit();
         return file;
     }
@@ -33,7 +45,6 @@ public class SwarmFileSystemImplTest extends TestCase {
     public void testWrite1() throws IOException {
         SwarmFileSystemImpl swarmFileSystem = new SwarmFileSystemImpl();
         File file1 = createFile("testWrite.1.txt");
-        file1.delete();
         swarmFileSystem.addSwarmFile(new SwarmFileImpl(file1, 1024));
 
         String testWrite1 = "testWrite File 1";
@@ -41,6 +52,7 @@ public class SwarmFileSystemImplTest extends TestCase {
         byteBuffer.put(testWrite1.getBytes());
         byteBuffer.flip();
         swarmFileSystem.write(byteBuffer, 0);
+        swarmFileSystem.close();
         Assert.assertTrue(file1.exists());
         Assert.assertEquals(testWrite1.length(), file1.length());
         Assert.assertEquals(testWrite1, new String(FileUtils.readFileFully(file1)));
@@ -48,61 +60,67 @@ public class SwarmFileSystemImplTest extends TestCase {
     }
 
     public void testRead1() throws IOException {
-        SwarmFileSystemImpl swarmFileSystem = new SwarmFileSystemImpl();
-        File file1 = createFile("testWrite.1.txt");
-        swarmFileSystem.addSwarmFile(new SwarmFileImpl(file1, 1024));
-        
-        String testWrite1 = "testWrite File 1";
-        ByteBuffer byteBuffer = ByteBuffer.allocate(testWrite1.length());
-        swarmFileSystem.read(byteBuffer, 0);
-        Assert.assertTrue(file1.exists());
-        Assert.assertEquals(testWrite1.length(), byteBuffer.position());
-        Assert.assertEquals(testWrite1, new String(byteBuffer.array()));
-    }
-    
-    public void testWriteMulti() throws IOException {
-        SwarmFileSystemImpl swarmFileSystem = new SwarmFileSystemImpl();
-        File file1 = createFile("testWriteMulti.1.txt");
-        file1.delete();
-        swarmFileSystem.addSwarmFile(new SwarmFileImpl(file1, 10));
-        
-        File file2 = createFile("testWriteMulti.2.txt");
-        file2.delete();
-        swarmFileSystem.addSwarmFile(new SwarmFileImpl(file2, 6));
-        
-        
 
+        SwarmFileSystemImpl swarmFileSystem = new SwarmFileSystemImpl();
+        File fileWrite1 = createFile("testWrite.1.txt");
+        swarmFileSystem.addSwarmFile(new SwarmFileImpl(fileWrite1, 1024));
         String testWrite1 = "testWrite File 1";
         ByteBuffer byteBuffer = ByteBuffer.allocate(testWrite1.length());
         byteBuffer.put(testWrite1.getBytes());
         byteBuffer.flip();
         swarmFileSystem.write(byteBuffer, 0);
+        swarmFileSystem.close();
+
+        SwarmFileSystemImpl swarmFileSystem2 = new SwarmFileSystemImpl();
+        swarmFileSystem2.addSwarmFile(new SwarmFileImpl(fileWrite1, 1024));
+        byteBuffer.flip();
+        swarmFileSystem2.read(byteBuffer, 0);
+        swarmFileSystem2.close();
+        Assert.assertTrue(fileWrite1.exists());
+        Assert.assertEquals(testWrite1.length(), byteBuffer.position());
+        Assert.assertEquals(testWrite1, new String(byteBuffer.array()));
+    }
+
+    public void testWriteMulti() throws IOException {
+        SwarmFileSystemImpl swarmFileSystem = new SwarmFileSystemImpl();
+        File file1 = createFile("testWriteMulti.1.txt");
+        swarmFileSystem.addSwarmFile(new SwarmFileImpl(file1, 10));
+        File file2 = createFile("testWriteMulti.2.txt");
+        swarmFileSystem.addSwarmFile(new SwarmFileImpl(file2, 6));
+        String testWrite1 = "testWrite File 1";
+        ByteBuffer byteBuffer = ByteBuffer.allocate(testWrite1.length());
+        byteBuffer.put(testWrite1.getBytes());
+        byteBuffer.flip();
+        swarmFileSystem.write(byteBuffer, 0);
+        swarmFileSystem.close();
+
         Assert.assertTrue(file1.exists());
         Assert.assertEquals(10, file1.length());
-        Assert.assertEquals(testWrite1.substring(0, 10), new String(FileUtils.readFileFully(file1)));
+        Assert
+                .assertEquals(testWrite1.substring(0, 10), new String(FileUtils
+                        .readFileFully(file1)));
         Assert.assertTrue(file2.exists());
         Assert.assertEquals(6, file2.length());
-        Assert.assertEquals(testWrite1.substring(10, 16), new String(FileUtils.readFileFully(file2)));
+        Assert.assertEquals(testWrite1.substring(10, 16),
+                new String(FileUtils.readFileFully(file2)));
 
     }
-    
+
     public void testWriteMultiPartial() throws IOException {
         SwarmFileSystemImpl swarmFileSystem = new SwarmFileSystemImpl();
         File file1 = createFile("testWriteMultiPartial.1.txt");
-        file1.delete();
         swarmFileSystem.addSwarmFile(new SwarmFileImpl(file1, 10));
-        
+
         File file2 = createFile("testWriteMultiPartial.2.txt");
-        file2.delete();
         swarmFileSystem.addSwarmFile(new SwarmFileImpl(file2, 6));
-        
-        
 
         String testWrite1 = "File 1";
         ByteBuffer byteBuffer = ByteBuffer.allocate(testWrite1.length());
         byteBuffer.put(testWrite1.getBytes());
         byteBuffer.flip();
         swarmFileSystem.write(byteBuffer, 10);
+        swarmFileSystem.close();
+
         Assert.assertFalse(file1.exists());
         Assert.assertEquals(0, file1.length());
         Assert.assertTrue(file2.exists());
