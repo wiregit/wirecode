794602d08e204b5aa8e03f844f1a90b935b65e75
mvolkmann <mvolkmann>
2008-09-06 12:31:15 +0000

    initial version

--- private-components/swingui/src/main/java/org/limewire/ui/swing/search/resultpanel/SearchResultMenu.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/resultpanel/SearchResultMenu.java
@@ -1,16 +1,80 @@
 package org.limewire.ui.swing.search.resultpanel;
 
 import java.awt.Window;
-
+import java.awt.event.ActionEvent;
+import java.util.Collection;
+import javax.swing.AbstractAction;
+import javax.swing.JMenu;
 import javax.swing.JPopupMenu;
-
+import org.limewire.core.api.endpoint.RemoteHost;
 import org.limewire.ui.swing.search.model.VisualSearchResult;
 
-
-//TODO: remove this, temp fix for the build
+/**
+ * This class implements the menu that is displayed
+ * when the user right clicks a search result.
+ * 
+ * @author R. Mark Volkmann, Object Computing, Inc.
+ */
 public class SearchResultMenu extends JPopupMenu {
 
-    public SearchResultMenu(Window win, VisualSearchResult vsr) {
-        
+    public SearchResultMenu(Window owner, VisualSearchResult vsr) {
+        add(new AbstractAction("Download") {
+            public void actionPerformed(ActionEvent e) {
+                // TODO: RMV Implement this!
+            }
+        });
+
+        add(new AbstractAction("Mark as junk") {
+            public void actionPerformed(ActionEvent e) {
+                // TODO: RMV Implement this!
+            }
+        });
+
+        add(new AbstractAction("Copy link to clipboard") {
+            public void actionPerformed(ActionEvent e) {
+                // TODO: RMV Implement this!
+            }
+        });
+
+        addSeparator();
+
+        Collection<RemoteHost> sources = vsr.getSources();
+        RemoteHost[] hosts = sources.toArray(new RemoteHost[] {});
+        boolean multipleSources = sources.size() > 1;
+
+        if (multipleSources) {
+            for (RemoteHost host : hosts) {
+                addSubmenu(host);
+            }
+        } else {
+            RemoteHost host = hosts[0];
+            addSubmenu(host);
+        }
+
+        addSeparator();
+
+        add(new AbstractAction("Properties") {
+            public void actionPerformed(ActionEvent e) {
+                // TODO: RMV Implement this!
+            }
+        });
+    }
+
+    private void addSubmenu(RemoteHost host) {
+        JMenu menu = new JMenu(host.getHostDescription());
+
+        menu.add(new AbstractAction("View library") {
+            public void actionPerformed(ActionEvent e) {
+                // TODO: RMV Implement this!
+            }
+        });
+
+        menu.add(new AbstractAction("Chat") {
+            public void actionPerformed(ActionEvent e) {
+                // TODO: RMV Implement this!
+            }
+        });
+
+        add(menu);
     }
 }
