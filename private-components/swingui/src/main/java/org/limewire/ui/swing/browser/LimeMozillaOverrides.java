af77b27f029e9fd652a64adc14894394aca2a905
pvertenten <pvertenten>
2008-09-08 15:29:47 +0000

    Removing some unused code.

--- private-components/swingui/src/main/java/org/limewire/ui/swing/browser/LimeMozillaOverrides.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/browser/LimeMozillaOverrides.java
@@ -13,13 +13,13 @@ import com.google.inject.Singleton;
 @Singleton
 public class LimeMozillaOverrides {
 
-    
     @Inject
-   public LimeMozillaOverrides(LimeMozillaDownloadManagerListenerImpl downloadManagerListener) {
+    public LimeMozillaOverrides(LimeMozillaDownloadManagerListenerImpl downloadManagerListener) {
         overrideMozillaDefaults(downloadManagerListener);
     }
-    
-    private void overrideMozillaDefaults(LimeMozillaDownloadManagerListenerImpl mozillaDownloadManagerListener) {
+
+    private void overrideMozillaDefaults(
+            LimeMozillaDownloadManagerListenerImpl mozillaDownloadManagerListener) {
         // lookup the preferences service by contract id.
         // by getting a proxy we do not need to run code through mozilla thread.
         nsIPrefService prefService = XPCOMUtils.getServiceProxy(
@@ -34,22 +34,15 @@ public class LimeMozillaOverrides {
         prefService.getBranch("browser.download.").setCharPref("dir", downloadDir);
         prefService.getBranch("browser.download.manager.").setBoolPref("showWhenStarting", 0);
         prefService.getBranch("browser.download.manager.").setBoolPref("showAlertOnComplete", 0);
-        
+
         // setup which mime types do not prompt to download
         // this will prevent the save or open dialogue from prompting
         prefService.getBranch("browser.helperApps.neverAsk.").setCharPref("saveToDisk",
                 MozillaSettings.DOWNLOAD_MIME_TYPES.getValue());
 
-        // register our own download manager to replace the one provided by
-        // mozilla
-        //registerMozillaComponent(mozillaDownloadManager);
-        
-        nsIDownloadManager downloadManager = XPCOMUtils.getServiceProxy(LimeMozillaDownloadManager.NS_IDOWNLOADMANAGER_CID, nsIDownloadManager.class);
+        // adding the download listener
+        nsIDownloadManager downloadManager = XPCOMUtils.getServiceProxy(
+                LimeMozillaDownloadManager.NS_IDOWNLOADMANAGER_CID, nsIDownloadManager.class);
         downloadManager.addListener(mozillaDownloadManagerListener);
     }
-
-//    private void registerMozillaComponent(LimeMozillaSingletonFactory factory) {
-//        nsIComponentRegistrar cr = Mozilla.getInstance().getComponentRegistrar();
-//        cr.registerFactory(factory.getIID(), factory.getComponentName(), factory.getCID(), factory);
-//    }
 }
