5412b984ba8b075a910db5c528d7aed73f256245
cvs2svn <admin@example.com>
2008-09-16 20:59:09 +0000

    This commit was manufactured by cvs2svn to create tag 'swarmer-updates- branch-point'.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/downloader/ResumeDownloader.java
+++
@@ -1,9 +0,0 @@
-package com.limegroup.gnutella.downloader;
-
-import java.io.File;
-
-public interface ResumeDownloader extends ManagedDownloader {
-
-    public void initIncompleteFile(File incompleteFile, long size);
-
-}
--- components/gnutella-core/src/main/java/com/limegroup/gnutella/tigertree/ThexReaderFactory.java
+++
@@ -1,7 +0,0 @@
-package com.limegroup.gnutella.tigertree;
-
-public interface ThexReaderFactory {
-    
-    public ThexReader createHashTreeReader(String sha1, String root32, long fileSize);
-
-}
