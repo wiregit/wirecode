dfa969de60557c64960303165d903f035f6465b4
fberger <fberger>
2008-09-16 18:59:33 +0000

    LWC-1618: BrowseHostHandler: don't try direct connect if peer is marked as firewalled: Moved to branch.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/net/address/gnutella/PushProxyAddress.java
+++
@@ -1,9 +0,0 @@
-package com.limegroup.gnutella.net.address.gnutella;
-
-import org.limewire.net.address.DirectConnectionAddress;
-
-/**
- * Represent the connection information for a push proxy
- */
-public interface PushProxyAddress extends DirectConnectionAddress {
-}
