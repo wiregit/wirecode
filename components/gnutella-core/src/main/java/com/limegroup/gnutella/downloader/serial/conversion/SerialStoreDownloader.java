0c1960a5dd6a8691c3cd4fb229c261ad9de62065
cvs2svn <admin@example.com>
2008-09-16 20:59:10 +0000

    This commit was manufactured by cvs2svn to create branch 'swarmer-updates- branch'.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/downloader/serial/conversion/SerialStoreDownloader.java
+++
@@ -1,9 +0,0 @@
-package com.limegroup.gnutella.downloader.serial.conversion;
-
-class SerialStoreDownloader extends SerialManagedDownloaderImpl {
-    private static final long serialVersionUID = 1672575739103885243L;
-    
-    protected SerialStoreDownloader() {}
-    
-
-}
