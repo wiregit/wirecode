c44943ddf82a7c362404b87be349fb98e5f53a3e
meverett <meverett>
2008-09-17 14:44:56 +0000

    fixed transfer handler in library

--- private-components/swingui/src/main/java/org/limewire/ui/swing/library/table/LibraryTable.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/library/table/LibraryTable.java
@@ -1,15 +1,21 @@
 package org.limewire.ui.swing.library.table;
 
+import java.awt.datatransfer.Transferable;
 import java.awt.event.ActionEvent;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.JComponent;
 import javax.swing.JViewport;
 import javax.swing.ListSelectionModel;
+import javax.swing.TransferHandler;
 
+import org.jdesktop.swingx.JXTable;
 import org.limewire.core.api.library.FileItem;
+import org.limewire.core.api.library.FileTransferable;
 import org.limewire.core.api.library.LocalFileItem;
 import org.limewire.ui.swing.library.sharing.LibrarySharePanel;
 import org.limewire.ui.swing.table.MouseableTable;
@@ -44,24 +50,24 @@ public class LibraryTable<T extends FileItem> extends MouseableTable {
         setDragEnabled(true);
         
        
-//        setTransferHandler(new TransferHandler(){
-//            @Override
-//            public int getSourceActions(JComponent comp) {
-//                return COPY;
-//            }
-//            
-//            @Override
-//            public Transferable createTransferable(JComponent comp) {
-//                JXTable table = (JXTable) comp;
-//                int indices[] = table.getSelectedRows();
-//                List<File> files = new ArrayList<File>();
-//                for(int i = 0; i < indices.length; i++) {
-//                    FileItem item = ((LibraryTableModel)table.getModel()).getFileItem(indices[i]);
-//                    files.add(item.getFile());
-//                }
-//                return new FileTransferable(files);
-//            }
-//        });
+        setTransferHandler(new TransferHandler(){
+            @Override
+            public int getSourceActions(JComponent comp) {
+                return COPY;
+            }
+            
+            @Override
+            public Transferable createTransferable(JComponent comp) {
+                JXTable table = (JXTable) comp;
+                int indices[] = table.getSelectedRows();
+                List<File> files = new ArrayList<File>();
+                for(int i = 0; i < indices.length; i++) {
+                    LocalFileItem item = (LocalFileItem)((LibraryTableModel)table.getModel()).getFileItem(indices[i]);
+                    files.add(item.getFile());
+                }
+                return new FileTransferable(files);
+            }
+        });
     }
     
     public void enableSharing(LibrarySharePanel librarySharePanel){
