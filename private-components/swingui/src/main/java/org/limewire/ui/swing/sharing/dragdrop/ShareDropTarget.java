86c22e119bf08ccb8472931caa9431a3d21ea55d
meverett <meverett>
2008-09-22 16:02:22 +0000

    added folder to support the sharing drop operation

--- private-components/swingui/src/main/java/org/limewire/ui/swing/sharing/dragdrop/ShareDropTarget.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/sharing/dragdrop/ShareDropTarget.java
@@ -10,6 +10,7 @@ import java.awt.dnd.DropTargetDropEvent;
 import java.awt.dnd.DropTargetEvent;
 import java.awt.dnd.DropTargetListener;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.limewire.core.api.library.LocalFileList;
@@ -18,10 +19,30 @@ import org.limewire.ui.swing.util.BackgroundExecutorService;
 import org.limewire.util.FileUtils;
 import org.limewire.util.MediaType;
 
+/**
+ * A drop target for adding a drop listener to a component. The drop listener
+ * is specialized for adding files to shared lists. Unlike the library, Programs
+ * and Documents may be rejected based on the user settings. 
+ * 
+ * This drop target accepts a list of files or directories. Subdirectories within
+ * a folder drop are ignored. If a drop contains multiple file types or a folder
+ * with multiple subfile types, the drop is still accepted if it contains programs
+ * or documents. Only those files of acceptable type are accepted.
+ * 
+ * Currently no feedback is displayed if one or more files are not added to the list
+ * depsite the drop being accepted.
+ */
+//TODO: users recieve no feedback when program or documents files are dropped and rejected.
 public class ShareDropTarget implements DropTargetListener {
 
     private DropTarget dropTarget;
     private LocalFileList fileList;
+    
+    /**
+     * Boolean value to ignore the document sharing setting in SharingSettings.
+     * If true, SharingSettings.DOCUMENT_SHARING_ENABLED value is ignored, if
+     * false, SharingSettings.DOCUMENT_SHARING_ENABLED value is  heeded.
+     */
     private boolean alwaysShareDocuments;
     
     public ShareDropTarget(Component component, LocalFileList fileList) {
@@ -63,28 +84,37 @@ public class ShareDropTarget implements DropTargetListener {
 
             try {
                 final LocalFileList currentModel = fileList;
-                final List filesList = (List) transferable.getTransferData(DataFlavor.javaFileListFlavor);
-                //if drop contains a file type we aren't defaultly sharing, reject the drop
+                final List droppedFiles = (List) transferable.getTransferData(DataFlavor.javaFileListFlavor); 
                 
-                //TODO: this is still doing fileIO on the SwingThread. Its relatively light weight
-                // stuff but could cause problems.
-                for(int i = 0; i < filesList.size(); i++) {
-                    if(!isAllowed(FileUtils.getFileExtension(((File)filesList.get(i)).getName())))  
-                        dtde.rejectDrop();
-                }
+                final List<File> acceptedFiles = new ArrayList<File>();
+          
+                // perform the file IO operations on its own thread. 
+                //TODO: give feedback for failed adds.
                 BackgroundExecutorService.schedule(new Runnable(){
                     public void run() {
-                        for(int i = 0; i < filesList.size(); i++) {       
-                            currentModel.addFile((File)filesList.get(i));
+                        for(int i = droppedFiles.size()-1; i >= 0; i--) { 
+                            File file = (File)droppedFiles.get(i);
+                            if(file == null)
+                                continue;
+                            if(file.isDirectory()) {
+                                readDirectory(file, acceptedFiles);
+                            }
+                            else if(isAllowed(FileUtils.getFileExtension(file.getName()))) { 
+                                acceptedFiles.add(file);
+                            }
+                        }
+
+                        for(File file : acceptedFiles) {       
+                            currentModel.addFile(file);
                         }
                     }
                 });
 
                 dtde.dropComplete(true);
-            } catch (Exception e) { 
+            } catch (Exception e) {
             	dtde.dropComplete(false);
             }
-          } else { 
+          } else {
           		dtde.rejectDrop();
           }
     }
@@ -93,16 +123,35 @@ public class ShareDropTarget implements DropTargetListener {
     public void dropActionChanged(DropTargetDragEvent dtde) {
     }
     
-    private boolean isAllowed(String fileName) {
+    /**
+     * Reads files in a directory. Subdirectories are ignored and any allowed file
+     * within this directory is added to the accept list.
+     */
+    private void readDirectory(File directory, List filesList) {
+        for(File file : directory.listFiles()) {
+            if(!file.isDirectory() && isAllowed(FileUtils.getFileExtension(file.getName()))) {
+                fileList.addFile(file);
+            }
+        }
+    }
+    
+    /**
+     * Tests whether the file is droppable. If the file is of type Program or Document,
+     * the validity is based on SharingSettings, otherwise the file is accepted.
+     * 
+     * @param fileExtension - the file extension to test
+     * @return true if the file is allowed, false otherwise.
+     */
+    private boolean isAllowed(String fileExtension) {
         if(!alwaysShareDocuments && !SharingSettings.DOCUMENT_SHARING_ENABLED.getValue()) {
-            MediaType type = MediaType.getMediaTypeForExtension(fileName);
-            if(type.getSchema().equals(MediaType.SCHEMA_DOCUMENTS)) {
+            MediaType type = MediaType.getMediaTypeForExtension(fileExtension);
+            if(type == null || type.getSchema().equals(MediaType.SCHEMA_DOCUMENTS)) {
                 return false;
             }
         }
         if(!SharingSettings.PROGRAM_SHARING_ENABLED.getValue()) {
-            MediaType type = MediaType.getMediaTypeForExtension(fileName);
-            if(type.getSchema().equals(MediaType.SCHEMA_PROGRAMS))
+            MediaType type = MediaType.getMediaTypeForExtension(fileExtension);
+            if(type == null || type.getSchema().equals(MediaType.SCHEMA_PROGRAMS))
                 return false;
         }
         return true;
