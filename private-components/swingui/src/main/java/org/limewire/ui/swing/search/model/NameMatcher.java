fa22ef553919ee4e157b249aaed57d1517d86cba
pvertenten <pvertenten>
2008-09-24 22:53:30 +0000

    Adding Null checking for the file names.

--- private-components/swingui/src/main/java/org/limewire/ui/swing/search/model/NameMatcher.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/model/NameMatcher.java
@@ -7,12 +7,16 @@ public class NameMatcher implements SearchResultMatcher {
     @Override
     public boolean matches(VisualSearchResult o1, VisualSearchResult o2) {
         for (SearchResult result1 : o1.getCoreSearchResults()) {
-            String name1 = result1.getProperty(SearchResult.PropertyKey.NAME).toString();
-            for (SearchResult result2 : o2.getCoreSearchResults()) {
-                String name2 = result2.getProperty(SearchResult.PropertyKey.NAME).toString();
-                int result = name1.compareTo(name2);
-                if (result == 0) {
-                    return true;
+            String name1 = (String) result1.getProperty(SearchResult.PropertyKey.NAME);
+            if (name1 != null) {
+                for (SearchResult result2 : o2.getCoreSearchResults()) {
+                    String name2 = (String) result2.getProperty(SearchResult.PropertyKey.NAME);
+                    if (name2 != null) {
+                        int result = name1.compareTo(name2);
+                        if (result == 0) {
+                            return true;
+                        }
+                    }
                 }
             }
         }
