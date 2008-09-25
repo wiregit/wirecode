70edf3a9b78ef8fab2eb2985863d076234b6acf6
pvertenten <pvertenten>
2008-09-25 15:15:51 +0000

    Updated to use name matcher.

--- private-components/swingui/src/main/java/org/limewire/ui/swing/search/model/SimilarResultsDetectorFactoryImpl.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/model/SimilarResultsDetectorFactoryImpl.java
@@ -2,12 +2,6 @@ package org.limewire.ui.swing.search.model;
 
 public class SimilarResultsDetectorFactoryImpl implements SimilarResultsDetectorFactory {
     public SimilarResultsDetector newSimilarResultsDetector() {
-        return new SimilarResultsMatchingDetector(new SearchResultMatcher() {
-
-            @Override
-            public boolean matches(VisualSearchResult o1, VisualSearchResult o2) {
-                return true;
-            }
-        });
+        return new SimilarResultsMatchingDetector(new NameMatcher());
     }
 }
