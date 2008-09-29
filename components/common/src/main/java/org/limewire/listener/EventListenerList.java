e1a997120b872053b97ab8700d604e806e7336a1
sberlin <sberlin>
2008-09-29 19:23:44 +0000

    add BlockingEvent, fix tests.

--- components/common/src/main/java/org/limewire/listener/EventListenerList.java
+++ components/common/src/main/java/org/limewire/listener/EventListenerList.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Method;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.limewire.concurrent.ThreadExecutor;
 import org.limewire.logging.Log;
 import org.limewire.logging.LogFactory;
 import org.limewire.util.Objects;
@@ -63,7 +64,7 @@ public class EventListenerList<E> implements ListenerSupport<E> {
     
     private static final class ListenerProxy<E> implements EventListener<E> {
         private final EventListener<E> delegate;
-        private volatile Boolean swingProxy = null;
+        private volatile DispatchType type = DispatchType.UNKNOWN;
         
         public ListenerProxy(EventListener<E> delegate) {
             this.delegate = delegate;
@@ -71,26 +72,20 @@ public class EventListenerList<E> implements ListenerSupport<E> {
         
         @Override
         public void handleEvent(final E event) {
-            if(swingProxy == null) {
-                swingProxy = analyze(delegate, event);                
+            // Note: this is not thread-safe, but it is OK to analyze multiple times.
+            if(type == DispatchType.UNKNOWN) {
+                type = analyze(delegate, event);                
             }
             
-            if(swingProxy.booleanValue() && !EventQueue.isDispatchThread()) {
-                EventQueue.invokeLater(new Runnable() {
-                    public void run() {
-                        delegate.handleEvent(event);
-                    }
-                });
-            } else {
-                delegate.handleEvent(event);
-            }
+            type.dispatch(delegate, event);
         }
         
         /**
          * Loops through all 'handleEvent' methods whose parameter type can match event's
-         * classes & superclasses.  When one is found, see if it is annotated with @SwingProxy.
+         * classes & superclasses.  When one is found, see if it is annotated with {@link SwingEDTEvent} or
+         * {@link BlockingEvent}.
          */
-        private Boolean analyze(EventListener<E> delegate, E event) {
+        private DispatchType analyze(EventListener<E> delegate, E event) {
             Class<?> eventClass = event.getClass();
             Method method = null;
             while(eventClass != null) {
@@ -105,9 +100,60 @@ public class EventListenerList<E> implements ListenerSupport<E> {
             if(method == null) {
                 throw new IllegalStateException("Unable to find method!");
             }
-
-            return method.getAnnotation(SwingEDTEvent.class) != null;
+            
+            if(method.getAnnotation(SwingEDTEvent.class) != null) {
+                return DispatchType.SWING;
+            } else if(method.getAnnotation(BlockingEvent.class) != null) {
+                return DispatchType.BLOCKING;
+            } else {
+                return DispatchType.INLINE;
+            }
         }
+        
+
+        private static enum DispatchType {
+            UNKNOWN() {
+                @Override
+                <E> void dispatch(EventListener<E> listener, E event) {
+                    throw new IllegalStateException("unknown dispatch!");
+                }
+            }
+
+            ,
+            INLINE() {
+                @Override
+                <E> void dispatch(EventListener<E> listener, E event) {
+                    listener.handleEvent(event);
+                }
+            },
+            SWING() {
+                @Override
+                <E> void dispatch(final EventListener<E> listener, final E event) {
+                    if(EventQueue.isDispatchThread()) {
+                        listener.handleEvent(event);
+                    } else {
+                        EventQueue.invokeLater(new Runnable() {
+                            public void run() {
+                                listener.handleEvent(event);
+                            }
+                        });
+                    }
+                }
+            },
+            BLOCKING() {
+                @Override
+                <E> void dispatch(final EventListener<E> listener, final E event) {
+                    ThreadExecutor.startThread(new Runnable() {
+                        public void run() {
+                            listener.handleEvent(event);
+                        }
+                    }, "BlockingEvent");
+                }
+            };
+
+            abstract <E> void dispatch(EventListener<E> listener, E event);
+
+        };
         
     }
 }
--- components/common/src/test/java/org/limewire/listener/EventListenerListTest.java
+++ components/common/src/test/java/org/limewire/listener/EventListenerListTest.java
@@ -1,6 +1,7 @@
 package org.limewire.listener;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import junit.framework.Test;
 
@@ -38,26 +39,44 @@ public class EventListenerListTest extends BaseTestCase {
         Listener l = new Listener();
         list.addListener(l);
         assertNull(l.thread);
-        l.handleEvent(new Object());
+        list.broadcast(new Object());
         assertEquals(Thread.currentThread(), l.thread);   
     }
     
-    public void testSwingProxyEvent() {
+    public void testSwingProxyEvent() throws Exception {
         EventListenerList<Foo> list = new EventListenerList<Foo>();
         SwingProxyListener l = new SwingProxyListener();
         list.addListener(l);
         assertNull(l.thread);
-        l.handleEvent(new Foo());
-        assertEquals(Thread.currentThread(), l.thread);
+        list.broadcast(new Foo());
+        assertTrue(l.latch.await(1, TimeUnit.SECONDS));
+        assertNotEquals(Thread.currentThread(), l.thread);
     }
     
-    public void testSwingProxyEventWithSubclassedEvent() {
+    public void testSwingProxyEventWithSubclassedEvent() throws Exception {
         EventListenerList<Foo> list = new EventListenerList<Foo>();
         SwingProxyListener l = new SwingProxyListener();
         list.addListener(l);
         assertNull(l.thread);
-        l.handleEvent(new SubFoo());
-        assertEquals(Thread.currentThread(), l.thread);
+        list.broadcast(new SubFoo());
+        assertTrue(l.latch.await(1, TimeUnit.SECONDS));
+        assertNotEquals(Thread.currentThread(), l.thread);
+    }
+    
+    
+    public void testBlockingEvent() throws Exception {
+        EventListenerList<Foo> list = new EventListenerList<Foo>();
+        BlockingListener l1 = new BlockingListener();
+        BlockingListener l2 = new BlockingListener();
+        list.addListener(l1);
+        list.addListener(l2);
+        assertNull(l1.thread);
+        assertNull(l2.thread);        
+        list.broadcast(new Foo());
+        assertTrue(BlockingListener.latch.await(1, TimeUnit.SECONDS));
+        assertNotEquals(Thread.currentThread(), l1.thread);
+        assertNotEquals(Thread.currentThread(), l2.thread);
+        assertNotEquals(l1.thread, l2.thread);
     }
     
     private static class Listener implements EventListener<Object> {
@@ -72,10 +91,24 @@ public class EventListenerListTest extends BaseTestCase {
         private volatile Thread thread;
         
         @Override
+        @SwingEDTEvent
         public void handleEvent(Foo event) {
             this.thread = Thread.currentThread();
             latch.countDown();
         }
+    }
+    
+    private static class BlockingListener implements EventListener<Foo> {
+        private static final CountDownLatch latch = new CountDownLatch(2);
+        private volatile Thread thread;
+        
+        @Override
+        @BlockingEvent
+        public void handleEvent(Foo event) {
+            this.thread = Thread.currentThread();
+            try { Thread.sleep(100); } catch(InterruptedException ix) {}
+            latch.countDown();
+        }        
     }
     
     private static class Foo {}
