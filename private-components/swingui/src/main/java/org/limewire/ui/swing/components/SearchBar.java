c7e8ded20411c763055e537a9da8c9bcf764b585
mturkel <mturkel>
2008-10-02 16:26:15 +0000

    SearchBar visual improvements

--- private-components/swingui/src/main/java/org/limewire/ui/swing/components/SearchBar.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/components/SearchBar.java
@@ -2,8 +2,9 @@ package org.limewire.ui.swing.components;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.geom.Area;
 import java.awt.geom.RoundRectangle2D;
 
@@ -14,25 +15,32 @@ import javax.swing.JComponent;
 import javax.swing.border.EmptyBorder;
 import javax.swing.plaf.basic.BasicComboBoxUI;
 
+import net.miginfocom.swing.MigLayout;
+
 import org.jdesktop.application.Resource;
 import org.jdesktop.swingx.JXPanel;
 import org.jdesktop.swingx.painter.AbstractPainter;
 import org.limewire.ui.swing.util.GuiUtils;
 
+
 public class SearchBar extends JXPanel {
+    //TODO: move to properties file
+    private static final int ARC_WIDTH = 20;
+    private static final int GAP = 5;
     @Resource
     private Icon buttonIcon;
 
     public SearchBar(JComboBox comboBox, JComponent textField) {
         //TODO:proper sizing and colors
-        super(new FlowLayout(FlowLayout.LEFT, 5, 2));
+        super(new MigLayout("ins 0, gapx 0, gapy 0"));
         GuiUtils.assignResources(this);
-        comboBox.setPreferredSize(new Dimension(104, 18));
-        textField.setPreferredSize(new Dimension(242, 18));
+        comboBox.setPreferredSize(new Dimension(104 - ARC_WIDTH, 22));
+        textField.setPreferredSize(new Dimension(242 - GAP, 22));
        // setPreferredSize(new Dimension(346, 22));
-        add(comboBox);
-        add(textField);
+        add(comboBox, "gapbefore " + ARC_WIDTH);
+        add(textField, "gapbefore " + GAP);
 
+        textField.setFont(textField.getFont().deriveFont(12));
         SearchComboUI comboUI = new SearchComboUI(buttonIcon);
         comboBox.setUI(comboUI);
         comboUI.init();
@@ -65,10 +73,10 @@ public class SearchBar extends JXPanel {
         @Override
         protected void doPaint(Graphics2D g, JXPanel object, int width, int height) {
             Area rightShape = new Area(
-                    new RoundRectangle2D.Float(leftComponent.getWidth() + 5, 0, object.getWidth() - leftComponent.getWidth() - 5, 
-                            object.getHeight(), 20, object.getHeight()));
+                    new RoundRectangle2D.Float(leftComponent.getWidth() + ARC_WIDTH, 0, object.getWidth() - leftComponent.getWidth() - ARC_WIDTH, 
+                            object.getHeight(), ARC_WIDTH, object.getHeight()));
             
-            Area leftArea = new Area(new RoundRectangle2D.Float(0, 0, object.getWidth(), object.getHeight(), 20, object.getHeight()));
+            Area leftArea = new Area(new RoundRectangle2D.Float(0, 0, object.getWidth(), object.getHeight(), ARC_WIDTH, object.getHeight()));
             leftArea.subtract(rightShape);
             
             g.setColor(leftColor);
@@ -97,12 +105,23 @@ public class SearchBar extends JXPanel {
             comboBox.setOpaque(false);
             comboBox.setBorder(new EmptyBorder(0,0,0,0));
             comboBox.setBackground(leftColor);
+            
+//            ListCellRenderer renderer = comboBox.getRenderer();
+//            if (renderer instanceof JLabel){
+//                ((JLabel)renderer).setHorizontalAlignment(SwingConstants.RIGHT);
+//            }
         }
         
+        @Override
         protected JButton createArrowButton() {
             JButton button = new IconButton(buttonIcon);
             button.setName("ComboBox.arrowButton");
             return button;
+        }
+        
+        @Override
+        public void paintCurrentValue(Graphics g,Rectangle bounds,boolean hasFocus) {
+            super.paintCurrentValue(g, bounds, false);
         }
     }
 }
