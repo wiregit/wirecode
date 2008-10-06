3b92747713dd609f91acfef09fd8ffdae8632e17
pvertenten <pvertenten>
2008-10-06 20:12:03 +0000

    Removing commented out edit distance code.

--- private-components/swingui/src/main/java/org/limewire/ui/swing/search/model/NameMatcher.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/model/NameMatcher.java
@@ -43,100 +43,13 @@ public class NameMatcher implements SearchResultMatcher {
         if (name1.equalsIgnoreCase(name2)) {
             return true;
         }
-        // else if (getPercentageDifferance(name1, name2) <
-        // getDifferanceTolerance(name1, name2)) {
-        // return true;
-        // }
+
         return false;
     }
 
     /**
-     * Returns a tolerance for differances between the strings based upon the
-     * length of the smaller string.
-     */
-    // private double getDifferanceTolerance(String string1, String string2) {
-    // double tolerance = 0;
-    // int minStringLength = Math.min(string1.length(), string2.length());
-    //
-    // if (minStringLength <= 5) {
-    // tolerance = .20;
-    // } else if (minStringLength <= 20) {
-    // tolerance = .10;
-    // } else if (tolerance <= 100) {
-    // tolerance = .05;
-    // } else {
-    // tolerance = .01;
-    // }
-    //
-    // return tolerance;
-    // }
-    /**
-     * Calculates the percentage differance between two strings as a function of
-     * the edit distance over the length of the smaller string.
-     */
-    // private double getPercentageDifferance(String string1, String string2) {
-    // int editDistance = getLevenshteinDistance(string1, string2);
-    // int minStringLength = Math.min(string1.length(), string2.length());
-    // return editDistance / (double) minStringLength;
-    // }
-    /**
-     * This is the same implentation as the commons lang library.
-     * 
-     * http://www.merriampark.com/ldjava.htm
-     * 
-     * TODO - could include jakarta commons lang library instead.
-     */
-    // private int getLevenshteinDistance(String s, String t) {
-    // int n = s.length(); // length of s
-    // int m = t.length(); // length of t
-    //
-    // if (n == 0) {
-    // return m;
-    // } else if (m == 0) {
-    // return n;
-    // }
-    //
-    // int p[] = new int[n + 1]; // 'previous' cost array, horizontally
-    // int d[] = new int[n + 1]; // cost array, horizontally
-    // int _d[]; // placeholder to assist in swapping p and d
-    //
-    // // indexes into strings s and t
-    // int i; // iterates through s
-    // int j; // iterates through t
-    //
-    // char t_j; // jth character of t
-    //
-    // int cost; // cost
-    //
-    // for (i = 0; i <= n; i++) {
-    // p[i] = i;
-    // }
-    //
-    // for (j = 1; j <= m; j++) {
-    // t_j = t.charAt(j - 1);
-    // d[0] = j;
-    //
-    // for (i = 1; i <= n; i++) {
-    // cost = s.charAt(i - 1) == t_j ? 0 : 1;
-    // // minimum of cell to the left+1, to the top+1, diagonally left
-    // // and up +cost
-    // d[i] = Math.min(Math.min(d[i - 1] + 1, p[i] + 1), p[i - 1] + cost);
-    // }
-    //
-    // // copy current distance counts to 'previous row' distance counts
-    // _d = p;
-    // p = d;
-    // d = _d;
-    // }
-    //
-    // // our last action in the above loop was to switch d and p, so p now
-    // // actually has the most recent cost counts
-    // return p[n];
-    //
-    // }
-    /**
-     * Removes all symbols and spaces in the string. Converts string to
-     * lowercase. Also removes any leaf elements on the name. file1.txt
+     * Removes all symbols and spaces in the string. 
+     * Also removes any leaf elements on the name. file1.txt
      * file1(1).txt, file1(2).txt
      */
     private String cleanString(String string) {
