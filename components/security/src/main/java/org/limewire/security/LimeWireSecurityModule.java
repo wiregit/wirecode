262186ae61cfba4b83df6163e65286ac4f6e6ceb
fberger <fberger>
2008-10-22 21:01:16 +0000

    LWC-1934: implement friend downloads: Removed user store.

--- components/security/src/main/java/org/limewire/security/LimeWireSecurityModule.java
+++ components/security/src/main/java/org/limewire/security/LimeWireSecurityModule.java
@@ -1,7 +1,5 @@
 package org.limewire.security;
 
-import org.limewire.security.auth.UserStore;
-import org.limewire.security.auth.UserStoreImpl;
 import org.limewire.security.certificate.LimeWireSecurityCertificateModule;
 
 import com.google.inject.AbstractModule;
@@ -11,7 +9,6 @@ public class LimeWireSecurityModule extends AbstractModule {
     @Override
     protected void configure() {
         binder().install(new LimeWireSecurityCertificateModule());
-        bind(UserStore.class).to(UserStoreImpl.class);
     }
 
 }
--- components/security/src/main/java/org/limewire/security/auth/UserStore.java
+++
@@ -1,7 +0,0 @@
-package org.limewire.security.auth;
-
-public interface UserStore {
-    String getPassword(String user);
-    void addUser(String user, String password);
-    boolean authenticate(String username, String password);
-}
