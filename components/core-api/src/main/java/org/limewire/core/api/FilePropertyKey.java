f0208d7a6d5304838e6183ebf85744c294ca3a3d
pvertenten <pvertenten>
2008-10-28 21:15:54 +0000

    Added TODO items next to FilePropertyKey enums that I see being accessed but never set, anywhere.

--- components/core-api/src/main/java/org/limewire/core/api/FilePropertyKey.java
+++ components/core-api/src/main/java/org/limewire/core/api/FilePropertyKey.java
@@ -8,20 +8,20 @@ public enum FilePropertyKey {
     AUTHOR,
     BITRATE, // ex. 128, 160, 192, 224, 320
     COMMENTS,
-    COMPANY,
+    COMPANY, //TODO this property is not being set on items
     DATE_CREATED,
     FILE_SIZE, // in megabytes
-    FILES_IN_ARCHIVE, // # of files in an archive file (zip, tar, ...)
+    FILES_IN_ARCHIVE, // # of files in an archive file (zip, tar, ...) //TODO this property is not being set on items
     GENRE,
     HEIGHT,
     LENGTH, // in time (hh:mm:ss)
     NAME,
-    OWNER,
-    PLATFORM,
+    OWNER,//TODO this property is not being set on items
+    PLATFORM,//TODO this property is not being set on items
     QUALITY,
-    RATING,
-    RELEVANCE,
-    SAMPLE_RATE, // ex. 44,100 Hz
+    RATING,//TODO this property is not being set on items
+    RELEVANCE,//TODO this property is not being set on items
+    SAMPLE_RATE, // ex. 44,100 Hz//TODO this property is not being set on items
     // get size from VisualSearchResult.getSize()
     TRACK_NUMBER,
     TRACK_NAME,
