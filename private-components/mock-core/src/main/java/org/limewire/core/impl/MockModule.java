d889c4cc5a6ce572b8bbd5d0beae5f94cc4bfc38
pvertenten <pvertenten>
2008-10-28 19:54:43 +0000

    Added MockConnectionManager.

--- private-components/mock-core/src/main/java/org/limewire/core/impl/MockModule.java
+++ private-components/mock-core/src/main/java/org/limewire/core/impl/MockModule.java
@@ -1,8 +1,10 @@
 package org.limewire.core.impl;
 
 import org.limewire.core.api.Application;
+import org.limewire.core.api.connection.ConnectionManager;
 import org.limewire.core.api.lifecycle.MockLifeCycleModule;
 import org.limewire.core.impl.browse.MockBrowseModule;
+import org.limewire.core.impl.connection.MockConnectionManagerImpl;
 import org.limewire.core.impl.download.MockDownloadModule;
 import org.limewire.core.impl.library.MockLibraryModule;
 import org.limewire.core.impl.player.MockPlayerModule;
@@ -11,6 +13,7 @@ import org.limewire.core.impl.spam.MockSpamModule;
 import org.limewire.core.impl.xmpp.MockXmppModule;
 
 import com.google.inject.AbstractModule;
+import com.limegroup.gnutella.connection.Connection;
 
 
 public class MockModule extends AbstractModule {
@@ -18,7 +21,7 @@ public class MockModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(Application.class).to(MockApplication.class);
-        
+        bind(ConnectionManager.class).to(MockConnectionManagerImpl.class);
         install(new MockLifeCycleModule());
         install(new MockSpamModule());
         install(new MockSearchModule());
