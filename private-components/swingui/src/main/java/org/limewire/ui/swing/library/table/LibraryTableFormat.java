b88207597a404462c0b048fcb6361e8b790c127e
mturkel <mturkel>
2008-11-06 15:59:47 +0000

    Added comment

--- private-components/swingui/src/main/java/org/limewire/ui/swing/library/table/LibraryTableFormat.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/library/table/LibraryTableFormat.java
@@ -7,7 +7,9 @@ import ca.odell.glazedlists.gui.WritableTableFormat;
 public interface LibraryTableFormat<T extends FileItem> extends WritableTableFormat<T>{
     
     int getActionColumn();
-    
+    /**
+     * Due to the inner workings of JXTable, these must be in reverse order for the hiding to work.  
+     */
     int[] getDefaultHiddenColums();
 
 }
