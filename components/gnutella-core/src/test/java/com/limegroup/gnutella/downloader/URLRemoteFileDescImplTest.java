ef4b2e87509411a9e673e2bedc356fc9ffa1d250
fberger <fberger>
2008-11-12 21:02:13 +0000

    LWC-1934: implement friend downloads: fixed.

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/downloader/URLRemoteFileDescImplTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/downloader/URLRemoteFileDescImplTest.java
@@ -4,6 +4,7 @@ import java.net.URL;
 
 import junit.framework.Test;
 
+import org.limewire.io.ConnectableImpl;
 import org.limewire.util.BaseTestCase;
 
 import com.limegroup.gnutella.RemoteFileDesc;
@@ -23,8 +24,8 @@ public class URLRemoteFileDescImplTest extends BaseTestCase {
     public void testFileDescsWithSameURLAreEqual() throws Exception {
         URN urn = URN.createSHA1Urn("urn:sha1:GLSTHIPQGSSZTS5FJUPAKPZWUGYQYPFB");
         URL url = new URL("http://magnet2.limewire.com:6346/uri-res/N2R?urn:sha1:GLSTHIPQGSSZTS5FJUPAKPZWUGYQYPFB");
-        RemoteFileDesc desc1 = new UrlRemoteFileDescImpl("host1.com", 80, "filename", 1000, new UrnSet(urn), url, null);
-        RemoteFileDesc desc2 = new UrlRemoteFileDescImpl("host1.com", 80, "filename", 1000, new UrnSet(urn), url, null);
+        RemoteFileDesc desc1 = new UrlRemoteFileDescImpl(new ConnectableImpl("host1.com", 80, false), "filename", 1000, new UrnSet(urn), url, null);
+        RemoteFileDesc desc2 = new UrlRemoteFileDescImpl(new ConnectableImpl("host1.com", 80, false), "filename", 1000, new UrnSet(urn), url, null);
         assertEquals(desc1, desc2);
         assertEquals(desc1.hashCode(), desc2.hashCode());
     }
