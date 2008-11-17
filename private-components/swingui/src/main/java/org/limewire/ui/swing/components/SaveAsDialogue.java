f4655f26748fdae67810ef3f11a3e8369fa9d931
pvertenten <pvertenten>
2008-11-17 19:57:23 +0000

    Adding comment about future functionality for save as dialogue.

--- private-components/swingui/src/main/java/org/limewire/ui/swing/components/SaveAsDialogue.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/components/SaveAsDialogue.java
@@ -124,7 +124,7 @@ public class SaveAsDialogue extends JDialog {
 
     public static void handleSaveLocationException(final DownLoadAction downLoadAction,
             final SaveLocationException sle, final Component component) {
-
+//TODO dependiong on append to file name setting, don't show the dialogue, instead append a number to the end of the name.
         if (sle.getErrorCode() != SaveLocationException.LocationCode.FILE_ALREADY_EXISTS
                 && sle.getErrorCode() != SaveLocationException.LocationCode.FILE_IS_ALREADY_DOWNLOADED_TO) {
             // TODO better user feedback
