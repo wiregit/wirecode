dfc00f0fc6130f658d7485a2fead9c8bfbd7a947
michaelt <michaelt>
2008-11-17 18:11:32 +0000

    Does a slight refactor.  Moves the visibilty code from inside the SharedFileCountPanel to it's container to avoid possible mysterious disappearing behaviour if things are changed later.

--- private-components/swingui/src/main/java/org/limewire/ui/swing/statusbar/SharedFileCountPanel.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/statusbar/SharedFileCountPanel.java
@@ -2,15 +2,11 @@ package org.limewire.ui.swing.statusbar;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 
 import javax.swing.BorderFactory;
 
 import org.jdesktop.application.Resource;
 import org.jdesktop.swingx.JXLabel;
-import org.limewire.core.api.connection.ConnectionStrength;
-import org.limewire.core.api.connection.GnutellaConnectionManager;
 import org.limewire.core.api.library.LocalFileItem;
 import org.limewire.core.api.library.ShareListManager;
 import org.limewire.ui.swing.util.GuiUtils;
@@ -30,7 +26,7 @@ class SharedFileCountPanel extends JXLabel {
     @Resource private Color foreground;
     
     @Inject
-    SharedFileCountPanel(GnutellaConnectionManager connectionManager, ShareListManager shareListManager) {
+    SharedFileCountPanel(ShareListManager shareListManager) {
         super(I18n.tr("Sharing {0} files", 0));
         
         GuiUtils.assignResources(this);
@@ -45,39 +41,6 @@ class SharedFileCountPanel extends JXLabel {
                 setText(I18n.tr("Sharing {0} files", listChanges.getSourceList().size()));
             }
         });
-        
-        connectionManager.addPropertyChangeListener(new PropertyChangeListener() {
-            @Override
-            public void propertyChange(PropertyChangeEvent evt) {
-                if(evt.getPropertyName().equals("strength")) {
-                    setConnectionStrength((ConnectionStrength)evt.getNewValue());
-                }
-            }
-        });
-        setConnectionStrength(connectionManager.getConnectionStrength());
-    }
-    
-    private void setConnectionStrength(ConnectionStrength strength) {
-        
-        boolean sharingVisible = false;
-        
-        switch(strength) {
-            
-        case WEAK:
-        case MEDIUM:
-        case FULL:
-        case TURBO:
-            
-            sharingVisible = true;
-            break;
-            
-        default:
-
-            sharingVisible = false;
-            break;
-        }
-        
-        this.setVisible(sharingVisible);
         
     }
 
--- private-components/swingui/src/main/java/org/limewire/ui/swing/statusbar/StatusPanel.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/statusbar/StatusPanel.java
@@ -1,6 +1,8 @@
 package org.limewire.ui.swing.statusbar;
 
 import java.awt.Dimension;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 import javax.swing.JComponent;
 import javax.swing.JLabel;
@@ -9,6 +11,8 @@ import net.miginfocom.swing.MigLayout;
 
 import org.jdesktop.application.Resource;
 import org.jdesktop.swingx.JXPanel;
+import org.limewire.core.api.connection.ConnectionStrength;
+import org.limewire.core.api.connection.GnutellaConnectionManager;
 import org.limewire.player.api.AudioPlayer;
 import org.limewire.ui.swing.painter.StatusBarPainter;
 import org.limewire.ui.swing.painter.StatusBarSectionPainter;
@@ -23,18 +27,21 @@ public class StatusPanel extends JXPanel {
     
     @Resource private int height;
 
+    private final SharedFileCountPanel sharedFileCountPanel;
+    
     @Inject
-    public StatusPanel(AudioPlayer player, FriendStatusPanel friendStatusPanel, 
+    public StatusPanel(GnutellaConnectionManager connectionManager, AudioPlayer player, 
+            FriendStatusPanel friendStatusPanel, 
             ConnectionStatusPanel connectionStatus, SharedFileCountPanel sharedFileCountPanel) {
         
         GuiUtils.assignResources(this);
         
+        this.sharedFileCountPanel = sharedFileCountPanel;
+        
         setLayout(new MigLayout("insets 0, gap 0, hidemode 3, fill"));
-   
         setMinimumSize(new Dimension(0, height + 2));
         setMaximumSize(new Dimension(Short.MAX_VALUE, height + 2));
         setPreferredSize(new Dimension(Short.MAX_VALUE, height + 2));
-        
         setBackgroundPainter(new StatusBarPainter());
  
         StatusBarSectionPainter<JComponent> sectionPainter = new StatusBarSectionPainter<JComponent>();
@@ -51,6 +58,39 @@ public class StatusPanel extends JXPanel {
         add(new JLabel(), "gapafter push, growx");
         
         add(friendStatusPanel.getComponent(), "gapbefore push");
+        
+        connectionManager.addPropertyChangeListener(new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                if(evt.getPropertyName().equals("strength")) {
+                    updateComponents((ConnectionStrength)evt.getNewValue());
+                }
+            }
+        });
+        updateComponents(connectionManager.getConnectionStrength());
+    }
+    
+    private void updateComponents(ConnectionStrength strength) {
+        
+        boolean sharingVisible = false;
+        
+        switch(strength) {
+            
+        case WEAK:
+        case MEDIUM:
+        case FULL:
+        case TURBO:
+            
+            sharingVisible = true;
+            break;
+            
+        default:
+
+            sharingVisible = false;
+            break;
+        }
+        
+        this.sharedFileCountPanel.setVisible(sharingVisible);
         
     }
 }
