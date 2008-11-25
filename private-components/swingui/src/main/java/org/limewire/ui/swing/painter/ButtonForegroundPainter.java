88b6c3be0f31c86e5c951de1fba5fe0c1f501287
michaelt <michaelt>
2008-11-25 18:08:13 +0000

    General foreground painter for JXButton and LimeComboBox that allows multiple foreground colours and icons dependent on the model state.
    Can now take foreground control out of LimeComboBox and have generic painters for all dark JXButtons that require different fonts depending on the mouse

--- private-components/swingui/src/main/java/org/limewire/ui/swing/painter/ButtonForegroundPainter.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/painter/ButtonForegroundPainter.java
@@ -1,17 +1,72 @@
 package org.limewire.ui.swing.painter;
 
 import java.awt.Graphics2D;
+import java.awt.Paint;
+
+import javax.swing.Icon;
 
 import org.jdesktop.swingx.JXButton;
 import org.jdesktop.swingx.painter.AbstractPainter;
-
-
+import org.limewire.ui.swing.components.LimeComboBox;
 
 public class ButtonForegroundPainter extends AbstractPainter<JXButton> {
 
+    private final Paint pressedForeground = null;
+    private final Paint hoverForeground = null;
+    
     @Override
     protected void doPaint(Graphics2D g, JXButton object, int width, int height) {
-        // TODO Auto-generated method stub
+        
+        int textBaseline = object.getHeight() - object.getHeight()/2 
+                + g.getFontMetrics().getAscent()/2 
+                - object.getInsets().top;
+        
+        Icon icon = null;
+        Paint foreground = null;
+                
+        if (object.getModel().isPressed()) {
+            icon = object.getPressedIcon();
+            foreground = pressedForeground; 
+        }
+        else if (object.getModel().isRollover()) {
+            icon = object.getRolloverIcon();
+            foreground = hoverForeground;
+        }
+        else {
+            icon = object.getRolloverIcon();
+        }
+            
+        if (foreground == null) {
+            foreground = object.getForeground();
+        }
+        
+        g.setFont(object.getFont());
+        g.setPaint(foreground);
+        
+        if (object.getText() != null) {
+            g.drawString(object.getText(), object.getInsets().left, textBaseline);
+            
+            if (icon != null) {
+                icon.paintIcon(object, g, object.getWidth() - object.getInsets().right + 3, 
+                        object.getHeight()/2 - icon.getIconHeight()/2);
+            }
+        } 
+        // TODO: should use a more OO solution in future
+        else if (object instanceof LimeComboBox) {
+            
+            LimeComboBox box = (LimeComboBox) object;
+            
+            if (box.getSelectedAction() != null) {
+                g.drawString(box.getSelectedAction().getValue("Name").toString(), object.getInsets().left, 
+                        textBaseline);
+            }
+            
+            if (icon != null) {
+                icon.paintIcon(box, g, box.getWidth() - object.getInsets().right + icon.getIconWidth(), 
+                        box.getHeight()/2 - icon.getIconHeight()/2);
+            }
+        }
+
         
     }
 
