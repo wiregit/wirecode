3d13a167fdf856929ccc113df696f85baa359f1c
tjulien <tjulien>
2008-12-02 00:00:25 +0000

    LWC-2038; friend downloads not shared; gnutella downloads shared (if configured to)

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/ShareSettingsOverride.java
+++
@@ -1,9 +0,0 @@
-package com.limegroup.gnutella;
-
-public enum ShareSettingsOverride {
-    /** use default share settings*/
-    DEFAULT,
-    /** do not automatically share downloaded file*/
-    NO_SHARE,
-
-}
--- components/xmpp-api/src/main/java/org/limewire/xmpp/api/client/XMPPAddress.java
+++ components/xmpp-api/src/main/java/org/limewire/xmpp/api/client/XMPPAddress.java
@@ -1,6 +1,5 @@
-package org.limewire.xmpp.client.impl;
+package org.limewire.xmpp.api.client;
 
-import org.jivesoftware.smack.util.StringUtils;
 import org.limewire.io.PermanentAddress;
 import org.limewire.util.Objects;
 
@@ -36,11 +35,35 @@ public class XMPPAddress implements PermanentAddress {
      * Returns the jabber id email address without resource. 
      */
     public String getId() {
-        return StringUtils.parseBareAddress(id);
+        return parseBareAddress(id);
     }
 
     @Override
     public String toString() {
         return org.limewire.util.StringUtils.toString(this);
+    }
+    
+    /**
+     * Returns the XMPP address with any resource information removed. For example,
+     * for the address "matt@jivesoftware.com/Smack", "matt@jivesoftware.com" would
+     * be returned.
+     *
+     * @param XMPPAddress the XMPP address.
+     * @return the bare XMPP address without resource information.
+     */
+    public static String parseBareAddress(String XMPPAddress) {
+        if (XMPPAddress == null) {
+            return null;
+        }
+        int slashIndex = XMPPAddress.indexOf("/");
+        if (slashIndex < 0) {
+            return XMPPAddress;
+        }
+        else if (slashIndex == 0) {
+            return "";
+        }
+        else {
+            return XMPPAddress.substring(0, slashIndex);
+        }
     }
 }
