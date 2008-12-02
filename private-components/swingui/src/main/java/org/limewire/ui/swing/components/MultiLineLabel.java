b499d81dcac3bf209d59f3263b6d853cf853e54e
sberlin <sberlin>
2008-12-02 16:55:31 +0000

    allow a multilinelabel with no specified line width.

--- private-components/swingui/src/main/java/org/limewire/ui/swing/components/MultiLineLabel.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/components/MultiLineLabel.java
@@ -3,6 +3,11 @@ package org.limewire.ui.swing.components;
 import org.jdesktop.swingx.JXLabel;
 
 public class MultiLineLabel extends JXLabel {
+    
+    public MultiLineLabel(String text) {
+        super(text);
+        setLineWrap(true);
+    }
 
     public MultiLineLabel(String text, int lineWidth) {
         super(text);
