8c8f4b95f1bc63a6c176f4d61fcb785a82b80d96
eyu <eyu>
2008-12-03 19:29:57 +0000

    [LWC-1591] implemented using component panels

--- private-components/swingui/src/main/java/org/limewire/ui/swing/advanced/ConnectionsPanel.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/advanced/ConnectionsPanel.java
@@ -1,25 +1,55 @@
 package org.limewire.ui.swing.advanced;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.BorderFactory;
+import javax.swing.Timer;
+
+import net.miginfocom.swing.MigLayout;
+
+import org.limewire.ui.swing.advanced.connection.AddConnectionPanel;
+import org.limewire.ui.swing.advanced.connection.ConnectionDetailPanel;
+import org.limewire.ui.swing.advanced.connection.ConnectionSummaryPanel;
+import org.limewire.ui.swing.advanced.connection.IncomingSearchPanel;
 
 import com.google.inject.Inject;
 
 /**
- * An Advanced Tools tab panel that displays the connections.
+ * The Connections tab panel for the Advanced Tools window.  This displays the
+ * current connections and incoming search phrases. 
  */
-public class ConnectionsPanel extends JPanel {
+public class ConnectionsPanel extends TabPanel implements ActionListener {
+    /** Default delay between timer events. */
+    private static final int DEFAULT_DELAY = 1000;
     
-    private BorderLayout panelLayout = new BorderLayout();
-    private JLabel todoLabel = new JLabel();
+    /** Delay time between update events in milliseconds. */
+    private int delay = DEFAULT_DELAY;
+    
+    /** Timer to refresh connection list. */
+    private Timer timer;
+    
+    private ConnectionSummaryPanel summaryPanel;
+    private ConnectionDetailPanel detailPanel;
+    private AddConnectionPanel addConnectionPanel;
+    private IncomingSearchPanel incomingPanel;
 
     /**
-     * Constructs a ConnectionsPanel. 
+     * Constructs a ConnectionsPanel with the specified component panels.
      */
     @Inject
-    public ConnectionsPanel() {
+    public ConnectionsPanel(
+            ConnectionSummaryPanel summaryPanel,
+            ConnectionDetailPanel detailPanel,
+            AddConnectionPanel addConnectionPanel,
+            IncomingSearchPanel incomingPanel) {
+
+        this.summaryPanel = summaryPanel;
+        this.detailPanel = detailPanel;
+        this.addConnectionPanel = addConnectionPanel;
+        this.incomingPanel = incomingPanel;
+        
         initComponents();
     }
     
@@ -27,12 +57,88 @@ public class ConnectionsPanel extends JPanel {
      * Initializes the components in the container.
      */
     private void initComponents() {
-        setLayout(panelLayout);
-        
-        todoLabel.setHorizontalAlignment(JLabel.CENTER);
-        todoLabel.setText("TODO connections");
-        
-        add(todoLabel, BorderLayout.CENTER);
+        setBorder(BorderFactory.createEmptyBorder(6, 6, 6, 6));
+        setLayout(new MigLayout("fill",
+            "[left,fill]12[left,grow,fill]",    // col constraints
+            "[top]12[top]12[top,grow,fill]"));  // row constraints
+
+        add(summaryPanel      , "cell 0 0");
+        add(detailPanel       , "cell 1 0 1 2");
+        add(addConnectionPanel, "cell 0 1");
+        add(incomingPanel     , "cell 0 2 2 1");
     }
 
+    /**
+     * Sets the background color for the panel.
+     */
+    @Override
+    public void setBackground(Color bgColor) {
+        super.setBackground(bgColor);
+        if (summaryPanel != null) {
+            summaryPanel.setBackground(bgColor);
+        }
+    }
+    
+    /**
+     * Performs startup tasks for the tab content.  This method is called
+     * when the tab is selected. 
+     */
+    @Override
+    public void start() {
+        // Initialize data models.
+        summaryPanel.initData();
+        detailPanel.initData();
+        incomingPanel.initData();
+        
+        // Start refresh timer.
+        startTimer();
+    }
+
+    /**
+     * Performs clean up tasks for the tab.  This method is called when the
+     * parent window is closed.
+     */
+    @Override
+    public void stop() {
+        // Stop refresh timer.
+        stopTimer();
+        
+        // Clear data models.  This disposes of instances of TransformedList 
+        // that are used to handle data.
+        summaryPanel.clearData();
+        detailPanel.clearData();
+        incomingPanel.clearData();
+    }
+
+    /**
+     * Handles action performed event from the timer to refresh the display.
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        summaryPanel.refresh();
+        detailPanel.refresh();
+    }
+    
+    /**
+     * Starts the timer used to refresh the display.
+     */
+    private void startTimer() {
+        // Stop current timer.
+        stopTimer();
+        
+        // Create new timer, and start.
+        timer = new Timer(delay, this);
+        timer.start();
+    }
+    
+    /**
+     * Stops the timer used to refresh the display.
+     */
+    private void stopTimer() {
+        if (timer != null) {
+            timer.stop();
+            timer.removeActionListener(this);
+            timer = null;
+        }
+    }
 }
