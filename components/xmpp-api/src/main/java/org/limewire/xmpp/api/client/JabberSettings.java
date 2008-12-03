d5a8c62d9eeadb20406100426964569e9079fd40
pvertenten <pvertenten>
2008-12-03 22:46:27 +0000

    Adding comments.

--- components/xmpp-api/src/main/java/org/limewire/xmpp/api/client/JabberSettings.java
+++ components/xmpp-api/src/main/java/org/limewire/xmpp/api/client/JabberSettings.java
@@ -4,5 +4,8 @@ package org.limewire.xmpp.api.client;
  * This class provides a means for core/component code to access XMPPSettings.
  */
 public interface JabberSettings {
+    /**
+     * Returns true if the user has decided to set the do not disturb option. 
+     */
     public boolean isDoNotDisturbSet();
 }
