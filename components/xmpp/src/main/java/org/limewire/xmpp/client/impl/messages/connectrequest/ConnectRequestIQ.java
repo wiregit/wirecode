b36edb6f04d7ce2de11ad2b8e6bc1f4d3353d5d1
fberger <fberger>
2008-12-03 17:06:21 +0000

    LWC-2040: implement push request over jabber: more logging.

--- components/xmpp/src/main/java/org/limewire/xmpp/client/impl/messages/connectrequest/ConnectRequestIQ.java
+++ components/xmpp/src/main/java/org/limewire/xmpp/client/impl/messages/connectrequest/ConnectRequestIQ.java
@@ -8,6 +8,8 @@ import org.jivesoftware.smack.packet.IQ;
 import org.limewire.io.Connectable;
 import org.limewire.io.GUID;
 import org.limewire.io.NetworkUtils;
+import org.limewire.logging.Log;
+import org.limewire.logging.LogFactory;
 import org.limewire.net.address.ConnectableSerializer;
 import org.limewire.util.Objects;
 import org.limewire.util.StringUtils;
@@ -27,6 +29,8 @@ import org.xmlpull.v1.XmlPullParserException;
  */
 public class ConnectRequestIQ extends IQ {
 
+    private final Log LOG = LogFactory.getLog(ConnectRequestIQ.class);
+    
     private final Connectable address;
     private final int supportedfwtVersion;
     private final GUID clientGuid;
@@ -44,6 +48,7 @@ public class ConnectRequestIQ extends IQ {
        int fwtVersion = -1;
        Connectable connectable = null;
        for (; eventType != XmlPullParser.END_DOCUMENT; eventType = parser.next()) {
+           LOG.debugf("eventType {0}, parser name", eventType, parser.getName());
            if (eventType == XmlPullParser.START_TAG) {
                if (parser.getName().equals("connect-request")) {
                    String value = parser.getAttributeValue(null, "client-guid");
