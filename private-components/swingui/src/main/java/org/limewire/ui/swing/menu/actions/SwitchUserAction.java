2313665d3c8f72e0837deb3fdea72fe9fdac89b4
mrogers <mrogers>
2008-12-05 22:50:30 +0000

    Don't delete the auto-login config when switching users (this will happen if/when the user logs in with another account).

--- private-components/swingui/src/main/java/org/limewire/ui/swing/menu/actions/SwitchUserAction.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/menu/actions/SwitchUserAction.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 
 import org.limewire.ui.swing.action.AbstractAction;
 import org.limewire.ui.swing.friends.login.FriendActions;
-import org.limewire.ui.swing.friends.settings.XMPPAccountConfigurationManager;
 import org.limewire.ui.swing.util.I18n;
 
 import com.google.inject.Inject;
@@ -12,19 +11,15 @@ import com.google.inject.Inject;
 public class SwitchUserAction extends AbstractAction {
 
     private final FriendActions friendActions;
-    private final XMPPAccountConfigurationManager accountManager;
 
     @Inject
-    SwitchUserAction(FriendActions friendActions, XMPPAccountConfigurationManager accountManager) {
-        super(I18n.tr("Switch user"));
+    SwitchUserAction(FriendActions friendActions) {
+        super(I18n.tr("Switch User"));
         this.friendActions = friendActions;
-        this.accountManager = accountManager;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         friendActions.signOut(true);
-        accountManager.setAutoLoginConfig(null);
-        friendActions.signIn();
     }
 }
