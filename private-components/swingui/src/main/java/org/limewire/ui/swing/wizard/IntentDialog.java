9e8ef851af29e5d48b2aaf7c93452d4c268dda18
michaelt <michaelt>
2008-12-08 06:46:43 +0000

    Paints the intent screen.

--- private-components/swingui/src/main/java/org/limewire/ui/swing/wizard/IntentDialog.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/wizard/IntentDialog.java
@@ -1,7 +1,10 @@
 package org.limewire.ui.swing.wizard;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 
@@ -16,15 +19,19 @@ import org.limewire.ui.swing.action.AbstractAction;
 import org.limewire.ui.swing.components.HyperLinkButton;
 import org.limewire.ui.swing.components.LimeJDialog;
 import org.limewire.ui.swing.components.MultiLineLabel;
-import org.limewire.ui.swing.util.FontUtils;
 import org.limewire.ui.swing.util.I18n;
 import org.limewire.ui.swing.util.NativeLaunchUtils;
 import org.limewire.util.SystemUtils;
 
 public class IntentDialog extends LimeJDialog {
 
+    private final Color backgroundColor = Color.WHITE;
+    private final Font headingFont = new Font("Arial", Font.BOLD, 16);
+    private final Font normalFont = new Font("Arial", Font.PLAIN, 14);
+    
+    
     private final String title = I18n.tr("LimeWire 5");
-    private final String heading  = I18n.tr("State your intent");
+    private final String heading  = I18n.tr("State Your Intent");
     
     private final String bodyText1 = I18n.tr("LimeWire Basic and LimeWire PRO are peer-to-peer programs for sharing authorized files only.  " +
     		"Installing and using either program does not constitute a license for obtaining or distributing " +
@@ -58,40 +65,55 @@ public class IntentDialog extends LimeJDialog {
             finish(true);
         }
     };    
-       
-    private Color backgroundColor = Color.WHITE;
     
     public IntentDialog(){
         super();
+        
+        setMinimumSize(new Dimension(590,400));
+        setMaximumSize(new Dimension(590,400));
+        setPreferredSize(new Dimension(590,400));
+        setSize(new Dimension(590,400));
+        
         setTitle(title);
         setModal(true);
-        
-        JPanel panel = new JPanel(new MigLayout());
-        panel.setBackground(backgroundColor);
+        setResizable(false);
+        setAlwaysOnTop(true);
+       
+        JPanel contentPane = new JPanel(new BorderLayout());
+        contentPane.setBackground(backgroundColor);
+        add(contentPane);
+        JPanel panel = new JPanel(new MigLayout("nogrid, insets 0, gap 0"));
+        panel.setOpaque(false);
         
         JLabel headingLabel = new JLabel(heading);
-        FontUtils.bold(headingLabel);
-        
+        headingLabel.setFont(headingFont);
+        JLabel agreeLabel = new MultiLineLabel(agreementText, 500);
+        agreeLabel.setFont(normalFont);
         HyperLinkButton linkButton = new HyperLinkButton(linkText, urlAction);
-        
-        JLabel agreeLabel = new JLabel(agreementText);
-        
-        MultiLineLabel bodyLabel = new MultiLineLabel(bodyText1, agreeLabel.getPreferredSize().width);        
-        
+        linkButton.setFont(normalFont);
+        linkButton.setForeground(new Color(0x2152a6));
+        MultiLineLabel bodyLabel = new MultiLineLabel(bodyText1, 500);        
+        bodyLabel.setFont(normalFont);
         JButton agreeButton = new JButton(agreeAction);
-        
         JButton exitButton = new JButton(exitAction);
         
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        bottomPanel.setOpaque(false);
+        JPanel innerPanel = new JPanel(new FlowLayout());
+        innerPanel.setOpaque(false);
+        innerPanel.add(agreeButton);
+        innerPanel.add(exitButton);
+        bottomPanel.add(innerPanel, BorderLayout.EAST);
         
-        int leftIndent = 50;
-        panel.add(headingLabel, "wrap");
-        panel.add(bodyLabel, "gapleft " + leftIndent +  ", gapright " + leftIndent + ", gaptop 50, wrap");
-        panel.add(linkButton, "gapleft " + leftIndent  +  ", gapright " + leftIndent +  ", wrap");
-        panel.add(agreeLabel, "gapleft " + leftIndent  +  ", gapright " + leftIndent +  ", gaptop 80, gapbottom 100, pushy, wrap");
-        panel.add(agreeButton, "tag ok");
-        panel.add(exitButton, "tag cancel");
+        int indent = 50;
+        panel.add(headingLabel, "gapleft 10, gaptop 15, wrap");
+        panel.add(bodyLabel, "gapleft " + indent + ", gaptop 20, wrap");
+        panel.add(linkButton, "gapleft " + indent +  ", gaptop 20, wrap");
+        panel.add(agreeLabel, "gapleft " + indent +  ", gaptop 70, wrap");
+                
+        contentPane.add(panel, BorderLayout.NORTH);
+        contentPane.add(bottomPanel, BorderLayout.SOUTH);
         
-        add(panel);
         pack();        
     }
     
