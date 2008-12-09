954f30fbeae1a193f0f24c1794d539a63bfb7b12
sberlin <sberlin>
2008-12-09 14:45:51 +0000

    rm another column from remote format

--- private-components/swingui/src/main/java/org/limewire/ui/swing/library/table/RemoteDocumentTableFormat.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/library/table/RemoteDocumentTableFormat.java
@@ -5,19 +5,19 @@ import java.util.Date;
 
 import org.limewire.core.api.FilePropertyKey;
 import org.limewire.core.api.library.FileItem;
+import org.limewire.core.api.library.RemoteFileItem;
 import org.limewire.ui.swing.util.I18n;
 
 /**
  * Table format for the Document Table for LW buddies and Browse hosts
  */
-public class RemoteDocumentTableFormat<T extends FileItem> extends AbstractRemoteLibraryFormat<T> {
+public class RemoteDocumentTableFormat<T extends RemoteFileItem> extends AbstractRemoteLibraryFormat<T> {
     public static final int NAME_COL = 0;
     public static final int TYPE_COL = NAME_COL + 1;
     public static final int CREATED_COL = TYPE_COL + 1;
     public static final int SIZE_COL = CREATED_COL + 1;
     public static final int AUTHOR_COL = SIZE_COL + 1;
-    public static final int MODIFIED_COL = AUTHOR_COL + 1;
-    public static final int ACTION_COL = MODIFIED_COL + 1;
+    public static final int ACTION_COL = AUTHOR_COL + 1;
     private static final int COLUMN_COUNT = ACTION_COL + 1;
 
     @Override
@@ -32,8 +32,6 @@ public class RemoteDocumentTableFormat<T extends FileItem> extends AbstractRemot
                  return I18n.tr("Author");
              case CREATED_COL:
                  return I18n.tr("Created");
-             case MODIFIED_COL:
-                 return I18n.tr("Modified");
              case NAME_COL:
                  return I18n.tr("Name");
              case ACTION_COL:
@@ -53,8 +51,6 @@ public class RemoteDocumentTableFormat<T extends FileItem> extends AbstractRemot
                  return baseObject.getProperty(FilePropertyKey.AUTHOR);
              case CREATED_COL:
                  return new Date(baseObject.getCreationTime());
-             case MODIFIED_COL:
-                 return new Date();
              case NAME_COL:
                  return baseObject;
              case ACTION_COL:
@@ -74,7 +70,7 @@ public class RemoteDocumentTableFormat<T extends FileItem> extends AbstractRemot
 
     @Override
     public int[] getDefaultHiddenColums() {
-        return new int[]{MODIFIED_COL, AUTHOR_COL, SIZE_COL};
+        return new int[]{ AUTHOR_COL, SIZE_COL};
     }
 
     @Override
