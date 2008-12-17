e20ce0cf28f47231a328e011a9c2895e537cb668
meverett <meverett>
2008-12-17 21:34:42 +0000

    Options searching ui changes

--- private-components/swingui/src/main/java/org/limewire/ui/swing/options/SearchOptionPanel.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/options/SearchOptionPanel.java
@@ -3,6 +3,7 @@ package org.limewire.ui.swing.options;
 import java.awt.Component;
 
 import javax.swing.DefaultListCellRenderer;
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
@@ -28,10 +29,17 @@ public class SearchOptionPanel extends OptionPanel {
 
     private SearchBarPanel searchBarPanel;
 
+    private JCheckBox groupSimilarResults;
+
     @Inject
     public SearchOptionPanel() {
+        groupSimilarResults = new JCheckBox(I18n.tr("Group similar search results together"));
+        groupSimilarResults.setContentAreaFilled(false);
+        
         setLayout(new MigLayout("insets 15 15 15 15, fillx, wrap", "", ""));
         add(getSearchBarPanel(), "pushx, growx");
+        
+        add(groupSimilarResults, "gaptop 10, gapleft 15");
     }
 
     private OptionPanel getSearchBarPanel() {
@@ -43,46 +51,46 @@ public class SearchOptionPanel extends OptionPanel {
 
     @Override
     boolean applyOptions() {
+        SearchSettings.GROUP_SIMILAR_RESULTS_ENABLED.setValue(groupSimilarResults.isSelected());
         return getSearchBarPanel().applyOptions();
     }
 
     @Override
     boolean hasChanged() {
-        return getSearchBarPanel().hasChanged();
+        return getSearchBarPanel().hasChanged() 
+        || groupSimilarResults.isSelected() != SearchSettings.GROUP_SIMILAR_RESULTS_ENABLED.getValue();
     }
 
     @Override
     public void initOptions() {
         getSearchBarPanel().initOptions();
+        
+        groupSimilarResults.setSelected(SearchSettings.GROUP_SIMILAR_RESULTS_ENABLED.getValue());
     }
 
     private class SearchBarPanel extends OptionPanel {
 
         private JComboBox defaultSearchSpinner;
 
-//        private JCheckBox searchTabNumberCheckBox;
-//
-//        private JCheckBox suggestFriendFiles;
+        private JCheckBox searchTabNumberCheckBox;
 
-        private JCheckBox groupSimilarResults;
+        private JCheckBox suggestFriendFiles;
 
-//        private JButton clearNowButton;
+        private JButton clearNowButton;
 
         public SearchBarPanel() {
-            super(I18n.tr("Searching"));
-            setLayout(new MigLayout("", "", "[][][]30[]"));
+            super(I18n.tr("Search bar"));
+            setLayout(new MigLayout("", "", "[][][]"));
             createComponents();
 
             add(new JLabel(I18n.tr("By default, search for")), "split");
             add(defaultSearchSpinner, "wrap");
 
-//            add(suggestFriendFiles, "split 4, wrap");
-//
-//            add(searchTabNumberCheckBox, "split 4, push");
-//
-//            add(clearNowButton, "wrap, alignx right");
+            add(suggestFriendFiles, "split 4, wrap");
 
-            add(groupSimilarResults, "split 4");
+            add(searchTabNumberCheckBox, "split 4, push");
+
+            add(clearNowButton, "wrap, alignx right");
         }
 
         private void createComponents() {
@@ -94,18 +102,15 @@ public class SearchOptionPanel extends OptionPanel {
                     LibrarySettings.ALLOW_PROGRAMS, SearchCategory.PROGRAM,
                     defaultSearchSpinner));
 
-//            suggestFriendFiles = new JCheckBox(I18n.tr("Show suggestions from my friends when signed on"));
-//            suggestFriendFiles.setContentAreaFilled(false);
-//
-//            searchTabNumberCheckBox = new JCheckBox(I18n.tr("Remember my recent searches"));
-//            searchTabNumberCheckBox.setContentAreaFilled(false);
-            
-            groupSimilarResults = new JCheckBox(I18n.tr("Group similar search results together"));
-            groupSimilarResults.setContentAreaFilled(false);
+            //TODO: suggest files, search tab number, clear now not tied functionaly to the UI anywhere
+            suggestFriendFiles = new JCheckBox(I18n.tr("Suggestions files from friends when signed on"));
+            suggestFriendFiles.setContentAreaFilled(false);
 
-//            clearNowButton = new JButton(I18n.tr("Clear Now"));
-//            //TODO this button should do something
-//            clearNowButton.setBorderPainted(false);
+            searchTabNumberCheckBox = new JCheckBox(I18n.tr("Remember my recent searches"));
+            searchTabNumberCheckBox.setContentAreaFilled(false);
+            
+            clearNowButton = new JButton(I18n.tr("Clear Now"));
+            clearNowButton.setBorderPainted(false);
         }
 
         @Override
@@ -113,35 +118,24 @@ public class SearchOptionPanel extends OptionPanel {
             SearchSettings.DEFAULT_SEARCH_CATEGORY_ID
                     .setValue(((SearchCategory) defaultSearchSpinner.getSelectedItem()).getId());
             
-//            SearchSettings.POPULATE_SEARCH_BAR_FRIEND_FILES.setValue(suggestFriendFiles.isSelected());
-//
-//            SearchSettings.REMEMBER_OLD_SEARCHES_SEARCH_BAR.setValue(searchTabNumberCheckBox
-//                    .isSelected());
-            SearchSettings.GROUP_SIMILAR_RESULTS_ENABLED.setValue(groupSimilarResults.isSelected());
+            SearchSettings.POPULATE_SEARCH_BAR_FRIEND_FILES.setValue(suggestFriendFiles.isSelected());
+
+            SearchSettings.REMEMBER_OLD_SEARCHES_SEARCH_BAR.setValue(searchTabNumberCheckBox.isSelected());
             return false;
         }
 
         @Override
         boolean hasChanged() {
-            return SearchSettings.DEFAULT_SEARCH_CATEGORY_ID.getValue() != ((SearchCategory) defaultSearchSpinner
-                    .getSelectedItem()).getId()
-//                    || SearchSettings.POPULATE_SEARCH_BAR_FRIEND_FILES.getValue() != suggestFriendFiles.isSelected()
-//                    || SearchSettings.REMEMBER_OLD_SEARCHES_SEARCH_BAR.getValue() != searchTabNumberCheckBox
-//                            .isSelected()
-                    || groupSimilarResults.isSelected() != SearchSettings.GROUP_SIMILAR_RESULTS_ENABLED
-                            .getValue();
+            return SearchSettings.DEFAULT_SEARCH_CATEGORY_ID.getValue() != ((SearchCategory) defaultSearchSpinner.getSelectedItem()).getId()
+                    || SearchSettings.POPULATE_SEARCH_BAR_FRIEND_FILES.getValue() != suggestFriendFiles.isSelected()
+                    || SearchSettings.REMEMBER_OLD_SEARCHES_SEARCH_BAR.getValue() != searchTabNumberCheckBox.isSelected();
         }
 
         @Override
         public void initOptions() {
-            defaultSearchSpinner.setSelectedItem(SearchCategory
-                    .forId(SearchSettings.DEFAULT_SEARCH_CATEGORY_ID.getValue()));
-//            suggestFriendFiles.setSelected(SearchSettings.POPULATE_SEARCH_BAR_FRIEND_FILES.getValue());
-//            // TODO: this setting isn't linked to anything
-//            searchTabNumberCheckBox.setSelected(SearchSettings.REMEMBER_OLD_SEARCHES_SEARCH_BAR
-//                    .getValue());
-            groupSimilarResults
-                    .setSelected(SearchSettings.GROUP_SIMILAR_RESULTS_ENABLED.getValue());
+            defaultSearchSpinner.setSelectedItem(SearchCategory.forId(SearchSettings.DEFAULT_SEARCH_CATEGORY_ID.getValue()));
+            suggestFriendFiles.setSelected(SearchSettings.POPULATE_SEARCH_BAR_FRIEND_FILES.getValue());
+            searchTabNumberCheckBox.setSelected(SearchSettings.REMEMBER_OLD_SEARCHES_SEARCH_BAR.getValue());
         }
     }
     
@@ -149,13 +143,7 @@ public class SearchOptionPanel extends OptionPanel {
         public CategoryCellRenderer() {
             setOpaque(true);
         }
-        public Component getListCellRendererComponent(
-            JList list,
-            Object value,
-            int index,
-            boolean isSelected,
-            boolean cellHasFocus)
-        {
+        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
             super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
             SearchCategory category = (SearchCategory)value;
             setText(SearchCategoryUtils.getOptionsName(category));
