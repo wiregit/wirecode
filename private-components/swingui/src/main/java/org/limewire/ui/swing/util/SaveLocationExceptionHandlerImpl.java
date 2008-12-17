d726586d172c90d43196deefa6f399d8ebba4343
eyu <eyu>
2008-12-17 21:29:03 +0000

    LWC-2240 revised to display user message when exception not handled; refactored to execute on UI thread

--- private-components/swingui/src/main/java/org/limewire/ui/swing/util/SaveLocationExceptionHandlerImpl.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/util/SaveLocationExceptionHandlerImpl.java
@@ -7,9 +7,11 @@ import java.io.File;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JToggleButton;
+import javax.swing.SwingUtilities;
 
 import net.miginfocom.swing.MigLayout;
 
@@ -17,25 +19,64 @@ import org.limewire.core.api.download.DownloadAction;
 import org.limewire.core.api.download.SaveLocationException;
 import org.limewire.core.api.download.SaveLocationManager;
 import org.limewire.core.settings.DownloadSettings;
+import org.limewire.logging.Log;
+import org.limewire.logging.LogFactory;
+import org.limewire.ui.swing.components.FocusJOptionPane;
 import org.limewire.ui.swing.components.LimeJDialog;
 import org.limewire.ui.swing.components.MultiLineLabel;
 import org.limewire.util.FileUtils;
-import org.limewire.util.NotImplementedException;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
+/**
+ * A universal handler for SaveLocationException messages generated while
+ * performing downloads.
+ */
 @Singleton
 public class SaveLocationExceptionHandlerImpl implements SaveLocationExceptionHandler {
+    private static final Log LOG = LogFactory.getLog(SaveLocationExceptionHandlerImpl.class);
 
     private final SaveLocationManager saveLocationManager;
-    
+
+    /**
+     * Constructs a SaveLocationExceptionHandler with the specified
+     * SaveLocationManager.
+     */
     @Inject
     public SaveLocationExceptionHandlerImpl(SaveLocationManager saveLocationManager) {
         this.saveLocationManager = saveLocationManager;
     }
     
+    /**
+     * Handles the supplied SaveLocationException.  The method may take one of
+     * several actions: eat the exception, try downloading again using the 
+     * supplied <code>downloadAction</code>, or popup a dialog to try and save
+     * the download in a new location.
+     */
     public void handleSaveLocationException(final DownloadAction downLoadAction,
+            final SaveLocationException sle, final boolean supportNewSaveDir) {
+        
+        if (SwingUtilities.isEventDispatchThread()) {
+            handleException(downLoadAction, sle, supportNewSaveDir);
+            
+        } else {
+            // Post Runnable on event queue to execute on UI thread.  This is
+            // necessary if the handler method has been invoked from a
+            // background thread.
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    handleException(downLoadAction, sle, supportNewSaveDir);
+                }
+            });
+        }
+    }
+
+    /**
+     * Handles the specified SaveLocationException.  The method may prompt the
+     * user for input, and should be executed from the UI thread.
+     */
+    private void handleException(final DownloadAction downLoadAction,
             final SaveLocationException sle, final boolean supportNewSaveDir) {
 
         if (sle.getErrorCode() == SaveLocationException.LocationCode.FILE_ALREADY_DOWNLOADING) {
@@ -43,10 +84,19 @@ public class SaveLocationExceptionHandlerImpl implements SaveLocationExceptionHa
             return;
         }
 
-        if (sle.getErrorCode() != SaveLocationException.LocationCode.FILE_ALREADY_EXISTS
-                && sle.getErrorCode() != SaveLocationException.LocationCode.FILE_IS_ALREADY_DOWNLOADED_TO) {
-            // TODO better user feedback
-            throw new NotImplementedException(sle);
+        if ((sle.getErrorCode() != SaveLocationException.LocationCode.FILE_ALREADY_EXISTS)
+            && (sle.getErrorCode() != SaveLocationException.LocationCode.FILE_IS_ALREADY_DOWNLOADED_TO)) {
+            // Create user message.
+            StringBuilder buf = new StringBuilder(I18n.tr("Unable to download"));
+            buf.append(": ").append(sle.getErrorCode());
+            buf.append("\nfile ").append(sle.getFile());
+            
+            // Log exception and display user message.
+            LOG.error(buf.toString(), sle);
+            FocusJOptionPane.showMessageDialog(GuiUtils.getMainFrame(), 
+                    buf.toString(), I18n.tr("Download"), 
+                    JOptionPane.INFORMATION_MESSAGE);
+            return;
         }
 
         File saveFile = null;
