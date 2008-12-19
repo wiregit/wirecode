c8b68eeb1a12ff7b5cf4afd8c3aae88d433dfd87
maquino <maquino>
2008-12-19 12:27:32 +0000

    Removed vertical gap between copy to clipboard and more info links.

--- private-components/swingui/src/main/java/org/limewire/ui/swing/properties/Dialog.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/properties/Dialog.java
@@ -119,12 +119,12 @@ public abstract class Dialog extends LimeJDialog {
         buttons.add(new JButton(new CancelAction()));
         mainPanel.add(buttons, "alignx right, cell 1 4");
         
-        overview = newPanel(new MigLayout("fillx", "[][]push[]", "[][][]"));
+        overview = newPanel(new MigLayout("fillx", "[][]push[]", "[]0[]"));
 
         overview.add(icon);
         overview.add(heading);
         overview.add(copyToClipboard, "wrap");
-        overview.add(metadata, "cell 1 2");
+        overview.add(metadata, "cell 1 1");
         overview.add(moreFileInfo);
 
         addOverview();
