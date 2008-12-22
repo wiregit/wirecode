49f889f3324a870bdd772c1c57ee47c4a2318e56
pvertenten <pvertenten>
2008-12-22 20:26:45 +0000

    Removing use of additional Cancelled state.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/uploader/HTTPUploader.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/uploader/HTTPUploader.java
@@ -87,9 +87,6 @@ public class HTTPUploader extends AbstractUploader implements Uploader {
         if (getSession().getIOSession() != null) {
             getSession().getIOSession().shutdown();
         }
-        if(getState() != UploadStatus.COMPLETE) {
-            setState(UploadStatus.CANCELLED);
-        }
 	}
 	
 	/**
