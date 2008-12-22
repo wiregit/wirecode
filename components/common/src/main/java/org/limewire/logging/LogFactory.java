f035759f4c6d8098e441619e564e398fa6725b05
tjulien <tjulien>
2008-12-22 22:42:24 +0000

    category Logging support

--- components/common/src/main/java/org/limewire/logging/LogFactory.java
+++ components/common/src/main/java/org/limewire/logging/LogFactory.java
@@ -12,10 +12,30 @@ public class LogFactory {
     
     private static Map<org.apache.commons.logging.Log, Log> logs = new IdentityHashMap<org.apache.commons.logging.Log, Log>();
 
+    /**
+     * @param clazz; can not be null
+     * @return a Log named by the fully qualifed class name of clazz
+     */
     public static Log getLog(Class clazz) {
+        return getLog(clazz, null);
+    }
+
+    /**
+     * @param clazz; cannot be null
+     * @param category; can be null
+     * @return a log with the name <code>category.classname</code>
+     * where <code>classname</code> is the non-qualified name of the 
+     * class.
+     */
+    public static Log getLog(Class clazz, String category) {
         // not synchronized as LogFactory is also not synchronized, Logs are created
         // at class load time which is synchronized
-        org.apache.commons.logging.Log log = org.apache.commons.logging.LogFactory.getLog(clazz);
+        org.apache.commons.logging.Log log;
+        if (category == null) {
+            log = org.apache.commons.logging.LogFactory.getLog(clazz);
+        } else {
+            log = org.apache.commons.logging.LogFactory.getLog(category + "." + clazz.getSimpleName());
+        }
         Log decorator = logs.get(log);
         if (decorator == null) {
             decorator = new LogImpl(log);
