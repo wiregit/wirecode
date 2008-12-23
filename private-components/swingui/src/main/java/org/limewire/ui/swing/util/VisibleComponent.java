e47752f627fe2cda77e18819855ece51c9af48b0
eyu <eyu>
2008-12-23 20:44:22 +0000

    LWC-2190 added API for actionEnabled listener and state

--- private-components/swingui/src/main/java/org/limewire/ui/swing/util/VisibleComponent.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/util/VisibleComponent.java
@@ -1,6 +1,28 @@
 package org.limewire.ui.swing.util;
 
+/**
+ * Defines a component with that can be made visible or invisible, and enabled
+ * or disabled for use.
+ */
 public interface VisibleComponent {
+
+    /**
+     * Adds the specified listener to the list that is notified when the 
+     * action enabled state changes.
+     */
+    void addEnabledListener(EnabledListener listener);
+
+    /**
+     * Removes the specified listener from the list that is notified when the 
+     * action enabled state changes.
+     */
+    void removeEnabledListener(EnabledListener listener);
+    
+    /**
+     * Returns true if the component is enabled for use. 
+     */
+    boolean isActionEnabled();
+    
     /**
      * Sets the visibility of this component.
      */
