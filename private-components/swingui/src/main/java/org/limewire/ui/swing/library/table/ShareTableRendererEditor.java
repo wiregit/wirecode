d8312266295dfddc07caf1ad0d191a1bf69b2fed
meverett <meverett>
2009-01-04 19:06:39 +0000

    LWC-2409: friends --> friend when sharing with only one person in my library

--- private-components/swingui/src/main/java/org/limewire/ui/swing/library/table/ShareTableRendererEditor.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/library/table/ShareTableRendererEditor.java
@@ -100,7 +100,10 @@ public class ShareTableRendererEditor extends TableRendererEditor implements Con
         }
         
         if(friendCount > 0) {   
-            shareButton.setText(I18n.tr("{0} friends",GuiUtils.toLocalizedInteger(item.getFriendShareCount())));
+            if(friendCount == 1)
+                shareButton.setText(I18n.tr("{0} friend",GuiUtils.toLocalizedInteger(item.getFriendShareCount())));
+            else
+                shareButton.setText(I18n.tr("{0} friends",GuiUtils.toLocalizedInteger(item.getFriendShareCount())));
             shareButton.setIcon(downIcon);
             shareButton.removeUnderline();
         } else {
