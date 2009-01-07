c8d3bb157c38a95c882e0156687cae8c4c2acf2f
meverett <meverett>
2009-01-07 20:19:18 +0000

    various changes for LWC-2377

--- private-components/swingui/src/main/java/org/limewire/ui/swing/search/resultpanel/classic/AllTableFormat.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/resultpanel/classic/AllTableFormat.java
@@ -2,15 +2,21 @@ package org.limewire.ui.swing.search.resultpanel.classic;
 
 import java.awt.Component;
 
+import org.limewire.core.api.Category;
 import org.limewire.ui.swing.search.model.VisualSearchResult;
 import org.limewire.ui.swing.search.resultpanel.ResultsTableFormat;
 import org.limewire.ui.swing.table.ColumnStateInfo;
 import org.limewire.ui.swing.util.I18n;
+import org.limewire.ui.swing.util.IconManager;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
 
 /**
  * This class specifies the content of a table that contains
  * objects representing any kind of media.
  */
+@Singleton
 public class AllTableFormat extends ResultsTableFormat<VisualSearchResult> {
     static final int FROM_INDEX = 0;
     static final int NAME_INDEX = 1;
@@ -18,7 +24,10 @@ public class AllTableFormat extends ResultsTableFormat<VisualSearchResult> {
     static final int TYPE_INDEX = 3;
     public static final int SIZE_INDEX = 4;
     
-    public AllTableFormat() {
+    private final IconManager iconManager;
+    
+    @Inject
+    public AllTableFormat(IconManager iconManager) {
         super(NAME_INDEX, FROM_INDEX, new ColumnStateInfo[] {
                 new ColumnStateInfo(FROM_INDEX, "CLASSIC_SEARCH_ALL_FROM", I18n.tr("From"), 55, true, true), 
                 new ColumnStateInfo(NAME_INDEX, "CLASSIC_SEARCH_ALL_NAME", I18n.tr("Name"), 550, true, true),     
@@ -26,6 +35,8 @@ public class AllTableFormat extends ResultsTableFormat<VisualSearchResult> {
                 new ColumnStateInfo(TYPE_INDEX, "CLASSIC_SEARCH_ALL_TYPE", I18n.tr("Type"), 60, true, true), 
                 new ColumnStateInfo(SIZE_INDEX, "CLASSIC_SEARCH_ALL_SIZE", I18n.tr("Size"), 60, true, true)
         });
+        
+        this.iconManager = iconManager;
     }
     
     @Override
@@ -43,7 +54,15 @@ public class AllTableFormat extends ResultsTableFormat<VisualSearchResult> {
         switch(column) {
             case FROM_INDEX: return vsr;
             case NAME_INDEX: return vsr;
-            case TYPE_INDEX: return vsr.getCategory();
+            case TYPE_INDEX: 
+                if( vsr.getCategory() == Category.DOCUMENT || vsr.getCategory() == Category.PROGRAM || vsr.getCategory() == Category.OTHER) {
+                    String mime = iconManager.getMIMEDescription(vsr.getFileExtension());
+                    if(mime != null)
+                        return I18n.tr(vsr.getCategory().getSingularName()) + " (" + mime + ")";
+                    else
+                        return I18n.tr(vsr.getCategory().getSingularName());
+                } else
+                    return I18n.tr(vsr.getCategory().getSingularName());
             case SIZE_INDEX: return vsr.getSize();
             case EXTENSION_INDEX: return vsr.getFileExtension();
         }
