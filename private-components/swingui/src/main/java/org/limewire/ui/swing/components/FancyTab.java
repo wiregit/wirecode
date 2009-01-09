64164b296bbe2b6083897bff05727e1da0da3903
michaelt <michaelt>
2009-01-09 20:22:48 +0000

    final fancy tab?

--- private-components/swingui/src/main/java/org/limewire/ui/swing/components/FancyTab.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/components/FancyTab.java
@@ -115,9 +115,10 @@ public class FancyTab extends JXPanel {
         setLayout(new MigLayout("insets 0 0 10 0, fill, gap 0"));        
         add(mainButton,     "gapbefore 6, aligny bottom, width min(pref,50):pref:max, cell 1 0");
         add(additionalText, "gapbefore 2, gapafter 4, aligny bottom, cell 2 0, hidemode 3");
-        add(busyLabel,      "gapbefore 4, gapafter 6, aligny bottom, alignx right, cell 3 0, hidemode 3");
-        add(removeButton,   "gapbefore 4, gapafter 6, aligny bottom, alignx right, cell 3 0, hidemode 3");
+        add(busyLabel,      "gapbefore 4, gapafter 6, gapbottom 1, aligny bottom, alignx right, cell 3 0, hidemode 3");
+        add(removeButton,   "gapbefore 4, gapafter 6, gapbottom 1, aligny bottom, alignx right, cell 3 0, hidemode 3");
     }
+    
     
     @Override
     public String toString() {
