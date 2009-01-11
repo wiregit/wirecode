26d044a8abceead1cb89d2e07b02305c039dabbe
mturkel <mturkel>
2009-01-11 19:23:48 +0000

    LWC-2525

--- private-components/swingui/src/main/java/org/limewire/ui/swing/library/sharing/MultiFileShareWidget.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/library/sharing/MultiFileShareWidget.java
@@ -16,7 +16,6 @@ public class MultiFileShareWidget implements ShareWidget<LocalFileItem[]>{
     public MultiFileShareWidget(ShareListManager shareListManager, Collection<Friend> allFriends, ShapeDialog shapeDialog){
         sharePanel = new LibrarySharePanel(allFriends, shapeDialog);
         sharePanel.setShareModel(new MultiFileShareModel(shareListManager));
-        sharePanel.setTableVisible(false);
     }
     
     @Override
@@ -28,8 +27,9 @@ public class MultiFileShareWidget implements ShareWidget<LocalFileItem[]>{
     public void setShareable(LocalFileItem[] files){
         ((MultiFileShareModel)sharePanel.getShareModel()).setFileItems(files);
         sharePanel.setTitleLabel(I18n.tr("Share {0} {1} files", files.length, files[0].getCategory().toString()));
+        sharePanel.setTopLabel(I18n.tr("Shared {0} files with:", files.length));
     }
-    
+
     @Override
     public void dispose() {
         sharePanel.dispose();
