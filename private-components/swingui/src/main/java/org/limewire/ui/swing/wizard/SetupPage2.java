60c527b8f33ec154efc28f6fc277800b2a9fa881
msorvillo <msorvillo>
2009-01-14 15:58:20 +0000

    committing alignment stuff

--- private-components/swingui/src/main/java/org/limewire/ui/swing/wizard/SetupPage2.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/wizard/SetupPage2.java
@@ -112,7 +112,7 @@ public class SetupPage2 extends WizardPage {
         
         JLabel label;
 
-        add(autoButton, "gaptop 10, gapleft 40");
+        add(autoButton, "gaptop 15, gapleft 40");
         if (isUpgrade) {
             label = new JLabel(autoTextUpgrade);
         } 
@@ -121,7 +121,7 @@ public class SetupPage2 extends WizardPage {
         }
         label.addMouseListener(new SetupComponentDecorator.ToggleExtenderListener(autoButton));
         decorator.decorateHeadingText(label);
-        add(label, "gaptop 5, gapleft 10, wrap");
+        add(label, "gaptop 15, gapleft 10, wrap");
         
         label = new MultiLineLabel(autoExplanation, 500);
         decorator.decorateNormalText(label);
@@ -138,7 +138,7 @@ public class SetupPage2 extends WizardPage {
                 }
             }
         });
-        add(manualButton, "gaptop 10, gapleft 40");
+        add(manualButton, "gaptop 25, gapleft 40");
         
         if (isUpgrade) {
             label = new JLabel(manualTextUpgrade);
@@ -149,7 +149,7 @@ public class SetupPage2 extends WizardPage {
 
         decorator.decorateHeadingText(label);
         label.addMouseListener(new SetupComponentDecorator.ToggleExtenderListener(manualButton));
-        add(label, "gaptop 5, gapleft 10, wrap");
+        add(label, "gaptop 25, gapleft 10, wrap");
 
         add(manualLabel, "gapleft 76, wrap");
         add(checkBoxes, "gaptop 5, gapleft 76, growx, wrap");
