2631d4a49e0cf0c4508a7d504d7eaa94a8773e64
michaelt <michaelt>
2009-01-14 19:23:17 +0000

    LWC-2629

--- private-components/swingui/src/main/java/org/limewire/ui/swing/components/HorizonalCheckBoxListPanel.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/components/HorizonalCheckBoxListPanel.java
@@ -10,6 +10,8 @@ import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
+import org.limewire.ui.swing.util.I18n;
+
 import net.miginfocom.swing.MigLayout;
 
 /**
@@ -23,10 +25,15 @@ public class HorizonalCheckBoxListPanel<K> extends JPanel {
         this(options, null);
     }
     
+    
+    public HorizonalCheckBoxListPanel(Collection<K> options, Collection<K> selected) {
+       this(options, selected, true); 
+    }
+    
     /**
      * Creates the panel and selects the set of boxes
      */
-    public HorizonalCheckBoxListPanel(Collection<K> options, Collection<K> selected) {
+    public HorizonalCheckBoxListPanel(Collection<K> options, Collection<K> selected, boolean translate) {
         
         setLayout(new MigLayout("gapx 5"));
         setOpaque(false);
@@ -34,7 +41,7 @@ public class HorizonalCheckBoxListPanel<K> extends JPanel {
         optionsMap = new HashMap<K,JCheckBox>();
         
         for ( K key : options ) {
-            JCheckBox box = new JCheckBox(key.toString());
+            JCheckBox box = new JCheckBox(translate ? I18n.tr(key.toString()) : key.toString() );
             box.setOpaque(false);
             box.setHorizontalTextPosition(SwingConstants.RIGHT);
             box.setSelected(selected != null && selected.contains(key));
