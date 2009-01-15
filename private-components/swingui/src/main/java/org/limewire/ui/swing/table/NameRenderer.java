f61cfe49a26db82a22b3b17b7bc03b55564314c7
pvertenten <pvertenten>
2009-01-15 23:35:34 +0000

    Checking for null value in NameRenderer.

--- private-components/swingui/src/main/java/org/limewire/ui/swing/table/NameRenderer.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/table/NameRenderer.java
@@ -27,8 +27,12 @@ public class NameRenderer extends DefaultLibraryRenderer {
             boolean hasFocus, int row, int column) {
         super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
 
-        LocalFileItem localFileItem = (LocalFileItem) value;
-        String name = getName(localFileItem);
+        String name = null;
+        if (value != null && value instanceof LocalFileItem) {
+            LocalFileItem localFileItem = (LocalFileItem) value;
+            name = getName(localFileItem);
+        }
+        
         if (name != null) {
             setText(name);
         } else {
@@ -43,15 +47,15 @@ public class NameRenderer extends DefaultLibraryRenderer {
 
         if (localFileItem.getCategory() == Category.AUDIO) {
             name = localFileItem.getPropertyString(FilePropertyKey.TITLE);
-        } else if( localFileItem.getCategory() == Category.VIDEO) {
+        } else if (localFileItem.getCategory() == Category.VIDEO) {
             name = localFileItem.getFileName();
         }
 
         if (name == null) {
             name = localFileItem.getPropertyString(FilePropertyKey.NAME);
         }
-        
-        if(name == null) {
+
+        if (name == null) {
             name = localFileItem.getFileName();
         }
 
