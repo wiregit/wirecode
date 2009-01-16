edc6262535b63575538693919e308ea633532e99
eyu <eyu>
2009-01-16 16:46:20 +0000

    LWC-2424 revised to define getDefaultSortKeys method

--- private-components/swingui/src/main/java/org/limewire/ui/swing/util/EventListTableSortFormat.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/util/EventListTableSortFormat.java
@@ -2,24 +2,28 @@ package org.limewire.ui.swing.util;
 
 import java.util.List;
 
+import org.jdesktop.swingx.decorator.SortKey;
+
 /**
  * Defines the sorting behavior for tables using event lists.
  */
 public interface EventListTableSortFormat {
 
     /**
-     * Returns the default sort column.  The method returns -1 if the default
-     * sort column is not specified.
+     * Returns a List of sort keys that define the initial sort order.  The 
+     * keys are listed in order of significance, with index 0 being the most 
+     * significant.  The method returns an empty list if there is no default 
+     * sort order.
      * 
-     * @return the model index of the default column
+     * @return List of sort keys for initial sort order
      */
-    int getDefaultSortColumn();
+    List<SortKey> getDefaultSortKeys();
 
     /**
      * Returns a List of the secondary sort columns associated with the 
-     * specified column.  The columns are listed in order of significance, so
-     * index 0 is most significant.  An empty list is returned if there are no
-     * secondary sort columns; the method never returns null.
+     * specified column.  The columns are listed in order of significance, with
+     * index 0 being the most significant.  The method returns an empty list if
+     * there are no secondary sort columns.
      * 
      * @param column the model index of the primary column
      * @return List of model indices for secondary columns
