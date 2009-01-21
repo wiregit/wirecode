95b7b6283f9cf7a54ae5070a80cda9c8ebf8b49d
fberger <fberger>
2009-01-21 15:23:54 +0000

    scan for flv.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/library/LibraryFileData.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/library/LibraryFileData.java
@@ -49,7 +49,7 @@ class LibraryFileData extends AbstractSettingsGroup {
     /** Default file extensions. */
     private final String DEFAULT_MANAGED_EXTENSIONS_STRING =
         "asx;html;htm;xml;txt;pdf;ps;rtf;doc;tex;mp3;mp4;wav;wax;au;aif;aiff;"+
-        "ra;ram;wma;wm;wmv;mp2v;mlv;mpa;mpv2;mid;midi;rmi;aifc;snd;flac;fla;"+
+        "ra;ram;wma;wm;wmv;mp2v;mlv;mpa;mpv2;mid;midi;rmi;aifc;snd;flac;fla;flv;"+
         "mpg;mpeg;asf;qt;mov;avi;mpe;swf;dcr;gif;jpg;jpeg;jpe;png;tif;tiff;bmp;"+
         "exe;zip;gz;gzip;hqx;tar;tgz;z;rmj;lqt;rar;ace;sit;smi;img;ogg;rm;"+
         "bin;dmg;jve;nsv;med;mod;7z;iso;lwtp;pmf;m4a;bz2;sea;pf;arc;arj;"+
