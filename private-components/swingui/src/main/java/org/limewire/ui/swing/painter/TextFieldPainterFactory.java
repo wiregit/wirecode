ad0ccb648e0f3b73c0fb22250efb16b93aff4272
eyu <eyu>
2009-01-26 23:00:10 +0000

    implemented createClearableBackgroundPainter() for filter fields

--- private-components/swingui/src/main/java/org/limewire/ui/swing/painter/TextFieldPainterFactory.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/painter/TextFieldPainterFactory.java
@@ -6,6 +6,9 @@ import java.awt.Paint;
 import javax.swing.JTextField;
 
 import org.jdesktop.application.Resource;
+import org.jdesktop.swingx.painter.CompoundPainter;
+import org.jdesktop.swingx.painter.Painter;
+import org.limewire.ui.swing.components.LimePromptTextField;
 import org.limewire.ui.swing.painter.BorderPainter.AccentType;
 import org.limewire.ui.swing.util.GuiUtils;
 
@@ -41,14 +44,40 @@ public class TextFieldPainterFactory {
                 bevelTop2, bevelRight, bevelBottom, arcWidth, arcHeight,
                 accentType);
     }
- 
-    public FilterPainter<JTextField> createClearableBackgroundPainter() {
-        // TODO: ernie
-        // return new FilterPainter<JTextField>(null);
-        return null;
+
+    /**
+     * Creates a background painter for the specified text field using the 
+     * specified accent type.  The returned painter includes an X icon that may
+     * be clicked to clear the field, and is suitable for filter fields. 
+     */
+    public Painter<JTextField> createClearableBackgroundPainter(
+            LimePromptTextField textField, AccentType accentType) {
+        
+        // Create filter painter.
+        FilterPainter<JTextField> filterPainter = new FilterPainter<JTextField>(
+                arcWidth, arcHeight);
+        
+        // Install filter painter on text field.
+        filterPainter.install(textField);
+        
+        // Create compound painter using area and border painters.
+        CompoundPainter<JTextField> painter = new CompoundPainter<JTextField>();
+        painter.setPainters(filterPainter, createBorderPainter(accentType));
+        painter.setCacheable(true);
+        
+        return painter;
     }
     
     public BasicTextFieldPromptPainter<JTextField> createBasicPromptPainter() {
         return new BasicTextFieldPromptPainter<JTextField>(promptForeground);
+    }
+
+    /**
+     * Creates a BorderPainter using the specified accent type.
+     */
+    private BorderPainter<JTextField> createBorderPainter(AccentType accentType) {
+        return new BorderPainter<JTextField>(arcWidth, arcHeight, border,
+                bevelLeft,  bevelTop1,  bevelTop2, bevelRight,  bevelBottom,
+                accentType);
     }
 }
