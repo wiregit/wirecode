95abcfb800f48efb2cc49d464ea30003ae87de69
meverett <meverett>
2009-01-27 16:54:12 +0000

    Fixes File Info for search results

--- private-components/swingui/src/main/java/org/limewire/ui/swing/search/resultpanel/SearchResultPropertiesFactory.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/resultpanel/SearchResultPropertiesFactory.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
 
 import javax.swing.AbstractAction;
-import javax.swing.DefaultComboBoxModel;
 import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 
@@ -17,7 +16,7 @@ import org.limewire.core.api.endpoint.RemoteHost;
 import org.limewire.core.api.search.SearchResult;
 import org.limewire.ui.swing.action.BitziLookupAction;
 import org.limewire.ui.swing.listener.MousePopupListener;
-import org.limewire.ui.swing.properties.Dialog;
+import org.limewire.ui.swing.properties.AbstractPropertiableFileDialog;
 import org.limewire.ui.swing.properties.DialogParam;
 import org.limewire.ui.swing.properties.FilterList;
 import org.limewire.ui.swing.properties.Properties;
@@ -43,7 +42,7 @@ public class SearchResultPropertiesFactory implements PropertiesFactory<VisualSe
         return new SearchResultProperties(dialogParam);
     }
 
-    public static class SearchResultProperties extends Dialog implements Properties<VisualSearchResult> {
+    public static class SearchResultProperties extends AbstractPropertiableFileDialog implements Properties<VisualSearchResult> {
         private final CategoryIconManager categoryIconManager;
         private final FilterList filterList;
         
@@ -53,27 +52,17 @@ public class SearchResultPropertiesFactory implements PropertiesFactory<VisualSe
             this.filterList = dialogParam.getFilterList();
             GuiUtils.assignResources(this);
             disableEditForAllCommonFields();
-
-            location.setLayout(new MigLayout("nocache", "[50%!]", "[]"));
-            readOnlyInfoModel.setColumnIdentifiers(new Object[] { tr("Address"), tr("Filename") });
-            location.add(new JScrollPane(readOnlyInfo));
         }
 
         @Override
         public void showProperties(VisualSearchResult vsr) {
             icon.setIcon(categoryIconManager.getIcon(vsr, iconManager));
-            heading.setText(vsr.getHeading());
-            filename.setText(vsr.getPropertyString(FilePropertyKey.NAME));
-            fileSize.setText(vsr.getPropertyString(FilePropertyKey.FILE_SIZE));
-            genre.setModel(new DefaultComboBoxModel(new String[]{ vsr.getPropertyString(FilePropertyKey.GENRE) }));
-            rating.setModel(new DefaultComboBoxModel(new String[]{ vsr.getPropertyString(FilePropertyKey.RATING) }));
-            populateMetadata(vsr);
+            location.setLayout(new MigLayout("nocache", "[50%!]", "[]"));
+            readOnlyInfoModel.setColumnIdentifiers(new Object[] { tr("Address"), tr("Filename") });
+            location.add(new JScrollPane(readOnlyInfo));
+            populateCommonFields(vsr);
             copyToClipboard.setAction(new CopyMagnetLinkToClipboardAction(vsr));
             moreFileInfo.setAction(new BitziLookupAction(vsr));
-            album.setText(vsr.getPropertyString(FilePropertyKey.ALBUM));
-            title.setText(vsr.getPropertyString(FilePropertyKey.TITLE));
-            year.setText(vsr.getPropertyString(FilePropertyKey.YEAR));
-            description.setText(vsr.getPropertyString(FilePropertyKey.DESCRIPTION));
 
             // Clear the table
             readOnlyInfoModel.setRowCount(0);
