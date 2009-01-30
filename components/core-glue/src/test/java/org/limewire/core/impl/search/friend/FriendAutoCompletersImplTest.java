cc187879eee81d5e50d12be00116d7c741234bb6
pvertenten <pvertenten>
2009-01-30 22:18:52 +0000

    Testing that AutoCompleter Dictionaries are returned for each of the SearchCategories.

---
+++ components/core-glue/src/test/java/org/limewire/core/impl/search/friend/FriendAutoCompletersImplTest.java
@@ -0,0 +1,28 @@
+package org.limewire.core.impl.search.friend;
+
+import org.jmock.Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.limewire.core.api.search.SearchCategory;
+import org.limewire.core.impl.library.FriendLibraries;
+import org.limewire.util.BaseTestCase;
+
+public class FriendAutoCompletersImplTest extends BaseTestCase {
+
+    public FriendAutoCompletersImplTest(String name) {
+        super(name);
+    }
+
+    public void testGetDictionary() {
+        Mockery context = new Mockery() {{
+            setImposteriser(ClassImposteriser.INSTANCE);
+        }};
+        
+        FriendLibraries friendLibraries = context.mock(FriendLibraries.class);
+        FriendAutoCompletersImpl friendAutoCompletersImpl = new FriendAutoCompletersImpl(
+                friendLibraries);
+        
+        for(SearchCategory searchCategory : SearchCategory.values()) {
+            assertNotNull(friendAutoCompletersImpl.getDictionary(searchCategory));
+        }
+    }
+}
