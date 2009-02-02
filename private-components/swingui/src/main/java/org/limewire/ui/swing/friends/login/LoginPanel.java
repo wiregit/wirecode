2a618ada2cc3405235935f8f42a9ee5864796aff
mturkel <mturkel>
2009-02-02 19:11:07 +0000

    LWC-2836 Null Pointer in LoginPanel

--- private-components/swingui/src/main/java/org/limewire/ui/swing/friends/login/LoginPanel.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/friends/login/LoginPanel.java
@@ -243,7 +243,7 @@ class LoginPanel extends JXPanel implements SettingListener {
     void disconnected(Exception reason) {
         setSignInComponentsEnabled(true);
         populateInputs();
-        if(reason.getMessage().toLowerCase(Locale.US).contains("auth")) {
+        if(reason !=null && reason.getMessage() != null && reason.getMessage().toLowerCase(Locale.US).contains("auth")) {
             authFailedLabel.setText(AUTHENTICATION_ERROR);
             passwordField.setText("");
         } else {
