d39b08bc4a29cd04d34c251b7c970ad26b0bf96c
michaelt <michaelt>
2009-02-03 17:23:08 +0000

    *** empty log message ***

--- components/core-glue/src/test/java/org/limewire/core/impl/upload/CoreUploadItemTest.java
+++ components/core-glue/src/test/java/org/limewire/core/impl/upload/CoreUploadItemTest.java
@@ -6,6 +6,7 @@ import org.limewire.util.BaseTestCase;
 
 import com.limegroup.gnutella.CategoryConverter;
 import com.limegroup.gnutella.Uploader;
+import com.limegroup.gnutella.Uploader.UploadStatus;
 
 public class CoreUploadItemTest extends BaseTestCase {
     
@@ -27,5 +28,27 @@ public class CoreUploadItemTest extends BaseTestCase {
         CoreUploadItem upload = new CoreUploadItem(uploader);
         
         assertEquals(upload.getCategory(), CategoryConverter.categoryForExtension("bmp"));
+        context.assertIsSatisfied();
     }
+    
+    public void testCancel() {
+        Mockery context = new Mockery();
+        
+        final Uploader uploader = context.mock(Uploader.class);
+        
+        context.checking(new Expectations() {
+            {
+                exactly(1).of(uploader).stop();
+                
+                allowing(uploader).getState();
+                will(returnValue(UploadStatus.UPLOADING));
+            }});
+        
+        CoreUploadItem upload = new CoreUploadItem(uploader);
+        
+        upload.cancel();
+                
+        context.assertIsSatisfied();
+    }
+    
 }
