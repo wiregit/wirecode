e068dff780ee58b5019a01676e3b1e68c3056663
michaelt <michaelt>
2009-02-04 19:42:19 +0000

    XMPPResourceFactoryImplTest

---
+++ components/core-glue/src/test/java/org/limewire/core/impl/xmpp/XMPPResourceFactoryImplTest.java
@@ -0,0 +1,26 @@
+package org.limewire.core.impl.xmpp;
+
+import org.limewire.util.BaseTestCase;
+
+public class XMPPResourceFactoryImplTest extends BaseTestCase  {
+    
+    public XMPPResourceFactoryImplTest(String name) {
+        super(name);
+    }
+    
+    /**
+     * Test the return from getResource is
+     *  non null and non empty
+     */
+    public void testGetResourceNonEmpty() {
+        
+    }
+    
+    /**
+     * Test the returns of two calls to
+     *  get resource are unique
+     */
+    public void testGetResourceUniqueness() {
+        
+    }
+}
