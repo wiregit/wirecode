c9d7c5e81bf39ebabe341d711d57869c374a3bfc
michaelt <michaelt>
2009-02-04 20:12:21 +0000

    place holder for XMPPRemoteFileDescDeserializerTest which I am doing now.

---
+++ components/core-glue/src/test/java/org/limewire/core/impl/xmpp/XMPPRemoteFileDescDeserializerTest.java
@@ -0,0 +1,11 @@
+package org.limewire.core.impl.xmpp;
+
+import org.limewire.util.BaseTestCase;
+
+public class XMPPRemoteFileDescDeserializerTest extends BaseTestCase {
+    
+    public XMPPRemoteFileDescDeserializerTest(String name) {
+        super(name);
+    }
+    
+}
