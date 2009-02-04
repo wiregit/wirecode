1b8a3dd2312d06006ceed7dfbbf39c2a384cd4a8
eyu <eyu>
2009-02-04 21:13:14 +0000

    added javadoc for interface and all methods

--- private-components/swingui/src/main/java/org/limewire/ui/swing/search/model/SearchResultsModel.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/model/SearchResultsModel.java
@@ -4,16 +4,36 @@ import org.limewire.core.api.search.SearchResult;
 
 import ca.odell.glazedlists.EventList;
 
+/**
+ * Defines an interface for the data model containing the results for a search.
+ */
 public interface SearchResultsModel {
 
-    public abstract int getResultCount();
+    /**
+     * Returns the total number of results in the search.
+     */
+    int getResultCount();
 
-    public abstract EventList<VisualSearchResult> getObservableSearchResults();
+    /**
+     * Returns an observable list of grouped results in the search.  The 
+     * observable list fires update events whenever a result is modified in 
+     * place. 
+     */
+    EventList<VisualSearchResult> getObservableSearchResults();
 
-    public abstract void addSearchResult(SearchResult result);
-
-    public abstract void removeSearchResult(SearchResult result);
-
+    /**
+     * Returns a list of grouped results in the search.
+     */
     EventList<VisualSearchResult> getGroupedSearchResults();
+
+    /**
+     * Adds the specified core search result to the data model.
+     */
+    void addSearchResult(SearchResult result);
+
+    /**
+     * Removes the specified core search result from the data model.
+     */
+    void removeSearchResult(SearchResult result);
 
 }
