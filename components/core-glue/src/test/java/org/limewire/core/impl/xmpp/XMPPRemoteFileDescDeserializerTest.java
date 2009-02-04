8d98be29a1f71dca63cb8d568fb93abd3ebc0c99
michaelt <michaelt>
2009-02-04 22:27:26 +0000

    add a test that exposes bug in XMPPRemoteFileDescImpl

--- components/core-glue/src/test/java/org/limewire/core/impl/xmpp/XMPPRemoteFileDescDeserializerTest.java
+++ components/core-glue/src/test/java/org/limewire/core/impl/xmpp/XMPPRemoteFileDescDeserializerTest.java
@@ -1,13 +1,22 @@
 package org.limewire.core.impl.xmpp;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.jmock.lib.legacy.ClassImposteriser;
+import org.limewire.io.Address;
 import org.limewire.net.address.AddressFactory;
 import org.limewire.util.BaseTestCase;
+import org.limewire.xmpp.api.client.XMPPAddress;
 import org.limewire.xmpp.client.impl.XMPPAddressResolver;
 
+import com.limegroup.gnutella.RemoteFileDesc;
+import com.limegroup.gnutella.URN;
 import com.limegroup.gnutella.downloader.RemoteFileDescFactory;
+import com.limegroup.gnutella.downloader.RemoteFileDescImpl;
+import com.limegroup.gnutella.xml.LimeXMLDocument;
 
 public class XMPPRemoteFileDescDeserializerTest extends BaseTestCase {
     
@@ -16,6 +25,24 @@ public class XMPPRemoteFileDescDeserializerTest extends BaseTestCase {
     }
     
     public void testRegester() {
+        Mockery context = new Mockery();
+
+        final RemoteFileDescFactory remoteFileDescFactory = context.mock(RemoteFileDescFactory.class);
+        
+        final XMPPRemoteFileDescDeserializer deserialiser
+            = new XMPPRemoteFileDescDeserializer(null, null);
+        
+        context.checking(new Expectations() {
+            {   exactly(1).of(remoteFileDescFactory).register(with(any(String.class)), with(same(deserialiser)));
+            }});
+        
+        deserialiser.register(remoteFileDescFactory);
+        
+        context.assertIsSatisfied();
+        
+    }
+    
+    public void testCreateClone() {
         Mockery context = new Mockery() {
             {   setImposteriser(ClassImposteriser.INSTANCE);
             }
@@ -24,21 +51,53 @@ public class XMPPRemoteFileDescDeserializerTest extends BaseTestCase {
         final AddressFactory addressFactory = context.mock(AddressFactory.class);
         final XMPPAddressResolver addressResolver = context.mock(XMPPAddressResolver.class);
         
-        final RemoteFileDescFactory remoteFileDescFactory = context.mock(RemoteFileDescFactory.class);
-        
         final XMPPRemoteFileDescDeserializer deserialiser
             = new XMPPRemoteFileDescDeserializer(addressFactory, addressResolver);
         
-        context.checking(new Expectations() {
-            {
-                exactly(1).of(remoteFileDescFactory).register(with(any(String.class)), with(same(deserialiser)));
+        final Address addressOrig = context.mock(Address.class);;
+        final long index = 10;
+        final String fileName = "file.txt";
+        final long size = 12;
+        final byte[] clientGUID = new byte[] {'h','e', 'y'};
+        final int speed = 20;
+        final boolean chat = false;
+        final int quality = 9;
+        final boolean browseHost = true;
+        final LimeXMLDocument xmlDoc = context.mock(LimeXMLDocument.class);
+        final Set<? extends URN> urns = new HashSet<URN>();
+        final boolean replyToMuticast = false;
+        final String vendor = "EGG";
+        final long createTime = 13423;
+        final boolean http11 = true;
+        final XMPPAddress addressNew = context.mock(XMPPAddress.class);
                 
+        context.checking(new Expectations() {
+            {  // Nothing internal to the mocks should be touched.
             }});
         
-        deserialiser.register(remoteFileDescFactory);
+        RemoteFileDesc rfdOrig = new RemoteFileDescImpl(addressOrig, index, fileName, size,
+                clientGUID, speed, chat, quality ,
+                browseHost, xmlDoc, urns, replyToMuticast, 
+                vendor, createTime, http11, addressFactory);
+                
+        RemoteFileDesc rfdNew = deserialiser.createClone(rfdOrig, addressNew);
+        
+        assertTrue(addressNew == rfdNew.getAddress());
+        assertTrue(index == rfdNew.getIndex());
+        assertTrue(fileName == rfdNew.getFileName());
+        assertTrue(size == rfdNew.getSize());
+        assertTrue(clientGUID == rfdNew.getClientGUID());
+        assertTrue(speed == rfdNew.getSpeed());
+        assertTrue(chat == rfdNew.isChatEnabled());
+        assertTrue(quality == rfdNew.getQuality());;
+        assertTrue(browseHost == rfdNew.isBrowseHostEnabled());
+        assertTrue(xmlDoc == rfdNew.getXMLDocument());
+        assertTrue(urns == rfdNew.getUrns());
+        assertTrue(vendor == rfdNew.getVendor());
+        assertTrue(createTime == rfdNew.getCreationTime());
+        assertTrue(http11 == rfdNew.isHTTP11());
         
         context.assertIsSatisfied();
-        
     }
     
 }
