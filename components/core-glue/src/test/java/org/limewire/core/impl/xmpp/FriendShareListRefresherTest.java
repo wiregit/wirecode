ee41e0f93255b92087b9efa96b0ee4c73e0a6f46
michaelt <michaelt>
2009-02-04 17:06:14 +0000

    FriendShareListRefresherTest::testFinishedLoadingListenerLoadComplete()

--- components/core-glue/src/test/java/org/limewire/core/impl/xmpp/FriendShareListRefresherTest.java
+++ components/core-glue/src/test/java/org/limewire/core/impl/xmpp/FriendShareListRefresherTest.java
@@ -1,5 +1,8 @@
 package org.limewire.core.impl.xmpp;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 import java.util.concurrent.ScheduledExecutorService;
 
 import org.jmock.Expectations;
@@ -8,6 +11,10 @@ import org.jmock.Sequence;
 import org.jmock.lib.legacy.ClassImposteriser;
 import org.limewire.core.api.browse.server.BrowseTracker;
 import org.limewire.core.api.friend.Friend;
+import org.limewire.core.api.friend.FriendPresence;
+import org.limewire.core.api.friend.feature.Feature;
+import org.limewire.core.api.friend.feature.features.LibraryChangedNotifier;
+import org.limewire.core.api.friend.feature.features.LibraryChangedNotifierFeature;
 import org.limewire.core.api.library.FriendShareListEvent;
 import org.limewire.core.api.library.LocalFileItem;
 import org.limewire.core.api.library.LocalFileList;
@@ -15,6 +22,8 @@ import org.limewire.core.impl.xmpp.FriendShareListRefresher.FinishedLoadingListe
 import org.limewire.core.impl.xmpp.FriendShareListRefresher.LibraryChangedSender;
 import org.limewire.listener.ListenerSupport;
 import org.limewire.util.BaseTestCase;
+import org.limewire.xmpp.api.client.User;
+import org.limewire.xmpp.api.client.XMPPConnection;
 import org.limewire.xmpp.api.client.XMPPService;
 
 import ca.odell.glazedlists.EventList;
@@ -80,6 +89,7 @@ public class FriendShareListRefresherTest extends BaseTestCase {
         
         context.checking(new Expectations() {
             {
+                // These should be checked
                 atLeast(1).of(event1).getType();
                 will(returnValue(FriendShareListEvent.Type.FRIEND_SHARE_LIST_ADDED));
                 
@@ -104,6 +114,7 @@ public class FriendShareListRefresherTest extends BaseTestCase {
                 atLeast(1).of(localFileList).getModel();
                 will(returnValue(eventList));
                 
+                // Assertions
                 exactly(1).of(eventList).addListEventListener(with(any(LibraryChangedSender.class)));
                 inSequence(resultSequence);
                 
@@ -148,6 +159,91 @@ public class FriendShareListRefresherTest extends BaseTestCase {
             }});
         
         finishedLoadingListener.handleEvent(event);
+        finishedLoadingListener.handleEvent(event);
+    }
+    
+    
+    /**
+     * Fire handleEvent() of the FinishedLoadingListener
+     * 
+     * Ensure that refreshes are sent to all friend and presences
+     */
+    @SuppressWarnings("unchecked")
+    public void testFinishedLoadingListenerLoadComplete() {
+        Mockery context = new Mockery() {
+            {   setImposteriser(ClassImposteriser.INSTANCE);
+            }
+        };
+
+        final BrowseTracker tracker = context.mock(BrowseTracker.class);
+        final XMPPService xmppService = context.mock(XMPPService.class);
+        final ScheduledExecutorService scheduledExecutorService = context.mock(ScheduledExecutorService.class);
+        final ManagedListStatusEvent event = context.mock(ManagedListStatusEvent.class);
+        
+        final XMPPConnection xmppConnection = context.mock(XMPPConnection.class);
+        
+        final LinkedList<User> users = new LinkedList<User>();
+        users.add(context.mock(User.class));
+        users.add(context.mock(User.class));
+        
+        final Map<String, FriendPresence> presences1 = new HashMap<String, FriendPresence>();
+        final Map<String, FriendPresence> presences2 = new HashMap<String, FriendPresence>();
+                
+        presences1.put("a", context.mock(FriendPresence.class));
+        presences1.put("b", context.mock(FriendPresence.class));
+        presences2.put("a", context.mock(FriendPresence.class));
+        
+        final Feature<LibraryChangedNotifier> featureA = context.mock(Feature.class);
+        final Feature<LibraryChangedNotifier> featureB = context.mock(Feature.class);
+        
+        final LibraryChangedNotifier notifierA = context.mock(LibraryChangedNotifier.class);
+        final LibraryChangedNotifier notifierB = context.mock(LibraryChangedNotifier.class); 
+        
+        final FriendShareListRefresher friendShareListRefresher = new FriendShareListRefresher(tracker,
+                xmppService, scheduledExecutorService);
+
+        final FinishedLoadingListener finishedLoadingListener = friendShareListRefresher.new FinishedLoadingListener();
+        
+        final String id1 = "1234";
+        final String id2 = "5678";
+        
+        context.checking(new Expectations() {
+            {   // These should be checked
+                atLeast(1).of(event).getType();
+                will(returnValue(ManagedListStatusEvent.Type.LOAD_COMPLETE));
+
+                // Non critical actions
+                allowing(xmppService).getActiveConnection();
+                will(returnValue(xmppConnection));
+                allowing(xmppConnection).getUsers();
+                will(returnValue(users));
+                allowing(users.get(0)).getId();
+                will(returnValue(id1));
+                allowing(users.get(1)).getId();
+                will(returnValue(id2));
+                allowing(users.get(0)).getFriendPresences();
+                will(returnValue(presences1));
+                allowing(users.get(1)).getFriendPresences();
+                will(returnValue(presences2));
+                allowing(presences1.get("a")).getFeature(LibraryChangedNotifierFeature.ID);
+                will(returnValue(featureA));
+                allowing(presences1.get("b")).getFeature(LibraryChangedNotifierFeature.ID);
+                will(returnValue(featureB));
+                allowing(presences2.get("a")).getFeature(LibraryChangedNotifierFeature.ID);
+                will(returnValue(featureA));
+                allowing(featureA).getFeature();
+                will(returnValue(notifierA));
+                allowing(featureB).getFeature();
+                will(returnValue(notifierB));
+                
+                // Assertions
+                exactly(1).of(tracker).sentRefresh(id1);
+                exactly(1).of(tracker).sentRefresh(id2);
+                exactly(2).of(notifierA).sendLibraryRefresh();
+                exactly(1).of(notifierB).sendLibraryRefresh();
+                
+            }});
+        
         finishedLoadingListener.handleEvent(event);
     }
     
