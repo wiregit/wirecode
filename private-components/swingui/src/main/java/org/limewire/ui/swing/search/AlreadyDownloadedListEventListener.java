f404c4cab7752b9b1165a1e65f8d89cfdd39106a
eyu <eyu>
2009-02-05 18:37:30 +0000

    added javadoc comments

--- private-components/swingui/src/main/java/org/limewire/ui/swing/search/AlreadyDownloadedListEventListener.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/AlreadyDownloadedListEventListener.java
@@ -12,21 +12,35 @@ import ca.odell.glazedlists.EventList;
 import ca.odell.glazedlists.event.ListEvent;
 import ca.odell.glazedlists.event.ListEventListener;
 
+/**
+ * A listener to handle changes to the list of visual search results.
+ */
 public class AlreadyDownloadedListEventListener implements ListEventListener<VisualSearchResult> {
+
     private final LibraryManager libraryManager;
 
     private final DownloadListManager downloadListManager;
 
+    /**
+     * Constructs an AlreadyDownloadedListEventListener with the specified
+     * library manager and download list manager.
+     */
     public AlreadyDownloadedListEventListener(LibraryManager libraryManager,
             DownloadListManager downloadListManager) {
         this.libraryManager = libraryManager;
         this.downloadListManager = downloadListManager;
     }
 
+    /**
+     * Handles a listChanged event to update the state of each visual search
+     * result associated with the event.
+     */
     @Override
     public void listChanged(ListEvent<VisualSearchResult> listChanges) {
+        // Get list of library files, and list of search results. 
         LibraryFileList libraryFileList = libraryManager.getLibraryManagedList();
         final EventList<VisualSearchResult> eventList = listChanges.getSourceList();
+        
         while (listChanges.next()) {
             boolean addOrUpdate = listChanges.getType() == ListEvent.INSERT
                     || listChanges.getType() == ListEvent.UPDATE;
@@ -36,10 +50,11 @@ public class AlreadyDownloadedListEventListener implements ListEventListener<Vis
                 //TODO should probably check more than just URN, can check the file save path as well.
                 URN urn = visualSearchResult.getUrn();
                 if (libraryFileList.contains(urn)) {
-                    //first checking library for file
+                    // Set download state when result is already in library.
                     visualSearchResult.setDownloadState(BasicDownloadState.LIBRARY);
+                    
                 } else {
-                    //next checking download list
+                    // Set download state when result is being downloaded.
                     DownloadItem downloadItem = downloadListManager.getDownloadItem(urn);
                     if(downloadItem != null) {
                         downloadItem.addPropertyChangeListener(new DownloadItemPropertyListener(visualSearchResult));
