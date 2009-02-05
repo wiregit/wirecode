35a086d2957e99716403073c35b6ff4b61758ff7
eyu <eyu>
2009-02-05 16:55:42 +0000

    added javadoc comments

--- private-components/swingui/src/main/java/org/limewire/ui/swing/search/SearchResultsPanelFactory.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/SearchResultsPanelFactory.java
@@ -5,9 +5,17 @@ import org.limewire.ui.swing.search.model.VisualSearchResult;
 
 import ca.odell.glazedlists.EventList;
 
+/**
+ * Defines a factory for creating the search results display container.
+ */
 public interface SearchResultsPanelFactory {
     
+    /**
+     * Creates a new SearchResultsPanel using the specified search information,
+     * list of search results, and Search object.
+     */
     public SearchResultsPanel createSearchResultsPanel(SearchInfo searchInfo,
             EventList<VisualSearchResult> visualSearchResults,
             Search search);
+    
 }
