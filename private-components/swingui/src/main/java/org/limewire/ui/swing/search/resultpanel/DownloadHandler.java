3dd1fbcf95d6aca204acf5cf054e02c2e6722f58
eyu <eyu>
2009-02-10 16:29:52 +0000

    added javadoc comments

--- private-components/swingui/src/main/java/org/limewire/ui/swing/search/resultpanel/DownloadHandler.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/resultpanel/DownloadHandler.java
@@ -2,6 +2,14 @@ package org.limewire.ui.swing.search.resultpanel;
 
 import org.limewire.ui.swing.search.model.VisualSearchResult;
 
+/**
+ * Defines an interface for handling download requests.
+ */
 public interface DownloadHandler {
+    
+    /**
+     * Initiates a download of the specified visual search result.
+     */
     void download(VisualSearchResult vsr);
+    
 }
