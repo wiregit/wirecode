2bb121eeb2e9b7a1439702fc6e69d1378e7be23f
eyu <eyu>
2009-02-10 20:13:24 +0000

    LWC-2840 revised to use WeakReference to avoid holding direct reference to search result, which can cause memory leak

--- private-components/swingui/src/main/java/org/limewire/ui/swing/search/DownloadItemPropertyListener.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/DownloadItemPropertyListener.java
@@ -2,6 +2,7 @@ package org.limewire.ui.swing.search;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.lang.ref.WeakReference;
 
 import org.limewire.core.api.download.DownloadState;
 import org.limewire.ui.swing.search.model.BasicDownloadState;
@@ -12,14 +13,18 @@ import org.limewire.ui.swing.search.model.VisualSearchResult;
  * visual search result.
  */
 public class DownloadItemPropertyListener implements PropertyChangeListener {
-    private final VisualSearchResult vsr;
+    
+    /** Reference to visual search result.  We use a WeakReference to allow
+     *  the result to be cleared if the search is closed. 
+     */
+    private final WeakReference<VisualSearchResult> vsrReference;
 
     /**
      * Constructs a DownloadItemPropertyListener for the specified visual
      * search result.
      */
     public DownloadItemPropertyListener(VisualSearchResult vsr) {
-        this.vsr = vsr;
+        this.vsrReference = new WeakReference<VisualSearchResult>(vsr);
     }
 
     /**
@@ -31,10 +36,10 @@ public class DownloadItemPropertyListener implements PropertyChangeListener {
             DownloadState state = (DownloadState) evt.getNewValue();
             switch (state) {
             case CANCELLED:
-                vsr.setDownloadState(BasicDownloadState.NOT_STARTED);
+                setDownloadState(BasicDownloadState.NOT_STARTED);
                 break;
             case DONE:
-                vsr.setDownloadState(BasicDownloadState.DOWNLOADED);
+                setDownloadState(BasicDownloadState.DOWNLOADED);
                 break;
             case ERROR:
             case CONNECTING:
@@ -44,9 +49,19 @@ public class DownloadItemPropertyListener implements PropertyChangeListener {
             case STALLED:
             case FINISHING:
             case DOWNLOADING:
-                vsr.setDownloadState(BasicDownloadState.DOWNLOADING);
+                setDownloadState(BasicDownloadState.DOWNLOADING);
                 break;
             }
+        }
+    }
+
+    /**
+     * Sets the download state in the visual search result.
+     */
+    private void setDownloadState(BasicDownloadState downloadState) {
+        VisualSearchResult vsr = vsrReference.get();
+        if (vsr != null) {
+            vsr.setDownloadState(downloadState);
         }
     }
 }
