5a82f767823d39f5ab26b22ba0d5060be2fd3cad
sberlin <sberlin>
2009-02-18 17:09:02 +0000

    fix for magnet URLs that don't have a size.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/downloader/MagnetDownloaderImpl.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/downloader/MagnetDownloaderImpl.java
@@ -5,6 +5,8 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.concurrent.ScheduledExecutorService;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.http.HttpException;
 import org.limewire.core.api.download.SaveLocationException;
 import org.limewire.core.api.download.SaveLocationManager;
@@ -61,6 +63,8 @@ import com.limegroup.gnutella.util.QueryUtils;
  * download.  
  */
 class MagnetDownloaderImpl extends ManagedDownloaderImpl implements MagnetDownloader {
+    
+    private static final Log LOG = LogFactory.getLog(MagnetDownloaderImpl.class);
         
 	private MagnetOptions magnet;
 
@@ -140,6 +144,9 @@ class MagnetDownloaderImpl extends ManagedDownloaderImpl implements MagnetDownlo
      */
     @Override
     protected DownloadState initializeDownload() {
+        if(LOG.isDebugEnabled()) {
+            LOG.debug("Initializing magnet download for: " + magnet);
+        }
         
         // ask ranker since the alt loc manager might have added extra alt locs
         // which were added to the ranker then
@@ -160,10 +167,15 @@ class MagnetDownloaderImpl extends ManagedDownloaderImpl implements MagnetDownlo
 				    fileSize = rfd.getSize();
 					initPropertiesMap(rfd);
 					addDownloadForced(rfd, true);
-				} catch (IOException badRFD) {} 
-                  catch (HttpException e) {} 
-                  catch (URISyntaxException e) {} 
-                  catch (InterruptedException e) {}
+				} catch (IOException e) {
+				    LOG.warn("error", e);
+				} catch (HttpException e) {
+				    LOG.warn("error", e);
+				} catch (URISyntaxException e) {
+				    LOG.warn("error", e);
+				} catch (InterruptedException e) {
+				    LOG.warn("error", e);
+				}
             }
         
 			// if all locations included in the magnet URI fail we can't do much
