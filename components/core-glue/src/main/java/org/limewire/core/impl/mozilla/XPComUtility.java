8b2f0390dbcd0d39de29287e01ffd1deba32123a
pvertenten <pvertenten>
2009-02-23 18:30:15 +0000

    Removing singletoness from the xpcom utility.

--- components/core-glue/src/main/java/org/limewire/core/impl/mozilla/XPComUtility.java
+++ components/core-glue/src/main/java/org/limewire/core/impl/mozilla/XPComUtility.java
@@ -3,14 +3,11 @@ package org.limewire.core.impl.mozilla;
 import org.mozilla.browser.XPCOMUtils;
 import org.mozilla.interfaces.nsISupports;
 
-import com.google.inject.Singleton;
-
 /**
  * This class acts as a wrapper for the mozilla XPCOMUtils class. Because the
  * XPCOMUtils class has static methods it makes unit testing code that used it
  * very hard. By injecting this class instead, the code becomes testable.
  */
-@Singleton
 public class XPComUtility {
 
     public <T extends nsISupports> T proxy(nsISupports elem, Class<T> class1) {
