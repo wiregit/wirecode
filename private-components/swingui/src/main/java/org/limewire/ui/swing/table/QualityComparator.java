7a78a46d6963c2ef775636adcafce4fbd6d0e285
mturkel <mturkel>
2009-03-02 20:28:08 +0000

    improved quality comparison

--- private-components/swingui/src/main/java/org/limewire/ui/swing/table/QualityComparator.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/table/QualityComparator.java
@@ -18,12 +18,29 @@ public class QualityComparator implements Comparator<PropertiableFile> {
             if (quality2 instanceof Number) {
                 int q1 = ((Number) quality1).intValue();
                 int q2 = ((Number) quality2).intValue();               
-                return (q1 == q2)? 0 : (q1 < q2)? -1 : 1;
+                return (q1 == q2)? compareBitRate(o1, o2) : (q1 < q2)? -1 : 1;
             } else {
                 return 1;
             }
         } else {
             return (quality2 instanceof Number) ? -1 : 0;
+        }
+    }
+    
+    private int compareBitRate(PropertiableFile o1, PropertiableFile o2){
+        Object bitRate1 = o1.getProperty(FilePropertyKey.BITRATE);
+        Object bitRate2 = o2.getProperty(FilePropertyKey.BITRATE);
+
+        if (bitRate1 instanceof Number) {
+            if (bitRate2 instanceof Number) {
+                int b1 = ((Number) bitRate1).intValue();
+                int b2 = ((Number) bitRate2).intValue();               
+                return (b1 == b2)? 0 : (b1 < b2)? -1 : 1;
+            } else {
+                return 1;
+            }
+        } else {
+            return (bitRate2 instanceof Number) ? -1 : 0;
         }
     }
 }
