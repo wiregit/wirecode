e5d5686f04d5578602d68edcd3baeef4560994a9
eyu <eyu>
2009-03-24 22:20:29 +0000

    remove RowSelectionPreserver and listener

--- private-components/swingui/src/main/java/org/limewire/ui/swing/search/LimeWireUiSearchModule.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/LimeWireUiSearchModule.java
@@ -41,7 +41,6 @@ public class LimeWireUiSearchModule extends AbstractModule {
         bind(SearchHandler.class).to(SearchHandlerImpl.class);
         bind(SearchHandler.class).annotatedWith(Names.named("p2p://")).to(P2PLinkSearchHandler.class);
         bind(SearchHandler.class).annotatedWith(Names.named("text")).to(TextSearchHandlerImpl.class);
-        bind(RowSelectionPreserver.class).to(RowSelectionPreserverImpl.class);
         bind(SimilarResultsDetectorFactory.class).to(SimilarResultsDetectorFactoryImpl.class);
         
         bind(SearchResultsPanelFactory.class).toProvider(
