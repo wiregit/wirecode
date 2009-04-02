5dff9b1bee67c95ee6646f424f065ad03720ba68
sberlin <sberlin>
2009-04-02 15:43:15 +0000

    update AbstractListEventListener API & javadocs to make it clearer how to use it.

--- components/collection/src/main/java/org/limewire/collection/glazedlists/AbstractListEventListener.java
+++ components/collection/src/main/java/org/limewire/collection/glazedlists/AbstractListEventListener.java
@@ -8,46 +8,56 @@ import ca.odell.glazedlists.event.ListEvent;
 import ca.odell.glazedlists.event.ListEventListener;
 import ca.odell.glazedlists.util.concurrent.Lock;
 
-/** 
- * An abstract ListEventListener for use with glazed lists when you really
- * need the deleted object.  Use this class sparingly, as it will cache
- * the contents of the list.
+/**
+ * An abstract ListEventListener for use with glazed lists when you really need
+ * the deleted object. Use this class sparingly, as it will cache the contents
+ * of the list.
+ * 
+ * To use, subclass {@link AbstractListEventListener} and {@link #install(EventList)}
+ * it on the list you want to listen to.
  */
-public abstract class AbstractListEventListener<E> implements ListEventListener<E> {
+public abstract class AbstractListEventListener<E> {
     private final List<E> cache = new ArrayList<E>();
-    
+
+    /**
+     * Adds this {@link AbstractListEventListener} as a listener on the source
+     * {@link EventList}.  Installation this way is required in order to
+     * guarantee that the {@link ListEventListener} has a cache of the current
+     * contents of the list so it can call {@link #itemRemoved(Object, int, EventList)}
+     * with the correct items.
+     */
     public void install(EventList<E> source) {
         Lock lock = source.getReadWriteLock().readLock();
         lock.lock();
         try {
             cache.addAll(source);
-            source.addListEventListener(this);
+            source.addListEventListener(new ListEventListener<E>() {
+                @Override
+                public void listChanged(ListEvent<E> changes) {
+                    while (changes.next()) {
+                        int type = changes.getType();
+                        int idx = changes.getIndex();
+                        EventList<E> source = changes.getSourceList();
+                        switch (type) {
+                        case ListEvent.INSERT:
+                            cache.add(idx, changes.getSourceList().get(idx));
+                            itemAdded(cache.get(idx), idx, source);
+                            break;
+                        case ListEvent.DELETE:
+                            E removed = cache.remove(idx);
+                            itemRemoved(removed, idx, source);
+                            break;
+                        case ListEvent.UPDATE:
+                            E prior = cache.get(idx);
+                            cache.set(idx, changes.getSourceList().get(idx));
+                            itemUpdated(cache.get(idx), prior, idx, source);
+                            break;
+                        }
+                    }
+                }
+            });
         } finally {
             lock.unlock();
-        }
-    }
-    
-    @Override
-    public void listChanged(ListEvent<E> changes) {
-        while (changes.next()) {
-            int type = changes.getType();
-            int idx = changes.getIndex();
-            EventList<E> source = changes.getSourceList();
-            switch (type) {
-            case ListEvent.INSERT:
-                cache.add(idx, changes.getSourceList().get(idx));
-                itemAdded(cache.get(idx), idx, source);
-                break;
-            case ListEvent.DELETE:
-                E removed = cache.remove(idx);
-                itemRemoved(removed, idx, source);
-                break;
-            case ListEvent.UPDATE:
-                E prior = cache.get(idx);
-                cache.set(idx, changes.getSourceList().get(idx));
-                itemUpdated(cache.get(idx), prior, idx, source);
-                break;
-            }
         }
     }
 
@@ -59,6 +69,5 @@ public abstract class AbstractListEventListener<E> implements ListEventListener<
 
     /** Notification that an item was replaced. */
     protected abstract void itemUpdated(E item, E priorItem, int idx, EventList<E> source);
-    
 
 }
