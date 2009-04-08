7743ed1c22e0cfe092f2997fb31484e79905e342
sberlin <sberlin>
2009-04-08 15:34:33 +0000

    add @LazySingleton

--- components/common/src/main/java/org/limewire/common/LimeWireCommonModule.java
+++ components/common/src/main/java/org/limewire/common/LimeWireCommonModule.java
@@ -1,5 +1,6 @@
 package org.limewire.common;
 
+import org.limewire.inject.LimeWireInjectModule;
 import org.limewire.lifecycle.LimeWireCommonLifecycleModule;
 import org.limewire.util.Clock;
 import org.limewire.util.ClockImpl;
@@ -10,6 +11,7 @@ public class LimeWireCommonModule extends AbstractModule {
     
     @Override
     protected void configure() {
+        install(new LimeWireInjectModule());
         install(new LimeWireCommonLifecycleModule());
         bind(Clock.class).to(ClockImpl.class);
     }
