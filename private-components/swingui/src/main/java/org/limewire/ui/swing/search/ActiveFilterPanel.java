885c8050ff9a6b736c4fe5b6b7ea48da4e741a91
eyu <eyu>
2009-04-22 21:43:41 +0000

    LWC-3032 added active filter resources

--- private-components/swingui/src/main/java/org/limewire/ui/swing/search/ActiveFilterPanel.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/ActiveFilterPanel.java
@@ -2,25 +2,35 @@ package org.limewire.ui.swing.search;
 
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.Action;
 import javax.swing.BorderFactory;
+import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 import net.miginfocom.swing.MigLayout;
+
+import org.jdesktop.application.Resource;
+import org.jdesktop.swingx.JXPanel;
+import org.jdesktop.swingx.painter.RectanglePainter;
+import org.limewire.ui.swing.util.GuiUtils;
 
 /**
  * Display panel for an active filter.
  */
-class ActiveFilterPanel extends JPanel {
+class ActiveFilterPanel extends JXPanel {
 
-    // TODO create resources
-    private Color removeForeColor = Color.RED;
+    @Resource private Color backgroundColor;
+    @Resource private Color borderColor;
+    @Resource private Color textColor;
+    @Resource private Font textFont;
+    @Resource private Icon removeDefaultIcon;
+    @Resource private Icon removeHoverIcon;
     
     private final JLabel label = new JLabel();
     private final JButton removeButton = new JButton();
@@ -29,35 +39,44 @@ class ActiveFilterPanel extends JPanel {
      * Constructs an ActiveFilterPanel with the specified remove action.
      */
     public ActiveFilterPanel(Action removeAction) {
-        setLayout(new MigLayout("insets 0 0 0 0, gap 0!", 
-                "[left][right]", ""));
-        setOpaque(false);
         
+        GuiUtils.assignResources(this);
+        
+        setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 0));
+        setLayout(new MigLayout("insets 0 0 0 0, gap 0!", 
+                "[left][right]", "[center]"));
+        setPreferredSize(new Dimension(0, 18));
+        setBackgroundPainter(new RectanglePainter(0, 0, 0, 0, 16, 16, true,
+                backgroundColor, 1.0f, borderColor));
+        
+        label.setFont(textFont);
+        label.setForeground(textColor);
         label.setHorizontalAlignment(JLabel.LEADING);
         label.setText((String) removeAction.getValue(Action.NAME));
         
         removeButton.setAction(removeAction);
-        removeButton.setBorder(BorderFactory.createEmptyBorder());
+        removeButton.setBorder(BorderFactory.createEmptyBorder(5, 5, 3, 5));
         removeButton.setContentAreaFilled(false);
-        removeButton.setFont(removeButton.getFont().deriveFont(Font.BOLD));
-        removeButton.setForeground(removeForeColor);
-        removeButton.setText("x");
+        removeButton.setIcon(removeDefaultIcon);
+        removeButton.setText(null);
         
         // Add listener to show cursor on mouse over.
         removeButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
-                e.getComponent().setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+                removeButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+                removeButton.setIcon(removeHoverIcon);
             }
             
             @Override
             public void mouseExited(MouseEvent e) {
-                e.getComponent().setCursor(Cursor.getDefaultCursor());
+                removeButton.setCursor(Cursor.getDefaultCursor());
+                removeButton.setIcon(removeDefaultIcon);
             }
         });
         
         add(label       , "wmax 126");
-        add(removeButton, "gap 5 5");
+        add(removeButton, "gap 0 0");
     }
     
 }
