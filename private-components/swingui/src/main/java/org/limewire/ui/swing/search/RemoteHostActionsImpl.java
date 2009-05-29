0f8d60afc803a0082a64197d7dca17474a65ef22
mturkel <mturkel>
2009-05-29 21:55:08 +0000

    Undo model stuff and add BrowseSearch

--- private-components/swingui/src/main/java/org/limewire/ui/swing/search/RemoteHostActionsImpl.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/RemoteHostActionsImpl.java
@@ -1,20 +1,18 @@
 package org.limewire.ui.swing.search;
 
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-
+import org.limewire.core.api.browse.BrowseFactory;
 import org.limewire.core.api.endpoint.RemoteHost;
 import org.limewire.core.api.friend.Friend;
-import org.limewire.core.api.library.LibraryState;
+import org.limewire.core.api.library.RemoteLibraryManager;
 import org.limewire.core.api.search.Search;
 import org.limewire.core.api.search.SearchCategory;
-import org.limewire.core.api.search.SearchListener;
 import org.limewire.logging.Log;
 import org.limewire.logging.LogFactory;
 import org.limewire.ui.swing.friends.chat.ChatFrame;
 import org.limewire.ui.swing.library.nav.LibraryNavigator;
 import org.limewire.ui.swing.nav.Navigator;
-import org.limewire.ui.swing.search.model.BrowseSearchResultsModel;
+import org.limewire.ui.swing.search.model.BrowseSearch;
+import org.limewire.ui.swing.search.model.SearchResultsModel;
 import org.limewire.ui.swing.search.model.SearchResultsModelFactory;
 import org.limewire.ui.swing.util.I18n;
 
@@ -36,15 +34,22 @@ public class RemoteHostActionsImpl implements RemoteHostActions {
     private final Provider<SearchNavigator> searchNavigator;
 
 
+    private final RemoteLibraryManager remoteLibraryManager;
+    private final BrowseFactory browseFactory;
+
+
     @Inject
     public RemoteHostActionsImpl(ChatFrame chatFrame, SearchResultsModelFactory searchResultsModelFactory, 
             SearchResultsPanelFactory searchResultsPanelFactory, Provider<SearchNavigator> searchNavigator,
-            LibraryNavigator libraryNavigator, Navigator navigator) {
+            LibraryNavigator libraryNavigator, Navigator navigator, RemoteLibraryManager remoteLibraryManager,
+            BrowseFactory browseFactory) {
         this.chatFrame = chatFrame;
         this.searchResultsModelFactory = searchResultsModelFactory;
         this.searchResultsPanelFactory = searchResultsPanelFactory;
         this.searchNavigator = searchNavigator;
         this.libraryNavigator = libraryNavigator; 
+        this.remoteLibraryManager = remoteLibraryManager;
+        this.browseFactory = browseFactory;
     }
  
 
@@ -68,53 +73,26 @@ public class RemoteHostActionsImpl implements RemoteHostActions {
         libraryNavigator.selectFriendShareList(friend);
     }
 
+    
     @Override
     public void viewLibraryOf(RemoteHost person) {
         LOG.debugf("viewLibraryOf: {0}", person.getFriendPresence().getFriend());
         // Create search results data model and display panel.
-        BrowseSearchResultsModel searchModel = searchResultsModelFactory.createSearchResultsModel(person.getFriendPresence());
+        
+        Search search = new BrowseSearch(remoteLibraryManager, browseFactory, person.getFriendPresence());
+        
+        //TODO: better SearchInfo
+        SearchInfo searchInfo= DefaultSearchInfo.createKeywordSearch("this is a browse", SearchCategory.ALL);
+        
+        SearchResultsModel searchModel = searchResultsModelFactory.createSearchResultsModel(searchInfo, search);
         final SearchResultsPanel searchPanel = searchResultsPanelFactory.createSearchResultsPanel(searchModel);
 
         // Add search results display to the UI, and select its navigation item.
+        //TODO: better title
         SearchNavItem item = searchNavigator.get().addSearch(
-                I18n.tr("Browse: {0}", person.getFriendPresence().getFriend().getRenderName()), searchPanel, new EmptySearch());
+                I18n.tr("Browse: {0}", person.getFriendPresence().getFriend().getRenderName()), searchPanel, search);
         item.select();
-
-        searchModel.addPropertyChangeListener(new PropertyChangeListener() {
-            @Override
-            public void propertyChange(final PropertyChangeEvent evt) {
-                if (evt.getNewValue() instanceof LibraryState)
-                    searchPanel.setBrowseState((LibraryState) evt.getNewValue());
-            }
-        });
-        searchPanel.setBrowseState(searchModel.getLibraryState());
+        searchModel.start(new SwingSearchListener(searchModel, searchPanel, item));
     }
-    
-    private static class EmptySearch implements Search {
 
-        @Override
-        public void addSearchListener(SearchListener searchListener) {
-        }
-
-        @Override
-        public void removeSearchListener(SearchListener searchListener) {
-        }
-
-        @Override
-        public SearchCategory getCategory() {
-            return SearchCategory.ALL;
-        }
-
-        @Override
-        public void repeat() {
-        }
-
-        @Override
-        public void start() {
-        }
-
-        @Override
-        public void stop() {
-        }
-    }
 }
---
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/model/BrowseSearch.java
@@ -0,0 +1,116 @@
+package org.limewire.ui.swing.search.model;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.limewire.core.api.browse.Browse;
+import org.limewire.core.api.browse.BrowseFactory;
+import org.limewire.core.api.browse.BrowseListener;
+import org.limewire.core.api.friend.FriendPresence;
+import org.limewire.core.api.library.PresenceLibrary;
+import org.limewire.core.api.library.RemoteFileItem;
+import org.limewire.core.api.library.RemoteLibraryManager;
+import org.limewire.core.api.search.Search;
+import org.limewire.core.api.search.SearchCategory;
+import org.limewire.core.api.search.SearchListener;
+import org.limewire.core.api.search.SearchResult;
+import org.limewire.util.NotImplementedException;
+
+import ca.odell.glazedlists.BasicEventList;
+import ca.odell.glazedlists.EventList;
+
+public class BrowseSearch implements Search {
+    private final CopyOnWriteArrayList<SearchListener> searchListeners = new CopyOnWriteArrayList<SearchListener>();
+    
+    private final FriendPresence friendPresence;
+    private final RemoteLibraryManager remoteLibraryManager;
+    private final BrowseFactory browseFactory;
+    private Browse browse;
+
+    public BrowseSearch(RemoteLibraryManager remoteLibraryManager, BrowseFactory browseFactory, FriendPresence friendPresence){
+        this.friendPresence = friendPresence;
+        this.browseFactory = browseFactory;
+        this.remoteLibraryManager = remoteLibraryManager;
+    }
+
+    @Override
+    public void addSearchListener(SearchListener searchListener) {
+        searchListeners.add(searchListener);
+    }
+    
+    @Override
+    public void removeSearchListener(SearchListener searchListener) {
+        searchListeners.remove(searchListener);
+    }
+
+    @Override
+    public SearchCategory getCategory() {
+        return SearchCategory.ALL;
+    }
+
+       @Override
+    public void repeat() {
+        throw new NotImplementedException("BrowseSearch.repeat() not implemented");
+
+    }
+
+    @Override
+    public void start() {      
+        if (isAnonymousBrowse()){
+            startAnonymousBrowse();
+        } else {
+            //TODO: RemoteFileItems are going away
+            EventList<RemoteFileItem> remoteFileItems = new BasicEventList<RemoteFileItem>();
+
+            if (friendPresence != null) {
+                PresenceLibrary presenceLibrary = remoteLibraryManager.addPresenceLibrary(friendPresence);
+                remoteFileItems.addAll(presenceLibrary.getModel());
+            } else {
+                remoteFileItems.addAll(remoteLibraryManager.getAllFriendsFileList().getModel());
+
+            }
+
+            for (RemoteFileItem item : remoteFileItems) {
+                for (SearchListener listener : searchListeners) {
+                    listener.handleSearchResult(this, item.getSearchResult());
+                }
+            }
+        }
+    }
+    
+    private void startAnonymousBrowse() {
+        System.out.println(friendPresence.getPresenceId());
+        browse = browseFactory.createBrowse(friendPresence);
+        browse.start(new BrowseListener() {
+            @Override
+            public void browseFinished(boolean success) {
+                System.out.println("browsefinished "+success);
+            }
+
+            @Override
+            public void handleBrowseResult(SearchResult searchResult) {
+                System.out.println("handle "+searchResult.getFileName());
+                for (SearchListener listener : searchListeners) {
+                    listener.handleSearchResult(BrowseSearch.this, searchResult);
+                }
+            }
+        });
+    }
+    
+
+    private void stopAnonymousBrowse() {
+        assert (browse != null);
+        browse.stop();
+    }
+    
+    private boolean isAnonymousBrowse(){
+        return friendPresence != null && friendPresence.getFriend().isAnonymous();
+    }
+
+    @Override
+    public void stop() {
+        if(isAnonymousBrowse()){
+            stopAnonymousBrowse();
+        }
+    }
+
+}
