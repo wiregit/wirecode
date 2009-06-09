dbd12e16bac21ccf4010d142813252f592d7db70
eyu <eyu>
2009-06-09 22:42:39 +0000

    LWC-3260 revised to use SourceItem

--- private-components/swingui/src/main/java/org/limewire/ui/swing/filter/SourceMatcher.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/filter/SourceMatcher.java
@@ -1,6 +1,9 @@
 package org.limewire.ui.swing.filter;
 
-import org.limewire.ui.swing.filter.SourceFilter.SourceType;
+import java.util.Collection;
+
+import org.limewire.core.api.friend.Friend;
+import org.limewire.util.Objects;
 
 import ca.odell.glazedlists.matchers.Matcher;
 
@@ -8,22 +11,39 @@ import ca.odell.glazedlists.matchers.Matcher;
  * Matcher used to filter a search result by its source.
  */
 class SourceMatcher<E extends FilterableItem> implements Matcher<E> {
-    /** Source type to filter. */
-    private final SourceType sourceType;
+    /** Source item to filter. */
+    private final SourceItem sourceItem;
 
     /**
-     * Constructs a SourceMatcher for the specified source type.
+     * Constructs a SourceMatcher for the specified source item.
      */
-    public SourceMatcher(SourceType sourceType) {
-        this.sourceType = sourceType;
+    public SourceMatcher(SourceItem sourceItem) {
+        this.sourceItem = sourceItem;
     }
 
     /**
-     * Returns true if the specified search result matches the source type.
+     * Returns true if the specified filterable item matches the source item.
      */
     @Override
     public boolean matches(E item) {
-        // TODO implement real logic
-        return (sourceType == SourceType.P2P);
+        switch (sourceItem.getType()) {
+        case ANONYMOUS:
+            return item.isAnonymous();
+            
+        case ANY_FRIEND:
+            return (item.getFriends().size() > 0);
+            
+        case FRIEND:
+        default:
+            // Compare friend names against SourceItem name.
+            Collection<Friend> friends = item.getFriends();
+            String name = sourceItem.getName();
+            for (Friend friend : friends) {
+                if (Objects.compareToNullIgnoreCase(name, friend.getName(), false) == 0) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
