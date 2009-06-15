51cd54d8495409469abe51a84dcc04ded47e53dc
meverett <meverett>
2009-06-15 18:30:35 +0000

    Refreshes NavList when sharing list size changes Remove unused icon

--- private-components/swingui/src/main/java/org/limewire/ui/swing/library/sharing/actions/ApplySharingAction.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/library/sharing/actions/ApplySharingAction.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.util.List;
 
 import org.limewire.ui.swing.action.AbstractAction;
+import org.limewire.ui.swing.library.navigator.LibraryNavigatorPanel;
 import org.limewire.ui.swing.library.sharing.LibrarySharingEditablePanel;
 import org.limewire.ui.swing.library.sharing.LibrarySharingPanel;
 import org.limewire.ui.swing.util.I18n;
@@ -15,20 +16,24 @@ public class ApplySharingAction extends AbstractAction {
 
     private final Provider<LibrarySharingPanel> librarySharingPanel;
     private final Provider<LibrarySharingEditablePanel> librarySharingEditablePanel;
+    private final Provider<LibraryNavigatorPanel> libraryNavigatorPanel;
     
     @Inject
     public ApplySharingAction(Provider<LibrarySharingPanel> librarySharingPanel,
-            Provider<LibrarySharingEditablePanel> librarySharingEditablePanel) {
+            Provider<LibrarySharingEditablePanel> librarySharingEditablePanel,
+            Provider<LibraryNavigatorPanel> libraryNavigatorPanel) {
         super(I18n.tr("Apply"));
         
         this.librarySharingPanel = librarySharingPanel;
         this.librarySharingEditablePanel = librarySharingEditablePanel;
+        this.libraryNavigatorPanel = libraryNavigatorPanel;
     }
     
     @Override
     public void actionPerformed(ActionEvent e) {
         List<String> friends = librarySharingEditablePanel.get().getSelectedFriends();
         librarySharingPanel.get().updateFriends(friends);
+        libraryNavigatorPanel.get().repaint();
         librarySharingPanel.get().showNonEditableView();
     }
 }
