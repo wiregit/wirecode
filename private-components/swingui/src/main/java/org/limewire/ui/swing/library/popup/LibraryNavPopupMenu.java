a09ebe575b16a5889121eab7bdf1ffd78f3a47cd
meverett <meverett>
2009-06-15 20:07:30 +0000

    Combined AddFile actions into one

--- private-components/swingui/src/main/java/org/limewire/ui/swing/library/popup/LibraryNavPopupMenu.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/library/popup/LibraryNavPopupMenu.java
@@ -2,10 +2,10 @@ package org.limewire.ui.swing.library.popup;
 
 import javax.swing.JPopupMenu;
 
+import org.limewire.ui.swing.library.actions.AddFileAction;
 import org.limewire.ui.swing.library.navigator.LibraryNavItem;
 import org.limewire.ui.swing.library.navigator.LibraryNavigatorPanel;
 import org.limewire.ui.swing.library.navigator.LibraryNavItem.NavType;
-import org.limewire.ui.swing.library.navigator.actions.AddFilesAction;
 import org.limewire.ui.swing.library.navigator.actions.ClearAction;
 import org.limewire.ui.swing.library.navigator.actions.DeleteListAction;
 import org.limewire.ui.swing.library.navigator.actions.ExportListAction;
@@ -17,7 +17,7 @@ import com.google.inject.Provider;
 
 public class LibraryNavPopupMenu extends JPopupMenu {
 
-    private final AddFilesAction addAction;
+    private final AddFileAction addAction;
     private final ImportListAction importAction;
     private final ExportListAction exportAction;
     private final Provider<RenameAction> renameAction;
@@ -26,7 +26,7 @@ public class LibraryNavPopupMenu extends JPopupMenu {
     
     @Inject
     public LibraryNavPopupMenu(LibraryNavigatorPanel navPanel, 
-            AddFilesAction addAction, ImportListAction importAction,
+            AddFileAction addAction, ImportListAction importAction,
             ExportListAction exportAction, Provider<RenameAction> renameAction,
             ClearAction clearAction, Provider<DeleteListAction> deleteAction) {
         this.addAction = addAction;
