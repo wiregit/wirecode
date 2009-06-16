7c28233dcad951cd802336e8d01cfed91ec36064
eyu <eyu>
2009-06-16 19:39:50 +0000

    LWC-3260 added isAnonymous, getFriends methods

--- private-components/swingui/src/main/java/org/limewire/ui/swing/filter/FilterableItem.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/filter/FilterableItem.java
@@ -1,8 +1,10 @@
 package org.limewire.ui.swing.filter;
 
+import java.util.Collection;
 import java.util.Map;
 
 import org.limewire.core.api.FilePropertyKey;
+import org.limewire.core.api.friend.Friend;
 import org.limewire.core.api.library.PropertiableFile;
 
 /**
@@ -12,9 +14,20 @@ import org.limewire.core.api.library.PropertiableFile;
 public interface FilterableItem extends PropertiableFile {
 
     /**
+     * Returns an indicator that determines if the item is from an anonymous
+     * source.
+     */
+    boolean isAnonymous();
+    
+    /**
      * Returns the file extension for the item.
      */
     String getFileExtension();
+    
+    /**
+     * Returns a Collection of friends that are sources for the item.
+     */
+    Collection<Friend> getFriends();
     
     /**
      * Returns the size of the item in bytes.
