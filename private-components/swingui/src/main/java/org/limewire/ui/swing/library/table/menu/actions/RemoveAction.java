f4f42f9e105b37597012cff0411e289bd99ee869
meverett <meverett>
2009-06-16 19:28:23 +0000

    Implements remove action in librarytable popup

--- private-components/swingui/src/main/java/org/limewire/ui/swing/library/table/menu/actions/RemoveAction.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/library/table/menu/actions/RemoveAction.java
@@ -2,7 +2,11 @@ package org.limewire.ui.swing.library.table.menu.actions;
 
 import java.awt.event.ActionEvent;
 
+import org.limewire.core.api.library.LocalFileItem;
 import org.limewire.ui.swing.action.AbstractAction;
+import org.limewire.ui.swing.library.navigator.LibraryNavItem;
+import org.limewire.ui.swing.library.navigator.LibraryNavigatorTable;
+import org.limewire.ui.swing.library.table.LibraryTable;
 import org.limewire.ui.swing.util.I18n;
 
 import com.google.inject.Inject;
@@ -11,18 +15,26 @@ import com.google.inject.Inject;
  * Removes the given file from the library. 
  */
 public class RemoveAction extends AbstractAction {
-//    private final LibraryManager libraryManager;
-//    private final LocalFileItem[] fileItemArray;
+    private final LibraryNavigatorTable libraryNavigatorTable;
+    private final LibraryTable libraryTable;
     
     @Inject
-    public RemoveAction() {//final LocalFileItem[] fileItemArray, LibraryManager libraryManager) {
+    public RemoveAction(LibraryNavigatorTable libraryNavigatorTable, LibraryTable libraryTable) {//final LocalFileItem[] fileItemArray, LibraryManager libraryManager) {
         super(I18n.tr("Remove from Library"));
-//        this.libraryManager = libraryManager;
-//        this.fileItemArray = fileItemArray;
+        
+        this.libraryNavigatorTable = libraryNavigatorTable;
+        this.libraryTable = libraryTable;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        LibraryNavItem navItem = libraryNavigatorTable.getSelectedItem();
+        LocalFileItem fileItem = libraryTable.getSelectedItem();
+        
+        navItem.getLocalFileList().removeFile(fileItem.getFile());
+        
+        //TODO: make this function for multi select
+        // 
 //        File currentSong = PlayerUtils.getCurrentSongFile();
 //        for (LocalFileItem fileItem : fileItemArray) {
 //            if(fileItem.getFile().equals(currentSong)){
