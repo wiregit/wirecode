a6cbd0ad70c80acd83494151f66bccbc8fc9c215
mturkel <mturkel>
2009-06-19 19:05:49 +0000

    Iterating over model in a lock.

--- components/core-glue/src/main/java/org/limewire/core/impl/library/LocalFileListImpl.java
+++ components/core-glue/src/main/java/org/limewire/core/impl/library/LocalFileListImpl.java
@@ -227,7 +227,14 @@ abstract class LocalFileListImpl implements LocalFileList {
     
     @Override
     public void removeFiles(Filter<LocalFileItem> filter) {
-        for(LocalFileItem localFileItem : getModel()) {
+        getModel().getReadWriteLock().readLock().lock();
+        List<LocalFileItem> files = null;
+        try {
+            files = new ArrayList<LocalFileItem>(getModel());
+        } finally {
+            getModel().getReadWriteLock().readLock().unlock();
+        }
+        for(LocalFileItem localFileItem : files) {
             if(filter.allow(localFileItem)) {
                 removeFile(localFileItem.getFile());
             }
