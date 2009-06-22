f5f004745f58c93112525db8ae9bf9007f2f5f16
dchen <dchen>
2009-06-22 15:50:57 +0000

    [LWC-3470] - made changes to avoid calling a notification method while holding a lock.  Addresses http://www.limewire.org/fisheye/cru/LWCR-471#c11367

--- components/facebook/src/main/java/org/limewire/facebook/service/ChatManager.java
+++ components/facebook/src/main/java/org/limewire/facebook/service/ChatManager.java
@@ -1,7 +1,7 @@
 package org.limewire.facebook.service;
 
 import java.util.Map;
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.limewire.friend.api.ChatState;
 import org.limewire.friend.api.FriendException;
@@ -15,17 +15,13 @@ import org.limewire.friend.api.MessageWriter;
  *
  * MessageReaders get added to ChatManager either by directly being
  * added via {@link #addMessageReader}, or by being created via
- * {@link IncomingChatListener#incomingChat(MessageWriter)}. Thus
- * IncomingChatListener objects are also managed by this class.
+ * {@link IncomingChatListener#incomingChat(MessageWriter)}.
  *
  */
 class ChatManager {
 
     private final Map<String, MessageReader> readers =
-            new HashMap<String, MessageReader>();
-
-    private final Map<String, IncomingChatListener> incomingChats =
-            new HashMap<String, IncomingChatListener>();
+            new ConcurrentHashMap<String, MessageReader>();
 
     private final FacebookFriendConnection facebookFriendConnection;
 
@@ -37,11 +33,11 @@ class ChatManager {
      * Associates a friend ID with a MessageReader.  Happens when preparing to
      * initiate a new chat with a friend by sending a chat message.
      *
-     * @param friendId ID of friend
-     * @param reader {link @MessageReader} to associate it with
-     * @return MessageWriter interface to use for sending chat messages
+     * @param friendId ID of friend.
+     * @param reader {link @MessageReader} to associate it with.
+     * @return MessageWriter interface to use for sending chat messages.
      */
-    synchronized MessageWriter addMessageReader(String friendId, MessageReader reader) {
+    MessageWriter addMessageReader(String friendId, MessageReader reader) {
         readers.put(friendId, reader);
         return new MessageWriterImpl(friendId);
     }
@@ -53,23 +49,19 @@ class ChatManager {
      * @param friendId ID of friend
      * @return MessageReader message reader associated with friend.
      */
-    synchronized MessageReader getMessageReader(String friendId) {
-        MessageReader handler = readers.get(friendId);
-
-        if (handler == null) {
-            handler = processNewIncomingChat(friendId);
-        }
-        return handler;
+    MessageReader getMessageReader(String friendId) {
+        return readers.get(friendId);
     }
 
     /**
-     * Sets an incoming chat listener for the friend whose ID id friendId
-     * A MessageReader will get associated with the friendId upon a chat being
-     * initiated by the friend.
+     * Sets an incoming chat listener for the friend whose ID is friendId
+     * Creates a {@link MessageReader} object wrapping the incoming chat listener.
      *
+     * @param friendId ID of the friend.
+     * @param listener {@link IncomingChatListener} used to create message readers.
      */
-    synchronized void setIncomingChatListener(String friendId, IncomingChatListener listener) {
-        incomingChats.put(friendId, listener);
+    void setIncomingChatListener(String friendId, IncomingChatListener listener) {
+        readers.put(friendId, new MessageReaderIncomingListenerWrapper(friendId, listener));
     }
 
     /**
@@ -77,23 +69,49 @@ class ChatManager {
      *
      * @param friendId ID of friend
      */
-    synchronized void removeChat(String friendId) {
-        incomingChats.remove(friendId);
+    void removeChat(String friendId) {
         readers.remove(friendId);
     }
 
-    private MessageReader processNewIncomingChat(String friendId) {
-        IncomingChatListener listener = incomingChats.get(friendId);
+    /**
+     * {@link MessageReader} wrapper object backed by a MessageReader created
+     * via incoming chat (using {@link IncomingChatListener#incomingChat(MessageWriter)}.
+     *
+     */
+    private class MessageReaderIncomingListenerWrapper implements MessageReader {
 
-        if (listener != null) {
-            MessageWriter writer = new MessageWriterImpl(friendId);
-            MessageReader reader = listener.incomingChat(writer);
-            readers.put(friendId, reader);
-            return reader;
+        private final MessageWriter messageWriter;
+        private final IncomingChatListener listener;
+        private MessageReader messagereader;
+        
+        public MessageReaderIncomingListenerWrapper(String friendId, IncomingChatListener listener) {
+            this.messageWriter = new MessageWriterImpl(friendId);
+            this.listener = listener;
+            this.messagereader = null;
         }
-        return null;
+
+        @Override
+        public void readMessage(String message) {
+            getMessageReader().readMessage(message);
+        }
+
+        @Override
+        public void newChatState(ChatState chatState) {
+            getMessageReader().newChatState(chatState);
+        }
+
+        private MessageReader getMessageReader() {
+            if (messagereader == null) {
+                messagereader = listener.incomingChat(messageWriter);
+            }
+            return messagereader;
+        }
     }
 
+    /**
+     * {@link MessageWriter} impl that delegates to the facebook connection
+     * to write messages or set chat states.
+     */
     private class MessageWriterImpl implements MessageWriter {
 
         private final String friendId;
