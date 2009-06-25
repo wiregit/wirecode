4854bb8e0354b7519ae5af4fbac848ceebd2c873
meverett <meverett>
2009-06-25 15:26:02 +0000

    LWC-3522

--- private-components/swingui/src/main/java/org/limewire/ui/swing/library/table/AddToListMenu.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/library/table/AddToListMenu.java
@@ -5,17 +5,20 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.Icon;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+import org.jdesktop.application.Resource;
 import org.limewire.core.api.library.LocalFileList;
 import org.limewire.core.api.library.SharedFileList;
 import org.limewire.core.api.library.SharedFileListManager;
 import org.limewire.ui.swing.action.AbstractAction;
 import org.limewire.ui.swing.library.LibrarySelected;
 import org.limewire.ui.swing.util.BackgroundExecutorService;
+import org.limewire.ui.swing.util.GuiUtils;
 import org.limewire.ui.swing.util.I18n;
 
 import com.google.inject.Inject;
@@ -23,12 +26,21 @@ import com.google.inject.Provider;
 
 public class AddToListMenu extends JMenu {
     
+    @Resource
+    private Icon publicIcon;
+    @Resource
+    private Icon unsharedIcon;
+    @Resource
+    private Icon sharedIcon;
+    
     private final Provider<List<File>> selectedFiles;
     
     @Inject
     public AddToListMenu(final SharedFileListManager manager,
             @LibrarySelected Provider<List<File>> selectedFiles) {
         super("Add to List");
+        
+        GuiUtils.assignResources(this);
         
         this.selectedFiles = selectedFiles;
         
@@ -41,7 +53,7 @@ public class AddToListMenu extends JMenu {
                 manager.getModel().getReadWriteLock().readLock().lock();
                 try { 
                     for(SharedFileList fileList : manager.getModel()) {
-                        menu.add(new AddListAction(fileList.getCollectionName(), fileList));
+                        menu.add(new AddListAction(fileList.getCollectionName(), getListIcon(fileList), fileList));
                     }
                 } finally {
                     manager.getModel().getReadWriteLock().readLock().unlock();
@@ -53,11 +65,21 @@ public class AddToListMenu extends JMenu {
         add(new JMenuItem(I18n.tr("Public Shared")));
     }
     
+    private Icon getListIcon(SharedFileList sharedFileList) {
+        if(sharedFileList.isPublic())
+            return publicIcon;
+        else if(sharedFileList.getFriendIds().size() == 0)
+            return unsharedIcon;
+        else
+            return sharedIcon;
+    }
+    
     private class AddListAction extends AbstractAction {
         private final LocalFileList localFileList;
         
-        public AddListAction(String text, LocalFileList localFileList) {
+        public AddListAction(String text, Icon icon, LocalFileList localFileList) {
             super(text);
+            putValue(SMALL_ICON, icon);
             this.localFileList = localFileList;
         }
         
