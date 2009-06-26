73b0b893aa4de928b9d48d0bfb90654f4ac15ba6
eyu <eyu>
2009-06-26 01:26:29 +0000

    LWC-3403 added bindings for table rect, event listener list

--- private-components/swingui/src/main/java/org/limewire/ui/swing/library/LimeWireUiLibraryModule.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/library/LimeWireUiLibraryModule.java
@@ -1,21 +1,28 @@
 package org.limewire.ui.swing.library;
 
+import java.awt.Rectangle;
 import java.io.File;
 import java.util.List;
 
 import org.limewire.core.api.library.LocalFileItem;
 import org.limewire.core.api.library.LocalFileList;
+import org.limewire.listener.EventListenerList;
 import org.limewire.ui.swing.library.navigator.LibraryNavItem;
 import org.limewire.ui.swing.library.navigator.LibraryNavigatorPanel;
+import org.limewire.ui.swing.library.sharing.LibrarySharingEvent;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provider;
 import com.google.inject.Provides;
+import com.google.inject.TypeLiteral;
 
 public class LimeWireUiLibraryModule extends AbstractModule {
     
     @Override
     protected void configure() {
+        // Bind listener support for LibrarySharingEvent.
+        EventListenerList<LibrarySharingEvent> listenerList = new EventListenerList<LibrarySharingEvent>();
+        bind(new TypeLiteral<EventListenerList<LibrarySharingEvent>>(){}).toInstance(listenerList);
     }
     
     @Provides @LibrarySelected LocalFileList selectedLFL(Provider<LibraryNavigatorPanel> navigator) {
@@ -33,6 +40,10 @@ public class LimeWireUiLibraryModule extends AbstractModule {
     
     @Provides @LibrarySelected List<LocalFileItem> selectedFileItems(Provider<LibraryPanel> libraryPanel) {
         return libraryPanel.get().getSelectedItems();
+    }
+    
+    @Provides @LibraryTableRect Rectangle tableRect(Provider<LibraryPanel> libraryPanel) {
+        return libraryPanel.get().getTableListRect();
     }
 
 }
