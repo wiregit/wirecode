a9474b35372032702f16319fb80f86d7dcceefd7
eyu <eyu>
2009-06-30 22:44:32 +0000

    LWC-3282 revised to set playlist for playable files

--- private-components/swingui/src/main/java/org/limewire/ui/swing/library/table/LaunchFileAction.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/library/table/LaunchFileAction.java
@@ -7,6 +7,10 @@ import java.util.List;
 import org.limewire.core.api.library.LocalFileItem;
 import org.limewire.ui.swing.action.AbstractAction;
 import org.limewire.ui.swing.library.LibrarySelected;
+import org.limewire.ui.swing.library.navigator.LibraryNavigatorPanel;
+import org.limewire.ui.swing.player.PlayerMediator;
+import org.limewire.ui.swing.player.PlayerUtils;
+import org.limewire.ui.swing.settings.SwingUiSettings;
 import org.limewire.ui.swing.util.I18n;
 import org.limewire.ui.swing.util.NativeLaunchUtils;
 
@@ -20,18 +24,34 @@ import com.google.inject.Provider;
  */
 class LaunchFileAction extends AbstractAction {
     private final Provider<List<LocalFileItem>> selectedLocalFileItems;
+    private final Provider<LibraryNavigatorPanel> libraryNavProvider;
+    private final Provider<PlayerMediator> playerProvider;
     
     @Inject
-    public LaunchFileAction(@LibrarySelected Provider<List<LocalFileItem>> selectedLocalFileItems) {
+    public LaunchFileAction(@LibrarySelected Provider<List<LocalFileItem>> selectedLocalFileItems,
+        Provider<LibraryNavigatorPanel> libraryNavProvider,
+        Provider<PlayerMediator> playerProvider) {
         super(I18n.tr("Open"));
 
         this.selectedLocalFileItems = selectedLocalFileItems;
+        this.libraryNavProvider = libraryNavProvider;
+        this.playerProvider = playerProvider;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         List<LocalFileItem> localFileItems = new ArrayList<LocalFileItem>(selectedLocalFileItems.get());
-        if(localFileItems.size() > 0)
-            NativeLaunchUtils.safeLaunchFile(localFileItems.get(0).getFile());
+        if (localFileItems.size() > 0) {
+            // Get first selected item.
+            LocalFileItem fileItem = localFileItems.get(0);
+            
+            if (SwingUiSettings.PLAYER_ENABLED.getValue() && PlayerUtils.isPlayableFile(fileItem.getFile())) {
+                // Set active playlist and play file item.
+                playerProvider.get().setActivePlaylist(libraryNavProvider.get().getSelectedNavItem());
+                playerProvider.get().play(fileItem);
+            } else {    
+                NativeLaunchUtils.safeLaunchFile(fileItem.getFile());
+            }
+        }
     }
 }
--- private-components/swingui/src/main/java/org/limewire/ui/swing/library/table/PlayAction.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/library/table/PlayAction.java
@@ -7,8 +7,11 @@ import java.util.List;
 import org.limewire.core.api.library.LocalFileItem;
 import org.limewire.ui.swing.action.AbstractAction;
 import org.limewire.ui.swing.library.LibrarySelected;
+import org.limewire.ui.swing.player.PlayerMediator;
 import org.limewire.ui.swing.player.PlayerUtils;
+import org.limewire.ui.swing.settings.SwingUiSettings;
 import org.limewire.ui.swing.util.I18n;
+import org.limewire.ui.swing.util.NativeLaunchUtils;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
@@ -19,19 +22,31 @@ import com.google.inject.Provider;
  */
 class PlayAction extends AbstractAction {
     private final Provider<List<LocalFileItem>> selectedLocalFileItems;
+    private final Provider<PlayerMediator> playerProvider;
     
     @Inject
-    public PlayAction(@LibrarySelected Provider<List<LocalFileItem>> selectedLocalFileItems) {
+    public PlayAction(@LibrarySelected Provider<List<LocalFileItem>> selectedLocalFileItems,
+            Provider<PlayerMediator> playerProvider) {
         super(I18n.tr("Play Once"));
 
         this.selectedLocalFileItems = selectedLocalFileItems;
+        this.playerProvider = playerProvider;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         List<LocalFileItem> localFileItems = new ArrayList<LocalFileItem>(selectedLocalFileItems.get());
-        if(localFileItems.size() > 0)
-            PlayerUtils.playOrLaunch(localFileItems.get(0).getFile());
-        //TODO: this should load the selected list as the current playlist
+        if (localFileItems.size() > 0) {
+            // Get first selected item.
+            LocalFileItem fileItem = localFileItems.get(0);
+            
+            if (SwingUiSettings.PLAYER_ENABLED.getValue() && PlayerUtils.isPlayableFile(fileItem.getFile())) {
+                // Clear active playlist and play file item.
+                playerProvider.get().setActivePlaylist(null);
+                playerProvider.get().play(fileItem);
+            } else {    
+                NativeLaunchUtils.safeLaunchFile(fileItem.getFile());
+            }
+        }
     }
 }
