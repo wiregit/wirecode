dbd436bb12807e611691b619164f8721c0942332
sberlin <sberlin>
2009-06-30 22:33:04 +0000

    spin a little circle while signing into friends.

--- components/common/src/main/java/org/limewire/listener/EventUtils.java
+++ components/common/src/main/java/org/limewire/listener/EventUtils.java
@@ -10,4 +10,13 @@ public class EventUtils {
         SourcedEvent<S> lastEvent = eventBean.getLastEvent();
         return lastEvent != null ? lastEvent.getSource() : null;
     }
+    
+    /**
+     * @return the type of the last event of an event bean or null if the 
+     * last event is null or the type is null.
+     */
+    public static final <T, E extends TypedEvent<T>> T getType(EventBean<E> eventBean) {
+        TypedEvent<T> lastEvent = eventBean.getLastEvent();
+        return lastEvent != null ? lastEvent.getType() : null;
+    }
 }
--- private-components/swingui/src/main/java/org/limewire/ui/swing/mainframe/FriendsButton.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/mainframe/FriendsButton.java
@@ -6,17 +6,23 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Insets;
 import java.awt.Point;
+import java.awt.Shape;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.Icon;
 import javax.swing.JPopupMenu;
+import javax.swing.Timer;
 import javax.swing.border.Border;
 
 import org.jdesktop.application.Resource;
 import org.jdesktop.swingx.JXButton;
 import org.jdesktop.swingx.painter.AbstractPainter;
+import org.jdesktop.swingx.painter.BusyPainter;
 import org.limewire.friend.api.FriendConnectionEvent;
 import org.limewire.listener.EventBean;
 import org.limewire.listener.EventListener;
+import org.limewire.listener.EventUtils;
 import org.limewire.listener.ListenerSupport;
 import org.limewire.listener.SwingEDTEvent;
 import org.limewire.ui.swing.components.LimeComboBox;
@@ -32,14 +38,19 @@ public class FriendsButton extends LimeComboBox {
     
     @Resource private Icon friendOnlineIcon;
     @Resource private Icon friendOfflineIcon;
+    @Resource private Icon friendLoadingIcon;
     @Resource private Icon friendOnlineSelectedIcon;
     @Resource private Icon friendOfflineSelectedIcon;
+    @Resource private Icon friendLoadingSelectedIcon;
     
     @Resource private Color borderForeground = PainterUtils.TRASPARENT;
     @Resource private Color borderInsideRightForeground = PainterUtils.TRASPARENT;
     @Resource private Color borderInsideBottomForeground = PainterUtils.TRASPARENT;
     @Resource private Color dividerForeground = PainterUtils.TRASPARENT;
     
+    private final BusyPainter busyPainter;
+    
+    private Timer busy;
     
     @Inject
     public FriendsButton(ComboBoxDecorator comboBoxDecorator,
@@ -93,35 +104,88 @@ public class FriendsButton extends LimeComboBox {
                     icon = object.getIcon();
                 }
                 icon.paintIcon(object, g, 0, (height-icon.getIconHeight())/2);
+                
+                // the width/heigth offset is very specific to the loading img.
+                if(busy != null) {
+                    g.translate(21, 22);
+                    busyPainter.paint(g, object, width, height);
+                    g.translate(-21, -22);
+                }
             }
         });
-        setPopupPosition(new Point(0, -4));
-  
+        setPopupPosition(new Point(0, -4));  
+        
+        busyPainter = new BusyPainter() {
+            @Override
+            protected void init(Shape point, Shape trajectory, Color b, Color h) {
+                super.init(getScaledDefaultPoint(8), 
+                        getScaledDefaultTrajectory(8),
+                        Color.decode("#acacac"),  Color.decode("#545454"));
+            }
+        };
+    }
+    
+    private void setIconFromType(FriendConnectionEvent.Type eventType) {
+        if(eventType == null) {
+            eventType = FriendConnectionEvent.Type.DISCONNECTED;
+        }
+        
+        switch(eventType) {
+        case CONNECT_FAILED:
+        case DISCONNECTED:
+            setIcon(friendOfflineIcon);
+            setPressedIcon(friendOfflineSelectedIcon);
+            stopAnimation();
+            break;
+        case CONNECTED:
+            setIcon(friendOnlineIcon);
+            setPressedIcon(friendOnlineSelectedIcon);
+            stopAnimation();
+            break;
+        case CONNECTING:
+            setIcon(friendLoadingIcon);
+            setPressedIcon(friendLoadingSelectedIcon);
+            startAnimation();
+        }
     }
 
     
     @Inject
     void register(EventBean<FriendConnectionEvent> connectBean, ListenerSupport<FriendConnectionEvent> connectionSupport) {
-        if(connectBean.getLastEvent() != null && connectBean.getLastEvent().getType() == FriendConnectionEvent.Type.CONNECTED) {
-            setIcon(friendOnlineIcon);
-            setPressedIcon(friendOnlineSelectedIcon);
-        } else {
-            setIcon(friendOfflineIcon);
-            setPressedIcon(friendOfflineSelectedIcon);
-        }
-        
+        setIconFromType(EventUtils.getType(connectBean));        
         connectionSupport.addListener(new EventListener<FriendConnectionEvent>() {
             @Override
             @SwingEDTEvent
             public void handleEvent(FriendConnectionEvent event) {
-                if(event.getType() == FriendConnectionEvent.Type.CONNECTED) {
-                    setIcon(friendOnlineIcon);
-                    setPressedIcon(friendOnlineSelectedIcon);
-                } else {
-                    setIcon(friendOfflineIcon);
-                    setPressedIcon(friendOfflineSelectedIcon);
-                }
+                setIconFromType(event.getType());
             }
         });
+    }
+    
+    
+    // animation code ripped from JXBusyLabel
+    private void startAnimation() {
+        if(busy != null) {
+            stopAnimation();
+        }
+        
+        busy = new Timer(100, new ActionListener() {
+            int frame = busyPainter.getPoints();
+            public void actionPerformed(ActionEvent e) {
+                frame = (frame+1)%busyPainter.getPoints();
+                busyPainter.setFrame(frame);
+                repaint();
+            }
+        });
+        busy.start();
+    }    
+    
+    private void stopAnimation() {
+        if (busy != null) {
+            busy.stop();
+            busyPainter.setFrame(-1);
+            repaint();
+            busy = null;
+        }
     }
 }
