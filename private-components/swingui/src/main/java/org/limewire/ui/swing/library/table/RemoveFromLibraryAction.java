ccea08bd0389085b1da28869c2afd1aea2d0fcd0
meverett <meverett>
2009-07-01 19:21:46 +0000

    Updates remove warning text depending on if the file is shared or not

--- private-components/swingui/src/main/java/org/limewire/ui/swing/library/table/RemoveFromLibraryAction.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/library/table/RemoveFromLibraryAction.java
@@ -9,6 +9,8 @@ import javax.swing.JOptionPane;
 
 import org.limewire.core.api.library.LibraryManager;
 import org.limewire.core.api.library.LocalFileItem;
+import org.limewire.core.api.library.SharedFileList;
+import org.limewire.core.api.library.SharedFileListManager;
 import org.limewire.ui.swing.action.AbstractAction;
 import org.limewire.ui.swing.components.FocusJOptionPane;
 import org.limewire.ui.swing.library.LibrarySelected;
@@ -25,14 +27,16 @@ import com.google.inject.Provider;
 class RemoveFromLibraryAction extends AbstractAction {
     private final Provider<List<LocalFileItem>> selectedLocalFileItems;
     private final LibraryManager libraryManager;
+    private final SharedFileListManager sharedFileListManager;
     
     @Inject
     public RemoveFromLibraryAction(@LibrarySelected Provider<List<LocalFileItem>> selectedLocalFileItems, 
-            LibraryManager libraryManager) {
+            LibraryManager libraryManager, SharedFileListManager sharedFileListManager) {
         super(I18n.tr("Remove from Library"));
         
         this.selectedLocalFileItems = selectedLocalFileItems;
         this.libraryManager = libraryManager;
+        this.sharedFileListManager = sharedFileListManager;
     }
 
     @Override
@@ -40,7 +44,7 @@ class RemoveFromLibraryAction extends AbstractAction {
         File currentSong = PlayerUtils.getCurrentSongFile();
         List<LocalFileItem> selected = selectedLocalFileItems.get();
         
-        int confirmation = FocusJOptionPane.showConfirmDialog(null, getMessage(selected), I18n.tr("Remove Files"), JOptionPane.OK_CANCEL_OPTION); 
+        int confirmation = FocusJOptionPane.showConfirmDialog(null, getMessage(selected), I18n.trn("Remove File","Remove Files", selected.size()), JOptionPane.OK_CANCEL_OPTION); 
         if (confirmation == JOptionPane.OK_OPTION) {
             final List<File> toRemove = new ArrayList<File>(selected.size());
             for(LocalFileItem item : selected) {
@@ -67,9 +71,40 @@ class RemoveFromLibraryAction extends AbstractAction {
     
     private String getMessage(List<LocalFileItem> list) {
         if(list.size() == 1) {
-            return I18n.tr("Remove {0} from Library? This will unshare this file with everyone.", list.get(0).getFileName());
+            if(containsSharedFile(list))
+                return I18n.tr("Remove {0} from Library? This will unshare this file with everyone.", list.get(0).getFileName());
+            else 
+                return I18n.tr("Remove {0} from Library?", list.get(0).getFileName());
         } else {
-            return I18n.tr("Remove {0} files from Library? This will unshare these files with everyone.", list.size());
+            if(containsSharedFile(list))
+                return I18n.tr("Remove {0} files from Library? This will unshare these files with everyone.", list.size());
+            else
+                return I18n.tr("Remove {0} files from Library?", list.size());
         }
+    }
+    
+    /**
+     * Returns true if at least one file list this list is contained in a 
+     * FileList that is shared with at least one person.
+     */
+    private boolean containsSharedFile(List<LocalFileItem> list) {
+        boolean hasSharedFile = false;
+        
+        sharedFileListManager.getModel().getReadWriteLock().readLock().lock();
+        try {
+            for(SharedFileList sharedFileList : sharedFileListManager.getModel()) {
+                if(sharedFileList.getFriendIds().size() > 0) {
+                    for(LocalFileItem item : list) {
+                        if(sharedFileList.contains(item.getFile())) {
+                            hasSharedFile = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        } finally {
+            sharedFileListManager.getModel().getReadWriteLock().readLock().unlock();
+        }
+        return hasSharedFile;
     }
 }
