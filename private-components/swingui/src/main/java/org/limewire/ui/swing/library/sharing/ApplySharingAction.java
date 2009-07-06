29560c934da71a399c135e0a62f6725e7b54b887
meverett <meverett>
2009-07-06 18:43:13 +0000

    forces table repaint when sharing list changes

--- private-components/swingui/src/main/java/org/limewire/ui/swing/library/sharing/ApplySharingAction.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/library/sharing/ApplySharingAction.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.limewire.ui.swing.action.AbstractAction;
 import org.limewire.ui.swing.library.navigator.LibraryNavigatorPanel;
+import org.limewire.ui.swing.library.table.LibraryTable;
 import org.limewire.ui.swing.util.I18n;
 
 import com.google.inject.Inject;
@@ -15,16 +16,19 @@ class ApplySharingAction extends AbstractAction {
     private final Provider<LibrarySharingPanel> librarySharingPanel;
     private final Provider<LibrarySharingEditablePanel> librarySharingEditablePanel;
     private final Provider<LibraryNavigatorPanel> libraryNavigatorPanel;
+    private final Provider<LibraryTable> libraryTable;
     
     @Inject
     public ApplySharingAction(Provider<LibrarySharingPanel> librarySharingPanel,
             Provider<LibrarySharingEditablePanel> librarySharingEditablePanel,
-            Provider<LibraryNavigatorPanel> libraryNavigatorPanel) {
+            Provider<LibraryNavigatorPanel> libraryNavigatorPanel,
+            Provider<LibraryTable> libraryTable) {
         super(I18n.tr("Apply"));
         
         this.librarySharingPanel = librarySharingPanel;
         this.librarySharingEditablePanel = librarySharingEditablePanel;
         this.libraryNavigatorPanel = libraryNavigatorPanel;
+        this.libraryTable = libraryTable;
     }
     
     @Override
@@ -33,5 +37,6 @@ class ApplySharingAction extends AbstractAction {
         librarySharingPanel.get().setFriendIdsForSharing(friends);
         libraryNavigatorPanel.get().repaint();
         librarySharingPanel.get().showFriendListView();
+        libraryTable.get().repaint();
     }
 }
