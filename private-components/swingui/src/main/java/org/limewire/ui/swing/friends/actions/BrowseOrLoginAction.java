8b6c7c7916b874e36d21d40aa8613a0dca43d0b7
michaelt <michaelt>
2009-07-07 20:33:34 +0000

    LWC-3749 - Add back code to prevent login while auto login is in progress

--- private-components/swingui/src/main/java/org/limewire/ui/swing/friends/actions/BrowseOrLoginAction.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/friends/actions/BrowseOrLoginAction.java
@@ -8,6 +8,7 @@ import org.limewire.friend.api.FriendConnection;
 import org.limewire.friend.api.FriendConnectionEvent;
 import org.limewire.listener.EventBean;
 import org.limewire.listener.EventUtils;
+import org.limewire.ui.swing.friends.login.AutoLoginService;
 
 import com.google.inject.Inject;
 
@@ -15,22 +16,27 @@ public class BrowseOrLoginAction extends AbstractAction {
     private final LoginAction loginAction;
 
     private final BrowseFriendsAction browseFriendAction;
-
+    private final AutoLoginService autoLoginService;
+    
     private final EventBean<FriendConnectionEvent> friendConnectionEventBean;
 
     @Inject
     private BrowseOrLoginAction(LoginAction loginAction,
             BrowseFriendsAction browseFriendAction,
-            EventBean<FriendConnectionEvent> friendConnectionEventBean) {
+            EventBean<FriendConnectionEvent> friendConnectionEventBean,
+            AutoLoginService autoLoginService) {
         this.loginAction = loginAction;
         this.browseFriendAction = browseFriendAction;
         this.friendConnectionEventBean = friendConnectionEventBean;
+        this.autoLoginService = autoLoginService;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         FriendConnection friendConnection = EventUtils.getSource(friendConnectionEventBean);
-        if(friendConnection != null && friendConnection.isLoggedIn()) {
+        if ((friendConnection != null 
+                && (friendConnection.isLoggedIn() || friendConnection.isLoggingIn())) 
+                || autoLoginService.isAttemptingLogin()) {
             browseFriendAction.actionPerformed(e);
         } else {
             loginAction.actionPerformed(e);
