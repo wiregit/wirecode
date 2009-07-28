8ed5f439ee444c5d91030b6e30af8a763295b7cf
dchen <dchen>
2009-07-28 20:06:58 +0000

    [LWC-3098] - Waiting for roster/connection events instead of doing a hard coded sleep.  Hopefully this will take care of the intermittent test failures.

--- components/xmpp/src/test/java/org/limewire/xmpp/client/impl/XmppFriendSubscriptionTest.java
+++ components/xmpp/src/test/java/org/limewire/xmpp/client/impl/XmppFriendSubscriptionTest.java
@@ -1,6 +1,9 @@
 package org.limewire.xmpp.client.impl;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -10,6 +13,8 @@ import org.limewire.friend.api.FriendConnectionConfiguration;
 import org.limewire.friend.api.FriendException;
 import org.limewire.friend.api.FriendRequest;
 import org.limewire.friend.api.FriendRequestEvent;
+import org.limewire.friend.api.RosterEvent;
+import org.limewire.friend.api.FriendConnectionEvent;
 import org.limewire.listener.ListenerSupport;
 import org.limewire.listener.EventListener;
 import org.limewire.logging.Log;
@@ -25,6 +30,8 @@ import com.google.inject.Key;
 public class XmppFriendSubscriptionTest extends XmppBaseTestCase {
     
     private static final Log LOG = LogFactory.getLog(XmppFriendSubscriptionTest.class);
+    private static final int MAX_WAIT_FRIEND_REQUEST = 20;
+    
        
     static {
         // get the hour (00 - 23) and modulo 4 in order to cycle thru
@@ -121,7 +128,7 @@ public class XmppFriendSubscriptionTest extends XmppBaseTestCase {
         connectionA.addNewFriend(USERNAME_B, USERNAME_B).get();
 
         // sleep to wait for automatedtestfriendb to confirm, friends to exchange roster packets, etc
-        Thread.sleep(SLEEP);
+        waitForFriendRequest(true);
 
         // check that both automatedtestfrienda and automatedtestfriendb have each other on their roster
         assertEquals(1, autoFriendARosterListener.getRosterSize());
@@ -162,7 +169,7 @@ public class XmppFriendSubscriptionTest extends XmppBaseTestCase {
         connectionA = (XMPPFriendConnectionImpl) factories[0].login(configFive).get();
         connectionB = (XMPPFriendConnectionImpl) factories[1].login(configSix).get();
 
-        Thread.sleep(SLEEP);
+        waitForConnection();
 
         // check that both A and B are not aware of each other (not subscribed)
         // Both users are signed in, but since they are not friends (subscribed to each others' statuses)
@@ -190,7 +197,7 @@ public class XmppFriendSubscriptionTest extends XmppBaseTestCase {
         connectionA.addNewFriend(USERNAME_B, USERNAME_B).get();
 
         // sleep to wait for automatedtestfriendb to confirm, friends to exchange roster packets, etc
-        Thread.sleep(SLEEP);
+        waitForFriendRequest(true);
 
         // check that both automatedtestfrienda and automatedtestfriendb have each other on their roster
         assertEquals(1, autoFriendARosterListener.getRosterSize());
@@ -200,7 +207,7 @@ public class XmppFriendSubscriptionTest extends XmppBaseTestCase {
 
         // test friend removal
         connectionA.removeFriend(USERNAME_B).get();
-        Thread.sleep(SLEEP);
+        waitForFriendRequest(false);
 
         connectionA.logout().get();
         connectionB.logout().get();
@@ -216,13 +223,63 @@ public class XmppFriendSubscriptionTest extends XmppBaseTestCase {
         connectionA = (XMPPFriendConnectionImpl) factories[0].login(configFive).get();
         connectionB = (XMPPFriendConnectionImpl) factories[1].login(configSix).get();
 
-        Thread.sleep(SLEEP);
+        waitForConnection();
         
         assertTrue(connectionA.isLoggedIn());
         assertTrue(connectionB.isLoggedIn());
 
         assertNull(autoFriendARosterListener.getFirstPresence(USERNAME_B));
         assertNull(autoFriendBRosterListener.getFirstPresence(USERNAME_A));
+    }
+    
+    private void waitForFriendRequest(boolean confirmed) throws Exception {
+        final CountDownLatch latch = new CountDownLatch(2);
+        final RosterEvent.Type expectedType = confirmed ? RosterEvent.Type.FRIENDS_ADDED :
+            RosterEvent.Type.FRIENDS_DELETED;
+        
+        class RosterEventListener implements EventListener<RosterEvent> {
+            RosterEventListener(String expectedFriend) {
+                this.expectedFriend = expectedFriend;    
+            }
+            AtomicBoolean friendInRoster = new AtomicBoolean(false);
+            String expectedFriend;
+            
+            @Override
+            public void handleEvent(RosterEvent event) {
+                if (event.getType() == expectedType) {
+                    assertEquals(1, event.getData().size());
+                    if (expectedFriend.equals(event.getData().iterator().next().getId())) {
+                        if (!friendInRoster.getAndSet(true)) {
+                            latch.countDown();
+                        }
+                    }
+                }
+            }
+        }
+        injectors[1].getInstance(Key.get(new TypeLiteral<ListenerSupport<RosterEvent>>(){})).
+                addListener(new RosterEventListener(USERNAME_A));
+        injectors[0].getInstance(Key.get(new TypeLiteral<ListenerSupport<RosterEvent>>(){})).
+                addListener(new RosterEventListener(USERNAME_B));
+        assertTrue(latch.await(MAX_WAIT_FRIEND_REQUEST, TimeUnit.SECONDS));
+        
+    }
+    
+    private void waitForConnection() throws Exception {
+        final CountDownLatch latch = new CountDownLatch(2);
+        
+        class FriendConnectionEventListener implements EventListener<FriendConnectionEvent> {
+            @Override
+            public void handleEvent(FriendConnectionEvent event) {
+                if (event.getType() == FriendConnectionEvent.Type.CONNECTED) {
+                    latch.countDown();
+                }
+            }    
+        }
+        injectors[1].getInstance(Key.get(new TypeLiteral<ListenerSupport<FriendConnectionEvent>>(){})).
+                addListener(new FriendConnectionEventListener());
+        injectors[0].getInstance(Key.get(new TypeLiteral<ListenerSupport<FriendConnectionEvent>>(){})).
+                addListener(new FriendConnectionEventListener());
+        assertTrue(latch.await(SLEEP, TimeUnit.SECONDS));
     }
 
     private void removeAllUsersFromRoster(FriendConnection conn) throws FriendException, ExecutionException, InterruptedException {
