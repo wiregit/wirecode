21c24ea6baa5269e0b00be11d0723fb89c5f1931
pvertenten <pvertenten>
2009-08-05 14:29:20 +0000

    Adding priority, size and total done to the TorrentFileEntry api.

--- components/bittorrent-api/src/main/java/org/limewire/bittorrent/TorrentFileEntry.java
+++ components/bittorrent-api/src/main/java/org/limewire/bittorrent/TorrentFileEntry.java
@@ -13,4 +13,24 @@ public interface TorrentFileEntry {
      * Returns the path of the file in the torrent.
      */
     public String getPath();
+    
+    /**
+     * Returns the size of this file entry in the torrent. 
+     */
+    public long getSize();
+    
+    /**
+     * Returns the total number of bytes downloaded for this file so far. 
+     */
+    public long getTotalDone();
+    
+    /**
+     * Returns the download priority for this file.
+     */
+    public int getPriority();
+    
+    /**
+     * Returns the progress for downloading this file. 
+     */
+    public float getProgress();
 }
--- components/libtorrent/src/main/java/org/limewire/libtorrent/LibTorrentFileEntry.java
+++ components/libtorrent/src/main/java/org/limewire/libtorrent/LibTorrentFileEntry.java
@@ -6,24 +6,57 @@ import com.sun.jna.Structure;
 import com.sun.jna.WString;
 
 /**
- * Represents a file in the torrent. 
+ * Represents a file in the torrent.
  */
-public class LibTorrentFileEntry extends Structure implements Structure.ByReference, TorrentFileEntry {
+public class LibTorrentFileEntry extends Structure implements Structure.ByReference,
+        TorrentFileEntry {
+
     public int index;
+
     public WString path;
+
+    public long size;
+
+    public long total_done;
+
+    public int priority;
 
     @Override
     public int getIndex() {
-       return index;
+        return index;
     }
 
     @Override
     public String getPath() {
         return path.toString();
     }
-    
+
+    @Override
+    public long getSize() {
+        return size;
+    }
+
+    @Override
+    public long getTotalDone() {
+        return total_done;
+    }
+
+    @Override
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public float getProgress() {
+        if (getSize() == 0) {
+            return 1;
+        }
+
+        return getTotalDone() / (float) getSize();
+    }
+
     @Override
     public String toString() {
-        return index + " - " + path;
+        return index + " - " + path + " - " + total_done + "/" + size + "priority: " + priority;
     }
 }
