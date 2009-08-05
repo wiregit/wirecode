5ed0a0f8dd068d46aaaecfe1e838e3ee180d6dce
pvertenten <pvertenten>
2009-08-05 21:20:02 +0000

    Adding more peer information.

--- components/libtorrent/src/main/java/org/limewire/libtorrent/LibTorrentPeer.java
+++ components/libtorrent/src/main/java/org/limewire/libtorrent/LibTorrentPeer.java
@@ -3,7 +3,13 @@ package org.limewire.libtorrent;
 import com.sun.jna.Structure;
 
 public class LibTorrentPeer extends Structure implements Structure.ByReference {
+    public String peer_id;
     public String ip;
     public short source;
-    
+    public float up_speed;
+    public float down_speed;
+    public float payload_up_speed;
+    public float payload_down_speed;
+    public float progress;
+    public String country;
 }
