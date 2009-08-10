968c26bc6314275b8ea223ed20b975e5811b292e
eyu <eyu>
2009-08-10 22:37:01 +0000

    added methods to set editor, renderer, style

--- private-components/swingui/src/main/java/org/limewire/ui/swing/search/resultpanel/ListViewTable.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/resultpanel/ListViewTable.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.swing.event.ChangeEvent;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 
 import org.jdesktop.application.Resource;
 import org.jdesktop.swingx.decorator.ColorHighlighter;
@@ -16,6 +18,7 @@ import org.limewire.core.api.search.store.StoreStyle;
 import org.limewire.ui.swing.search.model.VisualSearchResult;
 import org.limewire.ui.swing.search.model.VisualStoreResult;
 import org.limewire.ui.swing.search.resultpanel.list.ListViewRowHeightRule;
+import org.limewire.ui.swing.search.resultpanel.list.ListViewTableEditorRenderer;
 import org.limewire.ui.swing.search.resultpanel.list.ListViewRowHeightRule.RowDisplayResult;
 import org.limewire.ui.swing.util.GuiUtils;
 
@@ -30,6 +33,8 @@ public class ListViewTable extends ResultsTable<VisualSearchResult> {
     
     private final ListViewRowHeightRule rowHeightRule;
     
+    private final ColorHighlighter storeHighlighter;
+    
     /**
      * Cache for RowDisplayResult which could be expensive to generate with
      * large search result sets.
@@ -39,15 +44,26 @@ public class ListViewTable extends ResultsTable<VisualSearchResult> {
     
     private boolean ignoreRepaints;
     
+    private ListViewTableEditorRenderer listEditor;
+    
+    private ListViewTableEditorRenderer listRenderer;
+    
     /**
      * Constructs a ListViewTable using the specified row height rule and
      * store style.
      */
-    public ListViewTable(ListViewRowHeightRule rowHeightRule, StoreStyle storeStyle) {
+    public ListViewTable(ListViewRowHeightRule rowHeightRule) {
         super();
         this.rowHeightRule = rowHeightRule;
         
         GuiUtils.assignResources(this);
+        
+        storeHighlighter = new ColorHighlighter(new HighlightPredicate() {
+            public boolean isHighlighted(Component renderer, ComponentAdapter adapter) {
+                Object value = getValueAt(adapter.row, 0);
+                return (value instanceof VisualStoreResult);
+            }
+        }, getBackground(), null, getTableColors().selectionColor, null);
         
         setGridColor(Color.decode("#EBEBEB"));
         setHighlighters(
@@ -58,12 +74,7 @@ public class ListViewTable extends ResultsTable<VisualSearchResult> {
                         return vsr != null && vsr.isChildrenVisible();
                     }
                 }, similarResultParentBackgroundColor, null, getTableColors().selectionColor, null),
-                new ColorHighlighter(new HighlightPredicate() {
-                    public boolean isHighlighted(Component renderer, ComponentAdapter adapter) {
-                        Object value = getValueAt(adapter.row, 0);
-                        return (value instanceof VisualStoreResult);
-                    }
-                }, storeStyle.getBackground(), null, getTableColors().selectionColor, null));
+                storeHighlighter);
     }
     
     @Override
@@ -89,6 +100,52 @@ public class ListViewTable extends ResultsTable<VisualSearchResult> {
     
     private void setIgnoreRepaints(boolean ignore) {
         this.ignoreRepaints = ignore;
+    }
+    
+    /**
+     * Sets the cell editor for the list.
+     */
+    public void setListEditor(ListViewTableEditorRenderer editor) {
+        this.listEditor = editor;
+        
+        TableColumnModel tcm = getColumnModel();
+        int columnCount = getColumnCount();
+        for (int i = 0; i < columnCount; i++) {
+            TableColumn tc = tcm.getColumn(i);
+            tc.setCellEditor(editor);
+        }
+        
+        setDefaultEditor(VisualSearchResult.class, editor);
+    }
+    
+    /**
+     * Sets the cell renderer for the list.
+     */
+    public void setListRenderer(ListViewTableEditorRenderer renderer) {
+        this.listRenderer = renderer;
+        
+        TableColumnModel tcm = getColumnModel();
+        int columnCount = getColumnCount();
+        for (int i = 0; i < columnCount; i++) {
+            TableColumn tc = tcm.getColumn(i);
+            tc.setCellRenderer(renderer);
+        }
+    }
+    
+    /**
+     * Sets the store style for the list.
+     */
+    public void setStoreStyle(StoreStyle storeStyle) {
+        // Update background color.
+        storeHighlighter.setBackground(storeStyle.getBackground());
+        
+        // Set style in editor and renderer.
+        if (listEditor != null) {
+            listEditor.setStoreStyle(storeStyle);
+        }
+        if (listRenderer != null) {
+            listRenderer.setStoreStyle(storeStyle);
+        }
     }
     
     /**
