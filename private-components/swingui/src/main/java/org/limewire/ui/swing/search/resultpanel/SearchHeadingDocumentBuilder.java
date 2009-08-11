eff9894daeec0cedba97db5aca5b5f84ed7fa9b5
eyu <eyu>
2009-08-11 22:39:13 +0000

    overloaded getHeadingDocument to accept underline parameter

--- private-components/swingui/src/main/java/org/limewire/ui/swing/search/resultpanel/SearchHeadingDocumentBuilder.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/resultpanel/SearchHeadingDocumentBuilder.java
@@ -2,8 +2,22 @@ package org.limewire.ui.swing.search.resultpanel;
 
 import org.limewire.ui.swing.search.model.BasicDownloadState;
 
+/**
+ * Defines a factory to build the heading text for search results.
+ */
 public interface SearchHeadingDocumentBuilder {
 
-    String getHeadingDocument(SearchHeading heading, BasicDownloadState downloadState, boolean isSpam);
+    /**
+     * Returns the heading text for the specified heading object, download 
+     * state, and spam indicator.
+     */
+    String getHeadingDocument(SearchHeading heading, BasicDownloadState downloadState, 
+            boolean isSpam);
 
+    /**
+     * Returns the heading text for the specified heading object, download 
+     * state, spam indicator, and underline indicator.
+     */
+    String getHeadingDocument(SearchHeading heading, BasicDownloadState downloadState, 
+            boolean isSpam, boolean underline);
 }
