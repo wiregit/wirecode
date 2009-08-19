3c0d6b9c6c17875e9e73b6b1681ffa0d33a189a1
eyu <eyu>
2009-08-19 21:03:27 +0000

    revised store controller API

--- private-components/swingui/src/main/java/org/limewire/ui/swing/search/resultpanel/StoreController.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/resultpanel/StoreController.java
@@ -1,42 +1,84 @@
 package org.limewire.ui.swing.search.resultpanel;
 
+import org.limewire.core.api.FilePropertyKey;
+import org.limewire.core.api.search.store.StoreManager;
 import org.limewire.core.api.search.store.StoreStyle;
 import org.limewire.core.api.search.store.StoreTrackResult;
+import org.limewire.ui.swing.search.model.SearchResultsModel;
 import org.limewire.ui.swing.search.model.VisualStoreResult;
+import org.limewire.ui.swing.search.resultpanel.list.DefaultListStoreStyle;
+
+import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
 
 /**
- * Defines an interface for mediating interactions between the store UI
- * and various core services.
+ * A class that mediates interactions between the Store UI and various core
+ * services.
  */
-public interface StoreController {
+public class StoreController {
 
+    private final SearchResultsModel searchResultsModel;
+    private final StoreManager storeManager;
+    
+    /**
+     * Constructs a StoreController using the specified services.
+     */
+    @Inject
+    public StoreController(
+            @Assisted SearchResultsModel searchResultsModel,
+            StoreManager storeManager) {
+        this.searchResultsModel = searchResultsModel;
+        this.storeManager = storeManager;
+    }
+    
     /**
      * Returns true if store results are pay-as-you-go.
      */
-    boolean isPayAsYouGo();
-    
+    public boolean isPayAsYouGo() {
+        // TODO also return true if user owns pay-as-you-go account
+        return !storeManager.isLoggedIn();
+    }
+
     /**
      * Returns the current style for Lime Store results.
      */
-    StoreStyle getStoreStyle();
+    public StoreStyle getStoreStyle() {
+        StoreStyle storeStyle = searchResultsModel.getStoreStyle();
+        if (storeStyle == null) {
+            storeStyle = new DefaultListStoreStyle();
+        }
+        return storeStyle;
+    }
     
     /**
      * Initiates downloading of the specified visual store result.
      */
-    void download(VisualStoreResult vsr);
-    
+    public void download(VisualStoreResult vsr) {
+        // TODO implement
+        System.out.println("StoreController.download: " + vsr.getHeading());
+    }
+
     /**
      * Initiates downloading of the specified store track result.
      */
-    void downloadTrack(StoreTrackResult str);
-    
+    public void downloadTrack(StoreTrackResult str) {
+        // TODO implement
+        System.out.println("StoreController.downloadTrack: " + str.getProperty(FilePropertyKey.NAME));
+    }
+
     /**
      * Initiates streaming of the specified visual store result.
      */
-    void stream(VisualStoreResult vsr);
-    
+    public void stream(VisualStoreResult vsr) {
+        // TODO implement
+        System.out.println("StoreController.stream: " + vsr.getHeading());
+    }
+
     /**
      * Initiates streaming of the specified store track result.
      */
-    void streamTrack(StoreTrackResult str);
+    public void streamTrack(StoreTrackResult str) {
+        // TODO implement
+        System.out.println("StoreController.streamTrack: " + str.getProperty(FilePropertyKey.NAME));
+    }
 }
---
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/search/resultpanel/StoreControllerFactory.java
@@ -0,0 +1,15 @@
+package org.limewire.ui.swing.search.resultpanel;
+
+import org.limewire.ui.swing.search.model.SearchResultsModel;
+
+/**
+ * Defines a factory to create instances of StoreController.
+ */
+public interface StoreControllerFactory {
+
+    /**
+     * Creates a new StoreController object using the specified search results
+     * model.
+     */
+    StoreController create(SearchResultsModel searchResultsModel);
+}
