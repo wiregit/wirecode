c71ff61da67f2d5610b3798eada8762e4f2649ba
pvertenten <pvertenten>
2009-08-27 21:12:13 +0000

    Updating the way sensitive directories are found.
    Trying to prevent adding any users Documents folder, not just the current user.

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/library/LibraryUtils.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/library/LibraryUtils.java
@@ -3,7 +3,11 @@ package com.limegroup.gnutella.library;
 import static com.limegroup.gnutella.Constants.MAX_FILE_SIZE;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
 import org.limewire.core.settings.LibrarySettings;
@@ -24,6 +28,10 @@ public class LibraryUtils {
 
     /** Subdirectory used to share special application files. */
     public static final File APPLICATION_SPECIAL_SHARE;
+
+    private static final List<File> userDirectories;
+    
+    private static final List<String> sensitiveDirectories;
 
     static {
         File forceShare = new File(".", ".NetworkShare").getAbsoluteFile();
@@ -46,6 +54,114 @@ public class LibraryUtils {
             forceShare = FileUtils.getCanonicalFile(forceShare);
         } catch(IOException ignored) {}
         APPLICATION_SPECIAL_SHARE = forceShare;
+
+        userDirectories = getUserDirectories();
+        sensitiveDirectories = getSensitiveDirectories();
+    }
+    
+    /**
+     * Builds list of directories with a given name for each user, starting from thier home directory. 
+     */
+    private static List<String> getUserDirectories(String directoryName) {
+        List<String> userSubDirectories = new ArrayList<String>();
+        for(File userHome : userDirectories) {
+            File folder = new File(userHome, directoryName);
+            userSubDirectories.add(folder.getAbsolutePath());
+        }
+        return userSubDirectories;
+    }
+    
+    /**
+     * Returns list of sensitive directories. 
+     */
+    private static List<String> getSensitiveDirectories() {
+        List<String> sensitiveDirectories = new ArrayList<String>();
+        
+        if(OSUtils.isWindowsVista()) {
+            sensitiveDirectories.addAll(getUserDirectories("Documents"));
+        }
+        
+        if (OSUtils.isWindows()) {
+            sensitiveDirectories.addAll(getUserDirectories("My Documents"));
+            sensitiveDirectories.addAll(getUserDirectories("Desktop"));
+            sensitiveDirectories.add(File.separator + "Documents and Settings");
+            sensitiveDirectories.add(File.separator + "Program Files");
+            sensitiveDirectories.add(File.separator + "Windows");
+            sensitiveDirectories.add(File.separator + "WINDOWS");
+            sensitiveDirectories.add(File.separator + "WINNT");
+            sensitiveDirectories.add(File.separator + "Users");
+            sensitiveDirectories.add(File.separator + "Local Settings");
+            sensitiveDirectories.add(File.separator + "Application Data");
+            sensitiveDirectories.add(File.separator + "Temp");
+            sensitiveDirectories.add(File.separator + "Temporary Internet Files");
+        }
+
+        if (OSUtils.isMacOSX()) {
+            sensitiveDirectories.addAll(getUserDirectories("Documents"));
+            sensitiveDirectories.add(File.separator + "Users");
+            sensitiveDirectories.add(File.separator + "System");
+            sensitiveDirectories.add(File.separator + "System Folder");
+            sensitiveDirectories.add(File.separator + "Previous Systems");
+            sensitiveDirectories.add(File.separator + "private");
+            sensitiveDirectories.add(File.separator + "Volumes");
+            sensitiveDirectories.add(File.separator + "Desktop");
+            sensitiveDirectories.add(File.separator + "Applications");
+            sensitiveDirectories.add(File.separator + "Applications (Mac OS 9)");
+            sensitiveDirectories.add(File.separator + "Network");
+        }
+
+        if (OSUtils.isPOSIX()) {
+            sensitiveDirectories.addAll(getUserDirectories("Documents"));
+            sensitiveDirectories.add(File.separator + "bin");
+            sensitiveDirectories.add(File.separator + "boot");
+            sensitiveDirectories.add(File.separator + "dev");
+            sensitiveDirectories.add(File.separator + "etc");
+            sensitiveDirectories.add(File.separator + "home");
+            sensitiveDirectories.add(File.separator + "mnt");
+            sensitiveDirectories.add(File.separator + "opt");
+            sensitiveDirectories.add(File.separator + "proc");
+            sensitiveDirectories.add(File.separator + "root");
+            sensitiveDirectories.add(File.separator + "sbin");
+            sensitiveDirectories.add(File.separator + "usr");
+            sensitiveDirectories.add(File.separator + "var");
+        }
+        return sensitiveDirectories;
+    }
+
+    /**
+     * Returns list of possible user home directories. 
+     */
+    private static List<File> getUserDirectories() {
+        File userHome = new File(System.getProperty("user.home"));
+        List<File> userDirectories = new ArrayList<File>();
+        userDirectories.add(userHome.getAbsoluteFile());
+        
+        File commonHome = null;
+        if(OSUtils.isWindowsVista() || OSUtils.isWindows7()) {
+            commonHome = new File("C:/Users");
+        } else if(OSUtils.isWindows()) {
+            commonHome = new File("C:/Documents and Settings");
+        } else if( OSUtils.isMacOSX()) {
+            commonHome = new File("/Users");
+        } else if (OSUtils.isLinux()) {
+            commonHome = new File("/home");
+        }       
+        
+        //not all users will have their home directory under the commonHome, some might have custom locations.
+        
+        if(commonHome != null) {
+            File[] directories =  commonHome.listFiles(new FileFilter() {
+                @Override
+                public boolean accept(File file) {
+                    return file.isDirectory();
+                }
+            });
+            if(directories != null) {
+            	userDirectories.addAll(Arrays.asList(directories));
+            }
+        }
+        
+        return userDirectories;
     }
 
     /** 
@@ -104,43 +220,18 @@ public class LibraryUtils {
     /**
      * Returns true iff <tt>file</tt> is a sensitive directory.
      */
-    public static boolean isSensitiveDirectory(File folder) {
-        if (folder == null)
+        public static boolean isSensitiveDirectory(File folder) {
+        if (folder == null) {
             return false;
-
-        String userHome = System.getProperty("user.home");
-        if (folder.equals(new File(userHome)))
-            return true;
-        
-        String userHomeShortDir = userHome.substring(userHome.lastIndexOf(File.separator)+1);
-
-        String[] sensitive;
-        if (OSUtils.isWindowsVista()) {
-            //Windows Vista does not have the "My" prefix for the "My Documents" folder.  It is simply "Documents"
-            // TODO: Are Local Settings & everything after that right for Vista?
-            sensitive = new String[] { "Documents and Settings",
-                    userHomeShortDir + File.separator + "Documents", "Desktop", "Program Files",
-                    "Windows", "WINNT", "Users", "Local Settings", "Application Data", "Temp",
-                    "Temporary Internet Files" };
-        } else if (OSUtils.isWindows()) {
-            sensitive = new String[] { "Documents and Settings", "My Documents", "Desktop",
-                    "Program Files", "Windows", "WINNT", "Users", "Local Settings",
-                    "Application Data", "Temp", "Temporary Internet Files" };
-        } else if (OSUtils.isMacOSX()) {
-            sensitive = new String[] { "Users",
-                    userHomeShortDir + File.separator + "Documents", "System", "System Folder", "Previous Systems",
-                    "private", "Volumes", "Desktop", "Applications", "Applications (Mac OS 9)",
-                    "Network" };
-        } else if (OSUtils.isPOSIX()) {
-            sensitive = new String[] { "bin", userHomeShortDir + File.separator + "Documents", "boot", "dev", "etc", "home", "mnt", "opt", "proc",
-                    "root", "sbin", "usr", "var" };
-        } else {
-            sensitive = new String[0];
         }
 
+        if(userDirectories.contains(folder)) {
+            return true;
+        }
+        
         String folderPath = folder.getPath();
-        for (String name : sensitive) {
-            if (folderPath.endsWith(File.separator + name)){
+        for (String name : sensitiveDirectories) {
+            if (folderPath.endsWith(name)) {
                 return true;
             }
         }
