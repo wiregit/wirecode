9ed10f89c3cf2756e4c3f8498b1053e6fa79bd36
tjulien <tjulien>
2009-09-03 16:40:38 +0000

    not allowing inspection reporting intervals that are too short

--- components/core-glue/src/test/java/org/limewire/core/impl/inspections/InspectionsSpecTest.java
+++ components/core-glue/src/test/java/org/limewire/core/impl/inspections/InspectionsSpecTest.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+import org.limewire.core.settings.InspectionsSettings;
 import org.limewire.gnutella.tests.LimeTestCase;
+import org.limewire.io.InvalidDataException;
 
 /**
  * Test {@link InspectionsSpec} encoding/decoding, wire format to/from object.
@@ -44,6 +46,7 @@ public class InspectionsSpecTest extends LimeTestCase {
     }
 
     public void testWireFormatToInspectionsSpecSuccess() throws Exception {
+        InspectionsSettings.INSPECTION_SPEC_MINIMUM_INTERVAL.set(0);
         InspectionsSpec spec = new InspectionsSpec(Arrays.asList("one", "two", "three"), 5, 3);
         InspectionsSpec spec2 = new InspectionsSpec(Arrays.asList("four", "five", "six"), 6, 4);
         byte[] encoded = InspectionsTestUtils.getGzippedAndBencoded(Arrays.asList(spec, spec2));
@@ -57,5 +60,18 @@ public class InspectionsSpecTest extends LimeTestCase {
         assertEquals(spec2.getInterval(), parsedSpecs.get(1).getInterval());
         assertEquals(spec.getInspectionPoints(), parsedSpecs.get(0).getInspectionPoints());
         assertEquals(spec2.getInspectionPoints(), parsedSpecs.get(1).getInspectionPoints());
+    }
+    
+    public void testWireFormatToInspectionsSpecInvalidInterval() throws Exception {
+        InspectionsSpec spec = new InspectionsSpec(Arrays.asList("one", "two", "three"), 5, 3);
+        byte[] encoded = InspectionsTestUtils.getGzippedAndBencoded(Arrays.asList(spec));
+        
+        InspectionsParser parser = new InspectionsParser();
+        try {
+            parser.parseInspectionSpecs(encoded);
+            fail();
+        } catch (InvalidDataException e) {
+            // expected result
+        }
     }
 }
