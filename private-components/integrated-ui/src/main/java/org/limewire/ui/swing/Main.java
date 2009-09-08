9dbffe5ca0db126f5402826d85409ff48c5ff3da
sberlin <sberlin>
2009-09-08 22:13:11 +0000

    minor updates for LWC-4137

--- private-components/integrated-ui/src/main/java/org/limewire/ui/swing/Main.java
+++ private-components/integrated-ui/src/main/java/org/limewire/ui/swing/Main.java
@@ -28,22 +28,37 @@ public class Main {
         
             SplashScreen splashScreen = SplashScreen.getSplashScreen();
             Frame splash = null;
-            Image splashImage = getSplashImage();
+            Image splashImage = null; // begin with null, assuming we can get it from SplashScreen
 
             // show initial splash screen only if there are no arguments
             if (args == null || args.length == 0) {
-                Rectangle bounds = null;
+                Rectangle bounds;
                 
+                // If the splash screen existed & was visible,
+                // then we want to take the image of the splash screen
+                // and the bounds it was displayed at, so that
+                // when we convert to an AWT image, the same image
+                // is shown at the same place.
                 if(splashScreen != null && splashScreen.isVisible()) {
                     bounds = splashScreen.getBounds();
                     splashImage = Toolkit.getDefaultToolkit().createImage(splashScreen.getImageURL());    
-                } 
+                } else {
+                    // If no splash was already showing, then there's no pre-existing bounds
+                    // and we need to find our own image.
+                    bounds = null;
+                    splashImage = getSplashImage();
+                }
                 
+                // If we've discovered an image to show a splash, then show the
+                // AWT splash.  Note that if there was a pre-existing splash showing
+                // via SplashScreen, the showing of an AWT frame will automatically
+                // close & hide that splash.
                 if(splashImage != null) {
                     splash = AWTSplashWindow.splash(splashImage, bounds);
                 } else if (splashScreen != null && splashScreen.isVisible()) {
-                    splashScreen.close();
-                    
+                    // If we couldn't find an image to show via AWT, then just close
+                    // the existing splash and be done with it.
+                    splashScreen.close();                    
                 }
             } else {
                 // Hide the built-in splash if it was shown, since we don't want a splash
@@ -51,6 +66,13 @@ public class Main {
                 if(splashScreen != null && splashScreen.isVisible()) {
                     splashScreen.close();
                 }                
+            }
+            
+            // If we couldn't find a splashImage by this point, just try to get it.
+            // This is because the GuiLoader/Initializer code requires an Image,
+            // even if there's no AWT splash frame for displaying it.
+            if(splashImage == null) {
+                splashImage = getSplashImage();
             }
             
             // load the GUI through reflection so that we don't reference classes here,
