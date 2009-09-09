e04df45daa918ce79bc6e5c92615869459eb88a3
tjulien <tjulien>
2009-09-09 15:23:47 +0000

    monitor startup time

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/connection/ConnectionReporter.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/connection/ConnectionReporter.java
@@ -4,7 +4,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.http.client.methods.HttpGet;
-import org.limewire.http.httpclient.HttpClientInstanceUtils;
+import org.limewire.core.api.Application;
 import org.limewire.inject.EagerSingleton;
 import org.limewire.inspection.InspectablePrimitive;
 import org.limewire.util.Clock;
@@ -21,7 +21,7 @@ import com.limegroup.gnutella.http.HttpExecutor;
 @EagerSingleton
 public class ConnectionReporter implements ConnectionLifecycleListener {
     
-    private final HttpClientInstanceUtils httpClientInstanceUtils;
+    private final Application application;
     private final HttpExecutor httpExecutor;
     private final Clock clock;
 
@@ -36,10 +36,10 @@ public class ConnectionReporter implements ConnectionLifecycleListener {
     private long loadTime;
 
     @Inject
-    public ConnectionReporter(HttpClientInstanceUtils httpClientInstanceUtils,
+    public ConnectionReporter(Application application,
                               HttpExecutor httpExecutor,
                               Clock clock) {
-        this.httpClientInstanceUtils = httpClientInstanceUtils;
+        this.application = application;
         this.httpExecutor = httpExecutor;
         this.clock = clock;
     }
@@ -60,7 +60,7 @@ public class ConnectionReporter implements ConnectionLifecycleListener {
                 // TODO use CONNECTED event instead?
                 if(!connected.getAndSet(true)) {
                     connectionTime = clock.now() - startedConnecting.get();
-                    HttpGet request = new HttpGet(httpClientInstanceUtils.addClientInfoToUrl(REPORTING_URL) +
+                    HttpGet request = new HttpGet(application.addClientInfoToUrl(REPORTING_URL) +
                             "&connect_time=" + connectionTime +  
                             "&load_time=" + loadTime);  
                     httpExecutor.execute(request);
