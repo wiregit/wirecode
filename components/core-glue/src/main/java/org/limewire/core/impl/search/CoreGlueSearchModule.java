20ca0f5a5bc3a68b6419f3dcd10c54610eb77d9a
eyu <eyu>
2009-09-15 17:55:29 +0000

    added binding for StoreConnectionFactory

--- components/core-glue/src/main/java/org/limewire/core/impl/search/CoreGlueSearchModule.java
+++ components/core-glue/src/main/java/org/limewire/core/impl/search/CoreGlueSearchModule.java
@@ -2,7 +2,9 @@ package org.limewire.core.impl.search;
 
 import org.limewire.core.api.search.SearchEvent;
 import org.limewire.core.api.search.SearchFactory;
+import org.limewire.core.api.search.store.StoreConnectionFactory;
 import org.limewire.core.api.search.store.StoreManager;
+import org.limewire.core.impl.search.store.CoreStoreConnection;
 import org.limewire.core.impl.search.store.CoreStoreManager;
 import org.limewire.listener.EventBroadcaster;
 import org.limewire.listener.EventMulticaster;
@@ -13,12 +15,19 @@ import com.google.inject.AbstractModule;
 import com.google.inject.TypeLiteral;
 import com.google.inject.assistedinject.FactoryProvider;
 
+/**
+ * Guice module to configure the Search API for the live core. 
+ */
 public class CoreGlueSearchModule extends AbstractModule {
     
     @Override
     protected void configure() {
-        bind(SearchFactory.class).toProvider(FactoryProvider.newFactory(SearchFactory.class, CoreSearch.class));
+        bind(SearchFactory.class).toProvider(FactoryProvider.newFactory(
+                SearchFactory.class, CoreSearch.class));
         bind(StoreManager.class).to(CoreStoreManager.class);
+        bind(StoreConnectionFactory.class).toProvider(FactoryProvider.newFactory(
+                StoreConnectionFactory.class, CoreStoreConnection.class));
+        
         EventMulticaster<SearchEvent> searchMulticaster = new EventMulticasterImpl<SearchEvent>();
         bind(new TypeLiteral<EventBroadcaster<SearchEvent>>(){}).toInstance(searchMulticaster);
         bind(new TypeLiteral<ListenerSupport<SearchEvent>>(){}).toInstance(searchMulticaster);
