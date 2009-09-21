0d3a27fc5fb8a494ab7a164f5aa64e78b76fbae6
sberlin <sberlin>
2009-09-21 18:05:01 +0000

    fix regression from initial LWC-4245 commit

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/filters/response/ProgramsFilter.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/filters/response/ProgramsFilter.java
@@ -3,6 +3,7 @@ package com.limegroup.gnutella.filters.response;
 import org.limewire.core.api.Category;
 import org.limewire.core.api.file.CategoryManager;
 import org.limewire.core.settings.LibrarySettings;
+import org.limewire.util.FileUtils;
 
 import com.google.inject.Inject;
 import com.limegroup.gnutella.Response;
@@ -18,7 +19,8 @@ class ProgramsFilter implements ResponseFilter {
 
     @Override
     public boolean allow(QueryReply qr, Response response) {
-        if(categoryManager.getFilterForCategory(Category.PROGRAM).apply(response.getName())) {
+        String extension = FileUtils.getFileExtension(response.getName());
+        if(categoryManager.getFilterForCategory(Category.PROGRAM).apply(extension)) {
             return LibrarySettings.ALLOW_PROGRAMS.getValue();
         } else {
             return true;
