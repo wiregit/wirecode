4c94a6b79a5144efb19d9df68042b66ae613ddd5
meverett <meverett>
2009-09-21 18:01:39 +0000

    LWC-4291: fixing null pointer in downloads file info

--- private-components/swingui/src/main/java/org/limewire/ui/swing/downloads/table/DownloadActionHandler.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/downloads/table/DownloadActionHandler.java
@@ -109,7 +109,7 @@ public class DownloadActionHandler {
             if(item.getState() != DownloadState.DONE) {
                 JDialog dialog = fileInfoFactory.createFileInfoDialog(item, FileInfoType.DOWNLOADING_FILE);
                 dialog.setVisible(true);
-            } else {
+            } else if(item.getLaunchableFile() != null) {
                 // if finished downloading, try showing all the information from the localFileItem
                 LocalFileItem localItem = libraryManager.getLibraryManagedList().getFileItem(item.getLaunchableFile());
                 if(localItem != null) {
@@ -119,6 +119,9 @@ public class DownloadActionHandler {
                     JDialog dialog = fileInfoFactory.createFileInfoDialog(item, FileInfoType.DOWNLOADING_FILE);
                     dialog.setVisible(true);
                 }
+            } else { // if can't find the localFileItem, revert to the downloadItem
+                JDialog dialog = fileInfoFactory.createFileInfoDialog(item, FileInfoType.DOWNLOADING_FILE);
+                dialog.setVisible(true);
             }
         } else if (actionCommmand == REMOVE_COMMAND){
             downloadListManager.remove(item);
