0c0a84ed45b375e870c37c3d168fe89c99b9355b
gkellum <gkellum>
2009-10-26 19:32:32 +0000

    LWC-4377 "On OS-X exceptions are occuring in the dock library, which may be interferring with proper control of the OS-X dock" -- On OS-X I have been seeing sporadic error messages coming from the native library controlling the dock such as "CGContextDrawImage: invalid context 0x0". I looked into the code in this library and Apple's documentation, and Apple recommends placing the code for updating the dock in a custom view which gets called by the system after the system does a lot of work in advance including setting up the context for drawing. The code in the dock was not placed in such a view and set-up work of creating the drawing context was not always being done before the dock library started trying to update the dock, which caused the error messages I was seeing . So, I moved the existing code into a custom view, and now the Cocoa equivalent of repaint() is being called on this view so that it's "paint" method is being called only after a drawing context has been created.

--- components/common/src/main/native/osx/Dockjnilib.m
+++ components/common/src/main/native/osx/Dockjnilib.m
@@ -1,5 +1,4 @@
 #include <JavaVM/jni.h>
-#include <Carbon/Carbon.h>
 #include <Cocoa/Cocoa.h>
 
 // compile with:
@@ -61,71 +60,186 @@ JNIEXPORT void JNICALL OS_NATIVE(RestoreApplicationDockTileImage)
 	[pool release];
 }
 
-	// get application icon
-	//
-	// if overlay == true
-	//   convert jintArray to nsimage
-	//   composite image to app icon
-	//   set to new application icon
-	// else
-	//   composite application icon to blank image
-	//   set to new application icon
-	
-JNIEXPORT void JNICALL OS_NATIVE(DrawDockTileImage)
-	(JNIEnv *env, jobject clazz, jintArray pixel, jboolean overlay, jstring appdir)
+/**
+* This is a custom view for painting the icon for the application in the dock.
+* It's necessary to create a custom view in order to do this to ensure that
+* you have a valid graphics context object to paint with. An instance of
+* this object should be set to NSApp's dockTile and display should be called
+* on dockTile. Then, this View's drawRect() method will be called by the main
+* thread with a valid graphics context.
+*/
+@interface ViewForDrawingDockIcon : NSView
 {
-	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	
-	jint *icon = NULL;
-	CGDataProviderRef provider = NULL;
-	CGImageRef image = NULL;
-	CGColorSpaceRef cs = NULL;
-	NSImage *newImage = nil;
-	const char *appdircstr = NULL;
-	
-	appdircstr = (*env)->GetStringUTFChars(env, appdir, NULL);
-	icon = (*env)->GetIntArrayElements(env, pixel, 0);
-	provider = CGDataProviderCreateWithData(0, icon, ICON_WIDTH * ICON_HEIGHT * 4, 0);
-	cs = CGColorSpaceCreateDeviceRGB();
-	image = CGImageCreate(ICON_WIDTH, 
-												ICON_HEIGHT, 
-												8, 
-												32, 
-												ICON_WIDTH*4, 
-												cs, 
-												kCGBitmapByteOrder32Host | kCGImageAlphaFirst, 
-												provider, 
-												NULL, 
-												0, 
-												kCGRenderingIntentDefault);
-	
-	{
-		NSRect imageRect = NSMakeRect(0.0, 0.0, 0.0, 0.0);
-		
-		// Create a new image to receive the Quartz image data.
-		newImage = [Dock limeWireIconAtPath:[NSString stringWithUTF8String:appdircstr]];
-		imageRect.size.height = [newImage size].height;
-		imageRect.size.width = [newImage size].width;
-		
-		// Get the Quartz context and draw.
-		if (overlay) {
-			[newImage lockFocus];
-			CGContextRef imageContext = (CGContextRef)[[NSGraphicsContext currentContext] graphicsPort];
-			CGContextDrawImage(imageContext, *(CGRect*)&imageRect, image);
-			[newImage unlockFocus];
-		}
-	}
-	
-	[NSApp setApplicationIconImage:newImage];
-	
-	(*env)->ReleaseStringUTFChars(env, appdir, appdircstr);
-	CFRelease(cs);
-	CGDataProviderRelease(provider);
-	CGImageRelease(image);
-	(*env)->ReleaseIntArrayElements(env, pixel, icon, 0);
-	[pool release];
+    int* iconData;
+    int iconDataSize;
+    NSString* applicationDirectory;
+    bool overlay;
+    NSLock* iconDataLock;
+
 }
-	
+
+- (id) init: (NSString*) applicationDirectory; 
+- (void) dealloc; 
+- (int*) getIconData;
+- (int) getIconDataSize;
+- (void) lockIconData;
+- (void) unlockIconData;
+- (void) setApplicationDirectory: (NSString*) argApplicationDirectory;
+- (void) setOverlay: (bool) argOverlay;
+- (void) drawRect: (NSRect) argDirtyRectangle;
+@end
+
+@implementation ViewForDrawingDockIcon
+- (id) init: (NSString*) argApplicationDirectory {
+    applicationDirectory = argApplicationDirectory;
+    iconDataLock = [[NSLock alloc] init];
+    iconDataSize = ICON_WIDTH * ICON_HEIGHT * 4;
+    iconData = (int*) malloc(iconDataSize);
+    if ( iconData == NULL ) {
+        // Java out of memory exception should already have been thrown
+    }
+    overlay = false;
+
+    return [super init];
+}
+
+- (int) getIconDataSize {
+    return iconDataSize;
+}
+
+- (void) dealloc {
+    if (iconData != nil) {
+        free( (void*) iconData);
+        iconData = nil;
+    }
+
+    if (applicationDirectory != nil) {
+        [applicationDirectory release];
+    }
+
+    [iconDataLock release];
+    
+    [super dealloc];
+}
+
+- (const int*) getIconData {
+    return iconData;
+}
+
+- (void) lockIconData {
+    [iconDataLock lock];
+}
+
+- (void) unlockIconData {
+    [iconDataLock unlock];
+}
+
+- (void) setApplicationDirectory: (NSString*) argApplicationDirectory {
+    if (applicationDirectory != nil) {
+        [applicationDirectory release];
+    }
+
+    applicationDirectory = argApplicationDirectory;
+}
+
+- (void) setOverlay: (bool) argOverlay {
+    overlay = argOverlay;
+}
+
+- (void) drawRect: (NSRect) argDirtyRectangle {
+    
+    [self lockIconData];
+    
+    CGDataProviderRef provider = CGDataProviderCreateWithData(0, iconData, iconDataSize, 0);
+    CGColorSpaceRef cs = CGColorSpaceCreateDeviceRGB();
+    CGImageRef image = CGImageCreate(ICON_WIDTH, 
+                                     ICON_HEIGHT, 
+                                     8, 
+                                     32, 
+                                     ICON_WIDTH*4, 
+                                     cs, 
+                                     kCGBitmapByteOrder32Host | kCGImageAlphaFirst, 
+                                     provider, 
+                                     NULL, 
+                                     0, 
+                                     kCGRenderingIntentDefault);
+
+    NSImage* newImage = [Dock limeWireIconAtPath:applicationDirectory];
+    
+    NSRect imageRect = NSMakeRect(0.0, 0.0, 0.0, 0.0);
+    
+    // Create a new image to receive the Quartz image data.
+    imageRect.size.height = [newImage size].height;
+    imageRect.size.width = [newImage size].width;    
+    
+    // if overlay == true
+    //   composite image to app icon
+    //   set to new application icon
+    // else
+    //   composite application icon to blank image
+    //   set to new application icon
+    if (overlay) { 
+        // Get the Quartz context and draw.
+        [newImage lockFocus];
+        CGContextRef imageContext = (CGContextRef)[[NSGraphicsContext currentContext] graphicsPort];
+        CGContextDrawImage(imageContext, *(CGRect*)&imageRect, image);
+        [newImage unlockFocus];
+    }
+
+    [NSApp setApplicationIconImage:newImage];
+
+    [self unlockIconData];
+
+    CFRelease(cs);
+    CGDataProviderRelease(provider);
+    CGImageRelease(image);
+}
+@end
+
+ViewForDrawingDockIcon* customDockIconView = nil;
+
+JNIEXPORT void JNICALL OS_NATIVE(DrawDockTileImage)
+    (JNIEnv *env, jobject clazz, jintArray pixels, jboolean overlay, jstring appdir)
+{
+    // When the application first starts, we want to create a custom view for setting images
+    // to the dock, and we want to set it as the application's dock tile's view.
+    if ( [[NSApp dockTile] contentView] != customDockIconView ) {
+        // Convert the Java string for the application directory into an NSString object.
+        // Calling GetStringChars() preserves the string in UTF-16. So, no i18n information is lost.
+        const jchar *applicationDirectoryChars = (*env)->GetStringChars(env, appdir, NULL);
+        NSString* applicationDirectoryNSString = [NSString stringWithCharacters:(UniChar *)applicationDirectoryChars
+                                                  length:(*env)->GetStringLength(env, appdir)];
+        (*env)->ReleaseStringChars(env, appdir, applicationDirectoryChars);
+
+        customDockIconView = [[[ViewForDrawingDockIcon alloc] init:applicationDirectoryNSString] autorelease]; // Obj-C class
+        
+        [[NSApp dockTile] setContentView:customDockIconView];
+    }
+
+    // we need to free the image data when this method returns, but we still need
+    // a copy of the data afterward in our view object. so, let's copy the
+    // image data into an array that our custom view owns.
+    jint* newIconData = (*env)->GetIntArrayElements(env, pixels, 0);
+
+    [customDockIconView lockIconData];
+    int* iconData = [customDockIconView getIconData];
+    if (iconData != NULL) {
+        memcpy(iconData, newIconData, [customDockIconView getIconDataSize]);
+    }
+    [customDockIconView unlockIconData];
+
+    (*env)->ReleaseIntArrayElements(env, pixels, newIconData, 0);
+
+    // let's set whether the image is an overlay or not.
+    [customDockIconView setOverlay:overlay];
+
+    // Call display on the application's dock tile object. This will cause the 
+    // drawRect() method in our custom view to be called with a valid drawing context.
+    // This is analagous to calling repaint() in Java which will cause paint() to 
+    // be called with a valid Graphics object.
+    [[NSApp dockTile] display];
+}
+
 #ifdef __cplusplus
 }
 #endif
