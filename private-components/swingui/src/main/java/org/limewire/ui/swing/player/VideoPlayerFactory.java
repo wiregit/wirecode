4d3e9791acf2ca5444e1728f795107be6e3f248e
mturkel <mturkel>
2009-10-28 16:35:43 +0000

    Making video playback work on mac again.

--- private-components/swingui/src/main/java/org/limewire/ui/swing/player/VideoPlayerFactory.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/player/VideoPlayerFactory.java
@@ -2,17 +2,16 @@ package org.limewire.ui.swing.player;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 
 import javax.media.IncompatibleSourceException;
+import javax.media.MediaLocator;
 import javax.media.Player;
 import javax.media.protocol.DataSource;
-import javax.media.protocol.URLDataSource;
-
-import com.lti.utils.OSUtils;
 
 import net.sf.fmj.ejmf.toolkit.media.AbstractPlayer;
 import net.sf.fmj.utility.URLUtils;
+
+import com.lti.utils.OSUtils;
 
 
 public class VideoPlayerFactory {
@@ -22,7 +21,6 @@ public class VideoPlayerFactory {
         }
 
         AbstractPlayer handler;
-        DataSource source = new URLDataSource(new URL(URLUtils.createUrlStr(file)));
         if (OSUtils.isWindows()) {
             handler = new net.sf.fmj.ds.media.content.unknown.Handler();
         } else { // OSX
@@ -30,7 +28,7 @@ public class VideoPlayerFactory {
         }
 
         try {
-            handler.setSource(source);
+            handler.setSource(createDataSource(file));
         } catch (UnsatisfiedLinkError e) {
             // TODO: this is not the best way to handle unsatisfied links but
             // our native launch fallback will work
@@ -39,5 +37,12 @@ public class VideoPlayerFactory {
 
         handler.realize();
         return handler;
+    }
+    
+    private static DataSource createDataSource(File file){
+        //FMJ's handling of files is incredibly fragile.  This works with both quicktime and directshow.
+        DataSource source = new net.sf.fmj.media.protocol.file.DataSource();
+        source.setLocator(new MediaLocator(URLUtils.createUrlStr(file)));
+        return source;
     }
 }
