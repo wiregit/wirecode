6372132f85a71b149502dae7bf19a1ebc1ce4041
michaelt <michaelt>
2009-11-16 20:40:01 +0000

    LWC-4554 - Focus on some modal dialogues is lost after clicking outside the window
    - No need to null check mainFrame

--- private-components/swingui/src/main/java/org/limewire/ui/swing/mainframe/AboutAction.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/mainframe/AboutAction.java
@@ -26,9 +26,7 @@ public class AboutAction extends AbstractAction {
     public void actionPerformed(ActionEvent e) {
         // if the main frame is not visible on OS X when about is opened, then a zombie window popups up...
         JFrame mainFrame = GuiUtils.getMainFrame();
-        if (mainFrame != null) {
-            mainFrame.setVisible(true);
-        }
+        mainFrame.setVisible(true);
         new AboutWindow(mainFrame, application).showDialog();
     }
 }
