eabcb763fbb6eee18e27413cbc085153cd955567
mrogers <mrogers>
2009-12-08 11:18:05 +0000

    Scan multi-file torrent downloads with ScanDirectory.

--- components/gnutella-core/src/main/java/com/limegroup/bittorrent/BTDownloaderImpl.java
+++ components/gnutella-core/src/main/java/com/limegroup/bittorrent/BTDownloaderImpl.java
@@ -223,16 +223,17 @@ public class BTDownloaderImpl extends AbstractCoreDownloader implements BTDownlo
      * torrent, after stopping the download.
      */
     private boolean isInfectedOrDangerous() {
-        // FIXME: SCANNING state
-        // FIXME: pass in the directory as a single file
-        for(File f : getIncompleteFiles()) {
+        if(virusScanner.get().isSupported()) {
+            lastState.set(DownloadState.SCANNING);
             try {
-                if(isInfected(f))
+                if(isInfected(getIncompleteFile()))
                     return true;
             } catch(VirusScanException e) {
                 scanFailed.set(e.getDetail() == VirusScanException.Detail.DOWNLOADING_DEFINITIONS ?
                         DownloadState.SCAN_FAILED_DOWNLOADING_DEFINITIONS : DownloadState.SCAN_FAILED);
             }
+        }
+        for(File f : getIncompleteFiles()) {
             if(isDangerous(f))
                 return true;
         }
@@ -269,7 +270,7 @@ public class BTDownloaderImpl extends AbstractCoreDownloader implements BTDownlo
      * Returns true if the given file is infected, after stopping the download.
      */
     private boolean isInfected(File file) throws VirusScanException {
-        if(!virusScanner.get().isSupported() &&
+        if(virusScanner.get().isSupported() &&
                 virusScanner.get().isInfected(file)) {
             lastState.set(DownloadState.THREAT_FOUND);
             listeners.broadcast(new DownloadStateEvent(this, DownloadState.THREAT_FOUND));
