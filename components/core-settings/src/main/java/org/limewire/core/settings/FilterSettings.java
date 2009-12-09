bd7b35355ae7142d3d926a9788eac19b0c9af539
mrogers <mrogers>
2009-12-09 20:41:28 +0000

    Re-enabled the virus scanner.

--- components/core-settings/src/main/java/org/limewire/core/settings/FilterSettings.java
+++ components/core-settings/src/main/java/org/limewire/core/settings/FilterSettings.java
@@ -200,7 +200,7 @@ public class FilterSettings extends LimeProps {
      * Whether to enable the virus scanner.
      */
     public static final BooleanSetting USE_VIRUS_SCANNER =
-        FACTORY.createBooleanSetting("USE_VIRUS_SCANNER", false);
+        FACTORY.createBooleanSetting("USE_VIRUS_SCANNER", true);
     
     /**
      * Whether to enable the virus scanner for testing versions.
