d30ca72e7c3eb34d67d5cfdce6e3ff1ae7699690
meverett <meverett>
2010-01-07 22:34:05 +0000

    missing from last commit

--- components/activation/src/main/java/org/limewire/activation/impl/ActivationItemImpl.java
+++ components/activation/src/main/java/org/limewire/activation/impl/ActivationItemImpl.java
@@ -1,18 +1,40 @@
 package org.limewire.activation.impl;
 
-import java.net.URL;
-
 import org.limewire.activation.api.ActivationItem;
+import org.limewire.activation.api.ActivationID;
+import org.limewire.setting.ActivationSettings;
 
 public class ActivationItemImpl implements ActivationItem {
 
-    private String licenseName;
-    private long datePurchased;
-    private long dateExpired;
-    private boolean isExpired;
-    private String url;
-    private boolean isSubscription;
+    private final int intID;
+    private final ActivationID moduleID;
+    private final String licenseName;
+    private final long datePurchased;
+    private final long dateExpired;
+    private final boolean isExpired;
     
+    public ActivationItemImpl(int intID, String licenseName, long datePurchased, long dateExpired,
+            boolean isExpired, boolean isLoadedFromDisk) {
+        this.intID = intID;
+        this.moduleID = ActivationID.getActivationID(intID);
+        this.licenseName = licenseName;
+        this.datePurchased = datePurchased;
+        this.dateExpired = dateExpired;
+
+        // when loaded from disk, we compare the date expired with the current time of the system
+        // if the user's system clock is in a different time zone or extremely messed up this might
+        // fail but this is quite an edge case
+        if(isLoadedFromDisk) {
+             this.isExpired = System.currentTimeMillis() > dateExpired;
+        } else {
+            this.isExpired = isExpired;            
+        }
+    }
+    
+    @Override
+    public ActivationID getModuleID() {
+        return moduleID;
+    }
     
     @Override
     public long getDateExpired() {
@@ -28,25 +50,14 @@ public class ActivationItemImpl implements ActivationItem {
     }
     @Override
     public String getURL() {
-        return url;
+        return ActivationSettings.ACTIVATION_RENEWAL_HOST + Integer.toString(intID);
     }
     @Override
-    public boolean isActiveVersion() {
-        // TODO Auto-generated method stub
-        return false;
+    public boolean isUseable() {
+        return moduleID != ActivationID.UNKNOWN_MODULE;
     }
     @Override
-    public boolean isExpired() {
-        // TODO Auto-generated method stub
-        return false;
-    }
-    @Override
-    public boolean isSubscription() {
-        return isSubscription;
-    }
-    @Override
-    public String getFirstSupportedVersion() {
-        // TODO Auto-generated method stub
-        return null;
+    public boolean isActive() {
+        return !isExpired;
     }
 }
--- components/activation/src/main/java/org/limewire/activation/impl/ActivationManagerImpl.java
+++ components/activation/src/main/java/org/limewire/activation/impl/ActivationManagerImpl.java
@@ -1,39 +1,38 @@
 package org.limewire.activation.impl;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.limewire.activation.api.ActivationError;
 import org.limewire.activation.api.ActivationEvent;
+import org.limewire.activation.api.ActivationID;
 import org.limewire.activation.api.ActivationItem;
 import org.limewire.activation.api.ActivationManager;
+import org.limewire.activation.api.ActivationModuleEvent;
 import org.limewire.activation.api.ActivationState;
 import org.limewire.inject.EagerSingleton;
+import org.limewire.lifecycle.Service;
 import org.limewire.listener.EventListener;
 import org.limewire.listener.EventListenerList;
 import org.limewire.setting.ActivationSettings;
+
+import com.google.inject.Inject;
 
 //TODO: this needs to be a service, within the service start call 
 // we attempt to authenticate the key if the key exists. otherwise
 // it waits for user action.
 @EagerSingleton
-public class ActivationManagerImpl implements ActivationManager {
+public class ActivationManagerImpl implements ActivationManager, Service {
 
     private final EventListenerList<ActivationEvent> listeners = new EventListenerList<ActivationEvent>();
+    private final EventListenerList<ActivationModuleEvent> moduleListeners = new EventListenerList<ActivationModuleEvent>();
+    private final Map<ActivationID, ActivationItem> itemMap = new HashMap<ActivationID, ActivationItem>(4);
 
     private volatile ActivationState currentState = ActivationState.UNINITIALIZED;
     private volatile ActivationError activationError = ActivationError.NO_ERROR;
-    
-    @Override
-    public void addListener(EventListener<ActivationEvent> listener) {
-        listeners.addListener(listener);
-    }
 
-    @Override
-    public boolean removeListener(EventListener<ActivationEvent> listener) {
-        return listeners.removeListener(listener);
-    }
     
     @Override
     public void activateKey(final String key) {
@@ -51,6 +50,14 @@ public class ActivationManagerImpl implements ActivationManager {
                 if(key.equals("12345")) {
                     currentState = ActivationState.ACTIVATED;
                     activationError = ActivationError.NO_ERROR;
+                    
+                    // this is temporary
+                    List<ActivationItem> list = new ArrayList<ActivationItem>();
+                    list.add(new ActivationItemTest(0, "Test Active", false, true));
+                    list.add(new ActivationItemTest(1, "Test Inactive", false, false));
+                    list.add(new ActivationItemTest(2, "Test Expired", true, true));  
+                    setActivationItems(list);
+                    
                     listeners.broadcast(new ActivationEvent(ActivationState.ACTIVATED));
                     ActivationSettings.ACTIVATION_KEY.set(key);
                 } else if (key.equals("54321")) {
@@ -80,15 +87,40 @@ public class ActivationManagerImpl implements ActivationManager {
 
     @Override
     public List<ActivationItem> getActivationItems() {
-        if(currentState == ActivationState.ACTIVATED) {
-            List<ActivationItem> list = new ArrayList<ActivationItem>();
-            list.add(new ActivationItemTest("Test Active", false, true));
-            list.add(new ActivationItemTest("Test Inactive", false, false));
-            list.add(new ActivationItemTest("Test Expired", true, true));   
-            return list;
-        } else {
-            return Collections.emptyList();
+        synchronized (this) {
+            return new ArrayList<ActivationItem>(itemMap.values());            
         }
+//        if(currentState == ActivationState.ACTIVATED) {
+//            List<ActivationItem> list = new ArrayList<ActivationItem>();
+//            list.add(new ActivationItemTest("Test Active", false, true));
+//            list.add(new ActivationItemTest("Test Inactive", false, false));
+//            list.add(new ActivationItemTest("Test Expired", true, true));   
+//            return list;
+//        } else {
+//            return Collections.emptyList();
+//        }
+    }
+    
+    public void setActivationItems(List<ActivationItem> items) {
+        synchronized (this) {
+            itemMap.clear();
+            for(ActivationItem item : items) {
+                itemMap.put(item.getModuleID(), item);
+            }
+        }
+        save();
+        for(ActivationItem item : items) {
+            if(item.getModuleID() != ActivationID.UNKNOWN_MODULE)
+                moduleListeners.broadcast(new ActivationModuleEvent(item.getModuleID(), item.isActive()));
+        }
+    }
+    
+    public void load() {
+        
+    }
+    
+    public void save() {
+        
     }
 
     @Override
@@ -98,23 +130,76 @@ public class ActivationManagerImpl implements ActivationManager {
 
     @Override
     public boolean isValidKey(String key) {
-        // TODO Auto-generated method stub
-        return false;
+        return true;
     }
 
     @Override
-    public boolean isPro() {
-        return false;
+    public boolean isActive(ActivationID id) {
+        ActivationItem item;
+        synchronized (itemMap) {
+            item = itemMap.get(id);            
+        }
+        if(item == null)
+            return false;
+        else
+            return item.isActive();
+    }
+    
+
+    @Override
+    public String getServiceName() {
+        return "Activation-Manager Service";
+    }
+    
+    @Inject
+    void register(org.limewire.lifecycle.ServiceRegistry registry) {
+        registry.register(this);
+    }
+
+    @Override
+    public void initialize() {
+        load();
+    }
+
+    @Override
+    public void start() {
+        //TODO: if a PKey exists, start the server and try contacting the authentication server
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    @Override
+    public void addModuleListener(EventListener<ActivationModuleEvent> listener) {
+        moduleListeners.addListener(listener);
+    }
+    
+    @Override
+    public boolean removeModuleListener(EventListener<ActivationModuleEvent> listener) {
+        return moduleListeners.removeListener(listener);
+    }
+        
+    @Override
+    public void addListener(EventListener<ActivationEvent> listener) {
+        listeners.addListener(listener);
+    }
+
+    @Override
+    public boolean removeListener(EventListener<ActivationEvent> listener) {
+        return listeners.removeListener(listener);
     }
     
     //NOTE: for testing only
     private class ActivationItemTest implements ActivationItem {
 
+        private ActivationID moduelID;
         private String name;
         private boolean isExpired;
         private boolean isActivate;
         
-        public ActivationItemTest(String name, boolean isExpired, boolean isActive) {
+        public ActivationItemTest(int id, String name, boolean isExpired, boolean isActive) {
+            this.moduelID = ActivationID.getActivationID(id);
             this.name = name;
             this.isExpired = isExpired;
             this.isActivate = isActive;
@@ -142,24 +227,29 @@ public class ActivationManagerImpl implements ActivationManager {
         }
 
         @Override
-        public boolean isActiveVersion() {
+        public boolean isUseable() {
             return isActivate;
         }
 
         @Override
-        public boolean isExpired() {
-            return isExpired;
+        public boolean isActive() {
+            return !isExpired;
         }
 
-        @Override
-        public boolean isSubscription() {
-            // TODO Auto-generated method stub
-            return false;
-        }
+//        @Override
+//        public boolean isSubscription() {
+//            // TODO Auto-generated method stub
+//            return false;
+//        }
+//
+//        @Override
+//        public String getFirstSupportedVersion() {
+//            return "5.5";
+//        }
 
         @Override
-        public String getFirstSupportedVersion() {
-            return "5.5";
+        public ActivationID getModuleID() {
+            return moduelID;
         }
         
     }
