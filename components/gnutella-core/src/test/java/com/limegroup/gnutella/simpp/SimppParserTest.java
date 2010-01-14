1af36758b181356805e18250f718f44aade1922b
fberger <fberger>
2010-01-14 19:39:20 +0000

    LWC-4950: verify current simpp code accepts new fields in simpp.xml: done.

--- components/gnutella-core/src/test/java/com/limegroup/gnutella/simpp/SimppParserTest.java
+++ components/gnutella-core/src/test/java/com/limegroup/gnutella/simpp/SimppParserTest.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 import junit.framework.Test;
 
 import org.limewire.util.BaseTestCase;
+import org.limewire.util.StringUtils;
 
 public class SimppParserTest extends BaseTestCase {
 
@@ -32,4 +33,15 @@ public class SimppParserTest extends BaseTestCase {
         }   
     }
 
+    /**
+     * Test to ensure that the simpp parser before the introduction of the new
+     * simpp key scheme gracefully ignores new and unknown elements in the simpp
+     * message. 
+     */
+    public void testOldParserIgnoresNewXMLElements() throws Exception {
+        String simppWithExtraElements = "<simpp><version>333</version><newVersion>1</newVersion><cert>AKSDFklsddd|15|lSDFKK34555</cert><props>propsdata</props></simpp>";
+        SimppParser parser = new SimppParser(StringUtils.toUTF8Bytes(simppWithExtraElements));
+        assertEquals(333, parser.getVersion());
+        assertEquals("propsdata", parser.getPropsData());
+    }
 }
