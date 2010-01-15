0c4f780d71350c1b7ccd9442eb7f0ac27b549086
meverett <meverett>
2010-01-15 20:50:47 +0000

    fixed search result module

--- components/gnutella-core/src/main/java/com/limegroup/gnutella/search/QuerySettings.java
+++ components/gnutella-core/src/main/java/com/limegroup/gnutella/search/QuerySettings.java
@@ -20,6 +20,6 @@ public class QuerySettings {
     public int getUltrapeerResults() {
         //TODO: this needs to get changed in installer
 //        return 150;
-        return activationManager.isActive(ActivationID.TURBO_CHARGED_DOWNLOADS_MODULE) ? 250 : 150;
+        return activationManager.isActive(ActivationID.OPTIMIZED_SEARCH_RESULT_MODULE) ? 250 : 150;
     }
 }
