b3ef67625a938182be98a484f78197d7bdaad21d
meverett <meverett>
2010-02-05 00:51:00 +0000

    fixing dependecies

--- components/core-glue/src/main/java/org/limewire/core/impl/ActivationTestImpl.java
+++ components/core-glue/src/main/java/org/limewire/core/impl/ActivationTestImpl.java
@@ -1,40 +1,36 @@
 package org.limewire.core.impl;
 
 import org.limewire.core.api.ActivationTest;
-import org.limewire.util.PrivateAccessor;
 
 import com.google.inject.Inject;
-import com.limegroup.gnutella.ConnectionManager;
-import com.limegroup.gnutella.ConnectionManagerImpl;
-import com.limegroup.gnutella.downloader.ManagedDownloader;
-import com.limegroup.gnutella.search.QuerySettings;
 
 public class ActivationTestImpl implements ActivationTest {
 
-    private final QuerySettings querySetting;
+//    private final QuerySettings querySetting;
 //    private final ManagedDownloader managedDownloader;
-    private final ConnectionManager connectionManager;
+//    private final ConnectionManager connectionManager;
     
     @Inject
-    public ActivationTestImpl(QuerySettings querySetting, ManagedDownloader managedDownloader,
-            ConnectionManager connectionManager) {
-        this.querySetting = querySetting;
+    public ActivationTestImpl() {
+            //QuerySettings querySetting, ManagedDownloader managedDownloader,
+//            ConnectionManager connectionManager) {
+//        this.querySetting = querySetting;
 //        this.managedDownloader = managedDownloader;
-        this.connectionManager = connectionManager;
+//        this.connectionManager = connectionManager;
     }
 
     @Override
     public int getNumUltraPeers() {
-        if(connectionManager instanceof ConnectionManagerImpl) {
-            ConnectionManagerImpl impl = (ConnectionManagerImpl) connectionManager;
-            try {
-                PrivateAccessor accessor = new PrivateAccessor(ConnectionManagerImpl.class, impl, "_preferredConnections");
-                int value = (Integer) accessor.getValue();
-                return value;
-            } catch (Exception e) {
-                return 0;
-            } 
-        }
+//        if(connectionManager instanceof ConnectionManagerImpl) {
+//            ConnectionManagerImpl impl = (ConnectionManagerImpl) connectionManager;
+//            try {
+//                PrivateAccessor accessor = new PrivateAccessor(ConnectionManagerImpl.class, impl, "_preferredConnections");
+//                int value = (Integer) accessor.getValue();
+//                return value;
+//            } catch (Exception e) {
+//                return 0;
+//            } 
+//        }
         return -1;
     }
     
@@ -48,7 +44,8 @@ public class ActivationTestImpl implements ActivationTest {
     
     @Override
     public int getNumResults() {
-        return querySetting.getUltrapeerResults();
+        return 0;
+//        return querySetting.getUltrapeerResults();
     }
     
     
