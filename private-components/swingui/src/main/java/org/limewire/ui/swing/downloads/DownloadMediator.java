b142b1e6f1e89441d558a239aea471178e491264
mturkel <mturkel>
2010-02-08 16:29:17 +0000

    LWC-5224 java.lang.IllegalArgumentException: Unknown DownloadState: APPLYING_DEFINITION_UPDATE

--- private-components/swingui/src/main/java/org/limewire/ui/swing/downloads/DownloadMediator.java
+++ private-components/swingui/src/main/java/org/limewire/ui/swing/downloads/DownloadMediator.java
@@ -212,7 +212,7 @@ public class DownloadMediator {
             comparator = new SpeedComparator();
             break;
         case STATUS:
-            comparator = new StateComparator();
+            comparator = new DownloadStateComparator();
             break;
         case FILE_TYPE:
             comparator = new FileTypeComparator();
@@ -436,44 +436,7 @@ public class DownloadMediator {
         public void actionPerformed(ActionEvent e) {
             fixStalled();
         }
-    }
-    
-    private static class StateComparator implements Comparator<DownloadItem>{
-        
-        @Override
-        public int compare(DownloadItem o1, DownloadItem o2) {
-            if (o1 == o2){
-                return 0;
-            }
-
-            return getSortPriority(o1.getState()) - getSortPriority(o2.getState());
-        }  
-                
-        private int getSortPriority(DownloadState state){
-            
-            switch(state){
-            case DONE: return 1;
-            case FINISHING: return 2;
-            case DOWNLOADING: return 3;
-            case RESUMING: return 4;
-            case CONNECTING: return 5;
-            case PAUSED: return 6;
-            case REMOTE_QUEUED: return 7;
-            case LOCAL_QUEUED: return 8;
-            case TRYING_AGAIN: return 9;
-            case STALLED: return 10;
-            case ERROR: return 11;       
-            case CANCELLED: return 12;
-            case DANGEROUS: return 13;
-            case SCANNING: return 14;
-            case SCANNING_FRAGMENT: return 15;
-            case THREAT_FOUND: return 16;
-            case SCAN_FAILED: return 17;
-            }
-            
-           throw new IllegalArgumentException("Unknown DownloadState: " + state);
-        }
-    }
+    }  
     
     private static class OrderAddedComparator implements Comparator<DownloadItem>{
         
