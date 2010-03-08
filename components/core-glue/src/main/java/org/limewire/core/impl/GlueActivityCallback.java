c7f3444e91386c17432c26920e78672c2c8e9e34
mrogers <mrogers>
2010-03-08 23:02:29 +0000

    Slightly less shoddy warning message.

--- components/core-glue/src/main/java/org/limewire/core/impl/GlueActivityCallback.java
+++ components/core-glue/src/main/java/org/limewire/core/impl/GlueActivityCallback.java
@@ -223,7 +223,7 @@ class GlueActivityCallback implements ActivityCallback, QueryReplyListenerList,
             dloader.discardUnscannedPreview(true);
         } else {
             // FIXME: replace this with a proper warning message
-            String msg = I18nMarker.marktr("Oh noes! Preview anyway?");
+            String msg = I18nMarker.marktr("The file could not be scanned for viruses. Preview anyway?");
             boolean previewAnyway = guiCallback.promptUserQuestion(msg);
             dloader.discardUnscannedPreview(!previewAnyway);
         }
