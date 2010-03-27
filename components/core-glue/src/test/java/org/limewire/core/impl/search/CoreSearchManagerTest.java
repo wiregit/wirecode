454215e7b04cbdf3de70ba9a1c19252998e2f5d8
eyu <eyu>
2010-03-27 01:00:58 +0000

    fixed unit test

--- components/core-glue/src/test/java/org/limewire/core/impl/search/CoreSearchManagerTest.java
+++ components/core-glue/src/test/java/org/limewire/core/impl/search/CoreSearchManagerTest.java
@@ -2,6 +2,7 @@ package org.limewire.core.impl.search;
 
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
 import org.limewire.core.api.search.Search;
 import org.limewire.core.api.search.SearchDetails;
 import org.limewire.core.api.search.SearchResultList;
@@ -14,7 +15,9 @@ public class CoreSearchManagerTest extends BaseTestCase {
     /** Instance of class being tested. */
     CoreSearchManager searchManager;
 
-    private Mockery context = new Mockery();
+    private Mockery context = new Mockery() {{
+        setImposteriser(ClassImposteriser.INSTANCE);
+    }};
     
     /**
      * Constructs a test case for the specified method name.
