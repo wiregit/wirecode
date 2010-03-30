dd7a2634687086c50b437ae28dfcc00c8b0671cb
bwier <bwier>
2010-03-30 18:46:47 +0000

    new tests

--- components/core-glue/src/test/java/org/limewire/core/impl/integration/RestDownloadTest.java
+++ components/core-glue/src/test/java/org/limewire/core/impl/integration/RestDownloadTest.java
@@ -3,6 +3,7 @@ package org.limewire.core.impl.integration;
 import java.io.IOException;
 import java.util.Map;
 
+import org.apache.http.HttpStatus;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.limewire.core.api.URN;
@@ -36,39 +37,101 @@ public class RestDownloadTest extends AbstractRestIntegrationTestcase {
     }
 
     @Override public void setUp() throws Exception {
-        setUpModules(new MockDownloadModule());
-        loadMockQueries();
+        
+        String nm = getName();
+        if (nm.contains("NoMock")) {
+            super.setUp(); // no mocks
+        } else {
+            setUpModules(new MockDownloadModule());
+                loadMockDownloads(); 
+        }        
     }
 
     // -------------------------- tests --------------------------
 
     public void testBasic() throws Exception {
 
-        // download items are retrievable
-        int respSz = listGET(DOWNLOAD_PREFIX, NO_PARAMS).size();
-        assertEquals("download size unexpected", downItems.size(), respSz);
+        // all downloads progress metadata
+        int size = listGET(DOWNLOAD_PREFIX, NO_PARAMS).size();
+        assertEquals("download size unexpected", downItems.size(), size);
 
-        // single items retrievable
-        for (DownloadItem item : downItems) {
-            String urn = item.getUrn().toString();
-            Map<String, String> m = metadataGET(DOWNLOAD_PREFIX+"/"+urn,NO_PARAMS);
-            assertEquals("download item unexpected",urn,m.get("id"));
+        // individual download progress metadata
+        for (DownloadItem expectedItem : downItems) {
+            assertDownloadMD(expectedItem);
         }
     }
 
+    public void testCancel() throws Exception {
+        
+        int statusCode = -1;
+        
+        // before cancel
+        int size = listGET(DOWNLOAD_PREFIX, NO_PARAMS).size();
+        assertEquals("list size unexpected",downItems.size(),size);
+                
+        /**
+         * delete all: 
+         * expectations for sequence of calls invoked by delete set in loadMockQueries
+         */
+        for (DownloadItem item : downItems) {
+            statusCode = httpDelete(DOWNLOAD_PREFIX + "/" + item.getUrn(), NO_PARAMS);
+            assertEquals("unexpected status",HttpStatus.SC_OK,statusCode);            
+        }       
+        
+        // let's ensure an invalid download item
+        context.checking(new Expectations() {{
+                one(dlistMgr).getDownloadItem(with(any(URN.class)));
+                will(returnValue(null));
+        }});
+        
+        // try to cancel item again
+        URN urn = downItems.get(0).getUrn();
+        statusCode = httpDelete(DOWNLOAD_PREFIX + "/" + urn, NO_PARAMS);        
+        assertEquals("shouldn't be found",HttpStatus.SC_NOT_FOUND,statusCode);
+    }
+
+    
+    public void testCancelNoMocks() throws Exception {
+        
+        int statusCode = -1;
+        
+        // try to cancel random URN
+        statusCode = httpDelete(DOWNLOAD_PREFIX + "/" + generateURN(), NO_PARAMS);
+        assertEquals("shouldn't be found",HttpStatus.SC_NOT_FOUND,statusCode);
+     
+        // try to cancel a target (i.e. no urn)
+        statusCode = httpDelete(DOWNLOAD_PREFIX, NO_PARAMS);
+        assertEquals("no urn passed",HttpStatus.SC_NOT_IMPLEMENTED,statusCode);  
+        
+        // try to cancel garbage URN (LWC-5565)
+        //statusCode = httpDelete(DOWNLOAD_PREFIX + "/GARBAGE_NOT_BASE16", NO_PARAMS);
+        //assertEquals("garbage urn",HttpStatus.SC_NOT_FOUND,statusCode);        
+    }
+
+    
+    public void testPost() throws Exception { 
+        
+    }
+    
     // ---------------------- private ----------------------
+
+    /**
+     * compare download metadata GET with expected DownloadItem
+     */
+    private void assertDownloadMD(DownloadItem item) throws Exception {
+        Map<String, String> results = metadataGET(DOWNLOAD_PREFIX + "/" + item.getUrn(), NO_PARAMS);
+        assertEquals("urn unexpected", item.getUrn().toString(), results.get("id"));
+        assertEquals("state unexpected", item.getState().toString(), results.get("state"));
+        assertEquals("name unexpected", item.getFileName(), results.get("filename"));
+        assertEquals("size unexpected", String.valueOf(item.getTotalSize()), results.get("size"));
+        assertEquals("csize unexpected", String.valueOf(item.getCurrentSize()), results
+                .get("bytesDownloaded"));
+    }
 
     /**
      * populates mock download results
      */
-    private void loadMockQueries() throws Exception {
-
-        downItems = new BasicEventList<DownloadItem>();
-        context.checking(new Expectations() {{
-                allowing(dlistMgr).getDownloads();
-                will(returnValue(downItems));
-            }
-        });
+    private void loadMockDownloads() throws Exception {
         addMockDownload(DownloadState.DOWNLOADING, 20);
         addMockDownload(DownloadState.DOWNLOADING, 50);
         addMockDownload(DownloadState.DONE, 100);
@@ -78,17 +141,25 @@ public class RestDownloadTest extends AbstractRestIntegrationTestcase {
 
     /**
      * adds mock download file to the download items list
-     * 
-     * @throws IOException
      */
     private void addMockDownload(final DownloadState state, final int percentComplete)
             throws IOException {
 
+        if (downItems == null) {
+            downItems = new BasicEventList<DownloadItem>();
+            context.checking(new Expectations() {
+                {
+                    allowing(dlistMgr).getDownloads();
+                    will(returnValue(downItems));
+                }
+            });
+        }
+        final URN urn = generateURN();        
         final DownloadItem mockDownloadItem = context.mock(DownloadItem.class);
-        final URN urn = makeURN();
         downItems.add(mockDownloadItem);
 
-        context.checking(new Expectations() {{
+        context.checking(new Expectations() {
+            {
                 allowing(mockDownloadItem).getFileName();
                 will(returnValue("mockDownload" + urn.hashCode()));
                 allowing(mockDownloadItem).getUrn();
@@ -101,22 +172,29 @@ public class RestDownloadTest extends AbstractRestIntegrationTestcase {
                 will(returnValue(state));
                 one(dlistMgr).getDownloadItem(with(any(URN.class)));
                 will(returnValue(mockDownloadItem));
+                
+                if (state.equals(DownloadState.DONE)) {
+                    exactly(1).of(dlistMgr).remove(mockDownloadItem);                    
+                } else {
+                    exactly(1).of(mockDownloadItem).cancel();
+                }
+                
             }
         });
     }
 
     /**
-     * create a fake URN
+     * generate mock urn
      */
-    private URN makeURN() {
-        String url = "urn:sha1:" + GUID.toHexString(GUID.makeGuid());
-        return new MockURN(url);
+    private URN generateURN() {
+        final URN urn = new MockURN("urn:sha1:" + GUID.toHexString(GUID.makeGuid()));
+        return urn;
     }
 
     /**
      * mock download list manager
      */
-    private static class MockDownloadModule extends AbstractModule {
+    protected static class MockDownloadModule extends AbstractModule {
         @Override protected void configure() {
             bind(DownloadListManager.class).toInstance(context.mock(DownloadListManager.class));
         }
