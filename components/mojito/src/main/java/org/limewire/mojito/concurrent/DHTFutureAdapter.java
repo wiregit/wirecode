5265fe982d3275e89c4b5d9bdb166c521477e2ac
rkapsi <rkapsi>
2010-04-12 17:34:38 +0000

    Just JavaDoc

--- components/mojito/src/main/java/org/limewire/mojito/concurrent/DHTFutureAdapter.java
+++ components/mojito/src/main/java/org/limewire/mojito/concurrent/DHTFutureAdapter.java
@@ -5,18 +5,22 @@ import org.limewire.listener.BlockingEvent;
 import org.limewire.listener.EventListener;
 
 /**
+ * Instances of {@link DHTFutureAdapter} are using a predefined event
+ * {@link Thread}.
  * 
+ * @see EventListener
+ * @see BlockingEvent
  */
 public abstract class DHTFutureAdapter<V> implements EventListener<FutureEvent<V>> {
 
-    @BlockingEvent(queueName="AsyncFuture")
+    @BlockingEvent(queueName="MojitoEventThread")
     @Override
     public final void handleEvent(FutureEvent<V> event) {
         operationComplete(event);
     }
     
     /**
-     * 
+     * @see EventListener#handleEvent(Object)
      */
     protected abstract void operationComplete(FutureEvent<V> event);
 }
