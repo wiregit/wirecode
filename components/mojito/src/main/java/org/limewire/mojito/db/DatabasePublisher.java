960ba671a68913758fd9c103265de4631c79b938
rkapsi <rkapsi>
2010-04-20 21:10:23 +0000

    Use the same pattern for DatabasePublisher

--- components/mojito/src/main/java/org/limewire/mojito/db/DatabasePublisher.java
+++ components/mojito/src/main/java/org/limewire/mojito/db/DatabasePublisher.java
@@ -5,14 +5,19 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.limewire.concurrent.ExecutorsHelper;
+import org.limewire.concurrent.FutureEvent;
 import org.limewire.inspection.InspectablePrimitive;
+import org.limewire.listener.EventListener;
 import org.limewire.mojito.MojitoDHT2;
 import org.limewire.mojito.concurrent.ManagedRunnable;
+import org.limewire.mojito.entity.PingEntity;
+import org.limewire.mojito.util.EventUtils;
 
 /**
  * 
@@ -33,6 +38,17 @@ public class DatabasePublisher implements Closeable {
     
     private final ScheduledFuture<?> future;
     
+    /**
+     * 
+     */
+    private final AtomicBoolean barrier 
+        = new AtomicBoolean(true);
+    
+    /**
+     * 
+     */
+    private StoreTask storeTask = null;
+    
     public DatabasePublisher(MojitoDHT2 dht, 
             long frequency, TimeUnit unit) {
         
@@ -50,13 +66,71 @@ public class DatabasePublisher implements Closeable {
     }
     
     @Override
-    public void close() {
+    public synchronized void close() {
         if (future != null) {
             future.cancel(true);
         }
+        
+        if (storeTask != null) {
+            storeTask.close();
+        }
     }
     
-    private void process() {
+    private synchronized void process() {
+        if (barrier.getAndSet(false)) {
+            Runnable callback = new Runnable() {
+                @Override
+                public void run() {
+                    barrier.set(true);
+                }
+            };
+            
+            storeTask = new StoreTask(dht, callback, timeout, unit);
+        }
+    }
+    
+    private static class StoreTask implements Closeable {
         
+        private final EventListener<FutureEvent<PingEntity>> listener 
+                = new EventListener<FutureEvent<PingEntity>>() {
+            @Override
+            public void handleEvent(FutureEvent<PingEntity> event) {
+                doNext();
+            }
+        };
+
+        protected final MojitoDHT2 dht;
+        
+        protected final Runnable callback;
+        
+        protected final long timeout;
+        
+        protected final TimeUnit unit;
+        
+        protected volatile boolean open = true;
+        
+        public StoreTask(MojitoDHT2 dht, Runnable callback, 
+                long timeout, TimeUnit unit) {
+            
+            this.dht = dht;
+            this.callback = callback;
+            this.timeout = timeout;
+            this.unit = unit;
+        }
+        
+        @Override
+        public void close() {
+            open = false;
+        }
+        
+        private void doNext() {
+            if (!open || contacts == null 
+                    || index >= contacts.length) {
+                EventUtils.fireEvent(callback);
+                return;
+            }
+            
+            // DO STORE
+        }
     }
 }
