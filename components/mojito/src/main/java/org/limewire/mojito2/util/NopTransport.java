a7b11ed71690e71af4a32365f6cf8349fadc55b6
rkapsi <rkapsi>
2010-05-03 17:31:13 +0000

    Adding some missing methods

--- components/mojito/src/main/java/org/limewire/mojito2/util/NopTransport.java
+++ components/mojito/src/main/java/org/limewire/mojito2/util/NopTransport.java
@@ -3,21 +3,33 @@ package org.limewire.mojito2.util;
 import java.io.IOException;
 import java.net.SocketAddress;
 
-import org.limewire.mojito2.io.MessageDispatcher;
 import org.limewire.mojito2.io.Transport;
-import org.limewire.mojito2.message.Message;
 
 public class NopTransport implements Transport {
 
     public static final Transport NOP = new NopTransport();
     
     private NopTransport() {}
-    
+
     @Override
-    public void bind(MessageDispatcher messageDispatcher) {
+    public void bind(Callback callback) throws IOException {
     }
 
     @Override
-    public void send(SocketAddress dst, Message message) throws IOException {
+    public boolean isBound() {
+        return true;
+    }
+
+    @Override
+    public void send(SocketAddress dst, byte[] message, 
+            int offset, int length) throws IOException {
+    }
+
+    @Override
+    public void send(SocketAddress dst, byte[] message) throws IOException {
+    }
+
+    @Override
+    public void unbind() {
     }
 }
