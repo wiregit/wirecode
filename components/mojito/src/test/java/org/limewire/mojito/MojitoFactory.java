c77e0faa39c9ab290a0792a58e5b863b8facfdde
rkapsi <rkapsi>
2010-06-01 18:30:01 +0000

    Moving MojitoFactory to the test package.

--- components/mojito/src/test/java/org/limewire/mojito/MojitoFactory.java
+++ components/mojito/src/test/java/org/limewire/mojito/MojitoFactory.java
@@ -11,10 +11,16 @@ import org.limewire.mojito.routing.RouteTable;
 import org.limewire.mojito.routing.RouteTableImpl;
 import org.limewire.mojito.storage.DatabaseImpl;
 
+/**
+ * An utility class to create {@link MojitoDHT} instances.
+ */
 public class MojitoFactory {
     
     private MojitoFactory() {}
     
+    /**
+     * Creates and returns a {@link MojitoDHT} with the given name.
+     */
     public static MojitoDHT createDHT(String name) {
         MessageFactory messageFactory 
             = new DefaultMessageFactory();
@@ -26,16 +32,31 @@ public class MojitoFactory {
                 routeTable, database);
     }
     
+    /**
+     * Creates and returns a {@link MojitoDHT} that's bound to the given
+     * port number.
+     */
     public static MojitoDHT createDHT(String name, int port) throws IOException {
         MojitoDHT dht = createDHT(name);
         bind(dht, port);
         return dht;
     }
     
+    /**
+     * Binds the {@link MojitoDHT} to the given port number.
+     */
     public static Transport bind(MojitoDHT dht, int port) throws IOException {
         Transport transport = new DatagramTransport(port);
         dht.bind(transport);
         
+        // NOTE: We're doing this because we're very often doing 
+        // something like this in our tests:
+        //
+        // dh1.ping(dht2.getContactAddress());
+        //
+        // It's being assumed that dht2 has a working address which 
+        // it doesn't out of the box. To be more precise, it has a
+        // working InetAddress but the port number is 0 (zero).
         dht.setContactAddress(new InetSocketAddress("localhost", port));
         
         return transport;
