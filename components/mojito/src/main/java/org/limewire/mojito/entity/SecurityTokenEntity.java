501945deb8e04498e1abf1103d5c49afc4c3705e
rkapsi <rkapsi>
2010-06-07 21:42:55 +0000

    An another JavaDoc

--- components/mojito/src/main/java/org/limewire/mojito/entity/SecurityTokenEntity.java
+++ components/mojito/src/main/java/org/limewire/mojito/entity/SecurityTokenEntity.java
@@ -4,17 +4,18 @@ import org.limewire.mojito.routing.Contact;
 import org.limewire.security.SecurityToken;
 
 /**
- * 
+ * A {@link SecurityTokenEntity} is a special result of a 
+ * <tt>FIND_NODE</tt> operation.
  */
 public interface SecurityTokenEntity extends Entity {
 
     /**
-     * 
+     * Returns the {@link Contact} of the {@link SecurityToken}.
      */
     public Contact getContact();
     
     /**
-     * 
+     * Returns the {@link Contact}'s {@link SecurityToken}.
      */
     public SecurityToken getSecurityToken();
 }
